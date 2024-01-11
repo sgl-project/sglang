3e61150f9af9b9cc41f70fde38a681665a612223
hnyls2002 <hnyls2002@gmail.com>
2024-01-11 08:20:21 +0000

    bench other

---
+++ benchmark/multi_turns/README.md
@@ -0,0 +1,25 @@
+### Benchmark sglang
+
+```
+python -m sglang.launch_server --model-path meta-llama/Llama-2-7b-chat-hf --port 30000
+```
+
+```
+python bench_sglang.py --tokenizer meta-llama/Llama-2-7b-chat-hf
+```
+
+### Benchmark vLLM
+
+```
+python3 -m vllm.entrypoints.api_server --tokenizer-mode auto --model meta-llama/Llama-2-7b-chat-hf  --disable-log-requests --port 21000 --gpu 0.97
+```
+
+```
+python3 bench_other.py --tokenizer meta-llama/Llama-2-7b-chat-hf --backend vllm
+```
+
+### Benchmark guidance
+
+```
+python3 bench_other.py --tokenizer meta-llama/Llama-2-7b-chat-hf --backend guidance --parallel 1
+```
--- benchmark/multi_turns/bench_other.py
+++ benchmark/multi_turns/bench_other.py
@@ -3,13 +3,12 @@ import random
 import string
 import time
 from argparse import ArgumentParser
+from concurrent.futures import ThreadPoolExecutor
 
-import sglang as sgl
-from sglang.test.test_utils import (
-    add_common_sglang_args_and_parse,
-    select_sglang_backend,
-)
+import requests
+from sglang.test.test_utils import add_common_other_args_and_parse
 from sglang.utils import dump_state_text
+from tqdm import tqdm
 from vllm.transformers_utils.tokenizer import get_tokenizer
 
 
@@ -42,26 +41,71 @@ def main(args):
 
     multi_qas = gen_arguments(args, tokenizer)
 
-    backend = select_sglang_backend(args)
+    # Select backend
+    if args.backend == "vllm":
+        url = f"{args.host}:{args.port}/generate"
 
-    @sgl.function
-    def multi_turns(s, prompt, new_tokens):
+        def generate(prompt, max_tokens, stop=None, temperature=0, url=url, n=1):
+            data = {
+                "prompt": prompt,
+                "temperature": temperature,
+                "max_tokens": max_tokens,
+                "ignore_eos": True,
+                "stop": stop,
+                "stream": False,
+                "n": n,
+            }
+            res = requests.post(url, json=data)
+            assert res.status_code == 200
+            return res.json()["text"][0][len(prompt) :]
+
+    elif args.backend == "guidance":
+        from guidance import gen, models
+
+        model = models.LlamaCpp(
+            "/home/ubuntu/model_weights/Llama-2-7b-chat-hf/ggml-model-f16.gguf",
+            n_gpu_layers=-1,
+            n_ctx=4096,
+        )
+
+        def generate(prompt, max_tokens, stop=None):
+            out = (
+                model
+                + prompt
+                + gen(name="answer", max_tokens=max_tokens, temperature=0, stop=stop)
+            )
+            return out["answer"]
+
+        # warmup
+        generate("Hello!", max_tokens=8, stop=None)
+    else:
+        raise ValueError(f"Invalid backend: {args.backend}")
+
+    def multi_turns(generate, prompt, new_tokens):
+        s = ""
         for _ in range(args.turns):
             s += prompt
-            s += sgl.gen(
-                max_tokens=new_tokens
-                if isinstance(new_tokens, int)
-                else new_tokens.value
-            )
+            s += generate(s, max_tokens=new_tokens)
+        return s
+
+    states = [None] * args.num_qa
+
+    def get_one_answer(i):
+        states[i] = multi_turns(generate=generate, **multi_qas[i])
 
     tic = time.time()
-    states = multi_turns.run_batch(
-        multi_qas, temperature=0, backend=backend, num_threads=args.parallel
-    )
-    for state in states:
-        state.sync()
+    if args.parallel == 1:
+        for i in tqdm(range(len(multi_qas))):
+            get_one_answer(i)
+    else:
+        with ThreadPoolExecutor(args.parallel) as executor:
+            rets = executor.map(get_one_answer, list(range(len(multi_qas))))
+            for _ in rets:
+                pass
+
     latency = time.time() - tic
 
+    # Compute accuracy
     print(f"Latency: {latency:.3f}")
 
     dump_state_text(f"tmp_output_{args.backend}.txt", states)
@@ -90,5 +134,5 @@ if __name__ == "__main__":
     parser.add_argument("--tokenizer", type=str, required=True)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--trust-remote-code", action="store_true")
-    args = add_common_sglang_args_and_parse(parser)
+    args = add_common_other_args_and_parse(parser)
     main(args)
