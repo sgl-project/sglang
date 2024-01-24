71dc5172815b07858d3c6736683bd2b2805a164f
isaac-vidas <80056737+isaac-vidas@users.noreply.github.com>
2024-01-24 17:56:52 -0500

    Handle grayscale images in expand2square
    Fix handling grayscale images in expand2square

--- python/sglang/srt/mm_utils.py
+++ python/sglang/srt/mm_utils.py
@@ -164,10 +164,14 @@ def expand2square(pil_img, background_color):
     if width == height:
         return pil_img
     elif width > height:
+        if pil_img.mode == "L":
+            pil_img = pil_img.convert("RGB")
         result = Image.new(pil_img.mode, (width, width), background_color)
         result.paste(pil_img, (0, (width - height) // 2))
         return result
     else:
+        if pil_img.mode == "L":
+            pil_img = pil_img.convert("RGB")
         result = Image.new(pil_img.mode, (height, height), background_color)
         result.paste(pil_img, ((height - width) // 2, 0))
         return result
