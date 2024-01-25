8e48243818ae98074c61977f9a6d7c4963ea6a5b
comaniac <hao.yu.cody@gmail.com>
2024-01-25 21:18:31 +0000

    Dynamic model class loading

--- python/sglang/srt/models/mixtral.py
+++ python/sglang/srt/models/mixtral.py
@@ -376,3 +376,5 @@ class MixtralForCausalLM(nn.Module):
                 param = params_dict[name]
                 weight_loader = getattr(param, "weight_loader", default_weight_loader)
                 weight_loader(param, loaded_weight)
+
+EntryClass = MixtralForCausalLM
