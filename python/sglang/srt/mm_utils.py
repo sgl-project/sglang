df880dbfa4ce5fece6cf210c1928aa6c29525f9e
isaac-vidas <80056737+isaac-vidas@users.noreply.github.com>
2024-01-24 19:19:19 -0500

    Update mm_utils.py

--- python/sglang/srt/mm_utils.py
+++ python/sglang/srt/mm_utils.py
@@ -163,15 +163,13 @@ def expand2square(pil_img, background_color):
     width, height = pil_img.size
     if width == height:
         return pil_img
-    elif width > height:
-        if pil_img.mode == "L":
-            pil_img = pil_img.convert("RGB")
+    if pil_img.mode == "L":
+        pil_img = pil_img.convert("RGB")
+    if width > height:
         result = Image.new(pil_img.mode, (width, width), background_color)
         result.paste(pil_img, (0, (width - height) // 2))
         return result
     else:
-        if pil_img.mode == "L":
-            pil_img = pil_img.convert("RGB")
         result = Image.new(pil_img.mode, (height, height), background_color)
         result.paste(pil_img, ((height - width) // 2, 0))
         return result
