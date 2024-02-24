aad731e7e405197d1b19c4b7ab1ef000c17ca75a
Cody Yu <hao.yu.cody@gmail.com>
2024-02-23 23:06:17 -0800

    Support outlines > 0.0.31 (#219)

--- python/sglang/srt/constrained/__init__.py
+++ python/sglang/srt/constrained/__init__.py
@@ -1,9 +1,31 @@
+import json
+from typing import Dict, Optional, Union
+
 from outlines.caching import cache as disk_cache
 from outlines.caching import disable_cache
 from outlines.fsm.fsm import RegexFSM
-from outlines.fsm.json_schema import build_regex_from_object
 from outlines.fsm.regex import FSMInfo, make_deterministic_fsm
 from outlines.models.transformers import TransformerTokenizer
+from pydantic import BaseModel
+
+try:
+    from outlines.fsm.json_schema import build_regex_from_object
+except ImportError:
+    # Since outlines 0.0.32, build_regex_from_object is replaced by build_regex_from_schema,
+    # which only accepts string schema as input.
+    from outlines.fsm.json_schema import build_regex_from_schema
+
+    def build_regex_from_object(
+        object: Union[str, BaseModel, Dict], whitespace_pattern: Optional[str] = None
+    ):
+        if isinstance(object, type(BaseModel)):
+            schema = json.dumps(object.model_json_schema())
+        elif isinstance(object, Dict):
+            schema = json.dumps(object)
+        else:
+            schema = object
+        return build_regex_from_schema(schema, whitespace_pattern)
+
 
 __all__ = [
     "RegexFSM",
