2d580e7a8991b77f4bdc940fcc23b70c9fdc6b1e
Lianmin Zheng <lianminzheng@gmail.com>
2024-05-12 08:18:53 -0700

    Fix flashinfer (#430)

--- python/sglang/srt/managers/router/model_rpc.py
+++ python/sglang/srt/managers/router/model_rpc.py
@@ -113,7 +113,8 @@ class ModelRpcServer:
             f"max_prefill_num_token={self.max_prefill_num_token}, "
             f"context_len={self.model_config.context_len}, "
         )
-        logger.info(f"server_args: {server_args.print_mode_args()}")
+        if self.tp_rank == 0:
+            logger.info(f"server_args: {server_args.print_mode_args()}")
 
         # Init cache
         self.tree_cache = RadixCache(disable=server_args.disable_radix_cache)
