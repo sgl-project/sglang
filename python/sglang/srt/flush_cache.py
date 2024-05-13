562b8857d8060905b64a9698d334c6133c024dd9
Lianmin Zheng <lianminzheng@gmail.com>
2024-05-12 20:49:04 -0700

    Improve error handling (#433)

---
+++ python/sglang/srt/flush_cache.py
@@ -0,0 +1,16 @@
+"""
+Usage:
+python3 -m sglang.srt.flush_cache --url http://localhost:30000
+"""
+
+import argparse
+
+import requests
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", type=str, default="http://localhost:30000")
+    args = parser.parse_args()
+
+    response = requests.get(args.url + "/flush_cache")
+    assert response.status_code == 200
