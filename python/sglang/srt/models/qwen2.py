664287b2a787ff774b6ce9529b2a784e304ee38c
Kaichen Zhang - NTU <zhan0564@e.ntu.edu.sg>
2024-05-14 13:17:50 +0800

    [Feat] Add llava qwen, llava mistral (#419)
    Co-authored-by: Bo Li <drluodian@gmail.com>

--- python/sglang/srt/models/qwen2.py
+++ python/sglang/srt/models/qwen2.py
@@ -303,6 +303,8 @@ class Qwen2ForCausalLM(nn.Module):
                 # Skip loading extra bias for GPTQ models.
                 if name.endswith(".bias") and name not in params_dict:
                     continue
+                if name.startswith("model.vision_tower") and name not in params_dict:
+                    continue
                 param = params_dict[name]
                 weight_loader = param.weight_loader
                 weight_loader(param, loaded_weight, shard_id)
@@ -310,6 +312,8 @@ class Qwen2ForCausalLM(nn.Module):
             else:
                 # Skip loading extra bias for GPTQ models.
                 if name.endswith(".bias") and name not in params_dict:
+                    continue
+                if name.startswith("model.vision_tower") and name not in params_dict:
                     continue
                 param = params_dict[name]
                 weight_loader = getattr(param, "weight_loader", default_weight_loader)
