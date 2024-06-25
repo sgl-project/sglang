802c1a75f1602b6ac8b6a79bf9a84a6645431642
Lianmin Zheng <lianminzheng@gmail.com>
2024-06-25 02:34:02 -0700

    fix bench_llama_low_api.py

--- test/srt/model/bench_llama_low_api.py
+++ test/srt/model/bench_llama_low_api.py
@@ -1,258 +1,184 @@
-import multiprocessing as mp
-import time
-from dataclasses import dataclass
+"""
+Usage:
+python bench_llama_low_api.py --model-path TinyLlama/TinyLlama-1.1B-Chat-v0.4 --correct
 
-import torch
+
+
+"""
+
+import argparse
+import dataclasses
+import multiprocessing
+import time
+
+import numpy as np
 import torch.distributed as dist
 
+
+from sglang.srt.hf_transformers_utils import get_tokenizer
+from sglang.srt.managers.controller.infer_batch import Batch, ForwardMode, Req
 from sglang.srt.managers.controller.model_runner import ModelRunner
 from sglang.srt.model_config import ModelConfig
+from sglang.srt.sampling_params import SamplingParams
+from sglang.srt.server_args import ServerArgs
 
 
-@dataclass
-class BenchBatch:
-    req_to_token_pool: torch.Tensor
-    token_to_kv_pool: torch.Tensor
+@dataclasses.dataclass
+class BenchArgs:
+    batch_size: int = 1
+    input_len: int = 128
+    output_len: int = 16
+    cut_len: int = 4
+    correctness_test: bool = False
 
-    input_ids: torch.Tensor = None
-    position_ids_offsets: torch.Tensor = None
-    seq_lens: torch.Tensor = None
-    prefix_lens: torch.Tensor = None
-    req_pool_indices: torch.Tensor = None
-    out_cache_loc: torch.Tensor = None
-    out_cache_cont_start: torch.Tensor = None
-    out_cache_cont_end: torch.Tensor = None
+    @staticmethod
+    def add_cli_args(parser: argparse.ArgumentParser):
+        parser.add_argument("--batch-size", type=int, default=BenchArgs.batch_size)
+        parser.add_argument("--input-len", type=int, default=BenchArgs.input_len)
+        parser.add_argument("--output-len", type=int, default=BenchArgs.output_len)
+        parser.add_argument("--cut-len", type=int, default=BenchArgs.cut_len)
+        parser.add_argument("--correctness-test", action="store_true")
 
-    def __init__(self, model_runner: ModelRunner):
-        self.req_to_token_pool = model_runner.req_to_token_pool
-        self.token_to_kv_pool = model_runner.token_to_kv_pool
-
-    def init_prefill_batch(self, input_ids, batch_size, seq_len):
-        self.input_ids = input_ids
-        self.position_ids_offsets = torch.zeros(
-            batch_size, dtype=torch.int32, device="cuda"
-        )
-        self.seq_lens = torch.full(
-            (batch_size,), seq_len, dtype=torch.int32, device="cuda"
-        )
-        self.prefix_lens = torch.zeros(batch_size, dtype=torch.int32, device="cuda")
-        self.req_pool_indices = self.req_to_token_pool.alloc(batch_size)
-        self.out_cache_loc = self.token_to_kv_pool.alloc(batch_size * seq_len)
-
-        for i in range(batch_size):
-            n_idx = self.req_pool_indices[i].item()
-            self.req_to_token_pool.req_to_token[n_idx, :seq_len] = self.out_cache_loc[
-                i * seq_len : (i + 1) * seq_len
-            ]
-
-    def update_extend(
-        self, input_ids, batch_size, prefix_len, extend_len, prefix_req_idx
-    ):
-        self.input_ids = input_ids
-        self.position_ids_offsets = torch.zeros(
-            batch_size, dtype=torch.int32, device="cuda"
-        )
-        self.seq_lens = torch.full(
-            (batch_size,), prefix_len + extend_len, dtype=torch.int32, device="cuda"
-        )
-        self.prefix_lens = torch.full(
-            (batch_size,), prefix_len, dtype=torch.int32, device="cuda"
-        )
-        self.req_pool_indices = self.req_to_token_pool.alloc(batch_size)
-        self.out_cache_loc = self.token_to_kv_pool.alloc(batch_size * extend_len)
-
-        req_to_token = self.req_to_token_pool.req_to_token
-        fork_num = batch_size // prefix_req_idx.shape[0]
-        for i in range(batch_size):
-            p_idx = prefix_req_idx[i // fork_num].item()
-            n_idx = self.req_pool_indices[i].item()
-            req_to_token[n_idx, :prefix_len] = req_to_token[p_idx, :prefix_len]
-            req_to_token[n_idx, prefix_len : prefix_len + extend_len] = (
-                self.out_cache_loc[i * extend_len : (i + 1) * extend_len]
-            )
-
-    def update_decode(self, predict_ids, batch_size):
-        assert predict_ids.shape[0] == batch_size
-        assert batch_size == self.req_pool_indices.shape[0]
-
-        self.input_ids = predict_ids.reshape(-1)
-        self.prefix_lens = None
-        (
-            self.out_cache_loc,
-            self.out_cache_cont_start,
-            self.out_cache_cont_end,
-        ) = self.token_to_kv_pool.alloc_contiguous(batch_size)
-        self.req_to_token_pool.req_to_token[self.req_pool_indices, self.seq_lens] = (
-            self.out_cache_loc
-        )
-        self.seq_lens.add_(1)
+    @classmethod
+    def from_cli_args(cls, args: argparse.Namespace):
+        attrs = [attr.name for attr in dataclasses.fields(cls)]
+        return cls(**{attr: getattr(args, attr) for attr in attrs})
 
 
-def prefill(model_runner: ModelRunner, batch: BenchBatch):
-    logits, _ = model_runner.forward_extend(
-        batch.input_ids,
-        batch.req_pool_indices,
-        batch.seq_lens,
-        batch.prefix_lens,
-        batch.position_ids_offsets,
-        batch.out_cache_loc,
-        False,
-    )
-
-    prob_out = torch.softmax(logits, dim=-1)
-    predict_ids = torch.argmax(prob_out, dim=1, keepdim=True)
-    predict_ids = predict_ids.detach().cpu().numpy()
-
-    return predict_ids
-
-
-def extend(model_runner: ModelRunner, batch: BenchBatch):
-    logits, _ = model_runner.forward_extend(
-        batch.input_ids,
-        batch.req_pool_indices,
-        batch.seq_lens,
-        batch.prefix_lens,
-        batch.position_ids_offsets,
-        batch.out_cache_loc,
-        True,
-    )
-
-    prob_out = torch.softmax(logits, dim=-1)
-    predict_ids = torch.argmax(prob_out, dim=1, keepdim=True)
-    predict_ids = predict_ids.detach().cpu().numpy()
-
-    return predict_ids
-
-
-def decode(model_runner: ModelRunner, batch: BenchBatch):
-    logits = model_runner.forward_decode(
-        batch.input_ids,
-        batch.req_pool_indices,
-        batch.seq_lens,
-        None,
-        batch.position_ids_offsets,
-        None,
-        batch.out_cache_cont_start,
-        batch.out_cache_cont_end,
-    )
-
-    prob_out = torch.softmax(logits, dim=-1)
-    predict_ids = torch.argmax(prob_out, dim=1, keepdim=True)
-    predict_ids = predict_ids.detach().cpu().numpy()
-
-    return predict_ids
-
-
-def bench_generate_worker(
-    model_path,
-    tp_rank,
-    tp_size,
-    shared_num,
-    unique_num,
-    shared_len,
-    unique_len,
-    decode_len,
-    server_args_dict,
-):
-    assert unique_num % shared_num == 0
-
-    model_config = ModelConfig(path=model_path)
+def load_model(server_args, tp_rank):
+    model_config = ModelConfig(path=server_args.model_path)
     model_runner = ModelRunner(
         model_config=model_config,
-        mem_fraction_static=0.8,
+        mem_fraction_static=server_args.mem_fraction_static,
+        gpu_id=tp_rank,
         tp_rank=tp_rank,
-        tp_size=tp_size,
+        tp_size=server_args.tp_size,
         nccl_port=28888,
-        server_args_dict=server_args_dict,
+        server_args=server_args,
     )
-
-    batch = BenchBatch(model_runner)
-
-    # warm up
-    for _ in range(1):
-        input_ids = torch.randint(
-            low=5, high=100, size=(shared_num * shared_len,)
-        ).cuda()
-        batch.init_prefill_batch(input_ids, shared_num, shared_len)
-        _ = prefill(model_runner, batch)
-
-        input_ids = torch.randint(
-            low=5, high=100, size=(unique_num * unique_len,)
-        ).cuda()
-        batch.update_extend(
-            input_ids, unique_num, shared_len, unique_len, batch.req_pool_indices
-        )
-        predict_ids = extend(model_runner, batch)
-
-        for i in range(decode_len):
-            predict_ids = torch.from_numpy(predict_ids).cuda()
-            batch.update_decode(predict_ids, unique_num)
-            predict_ids = decode(model_runner, batch)
-
-        model_runner.req_to_token_pool.clear()
-        model_runner.token_to_kv_pool.clear()
-
-    if tp_size > 1:
+    tokenizer = get_tokenizer(
+        server_args.tokenizer_path,
+        tokenizer_mode=server_args.tokenizer_mode,
+        trust_remote_code=server_args.trust_remote_code,
+    )
+    if server_args.tp_size > 1:
         dist.barrier()
+    return model_runner, tokenizer
 
-    prefill_start = time.time()
-    input_ids = torch.randint(low=5, high=100, size=(shared_num * shared_len,)).cuda()
-    batch.init_prefill_batch(input_ids, shared_num, shared_len)
-    _ = prefill(model_runner, batch)
-    if tp_rank == 0:
-        print(f"prefill: {(time.time() - prefill_start) * 1000:.2f} ms")
 
-    extend_start = time.time()
-    input_ids = torch.randint(low=5, high=100, size=(unique_num * unique_len,)).cuda()
-    batch.update_extend(
-        input_ids, unique_num, shared_len, unique_len, batch.req_pool_indices
+def prepare_inputs(bench_args, tokenizer):
+    prompts = [
+        "The capital of France is",
+        "The capital of the United Kindom is",
+        "Today is a sunny day and I like",
+    ]
+    input_ids = [tokenizer.encode(p) for p in prompts]
+    sampling_params = SamplingParams(
+        temperature=0,
+        max_new_tokens=BenchArgs.output_len,
     )
-    predict_ids = extend(model_runner, batch)
-    if tp_rank == 0:
-        print(f"extend: {(time.time() - extend_start) * 1000:.2f} ms")
 
-    for i in range(decode_len):
-        decode_start = time.time()
-        predict_ids = torch.from_numpy(predict_ids).cuda()
-        batch.update_decode(predict_ids, unique_num)
-        predict_ids = decode(model_runner, batch)
-        if tp_rank == 0:
-            print(f"decode {i}: {(time.time() - decode_start) * 1000:.2f} ms")
+    reqs = []
+    for i in range(len(prompts)):
+        assert len(input_ids[i]) > bench_args.cut_len
+
+        tmp_input_ids = input_ids[i][:bench_args.cut_len]
+        req = Req(rid=i, origin_input_text=prompts[i], origin_input_ids=tmp_input_ids)
+        req.prefix_indices = []
+        req.sampling_params = sampling_params
+        req.input_ids = req.origin_input_ids
+        reqs.append(req)
+
+    return input_ids, reqs
 
 
-def bench_generate(
-    model_path,
-    tp_size,
-    shared_num,
-    unique_num,
-    shared_len,
-    unique_len,
-    decode_len,
-    server_args_dict,
+def prepare_extend_inputs(bench_args, input_ids, reqs, model_runner):
+    for i in range(len(reqs)):
+        req = reqs[i]
+        req.input_ids += input_ids[i][bench_args.cut_len:]
+        req.prefix_indices = model_runner.req_to_token_pool.req_to_token[
+            i, :bench_args.cut_len
+        ]
+    return reqs
+
+
+def extend(reqs, model_runner):
+    batch = Batch.init_new(
+        reqs=reqs,
+        req_to_token_pool=model_runner.req_to_token_pool,
+        token_to_kv_pool=model_runner.token_to_kv_pool,
+        tree_cache=None)
+    batch.prepare_for_extend(model_runner.model_config.vocab_size, None)
+    output = model_runner.forward(batch, ForwardMode.EXTEND)
+    next_token_ids, _ = batch.sample(output.next_token_logits)
+    return next_token_ids, output.next_token_logits, batch
+
+
+def decode(input_token_ids, batch, model_runner):
+    batch.prepare_for_decode(input_token_ids.cpu().numpy())
+    output = model_runner.forward(batch, ForwardMode.DECODE)
+    next_token_ids, _ = batch.sample(output.next_token_logits)
+    return next_token_ids, output.next_token_logits
+
+
+def correctness_test(
+    server_args,
+    bench_args,
+    tp_rank,
 ):
-    print(
-        f"tp_size: {tp_size}, "
-        f"shared_num: {shared_num}, "
-        f"unique_num: {unique_num}, "
-        f"shared_len: {shared_len}, "
-        f"unique_len: {unique_len}, "
-        f"decode_len: {decode_len}, "
-        f"server_args: {server_args_dict}"
-    )
+    # Load the model
+    model_runner, tokenizer = load_model(server_args, tp_rank)
+
+    # Prepare inputs
+    input_ids, reqs = prepare_inputs(bench_args, tokenizer)
+
+    # Prefill
+    next_token_ids, next_token_logits, batch = extend(reqs, model_runner)
+    print("prefill logits (first half)", next_token_logits)
+
+    # Prepare extend inputs
+    reqs = prepare_extend_inputs(bench_args, input_ids, reqs, model_runner)
+
+    # Extend
+    next_token_ids, next_token_logits, batch = extend(reqs, model_runner)
+    print("prefill logits (final)", next_token_logits)
+
+    # Decode
+    output_ids = [list(req.input_ids) for req in reqs]
+    for _ in range(bench_args.output_len):
+        next_token_ids, _ = decode(next_token_ids, batch, model_runner)
+        for i in range(len(reqs)):
+            output_ids[i].append(next_token_ids[i])
+
+    # Print
+    for i in range(len(reqs)):
+        print(tokenizer.decode(output_ids[i]))
+
+
+def latency_test(
+    server_args,
+    bench_args,
+    tp_rank,
+):
+    pass
+
+
+def main(server_args, bench_args):
+    print(bench_args)
+
+    if bench_args.correctness_test:
+        work_func = correctness_test
+    else:
+        work_func = latency_test
+
     workers = []
-    for tp_rank in range(tp_size):
-        proc = mp.Process(
-            target=bench_generate_worker,
+    for tp_rank in range(server_args.tp_size):
+        proc = multiprocessing.Process(
+            target=work_func,
             args=(
-                model_path,
+                server_args,
+                bench_args,
                 tp_rank,
-                tp_size,
-                shared_num,
-                unique_num,
-                shared_len,
-                unique_len,
-                decode_len,
-                server_args_dict,
             ),
         )
         proc.start()
@@ -263,13 +189,12 @@ def bench_generate(
 
 
 if __name__ == "__main__":
-    bench_generate(
-        model_path="meta-llama/Llama-2-7b-chat-hf",
-        tp_size=1,
-        shared_num=1,
-        unique_num=32,
-        shared_len=256,
-        unique_len=256,
-        decode_len=8,
-        server_args_dict={},
-    )
+    parser = argparse.ArgumentParser()
+    ServerArgs.add_cli_args(parser)
+    BenchArgs.add_cli_args(parser)
+    args = parser.parse_args()
+
+    server_args = ServerArgs.from_cli_args(args)
+    bench_args = BenchArgs.from_cli_args(args)
+
+    main(server_args, bench_args)
