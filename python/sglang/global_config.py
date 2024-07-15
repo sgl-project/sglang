5ac8b80677614a9c024740e94f9a087a39eb3499
Mingyi <wisclmy0611@gmail.com>
2024-07-15 02:01:09 -0700

    Simplify mem state (#623)

--- python/sglang/global_config.py
+++ python/sglang/global_config.py
@@ -25,7 +25,8 @@ class GlobalConfig:
         # This can improve the speed for large batch sizes during prefill.
         self.layer_sync_threshold = 8192
 
-        # Runtime constants: Flashinfer
+        # Runtime constants: others
+        self.num_continue_decode_steps = 10
         self.flashinfer_workspace_size = 192 * 1024 * 1024
 
         # Output tokenization configs
