476584cb6e1c4535e09e2439ff139357ca78477a
Ying Sheng <sqy1415@gmail.com>
2024-07-17 15:44:41 -0700

    Increase the capacity of the memory pool (#643)

--- python/sglang/srt/managers/controller/model_runner.py
+++ python/sglang/srt/managers/controller/model_runner.py
@@ -9,6 +9,12 @@ from typing import Optional, Type
 
 import torch
 import torch.nn as nn
+from flashinfer import (
+    BatchDecodeWithPagedKVCacheWrapper,
+    BatchPrefillWithPagedKVCacheWrapper,
+    BatchPrefillWithRaggedKVCacheWrapper,
+)
+from flashinfer.decode import _grouped_size_compiled_for_decode_kernels
 from vllm.config import DeviceConfig, LoadConfig
 from vllm.config import ModelConfig as VllmModelConfig
 from vllm.distributed import (
@@ -162,7 +168,7 @@ class ModelRunner:
             )
 
         self.req_to_token_pool = ReqToTokenPool(
-            int(self.max_total_num_tokens / self.model_config.context_len * 256),
+            max(int(self.max_total_num_tokens / self.model_config.context_len * 512), 2048),
             self.model_config.context_len + 8,
         )
         self.token_to_kv_pool = TokenToKVPool(
@@ -192,13 +198,6 @@ class ModelRunner:
             self.flashinfer_prefill_wrapper_paged = None
             self.flashinfer_decode_wrapper = None
             return
-
-        from flashinfer import (
-            BatchDecodeWithPagedKVCacheWrapper,
-            BatchPrefillWithPagedKVCacheWrapper,
-            BatchPrefillWithRaggedKVCacheWrapper,
-        )
-        from flashinfer.decode import _grouped_size_compiled_for_decode_kernels
 
         if not _grouped_size_compiled_for_decode_kernels(
             self.model_config.num_attention_heads // self.tp_size,
