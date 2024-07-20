0355b48c0fad9814be85385f3c014772618a19f8
Lianmin Zheng <lianminzheng@gmail.com>
2024-07-20 03:01:30 -0700

    fix io_struct

--- python/sglang/srt/managers/io_struct.py
+++ python/sglang/srt/managers/io_struct.py
@@ -14,7 +14,7 @@ from sglang.srt.sampling_params import SamplingParams
 @dataclass
 class GenerateReqInput:
     # The input prompt. It can be a single prompt or a batch of prompts.
-    text: Union[List[str], str]
+    text: Optional[Union[List[str], str]] = None
     # The token ids for text; one can either specify text or input_ids.
     input_ids: Optional[Union[List[List[int]], List[int]]] = None
     # The image input. It can be a file name, a url, or base64 encoded string.
