f52eda35ea4dae83a4ada499d0a752252e04b938
Yineng Zhang <me@zhyncs.com>
2024-07-30 19:19:23 +1000

    misc: update e2e test benchmark config (#825)

--- python/sglang/bench_serving.py
+++ python/sglang/bench_serving.py
@@ -1,5 +1,6 @@
 # Adapted from https://github.com/vllm-project/vllm/blob/6366efc67b0aedd2c1721c14385370e50b297fb3/benchmarks/backend_request_func.py
 # Adapted from https://github.com/vllm-project/vllm/blob/6366efc67b0aedd2c1721c14385370e50b297fb3/benchmarks/benchmark_serving.py
+
 """
 Benchmark online serving.
 
