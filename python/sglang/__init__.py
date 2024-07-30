db6089e6f341878080e645860a75766fea6207db
Ying Sheng <sqy1415@gmail.com>
2024-07-29 19:40:28 -0700

    Revert "Organize public APIs" (#815)

--- python/sglang/__init__.py
+++ python/sglang/__init__.py
@@ -22,53 +22,46 @@ from sglang.api import (
     video,
 )
 
-# SGLang DSL APIs
-__all__ = [
-    "Runtime",
-    "assistant",
-    "assistant_begin",
-    "assistant_end",
-    "flush_cache",
-    "function",
-    "gen",
-    "gen_int",
-    "gen_string",
-    "get_server_args",
-    "image",
-    "select",
-    "set_default_backend",
-    "system",
-    "system_begin",
-    "system_end",
-    "user",
-    "user_begin",
-    "user_end",
-    "video",
-]
-
-
 # Global Configurations
 from sglang.global_config import global_config
-
-__all__ += ["global_config"]
 
 # SGL Backends
 from sglang.lang.backend.runtime_endpoint import RuntimeEndpoint
 from sglang.utils import LazyImport
+from sglang.version import __version__
 
 Anthropic = LazyImport("sglang.lang.backend.anthropic", "Anthropic")
 LiteLLM = LazyImport("sglang.lang.backend.litellm", "LiteLLM")
 OpenAI = LazyImport("sglang.lang.backend.openai", "OpenAI")
 VertexAI = LazyImport("sglang.lang.backend.vertexai", "VertexAI")
 
-__all__ += ["RuntimeEndpoint", "Anthropic", "LiteLLM", "OpenAI", "VertexAI"]
 
-# Version
-from sglang.version import __version__
-
-__all__ += ["__version__"]
-
-# Core Benchmarks
-from sglang.benchmarks import bench_latency, bench_serving
-
-__all__ += ["bench_latency", "bench_serving"]
+# public APIs management
+__all__ = [
+    "global_config",
+    "Anthropic",
+    "LiteLLM",
+    "OpenAI",
+    "RuntimeEndpoint",
+    "VertexAI",
+    "function",
+    "Runtime",
+    "set_default_backend",
+    "flush_cache",
+    "get_server_args",
+    "gen",
+    "gen_int",
+    "gen_string",
+    "image",
+    "video",
+    "select",
+    "system",
+    "user",
+    "assistant",
+    "user_begin",
+    "user_end",
+    "assistant_begin",
+    "assistant_end",
+    "system_begin",
+    "system_end",
+]
--- python/sglang/benchmarks/__init__.py
+++
@@ -1,1 +0,0 @@
-"""SGLang core benchmarks."""
