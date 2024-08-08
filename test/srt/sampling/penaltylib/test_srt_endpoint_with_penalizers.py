7a01b343e2aa42fd62b1e94c0ad4bb0f98e1a0c2
ryan <ryan@tmfi.us>
2024-08-08 03:20:33 -0700

    avoid using localhost in test_srt_endpoint_with_penalizers

--- test/srt/sampling/penaltylib/test_srt_endpoint_with_penalizers.py
+++ test/srt/sampling/penaltylib/test_srt_endpoint_with_penalizers.py
@@ -12,7 +12,7 @@ class TestBatchPenalizerE2E(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.model = DEFAULT_MODEL_NAME_FOR_TEST
-        cls.base_url = f"http://localhost:{8157}"
+        cls.base_url = f"http://127.0.0.1:{8157}"
         cls.process = popen_launch_server(
             cls.model,
             cls.base_url,
