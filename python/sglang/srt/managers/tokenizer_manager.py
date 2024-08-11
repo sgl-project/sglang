b273e7fadabcea7ca1e635b3ec08ac41310633d6
yichuan520030910320 <yichuanjiaoda@sjtu.edu.cn>
2024-08-11 19:04:22 +0000

    fix strat len of logprobs

--- python/sglang/srt/managers/tokenizer_manager.py
+++ python/sglang/srt/managers/tokenizer_manager.py
@@ -195,6 +195,8 @@ class TokenizerManager:
                     if not_use_index
                     else obj.logprob_start_len[index]
                 )
+                if return_logprob and logprob_start_len == 0:
+                    logprob_start_len = len(input_ids)
                 top_logprobs_num = (
                     obj.top_logprobs_num
                     if not_use_index
@@ -242,6 +244,8 @@ class TokenizerManager:
             )
             return_logprob = obj.return_logprob[0]
             logprob_start_len = obj.logprob_start_len[0]
+            if return_logprob and logprob_start_len == 0:
+                logprob_start_len = len(input_ids)
             top_logprobs_num = obj.top_logprobs_num[0]
 
         if self.is_generation:
@@ -331,6 +335,8 @@ class TokenizerManager:
                     else:
                         input_text = None
                         input_ids = obj.input_ids[i]
+                if obj.return_logprob[index] and obj.logprob_start_len[index] == 0:
+                    obj.logprob_start_len[index] = len(input_ids)
                 sampling_params = self._get_sampling_params(obj.sampling_params[index])
 
                 if self.is_generation:
