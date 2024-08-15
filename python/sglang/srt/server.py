326df4bab25583eb1dcfaaf0f5f1f28b20d35ae7
Lianmin Zheng <lianminzheng@gmail.com>
2024-08-14 19:25:37 -0700

    Use a single workspace for flashinfer (#1077)

--- python/sglang/srt/server.py
+++ python/sglang/srt/server.py
@@ -381,7 +381,7 @@ def _set_envs_and_config(server_args: ServerArgs):
     if not server_args.disable_flashinfer:
         assert_pkg_version(
             "flashinfer",
-            "0.1.4",
+            "0.1.5",
             "Please uninstall the old version and "
             "reinstall the latest version by following the instructions "
             "at https://docs.flashinfer.ai/installation.html.",
