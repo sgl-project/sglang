621c719e8b950e62e800eb9e55a1274a9ff1ffb5
kcz358 <kaichenzhang358@outlook.com>
2024-08-16 03:19:29 +0000

    Bring test multi-images server from onevision

---
+++ test/srt/test_multi_image_openai_server.py
@@ -0,0 +1,97 @@
+import base64
+import io
+import sys
+
+import numpy as np
+import openai
+from decord import VideoReader, cpu
+from PIL import Image
+
+client = openai.Client(api_key="EMPTY", base_url="http://127.0.0.1:30000/v1")
+
+
+request_1 = client.chat.completions.create(
+    model="default",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://raw.githubusercontent.com/sgl-project/sglang/main/assets/logo.png"
+                    },
+                },
+                {
+                    "type": "text",
+                    "text": "Please describe this image. Please list the benchmarks and the models.",
+                },
+            ],
+        },
+    ],
+    temperature=0.7,
+    max_tokens=1024,
+    stream=True,
+)
+response_1 = ""
+
+
+for chunk in request_1:
+    if chunk.choices[0].delta.content is not None:
+        content = chunk.choices[0].delta.content
+        response_1 += content
+        sys.stdout.write(content)
+        sys.stdout.flush()
+
+print()  # Add a newline at the end of the stream
+
+video_path = "./assets/jobs.mp4"
+max_frames_num = 32
+vr = VideoReader(video_path, ctx=cpu(0))
+total_frame_num = len(vr)
+uniform_sampled_frames = np.linspace(0, total_frame_num - 1, max_frames_num, dtype=int)
+frame_idx = uniform_sampled_frames.tolist()
+frames = vr.get_batch(frame_idx).asnumpy()
+# to pil and then base64
+base64_frames = []
+for frame in frames:
+    pil_img = Image.fromarray(frame)
+    buff = io.BytesIO()
+    pil_img.save(buff, format="JPEG")
+    base64_str = base64.b64encode(buff.getvalue()).decode("utf-8")
+    base64_frames.append(base64_str)
+
+messages = [
+    {
+        "role": "user",
+        "content": [],
+    },
+]
+# frame format
+frame_format = {
+    "type": "image_url",
+    "image_url": {"url": "data:image/jpeg;base64,{}"},
+}
+
+for base64_frame in base64_frames:
+    frame_format["image_url"]["url"] = "data:image/jpeg;base64,{}".format(base64_frame)
+    messages[0]["content"].append(frame_format)
+prompt = {"type": "text", "text": "What is the setting of the video?"}
+messages[0]["content"].append(prompt)
+
+request_3 = client.chat.completions.create(
+    model="default",
+    messages=messages,
+    temperature=0.7,
+    max_tokens=1024,
+    stream=True,
+)
+print("-" * 30)
+response_3 = ""
+
+for chunk in request_3:
+    if chunk.choices[0].delta.content is not None:
+        content = chunk.choices[0].delta.content
+        response_3 += content
+        sys.stdout.write(content)
+        sys.stdout.flush()
