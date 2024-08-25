35b13abc532f5ca4d2dd2a366e757292212ced77
Ying Sheng <sqy1415@gmail.com>
2024-08-25 16:21:37 -0700

    [CI] Fix the issue of unit test hanging (#1211)

--- test/srt/models/test_embedding_models.py
+++ test/srt/models/test_embedding_models.py
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import multiprocessing as mp
 import unittest
 
 import torch
@@ -71,4 +72,9 @@ class TestEmbeddingModels(unittest.TestCase):
 
 
 if __name__ == "__main__":
+    try:
+        mp.set_start_method("spawn")
+    except RuntimeError:
+        pass
+
     unittest.main(warnings="ignore")
