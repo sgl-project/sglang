fa4c72dbac7ae5f444efba924d0468e697a7da01
김종곤 <149566442+Deepfocused@users.noreply.github.com>
2024-08-30 16:37:38 +0900

    Update sqlang/srt/configs/exaone.py
    add Copyright 2023-2024 SGLang Team

--- python/sglang/srt/configs/exaone.py
+++ python/sglang/srt/configs/exaone.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 # Copyright 2021 The LG AI Research EXAONE Lab. All rights reserved.
+# Copyright 2023-2024 SGLang Team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
