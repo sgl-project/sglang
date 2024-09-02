4e7182e89baadd25fcec68aafeed082716f8d1b6
Jani Monoses <jani.monoses@gmail.com>
2024-09-02 09:14:08 +0300

    Load Phi-3 models

--- python/sglang/srt/models/llama.py
+++ python/sglang/srt/models/llama.py
@@ -324,11 +324,11 @@ class LlamaForCausalLM(nn.Module):
     def load_weights(self, weights: Iterable[Tuple[str, torch.Tensor]]):
         stacked_params_mapping = [
             # (param_name, shard_name, shard_id)
-            ("qkv_proj", "q_proj", "q"),
-            ("qkv_proj", "k_proj", "k"),
-            ("qkv_proj", "v_proj", "v"),
-            ("gate_up_proj", "gate_proj", 0),
-            ("gate_up_proj", "up_proj", 1),
+            (".qkv_proj", ".q_proj", "q"),
+            (".qkv_proj", ".k_proj", "k"),
+            (".qkv_proj", ".v_proj", "v"),
+            (".gate_up_proj", ".gate_proj", 0),
+            (".gate_up_proj", ".up_proj", 1),
         ]
         params_dict = self.param_dict
 
@@ -363,3 +363,4 @@ class LlamaForCausalLM(nn.Module):
 
 
 EntryClass = LlamaForCausalLM
+EntryClassRemapping = [("Phi3ForCausalLM", LlamaForCausalLM)]
