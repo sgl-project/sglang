d9ac68ea3b90eb0b75f381eef1e251207ef3369b
Lianmin Zheng <lianminzheng@gmail.com>
2024-09-02 21:02:37 -0700

    fix

--- python/sglang/srt/models/llama_classification.py
+++ python/sglang/srt/models/llama_classification.py
@@ -106,7 +106,7 @@ class LlamaForClassification(nn.Module):
             elif "lm_head" in name:
                 continue
             else:
-                LlamaForCausalLM.load_weights(self, (name, loaded_weight))
+                LlamaForCausalLM.load_weights(self, [(name, loaded_weight)])
 
 
 EntryClass = LlamaForClassification
