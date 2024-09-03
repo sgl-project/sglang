1b26358cbc560bc4bdf86e1772822aa95de765cd
Lianmin Zheng <lianminzheng@gmail.com>
2024-09-02 20:39:22 -0700

    Fix

--- python/sglang/srt/models/llava.py
+++ python/sglang/srt/models/llava.py
@@ -394,6 +394,7 @@ class LlavaBaseForCausalLM(nn.Module):
         projector_weights = {
             "model.mm_projector.0": "multi_modal_projector.linear_1",
             "model.mm_projector.2": "multi_modal_projector.linear_2",
+            "model.image_newline": "language_model.model.image_newline",  # Update the vision tower weights if we find them in the checkpoint (it may be finetuned).
             "model.vision_tower.vision_tower": "vision_tower",  # Update the vision tower weights if we find them in the checkpoint (it may be finetuned).
         }
         params_dict = dict(self.named_parameters())
