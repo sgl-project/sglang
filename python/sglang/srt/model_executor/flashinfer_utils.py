1127ba338cb5bf597cb2b86cefa2d9146a751cb0
hnyls2002 <hnyls2002@gmail.com>
2024-09-10 23:18:00 +0000

    update window indices

--- python/sglang/srt/model_executor/flashinfer_utils.py
+++ python/sglang/srt/model_executor/flashinfer_utils.py
@@ -39,89 +39,6 @@ def create_flashinfer_kv_indices_triton(
         st_offset += BLOCK_SIZE
 
 
-def update_flashinfer_indices_window(
-    forward_mode,
-    model_runner,
-    req_pool_indices,
-    seq_lens,
-    prefix_lens,
-    flashinfer_decode_wrapper=None,
-    flashinfer_use_ragged=False,
-):
-    """Init auxiliary variables for FlashInfer attention backend."""
-    num_qo_heads = model_runner.model_config.num_attention_heads // model_runner.tp_size
-    num_kv_heads = model_runner.model_config.get_num_kv_heads(model_runner.tp_size)
-    head_dim = model_runner.model_config.head_dim
-    batch_size = len(req_pool_indices)
-    # window attention use paged only
-    kv_last_page_len = torch.ones((batch_size,), dtype=torch.int32, device="cuda")
-    for wrapper_id in range(2):
-        if wrapper_id == 0:
-            if forward_mode.is_decode():
-                paged_kernel_lens = torch.minimum(
-                    seq_lens, torch.tensor(model_runner.sliding_window_size + 1)
-                )
-            else:
-                paged_kernel_lens = torch.minimum(
-                    seq_lens,
-                    torch.tensor(model_runner.sliding_window_size)
-                    + seq_lens
-                    - prefix_lens,
-                )
-        else:
-            paged_kernel_lens = seq_lens
-
-        kv_start_idx = seq_lens - paged_kernel_lens
-
-        kv_indptr = torch.zeros((batch_size + 1,), dtype=torch.int32, device="cuda")
-        kv_indptr[1:] = torch.cumsum(paged_kernel_lens, dim=0)
-
-        kv_indices = torch.empty(kv_indptr[-1], dtype=torch.int32, device="cuda")
-        create_flashinfer_kv_indices_triton[(batch_size,)](
-            model_runner.req_to_token_pool.req_to_token,
-            req_pool_indices,
-            paged_kernel_lens,
-            kv_indptr,
-            kv_start_idx,
-            model_runner.req_to_token_pool.req_to_token.size(1),
-            kv_indices,
-        )
-
-        if forward_mode.is_decode():
-            # CUDA graph uses different flashinfer_decode_wrapper
-            if flashinfer_decode_wrapper is None:
-                flashinfer_decode_wrapper = model_runner.flashinfer_decode_wrapper
-
-            flashinfer_decode_wrapper[wrapper_id].end_forward()
-            flashinfer_decode_wrapper[wrapper_id].begin_forward(
-                kv_indptr,
-                kv_indices,
-                kv_last_page_len,
-                num_qo_heads,
-                num_kv_heads,
-                head_dim,
-                1,
-                data_type=model_runner.kv_cache_dtype,
-                q_data_type=model_runner.dtype,
-            )
-        else:
-            # extend part
-            qo_indptr = torch.zeros((batch_size + 1,), dtype=torch.int32, device="cuda")
-            qo_indptr[1:] = torch.cumsum(seq_lens - prefix_lens, dim=0)
-
-            model_runner.flashinfer_prefill_wrapper_paged[wrapper_id].end_forward()
-            model_runner.flashinfer_prefill_wrapper_paged[wrapper_id].begin_forward(
-                qo_indptr,
-                kv_indptr,
-                kv_indices,
-                kv_last_page_len,
-                num_qo_heads,
-                num_kv_heads,
-                head_dim,
-                1,
-            )
-
-
 def update_flashinfer_indices(
     forward_mode,
     model_runner,
@@ -163,7 +80,6 @@ class FlashinferUpdater:
         self.req_pool_indices = req_pool_indices
         self.seq_lens = seq_lens
         self.prefix_lens = prefix_lens
-        self.flashinfer_decode_wrapper = flashinfer_decode_wrapper
         self.flashinfer_use_ragged = flashinfer_use_ragged
 
         self.num_qo_heads = (
@@ -175,40 +91,86 @@ class FlashinferUpdater:
         self.head_dim = model_runner.model_config.head_dim
         self.batch_size = len(req_pool_indices)
 
-        if flashinfer_use_ragged:
-            self.paged_kernel_lens = prefix_lens
+        self.kv_last_page_len = torch.ones(
+            (self.batch_size,), dtype=torch.int32, device="cuda"
+        )
+
+        (
+            self.flashinfer_decode_wrapper,
+            self.flashinfer_prefill_wrapper_ragged,
+            self.flashinfer_prefill_wrapper_paged,
+        ) = (
+            flashinfer_decode_wrapper,
+            self.model_runner.flashinfer_prefill_wrapper_ragged,
+            self.model_runner.flashinfer_prefill_wrapper_paged,
+        )
+        # CUDA graph uses different flashinfer_decode_wrapper
+        if self.flashinfer_decode_wrapper is None:
+            self.flashinfer_decode_wrapper = self.model_runner.flashinfer_decode_wrapper
+
+    def _init_indices_no_window(self):
+        if self.flashinfer_use_ragged:
+            paged_kernel_lens = self.prefix_lens
         else:
-            self.paged_kernel_lens = seq_lens
+            paged_kernel_lens = self.seq_lens
 
         self.kv_indptr = torch.zeros(
             (self.batch_size + 1,), dtype=torch.int32, device="cuda"
         )
-        self.kv_indptr[1:] = torch.cumsum(self.paged_kernel_lens, dim=0)
+        self.kv_indptr[1:] = torch.cumsum(paged_kernel_lens, dim=0)
         self.kv_indices = torch.empty(
             self.kv_indptr[-1], dtype=torch.int32, device="cuda"
         )
 
         create_flashinfer_kv_indices_triton[(self.batch_size,)](
-            model_runner.req_to_token_pool.req_to_token,
-            req_pool_indices,
-            self.paged_kernel_lens,
+            self.model_runner.req_to_token_pool.req_to_token,
+            self.req_pool_indices,
+            paged_kernel_lens,
             self.kv_indptr,
             None,
-            model_runner.req_to_token_pool.req_to_token.size(1),
+            self.model_runner.req_to_token_pool.req_to_token.size(1),
             self.kv_indices,
         )
 
-        self.kv_last_page_len = torch.ones(
-            (self.batch_size,), dtype=torch.int32, device="cuda"
+    def _init_indices_window(self, wrapper_id):
+        # window attention use paged only
+        if wrapper_id == 0:
+            if self.forward_mode.is_decode():
+                paged_kernel_lens = torch.minimum(
+                    self.seq_lens,
+                    torch.tensor(self.model_runner.sliding_window_size + 1),
+                )
+            else:
+                paged_kernel_lens = torch.minimum(
+                    self.seq_lens,
+                    torch.tensor(self.model_runner.sliding_window_size)
+                    + self.seq_lens
+                    - self.prefix_lens,
+                )
+        else:
+            paged_kernel_lens = self.seq_lens
+
+        kv_start_idx = self.seq_lens - paged_kernel_lens
+        self.kv_indptr = torch.zeros(
+            (self.batch_size + 1,), dtype=torch.int32, device="cuda"
+        )
+        self.kv_indptr[1:] = torch.cumsum(paged_kernel_lens, dim=0)
+        self.kv_indices = torch.empty(
+            self.kv_indptr[-1], dtype=torch.int32, device="cuda"
+        )
+        create_flashinfer_kv_indices_triton[(self.batch_size,)](
+            self.model_runner.req_to_token_pool.req_to_token,
+            self.req_pool_indices,
+            paged_kernel_lens,
+            self.kv_indptr,
+            kv_start_idx,
+            self.model_runner.req_to_token_pool.req_to_token.size(1),
+            self.kv_indices,
         )
 
-        # CUDA graph uses different flashinfer_decode_wrapper
-        if self.flashinfer_decode_wrapper is None:
-            self.flashinfer_decode_wrapper = self.model_runner.flashinfer_decode_wrapper
-
-    def _update_decode_indices(self):
-        self.flashinfer_decode_wrapper.end_forward()
-        self.flashinfer_decode_wrapper.begin_forward(
+    def _update_decode_indices(self, decode_wrapper):
+        decode_wrapper.end_forward()
+        decode_wrapper.begin_forward(
             self.kv_indptr,
             self.kv_indices,
             self.kv_last_page_len,
@@ -220,7 +182,7 @@ class FlashinferUpdater:
             q_data_type=self.model_runner.dtype,
         )
 
-    def _update_extend_indices(self):
+    def _update_extend_indices(self, ragged_wrapper, paged_wrapper):
         # extend part
         qo_indptr = torch.zeros(
             (self.batch_size + 1,), dtype=torch.int32, device="cuda"
@@ -228,8 +190,8 @@ class FlashinferUpdater:
         qo_indptr[1:] = torch.cumsum(self.seq_lens - self.prefix_lens, dim=0)
 
         if self.flashinfer_use_ragged:
-            self.model_runner.flashinfer_prefill_wrapper_ragged.end_forward()
-            self.model_runner.flashinfer_prefill_wrapper_ragged.begin_forward(
+            ragged_wrapper.end_forward()
+            ragged_wrapper.begin_forward(
                 qo_indptr,
                 qo_indptr,
                 self.num_qo_heads,
@@ -238,8 +200,8 @@ class FlashinferUpdater:
             )
 
         # cached part
-        self.model_runner.flashinfer_prefill_wrapper_paged.end_forward()
-        self.model_runner.flashinfer_prefill_wrapper_paged.begin_forward(
+        paged_wrapper.end_forward()
+        paged_wrapper.begin_forward(
             qo_indptr,
             self.kv_indptr,
             self.kv_indices,
@@ -251,20 +213,25 @@ class FlashinferUpdater:
         )
 
     def update_indices_no_window(self):
-        """Init auxiliary variables for FlashInfer attention backend."""
+        self._init_indices_no_window()
 
         if self.forward_mode.is_decode():
-            self._update_decode_indices()
+            self._update_decode_indices(self.flashinfer_decode_wrapper)
         else:
-            self._update_extend_indices()
+            self._update_extend_indices(
+                self.flashinfer_prefill_wrapper_ragged,
+                self.flashinfer_prefill_wrapper_paged,
+            )
 
     def update_indices_window(self):
-        update_flashinfer_indices_window(
-            self.forward_mode,
-            self.model_runner,
-            self.req_pool_indices,
-            self.seq_lens,
-            self.prefix_lens,
-            self.flashinfer_decode_wrapper,
-            self.flashinfer_use_ragged,
-        )
+        assert self.flashinfer_use_ragged is False
+
+        for wrapper_id in range(2):
+            self._init_indices_window(wrapper_id)
+            if self.forward_mode.is_decode():
+                self._update_decode_indices(self.flashinfer_decode_wrapper[wrapper_id])
+            else:
+                self._update_extend_indices(
+                    None,
+                    self.flashinfer_prefill_wrapper_paged[wrapper_id],
+                )
