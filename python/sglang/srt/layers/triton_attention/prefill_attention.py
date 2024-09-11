e57748a2790b6764b62944dc4612a49ec4eb4bb0
Byron Hsu <byronhsu1230@gmail.com>
2024-09-11 03:22:07 -0700

    remove assertion in triton attention and add an unit test (#1385)

--- python/sglang/srt/layers/triton_attention/prefill_attention.py
+++ python/sglang/srt/layers/triton_attention/prefill_attention.py
@@ -151,8 +151,6 @@ def context_attention_fwd(q, k, v, o, b_start_loc, b_seq_len, max_input_len):
         BLOCK = 64
 
     Lq, Lk, Lv = q.shape[-1], k.shape[-1], v.shape[-1]
-    assert Lq == Lk and Lk == Lv
-    assert Lk in {16, 32, 64, 96, 128, 256}
 
     sm_scale = 1.0 / (Lq**0.5)
     batch, head = b_seq_len.shape[0], q.shape[1]
