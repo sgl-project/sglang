d3f57d48579a386b4aecc6cd12fb170d0e1d38e0
Ying Sheng <sqy1415@gmail.com>
2024-09-13 03:59:39 +0000

    raise exception for wrong import

---
+++ __init__.py
@@ -0,0 +1,3 @@
+raise Exception(
+    "SGLang was imported from the wrong directory 'sglang', but it should be imported from 'sglang/python/sglang' instead. Please do not place your SGL program in the same directory as 'sglang'."
+)
