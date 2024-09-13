f302c85867c3164db804b1b3fd49dc00c124ab56
Lianmin Zheng <lianminzheng@gmail.com>
2024-09-12 20:58:13 -0700

    fix meo

--- test/srt/test_bench_serving.py
+++ test/srt/test_bench_serving.py
@@ -60,6 +60,28 @@ class TestBenchServing(unittest.TestCase):
             other_server_args=[],
         )
 
+    def test_moe_offline_throughput_default(self):
+        res = run_bench_serving(
+            model=DEFAULT_MODEL_NAME_FOR_TEST,
+            num_prompts=300,
+            request_rate=float("inf"),
+            other_server_args=[],
+        )
+
+        if os.getenv("SGLANG_IS_IN_CI", "false") == "true":
+            assert res["output_throughput"] > 1800
+
+    def test_moe_offline_throughput_without_radix_cache(self):
+        res = run_bench_serving(
+            model=DEFAULT_MODEL_NAME_FOR_TEST,
+            num_prompts=300,
+            request_rate=float("inf"),
+            other_server_args=["--disable-radix-cache"],
+        )
+
+        if os.getenv("SGLANG_IS_IN_CI", "false") == "true":
+            assert res["output_throughput"] > 1950
+
 
 if __name__ == "__main__":
     unittest.main()
