a66124ac0ce1fcb54b6ea7fe92ec96db2c8f25de
Lianmin Zheng <lianminzheng@gmail.com>
2024-09-18 04:05:58 -0700

    fix max-running

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -400,8 +400,8 @@ class ModelRunner:
             )
 
         self.req_to_token_pool = ReqToTokenPool(
-            max_num_reqs,
-            self.model_config.context_len + 8,
+            max_num_reqs + 1,
+            self.model_config.context_len + 4,
         )
         if (
             self.model_config.attention_arch == AttentionArch.MLA
