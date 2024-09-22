651b10a6454daa47e8076064f53fa69e30a9ceb5
Yudi Xue <10211+binarycrayon@users.noreply.github.com>
2024-09-22 18:36:48 +0000

    Add default value to metrics types

--- python/sglang/srt/metrics/metrics_types.py
+++ python/sglang/srt/metrics/metrics_types.py
@@ -15,17 +15,17 @@ limitations under the License.
 
 """Metrics Types"""
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
 
 
 @dataclass
 class ConfigStats:
     # Model config
-    max_total_num_tokens: int
-    max_prefill_tokens: int
-    max_running_requests: int
-    context_len: int
+    max_total_num_tokens: int = 0
+    max_prefill_tokens: int = 0
+    max_running_requests: int = 0
+    context_len: int = 0
 
 
 @dataclass
@@ -33,21 +33,23 @@ class PrefillStats:
     # Request stats
     #   TODO Latency
     #   Metadata
-    num_prompt_tokens_requests: List[int]
-    num_generation_tokens_requests: List[int]
+    num_prompt_tokens_requests: List[int] = field(default_factory=list)
+    num_generation_tokens_requests: List[int] = field(default_factory=list)
     # best_of_requests: List[int]
     # n_requests: List[int]
-    finished_reason_requests: List[str]
+    finished_reason_requests: List[str] = field(default_factory=list)
 
 
 @dataclass
 class DecodeStats:
     # System stats (should have _sys suffix)
     #   Scheduler State
-    num_running_sys: int
-    num_waiting_sys: int
-    gen_throughput: int
-    token_usage: int
+    num_running_sys: int = 0
+    num_waiting_sys: int = 0
+    gen_throughput: float = 0.0
+    num_token: int = 0
+    token_usage: float = 0.0
+    waiting_queue: int = 0
 
 
 @dataclass
@@ -55,12 +57,13 @@ class SystemStats:
     #   KV Cache Usage in %
     # gpu_cache_usage_sys: float
     #   Prefix caching block hit rate
-    new_seq: int
-    new_token: int
-    cached_token: int
-    cache_hit_rate: float
-    running_req: int
-    queue_req: int
+    is_mixed_chunk: bool = False
+    new_seq: int = 0
+    new_token: int = 0
+    cached_token: int = 0
+    cache_hit_rate: float = 0.0
+    running_req: int = 0
+    queue_req: int = 0
 
 
 # TODO Iteration stats
