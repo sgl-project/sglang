5749dbb386811fccaa851e313ff9a173d794994b
Zhang, Liangang <liangang.zhang@intel.com>
2024-09-24 10:53:42 +0800

    Update python/sglang/srt/server_args.py
    Co-authored-by: Lianmin Zheng <lianminzheng@gmail.com>

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -256,7 +256,7 @@ class ServerArgs:
             type=str,
             default="cuda",
             choices=["cuda", "xpu"],
-            help="device type to run SGLang."
+            help="The device type."
         )
         parser.add_argument(
             "--kv-cache-dtype",
