28b6752a6f64d9b8fac7d16646a670c47d917465
ByronHsu <byronhsu1230@gmail.com>
2024-10-04 22:59:06 +0000

    hack around order issue

--- test/lang/test_srt_backend.py
+++ test/lang/test_srt_backend.py
@@ -16,7 +16,10 @@ from sglang.test.test_programs import (
     test_stream,
     test_tool_use,
 )
-from sglang.test.test_utils import DEFAULT_MODEL_NAME_FOR_TEST
+
+# from sglang.test.test_utils import DEFAULT_MODEL_NAME_FOR_TEST
+
+DEFAULT_MODEL_NAME_FOR_TEST = "/shared/public/elr-models/meta-llama/Meta-Llama-3.1-8B-Instruct/07eb05b21d191a58c577b4a45982fe0c049d0693/"
 
 
 class TestSRTBackend(unittest.TestCase):
@@ -31,46 +34,48 @@ class TestSRTBackend(unittest.TestCase):
     def tearDownClass(cls):
         cls.backend.shutdown()
 
-    # def test_few_shot_qa(self):
-    #     test_few_shot_qa()
+    def test_few_shot_qa(self):
+        test_few_shot_qa()
 
-    # def test_mt_bench(self):
-    #     test_mt_bench()
+    def test_mt_bench(self):
+        test_mt_bench()
 
-    # def test_select(self):
-    #     test_select(check_answer=False)
+    def test_select(self):
+        test_select(check_answer=False)
 
-    # def test_decode_int(self):
-    #     test_decode_int()
+    def test_decode_int(self):
+        test_decode_int()
 
-    # def test_decode_json_regex(self):
-    #     test_decode_json_regex()
+    def test_decode_json_regex(self):
+        test_decode_json_regex()
 
-    # def test_expert_answer(self):
-    #     test_expert_answer()
+    def test_expert_answer(self):
+        test_expert_answer()
 
-    # def test_tool_use(self):
-    #     test_tool_use()
+    def test_tool_use(self):
+        test_tool_use()
 
-    # def test_parallel_decoding(self):
-    #     test_parallel_decoding()
+    def test_parallel_decoding(self):
+        test_parallel_decoding()
 
-    # def test_stream(self):
-    #     test_stream()
+    def test_stream(self):
+        test_stream()
 
-    # def test_regex(self):
-    #     test_regex()
+    def test_regex(self):
+        test_regex()
 
-    # def test_dtype_gen(self):
-    #     test_dtype_gen()
+    def test_dtype_gen(self):
+        test_dtype_gen()
 
-    # def test_hellaswag_select(self):
-    #     # Run twice to capture more bugs
-    #     for _ in range(2):
-    #         accuracy, latency = test_hellaswag_select()
-    #         assert accuracy > 0.71, f"{accuracy=}"
+    def test_hellaswag_select(self):
+        # Run twice to capture more bugs
+        for _ in range(2):
+            accuracy, latency = test_hellaswag_select()
+            assert accuracy > 0.71, f"{accuracy=}"
 
-    def test_gen_min_new_tokens(self):
+    # TODO (ByronHsu): intentionally add "0" in the test name to ensure the alpha-numeric order is ahead, so this test is run before test_decode_int
+    # See issue at https://github.com/sgl-project/sglang/issues/1575
+    def test_0_gen_min_new_tokens(self):
         test_gen_min_new_tokens(DEFAULT_MODEL_NAME_FOR_TEST)
 
 
