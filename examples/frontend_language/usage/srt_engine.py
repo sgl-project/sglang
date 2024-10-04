b509312a87e11f0af4cbd217617a26e77657be7c
ByronHsu <byronhsu1230@gmail.com>
2024-10-04 08:09:38 +0000

    update example code

--- examples/frontend_language/usage/srt_engine.py
+++ examples/frontend_language/usage/srt_engine.py
@@ -2,20 +2,19 @@ import sglang as sgl
 
 # Sample prompts.
 prompts = [
-    "The capital of China is",
-    "The square root of 144 is",
+    "Hello, my name is",
+    "The president of the United States is",
+    "The capital of France is",
+    "The future of AI is",
 ]
 # Create a sampling params object.
-sampling_params = {"temperature": 0.8, "top_p": 0.95, "max_new_tokens": 128}
+sampling_params = {"temperature": 0.8, "top_p": 0.95}
 
 # Create an LLM.
-llm = sgl.Engine(model_path="/shared/public/models/Meta-Llama-3.1-8B-Instruct")
+llm = sgl.Engine(model_path="meta-llama/Meta-Llama-3.1-8B-Instruct")
 
 outputs = llm.generate(prompts, sampling_params)
 # Print the outputs.
 for prompt, output in zip(prompts, outputs):
     print("===============================")
     print(f"Prompt: {prompt}\nGenerated text: {output['text']}")
-
-assert "Beijing" in outputs[0]["text"]
-assert "12" in outputs[1]["text"]
