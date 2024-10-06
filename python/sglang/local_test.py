0b9725af9b2fe06f01feb307884b491198a94b98
ByronHsu <byronhsu1230@gmail.com>
2024-10-06 00:46:18 +0000

    tmp

--- python/sglang/local_test.py
+++ python/sglang/local_test.py
@@ -1,3 +1,4 @@
+import json
 import os
 import random
 
@@ -6,7 +7,7 @@ import torch
 
 import sglang as sgl
 
-model_path = "/shared/public/models/Qwen/Qwen2.5-1.5B-Instruct/"
+model_path = "/shared/public/elr-models/meta-llama/Meta-Llama-3.1-8B-Instruct/07eb05b21d191a58c577b4a45982fe0c049d0693/"  # "/shared/public/models/Qwen/Qwen2.5-1.5B-Instruct/"
 
 # Set a fixed seed for reproducibility
 SEED = 42
@@ -26,11 +27,25 @@ if __name__ == "__main__":
     set_seed(SEED)
 
     runtime = sgl.Runtime(model_path=model_path)
-    print(runtime.generate("Who is Steve Jobs?", {"top_k": 1}))
-    runtime.shutdown()
+    print("==== Answer 1 ====")
+    ans = json.loads(runtime.generate("Who is Steve Jobs?", {"top_k": 1}))
+    print(ans["text"])
+    # runtime.shutdown()
 
+    sgl.flush_cache()
     set_seed(SEED)
-    engine = sgl.Engine(model_path=model_path)
+    # engine = sgl.Engine(model_path=model_path)
     # bug: default sampling param should be {}
-    print(engine.generate("Who is Steve Jobs?", {"top_k": 1}))
-    engine.shutdown()
+    print("==== Answer 2 ====")
+    ans = json.loads(runtime.generate("Who is Steve Jobs?", {"top_k": 1}))
+    print(ans["text"])
+
+    sgl.flush_cache()
+    set_seed(SEED)
+    # engine = sgl.Engine(model_path=model_path)
+    # bug: default sampling param should be {}
+    print("==== Answer 3 ====")
+    ans = json.loads(runtime.generate("Who is Steve Jobs?", {"top_k": 1}))
+    print(ans["text"])
+
+    runtime.shutdown()
