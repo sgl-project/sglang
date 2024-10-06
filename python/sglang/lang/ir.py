dde8bb16fe9180bd1642bdb8d4f0aa283b120ee4
Byron Hsu <byronhsu1230@gmail.com>
2024-10-05 17:27:43 -0700

    default sampling param should be deepcopied (#1581)

--- python/sglang/lang/ir.py
+++ python/sglang/lang/ir.py
@@ -150,8 +150,8 @@ class SglFunction:
         self,
         *args,
         max_new_tokens: int = 128,
-        stop: Union[str, List[str]] = [],
-        stop_token_ids: Optional[List[int]] = [],
+        stop: Union[str, List[str]] = None,
+        stop_token_ids: Optional[List[int]] = None,
         temperature: float = 1.0,
         top_p: float = 1.0,
         top_k: int = -1,
@@ -168,6 +168,12 @@ class SglFunction:
         **kwargs,
     ):
         from sglang.lang.interpreter import run_program
+
+        # avoid using [] as the default arg: https://nikos7am.com/posts/mutable-default-arguments/
+        if stop is None:
+            stop = []
+        if stop_token_ids is None:
+            stop_token_ids = []
 
         default_sampling_para = SglSamplingParams(
             max_new_tokens=max_new_tokens,
@@ -193,8 +199,8 @@ class SglFunction:
         batch_kwargs,
         *,
         max_new_tokens: int = 128,
-        stop: Union[str, List[str]] = (),
-        stop_token_ids: Optional[List[int]] = [],
+        stop: Union[str, List[str]] = None,
+        stop_token_ids: Optional[List[int]] = None,
         temperature: float = 1.0,
         top_p: float = 1.0,
         top_k: int = -1,
@@ -211,6 +217,11 @@ class SglFunction:
         progress_bar: bool = False,
     ):
         from sglang.lang.interpreter import run_program_batch
+
+        if stop is None:
+            stop = []
+        if stop_token_ids is None:
+            stop_token_ids = []
 
         assert isinstance(batch_kwargs, (list, tuple))
         if len(batch_kwargs) == 0:
