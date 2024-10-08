e8613df071fb126f97c0d1254977586f39362e08
Byron Hsu <byronhsu1230@gmail.com>
2024-10-07 21:26:56 -0700

    [Engine] Fix generate hanging issue after the first call (#1606)

--- test/srt/test_srt_engine.py
+++ test/srt/test_srt_engine.py
@@ -28,6 +28,18 @@ class TestSRTBackend(unittest.TestCase):
         print(out2)
         assert out1 == out2, f"{out1} != {out2}"
 
+    def test_engine_multiple_generate(self):
+        # just to ensure there is no issue running multiple generate calls
+        prompt = "Today is a sunny day and I like"
+        model_path = DEFAULT_MODEL_NAME_FOR_TEST
+
+        sampling_params = {"temperature": 0, "max_new_tokens": 8}
+
+        engine = sgl.Engine(model_path=model_path, random_seed=42)
+        engine.generate(prompt, sampling_params)
+        engine.generate(prompt, sampling_params)
+        engine.shutdown()
+
 
 if __name__ == "__main__":
     unittest.main()
