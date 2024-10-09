92b2c145dfda7bc1b155941f3ea17bf2335298e7
ByronHsu <byronhsu1230@gmail.com>
2024-10-09 01:12:05 +0000

    async works!

--- examples/runtime/srt_engine.py
+++ examples/runtime/srt_engine.py
@@ -1,4 +1,13 @@
 import sglang as sgl
+import asyncio
+
+async def async_streaming(engine):
+
+    generator = await engine.async_generate("Elon Musk is", {"temperature": 0.8, "top_p": 0.95}, stream=True)
+
+    async for output in generator:
+        print(output["text"], end="", flush=True)
+    print()
 
 
 def main():
@@ -9,19 +18,32 @@ def main():
     sampling_params = {"temperature": 0.8, "top_p": 0.95}
 
     # Create an LLM.
-    llm = sgl.Engine(model_path="/shared/public/models/Qwen/Qwen2.5-1.5B-Instruct/", log_level="warning")
+    llm = sgl.Engine(model_path="/shared/public/elr-models/meta-llama/Meta-Llama-3.1-8B-Instruct/07eb05b21d191a58c577b4a45982fe0c049d0693/", log_level="warning")
 
-    output_iterator = llm.generate(prompts, sampling_params, stream=True)
+    # 1. sync + non streaming
+    print("\n\n==== 1. sync + non streaming ====")
+    output = llm.generate(prompts, sampling_params)
 
-    for o in output_iterator:
-        print(o["text"], end="", flush=True)
+    print(output["text"])
 
+    # 2. sync + streaming
+    print("\n\n==== 2. sync + streaming ====")
+    output_generator = llm.generate(prompts, sampling_params, stream=True)
+    for output in output_generator:
+        print(output["text"], end="", flush=True)
     print()
-    
-    # # Print the outputs.
-    # for prompt, output in zip(prompts, outputs):
-    #     print("===============================")
-    #     print(f"Prompt: {prompt}\nGenerated text: {output['text']}")
+
+
+    loop = asyncio.get_event_loop()
+    # 3. async + non_streaming
+    print("\n\n==== 3. async + non streaming ====")
+    output = loop.run_until_complete(llm.async_generate(prompts, sampling_params))
+    print(output["text"])
+
+    # 4. async + streaming
+    print("\n\n==== 4. async + streaming ====")
+    loop.run_until_complete(async_streaming(llm))
+
 
 
 # The __main__ condition is necessary here because we use "spawn" to create subprocesses
