7b0ab72e30b4ce99b16f9fd41143fee9633bb9d5
Ying Sheng <sqy1415@gmail.com>
2024-10-13 20:30:03 -0700

    [Minor] Rename no_eos_trim to no_stop_trim (#1661)

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -885,7 +885,7 @@ class Scheduler:
             output_read_offsets = []
             output_skip_special_tokens = []
             output_spaces_between_special_tokens = []
-            output_no_eos_trim = []
+            output_no_stop_trim = []
         else:  # embedding or reward model
             output_embeddings = []
         unfinished_indices = []
@@ -917,7 +917,7 @@ class Scheduler:
                     output_spaces_between_special_tokens.append(
                         req.sampling_params.spaces_between_special_tokens
                     )
-                    output_no_eos_trim.append(req.sampling_params.no_eos_trim)
+                    output_no_stop_trim.append(req.sampling_params.no_stop_trim)
 
                     meta_info = {
                         "prompt_tokens": len(req.origin_input_ids),
@@ -968,7 +968,7 @@ class Scheduler:
                         output_spaces_between_special_tokens,
                         output_meta_info,
                         output_finished_reason,
-                        output_no_eos_trim,
+                        output_no_stop_trim,
                     )
                 )
             else:  # embedding or reward model
