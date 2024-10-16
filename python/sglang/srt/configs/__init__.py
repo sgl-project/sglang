9ab6921ae10b1d79eded0c90c70a34231250402e
Zhangyi <1109276519@qq.com>
2024-10-16 09:32:31 +0800

    add config for qwen2vl to solve transformers bug

--- python/sglang/srt/configs/__init__.py
+++ python/sglang/srt/configs/__init__.py
@@ -1,5 +1,8 @@
 from sglang.srt.configs.exaone import ExaoneConfig
+from sglang.srt.configs.qwen2vl import Qwen2VLVisionConfig, Qwen2VLConfig
 
 __all__ = [
     "ExaoneConfig",
+    "Qwen2VLConfig",
+    "Qwen2VLVisionConfig",
 ]
