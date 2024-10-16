62fe6902767bf3e8716dc0e281e7e4675043f80f
Lianmin Zheng <lianminzheng@gmail.com>
2024-10-16 00:36:51 -0700

    Simplify code

--- python/sglang/srt/mem_cache/radix_cache.py
+++ python/sglang/srt/mem_cache/radix_cache.py
@@ -101,35 +101,33 @@ class RadixCache(BasePrefixCache):
         self, req: Req, token_ids: Optional[List[int]] = None, free_delta: int = 0
     ):
         """Cache request when it finishes."""
+        if self.disable:
+            if token_ids is None:
+                token_ids_len = len(req.origin_input_ids) + len(req.output_ids) - 1
+            else:
+                token_ids_len = len(token_ids)
+
+            kv_indices = self.req_to_token_pool.req_to_token[
+                req.req_pool_idx, : token_ids_len + free_delta
+            ]
+            self.token_to_kv_pool.free(kv_indices)
+            self.req_to_token_pool.free(req.req_pool_idx)
+            return
+
         if token_ids is None:
             token_ids = (req.origin_input_ids + req.output_ids)[:-1]
         kv_indices = self.req_to_token_pool.req_to_token[
             req.req_pool_idx, : len(token_ids)
         ]
 
-        if self.disable:
-            if free_delta:
-                self.token_to_kv_pool.free(
-                    self.req_to_token_pool.req_to_token[
-                        req.req_pool_idx, : len(token_ids) + 1
-                    ]
-                )
-            else:
-                self.token_to_kv_pool.free(kv_indices)
-            self.req_to_token_pool.free(req.req_pool_idx)
-            return
-
         # Radix Cache takes one ref in memory pool
         new_prefix_len = self.insert(token_ids, kv_indices.clone())
+        self.token_to_kv_pool.free(kv_indices[len(req.prefix_indices) : new_prefix_len])
         if free_delta:
             self.token_to_kv_pool.free(
                 self.req_to_token_pool.req_to_token[
                     req.req_pool_idx, len(token_ids) : len(token_ids) + 1
                 ]
-            )
-        else:
-            self.token_to_kv_pool.free(
-                kv_indices[len(req.prefix_indices) : new_prefix_len]
             )
 
         # Remove req slot release the cache lock
