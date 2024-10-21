0beca85c0b32ed0ad09724be11600502da95d306
Lianmin Zheng <lianminzheng@gmail.com>
2024-10-21 00:59:10 -0700

    Fix triton backedn

--- python/sglang/srt/layers/attention/triton_backend.py
+++ python/sglang/srt/layers/attention/triton_backend.py
@@ -91,7 +91,11 @@ class TritonAttnBackend(AttentionBackend):
         )
 
     def init_forward_metadata_replay_cuda_graph(
-        self, bs: int, req_pool_indices: torch.Tensor, seq_lens: torch.Tensor
+        self,
+        bs: int,
+        req_pool_indices: torch.Tensor,
+        seq_lens: torch.Tensor,
+        seq_lens_sum: int,
     ):
         self.cuda_graph_start_loc.zero_()
         self.cuda_graph_start_loc[1:bs] = torch.cumsum(seq_lens[: bs - 1], dim=0)
