953198ef29135ab832c2be8f1a3b7d2cd5b01ca9
BBuf <1182563586@qq.com>
2024-10-24 21:51:12 +0800

    format

--- python/sglang/srt/configs/model_config.py
+++ python/sglang/srt/configs/model_config.py
@@ -13,8 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import os
 import logging
+import os
 from enum import IntEnum, auto
 from typing import Optional
 
@@ -23,6 +23,7 @@ from transformers import PretrainedConfig
 from sglang.srt.hf_transformers_utils import get_config, get_context_length
 
 logger = logging.getLogger(__name__)
+
 
 class AttentionArch(IntEnum):
     MLA = auto()
@@ -50,18 +51,24 @@ class ModelConfig:
         )
         self.hf_text_config = get_hf_text_config(self.hf_config)
         derived_context_len = get_context_length(self.hf_text_config)
-        allow_long_context = os.environ.get("SGLANG_ALLOW_OVERWRITE_LONGER_CONTEXT_LEN", None)
-        
+        allow_long_context = os.environ.get(
+            "SGLANG_ALLOW_OVERWRITE_LONGER_CONTEXT_LEN", None
+        )
+
         if context_length is not None:
             if context_length > derived_context_len:
                 if allow_long_context:
-                    logger.warning(f"Warning: User-specified context_length ({context_length}) is greater than the derived context_length ({derived_context_len}). "
-                          f"This may lead to incorrect model outputs or CUDA errors.")
+                    logger.warning(
+                        f"Warning: User-specified context_length ({context_length}) is greater than the derived context_length ({derived_context_len}). "
+                        f"This may lead to incorrect model outputs or CUDA errors."
+                    )
                     self.context_len = context_length
                 else:
-                    raise ValueError(f"User-specified context_length ({context_length}) is greater than the derived context_length ({derived_context_len}). "
-                                     f"This may lead to incorrect model outputs or CUDA errors. Note that the derived context_length may differ from max_position_embeddings in the model's config. "
-                                     f"To allow overriding this maximum, set the env var SGLANG_ALLOW_OVERWRITE_LONGER_CONTEXT_LEN=1")
+                    raise ValueError(
+                        f"User-specified context_length ({context_length}) is greater than the derived context_length ({derived_context_len}). "
+                        f"This may lead to incorrect model outputs or CUDA errors. Note that the derived context_length may differ from max_position_embeddings in the model's config. "
+                        f"To allow overriding this maximum, set the env var SGLANG_ALLOW_OVERWRITE_LONGER_CONTEXT_LEN=1"
+                    )
             else:
                 self.context_len = context_length
         else:
