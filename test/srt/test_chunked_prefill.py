69f8214196be54ba5c6a4f656dced4e71a9273e8
Lianmin Zheng <lianminzheng@gmail.com>
2024-10-24 20:08:40 -0700

    Fix unit tests

--- test/srt/test_chunked_prefill.py
+++ test/srt/test_chunked_prefill.py
@@ -2,99 +2,30 @@
 python3 -m unittest test_chunked_prefill.TestChunkedPrefill.test_mixed_chunked_prefill_without_radix_cache
 """
 
-import random
-import threading
-import time
 import unittest
-from types import SimpleNamespace
 
-from sglang.srt.utils import kill_child_process
-from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
     DEFAULT_MODEL_NAME_FOR_TEST,
-    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-    popen_launch_server,
     run_bench_serving,
+    run_mmlu_test,
 )
 
 
-def read_output(process, output_lines):
-    # Read the outputs to prevent blocking on the buffer
-    for line in iter(process.stderr.readline, ""):
-        print(line, end="", flush=True)
-        output_lines.append(line)
-
-
 class TestChunkedPrefill(unittest.TestCase):
-    def run_mmlu(
-        self, disable_radix_cache, enable_mixed_chunk, chunked_prefill_size=32
-    ):
-        other_args = ["--chunked-prefill-size", str(chunked_prefill_size)]
-        if disable_radix_cache:
-            other_args += ["--disable-radix-cache"]
-
-        if enable_mixed_chunk:
-            other_args += ["--enable-mixed-chunk"]
-
-        model = DEFAULT_MODEL_NAME_FOR_TEST
-        port = random.randint(4000, 5000)
-        base_url = f"http://127.0.0.1:{port}"
-        process = popen_launch_server(
-            model,
-            base_url,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=other_args,
-            return_stdout_stderr=True,
-        )
-
-        output_lines = []
-        t = threading.Thread(
-            target=read_output, args=(process, output_lines), daemon=True
-        )
-        t.start()
-
-        args = SimpleNamespace(
-            base_url=base_url,
-            model=model,
-            eval_name="mmlu",
-            num_examples=128,
-            num_threads=128,
-        )
-
-        try:
-            metrics = run_eval(args)
-            print(f"{metrics=}")
-            assert metrics["score"] >= 0.65
-        finally:
-            time.sleep(1)
-            kill_child_process(process.pid)
-            kill_child_process(process.pid)
-
-        has_new_server = False
-        has_leak = False
-        for line in output_lines:
-            if "The server is fired" in line:
-                has_new_server = True
-            if "leak" in line:
-                has_leak = True
-
-        assert has_new_server
-        # assert not has_leak
-
     def test_chunked_prefill(self):
-        self.run_mmlu(disable_radix_cache=False, enable_mixed_chunk=False)
+        run_mmlu_test(disable_radix_cache=False, enable_mixed_chunk=False)
 
     def test_mixed_chunked_prefill(self):
-        self.run_mmlu(disable_radix_cache=False, enable_mixed_chunk=True)
+        run_mmlu_test(disable_radix_cache=False, enable_mixed_chunk=True)
 
     def test_chunked_prefill_without_radix_cache(self):
-        self.run_mmlu(disable_radix_cache=True, enable_mixed_chunk=False)
+        run_mmlu_test(disable_radix_cache=True, enable_mixed_chunk=False)
 
     def test_mixed_chunked_prefill_without_radix_cache(self):
-        self.run_mmlu(disable_radix_cache=True, enable_mixed_chunk=True)
+        run_mmlu_test(disable_radix_cache=True, enable_mixed_chunk=True)
 
     def test_no_chunked_prefill(self):
-        self.run_mmlu(
+        run_mmlu_test(
             disable_radix_cache=False, enable_mixed_chunk=False, chunked_prefill_size=-1
         )
 
--- test/srt/test_overlap_schedule.py
+++ test/srt/test_overlap_schedule.py
@@ -4,92 +4,24 @@ python3 -m unittest test_overlap_schedule.TestOverlapSchedule.test_radix_attenti
 python3 test_overlap_schedule.py
 """
 
-import random
-import threading
-import time
 import unittest
-from types import SimpleNamespace
 
-from sglang.srt.utils import kill_child_process
-from sglang.test.run_eval import run_eval
-from sglang.test.test_utils import (
-    DEFAULT_MODEL_NAME_FOR_TEST,
-    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-    popen_launch_server,
-)
-
-
-def read_output(process, output_lines):
-    # Read the outputs to prevent blocking on the buffer
-    for line in iter(process.stderr.readline, ""):
-        print(line, end="", flush=True)
-        output_lines.append(line)
+from sglang.test.test_utils import run_mmlu_test
 
 
 class TestOverlapSchedule(unittest.TestCase):
-    def run_mmlu(self, disable_radix_cache, chunked_prefill_size=32):
-        other_args = ["--chunked-prefill-size", str(chunked_prefill_size)]
-        if disable_radix_cache:
-            other_args += ["--disable-radix-cache"]
-        other_args += ["--enable-overlap-schedule"]
-
-        model = DEFAULT_MODEL_NAME_FOR_TEST
-        port = random.randint(4000, 5000)
-        base_url = f"http://127.0.0.1:{port}"
-        process = popen_launch_server(
-            model,
-            base_url,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=other_args,
-            return_stdout_stderr=True,
-        )
-
-        output_lines = []
-        t = threading.Thread(
-            target=read_output, args=(process, output_lines), daemon=True
-        )
-        t.start()
-
-        args = SimpleNamespace(
-            base_url=base_url,
-            model=model,
-            eval_name="mmlu",
-            num_examples=128,
-            num_threads=128,
-        )
-
-        try:
-            metrics = run_eval(args)
-            assert metrics["score"] >= 0.65
-        finally:
-            time.sleep(1)
-            kill_child_process(process.pid)
-            kill_child_process(process.pid)
-
-        has_new_server = False
-        has_leak = False
-        for line in output_lines:
-            if "The server is fired" in line:
-                has_new_server = True
-            if "leak" in line:
-                has_leak = True
-
-        assert has_new_server
-        # assert not has_leak
-
     def test_no_radix_attention_chunked_prefill(self):
-        self.run_mmlu(disable_radix_cache=True, chunked_prefill_size=32)
+        run_mmlu_test(disable_radix_cache=True, chunked_prefill_size=32)
 
     def test_no_radix_attention_no_chunked_prefill(self):
-        self.run_mmlu(disable_radix_cache=True, chunked_prefill_size=-1)
+        run_mmlu_test(disable_radix_cache=True, chunked_prefill_size=-1)
 
     def test_radix_attention_chunked_prefill(self):
-        self.run_mmlu(disable_radix_cache=False, chunked_prefill_size=32)
+        run_mmlu_test(disable_radix_cache=False, chunked_prefill_size=32)
 
     def test_radix_attention_no_chunked_prefill(self):
-        self.run_mmlu(disable_radix_cache=False, chunked_prefill_size=-1)
+        run_mmlu_test(disable_radix_cache=False, chunked_prefill_size=-1)
 
 
 if __name__ == "__main__":
     unittest.main()
-    # @unittest.skip("did not support")
