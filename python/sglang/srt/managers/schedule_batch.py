96cb4a9d14d2ddecd4c3bd9e24937b5368a17d8c
Lianmin Zheng <lianminzheng@gmail.com>
2024-10-25 10:24:24 -0700

    Revert "Fix memory leak when doing chunked prefill (#1787)"
    This reverts commit a2f5e7555fdd0d94d00e9feaa1d463505ec7cc7b.

--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -222,7 +222,7 @@ class Req:
         self.prefix_indices = []
         self.extend_input_len = 0
         self.last_node = None
-        self.is_being_chunked = False
+        self.is_inflight_req = 0
 
         # Logprobs (arguments)
         self.return_logprob = False
@@ -906,14 +906,15 @@ class ScheduleBatch:
 
     def filter_batch(
         self,
-        being_chunked_req: Optional[Req] = None,
+        current_inflight_req: Optional[Req] = None,
         keep_indices: Optional[List[int]] = None,
     ):
         if keep_indices is None:
             keep_indices = [
                 i
                 for i in range(len(self.reqs))
-                if not self.reqs[i].finished() and self.reqs[i] is not being_chunked_req
+                if not self.reqs[i].finished()
+                and self.reqs[i] is not current_inflight_req
             ]
 
         if keep_indices is None or len(keep_indices) == 0:
