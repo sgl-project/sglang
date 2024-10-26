16be243e0f962beac3e6e99379ef70437f1decc5
Chayenne <zhaochenyang@g.ucla.edu>
2024-10-26 15:20:03 -0700

    locally fix conf

--- test/srt/test_large_max_new_tokens.py
+++ test/srt/test_large_max_new_tokens.py
@@ -34,7 +34,7 @@ class TestLargeMaxNewTokens(unittest.TestCase):
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             api_key=cls.api_key,
             other_args=("--max-total-token", "1024", "--context-len", "8192"),
-            env={"SGLANG_CLIP_MAX_NEW_TOKENS": "256", **os.environ},
+            env={"SGLANG_CLIP_MAX_NEW_TOKENS_ESTIMATION": "256", **os.environ},
             return_stdout_stderr=(cls.stdout, cls.stderr),
         )
         cls.base_url += "/v1"
