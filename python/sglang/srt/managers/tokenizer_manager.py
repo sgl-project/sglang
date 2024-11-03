8be7fd24d7a1cc166ca02aa71b8bac2675ce8094
Lianmin Zheng <lianminzheng@gmail.com>
2024-11-03 08:14:53 -0800

    Fix parallel sampling

--- python/sglang/srt/managers/tokenizer_manager.py
+++ python/sglang/srt/managers/tokenizer_manager.py
@@ -288,7 +288,7 @@ class TokenizerManager:
 
         generators = []
         rids = []
-        if getattr(obj, "parallel_sample_num", 1):
+        if getattr(obj, "parallel_sample_num", 1) == 1:
             # Send all requests
             for i in range(batch_size):
                 tmp_obj = obj[i]
