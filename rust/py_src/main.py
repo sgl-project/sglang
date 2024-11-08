d9e56b3b07f1cc3ee6721c12d400a3526c8a7013
ByronHsu <byronhsu1230@gmail.com>
2024-11-08 02:01:40 +0000

    working

---
+++ rust/py_src/main.py
@@ -0,0 +1,13 @@
+from sglang_router import Router, PolicyType
+
+
+router = Router(
+    worker_urls=[
+        "http://localhost:30000",
+        "http://localhost:30001",
+    ],
+    policy=PolicyType.ApproxTree,
+    tokenizer_path="/shared/public/elr-models/meta-llama/Meta-Llama-3.1-8B-Instruct/07eb05b21d191a58c577b4a45982fe0c049d0693/tokenizer.json"
+)
+
+router.start()
