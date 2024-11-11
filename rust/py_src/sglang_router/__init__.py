86c37d010aeabb1bd2f4a05e19b6bb7f14c3d8da
Byron Hsu <byronhsu1230@gmail.com>
2024-11-11 15:20:14 -0800

    fix sglang_router not found (#2005)

--- rust/py_src/sglang_router/__init__.py
+++ rust/py_src/sglang_router/__init__.py
@@ -1,5 +1,5 @@
 # a lightweihgt wrapper on router with argument type and comments
-# no wrapper on policy type => direct export
 from sglang_router_rs import PolicyType
 
+# no wrapper on policy type => direct export
 from .router import Router
