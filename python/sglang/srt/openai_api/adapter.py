a1bd7190315d021c7326ed34fe7d73a368c0f572
Xiaoyu Zhang <35585791+BBuf@users.noreply.github.com>
2024-11-12 16:49:45 +0800

    fix a bug in v1_embeeding_request (#2014)

--- python/sglang/srt/openai_api/adapter.py
+++ python/sglang/srt/openai_api/adapter.py
@@ -1281,7 +1281,7 @@ def v1_embedding_request(all_requests, tokenizer_manager):
         else:
             prompt_kwargs = {"input_ids": prompt}
     else:
-        if isinstance(prompts[0], str) or isinstance(propmt[0][0], str):
+        if isinstance(prompts[0], str) or isinstance(propmts[0][0], str):
             prompt_kwargs = {"text": prompts}
         else:
             prompt_kwargs = {"input_ids": prompts}
