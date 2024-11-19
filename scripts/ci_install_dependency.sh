41c58eb44634948712b44976cf259d0ebb5b2cce
James Xu <jamesxu1288@gmail.com>
2024-11-19 10:18:39 -0500

    Test use flashinfer nightly build

--- scripts/ci_install_dependency.sh
+++ scripts/ci_install_dependency.sh
@@ -5,7 +5,4 @@ Install the dependency in CI.
 pip install --upgrade pip
 pip install -e "python[all]"
 pip install transformers==4.45.2 sentence_transformers accelerate peft
-git clone https://github.com/flashinfer-ai/flashinfer.git --recursive
-cd flashinfer/python
-pip install -e .
-pip install ninja # Ninja is required to load C++ extensions
+pip install https://github.com/flashinfer-ai/flashinfer-nightly/releases/download/0.1.6%2Bdd3c836/flashinfer-0.1.6+dd3c836.cu121torch2.4-cp310-cp310-linux_x86_64.whl --force-reinstall
