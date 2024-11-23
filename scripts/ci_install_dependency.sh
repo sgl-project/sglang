66557276625f944d34bf3f81cf332e8ab74e36cf
James Xu <jamesxu1288@gmail.com>
2024-11-23 11:16:45 -0500

    Bump flashinfer nightly version

--- scripts/ci_install_dependency.sh
+++ scripts/ci_install_dependency.sh
@@ -5,5 +5,5 @@ Install the dependency in CI.
 pip install --upgrade pip
 pip install -e "python[all]"
 pip install transformers==4.45.2 sentence_transformers accelerate peft
-pip install https://github.com/flashinfer-ai/flashinfer-nightly/releases/download/0.1.6%2B3ed9a8b/flashinfer-0.1.6+3ed9a8b.cu121torch2.4-cp310-cp310-linux_x86_64.whl --force-reinstall
+pip install https://github.com/flashinfer-ai/flashinfer-nightly/releases/download/0.1.6%2B9cba9fb/flashinfer-0.1.6+9cba9fb.cu121torch2.4-cp310-cp310-linux_x86_64.whl --force-reinstall
 pip install ninja
