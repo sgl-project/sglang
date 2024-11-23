8bec76e9b96a5904645a5d479893af4ba0e5d53a
Lianmin Zheng <lianminzheng@gmail.com>
2024-11-23 01:18:12 -0800

    Fix dp print message

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1388,8 +1388,8 @@ def run_scheduler_process(
     pipe_writer,
 ):
     # [For Router] if env var "DP_RANK" exist, set dp_rank to the value of the env var
-    if dp_rank is None:
-        dp_rank = int(os.getenv("DP_RANK", -1))
+    if dp_rank is None and "DP_RANK" in os.environ:
+        dp_rank = int(os["DP_RANK"])
 
     if dp_rank is None:
         configure_logger(server_args, prefix=f" TP{tp_rank}")
