e3160e7d51d14f711cee6de299e465beef9c85f0
Lianmin Zheng <lianminzheng@gmail.com>
2024-11-25 17:12:08 -0800

    Rename double sparsity config file (#2188)

--- test/srt/test_double_sparsity.py
+++ test/srt/test_double_sparsity.py
@@ -18,7 +18,9 @@ class TestDoubleSparsity(unittest.TestCase):
         cls.model = DEFAULT_MODEL_NAME_FOR_TEST
         cls.base_url = DEFAULT_URL_FOR_TEST
         dirpath = os.path.dirname(__file__)
-        config_file = os.path.join(dirpath, "Llama-3.1-8B-Instruct.json")
+        config_file = os.path.join(
+            dirpath, "double-sparsity-config-Llama-3.1-8B-Instruct.json"
+        )
         # NOTE: Generate the config file by running https://github.com/andy-yang-1/DoubleSparse/blob/main/evaluation/group_channel_config.py
         cls.process = popen_launch_server(
             cls.model,
