c754652fcd1a5ac0e727343486657f5ef71b3252
Lianmin Zheng <lianminzheng@gmail.com>
2024-11-26 23:06:20 -0800

    Fix flasky tests (#2212)

--- test/srt/test_pytorch_sampling_backend.py
+++ test/srt/test_pytorch_sampling_backend.py
@@ -51,7 +51,7 @@ class TestPyTorchSamplingBackend(unittest.TestCase):
             response_single = requests.post(
                 self.base_url + "/generate",
                 json={
-                    "text": "The capital of France is",
+                    "text": "The capital of Germany is",
                     "sampling_params": {
                         "temperature": 0,
                         "max_new_tokens": 32,
@@ -69,7 +69,7 @@ class TestPyTorchSamplingBackend(unittest.TestCase):
         response_batch = requests.post(
             self.base_url + "/generate",
             json={
-                "text": ["The capital of France is"] * 10,
+                "text": ["The capital of Germany is"] * 10,
                 "sampling_params": {
                     "temperature": 0,
                     "max_new_tokens": 32,
