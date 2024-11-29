8b48496aaf90fdcd90698dbe9a9e11acdfc4a4d3
Ying Sheng <sqy1415@gmail.com>
2024-11-28 23:58:54 -0800

    Revert "Revert "Add simple CPU offloading support"" (#2253)
    Co-authored-by: Jani Monoses <jani.monoses@gmail.com>
    Co-authored-by: youkaichao <youkaichao@gmail.com>

--- python/sglang/srt/models/llama.py
+++ python/sglang/srt/models/llama.py
@@ -43,6 +43,7 @@ from sglang.srt.layers.vocab_parallel_embedding import (
 )
 from sglang.srt.managers.schedule_batch import global_server_args_dict
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch
+from sglang.srt.utils import make_layers
 
 
 class LlamaMLP(nn.Module):
@@ -255,13 +256,12 @@ class LlamaModel(nn.Module):
             config.vocab_size,
             config.hidden_size,
         )
-        self.layers = nn.ModuleList(
-            [
-                LlamaDecoderLayer(
-                    config, i, quant_config=quant_config, prefix=f"model.layers.{i}"
-                )
-                for i in range(config.num_hidden_layers)
-            ]
+        self.layers = make_layers(
+            config.num_hidden_layers,
+            lambda idx, prefix: LlamaDecoderLayer(
+                config=config, quant_config=quant_config, layer_id=idx, prefix=prefix
+            ),
+            prefix="model.layers",
         )
 
         self.norm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
