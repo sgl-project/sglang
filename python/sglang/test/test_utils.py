01017d4c206ccfe01c9fc458f3acafc52d81b848
bjmsong <wq.songbob@gmail.com>
2024-11-30 08:13:38 +0800

    Support LoRA in Completion API (#2243)
    Co-authored-by: root <bjmsong@126.com>

--- python/sglang/test/test_utils.py
+++ python/sglang/test/test_utils.py
@@ -567,6 +567,7 @@ def run_bench_serving(
         disable_tqdm=False,
         disable_stream=disable_stream,
         disable_ignore_eos=False,
+        lora_name=None,
         extra_request_body=None,
         profile=None,
     )
