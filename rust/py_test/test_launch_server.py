cbc003fd4deca5c60be50b95063365f3128ecd29
ByronHsu <byronhsu1230@gmail.com>
2024-12-06 09:02:05 +0000

    fix security

--- rust/py_test/test_launch_server.py
+++ rust/py_test/test_launch_server.py
@@ -69,7 +69,7 @@ def popen_launch_router(
 
 def find_available_port():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("", 0))
+        s.bind(("127.0.0.1", 0))
         return s.getsockname()[1]
 
 
