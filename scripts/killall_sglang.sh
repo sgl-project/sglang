fcb786b7d8d6b3153cb9e94d20ff3457a964e3c2
zhyncs <me@zhyncs.com>
2024-12-08 04:09:05 +0800

    upd

--- scripts/killall_sglang.sh
+++ scripts/killall_sglang.sh
@@ -4,4 +4,6 @@ nvidia-smi
 kill -9 $(ps aux | grep 'multiprocessing.spawn' | grep -v 'grep' | awk '{print $2}')
 kill -9 $(ps aux | grep 'sglang.launch_server' | grep -v 'grep' | awk '{print $2}')
 kill -9 $(ps aux | grep 'sglang.bench' | grep -v 'grep' | awk '{print $2}')
-kill -9 $(nvidia-smi | sed -n '/Processes:/,$p' | grep "   [0-9]" | awk '{print $5}')
+
+# Kill all processes occupying GPU memory, and enable it when needed.
+# kill -9 $(nvidia-smi | sed -n '/Processes:/,$p' | grep "   [0-9]" | awk '{print $5}')
