77919029ac815fb175b0917555c369ee3c099eb5
zhyncs <me@zhyncs.com>
2024-12-07 21:35:00 +0800

    upd

--- scripts/killall_sglang.sh
+++ scripts/killall_sglang.sh
@@ -1,5 +1,6 @@
 # Kill all SGLang processes and free the GPU memory.
 
+nvidia-smi
 kill -9 $(ps aux | grep 'multiprocessing.spawn' | grep -v 'grep' | awk '{print $2}')
 kill -9 $(ps aux | grep 'sglang.launch_server' | grep -v 'grep' | awk '{print $2}')
 kill -9 $(ps aux | grep 'sglang.bench' | grep -v 'grep' | awk '{print $2}')
