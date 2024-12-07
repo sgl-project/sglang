8543138c7aad53f61162d30724caccd6e2326788
zhyncs <me@zhyncs.com>
2024-12-07 21:29:54 +0800

    fix: update killall script

--- scripts/killall_sglang.sh
+++ scripts/killall_sglang.sh
@@ -3,3 +3,4 @@
 kill -9 $(ps aux | grep 'multiprocessing.spawn' | grep -v 'grep' | awk '{print $2}')
 kill -9 $(ps aux | grep 'sglang.launch_server' | grep -v 'grep' | awk '{print $2}')
 kill -9 $(ps aux | grep 'sglang.bench' | grep -v 'grep' | awk '{print $2}')
+kill -9 $(nvidia-smi | sed -n '/Processes:/,$p' | grep "   [0-9]" | awk '{print $5}')
