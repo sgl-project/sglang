46df6d8792728e7f2bed5ccabd4ca2f03382e4b7
Ankur Kumar <ankur26196@outlook.com>
2024-12-09 01:50:13 -0800

    fix download path

--- benchmark/infinitebench/download_infinitebench.sh
+++ benchmark/infinitebench/download_infinitebench.sh
@@ -4,5 +4,5 @@ save_dir=$1
 mkdir -p ${save_dir}
 
 for file in code_debug code_run kv_retrieval longbook_choice_eng longbook_qa_chn longbook_qa_eng longbook_sum_eng longdialogue_qa_eng math_calc math_find number_string passkey; do
-    wget -c https://huggingface.co/datasets/xinrongzhang2022/InfiniteBench/resolve/main/${file}.jsonl?download=true -O ./${save_dir}/${file}.jsonl
+    wget -c https://huggingface.co/datasets/xinrongzhang2022/InfiniteBench/resolve/main/${file}.jsonl?download=true -O ${save_dir}/${file}.jsonl
 done
