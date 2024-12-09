3835eba9a6f5157585ab419d075969452d27e29d
Zhangyi <1109276519@qq.com>
2024-12-10 01:52:52 +0800

    init setup kernel ci

--- sgl-kernel/build.sh
+++ sgl-kernel/build.sh
@@ -2,12 +2,16 @@
 
 set -ex
 
+PYTHON_VERSION=$1
+CUDA_VERSION=$2
+PYTHON_ROOT_PATH=/opt/python/cp${PYTHON_VERSION//.}-cp${PYTHON_VERSION//.}
+
 docker run --rm -it \
     -v "$(pwd)":/sgl-kernel \
-    pytorch/manylinux-builder:cuda12.1 \
+    pytorch/manylinux-builder:cuda${CUDA_VERSION} \
     bash -c "
-    pip install --no-cache-dir torch==2.4.0 --index-url https://download.pytorch.org/whl/cu121 && \
+    ${PYTHON_ROOT_PATH}/bin/pip install --no-cache-dir torch==2.4.0 --index-url https://download.pytorch.org/whl/cu${CUDA_VERSION//.} && \
     export TORCH_CUDA_ARCH_LIST='7.5 8.0 8.9 9.0+PTX' && \
     cd /sgl-kernel && \
-    python setup.py bdist_wheel
+    ${PYTHON_ROOT_PATH}/bin/python setup.py bdist_wheel
     "
