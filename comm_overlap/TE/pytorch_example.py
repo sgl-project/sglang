650453806b7058714ceac1eb2afbbe9a9e55ce5c
zhuohaol <zhuohaol058@gmail.com>
2024-12-12 00:01:52 +0000

    TE example

---
+++ comm_overlap/TE/pytorch_example.py
@@ -0,0 +1,22 @@
+import torch
+import transformer_engine.pytorch as te
+from transformer_engine.common import recipe
+
+# Set dimensions.
+in_features = 768
+out_features = 3072
+hidden_size = 2048
+
+# Initialize model and inputs.
+model = te.Linear(in_features, out_features, bias=True)
+inp = torch.randn(hidden_size, in_features, device="cuda")
+
+# Create an FP8 recipe. Note: All input args are optional.
+fp8_recipe = recipe.DelayedScaling(margin=0, fp8_format=recipe.Format.E4M3)
+
+# Enable autocasting for the forward pass
+with te.fp8_autocast(enabled=True, fp8_recipe=fp8_recipe):
+    out = model(inp)
+
+loss = out.sum()
+loss.backward()
