fccbfa3752abc4c599a58be32573246a6215b747
Yineng Zhang <me@zhyncs.com>
2024-12-14 22:36:04 +0800

    format: add clang-format for sgl-kernel (#2483)

--- sgl-kernel/src/sgl-kernel/csrc/warp_reduce.cc
+++ sgl-kernel/src/sgl-kernel/csrc/warp_reduce.cc
@@ -2,12 +2,10 @@
 
 torch::Tensor warp_reduce_cuda(torch::Tensor input);
 
-#define CHECK_CUDA(x)                                                          \
-  TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x)                                                    \
-  TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_INPUT(x)                                                         \
-  CHECK_CUDA(x);                                                               \
+#define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x) \
+  CHECK_CUDA(x);       \
   CHECK_CONTIGUOUS(x)
 
 torch::Tensor warp_reduce(torch::Tensor input) {
