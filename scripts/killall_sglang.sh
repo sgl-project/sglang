f07ff6cac248b8a3127c23e3842108a457b65131
Ata Fatahi <immrata@gmail.com>
2024-12-17 06:42:40 -0500

    Clean up GPU memory after killing sglang processes (#2457)
    Signed-off-by: Ata Fatahi <immrata@gmail.com>

--- scripts/killall_sglang.sh
+++ scripts/killall_sglang.sh
@@ -11,4 +11,9 @@ kill -9 $(ps aux | grep 'sglang.bench' | grep -v 'grep' | awk '{print $2}') 2>/d
 # Clean all GPU processes if any argument is provided
 if [ $# -gt 0 ]; then
     kill -9 $(nvidia-smi | sed -n '/Processes:/,$p' | grep "   [0-9]" | awk '{print $5}') 2>/dev/null
+    lsof /dev/nvidia* | awk '{print $2}' | xargs kill -9 2>/dev/null
 fi
+
+
+# Show GPU status after clean up
+nvidia-smi
