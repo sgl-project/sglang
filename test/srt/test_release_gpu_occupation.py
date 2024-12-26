eaa98086b5ccb75522d620cb6bec688aa97185f2
fzyzcjy <ch271828n@outlook.com>
2024-12-26 14:42:19 +0800

    more

---
+++ test/srt/test_release_gpu_occupation.py
@@ -0,0 +1,32 @@
+import unittest
+import sglang as sgl
+from sglang.test.test_utils import (
+    DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
+)
+
+
+class TestReleaseGPUOccupation(unittest.TestCase):
+    def test_release_and_resume_occupation(self):
+        prompt = "Today is a sunny day and I like"
+        expect_output = "TODO"
+        sampling_params = {"temperature": 0, "max_new_tokens": 8}
+
+        engine = sgl.Engine(
+            model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
+            random_seed=42,
+        )
+
+        outputs = engine.generate(prompt, sampling_params)["text"]
+        self.assertEqual(outputs, expect_output)
+
+        engine.release_gpu_occupation()
+        engine.resume_gpu_occupation()
+
+        outputs = engine.generate(prompt, sampling_params)["text"]
+        self.assertEqual(outputs, expect_output)
+
+        engine.shutdown()
+
+
+if __name__ == "__main__":
+    unittest.main()
