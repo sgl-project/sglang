5fdfef0b368a1ce5ccb17c0b36845516d8a2214a
HaiShaw <hixiao@gmail.com>
2024-12-26 18:12:33 -0800

    cache is_hip() for faster reference

--- python/sglang/srt/layers/moe/fused_moe_triton/fused_moe.py
+++ python/sglang/srt/layers/moe/fused_moe_triton/fused_moe.py
@@ -17,8 +17,10 @@ from sglang.srt.layers.moe.topk import select_experts
 from sglang.srt.layers.quantization.fp8_kernel import per_token_group_quant_fp8
 from sglang.srt.utils import direct_register_custom_op, get_device_name, is_hip
 
+not_hip = False
 if not is_hip():
     from sgl_kernel import moe_align_block_size as sgl_moe_align_block_size
+    not_hip = True
 
 logger = logging.getLogger(__name__)
 padding_size = 128 if bool(int(os.getenv("MOE_PADDING", "0"))) else 0
@@ -270,7 +272,7 @@ def moe_align_block_size(
     )
     num_tokens_post_pad = torch.empty((1), dtype=torch.int32, device=topk_ids.device)
     # FIXME(zhyncs)
-    if not is_hip() and num_experts >= 256:
+    if not_hip and num_experts >= 256:
         sgl_moe_align_block_size(
             topk_ids,
             num_experts,
