e816777b48b39797efafd1390d9de51f37a9cdf5
fzyzcjy <ch271828n@outlook.com>
2024-12-28 20:46:50 +0800

    more

--- test/srt/test_update_weights_from_tensor.py
+++ test/srt/test_update_weights_from_tensor.py
@@ -1,7 +1,5 @@
-import json
 import unittest
 
-import requests
 from sglang.srt.utils import kill_process_tree
 from sglang.test.test_utils import (
     DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
@@ -12,78 +10,14 @@ from sglang.test.test_utils import (
 
 
 class TestUpdateWeightsFromTensor(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.model = DEFAULT_SMALL_MODEL_NAME_FOR_TEST
-        cls.base_url = DEFAULT_URL_FOR_TEST
-        cls.process = popen_launch_server(
-            cls.model, cls.base_url, timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
-        )
-
-    @classmethod
-    def tearDownClass(cls):
-        kill_process_tree(cls.process.pid)
-
-    def run_decode(self):
-        response = requests.post(
-            self.base_url + "/generate",
-            json={
-                "text": "The capital of France is",
-                "sampling_params": {
-                    "temperature": 0,
-                    "max_new_tokens": 32,
-                },
-            },
-        )
-        print(json.dumps(response.json()))
-        print("=" * 100)
-        text = response.json()["text"]
-        return text
-
-    def get_model_info(self):
-        response = requests.get(self.base_url + "/get_model_info")
-        model_path = response.json()["model_path"]
-        print(json.dumps(response.json()))
-        return model_path
-
-    def run_update_weights(self, model_path):
-        response = requests.post(
-            self.base_url + "/update_weights_from_disk",
-            json={
-                "model_path": model_path,
-            },
-        )
-        ret = response.json()
-        print(json.dumps(response.json()))
-        return ret
-
     def test_update_weights(self):
-        origin_model_path = self.get_model_info()
-        print(f"origin_model_path: {origin_model_path}")
-        origin_response = self.run_decode()
+        process = popen_launch_server(
+            DEFAULT_SMALL_MODEL_NAME_FOR_TEST, DEFAULT_URL_FOR_TEST, timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
+        )
+       
+        TODO
 
-        # update weights
-        new_model_path = DEFAULT_SMALL_MODEL_NAME_FOR_TEST.replace("-Instruct", "")
-        ret = self.run_update_weights(new_model_path)
-        assert ret["success"]
-
-        updated_model_path = self.get_model_info()
-        print(f"updated_model_path: {updated_model_path}")
-        assert updated_model_path == new_model_path
-        assert updated_model_path != origin_model_path
-
-        updated_response = self.run_decode()
-        assert origin_response[:32] != updated_response[:32]
-
-        # update weights back
-        ret = self.run_update_weights(origin_model_path)
-        assert ret["success"]
-
-        updated_model_path = self.get_model_info()
-        assert updated_model_path == origin_model_path
-
-        updated_response = self.run_decode()
-        assert origin_response[:32] == updated_response[:32]
+        kill_process_tree(process.pid)
 
 
 if __name__ == "__main__":
