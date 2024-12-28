3a46eedba0ef57c9efea772d85060b21e06f4719
fzyzcjy <ch271828n@outlook.com>
2024-12-28 20:51:36 +0800

    more

--- test/srt/test_update_weights_from_tensor.py
+++ test/srt/test_update_weights_from_tensor.py
@@ -6,22 +6,15 @@ from sglang.test.test_utils import DEFAULT_SMALL_MODEL_NAME_FOR_TEST
 
 class TestReleaseGPUOccupation(unittest.TestCase):
     def test_release_and_resume_occupation(self):
-        prompt = "Today is a sunny day and I like"
-        expect_output = " to spend it outdoors. I decided to"
-        sampling_params = {"temperature": 0, "max_new_tokens": 8}
+        engine = sgl.Engine(model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST)
 
-        engine = sgl.Engine(
-            model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
-            random_seed=42,
-        )
+        param_name = "model.layers.2.self_attn.k_proj.weight"
 
-        outputs = engine.generate(prompt, sampling_params)["text"]
-        self.assertEqual(outputs, expect_output)
+        assert engine.get_weights_by_name(param_name, truncate_size=8) == ['TODO']
+      
+        engine.update_weights_from_tensor(param_name, TODO)
 
-        engine.update_weights_from_tensor(TODO, TODO)
-
-        outputs = engine.generate(prompt, sampling_params)["text"]
-        self.assertEqual(outputs, expect_output)
+        assert engine.get_weights_by_name(param_name, truncate_size=8) == ['TODO']
 
         engine.shutdown()
 
