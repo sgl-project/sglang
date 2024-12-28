1fcf7de95d232ac0e2a2b0b3c3ab4620daf664cb
fzyzcjy <ch271828n@outlook.com>
2024-12-28 20:52:02 +0800

    more

--- test/srt/test_update_weights_from_tensor.py
+++ test/srt/test_update_weights_from_tensor.py
@@ -1,6 +1,7 @@
 import unittest
 
 import sglang as sgl
+import torch
 from sglang.test.test_utils import DEFAULT_SMALL_MODEL_NAME_FOR_TEST
 
 
@@ -11,8 +12,9 @@ class TestReleaseGPUOccupation(unittest.TestCase):
         param_name = "model.layers.2.self_attn.k_proj.weight"
 
         assert engine.get_weights_by_name(param_name, truncate_size=8) == ['TODO']
-      
-        engine.update_weights_from_tensor(param_name, TODO)
+
+        new_tensor = torch.full((100,), 42)  # TODO
+        engine.update_weights_from_tensor(param_name, new_tensor)
 
         assert engine.get_weights_by_name(param_name, truncate_size=8) == ['TODO']
 
