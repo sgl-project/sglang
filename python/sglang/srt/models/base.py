35fb9c61541b8c0a7c980d1c010634086192a264
fzyzcjy <ch271828n@outlook.com>
2024-12-29 08:34:21 +0800

    more

--- python/sglang/srt/models/base.py
+++ python/sglang/srt/models/base.py
@@ -1,4 +1,11 @@
 from torch import nn
 
+
 class BaseCausalLM(nn.Module):
-    pass # TODO
+    def export_static_params(self):
+        for name, param in self.named_parameters():
+            TODO
+        return TODO
+
+    def import_static_params(self, static_params):
+        TODO
