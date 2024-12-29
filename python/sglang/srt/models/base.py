e2243924673499a0864d635b48b33b98165dedf6
fzyzcjy <ch271828n@outlook.com>
2024-12-29 08:36:29 +0800

    more

--- python/sglang/srt/models/base.py
+++ python/sglang/srt/models/base.py
@@ -1,11 +1,18 @@
+from typing import List, Tuple
+
+import torch
 from torch import nn
 
 
 class BaseCausalLM(nn.Module):
-    def export_static_params(self):
+    def export_static_params(self) -> List[Tuple[str, torch.Tensor]]:
+        static_params = []
         for name, param in self.named_parameters():
-            TODO
-        return TODO
+            if 'rotary_emb' in name:
+                static_params.append((name, param.data.detach().clone()))
+        return static_params
 
-    def import_static_params(self, static_params):
-        TODO
+    def import_static_params(self, static_params: List[Tuple[str, torch.Tensor]]):
+        self_named_parameters = dict(self.named_parameters())
+        for name, tensor in static_params:
+            self_named_parameters[name].data[...] = tensor
