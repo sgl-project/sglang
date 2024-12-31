74c68adb09e879bde8ee6968cc80a3d38d1d36c5
fzyzcjy <ch271828n@outlook.com>
2024-12-31 09:06:52 +0800

    more

--- scripts/ci_install_dependency.sh
+++ scripts/ci_install_dependency.sh
@@ -11,6 +11,7 @@ pip install -e "python[all]" --find-links https://flashinfer.ai/whl/cu121/torch2
 
 # Force reinstall flashinfer
 pip install flashinfer==0.1.6 --find-links ${FLASHINFER_REPO} --force-reinstall --no-deps
+pip uninstall -y torch_memory_saver && pip install torch_memory_saver
 
 pip install transformers==4.45.2 sentence_transformers accelerate peft
 
