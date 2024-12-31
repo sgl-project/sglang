333fe8c3a63645aab11e8a483709aae9976d86e0
Lianmin Zheng <lianminzheng@gmail.com>
2024-12-31 01:53:38 -0800

    update

--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 # Copyright 2023-2024 SGLang Team
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
