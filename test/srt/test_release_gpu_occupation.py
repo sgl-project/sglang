6000d072f940216f7ef7057cd2fb6f7343e3ab92
fzyzcjy <ch271828n@outlook.com>
2024-12-31 09:29:32 +0800

    bump

--- test/srt/test_release_gpu_occupation.py
+++ test/srt/test_release_gpu_occupation.py
@@ -6,7 +6,7 @@ from sglang.test.test_utils import DEFAULT_SMALL_MODEL_NAME_FOR_TEST
 from transformers import AutoModelForCausalLM
 
 # (temporarily) set to true to observe memory usage in nvidia-smi more clearly
-_DEBUG_EXTRA = True
+_DEBUG_EXTRA = False
 
 
 class TestReleaseGPUOccupation(unittest.TestCase):
