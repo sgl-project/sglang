a2aa4ec057f8736beddda897176874da28e292b8
zhyncs <me@zhyncs.com>
2024-12-30 22:49:03 -0800

    minor: cleanup sgl-kernel

--- sgl-kernel/src/sgl-kernel/__init__.py
+++ sgl-kernel/src/sgl-kernel/__init__.py
@@ -3,12 +3,10 @@ from sgl_kernel.ops import (
     custom_reduce,
     init_custom_reduce,
     moe_align_block_size,
-    warp_reduce,
 )
 
 __all__ = [
     "moe_align_block_size",
-    "warp_reduce",
     "init_custom_reduce",
     "custom_dispose",
     "custom_reduce",
