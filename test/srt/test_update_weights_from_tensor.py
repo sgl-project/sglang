e27022e24410d3c036178cc1cd30dff2116ea86d
fzyzcjy <ch271828n@outlook.com>
2025-01-01 21:08:22 +0800

    more

--- test/srt/test_update_weights_from_tensor.py
+++ test/srt/test_update_weights_from_tensor.py
@@ -10,11 +10,11 @@ class TestUpdateWeightsFromTensor(unittest.TestCase):
     def test_update_weights_from_tensor(self):
         engine = sgl.Engine(model_path=DEFAULT_MODEL_NAME_FOR_TEST)
 
-        param_name = "model.layers.2.self_attn.k_proj.weight"
+        param_name = "model.layers.6.mlp.up_proj.weight"
 
-        _check_param(engine, param_name, [0.0078, -0.0060, -0.0134, -0.0403, -0.0103])
+        _check_param(engine, param_name, [-0.0140, -0.0176, 0.0093, -0.0105, 0.0015])
 
-        new_tensor = torch.full((3072, 2048), 1.5)
+        new_tensor = torch.full((28672, 4096), 1.5)
 
         time_start = time.time()
         engine.update_weights_from_tensor(param_name, new_tensor)
