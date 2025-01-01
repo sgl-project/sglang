8f4abe048f4edb3be025819752731821b9e38e19
fzyzcjy <ch271828n@outlook.com>
2025-01-01 20:53:27 +0800

    fix name

--- test/srt/test_update_weights_from_tensor.py
+++ test/srt/test_update_weights_from_tensor.py
@@ -1,13 +1,12 @@
 import unittest
 
-import torch
-
 import sglang as sgl
+import torch
 from sglang.test.test_utils import DEFAULT_SMALL_MODEL_NAME_FOR_TEST
 
 
-class TestReleaseGPUOccupation(unittest.TestCase):
-    def test_release_and_resume_occupation(self):
+class TestUpdateWeightsFromTensor(unittest.TestCase):
+    def test_update_weights_from_tensor(self):
         engine = sgl.Engine(model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST)
 
         param_name = "model.layers.2.self_attn.k_proj.weight"
