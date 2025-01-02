60cc8ba47780c6f667a411a6f33540cd6aa9c544
fzyzcjy <ch271828n@outlook.com>
2025-01-02 09:54:36 +0800

    more

--- test/srt/test_update_weights_from_tensor.py
+++ test/srt/test_update_weights_from_tensor.py
@@ -4,20 +4,18 @@ import unittest
 import torch
 
 import sglang as sgl
-from sglang.test.test_utils import DEFAULT_MODEL_NAME_FOR_TEST
+from sglang.test.test_utils import DEFAULT_SMALL_MODEL_NAME_FOR_TEST
 
 
 class TestUpdateWeightsFromTensor(unittest.TestCase):
     def test_update_weights_from_tensor(self):
-        engine = sgl.Engine(model_path=DEFAULT_MODEL_NAME_FOR_TEST)
+        engine = sgl.Engine(model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST)
 
         param_names = [f"model.layers.{i}.mlp.up_proj.weight" for i in range(6, 16)]
 
-        _check_param(
-            engine, param_names[0], [-0.0140, -0.0176, 0.0093, -0.0105, 0.0015]
-        )
+        _check_param(engine, param_names[0], [0.0087, -0.0214, -0.0004, 0.0039, 0.0110])
 
-        new_tensor = torch.full((28672, 4096), 1.5)
+        new_tensor = torch.full((16384, 2048), 1.5)
 
         time_start = time.time()
         engine.update_weights_from_tensor([(x, new_tensor) for x in param_names])
