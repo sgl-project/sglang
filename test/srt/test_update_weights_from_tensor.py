80de3ebbee1091a9223b9b15c590232cd2b55419
fzyzcjy <ch271828n@outlook.com>
2025-01-02 10:05:17 +0800

    rm

--- test/srt/test_update_weights_from_tensor.py
+++ test/srt/test_update_weights_from_tensor.py
@@ -19,7 +19,7 @@ class TestUpdateWeightsFromTensor(unittest.TestCase):
 
         time_start = time.time()
         engine.update_weights_from_tensor([(x, new_tensor) for x in param_names])
-        print(f"Time delta: {time.time() - time_start:.03f}")
+        # print(f"Time delta: {time.time() - time_start:.03f}")
 
         for param_name in param_names[:3]:
             _check_param(engine, param_name, [1.5] * 5)
