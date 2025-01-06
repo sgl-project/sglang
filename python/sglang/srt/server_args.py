109e466a78be558ef6974cf9d31640c3f4ac7225
fzyzcjy <ch271828n@outlook.com>
2025-01-06 19:26:49 +0800

    more

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -21,7 +21,6 @@ import tempfile
 from typing import List, Optional
 
 import torch
-
 from sglang.srt.hf_transformers_utils import check_gguf_file
 from sglang.srt.utils import (
     get_amdgpu_memory_capacity,
@@ -79,7 +78,8 @@ class ServerArgs:
     watchdog_timeout: float = 300
     download_dir: Optional[str] = None
     base_gpu_id: int = 0
-    fragment: Optional["ServerFragmentArgs"] = None
+    fragment_tp_rank: Optional[int] = None
+    fragment_nccl_port: Optional[int] = None
 
     # Logging
     log_level: str = "info"
@@ -292,8 +292,8 @@ class ServerArgs:
             default=ServerArgs.tokenizer_mode,
             choices=["auto", "slow"],
             help="Tokenizer mode. 'auto' will use the fast "
-            "tokenizer if available, and 'slow' will "
-            "always use the slow tokenizer.",
+                 "tokenizer if available, and 'slow' will "
+                 "always use the slow tokenizer.",
         )
         parser.add_argument(
             "--load-format",
@@ -309,18 +309,18 @@ class ServerArgs:
                 "bitsandbytes",
             ],
             help="The format of the model weights to load. "
-            '"auto" will try to load the weights in the safetensors format '
-            "and fall back to the pytorch bin format if safetensors format "
-            "is not available. "
-            '"pt" will load the weights in the pytorch bin format. '
-            '"safetensors" will load the weights in the safetensors format. '
-            '"npcache" will load the weights in pytorch format and store '
-            "a numpy cache to speed up the loading. "
-            '"dummy" will initialize the weights with random values, '
-            "which is mainly for profiling."
-            '"gguf" will load the weights in the gguf format. '
-            '"bitsandbytes" will load the weights using bitsandbytes '
-            "quantization.",
+                 '"auto" will try to load the weights in the safetensors format '
+                 "and fall back to the pytorch bin format if safetensors format "
+                 "is not available. "
+                 '"pt" will load the weights in the pytorch bin format. '
+                 '"safetensors" will load the weights in the safetensors format. '
+                 '"npcache" will load the weights in pytorch format and store '
+                 "a numpy cache to speed up the loading. "
+                 '"dummy" will initialize the weights with random values, '
+                 "which is mainly for profiling."
+                 '"gguf" will load the weights in the gguf format. '
+                 '"bitsandbytes" will load the weights using bitsandbytes '
+                 "quantization.",
         )
         parser.add_argument(
             "--trust-remote-code",
@@ -333,13 +333,13 @@ class ServerArgs:
             default=ServerArgs.dtype,
             choices=["auto", "half", "float16", "bfloat16", "float", "float32"],
             help="Data type for model weights and activations.\n\n"
-            '* "auto" will use FP16 precision for FP32 and FP16 models, and '
-            "BF16 precision for BF16 models.\n"
-            '* "half" for FP16. Recommended for AWQ quantization.\n'
-            '* "float16" is the same as "half".\n'
-            '* "bfloat16" for a balance between precision and range.\n'
-            '* "float" is shorthand for FP32 precision.\n'
-            '* "float32" for FP32 precision.',
+                 '* "auto" will use FP16 precision for FP32 and FP16 models, and '
+                 "BF16 precision for BF16 models.\n"
+                 '* "half" for FP16. Recommended for AWQ quantization.\n'
+                 '* "float16" is the same as "half".\n'
+                 '* "bfloat16" for a balance between precision and range.\n'
+                 '* "float" is shorthand for FP32 precision.\n'
+                 '* "float32" for FP32 precision.',
         )
         parser.add_argument(
             "--kv-cache-dtype",
@@ -399,8 +399,8 @@ class ServerArgs:
             type=str,
             default=None,
             help="The specific model version to use. It can be a branch "
-            "name, a tag name, or a commit id. If unspecified, will use "
-            "the default version.",
+                 "name, a tag name, or a commit id. If unspecified, will use "
+                 "the default version.",
         )
         parser.add_argument(
             "--skip-tokenizer-init",
@@ -432,7 +432,7 @@ class ServerArgs:
             type=int,
             default=ServerArgs.max_total_tokens,
             help="The maximum number of tokens in the memory pool. If not specified, it will be automatically calculated based on the memory usage fraction. "
-            "This option is typically used for development and debugging purposes.",
+                 "This option is typically used for development and debugging purposes.",
         )
         parser.add_argument(
             "--chunked-prefill-size",
@@ -515,6 +515,18 @@ class ServerArgs:
             type=int,
             default=ServerArgs.base_gpu_id,
             help="The base GPU ID to start allocating GPUs from. Useful when running multiple instances on the same machine.",
+        )
+        parser.add_argument(
+            "--fragment-tp-rank",
+            type=int,
+            default=ServerArgs.fragment_tp_rank,
+            help="TP rank when using fragment feature",
+        )
+        parser.add_argument(
+            "--fragment-nccl-port",
+            type=int,
+            default=ServerArgs.fragment_nccl_port,
+            help="NCCL port when using fragment feature",
         )
 
         # Logging
@@ -822,7 +834,7 @@ class ServerArgs:
             "--triton-attention-reduce-in-fp32",
             action="store_true",
             help="Cast the intermidiate attention results to fp32 to avoid possible crashes related to fp16."
-            "This only affects Triton attention kernels.",
+                 "This only affects Triton attention kernels.",
         )
         parser.add_argument(
             "--triton-attention-num-kv-splits",
@@ -835,8 +847,8 @@ class ServerArgs:
             type=int,
             default=ServerArgs.num_continuous_decode_steps,
             help="Run multiple continuous decoding steps to reduce scheduling overhead. "
-            "This can potentially increase throughput but may also increase time-to-first-token latency. "
-            "The default value is 1, meaning only run one decoding step at a time.",
+                 "This can potentially increase throughput but may also increase time-to-first-token latency. "
+                 "The default value is 1, meaning only run one decoding step at a time.",
         )
         parser.add_argument(
             "--delete-ckpt-after-loading",
@@ -900,12 +912,6 @@ def prepare_server_args(argv: List[str]) -> ServerArgs:
     raw_args = parser.parse_args(argv)
     server_args = ServerArgs.from_cli_args(raw_args)
     return server_args
-
-
-@dataclasses.dataclass
-class ServerFragmentArgs:
-    tp_rank: int
-    nccl_port: int
 
 
 @dataclasses.dataclass
