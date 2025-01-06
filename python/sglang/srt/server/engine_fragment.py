bc40c201cf4b87691630199ad001eadb8828e620
fzyzcjy <ch271828n@outlook.com>
2025-01-06 14:18:25 +0800

    more

--- python/sglang/srt/server/engine_fragment.py
+++ python/sglang/srt/server/engine_fragment.py
@@ -4,3 +4,6 @@ class EngineFragment:
     Similar to `Engine`. The difference is that, `Engine` handles TP internally, thus users only need
     to have one single `Engine`. Contrary to that, users need to have one `EngineFragment` per TP rank.
     """
+
+    def __init__(self, log_level: str = "error", *args, **kwargs):
+        TODO
