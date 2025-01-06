24b72b59318f4d9c1a618e3d06bfcf3a56f6d553
fzyzcjy <ch271828n@outlook.com>
2025-01-06 14:32:58 +0800

    more

--- python/sglang/srt/server/engine_fragment.py
+++ python/sglang/srt/server/engine_fragment.py
@@ -1,5 +1,8 @@
+from typing import Union
+
 from fastapi import Request
-from sglang.srt.managers.io_struct import GenerateReqInput
+from sglang.srt.managers.io_struct import GenerateReqInput, EmbeddingReqInput
+from sglang.srt.managers.scheduler.core import SchedulerCore
 from sglang.srt.server.engine_base import EngineBase
 
 
@@ -11,9 +14,15 @@ class EngineFragment(EngineBase):
     """
 
     def __init__(self, log_level: str = "error", *args, **kwargs):
-        TODO
+        self._scheduler_core = SchedulerCore(
+            server_args=server_args, nccl_port=nccl_port,
+            gpu_id=gpu_id, tp_rank=tp_rank, dp_rank=dp_rank,
+        )
+        self._scheduler_core.callback = TODO
 
-    async def _generate_request_impl(self, obj: GenerateReqInput, request: Request):
+    async def _generate_request_impl(self, obj: Union[GenerateReqInput, EmbeddingReqInput], request: Request):
+        self._scheduler_core.handle_generate_request(TODO)
+        self._scheduler_core.handle_embedding_request(TODO)
         return TODO
 
     def _create_abort_task_impl(self, obj: GenerateReqInput):
