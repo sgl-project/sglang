089a980e98ddc98b53b75bb86220662514fc977e
fzyzcjy <ch271828n@outlook.com>
2025-01-06 17:02:29 +0800

    more

--- examples/runtime/engine/offline_batch_inference_torchrun.py
+++ examples/runtime/engine/offline_batch_inference_torchrun.py
@@ -2,10 +2,9 @@ import datetime
 import os
 import sys
 
-from sglang.srt.managers.io_struct import TokenizedGenerateReqInput
-from sglang.srt.managers.scheduler import Scheduler
+from sglang import Engine
 from sglang.srt.sampling.sampling_params import SamplingParams
-from sglang.srt.server_args import ServerArgs, PortArgs
+from sglang.srt.server_args import ServerFragmentArgs
 from transformers import AutoTokenizer
 
 
@@ -25,7 +24,6 @@ def run():
     # torch.cuda.set_device(rank)
     # _log(f"now {os.environ['CUDA_VISIBLE_DEVICES']=}")
 
-    # TODO support dp>1
     dp_size, tp_size = 1, 4
     assert world_size == dp_size * tp_size, f'{world_size=}'
 
@@ -48,29 +46,21 @@ def run():
     # state_dict = fsdp_model.state_dict()
     # # sync weights between actor and rollout, support several format: DTensor and Megatron (sharded)
     # inference_engine.sync_model_weights(actor_weights=state_dict, load_format='dtensor')
-
-    name = sys.argv[1]
+    #
+    # name = sys.argv[1]
 
     # [Optional] build device mesh for inference engine
     # gen_device_mesh = init_device_mesh('cuda', mesh_shape=(dp_size, tp_size), mesh_dim_names=['dp', 'tp'])
     # build inference engine
-    inference_engine = Scheduler(
-        server_args=ServerArgs(
-            model_path=model_name,
-            mem_fraction_static=mem_fraction_static,
-            tp_size=tp_size,
-            dp_size=dp_size,
-        ),
-        port_args=PortArgs(
-            tokenizer_ipc_name=f'/tmp/{name}/tokenizer_ipc',
-            scheduler_input_ipc_name=f'/tmp/{name}/scheduler_input_ipc',
-            detokenizer_ipc_name=f'/tmp/{name}/detokenizer_ipc',
+    inference_engine = Engine(
+        model_path=model_name,
+        mem_fraction_static=mem_fraction_static,
+        tp_size=tp_size,
+        dp_size=dp_size,
+        fragment=ServerFragmentArgs(
+            tp_rank=tp_rank,
             nccl_port=12345,
         ),
-        gpu_id=local_rank,  # TODO
-        tp_rank=tp_rank,
-        dp_rank=None,  # TODO
-        # dp_rank=dp_rank,
     )
     _log(f'{inference_engine=}')
 
@@ -79,32 +69,15 @@ def run():
     # inference_engine.update_parallel_state(TP=device_mesh["tp"])
 
     input_text = "Today is a sunny day and I like"
-    input_ids = hf_tokenizer(input_text)['input_ids']
-    _log(f'{input_ids=}')
-
-    def hack_send_to_detokenizer_callback(out):
-        _log(f'hack_send_to_detokenizer_callback {hf_tokenizer.decode(out.decode_ids[0])=}')
-
-    inference_engine.hack_send_to_detokenizer_callback = hack_send_to_detokenizer_callback
-
-    sampling_params = SamplingParams(max_new_tokens=16)
-    sampling_params.normalize(tokenizer=None)
+    # input_ids = hf_tokenizer(input_text)['input_ids']
+    # _log(f'{input_ids=}')
 
     # generate sequence, it would be better if the output is a list of Tensor not list of list[str]
-    inference_engine.handle_generate_request(TokenizedGenerateReqInput(
-        rid='req-0',  # TODO when multi req, handle this
-        input_text=input_text,
-        input_ids=input_ids,
-        image_inputs=None,
-        sampling_params=sampling_params,
-        return_logprob=False,
-        logprob_start_len=0,
-        top_logprobs_num=0,
-        stream=True,  # TODO ?
-    ))
-
-    _log('event_loop_normal')
-    inference_engine.event_loop_normal()
+    output = inference_engine.generate(
+        prompt=input_text,
+        sampling_params=SamplingParams(max_new_tokens=16),
+    )
+    print(f'{output=}')
 
     _log('exit')
 
