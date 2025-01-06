8383da700922f6e616d2a8157b093c293daa1f81
fzyzcjy <ch271828n@outlook.com>
2025-01-06 13:42:31 +0800

    more

--- python/sglang/srt/server/fastapi_app.py
+++ python/sglang/srt/server/fastapi_app.py
@@ -1,7 +1,7 @@
 import dataclasses
 import logging
 from http import HTTPStatus
-from typing import AsyncIterator, Dict, Optional
+from typing import AsyncIterator, Optional
 
 import orjson
 from fastapi import FastAPI, File, Form, Request, UploadFile
@@ -17,7 +17,6 @@ from sglang.srt.managers.io_struct import (
     UpdateWeightFromDiskReqInput,
     UpdateWeightsFromDistributedReqInput,
 )
-from sglang.srt.managers.tokenizer_manager import TokenizerManager
 from sglang.srt.metrics.func_timer import time_func_latency
 from sglang.srt.openai_api.adapter import (
     v1_batches,
@@ -32,6 +31,7 @@ from sglang.srt.openai_api.adapter import (
     v1_retrieve_file_content,
 )
 from sglang.srt.openai_api.protocol import ModelCard, ModelList
+from sglang.srt.server.engine import Engine
 from sglang.version import __version__
 
 logger = logging.getLogger(__name__)
@@ -51,8 +51,7 @@ app.add_middleware(
 
 @dataclasses.dataclass
 class _GlobalState:
-    tokenizer_manager: TokenizerManager
-    scheduler_info: Dict  # TODO put it here?
+    engine: Engine
 
 
 _global_state: Optional[_GlobalState] = None
@@ -71,7 +70,7 @@ async def health() -> Response:
 async def health_generate(request: Request) -> Response:
     """Check the health of the inference server by generating one token."""
 
-    if _global_state.tokenizer_manager.is_generation:
+    if _global_state.engine.tokenizer_manager.is_generation:
         gri = GenerateReqInput(
             input_ids=[0], sampling_params={"max_new_tokens": 1, "temperature": 0.7}
         )
@@ -81,7 +80,7 @@ async def health_generate(request: Request) -> Response:
         )
 
     try:
-        async for _ in _global_state.tokenizer_manager.generate_request(gri, request):
+        async for _ in _global_state.engine.tokenizer_manager.generate_request(gri, request):
             break
         return Response(status_code=200)
     except Exception as e:
@@ -93,9 +92,9 @@ async def health_generate(request: Request) -> Response:
 async def get_model_info():
     """Get the model information."""
     result = {
-        "model_path": _global_state.tokenizer_manager.model_path,
-        "tokenizer_path": _global_state.tokenizer_manager.server_args.tokenizer_path,
-        "is_generation": _global_state.tokenizer_manager.is_generation,
+        "model_path": _global_state.engine.tokenizer_manager.model_path,
+        "tokenizer_path": _global_state.engine.tokenizer_manager.server_args.tokenizer_path,
+        "is_generation": _global_state.engine.tokenizer_manager.is_generation,
     }
     return result
 
@@ -103,8 +102,8 @@ async def get_model_info():
 @app.get("/get_server_info")
 async def get_server_info():
     return {
-        **dataclasses.asdict(_global_state.tokenizer_manager.server_args),  # server args
-        **_global_state.scheduler_info,
+        **dataclasses.asdict(_global_state.engine.tokenizer_manager.server_args),  # server args
+        **_global_state.engine.scheduler_info,
         "version": __version__,
     }
 
@@ -112,7 +111,7 @@ async def get_server_info():
 @app.post("/flush_cache")
 async def flush_cache():
     """Flush the radix cache."""
-    _global_state.tokenizer_manager.flush_cache()
+    _global_state.engine.tokenizer_manager.flush_cache()
     return Response(
         content="Cache flushed.\nPlease check backend logs for more details. "
                 "(When there are running or waiting requests, the operation will not be performed.)\n",
@@ -124,7 +123,7 @@ async def flush_cache():
 @app.post("/start_profile")
 async def start_profile_async():
     """Start profiling."""
-    _global_state.tokenizer_manager.start_profile()
+    _global_state.engine.tokenizer_manager.start_profile()
     return Response(
         content="Start profiling.\n",
         status_code=200,
@@ -135,7 +134,7 @@ async def start_profile_async():
 @app.post("/stop_profile")
 async def stop_profile_async():
     """Stop profiling."""
-    _global_state.tokenizer_manager.stop_profile()
+    _global_state.engine.tokenizer_manager.stop_profile()
     return Response(
         content="Stop profiling. This will take some time.\n",
         status_code=200,
@@ -146,7 +145,7 @@ async def stop_profile_async():
 @time_func_latency
 async def update_weights_from_disk(obj: UpdateWeightFromDiskReqInput, request: Request):
     """Update the weights from disk in-place without re-launching the server."""
-    success, message = await _global_state.tokenizer_manager.update_weights_from_disk(obj, request)
+    success, message = await _global_state.engine.tokenizer_manager.update_weights_from_disk(obj, request)
     content = {"success": success, "message": message}
     if success:
         return ORJSONResponse(
@@ -165,7 +164,7 @@ async def init_weights_update_group(
     obj: InitWeightsUpdateGroupReqInput, request: Request
 ):
     """Initialize the parameter update group."""
-    success, message = await _global_state.tokenizer_manager.init_weights_update_group(obj, request)
+    success, message = await _global_state.engine.tokenizer_manager.init_weights_update_group(obj, request)
     content = {"success": success, "message": message}
     if success:
         return ORJSONResponse(content, status_code=200)
@@ -178,7 +177,7 @@ async def update_weights_from_distributed(
     obj: UpdateWeightsFromDistributedReqInput, request: Request
 ):
     """Update model parameter from distributed online."""
-    success, message = await _global_state.tokenizer_manager.update_weights_from_distributed(
+    success, message = await _global_state.engine.tokenizer_manager.update_weights_from_distributed(
         obj, request
     )
     content = {"success": success, "message": message}
@@ -192,7 +191,7 @@ async def update_weights_from_distributed(
 async def get_weights_by_name(obj: GetWeightsByNameReqInput, request: Request):
     """Get model parameter by name."""
     try:
-        ret = await _global_state.tokenizer_manager.get_weights_by_name(obj, request)
+        ret = await _global_state.engine.tokenizer_manager.get_weights_by_name(obj, request)
         if ret is None:
             return _create_error_response("Get parameter by name failed")
         else:
@@ -205,7 +204,7 @@ async def get_weights_by_name(obj: GetWeightsByNameReqInput, request: Request):
 async def open_session(obj: OpenSessionReqInput, request: Request):
     """Open a session, and return its unique session id."""
     try:
-        session_id = await _global_state.tokenizer_manager.open_session(obj, request)
+        session_id = await _global_state.engine.tokenizer_manager.open_session(obj, request)
         if session_id is None:
             raise Exception(
                 "Failed to open the session. Check if a session with the same id is still open."
@@ -219,7 +218,7 @@ async def open_session(obj: OpenSessionReqInput, request: Request):
 async def close_session(obj: CloseSessionReqInput, request: Request):
     """Close the session"""
     try:
-        await _global_state.tokenizer_manager.close_session(obj, request)
+        await _global_state.engine.tokenizer_manager.close_session(obj, request)
         return Response(status_code=200)
     except Exception as e:
         return _create_error_response(e)
@@ -234,7 +233,7 @@ async def generate_request(obj: GenerateReqInput, request: Request):
 
         async def stream_results() -> AsyncIterator[bytes]:
             try:
-                async for out in _global_state.tokenizer_manager.generate_request(obj, request):
+                async for out in _global_state.engine.tokenizer_manager.generate_request(obj, request):
                     yield b"data: " + orjson.dumps(
                         out, option=orjson.OPT_NON_STR_KEYS
                     ) + b"\n\n"
@@ -248,11 +247,11 @@ async def generate_request(obj: GenerateReqInput, request: Request):
         return StreamingResponse(
             stream_results(),
             media_type="text/event-stream",
-            background=_global_state.tokenizer_manager.create_abort_task(obj),
+            background=_global_state.engine.tokenizer_manager.create_abort_task(obj),
         )
     else:
         try:
-            ret = await _global_state.tokenizer_manager.generate_request(obj, request).__anext__()
+            ret = await _global_state.engine.tokenizer_manager.generate_request(obj, request).__anext__()
             return ret
         except ValueError as e:
             logger.error(f"Error: {e}")
@@ -264,7 +263,7 @@ async def generate_request(obj: GenerateReqInput, request: Request):
 async def encode_request(obj: EmbeddingReqInput, request: Request):
     """Handle an embedding request."""
     try:
-        ret = await _global_state.tokenizer_manager.generate_request(obj, request).__anext__()
+        ret = await _global_state.engine.tokenizer_manager.generate_request(obj, request).__anext__()
         return ret
     except ValueError as e:
         return _create_error_response(e)
@@ -275,7 +274,7 @@ async def encode_request(obj: EmbeddingReqInput, request: Request):
 async def classify_request(obj: EmbeddingReqInput, request: Request):
     """Handle a reward model request. Now the arguments and return values are the same as embedding models."""
     try:
-        ret = await _global_state.tokenizer_manager.generate_request(obj, request).__anext__()
+        ret = await _global_state.engine.tokenizer_manager.generate_request(obj, request).__anext__()
         return ret
     except ValueError as e:
         return _create_error_response(e)
@@ -287,26 +286,26 @@ async def classify_request(obj: EmbeddingReqInput, request: Request):
 @app.post("/v1/completions")
 @time_func_latency
 async def openai_v1_completions(raw_request: Request):
-    return await v1_completions(_global_state.tokenizer_manager, raw_request)
+    return await v1_completions(_global_state.engine.tokenizer_manager, raw_request)
 
 
 @app.post("/v1/chat/completions")
 @time_func_latency
 async def openai_v1_chat_completions(raw_request: Request):
-    return await v1_chat_completions(_global_state.tokenizer_manager, raw_request)
+    return await v1_chat_completions(_global_state.engine.tokenizer_manager, raw_request)
 
 
 @app.post("/v1/embeddings", response_class=ORJSONResponse)
 @time_func_latency
 async def openai_v1_embeddings(raw_request: Request):
-    response = await v1_embeddings(_global_state.tokenizer_manager, raw_request)
+    response = await v1_embeddings(_global_state.engine.tokenizer_manager, raw_request)
     return response
 
 
 @app.get("/v1/models", response_class=ORJSONResponse)
 def available_models():
     """Show available models."""
-    served_model_names = [_global_state.tokenizer_manager.served_model_name]
+    served_model_names = [_global_state.engine.tokenizer_manager.served_model_name]
     model_cards = []
     for served_model_name in served_model_names:
         model_cards.append(ModelCard(id=served_model_name, root=served_model_name))
@@ -316,7 +315,7 @@ def available_models():
 @app.post("/v1/files")
 async def openai_v1_files(file: UploadFile = File(...), purpose: str = Form("batch")):
     return await v1_files_create(
-        file, purpose, _global_state.tokenizer_manager.server_args.file_storage_pth
+        file, purpose, _global_state.engine.tokenizer_manager.server_args.file_storage_pth
     )
 
 
@@ -328,13 +327,13 @@ async def delete_file(file_id: str):
 
 @app.post("/v1/batches")
 async def openai_v1_batches(raw_request: Request):
-    return await v1_batches(_global_state.tokenizer_manager, raw_request)
+    return await v1_batches(_global_state.engine.tokenizer_manager, raw_request)
 
 
 @app.post("/v1/batches/{batch_id}/cancel")
 async def cancel_batches(batch_id: str):
     # https://platform.openai.com/docs/api-reference/batch/cancel
-    return await v1_cancel_batch(_global_state.tokenizer_manager, batch_id)
+    return await v1_cancel_batch(_global_state.engine.tokenizer_manager, batch_id)
 
 
 @app.get("/v1/batches/{batch_id}")
