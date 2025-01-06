cde20c885d109d1bbccbbcf86d8fedb10365726f
fzyzcjy <ch271828n@outlook.com>
2025-01-06 19:33:15 +0800

    more

--- test/srt/test_fragment.py
+++ test/srt/test_fragment.py
@@ -1,9 +1,23 @@
 import unittest
+from multiprocessing import Process
 
 
 class TestFragment(unittest.TestCase):
     def test_fragment(self):
-        TODO
+        tp_size = 2
+
+        processes = []
+        for tp_rank in range(tp_size):
+            p = Process(target=_run_subprocess, args=(tp_rank,))
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+
+
+def _run_subprocess(tp_rank: int):
+    TODO
 
 
 if __name__ == "__main__":
