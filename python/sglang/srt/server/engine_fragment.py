a6de867ca6f29d494f24bbea2ead452e69c270ee
fzyzcjy <ch271828n@outlook.com>
2025-01-07 17:29:18 +0800

    more

--- python/sglang/srt/server/engine_fragment.py
+++ python/sglang/srt/server/engine_fragment.py
@@ -1,16 +1,21 @@
 import multiprocessing as mp
 
 from sglang.srt.managers.scheduler import run_scheduler_process
+from sglang.srt.server.subprocess_launcher import _set_envs_and_config
 from sglang.srt.server_args import EngineFragmentArgs
 
 
 class EngineFragment:
     def __init__(
-        self,
-        tp_rank: int,
-        gpu_id: int,
-        fragment_args: "EngineFragmentArgs",
+            self,
+            tp_rank: int,
+            gpu_id: int,
+            fragment_args: "EngineFragmentArgs",
     ):
+        # Not a good idea to change *current* process's env; but it seems Engine currently does so,
+        # so here EngineFragment does the same.
+        _set_envs_and_config(fragment_args.server_args)
+
         self._proc = mp.Process(
             target=run_scheduler_process,
             kwargs=dict(
