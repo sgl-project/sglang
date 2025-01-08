35d79896415d38e9d5a09218f46a6d0f5f15a5a8
fzyzcjy <ch271828n@outlook.com>
2025-01-08 16:30:30 +0800

    more

--- test/srt/test_fragment.py
+++ test/srt/test_fragment.py
@@ -4,7 +4,6 @@ import traceback
 import unittest
 from multiprocessing import Process
 
-from sglang import Engine
 from sglang.srt.server.engine_fragment import EngineFragment
 from sglang.test.test_utils import DEFAULT_SMALL_MODEL_NAME_FOR_TEST
 
@@ -14,8 +13,6 @@ _TP_SIZE = 2
 class TestFragment(unittest.TestCase):
     def test_fragment(self):
         multiprocessing.set_start_method("spawn")
-
-        queue = multiprocessing.Queue()
 
         processes = []
         output_reader, output_writer = mp.Pipe(duplex=False)
@@ -27,84 +24,57 @@ class TestFragment(unittest.TestCase):
             p.start()
             processes.append(p)
 
-        output = output_reader.recv()
-        print(output)
-        self.assertEqual(
-            output,
-            [
-                " to spend it outdoors. I decided to take a walk in the nearby park.",
-                " how to improve the performance of my website. I've been doing some research and",
-                " a new user of the platform. I am looking for a new laptop to buy",
-                " I'm looking for someone to help me with a project.\nI'm a student",
-                " the science of numbers and their properties. It is a vast and complex field that",
-            ],
-        )
+        outputs = [output_reader.recv() for _ in range(_TP_SIZE)]
+        print(outputs)
+        for output in outputs:
+            self.assertEqual(
+                output,
+                [
+                    " to spend it outdoors. I decided to take a walk in the nearby park.",
+                    " how to improve the performance of my website. I've been doing some research and",
+                    " a new user of the platform. I am looking for a new laptop to buy",
+                    " I'm looking for someone to help me with a project.\nI'm a student",
+                    " the science of numbers and their properties. It is a vast and complex field that",
+                ],
+            )
 
         for p in processes:
             p.join()
 
 
-def _run_subprocess(tp_rank: int, queue: multiprocessing.Queue, output_writer):
+def _run_subprocess(tp_rank: int, output_writer):
     try:
         print(f"subprocess[{tp_rank=}] Start")
 
-        # Engine can be put anywhere, e.g. tp_rank=0, or other places
-        if tp_rank == 0:
-            engine = Engine(
-                model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
-                mem_fraction_static=0.1,
-                tp_size=_TP_SIZE,
-                random_seed=42,
-                fragment=True,
-            )
-            print(f"subprocess[{tp_rank=}] {engine=}", flush=True)
-
-            for _ in range(_TP_SIZE):
-                queue.put(engine.fragment_args)
-
-        # can use e.g. torch.distributed to broadcast it; here for simplicity we do not init torch.distributed
-        fragment_args = queue.get()
-
         fragment = EngineFragment(
-            fragment_args=fragment_args,
+            model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
+            mem_fraction_static=0.1,
+            tp_size=_TP_SIZE,
+            random_seed=42,
+            # fragment args
             tp_rank=tp_rank,
             gpu_id=tp_rank,
+            nccl_port=TODO,
         )
         print(f"subprocess[{tp_rank=}] {fragment=}", flush=True)
 
-        if tp_rank == 0:
-            engine.await_fragments()
-            print(f"subprocess[{tp_rank=}] end wait engine launch", flush=True)
+        # NOTE: We deliberately call fragment.generate *twice* to confirm this function can be called multiple times
+        # In real batch generation, surely we should only call fragment.generate once
+        ans = []
+        for prompt in [
+            ["Today is a sunny day and I like", "I have a very good idea on"],
+            ["Hello, I am", "What is your name?", "Mathematics is defined as"],
+        ]:
+            print(f"subprocess[{tp_rank=}] Start generation", flush=True)
+            outputs = fragment.generate(
+                prompt=prompt,
+                sampling_params=[dict(max_new_tokens=16, temperature=0.0)] * len(prompt),
+            )
+            print(f"subprocess[{tp_rank=}] End generation {prompt=} {outputs=}", flush=True)
+            ans += [o["text"] for o in outputs]
 
-            ans = []
-            for prompt in [
-                ["Today is a sunny day and I like", "I have a very good idea on"],
-                ["Hello, I am", "What is your name?", "Mathematics is defined as"],
-            ]:
-                print(f"subprocess[{tp_rank=}] Start generation", flush=True)
-                outputs = engine.generate(
-                    prompt=prompt,
-                    sampling_params=[dict(max_new_tokens=16, temperature=0.0)]
-                                    * len(prompt),
-                )
-                print(
-                    f"subprocess[{tp_rank=}] End generation {tp_rank=} {prompt=} {outputs=}",
-                    flush=True,
-                )
-                ans += [o["text"] for o in outputs]
-
-            output_writer.send(ans)
-            output_writer.close()
-
-            print(f"subprocess[{tp_rank=}] engine.shutdown", flush=True)
-            engine.shutdown()
-
-            for _ in range(_TP_SIZE):
-                queue.put("END")
-
-        # Again, can use torch barrier
-        assert queue.get() == "END"
-        print(f"subprocess[{tp_rank=}] end", flush=True)
+        output_writer.send(ans)
+        output_writer.close()
 
     except Exception as e:
         print(f"subprocess[{tp_rank=}] has error: {e}", flush=True)
