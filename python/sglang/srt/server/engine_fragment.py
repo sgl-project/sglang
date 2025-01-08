9dc6510bca1ff5f84388d7d9381f8596e6209e3c
fzyzcjy <ch271828n@outlook.com>
2025-01-08 16:00:38 +0800

    more

--- python/sglang/srt/server/engine_fragment.py
+++ python/sglang/srt/server/engine_fragment.py
@@ -1,15 +1,26 @@
 from sglang.srt.managers.io_struct import GenerateReqInput
 from sglang.srt.orchestration.spmd.entrypoint import Entrypoint
 from sglang.srt.server.engine_base import EngineBase
+from sglang.srt.server_args import ServerArgs
 
 
 class EngineFragment(EngineBase):
     def __init__(
             self,
-            tp_rank: int,
+            nccl_port: int,
             gpu_id: int,
+            tp_rank: int,
+            log_level: str = "error",
+            *args,
+            **kwargs,
     ):
-        self._entrypoint = Entrypoint()
+        server_args = ServerArgs(*args, log_level=log_level, **kwargs)
+        self._entrypoint = Entrypoint(
+            server_args=server_args,
+            nccl_port=nccl_port,
+            gpu_id=gpu_id,
+            tp_rank=tp_rank,
+        )
 
     def _generate_impl(self, obj: GenerateReqInput):
         return self._entrypoint.generate(obj)
