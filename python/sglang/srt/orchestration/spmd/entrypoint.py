044bd12c8630674de051dc51c39c1531035d6c66
fzyzcjy <ch271828n@outlook.com>
2025-01-08 15:59:18 +0800

    more

--- python/sglang/srt/orchestration/spmd/entrypoint.py
+++ python/sglang/srt/orchestration/spmd/entrypoint.py
@@ -1,9 +1,23 @@
 from sglang.srt.managers.io_struct import GenerateReqInput
+from sglang.srt.managers.scheduler import Scheduler
+from sglang.srt.server_args import ServerArgs
 
 
 class Entrypoint:
-    def __init__(self):
-        TODO
+    def __init__(
+        self,
+        server_args: ServerArgs,
+        nccl_port: int,
+        gpu_id: int,
+        tp_rank: int,
+    ):
+        self._scheduler = Scheduler(
+            server_args=server_args,
+            nccl_port=nccl_port,
+            gpu_id=gpu_id,
+            tp_rank=tp_rank,
+            dp_rank=None,
+        )
 
     def generate(self, obj: GenerateReqInput):
         return TODO
