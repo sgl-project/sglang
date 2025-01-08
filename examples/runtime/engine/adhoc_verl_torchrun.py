1c1a886f8a3767ae6ef99a12674724fc6e9c26b7
fzyzcjy <ch271828n@outlook.com>
2025-01-08 18:46:26 +0800

    more

--- examples/runtime/engine/adhoc_verl_torchrun.py
+++ examples/runtime/engine/adhoc_verl_torchrun.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 
 import os
+from typing import List
+
+import torch.nn.functional as F
 from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP, ShardingStrategy, MixedPrecision, CPUOffload
 from torch.distributed.fsdp.api import ShardingStrategy, ShardedStateDictConfig, StateDictType
@@ -24,12 +27,13 @@ def main():
     assert torch.cuda.is_available(), 'CUDA must be present to run FSDP vLLM example'
     local_rank, rank, world_size = initialize_global_process_group()
 
-    local_cache_path = '~/.cache/verl/rlhf'
-    local_cache_path = os.path.expanduser(local_cache_path)
+    # local_cache_path = '~/.cache/verl/rlhf'
+    # local_cache_path = os.path.expanduser(local_cache_path)
     hdfs_path = 'Qwen/Qwen2-7B-Instruct'
 
-    from verl.utils.fs import copy_local_path_from_hdfs
-    local_model_path = copy_local_path_from_hdfs(src=hdfs_path, cache_dir=local_cache_path)
+    # from verl.utils.fs import copy_local_path_from_hdfs
+    # local_model_path = copy_local_path_from_hdfs(src=hdfs_path, cache_dir=local_cache_path)
+    local_model_path = hdfs_path
     tokenizer = AutoTokenizer.from_pretrained(local_model_path, trust_remote_code=True)
     actor_model_config = AutoConfig.from_pretrained(local_model_path, trust_remote_code=True)
     with torch.device("cuda"):
@@ -47,7 +51,6 @@ def main():
     prompts = tokenizer(preencode_prompts, return_tensors='pt', padding=True)
     input_ids = prompts['input_ids']
     attention_mask = prompts['attention_mask']
-    from verl.utils.torch_functional import pad_sequence_to_length
     input_ids = pad_sequence_to_length(input_ids, max_prompt_length, tokenizer.pad_token_id, left_pad=True).cuda()
     attention_mask = pad_sequence_to_length(attention_mask, max_prompt_length, 0, left_pad=True).cuda()
 
@@ -142,6 +145,27 @@ def initialize_global_process_group(timeout_second=36000):
         torch.cuda.set_device(local_rank)
     return local_rank, rank, world_size
 
+# COPIED FROM verl
+def pad_sequence_to_length(tensors, max_seq_len, pad_token_id, left_pad=False):
+    """
+    pad a 2D tensors (e.g. responses, logprobs) in the last dim to max_seq_length.
+    input shape: [bs, seq_length]
+    output shape: [bs, max_seq_length]
+    (0, max_seq_len - tensors.shape[-1]) means right pad to max_seq_length and no left pad
+    """
+    if tensors.shape[-1] >= max_seq_len:
+        return tensors
+    pad_tuple = (max_seq_len - tensors.shape[-1], 0) if left_pad else (0, max_seq_len - tensors.shape[-1])
+    return F.pad(tensors, pad_tuple, 'constant', pad_token_id)
+
+# COPIED FROM verl
+# NOTE(sgm): add for verl. We can optimize it by making the dataloader yield List[int] without padding.
+def _pre_process_inputs(pad_token_id, prompt_token_ids: torch.Tensor) -> List[int]:
+    # remove the left padding in the prompt token_id
+    # pad_token_id = self.llm_engine.tokenizer.pad_token_id if self.llm_engine.tokenizer.pad_token_id is not None else self.llm_engine.tokenizer.eos_token_id
+    non_pad_index = torch.nonzero(prompt_token_ids != pad_token_id, as_tuple=False)[0][0]
+    token_ids = prompt_token_ids[non_pad_index:].tolist()
+    return token_ids
 
 if __name__ == "__main__":
     main()
