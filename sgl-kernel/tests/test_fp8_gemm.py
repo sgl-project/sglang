4cac9fb925f58e3e90fa5c7053ad10d42afa099b
HandH1998 <1335248067@qq.com>
2025-01-09 17:41:46 +0800

    support bias

--- sgl-kernel/tests/test_fp8_gemm.py
+++ sgl-kernel/tests/test_fp8_gemm.py
@@ -1,7 +1,6 @@
 import unittest
 
 import torch
-from vllm._custom_ops import cutlass_scaled_mm as vllm_scaled_mm
 from vllm._custom_ops import scaled_fp8_quant as vllm_scaled_fp8_quant
 from sgl_kernel import fp8_scaled_mm
 
@@ -13,6 +12,8 @@ def torch_scaled_mm(a, b, scale_a, scale_b, out_dtype, bias):
     temp1 = o * scale_a.view(-1, 1)
     temp2 = temp1 * scale_b.view(1, -1)
     final = temp2.to(out_dtype)
+    if bias is not None:
+        final = final + bias.view(1, -1)
 
     return final
 
@@ -22,10 +23,9 @@ class TestFp8Gemm(unittest.TestCase):
         a = torch.randn((M, K), device=device)
         b = torch.randn((N, K), device=device)
 
-        scale_a = torch.randn((M,), device="cuda", dtype=torch.float32) * 0.01
-        scale_b = torch.randn((N,), device="cuda", dtype=torch.float32) * 0.01
+        scale_a = torch.randn((M,), device="cuda", dtype=torch.float32) * 0.001
+        scale_b = torch.randn((N,), device="cuda", dtype=torch.float32) * 0.001
         if with_bias:
-            # bias = torch.ones((N,), device="cuda", dtype=out_dtype) * 10
             bias = torch.randn((N,), device="cuda", dtype=out_dtype)
         else:
             bias = None
@@ -34,11 +34,9 @@ class TestFp8Gemm(unittest.TestCase):
         b_fp8 = b_fp8.t()
         a_fp8, scale_a_fp8 = vllm_scaled_fp8_quant(a, scale_a)
         o = torch_scaled_mm(a_fp8, b_fp8, scale_a_fp8, scale_b_fp8, out_dtype, bias)
-        # o1 = vllm_scaled_mm(a_fp8, b_fp8, scale_a_fp8, scale_b_fp8, out_dtype, bias)
         o1 = fp8_scaled_mm(a_fp8, b_fp8, scale_a_fp8, scale_b_fp8, out_dtype, bias)
-        max_val = max(o.abs().max().item(), o1.abs().max().item())
-        rtol = 4e-3
-        atol = max_val * rtol
+        rtol = 0.01
+        atol = 0.1
         torch.testing.assert_close(o, o1, rtol=rtol, atol=atol)
         print(f"M={M}, N={N}, K={K}, with_bias={with_bias}, out_dtype={out_dtype}: OK")
 
@@ -46,13 +44,8 @@ class TestFp8Gemm(unittest.TestCase):
         Ms = [1, 128, 512, 1024, 4096]
         Ns = [16, 128, 512, 1024, 4096]
         Ks = [512, 1024, 4096, 8192, 16384]
-        # Ms = [128]
-        # Ns = [512]
-        # Ks = [4096]
-        # bias_opts = [True, False]
-        bias_opts = [False]
+        bias_opts = [True, False]
         out_dtypes = [torch.bfloat16, torch.float16]
-        # out_dtypes = [torch.float16]
         for M in Ms:
             for N in Ns:
                 for K in Ks:
