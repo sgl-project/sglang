fe49867e69c7e25c377e998ef6b7e3964306a541
fzyzcjy <ch271828n@outlook.com>
2025-01-10 10:27:20 +0800

    more

--- python/sglang/srt/distributed/parallel_state.py
+++ python/sglang/srt/distributed/parallel_state.py
@@ -5,40 +5,11 @@ import vllm.distributed.parallel_state as _ps
 
 logger = logging.getLogger(__name__)
 
-def init_distributed_environment(
-    world_size: int = -1,
-    rank: int = -1,
-    distributed_init_method: str = "env://",
-    local_rank: int = -1,
-    backend: str = "nccl",
+
+def init_distributed_environment_via_existing(
+    local_rank: int,
+    backend: str,
 ):
-    logger.debug(
-        "world_size=%d rank=%d local_rank=%d "
-        "distributed_init_method=%s backend=%s", world_size, rank, local_rank,
-        distributed_init_method, backend)
-    if not torch.distributed.is_initialized():
-        assert distributed_init_method is not None, (
-            "distributed_init_method must be provided when initializing "
-            "distributed environment")
-        # this backend is used for WORLD
-        torch.distributed.init_process_group(
-            backend=backend,
-            init_method=distributed_init_method,
-            world_size=world_size,
-            rank=rank)
-    # set the local rank
-    # local_rank is not available in torch ProcessGroup,
-    # see https://github.com/pytorch/pytorch/issues/122816
-    if local_rank == -1:
-        # local rank not set, this usually happens in single-node
-        # setting, where we can use rank as local rank
-        if distributed_init_method == "env://":
-            local_rank = envs.LOCAL_RANK
-        else:
-            local_rank = rank
-    if _ps._WORLD is None:
-        ranks = list(range(torch.distributed.get_world_size()))
-        _ps._WORLD = init_world_group(ranks, local_rank, backend)
-    else:
-        assert _ps._WORLD.world_size == torch.distributed.get_world_size(), (
-            "world group already initialized with a different world size")
+    assert _ps._WORLD is None
+    ranks = list(range(torch.distributed.get_world_size()))
+    _ps._WORLD = init_world_group(ranks, local_rank, backend)
