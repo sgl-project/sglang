8910aff414665fac713d11a73a0aabba00fe0336
fzyzcjy <ch271828n@outlook.com>
2025-01-10 16:29:21 +0800

    Revert "more"
    This reverts commit 95f4158107a8cc9bd4fbdfac9abfd53daf11770d.

--- python/sglang/srt/model_loader/weight_utils.py
+++ python/sglang/srt/model_loader/weight_utils.py
@@ -489,9 +489,6 @@ def convert_pyslice_to_tensor(x: Any) -> torch.Tensor:
 def default_weight_loader(param: torch.Tensor, loaded_weight: torch.Tensor) -> None:
     """Default weight loader."""
     try:
-        if isinstance(loaded_weight, DTensor):
-            loaded_weight = loaded_weight.full_tensor()
-
         if param.numel() == 1 and loaded_weight.numel() == 1:
             # Sometimes scalar values aren't considered tensors with shapes
             # so if both param and loaded_weight are a scalar,
