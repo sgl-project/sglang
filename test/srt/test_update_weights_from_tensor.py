8be0a2ec7645f0e6ffc3d73367c0aa9e6635ae34
fzyzcjy <ch271828n@outlook.com>
2025-01-12 18:20:14 +0800

    more

--- test/srt/test_update_weights_from_tensor.py
+++ test/srt/test_update_weights_from_tensor.py
@@ -1,4 +1,3 @@
-import time
 import unittest
 
 import sglang as sgl
@@ -7,23 +6,23 @@ from sglang.test.test_utils import DEFAULT_SMALL_MODEL_NAME_FOR_TEST
 
 
 class TestUpdateWeightsFromTensor(unittest.TestCase):
-    def test_update_weights_from_tensor_load_format_default(self):
-        engine = sgl.Engine(model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST)
-
-        param_names = [f"model.layers.{i}.mlp.up_proj.weight" for i in range(6, 16)]
-
-        _check_param(engine, param_names[0], [0.0087, -0.0214, -0.0004, 0.0039, 0.0110])
-
-        new_tensor = torch.full((16384, 2048), 1.5)
-
-        time_start = time.time()
-        engine.update_weights_from_tensor([(x, new_tensor) for x in param_names])
-        print(f"Time delta: {time.time() - time_start:.03f}")
-
-        for param_name in param_names[:3]:
-            _check_param(engine, param_name, [1.5] * 5)
-
-        engine.shutdown()
+    # def test_update_weights_from_tensor_load_format_default(self):
+    #     engine = sgl.Engine(model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST)
+    #
+    #     param_names = [f"model.layers.{i}.mlp.up_proj.weight" for i in range(6, 16)]
+    #
+    #     _check_param(engine, param_names[0], [0.0087, -0.0214, -0.0004, 0.0039, 0.0110])
+    #
+    #     new_tensor = torch.full((16384, 2048), 1.5)
+    #
+    #     time_start = time.time()
+    #     engine.update_weights_from_tensor([(x, new_tensor) for x in param_names])
+    #     print(f"Time delta: {time.time() - time_start:.03f}")
+    #
+    #     for param_name in param_names[:3]:
+    #         _check_param(engine, param_name, [1.5] * 5)
+    #
+    #     engine.shutdown()
 
     def test_update_weights_from_tensor_load_format_direct(self):
         engine = sgl.Engine(model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST)
@@ -31,9 +30,9 @@ class TestUpdateWeightsFromTensor(unittest.TestCase):
         write_param_name = f"model.layers.6.self_attn.qkv_proj.weight"
         read_param_name = f"model.layers.6.self_attn.k_proj.weight"
 
-        _check_param(engine, read_param_name, [0.0087, -0.0214, -0.0004, 0.0039, 0.0110])
+        _check_param(engine, read_param_name, [-0.0198, 0.0227, 0.0168, 0.0232, -0.0178])
 
-        new_tensor = torch.full((16384, 2048), 1.5)
+        new_tensor = torch.full((3072, 2048), 1.5)
         engine.update_weights_from_tensor([(write_param_name, new_tensor)], load_format='direct')
 
         _check_param(engine, read_param_name, [1.5] * 5)
