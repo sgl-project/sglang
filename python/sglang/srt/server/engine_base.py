69bb1abdc0889a94a76c046d0523f955e2e6944c
fzyzcjy <ch271828n@outlook.com>
2025-01-12 18:23:18 +0800

    doc

--- python/sglang/srt/server/engine_base.py
+++ python/sglang/srt/server/engine_base.py
@@ -7,17 +7,17 @@ from sglang.srt.managers.io_struct import GenerateReqInput
 class EngineBase:
     # Make it in base class to ensure API is exactly the same, as well as extracting common logic
     def generate(
-            self,
-            # The input prompt. It can be a single prompt or a batch of prompts.
-            prompt: Optional[Union[List[str], str]] = None,
-            sampling_params: Optional[Union[List[Dict], Dict]] = None,
-            # The token ids for text; one can either specify text or input_ids.
-            input_ids: Optional[Union[List[List[int]], List[int]]] = None,
-            return_logprob: Optional[Union[List[bool], bool]] = False,
-            logprob_start_len: Optional[Union[List[int], int]] = None,
-            top_logprobs_num: Optional[Union[List[int], int]] = None,
-            lora_path: Optional[List[Optional[str]]] = None,
-            stream: bool = False,
+        self,
+        # The input prompt. It can be a single prompt or a batch of prompts.
+        prompt: Optional[Union[List[str], str]] = None,
+        sampling_params: Optional[Union[List[Dict], Dict]] = None,
+        # The token ids for text; one can either specify text or input_ids.
+        input_ids: Optional[Union[List[List[int]], List[int]]] = None,
+        return_logprob: Optional[Union[List[bool], bool]] = False,
+        logprob_start_len: Optional[Union[List[int], int]] = None,
+        top_logprobs_num: Optional[Union[List[int], int]] = None,
+        lora_path: Optional[List[Optional[str]]] = None,
+        stream: bool = False,
     ):
         obj = GenerateReqInput(
             text=prompt,
@@ -33,7 +33,13 @@ class EngineBase:
 
     def update_weights_from_tensor(self, named_tensors: List[Tuple[str, torch.Tensor]],
                                    load_format: Optional[str] = None):
-        """Update weights from distributed source."""
+        """
+        Update weights from distributed source.
+
+        Args:
+            named_tensors: The name and value of the tensors used to update weights
+            load_format: `None` for default HF format, and `direct` for 1-to-1 matching the raw SGLang format
+        """
         raise NotImplementedError
 
     def _generate_impl(self, obj: GenerateReqInput):
