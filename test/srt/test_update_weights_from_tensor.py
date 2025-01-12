a4e6f703e80f4745881862df5950d16b8fb7cc4f
fzyzcjy <ch271828n@outlook.com>
2025-01-12 18:14:49 +0800

    more

--- test/srt/test_update_weights_from_tensor.py
+++ test/srt/test_update_weights_from_tensor.py
@@ -8,12 +8,6 @@ from sglang.test.test_utils import DEFAULT_SMALL_MODEL_NAME_FOR_TEST
 
 class TestUpdateWeightsFromTensor(unittest.TestCase):
     def test_update_weights_from_tensor_load_format_default(self):
-        self._test_raw(load_format=None)
-
-    def test_update_weights_from_tensor_load_format_direct(self):
-        self._test_raw(load_format='direct')
-
-    def _test_raw(self, load_format):
         engine = sgl.Engine(model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST)
 
         param_names = [f"model.layers.{i}.mlp.up_proj.weight" for i in range(6, 16)]
@@ -23,11 +17,25 @@ class TestUpdateWeightsFromTensor(unittest.TestCase):
         new_tensor = torch.full((16384, 2048), 1.5)
 
         time_start = time.time()
-        engine.update_weights_from_tensor([(x, new_tensor) for x in param_names], load_format=load_format)
+        engine.update_weights_from_tensor([(x, new_tensor) for x in param_names])
         print(f"Time delta: {time.time() - time_start:.03f}")
 
         for param_name in param_names[:3]:
             _check_param(engine, param_name, [1.5] * 5)
+
+        engine.shutdown()
+
+    def test_update_weights_from_tensor_load_format_direct(self):
+        engine = sgl.Engine(model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST)
+
+        param_name = f"model.layers.6.self_attn.qkv_proj.weight"
+
+        _check_param(engine, param_name, [0.0087, -0.0214, -0.0004, 0.0039, 0.0110])
+
+        new_tensor = torch.full((16384, 2048), 1.5)
+        engine.update_weights_from_tensor([(param_name, new_tensor)], load_format='direct')
+
+        _check_param(engine, param_name, [1.5] * 5)
 
         engine.shutdown()
 
