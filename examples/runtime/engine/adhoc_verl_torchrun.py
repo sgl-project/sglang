0a8a7d23f906c1eff1645eeff2aeb16669ac6319
fzyzcjy <ch271828n@outlook.com>
2025-01-13 14:29:53 +0800

    fmt

--- examples/runtime/engine/adhoc_verl_torchrun.py
+++ examples/runtime/engine/adhoc_verl_torchrun.py
@@ -17,8 +17,6 @@ from typing import List
 
 import torch
 import torch.nn.functional as F
-from sglang.srt.distributed import ParallelProcessGroups
-from sglang.srt.server.engine_fragment import EngineFragment
 from torch.distributed.device_mesh import init_device_mesh
 from torch.distributed.fsdp import CPUOffload
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
@@ -29,6 +27,9 @@ from torch.distributed.fsdp.api import (
     StateDictType,
 )
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
+
+from sglang.srt.distributed import ParallelProcessGroups
+from sglang.srt.server.engine_fragment import EngineFragment
 
 
 def main():
