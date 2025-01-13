aa0df0c82d7bb5c3e0d0b66a865299ce0218a8f8
fzyzcjy <ch271828n@outlook.com>
2025-01-13 14:53:58 +0800

    fix text

--- scripts/ci_install_dependency.sh
+++ scripts/ci_install_dependency.sh
@@ -12,7 +12,7 @@ bash "${SCRIPT_DIR}/killall_sglang.sh"
 pip install --upgrade pip
 pip install -e "python[all]" --find-links https://flashinfer.ai/whl/cu124/torch2.4/flashinfer/
 
-# Force reinstall flashinfer
+# Force reinstall flashinfer and torch_memory_saver
 pip install flashinfer==0.1.6 --find-links ${FLASHINFER_REPO} --force-reinstall --no-deps
 pip install torch_memory_saver --force-reinstall
 
