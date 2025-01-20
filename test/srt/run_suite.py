1972324944382cdac6a97f0a5789043fe233ffa0
Lianmin Zheng <lianminzheng@gmail.com>
2025-01-19 17:25:50 -0800

    balance tests

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -42,8 +42,7 @@ suites = {
         "test_srt_endpoint.py",
         "test_torch_compile.py",
         "test_torch_compile_moe.py",
-        # Temporarily disable this because it requires PyTorch >= 2.5
-        # "test_torch_native_attention_backend.py",
+        "test_torch_native_attention_backend.py",
         "test_torchao.py",
         "test_triton_attention_kernels.py",
         "test_triton_attention_backend.py",
