60b0d17f212c94757a034154de7e16fba3161a33
Yineng Zhang <me@zhyncs.com>
2025-01-21 20:44:49 +0800

    feat: add flashinfer as 3rdparty and use rmsnorm as example (#3033)

--- sgl-kernel/src/sgl-kernel/__init__.py
+++ sgl-kernel/src/sgl-kernel/__init__.py
@@ -6,6 +6,7 @@ from sgl_kernel.ops import (
     int8_scaled_mm,
     moe_align_block_size,
     register_graph_buffers,
+    rmsnorm,
     rotary_embedding,
     sampling_scaling_penalties,
 )
@@ -20,4 +21,5 @@ __all__ = [
     "get_graph_buffer_ipc_meta",
     "register_graph_buffers",
     "rotary_embedding",
+    "rmsnorm",
 ]
