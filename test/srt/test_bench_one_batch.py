e82c3e303ca9a97c6bab84c6852faa8d6d305668
Lianmin Zheng <lianminzheng@gmail.com>
2025-01-20 22:01:00 -0800

    update

--- test/srt/test_bench_one_batch.py
+++ test/srt/test_bench_one_batch.py
@@ -10,36 +10,36 @@ from sglang.test.test_utils import (
 
 
 class TestBenchOneBatch(unittest.TestCase):
-    def test_default(self):
+    def test_bs1(self):
         output_throughput = run_bench_one_batch(DEFAULT_MODEL_NAME_FOR_TEST, [])
 
         if is_in_ci():
             write_github_step_summary(
-                f"### test_default\n"
+                f"### test_bs1\n"
                 f"output_throughput : {output_throughput:.2f} token/s\n"
             )
             self.assertGreater(output_throughput, 135)
 
-    def test_moe_tp2(self):
+    def test_moe_tp2_bs1(self):
         output_throughput = run_bench_one_batch(
             DEFAULT_MOE_MODEL_NAME_FOR_TEST, ["--tp", "2"]
         )
 
         if is_in_ci():
             write_github_step_summary(
-                f"### test_moe_tp2\n"
+                f"### test_moe_tp2_bs1\n"
                 f"output_throughput : {output_throughput:.2f} token/s\n"
             )
             self.assertGreater(output_throughput, 125)
 
-    def test_torch_compile_tp2(self):
+    def test_torch_compile_tp2_bs1(self):
         output_throughput = run_bench_one_batch(
             DEFAULT_MODEL_NAME_FOR_TEST, ["--tp", "2", "--enable-torch-compile"]
         )
 
         if is_in_ci():
             write_github_step_summary(
-                f"### test_torch_compile_tp2\n"
+                f"### test_torch_compile_tp2_bs1\n"
                 f"output_throughput : {output_throughput:.2f} token/s\n"
             )
             self.assertGreater(output_throughput, 250)
