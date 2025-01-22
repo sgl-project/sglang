5636d44c17b24733b5bf0685cddecc92f7287228
Harsh Menon <harsh@nod-labs.com>
2025-01-21 16:13:53 -0800

    Add wave backend to sglang

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -1313,6 +1313,7 @@ class ServerArgs:
             "trtllm_mla",
             "trtllm_mha",
             "dual_chunk_flash_attn",
+            "wave"
         ]
         parser.add_argument(
             "--attention-backend",
