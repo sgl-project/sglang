77e6f8606d9b8e4961d768a1fdbe6b251708630b
Ke Bao <ISPObaoke@163.com>
2025-01-25 23:57:09 +0800

    Update whl index path (#3128)

--- scripts/update_kernel_whl_index.py
+++ scripts/update_kernel_whl_index.py
@@ -8,7 +8,7 @@ for path in sorted(pathlib.Path("sgl-kernel/dist").glob("*.whl")):
     with open(path, "rb") as f:
         sha256 = hashlib.sha256(f.read()).hexdigest()
     ver = re.findall(r"sgl_kernel-([0-9.]+(?:\.post[0-9]+)?)-", path.name)[0]
-    index_dir = pathlib.Path(f"sgl-whl/cu118")
+    index_dir = pathlib.Path(f"sgl-whl/cu118/sgl-kernel")
     index_dir.mkdir(exist_ok=True)
     base_url = "https://github.com/sgl-project/whl/releases/download"
     full_url = f"{base_url}/v{ver}/{path.name}#sha256={sha256}"
