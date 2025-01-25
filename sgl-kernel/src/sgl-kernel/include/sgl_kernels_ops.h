ec06e03f633e1c00340b55bc1c371c40e2ed9125
yinfan98 <1106310035@qq.com>
2025-01-26 00:03:35 +0800

    Update sgl_kernels_ops.h

--- sgl-kernel/src/sgl-kernel/include/sgl_kernels_ops.h
+++ sgl-kernel/src/sgl-kernel/include/sgl_kernels_ops.h
@@ -3,6 +3,23 @@
 
 #include <torch/extension.h>
 #include "utils.h"
+#include <Python.h>
+
+#define _CONCAT(A, B) A##B
+#define CONCAT(A, B) _CONCAT(A, B)
+
+#define _STRINGIFY(A) #A
+#define STRINGIFY(A) _STRINGIFY(A)
+
+#define TORCH_LIBRARY_EXPAND(NAME, MODULE) TORCH_LIBRARY(NAME, MODULE)
+
+#define REGISTER_EXTENSION(NAME)                                               \
+  PyMODINIT_FUNC CONCAT(PyInit_, NAME)() {                                     \
+    static struct PyModuleDef module = {PyModuleDef_HEAD_INIT,                 \
+                                        STRINGIFY(NAME), nullptr, 0, nullptr}; \
+    return PyModule_Create(&module);                                           \
+  }
+
 
 // trt_reduce
 using fptr_t = int64_t;
