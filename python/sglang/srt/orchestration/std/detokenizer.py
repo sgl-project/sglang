91c604d6781ae6e18fb34a2d0f1527b88a1121ec
fzyzcjy <ch271828n@outlook.com>
2025-01-25 10:12:49 +0800

    extract detokenizer_manager.init

--- python/sglang/srt/orchestration/std/detokenizer.py
+++ python/sglang/srt/orchestration/std/detokenizer.py
@@ -1,15 +1,10 @@
 import logging
-import signal
 
-import psutil
-import setproctitle
 import zmq
 
 from sglang.srt.managers.detokenizer_manager import DetokenizerManager
-from sglang.srt.managers.io_struct import BatchEmbeddingOut, BatchTokenIDOut
-from sglang.srt.server_args import PortArgs, ServerArgs
-from sglang.srt.utils import configure_logger, get_zmq_socket
-from sglang.utils import TypeBasedDispatcher, get_exception_traceback
+from sglang.srt.server_args import PortArgs
+from sglang.srt.utils import get_zmq_socket
 
 logger = logging.getLogger(__name__)
 
@@ -18,4 +13,11 @@ class DetokenizerManagerCommunicator:
     def __init__(self, core: DetokenizerManager, port_args: PortArgs):
         self.core = core
 
-        TODO
+        # Init inter-process communication
+        context = zmq.Context(2)
+        self.recv_from_scheduler = get_zmq_socket(
+            context, zmq.PULL, port_args.detokenizer_ipc_name, True
+        )
+        self.send_to_tokenizer = get_zmq_socket(
+            context, zmq.PUSH, port_args.tokenizer_ipc_name, False
+        )
