d35b5f70ed8e33c2bbf82e07674cb82f9bff3e73
Yineng Zhang <me@zhyncs.com>
2025-02-01 18:56:44 +0800

    update and simplify CustomOp (#3249)

--- python/sglang/srt/model_executor/cuda_graph_runner.py
+++ python/sglang/srt/model_executor/cuda_graph_runner.py
@@ -21,8 +21,8 @@ from typing import TYPE_CHECKING, Callable
 
 import torch
 import tqdm
-from vllm.model_executor.custom_op import CustomOp
 
+from sglang.srt.custom_op import CustomOp
 from sglang.srt.distributed import get_tensor_model_parallel_rank
 from sglang.srt.distributed.parallel_state import GroupCoordinator, graph_capture
 from sglang.srt.layers.logits_processor import LogitsProcessorOutput
