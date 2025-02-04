0d5728d821b23543f2f24567e6ad33a0dea8364e
Geon Park <mujjingun@gmail.com>
2025-02-05 00:14:27 +0900

    add test for hip attention

---
+++ test/srt/test_hip_attention_backend.py
@@ -0,0 +1,68 @@
+"""
+Usage:
+python3 -m unittest test_hip_attention_backend.TestHiPAttnBackend.test_mmlu
+"""
+
+import unittest
+from types import SimpleNamespace
+
+from sglang.srt.utils import kill_process_tree
+from sglang.test.run_eval import run_eval
+from sglang.test.test_utils import (
+    DEFAULT_MODEL_NAME_FOR_TEST,
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    is_in_ci,
+    popen_launch_server,
+    run_bench_one_batch,
+)
+
+
+class TestHiPAttnBackend(unittest.TestCase):
+    def test_latency(self):
+        output_throughput = run_bench_one_batch(
+            DEFAULT_MODEL_NAME_FOR_TEST,
+            [
+                "--enable-hip-attention",
+                "--cuda-graph-max-bs",
+                "1",
+            ],
+        )
+
+        print("THROUGHPUT =", output_throughput)
+
+        if is_in_ci():
+            self.assertGreater(output_throughput, 153)
+
+    def test_mmlu(self):
+        model = DEFAULT_MODEL_NAME_FOR_TEST
+        base_url = DEFAULT_URL_FOR_TEST
+        process = popen_launch_server(
+            model,
+            base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--enable-hip-attention",
+                "--cuda-graph-max-bs",
+                "1",
+            ],
+        )
+
+        try:
+            args = SimpleNamespace(
+                base_url=base_url,
+                model=model,
+                eval_name="mmlu",
+                num_examples=64,
+                num_threads=32,
+            )
+
+            metrics = run_eval(args)
+
+            self.assertGreaterEqual(metrics["score"], 0.65)
+        finally:
+            kill_process_tree(process.pid)
+
+
+if __name__ == "__main__":
+    unittest.main()
