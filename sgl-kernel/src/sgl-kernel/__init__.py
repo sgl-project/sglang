1ccaef0303532fd09bf11be525e50efed8546d85
Hubert Lu <Hubert.Lu@amd.com>
2025-02-08 00:55:26 +0000

    Add custom_ar in sgl-kernel

--- sgl-kernel/src/sgl-kernel/__init__.py
+++ sgl-kernel/src/sgl-kernel/__init__.py
@@ -1,51 +1,131 @@
-from sgl_kernel.ops import (
-    apply_rope_with_cos_sin_cache_inplace,
-    bmm_fp8,
-    custom_dispose,
-    custom_reduce,
-    fp8_scaled_mm,
-    fused_add_rmsnorm,
-    gelu_and_mul,
-    gelu_tanh_and_mul,
-    gemma_fused_add_rmsnorm,
-    gemma_rmsnorm,
-    get_graph_buffer_ipc_meta,
-    init_custom_reduce,
-    int8_scaled_mm,
-    lightning_attention_decode,
-    min_p_sampling_from_probs,
-    moe_align_block_size,
-    register_graph_buffers,
-    rmsnorm,
-    sampling_scaling_penalties,
-    silu_and_mul,
-    top_k_renorm_prob,
-    top_k_top_p_sampling_from_probs,
-    top_p_renorm_prob,
-)
+import torch
+if torch.version.hip is not None:
+    # TODO (hubert): check this for custom_reduce
+    # init_custom_ar
+    # all_reduce_reg
+    # all_reduce_unreg
+    # dispose
+    # meta_size
+    # register_buffer
+    # get_graph_buffer_ipc_meta
+    # register_graph_buffers
+    # allocate_meta_buffer
+    # get_meta_buffer_ipc_handle
+    from sgl_kernel.ops import (
+        all_reduce_reg,        # TODO (hubert), ok
+        all_reduce_unreg,      # TODO (hubert), ok
+        allocate_meta_buffer,  # TODO (hubert), ok
+        apply_rope_with_cos_sin_cache_inplace,
+        bmm_fp8,
+        dispose,              # TODO (hubert), ok
+        fp8_scaled_mm,
+        fused_add_rmsnorm,
+        gelu_and_mul,
+        gelu_tanh_and_mul,
+        gemma_fused_add_rmsnorm,
+        gemma_rmsnorm,
+        get_graph_buffer_ipc_meta,  # TODO (hubert), ok
+        get_meta_buffer_ipc_handle, # TODO (hubert), ok
+        init_custom_ar,             # TODO (hubert), ok
+        int8_scaled_mm,
+        lightning_attention_decode,
+        meta_size,                  # TODO (hubert), ok
+        min_p_sampling_from_probs,
+        moe_align_block_size,
+        register_buffer,            # TODO (hubert), ok
+        register_graph_buffers,     # TODO (hubert), ok
+        rmsnorm,
+        sampling_scaling_penalties,
+        silu_and_mul,
+        top_k_renorm_prob,
+        top_k_top_p_sampling_from_probs,
+        top_p_renorm_prob,
+    )
 
-__all__ = [
-    "apply_rope_with_cos_sin_cache_inplace",
-    "bmm_fp8",
-    "custom_dispose",
-    "custom_reduce",
-    "fp8_scaled_mm",
-    "fused_add_rmsnorm",
-    "gelu_and_mul",
-    "gelu_tanh_and_mul",
-    "gemma_fused_add_rmsnorm",
-    "gemma_rmsnorm",
-    "get_graph_buffer_ipc_meta",
-    "init_custom_reduce",
-    "int8_scaled_mm",
-    "lightning_attention_decode",
-    "min_p_sampling_from_probs",
-    "moe_align_block_size",
-    "register_graph_buffers",
-    "rmsnorm",
-    "sampling_scaling_penalties",
-    "silu_and_mul",
-    "top_k_renorm_prob",
-    "top_k_top_p_sampling_from_probs",
-    "top_p_renorm_prob",
-]
+    __all__ = [
+        "all_reduce_reg",       # TODO (hubert), ok
+        "all_reduce_unreg",     # TODO (hubert), ok
+        "allocate_meta_buffer", # TODO (hubert), ok
+        "apply_rope_with_cos_sin_cache_inplace",
+        "bmm_fp8",
+        "dispose",              # TODO (hubert), ok
+        "fp8_scaled_mm",
+        "fused_add_rmsnorm",
+        "gelu_and_mul",
+        "gelu_tanh_and_mul",
+        "gemma_fused_add_rmsnorm",
+        "gemma_rmsnorm",
+        "get_graph_buffer_ipc_meta",  # TODO (hubert), ok
+        "get_meta_buffer_ipc_handle", # TODO (hubert), ok
+        "init_custom_ar",             # TODO (hubert), ok
+        "int8_scaled_mm",
+        "lightning_attention_decode",
+        "meta_size",                  # TODO (hubert), ok
+        "min_p_sampling_from_probs",
+        "moe_align_block_size",
+        "register_buffer",            # TODO (hubert), ok
+        "register_graph_buffers",     # TODO (hubert), ok
+        "rmsnorm",
+        "sampling_scaling_penalties",
+        "silu_and_mul",
+        "top_k_renorm_prob",
+        "top_k_top_p_sampling_from_probs",
+        "top_p_renorm_prob",
+    ]
+else:
+    # get_graph_buffer_ipc_meta, # TODO (hubert)
+    # init_custom_reduce,        # TODO (hubert)
+    # get_graph_buffer_ipc_meta, # TODO (hubert)
+    # init_custom_reduce,        # TODO (hubert)
+    # register_graph_buffers,    # TODO (hubert)
+    from sgl_kernel.ops import (
+        apply_rope_with_cos_sin_cache_inplace,
+        bmm_fp8,
+        custom_dispose, # TODO (hubert)
+        custom_reduce,  # TODO (hubert)
+        fp8_scaled_mm,
+        fused_add_rmsnorm,
+        gelu_and_mul,
+        gelu_tanh_and_mul,
+        gemma_fused_add_rmsnorm,
+        gemma_rmsnorm,
+        get_graph_buffer_ipc_meta, # TODO (hubert)
+        init_custom_reduce,        # TODO (hubert)
+        int8_scaled_mm,
+        lightning_attention_decode,
+        min_p_sampling_from_probs,
+        moe_align_block_size,
+        register_graph_buffers,    # TODO (hubert)
+        rmsnorm,
+        sampling_scaling_penalties,
+        silu_and_mul,
+        top_k_renorm_prob,
+        top_k_top_p_sampling_from_probs,
+        top_p_renorm_prob,
+    )
+
+    __all__ = [
+        "apply_rope_with_cos_sin_cache_inplace",
+        "bmm_fp8",
+        "custom_dispose",    # TODO (hubert)
+        "custom_reduce",     # TODO (hubert)
+        "fp8_scaled_mm",
+        "fused_add_rmsnorm",
+        "gelu_and_mul",
+        "gelu_tanh_and_mul",
+        "gemma_fused_add_rmsnorm",
+        "gemma_rmsnorm",
+        "get_graph_buffer_ipc_meta",    # TODO (hubert)
+        "init_custom_reduce",           # TODO (hubert)
+        "int8_scaled_mm",
+        "lightning_attention_decode",
+        "min_p_sampling_from_probs",
+        "moe_align_block_size",
+        "register_graph_buffers",       # TODO (hubert)
+        "rmsnorm",
+        "sampling_scaling_penalties",
+        "silu_and_mul",
+        "top_k_renorm_prob",
+        "top_k_top_p_sampling_from_probs",
+        "top_p_renorm_prob",
+    ]
--- sgl-kernel/src/sgl-kernel/ops/__init__.py
+++ sgl-kernel/src/sgl-kernel/ops/__init__.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Union, List
 
 import sgl_kernel.ops._kernels
 import torch
@@ -63,30 +63,66 @@ def apply_rope_with_cos_sin_cache_inplace(
             cuda_stream=_get_cuda_stream(device),
         )
 
+if torch.version.hip is not None:
+    # TODO (hubert): custom allreduce
+    def init_custom_ar(meta: torch.Tensor, rank_data: torch.Tensor,
+                    handles: List[str], offsets: List[int], rank: int,
+                    full_nvlink: bool) -> int:
+        return torch.ops.sgl_kernels.init_custom_ar(meta, rank_data, handles,
+                                                    offsets, rank, full_nvlink)
 
-def init_custom_reduce(
-    rank_id, num_devices, rank_data, buffers, tmp_buffers, barrier_in, barrier_out
-):
-    return torch.ops.sgl_kernels.init_custom_ar(
+    def all_reduce_reg(fa: int, inp: torch.Tensor, out: torch.Tensor) -> None:
+        torch.ops.sgl_kernels.all_reduce_reg(fa, inp, out)
+
+    def all_reduce_unreg(fa: int, inp: torch.Tensor, reg_buffer: torch.Tensor,
+                        out: torch.Tensor) -> None:
+        torch.ops.sgl_kernels.all_reduce_unreg(fa, inp, reg_buffer, out)
+
+    def dispose(fa: int) -> None:
+        torch.ops.sgl_kernels.dispose(fa)
+
+    def meta_size() -> int:
+        return torch.ops.sgl_kernels.meta_size()
+
+    def register_buffer(fa: int, t: torch.Tensor, handles: List[str],
+                        offsets: List[int]) -> None:
+        return torch.ops.sgl_kernels.register_buffer(fa, t, handles, offsets)
+
+    def get_graph_buffer_ipc_meta(fa: int) -> Tuple[torch.Tensor, List[int]]:
+        return torch.ops.sgl_kernels.get_graph_buffer_ipc_meta(fa)
+
+    def register_graph_buffers(fa: int, handles: List[str],
+                            offsets: List[List[int]]) -> None:
+        torch.ops.sgl_kernels.register_graph_buffers(fa, handles, offsets)
+
+    def allocate_meta_buffer(size: int) -> torch.Tensor:
+        return torch.ops.sgl_kernels.allocate_meta_buffer(size)
+
+    def get_meta_buffer_ipc_handle(inp: torch.Tensor) -> torch.Tensor:
+        return torch.ops.sgl_kernels.get_meta_buffer_ipc_handle(inp)
+else:
+    # TODO (hubert): trt_reduce
+    def init_custom_reduce(
         rank_id, num_devices, rank_data, buffers, tmp_buffers, barrier_in, barrier_out
-    )
+    ):
+        return torch.ops.sgl_kernels.init_custom_ar(
+            rank_id, num_devices, rank_data, buffers, tmp_buffers, barrier_in, barrier_out
+        )
+
+    def custom_dispose(fa):
+        torch.ops.sgl_kernels.dispose(fa)
 
 
-def custom_dispose(fa):
-    torch.ops.sgl_kernels.dispose(fa)
+    def custom_reduce(fa, inp, out):
+        torch.ops.sgl_kernels.all_reduce(fa, inp, out)
 
 
-def custom_reduce(fa, inp, out):
-    torch.ops.sgl_kernels.all_reduce(fa, inp, out)
+    def get_graph_buffer_ipc_meta(fa):
+        return torch.ops.sgl_kernels.get_graph_buffer_ipc_meta(fa)
 
 
-def get_graph_buffer_ipc_meta(fa):
-    return torch.ops.sgl_kernels.get_graph_buffer_ipc_meta(fa)
-
-
-def register_graph_buffers(fa, handles, offsets):
-    torch.ops.sgl_kernels.register_graph_buffers(fa, handles, offsets)
-
+    def register_graph_buffers(fa, handles, offsets):
+        torch.ops.sgl_kernels.register_graph_buffers(fa, handles, offsets)
 
 def moe_align_block_size(
     topk_ids,
