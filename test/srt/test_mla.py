b14afbd9278baebb48e1eaa4b03682e04a15b058
qunyang <quyang@habana.ai>
2025-02-11 02:27:01 +0200

    add ut

--- test/srt/test_mla.py
+++ test/srt/test_mla.py
@@ -59,6 +59,46 @@ class TestMLA(CustomTestCase):
         metrics = run_eval(args)
         self.assertGreater(metrics["score"], 0.8)
 
+class TestTorchNativeAttnBackendMLA(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = DEFAULT_MLA_MODEL_NAME_FOR_TEST
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=["--trust-remote-code", "--attention-backend", "torch_native"],
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_mmlu(self):
+        args = SimpleNamespace(
+            base_url=self.base_url,
+            model=self.model,
+            eval_name="mmlu",
+            num_examples=64,
+            num_threads=32,
+        )
+
+        metrics = run_eval(args)
+        self.assertGreater(metrics["score"], 0.5)
+
+    def test_mgsm_en(self):
+        args = SimpleNamespace(
+            base_url=self.base_url,
+            model=self.model,
+            eval_name="mgsm_en",
+            num_examples=None,
+            num_threads=1024,
+        )
+
+        metrics = run_eval(args)
+        self.assertGreater(metrics["score"], 0.8)
+
 
 if __name__ == "__main__":
     unittest.main()
