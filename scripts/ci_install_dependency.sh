ea4d7a948829938fcb9764101d4bdc31d16e873b
zhyncs <me@zhyncs.com>
2025-02-13 16:57:37 +0000

    upd

--- scripts/ci_install_dependency.sh
+++ scripts/ci_install_dependency.sh
@@ -4,17 +4,18 @@ set -euxo pipefail
 # Install the dependency in CI.
 
 # Use repo from environment variable, passed from GitHub Actions
-FLASHINFER_REPO="${FLASHINFER_REPO:-https://flashinfer.ai/whl/cu124/torch2.5/flashinfer}"
+FLASHINFER_REPO="${FLASHINFER_REPO:-https://flashinfer.ai/whl/cu124/torch2.5/flashinfer-python}"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bash "${SCRIPT_DIR}/killall_sglang.sh"
 
 pip install --upgrade pip
 pip uninstall flashinfer -y
-pip install -e "python[all]" --find-links https://flashinfer.ai/whl/cu124/torch2.5/flashinfer/
+pip install -e "python[all]" --find-links https://flashinfer.ai/whl/cu124/torch2.5/flashinfer-python
 
+rm -rf /root/.cache/flashinfer
 # Force reinstall flashinfer and torch_memory_saver
-pip install https://github.com/flashinfer-ai/flashinfer-nightly/releases/download/0.2.1%2Bdbb1e4e/flashinfer_python-0.2.1+dbb1e4e.cu124torch2.5-cp310-cp310-linux_x86_64.whl --force-reinstall --no-deps
+pip install flashinfer_python==0.2.1.post1 --find-links ${FLASHINFER_REPO} --force-reinstall --no-deps
 
 pip install torch_memory_saver --force-reinstall
 
