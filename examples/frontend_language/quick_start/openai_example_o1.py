73a968cdfe8ee8c5117467c5cde74e42a88896ba
Chuyue Sun <chuyue99@gmail.com>
2025-02-12 19:04:06 -0600

    lint

--- examples/frontend_language/quick_start/openai_example_o1.py
+++ examples/frontend_language/quick_start/openai_example_o1.py
@@ -11,7 +11,7 @@ import sglang as sgl
 def multi_turn_question(s, question_1, question_2):
     s += sgl.system("You are a helpful assistant.")
     s += sgl.user(question_1)
-    s += sgl.assistant(sgl.gen("answer_1", max_tokens=100))    
+    s += sgl.assistant(sgl.gen("answer_1", max_tokens=100))
     s += sgl.user(question_2)
     s += sgl.assistant(sgl.gen("answer_2"))
 
