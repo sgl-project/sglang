707ddffeae2df6cf0f6509bbc3a9c56c8562af4e
zhousx <jx85128290@163.com>
2025-02-17 14:25:37 +0000

    opt eagle lm_head

--- python/sglang/srt/speculative/eagle_worker.py
+++ python/sglang/srt/speculative/eagle_worker.py
@@ -44,6 +44,15 @@ class EAGLEWorker(TpModelWorker):
         # We will capture it later
         backup_disable_cuda_graph = server_args.disable_cuda_graph
         server_args.disable_cuda_graph = True
+
+        if server_args.speculative_token_map is not None:
+            try:
+                self.hot_token_id = torch.load(server_args.speculative_token_map)
+            except:
+                raise RuntimeError(f"there is not hot_token_ids.pt file in {self.server_args.speculative_token_map}")
+            self.hot_token_id = torch.tensor(self.hot_token_id, dtype=torch.int32, device='cuda')
+            server_args.json_model_override_args=f'{{"hot_vocab_size": {len(self.hot_token_id)}}}'
+            
         super().__init__(
             gpu_id=gpu_id,
             tp_rank=tp_rank,
@@ -66,6 +75,11 @@ class EAGLEWorker(TpModelWorker):
         # Share the embedding and lm_head
         if not self.speculative_algorithm.is_nextn():
             embed, head = self.target_worker.model_runner.model.get_embed_and_head()
+            if server_args.speculative_token_map is not None:
+                head = head.clone()
+                head.data = head.data[self.hot_token_id]
+            else:
+                self.hot_token_id = None
             self.model_runner.model.set_embed_and_head(embed, head)
         self.model_runner.server_args.disable_cuda_graph = backup_disable_cuda_graph
 
@@ -223,6 +237,7 @@ class EAGLEWorker(TpModelWorker):
             spec_info.topk_index,
             spec_info.hidden_states,
         )
+        topk_index = self.hot_token_id[topk_index] if self.hot_token_id is not None else topk_index
 
         # Return values
         score_list: List[torch.Tensor] = []
@@ -262,6 +277,7 @@ class EAGLEWorker(TpModelWorker):
             )
             probs = torch.softmax(logits_output.next_token_logits, dim=-1)
             topk_p, topk_index = fast_topk(probs, self.topk, dim=-1)
+            topk_index = self.hot_token_id[topk_index] if self.hot_token_id is not None else topk_index
             hidden_states = logits_output.hidden_states
 
         return score_list, token_list, parents_list
