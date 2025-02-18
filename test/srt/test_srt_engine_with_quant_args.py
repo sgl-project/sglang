bcb42266ff25d3a930b9d637e51bd08e0d650e63
Stefan He <hebiaobuaa@gmail.com>
2025-02-18 00:02:58 -0800

    format

--- test/srt/test_srt_engine_with_quant_args.py
+++ test/srt/test_srt_engine_with_quant_args.py
@@ -35,16 +35,20 @@ class TestSRTEngineWithQuantArgs(unittest.TestCase):
     def test_2_torchao_args(self):
 
         # we don't test int8dq because currently there is conflict between int8dq and capture cuda graph
-        torchao_args_list = [
-            # "int8dq",
-            "int8wo",
-            "fp8wo",
-            "fp8dq-per_tensor",
-            "fp8dq-per_row",
-        ] + [f"int4wo-{group_size}" for group_size in [32, 64, 128, 256]] + [
-            "gemlite-32-8-None",
-            "gemlite-8-8-None",
-        ]
+        torchao_args_list = (
+            [
+                # "int8dq",
+                "int8wo",
+                "fp8wo",
+                "fp8dq-per_tensor",
+                "fp8dq-per_row",
+            ]
+            + [f"int4wo-{group_size}" for group_size in [32, 64, 128, 256]]
+            + [
+                "gemlite-32-8-None",
+                "gemlite-8-8-None",
+            ]
+        )
         # gemlite-<packing_bitwidth>-<bit_width>-<group_size> or
         # gemlite-<bit_width>-<group_size> (packing_bitwidth defaults to 32)
         # We don't exhausted all combinations, only test the main logic
