3e3a1e4c0e128a6612f8da1bf2b54ad520831234
yiakwy-xpu-ml-framework-team <961186938@qq.com>
2025-02-19 21:52:20 +0800

    add flash vec_t in amd platform

--- sgl-kernel/src/sgl-kernel/csrc/gemm/per_token_group_quant_fp8.cu
+++ sgl-kernel/src/sgl-kernel/csrc/gemm/per_token_group_quant_fp8.cu
@@ -58,7 +58,7 @@ __global__ void per_token_group_quant_fp8_kernel(const T* __restrict__ input, vo
 
 #pragma unroll
     for (uint32_t j = 0; j < vec_size; ++j) {
-      float val = static_cast<float>(input_vec[j]);
+      float val = castToFloat(input_vec[j]);
       float abs_val = fabsf(val);
       local_absmax = fmaxf(local_absmax, abs_val);
     }
@@ -84,7 +84,7 @@ __global__ void per_token_group_quant_fp8_kernel(const T* __restrict__ input, vo
 
 #pragma unroll
     for (uint32_t j = 0; j < vec_size; ++j) {
-      float val = static_cast<float>(input_vec[j]);
+      float val = castToFloat(input_vec[j]);
       float q_val = fminf(fmaxf(val / y_s, fp8_min), fp8_max);
       group_output[i * vec_size + j] = FP8_TYPE(q_val);
     }
