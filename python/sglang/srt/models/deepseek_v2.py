2d5a5f815d4a571f5ee9ff84f77256c77455fd2f
zjli2013 <leezhengjiang@gmail.com>
2025-02-20 15:39:48 +0800

    fix serving perf regression

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -544,17 +544,18 @@ class DeepseekV2AttentionMLA(nn.Module):
         forward_batch: ForwardBatch,
     ) -> torch.Tensor:
         if self.q_lora_rank is not None:
-            if not self.use_dp_linear:
+            bs = hidden_states.shape[0]
+            if (not self.use_dp_linear) or (bs % self.tp_size != 0) :
                 q = self.q_a_proj(hidden_states)[0]
-            else:
-                bs = hidden_states.shape[0]
+                print(f"[DEBUG] hidden_state.shape={hidden_states.shape}, bs={hidden_states.shape[0]}")
+            else :
                 assert bs % self.tp_size == 0, "hidden_states[0] is not divided by tp_size"
                 local_bs = bs // self.tp_size
                 start_idx = self.tp_rank * local_bs 
                 end_idx = start_idx + local_bs    
                 local_x = hidden_states[start_idx:end_idx, :] 
                 local_q = self.q_a_proj(local_x)[0]
-                assert local_q.shape[1] == self.q_lora_rank 
+                #assert local_q.shape[1] == self.q_lora_rank 
                 q = torch.zeros(bs, self.q_lora_rank, dtype=local_q.dtype, device=local_q.device)
                 torch.distributed.all_gather_into_tensor(q, local_q)
             q = self.q_a_layernorm(q)
@@ -564,19 +565,19 @@ class DeepseekV2AttentionMLA(nn.Module):
                 -1, self.num_local_heads, self.qk_head_dim
             )
         _, q_pe = q.split([self.qk_nope_head_dim, self.qk_rope_head_dim], dim=-1)
-        if not self.use_dp_linear:
+        bs = hidden_states.shape[0]
+        if (not self.use_dp_linear) or (bs % self.tp_size != 0):
             latent_cache = self.kv_a_proj_with_mqa(hidden_states)[0]
         else:
-            bs = hidden_states.shape[0]
             assert bs % self.tp_size == 0, "hidden_states[0] is not divided by tp_size"
             local_bs = bs // self.tp_size
             start_idx = self.tp_rank * local_bs 
             end_idx = start_idx + local_bs    
             local_x = hidden_states[start_idx:end_idx, :] 
             local_compressed_kv = self.kv_a_proj_with_mqa(local_x)[0]
-            assert local_compressed_kv.shape[1] == self.kv_lora_rank + self.qk_rope_head_dim 
+            #assert local_compressed_kv.shape[1] == self.kv_lora_rank + self.qk_rope_head_dim 
             latent_cache = torch.zeros(bs, local_compressed_kv.shape[1], dtype=local_compressed_kv.dtype, device=local_compressed_kv.device)
-            torch.distributed.all_gather_into_tensor(latent_cache, local_compressed_kv)            
+            torch.distributed.all_gather_into_tensor(latent_cache, local_compressed_kv)                     
         kv_a, _ = latent_cache.split([self.kv_lora_rank, self.qk_rope_head_dim], dim=-1)
         latent_cache = latent_cache.unsqueeze(1)
         kv_a = self.kv_a_layernorm(kv_a.contiguous())
@@ -610,6 +611,7 @@ class DeepseekV2AttentionMLA(nn.Module):
         forward_batch: ForwardBatch,
     ) -> torch.Tensor:
         q_len = hidden_states.shape[0]
+        print(f"[DEBUG] decode bs ={q_len}")
         q_input = hidden_states.new_empty(
             q_len, self.num_local_heads, self.kv_lora_rank + self.qk_rope_head_dim
         )
