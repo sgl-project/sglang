c7739693d3538f794bb4366b3153a94f819041d8
fzyzcjy <ch271828n@outlook.com>
2025-02-21 16:32:52 +0800

    detokenizer_manager

--- python/sglang/srt/managers/detokenizer_manager.py
+++ python/sglang/srt/managers/detokenizer_manager.py
@@ -195,6 +195,7 @@ class DetokenizerManager:
             input_top_logprobs_idx=recv_obj.input_top_logprobs_idx,
             output_top_logprobs_val=recv_obj.output_top_logprobs_val,
             output_top_logprobs_idx=recv_obj.output_top_logprobs_idx,
+            output_hidden_states=recv_obj.output_hidden_states,
         )
 
 
