1ae13bf54d976dfbd9ccc766f96d9b3b16f92730
fzyzcjy <ch271828n@outlook.com>
2025-02-21 19:43:00 +0800

    cp demo

--- examples/runtime/engine/offline_batch_inference_torchrun.py
+++ examples/runtime/engine/offline_batch_inference_torchrun.py
@@ -2,14 +2,16 @@ import datetime
 import os
 import sys
 
+from sglang.srt.distributed import ParallelProcessGroups
 from sglang.srt.entrypoints.engine_fragment import EngineFragment
+from torch.distributed.device_mesh import init_device_mesh
 
 
 def run():
     """
     Example command:
     ```
-    torchrun --nproc_per_node=4 offline_batch_inference_torchrun.py
+    torchrun --nproc_per_node=8 offline_batch_inference_torchrun.py
     ```
     """
 
@@ -25,9 +27,20 @@ def run():
         f'start {local_rank=} {rank=} {world_size=} {sys.argv=} {os.environ.get("CUDA_VISIBLE_DEVICES")}'
     )
 
-    tp_size = world_size
-    tp_rank = rank
-    _log(f"{tp_rank=} {tp_size=}")
+    tp_size = 4
+    dp_size = 2
+    assert world_size == tp_size * dp_size
+
+    device_mesh_kwargs = dict(
+        mesh_shape=(tp_size, dp_size, 1), mesh_dim_names=["tp", "dp", "pp"]
+    )
+    device_mesh_device = init_device_mesh("cuda", **device_mesh_kwargs)
+    device_mesh_cpu = init_device_mesh("cpu", **device_mesh_kwargs)
+    _log(f"{device_mesh_device=} {device_mesh_cpu=}")
+
+    tp_rank = device_mesh_device.get_local_rank("tp")
+    dp_rank = device_mesh_device.get_local_rank("dp")
+    _log(f"{tp_rank=} {tp_size=} ; {dp_rank=} {dp_size=}")
 
     model_name, mem_fraction_static = "meta-llama/Llama-3.2-1B-Instruct", 0.1
     # model_name, mem_fraction_static = "meta-llama/Llama-3.1-70B-Instruct", 0.9 # test large models
@@ -62,15 +75,27 @@ def run():
         tp_size=tp_size,
         tp_rank=tp_rank,
         nccl_port=23456,
-        gpu_id=tp_rank,
+        gpu_id=local_rank,
+        parallel_process_groups=ParallelProcessGroups.from_devices_meshes(
+            device_mesh_device=device_mesh_device,
+            device_mesh_cpu=device_mesh_cpu,
+            dim_tp="tp",
+            dim_pp="pp",
+        ),
     )
     _log(f"{fragment=}")
 
+    prompt_all = [
+        ["1+1=2, 1+2=3, 1+3=4, 1+4=", "9-1=8, 8-1=7, 7-1="],
+        ["2*1=2, 2*2=4, 2*3=", "8/2=4, 6/2="],
+    ]
+    prompt = prompt_all[dp_rank]
+
     output = fragment.generate(
-        prompt=["1+1=2, 1+2=3, 1+3=4, 1+4=", "9-1=8, 8-1=7, 7-1="],
+        prompt=prompt,
         sampling_params=dict(max_new_tokens=16, temperature=0.0),
     )
-    _log(f"{output=}")
+    _log(f"{prompt=} {output=}")
 
     fragment.shutdown()
     _log(f"End script")
