a7050b96793411e2b807d7dd091f0c1890b9efa6
Song Zhang <gepin.zs@antgroup.com>
2025-02-24 19:39:53 +0800

    Support run on multiple NPUs
    Signed-off-by: Song Zhang <gepin.zs@antgroup.com>

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -144,6 +144,9 @@ def is_hpu() -> bool:
 def is_xpu() -> bool:
     return hasattr(torch, "xpu") and torch.xpu.is_available()
 
+def is_npu() -> bool:
+    return hasattr(torch, "npu") and torch.npu.is_available()
+
 
 def is_flashinfer_available():
     """
@@ -1357,6 +1360,9 @@ def get_device_name(device_id: int = 0) -> str:
 
     if hasattr(torch, "hpu") and torch.hpu.is_available():
         return torch.hpu.get_device_name(device_id)
+
+    if hasattr(torch, "npu") and torch.npu.is_available():
+        return torch.npu.get_device_name(device_id)
 
 
 @lru_cache(maxsize=1)
