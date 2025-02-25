113e6e2ed4fc7a6cc8e36de26d8185bb9b15f1a7
fzyzcjy <ch271828n@outlook.com>
2025-02-25 20:57:43 +0800

    more

--- examples/runtime/engine/offline_batch_inference_torchrun.py
+++ examples/runtime/engine/offline_batch_inference_torchrun.py
@@ -70,10 +70,9 @@ def run():
     fragment = VerlEngine(
         model_path=model_name,
         mem_fraction_static=mem_fraction_static,
-        first_rank_in_node=tp_rank == 0,
         device_mesh_cpu=device_mesh_cpu['tp'],
-        # TODO not true when dp>1
-        base_gpu_id=0,
+        first_rank_in_node=tp_rank == 0,
+        base_gpu_id=dp_rank,
         gpu_id_step=dp_size,
     )
     _log(f"{fragment=}")
