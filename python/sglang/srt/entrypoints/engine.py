bb3119b6ff580409f84259d3d7a7a911daa39993
fzyzcjy <ch271828n@outlook.com>
2025-02-25 20:39:29 +0800

    more

--- python/sglang/srt/entrypoints/engine.py
+++ python/sglang/srt/entrypoints/engine.py
@@ -271,6 +271,7 @@ class Engine:
             self.tokenizer_manager.update_weights_from_distributed(obj, None)
         )
 
+    # TODO: Should the `List[bytes]` "tensor" type be public API
     def update_weights_from_tensor(self, named_tensors: List[Tuple[str, torch.Tensor]],
                                    load_format: Optional[str] = None, has_more: bool = False):
         """Update weights from distributed source. If there are going to be more updates, set `has_more` to be true
