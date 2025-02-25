740c7ddbf2453539e2ff40e44a1d6ce93c11799d
fzyzcjy <ch271828n@outlook.com>
2025-02-25 21:59:41 +0800

    cp old test

--- test/srt/test_update_weights_from_tensor.py
+++ test/srt/test_update_weights_from_tensor.py
@@ -26,6 +26,25 @@ class TestUpdateWeightsFromTensor(unittest.TestCase):
 
         engine.shutdown()
 
+    def test_update_weights_from_tensor_load_format_direct(self):
+        engine = sgl.Engine(model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST)
+
+        write_param_name = f"model.layers.6.self_attn.qkv_proj.weight"
+        read_param_name = f"model.layers.6.self_attn.k_proj.weight"
+
+        _check_param(
+            engine, read_param_name, [-0.0198, 0.0227, 0.0168, 0.0232, -0.0178]
+        )
+
+        new_tensor = torch.full((3072, 2048), 1.5)
+        engine.update_weights_from_tensor(
+            [(write_param_name, new_tensor)], load_format="direct"
+        )
+
+        _check_param(engine, read_param_name, [1.5] * 5)
+
+        engine.shutdown()
+
 
 def _check_param(engine, param_name, expect_values):
     actual_values = torch.tensor(engine.get_weights_by_name(param_name))[0, :5]
