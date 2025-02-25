6827ecbee43e4501339908a14383900dc2901c45
fzyzcjy <ch271828n@outlook.com>
2025-02-25 20:09:04 +0800

    more

--- python/sglang/srt/entrypoints/verl_engine.py
+++ python/sglang/srt/entrypoints/verl_engine.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-from sglang import Engine
+from sglang.srt.server import Engine
 from torch.distributed.tensor import DeviceMesh
 
 
