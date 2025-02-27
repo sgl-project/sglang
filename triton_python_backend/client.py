29fc9bac7d109175112ec8914a2d4c6a4df76831
yych0745 <1398089567@qq.com>
2025-02-27 15:03:16 +0800

    triton python backend Co-authored-by: thomas-zhu-2006 <thomas.zhu.2006@gmail.com> Co-authored-by: b0urnee <2769086541@qq.com> Co-authored-by: XuruiYang <530534756@qq.com>

---
+++ triton_python_backend/client.py
@@ -0,0 +1,79 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import threading
+from functools import partial
+from queue import Queue
+
+import numpy as np
+import tritonclient.grpc as grpcclient
+from tritonclient.utils import np_to_triton_dtype
+
+
+def prepare_tensor(name, input_tensor):
+    """Create grpcclient's InferInput instance according to a given tensor."""
+    t = grpcclient.InferInput(name, list(input_tensor.shape),
+                              np_to_triton_dtype(input_tensor.dtype))
+    t.set_data_from_numpy(input_tensor)
+    return t
+
+
+def stream_callback(que, result, error):
+    """Callback function invoked by triton."""
+    que.put((result, error))
+
+
+def process_result(que):
+    """Process and print results in queue."""
+    while True:
+        res = que.get()
+        if res is not None:
+            result, err = res
+            if err is not None:
+                print(err)
+            else:
+                response = result.as_numpy('response')
+                print(f'generated text: {response}')
+        else:
+            break
+
+
+if __name__ == '__main__':
+    model_name = 'sglang_model'
+    prompts = 'please introduce yourself'
+    max_tokens = 512
+    temperature = 1.0
+    top_p = 1.0
+    top_k = 1
+    ignore_eos = False
+    stream = True
+    bad_words = ['User:', 'GPT:']
+
+    res_que = Queue()
+
+    process_thread = threading.Thread(target=process_result, args=(res_que, ))
+    process_thread.start()
+
+    with grpcclient.InferenceServerClient('0.0.0.0:33337') as client:
+        inputs = [
+            prepare_tensor('prompt',
+                           np.array([prompts.encode()], dtype=np.object_)),
+            prepare_tensor('max_tokens', np.array([max_tokens],
+                                                  dtype=np.int32)),
+            prepare_tensor('temperature',
+                           np.array([temperature], dtype=np.float_)),
+            prepare_tensor('top_p', np.array([top_p], dtype=np.float_)),
+            prepare_tensor('top_k', np.array([top_k], dtype=np.int32)),
+            prepare_tensor('ignore_eos', np.array([ignore_eos],
+                                                  dtype=np.bool_)),
+            prepare_tensor('stream', np.array([stream], dtype=np.bool_)),
+            prepare_tensor('bad_words', np.array(bad_words, dtype=np.object_)),
+        ]
+
+        # async_stream
+        client.start_stream(partial(stream_callback, res_que))
+        client.async_stream_infer(model_name,
+                                  inputs,
+                                  sequence_start=True,
+                                  sequence_end=True)
+
+    res_que.put(None)
+    process_thread.join()
---
+++ triton_python_backend/model.py
@@ -0,0 +1,283 @@
+import sys
+
+"""
+python3 pressure_grpc_custom.py --backend sglang-grpc --dataset-name custom-json --dataset-path data.json --num-prompt 1000 --tokenizer /home/hadoop-pampas/triton_model/qwen328bit/2/weights --disable-stream --sharegpt-output-len 10 --base-url 127.0.0.1 --grpc-port 26384 --model "qwen328bit"
+"""
+
+import asyncio
+import json
+import os
+import pdb
+import threading
+import time
+import traceback
+from json.decoder import JSONDecodeError
+
+import numpy as np
+import torch
+import triton_python_backend_utils as pb_utils
+from pycat import Cat
+
+from sglang.api import Engine
+
+
+class TritonPythonModel:
+    def initialize(self, args):
+        self.logger = pb_utils.Logger
+
+        # parse model configs
+        self.model_config = json.loads(args["model_config"])
+        self.triton_model_name = self.model_config["name"]
+
+        # parse parameters
+        parameters = self.model_config["parameters"]
+
+        self.is_chat = parameters["is_chat"]["string_value"].lower() == "true"
+        self.app_key = parameters["appkey"]["string_value"].lower()
+        enable_prefix_caching = (
+            parameters["enable_prefix_caching"]["string_value"].lower() == "true"
+        )
+        tp_size = torch.cuda.device_count()
+
+        quant_policy = int(parameters["quant_policy"]["string_value"])
+
+        # load engine
+        model_path = os.path.join(
+            args["model_repository"], args["model_version"], "weights"
+        )
+        self.engine = Engine(
+            model_path=model_path,
+            tp_size=tp_size,
+            disable_radix_cache=not enable_prefix_caching,
+            quantization=quant_policy,
+        )
+        self.tokenizer = self.engine.tokenizermanager.tokenizer
+        self.request_id = 0
+        # warm up
+        self.engine.generate(
+            prompt="warm up",
+        )
+        self.event_loop = asyncio.get_event_loop()
+        self.engine_thread = threading.Thread(target=self._engine_loop)
+        self.shutdown_event = asyncio.Event()
+        self.engine_thread.start()
+
+        self.logger.log_info("SGLang backend started")
+
+    def _engine_loop(self):
+        self.logger.log_info("Engine loop started")
+        self.event_loop.run_until_complete(self._await_shutdown())
+        self.event_loop.close()
+        self.logger.log_info("Engine loop closed")
+
+    async def _await_shutdown(self):
+        # await the shutdown signal
+        await self.shutdown_event.wait()
+        self.logger.log_info("Get shutdown signal")
+
+        # cancel unfinished tasks
+        for task in asyncio.all_tasks(loop=self.event_loop):
+            if task is not asyncio.current_task(loop=self.event_loop):
+                task.cancel()
+                try:
+                    await task
+                except asyncio.CancelledError:
+                    self.logger.log_info("Unfinished task is cancelled")
+
+    def build_no_stream_json(
+        self, answer, request_id, input_len, output_len, finish_reason
+    ):
+        # Build non-streaming JSON response
+        response = {
+            "choices": [
+                {
+                    "finish_reason": finish_reason,
+                    "index": 0,
+                    "message": {"content": answer},
+                }
+            ],
+            "model": self.triton_model_name,
+            "id": request_id,
+            "usage": {
+                "completion_tokens": output_len,
+                "prompt_tokens": input_len,
+                "total_tokens": input_len + output_len,
+            },
+        }
+        return json.dumps(response, ensure_ascii=False)
+
+    def _get_optional_configs(self, request):
+        optional_configs = {}
+        config_names = [
+            "temperature",
+            "top_p",
+            "top_k",
+            "repetition_penalty",
+            "n",
+            "best_of",
+            "presence_penalty",
+            "frequency_penalty",
+            "ignore_eos",
+        ]
+        for config_name in config_names:
+            input_tensor = pb_utils.get_input_tensor_by_name(request, config_name)
+            if input_tensor is not None:
+                optional_configs[config_name] = input_tensor.as_numpy().item()
+
+        return optional_configs
+
+    async def _process_request(self, request_id, request):
+        response_sender = request.get_response_sender()
+        try:
+            process_request_time = Cat.new_transaction(
+                "Turing.Model.Local", "SingleGlobalInput 100%" + self.triton_model_name
+            )
+            first_token_latency = Cat.new_transaction(
+                "first_token_latency", self.triton_model_name
+            )
+
+            # parse request
+            # Construct prompt
+            if self.is_chat:
+                messages = pb_utils.get_input_tensor_by_name(
+                    request, "messages"
+                ).as_numpy()[0]
+                if isinstance(messages, bytes):
+                    messages = messages.decode("utf-8")
+                prompt = json.loads(messages)
+                prompt = self.tokenizer.apply_chat_template(
+                    prompt,
+                    tokenize=False,
+                    add_generation_prompt=True,
+                )
+            else:
+                text = pb_utils.get_input_tensor_by_name(request, "text").as_numpy()[0]
+                if isinstance(text, bytes):
+                    text = text.decode("utf-8")
+                prompt = text
+            max_tokens = (
+                pb_utils.get_input_tensor_by_name(request, "max_tokens")
+                .as_numpy()
+                .item()
+            )
+            stream = (
+                pb_utils.get_input_tensor_by_name(request, "stream").as_numpy().item()
+            )
+            optional_configs = self._get_optional_configs(request)
+
+            outputs = []
+            input_token_len = 0
+            finish_reason = "None"
+            output_token_len = 0
+            is_first = True
+            is_second = False
+            sampling_params = {"max_new_tokens": max_tokens, **optional_configs}
+            gen = await self.engine.async_generate(
+                prompt=prompt,
+                sampling_params=sampling_params,
+                stream=stream,
+            )
+            if stream:
+                async for output in gen:
+                    if is_first and stream:
+                        first_token_latency.complete()
+                        is_second = True
+                        per_token_latency = Cat.new_transaction(
+                            "per_token_latency", self.triton_model_name
+                        )
+
+                    if is_second and is_first is False and stream:
+                        is_second = False
+                        per_token_latency.complete()
+
+                    # for stream mode, send the partial response one by one
+                    triton_output_tensor = pb_utils.Tensor(
+                        "answer", np.asarray(output["text"], dtype=np.object_)
+                    )
+                    resp = pb_utils.InferenceResponse(
+                        output_tensors=[triton_output_tensor]
+                    )
+                    if output["meta_info"]["finish_reason"] is not None:
+                        response_sender.send(
+                            resp, flags=pb_utils.TRITONSERVER_RESPONSE_COMPLETE_FINAL
+                        )
+                    else:
+                        response_sender.send(resp)
+
+                    if is_first:
+                        is_first = False
+            else:
+                if type(gen) == list:
+                    output = gen[0]
+                else:
+                    output = gen
+                outputs.append(output["text"])
+                input_token_len = output["meta_info"]["prompt_tokens"]
+                output_token_len = output["meta_info"]["completion_tokens"]
+                if output["meta_info"]["finish_reason"] is not None:
+                    finish_reason = output["meta_info"]["finish_reason"]
+                # for non-stream mode, send concatenated response at one time
+                text_outputs = self.build_no_stream_json(
+                    "".join(outputs),
+                    request_id,
+                    input_token_len,
+                    output_token_len,
+                    finish_reason,
+                )
+                triton_output_tensor = pb_utils.Tensor(
+                    "answer", np.asarray(text_outputs, dtype=np.object_)
+                )
+                resp = pb_utils.InferenceResponse(output_tensors=[triton_output_tensor])
+                response_sender.send(
+                    resp, flags=pb_utils.TRITONSERVER_RESPONSE_COMPLETE_FINAL
+                )
+                if is_first:
+                    is_first = False
+        except Exception as e:
+            self.logger.log_info(
+                f"Error when processing request: {traceback.format_exc()}"
+            )
+            if isinstance(e, JSONDecodeError):
+                error = pb_utils.TritonError(
+                    "Error when parsing prompt, please make sure correct JSON format"
+                )
+            else:
+                error = pb_utils.TritonError(
+                    f"Error when processing request: {traceback.format_exc()}"
+                )
+            triton_output_tensor = pb_utils.Tensor(
+                "answer", np.asarray(["N/A"], dtype=np.object_)
+            )
+            resp = pb_utils.InferenceResponse(
+                output_tensors=[triton_output_tensor], error=error
+            )
+            response_sender.send(
+                resp, flags=pb_utils.TRITONSERVER_RESPONSE_COMPLETE_FINAL
+            )
+            process_request_time.set_status_for_exception(e)
+            first_token_latency.set_status_for_exception(e)
+            raise e
+        finally:
+            process_request_time.complete()
+
+    def execute(self, requests):
+        for request in requests:
+            future = asyncio.run_coroutine_threadsafe(
+                self._process_request(self.request_id, request), self.event_loop
+            )
+            try:
+                future.result(timeout=30)
+            except Exception as e:
+                print("Task timeout")
+                self.logger.log_error(f"Request {self.request_id} failed: {str(e)}")
+                future.cancel()
+            finally:
+                self.request_id += 1
+        return None
+
+    def finalize(self):
+        self.logger.log_info("Finalizing SGLang backend")
+        self.event_loop.call_soon_threadsafe(self.shutdown_event.set)
+        if self.engine_thread is not None:
+            self.engine_thread.join()
+            self.engine_thread = None
