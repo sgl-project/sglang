3e030a51e4ebf72781a09083202db1d5278448c3
fzyzcjy <ch271828n@outlook.com>
2025-03-03 20:13:56 +0800

    more

--- python/sglang/srt/multi_batch_executor.py
+++ python/sglang/srt/multi_batch_executor.py
@@ -1,5 +1,10 @@
 def execute_single_batch(inputs, fn):
-    TODO
+    generator = fn(**inputs)
+    while True:
+        try:
+            next(generator)
+        except StopIteration as e:
+            return e.value
 
 
 def execute_two_batch(inputs, fn):
