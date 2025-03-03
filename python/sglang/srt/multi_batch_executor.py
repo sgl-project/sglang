bc5495aa02dd743c6b2b0e80db1ad8f902288983
fzyzcjy <ch271828n@outlook.com>
2025-03-03 20:16:25 +0800

    more

--- python/sglang/srt/multi_batch_executor.py
+++ python/sglang/srt/multi_batch_executor.py
@@ -7,5 +7,31 @@ def execute_single_batch(inputs, fn):
             return e.value
 
 
-def execute_two_batch(inputs, fn):
-    TODO
+def execute_two_batch(inputs, fn, delta_stages: int):
+    output_a = output_b = None
+
+    generator_a = fn(**inputs)
+    generator_b = fn(**inputs)
+
+    for _ in range(delta_stages):
+        next(generator_a)
+
+    while output_a is None:
+        try:
+            next(generator_a)
+        except StopIteration as e:
+            assert e.value is not None
+            output_a = e.value
+
+        next(generator_b)
+
+    for _ in range(delta_stages - 1):
+        next(generator_b)
+
+    try:
+        next(generator_b)
+    except StopIteration as e:
+        assert e.value is not None
+        output_b = e.value
+
+    return output_a, output_b
