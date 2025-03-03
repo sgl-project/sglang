f347886c25706d31c088f73d148494d20981ec9e
Lianmin Zheng <lianminzheng@gmail.com>
2025-03-03 03:19:59 -0800

    Fix try

--- sgl-kernel/src/sgl-kernel/csrc/speculative/speculative_sampling.cu
+++ sgl-kernel/src/sgl-kernel/csrc/speculative/speculative_sampling.cu
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <speculative_sampling.cuh>
+#include "speculative_sampling.cuh"
 
 #include "pytorch_extension_utils.h"
 
