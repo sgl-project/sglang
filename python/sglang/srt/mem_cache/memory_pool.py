ae1ba1b53d0c4054cf654505ddf6a236eeab7ec1
Geon Park <mujjingun@gmail.com>
2025-03-04 12:14:43 -0500

    Support HiP Attention
    Co-authored-by: Heejun Lee <gmlwns5176@gmail.com>
    Co-authored-by: Bumsik Kim <k.bumsik@gmail.com>

--- python/sglang/srt/mem_cache/memory_pool.py
+++ python/sglang/srt/mem_cache/memory_pool.py
@@ -36,6 +36,7 @@ import psutil
 import torch
 
 from sglang.srt.layers.radix_attention import RadixAttention
+from sglang.srt.model_executor.forward_batch_info import ForwardBatch
 from sglang.srt.utils import debug_timing, get_compiler_backend
 
 logger = logging.getLogger(__name__)
@@ -129,6 +130,18 @@ class KVCache(abc.ABC):
 
     def register_layer_transfer_counter(self, layer_transfer_counter):
         self.layer_transfer_counter = layer_transfer_counter
+
+    def on_model_start(self, forward_batch: ForwardBatch):
+        pass
+
+    def on_model_end(self, forward_batch: ForwardBatch):
+        pass
+
+    def on_layer_start(self, forward_batch: ForwardBatch, layer_id: int):
+        pass
+
+    def on_layer_end(self, forward_batch: ForwardBatch, layer_id: int):
+        pass
 
 
 class TokenToKVPoolAllocator:
