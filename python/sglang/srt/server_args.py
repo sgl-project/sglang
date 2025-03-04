9c6297bb70af513ea147b075ef1fbf49ad47b5de
vincent-4 <vincentzhongy+githubvincent4@gmail.com>
2025-03-04 09:47:44 -0500

    use util function

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -1109,36 +1109,7 @@ class PortArgs:
                 dist_init_addr = ("127.0.0.1", server_args.port + ZMQ_TCP_PORT_DELTA)
             elif server_args.dist_init_addr.startswith("["):  # ipv6 address
 
-                addr = server_args.dist_init_addr
-                end = addr.find("]")
-                if end == -1:
-                    raise ValueError("invalid IPv6 address format: missing ']'")
-
-                host = addr[: end + 1]
-
-                # this only validates the address without brackets: we still need the below checks.
-                # if it's invalid, immediately raise an error so we know it's not formatting issues.
-                if not is_valid_ipv6_address(host[1:end]):
-                    raise ValueError(f"invalid IPv6 address: {host}")
-
-                port_str = None
-                if len(addr) > end + 1:
-                    if addr[end + 1] == ":":
-                        port_str = addr[end + 2 :]
-                    else:
-                        raise ValueError(
-                            "received IPv6 address format: expected ':' after ']'"
-                        )
-
-                if not port_str:
-                    raise ValueError(
-                        "a port must be specified in IPv6 address (format: [ipv6]:port)"
-                    )
-
-                try:
-                    port = int(port_str)
-                except ValueError:
-                    raise ValueError(f"invalid port in IPv6 address: '{port_str}'")
+                port, host = PortArgs.configure_ipv6(server_args)
 
                 dist_init_addr = (host, str(port))
             else:
