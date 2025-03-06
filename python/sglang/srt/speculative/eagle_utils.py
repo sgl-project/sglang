dba3263ce2167544fe0175006db74574a3ec0a2d
Lianmin Zheng <lianminzheng@gmail.com>
2025-03-06 02:09:13 -0800

    Fix eagle_utils.py

--- python/sglang/srt/speculative/eagle_utils.py
+++ python/sglang/srt/speculative/eagle_utils.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Dict, List
+from typing import TYPE_CHECKING, List
 
 import torch
 import torch.nn.functional as F
