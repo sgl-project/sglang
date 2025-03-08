8ed5d77a506f4227ce195f1e20bf62b8360c5a45
lukec <118525388+sleepcoo@users.noreply.github.com>
2025-03-08 14:42:16 +0800

    Remove the vllm dependency from the moe_align function  (#4164)
    Co-authored-by: Hongbosherlock <hongbosherlock@gmail.com>

--- sgl-kernel/tests/test_moe_align.py
+++ sgl-kernel/tests/test_moe_align.py
@@ -138,18 +138,20 @@ def moe_align_block_size_triton(
 
 
 @pytest.mark.parametrize(
-    "block_size,num_tokens,topk",
+    "block_size,num_tokens,topk,num_experts",
     list(
         itertools.product(
             [32, 64, 128, 256],  # block_size
             [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096],  # num_tokens
             [1, 2, 4, 8, 16, 32, 64],  # topk
+            [64, 160, 256],  #  num_experts
         )
     ),
 )
-def test_moe_align_block_size_compare_implementations(block_size, num_tokens, topk):
+def test_moe_align_block_size_compare_implementations(
+    block_size, num_tokens, topk, num_experts
+):
     # For DeepSeek V3, we have 256 experts
-    num_experts = 256
 
     topk_ids = torch.stack(
         [
