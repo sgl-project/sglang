2bfcfb0c38afd5e92e3c0214f9c73c61744ab518
james <jamesliu@together.ai>
2025-03-09 20:40:08 -0700

    nvm

--- python/sglang/srt/models/llama.py
+++ python/sglang/srt/models/llama.py
@@ -315,7 +315,7 @@ class LlamaModel(nn.Module):
 
         hidden_states, _ = self.norm(hidden_states, residual)
 
-        if not self.capture_aux_hidden_states:
+        if len(aux_hidden_states) == 0:
             return hidden_states
 
         return hidden_states, aux_hidden_states
