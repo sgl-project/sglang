b5caa0c874cf45fac17fef74bf329c704bf34278
Lianmin Zheng <lianminzheng@gmail.com>
2025-03-09 18:10:01 -0700

    Update test_mla_flashinfer.py

--- test/srt/test_mla_flashinfer.py
+++ test/srt/test_mla_flashinfer.py
@@ -121,11 +121,11 @@ class TestFlashinferMLAMTP(unittest.TestCase):
                     "--speculative-draft",
                     "lmsys/sglang-ci-dsv3-test-NextN",
                     "--speculative-num-steps",
-                    "2",
+                    "3",
                     "--speculative-eagle-topk",
                     "1",
                     "--speculative-num-draft-tokens",
-                    "2",
+                    "4",
                     "--enable-flashinfer-mla",
                 ]
             )
