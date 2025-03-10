ecd984c72c2f75cd61bbed55d2de2712a7fb9675
Lianmin Zheng <lianminzheng@gmail.com>
2025-03-09 21:50:09 -0700

    Fix lint

--- sgl-kernel/csrc/elementwise/activation.cu
+++ sgl-kernel/csrc/elementwise/activation.cu
@@ -13,69 +13,70 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- #include <flashinfer/activation.cuh>
+#include <flashinfer/activation.cuh>
 
- #include "pytorch_extension_utils.h"
+#include "pytorch_extension_utils.h"
 
- using namespace flashinfer;
+using namespace flashinfer;
 
- __device__ __forceinline__ float silu(const float& val) { return val / (1.0f + __expf(-val)); }
+__device__ __forceinline__ float silu(const float& val) {
+  return val / (1.0f + __expf(-val));
+}
 
- __device__ __forceinline__ float gelu(const float& val) {
-   constexpr float kAlpha = M_SQRT1_2;
-   return val * 0.5f * (1.0f + ::erf(val * kAlpha));
- }
+__device__ __forceinline__ float gelu(const float& val) {
+  constexpr float kAlpha = M_SQRT1_2;
+  return val * 0.5f * (1.0f + ::erf(val * kAlpha));
+}
 
- __device__ __forceinline__ float gelu_tanh(const float& val) {
-   const float cdf =
-       0.5f * (1.0f + math::tanh((0.7978845608028654f * (val + 0.044715f * val * val * val))));
-   return val * cdf;
- }
+__device__ __forceinline__ float gelu_tanh(const float& val) {
+  const float cdf = 0.5f * (1.0f + math::tanh((0.7978845608028654f * (val + 0.044715f * val * val * val))));
+  return val * cdf;
+}
 
- void silu_and_mul(at::Tensor& out, at::Tensor& input, int64_t cuda_stream) {
-   int d = input.size(-1) / 2;
-   int64_t num_tokens = input.numel() / input.size(-1);
-   dim3 grid(num_tokens);
+void silu_and_mul(at::Tensor& out, at::Tensor& input, int64_t cuda_stream) {
+  int d = input.size(-1) / 2;
+  int64_t num_tokens = input.numel() / input.size(-1);
+  dim3 grid(num_tokens);
 
-   cudaStream_t stream = reinterpret_cast<cudaStream_t>(cuda_stream);
-   DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP16(input.scalar_type(), c_type, [&] {
-     uint32_t vec_size = 16 / sizeof(c_type);
-     dim3 block(std::min(d / vec_size, 1024U));
-     flashinfer::activation::act_and_mul_kernel<c_type, silu><<<grid, block, 0, stream>>>(
-         static_cast<c_type*>(out.data_ptr()), static_cast<c_type*>(input.data_ptr()), d);
+  cudaStream_t stream = reinterpret_cast<cudaStream_t>(cuda_stream);
+  DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP16(input.scalar_type(), c_type, [&] {
+    uint32_t vec_size = 16 / sizeof(c_type);
+    dim3 block(std::min(d / vec_size, 1024U));
+    flashinfer::activation::act_and_mul_kernel<c_type, silu>
+        <<<grid, block, 0, stream>>>(static_cast<c_type*>(out.data_ptr()), static_cast<c_type*>(input.data_ptr()), d);
 
-     return true;
-   });
- }
+    return true;
+  });
+}
 
- void gelu_tanh_and_mul(at::Tensor& out, at::Tensor& input, int64_t cuda_stream) {
-   int d = input.size(-1) / 2;
-   int64_t num_tokens = input.numel() / input.size(-1);
-   dim3 grid(num_tokens);
+void gelu_tanh_and_mul(at::Tensor& out, at::Tensor& input, int64_t cuda_stream) {
+  int d = input.size(-1) / 2;
+  int64_t num_tokens = input.numel() / input.size(-1);
+  dim3 grid(num_tokens);
 
-   cudaStream_t stream = reinterpret_cast<cudaStream_t>(cuda_stream);
-   DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP16(input.scalar_type(), c_type, [&] {
-     uint32_t vec_size = 16 / sizeof(c_type);
-     dim3 block(std::min(d / vec_size, 1024U));
-     flashinfer::activation::act_and_mul_kernel<c_type, gelu_tanh><<<grid, block, 0, stream>>>(
-         static_cast<c_type*>(out.data_ptr()), static_cast<c_type*>(input.data_ptr()), d);
+  cudaStream_t stream = reinterpret_cast<cudaStream_t>(cuda_stream);
+  DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP16(input.scalar_type(), c_type, [&] {
+    uint32_t vec_size = 16 / sizeof(c_type);
+    dim3 block(std::min(d / vec_size, 1024U));
+    flashinfer::activation::act_and_mul_kernel<c_type, gelu_tanh>
+        <<<grid, block, 0, stream>>>(static_cast<c_type*>(out.data_ptr()), static_cast<c_type*>(input.data_ptr()), d);
 
-     return true;
-   });
- }
+    return true;
+  });
+}
 
- void gelu_and_mul(at::Tensor& out, at::Tensor& input, int64_t cuda_stream) {
-   int d = input.size(-1) / 2;
-   int64_t num_tokens = input.numel() / input.size(-1);
-   dim3 grid(num_tokens);
+void gelu_and_mul(at::Tensor& out, at::Tensor& input, int64_t cuda_stream) {
+  int d = input.size(-1) / 2;
+  int64_t num_tokens = input.numel() / input.size(-1);
+  dim3 grid(num_tokens);
 
-   cudaStream_t stream = reinterpret_cast<cudaStream_t>(cuda_stream);
-   DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP16(input.scalar_type(), c_type, [&] {
-     uint32_t vec_size = 16 / sizeof(c_type);
-     dim3 block(std::min(d / vec_size, 1024U));
-     flashinfer::activation::act_and_mul_kernel<c_type, gelu><<<grid, block, 0, stream>>>(
-         static_cast<c_type*>(out.data_ptr()), static_cast<c_type*>(input.data_ptr()), d);
+  cudaStream_t stream = reinterpret_cast<cudaStream_t>(cuda_stream);
+  DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP16(input.scalar_type(), c_type, [&] {
+    uint32_t vec_size = 16 / sizeof(c_type);
+    dim3 block(std::min(d / vec_size, 1024U));
+    flashinfer::activation::act_and_mul_kernel<c_type, gelu>
+        <<<grid, block, 0, stream>>>(static_cast<c_type*>(out.data_ptr()), static_cast<c_type*>(input.data_ptr()), d);
 
-     return true;
-   });
- }
+    return true;
+  });
+}
