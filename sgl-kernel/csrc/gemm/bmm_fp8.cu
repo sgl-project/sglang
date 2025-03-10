ff2bfbf49770f4e6172689be792517db1568e4a1
Lianmin Zheng <lianminzheng@gmail.com>
2025-03-09 18:12:50 -0700

    Format code

--- sgl-kernel/csrc/gemm/bmm_fp8.cu
+++ sgl-kernel/csrc/gemm/bmm_fp8.cu
@@ -14,63 +14,63 @@
  * limitations under the License.
  */
 
- #include <driver_types.h>
+#include <driver_types.h>
 
- #include <flashinfer/gemm/bmm_fp8.cuh>
+#include <flashinfer/gemm/bmm_fp8.cuh>
 
- #include "pytorch_extension_utils.h"
+#include "pytorch_extension_utils.h"
 
- void bmm_fp8(
-     at::Tensor A,
-     at::Tensor B,
-     at::Tensor D,
-     at::Tensor A_scale,
-     at::Tensor B_scale,
-     at::Tensor workspace_buffer,
-     int64_t cublas_handle,
-     int64_t cuda_stream) {
-   TORCH_CHECK(A.is_cuda(), "A must be a CUDA tensor");
-   TORCH_CHECK(B.is_cuda(), "B must be a CUDA tensor");
-   TORCH_CHECK(D.is_cuda(), "D must be a CUDA tensor");
-   TORCH_CHECK(A.dim() == 3, "Expected 3D tensor for A");
-   TORCH_CHECK(B.dim() == 3, "Expected 3D tensor for B");
-   TORCH_CHECK(D.dim() == 3, "Expected 3D tensor for D");
-   TORCH_CHECK(A.size(0) == B.size(0) && A.size(0) == D.size(0), "Batch sizes must match");
-   TORCH_CHECK(A.size(2) == B.size(1), "Incompatible matrix sizes");
-   TORCH_CHECK(A.size(1) == D.size(1) && B.size(2) == D.size(2), "Result tensor has incorrect shape");
+void bmm_fp8(
+    at::Tensor A,
+    at::Tensor B,
+    at::Tensor D,
+    at::Tensor A_scale,
+    at::Tensor B_scale,
+    at::Tensor workspace_buffer,
+    int64_t cublas_handle,
+    int64_t cuda_stream) {
+  TORCH_CHECK(A.is_cuda(), "A must be a CUDA tensor");
+  TORCH_CHECK(B.is_cuda(), "B must be a CUDA tensor");
+  TORCH_CHECK(D.is_cuda(), "D must be a CUDA tensor");
+  TORCH_CHECK(A.dim() == 3, "Expected 3D tensor for A");
+  TORCH_CHECK(B.dim() == 3, "Expected 3D tensor for B");
+  TORCH_CHECK(D.dim() == 3, "Expected 3D tensor for D");
+  TORCH_CHECK(A.size(0) == B.size(0) && A.size(0) == D.size(0), "Batch sizes must match");
+  TORCH_CHECK(A.size(2) == B.size(1), "Incompatible matrix sizes");
+  TORCH_CHECK(A.size(1) == D.size(1) && B.size(2) == D.size(2), "Result tensor has incorrect shape");
 
-   // PyTorch is row major by default. cuBLASLt is column major by default.
-   // We need row major D as expected.
-   // A ^ T * B = D, so D ^ T = B ^ T * A
-   DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP8(B.scalar_type(), b_type, [&] {
-     return DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP8(A.scalar_type(), a_type, [&] {
-       return DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP16(D.scalar_type(), d_type, [&] {
-         auto batch_size = A.size(0);
-         auto m = A.size(1);
-         auto k = A.size(2);
-         auto n = B.size(2);
+  // PyTorch is row major by default. cuBLASLt is column major by default.
+  // We need row major D as expected.
+  // A ^ T * B = D, so D ^ T = B ^ T * A
+  DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP8(B.scalar_type(), b_type, [&] {
+    return DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP8(A.scalar_type(), a_type, [&] {
+      return DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP16(D.scalar_type(), d_type, [&] {
+        auto batch_size = A.size(0);
+        auto m = A.size(1);
+        auto k = A.size(2);
+        auto n = B.size(2);
 
-         auto lt_handle = reinterpret_cast<cublasLtHandle_t>(cublas_handle);
-         auto stream = reinterpret_cast<cudaStream_t>(cuda_stream);
+        auto lt_handle = reinterpret_cast<cublasLtHandle_t>(cublas_handle);
+        auto stream = reinterpret_cast<cudaStream_t>(cuda_stream);
 
-         auto status = flashinfer::bmm_fp8::bmm_fp8_internal_cublaslt(
-             workspace_buffer.data_ptr(),
-             workspace_buffer.numel(),
-             static_cast<b_type*>(B.data_ptr()),
-             static_cast<a_type*>(A.data_ptr()),
-             static_cast<d_type*>(D.data_ptr()),
-             batch_size,
-             n,
-             m,
-             k,
-             static_cast<float*>(B_scale.data_ptr()),
-             static_cast<float*>(A_scale.data_ptr()),
-             lt_handle,
-             stream);
-         TORCH_CHECK(
-             status == CUBLAS_STATUS_SUCCESS, "bmm_fp8_internal_cublaslt failed: ", cublasGetStatusString(status));
-         return true;
-       });
-     });
-   });
- }
+        auto status = flashinfer::bmm_fp8::bmm_fp8_internal_cublaslt(
+            workspace_buffer.data_ptr(),
+            workspace_buffer.numel(),
+            static_cast<b_type*>(B.data_ptr()),
+            static_cast<a_type*>(A.data_ptr()),
+            static_cast<d_type*>(D.data_ptr()),
+            batch_size,
+            n,
+            m,
+            k,
+            static_cast<float*>(B_scale.data_ptr()),
+            static_cast<float*>(A_scale.data_ptr()),
+            lt_handle,
+            stream);
+        TORCH_CHECK(
+            status == CUBLAS_STATUS_SUCCESS, "bmm_fp8_internal_cublaslt failed: ", cublasGetStatusString(status));
+        return true;
+      });
+    });
+  });
+}
