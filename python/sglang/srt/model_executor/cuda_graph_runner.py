074b82daef030a6d9fbd6c5b0159363c89e2b4a0
james <jamesliu@together.ai>
2025-03-09 19:25:36 -0700

    linting

--- python/sglang/srt/model_executor/cuda_graph_runner.py
+++ python/sglang/srt/model_executor/cuda_graph_runner.py
@@ -219,7 +219,10 @@ class CudaGraphRunner:
             self.mrope_positions = torch.zeros((3, self.max_bs), dtype=torch.int64)
 
             # Speculative_inference
-            if model_runner.spec_algorithm.is_eagle3() and not model_runner.is_draft_worker:
+            if (
+                model_runner.spec_algorithm.is_eagle3()
+                and not model_runner.is_draft_worker
+            ):
                 self.hidden_states = torch.zeros(
                     (
                         self.max_num_token,
@@ -227,7 +230,7 @@ class CudaGraphRunner:
                     ),
                     dtype=self.model_runner.dtype,
                 )
-                 # auxiliary hidden capture mode. TODO: expose this to server args?
+                # auxiliary hidden capture mode. TODO: expose this to server args?
                 num_layers = self.model_runner.model.config.num_hidden_layers
                 layers_to_capture = [2, num_layers // 2, num_layers - 3]
                 self.model_runner.model.set_layers_to_capture(layers_to_capture)
