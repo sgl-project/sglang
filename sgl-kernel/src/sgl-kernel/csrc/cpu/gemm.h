46b44da0224f7ecce4d25a66652f122722b602a0
mingfeima <mingfei.ma@intel.com>
2025-03-10 19:56:32 -0700

    fused moe: fix when w13 has OC not multiples of 64
    w13 has output channel of 1408 and when TP=3, each rank will have 480 after padding
    which is 15 * 32.

--- sgl-kernel/src/sgl-kernel/csrc/cpu/gemm.h
+++ sgl-kernel/src/sgl-kernel/csrc/cpu/gemm.h
@@ -10,8 +10,8 @@
 #define TILE_SIZE 512
 
 // block size for AMX gemm
-constexpr int block_size_m() { return 4 * TILE_M; }
-constexpr int block_size_n() { return 4 * TILE_N; }
+constexpr int block_size_m() { return 2 * TILE_M; }
+constexpr int block_size_n() { return 2 * TILE_N; }
 
 // work around compiler internal error
 #define BLOCK_K 128 // 4 * TILE_K
