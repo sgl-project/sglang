3370df593f0a6a82da5b409f1625b30bb27b173f
yigex <yigex@amd.com>
2025-03-11 15:07:21 +0000

    add aiter car

--- python/sglang/srt/_custom_ops.py
+++ python/sglang/srt/_custom_ops.py
@@ -114,6 +114,54 @@ else:
         def get_meta_buffer_ipc_handle(inp: torch.Tensor) -> torch.Tensor:
             return sgl_kernel.allreduce.get_meta_buffer_ipc_handle(inp)
 
+    elif is_hip_ and get_bool_env_var("CK_MOE"):
+        import aiter
+        import aiter.custom_all_reduce as aiter_custom_ar
+
+        def init_custom_ar(
+            meta: torch.Tensor,
+            rank_data: torch.Tensor,
+            handles: List[str],
+            offsets: List[int],
+            rank: int,
+            full_nvlink: bool,
+        ) -> int:
+            return aiter_custom_ar.init_custom_ar(
+                meta, rank_data, handles, offsets, rank, full_nvlink
+            )
+
+        def all_reduce_reg(fa: int, inp: torch.Tensor, out: torch.Tensor) -> None:
+            aiter_custom_ar.all_reduce_reg(fa, inp, out)
+
+        def all_reduce_unreg(
+            fa: int, inp: torch.Tensor, reg_buffer: torch.Tensor, out: torch.Tensor
+        ) -> None:
+            aiter_custom_ar.all_reduce_unreg(fa, inp, reg_buffer, out)
+
+        def dispose(fa: int) -> None:
+            aiter_custom_ar.dispose(fa)
+
+        def meta_size() -> int:
+            return aiter_custom_ar.meta_size()
+
+        def register_buffer(
+            fa: int, t: torch.Tensor, handles: List[str], offsets: List[int]
+        ) -> None:
+            return aiter_custom_ar.register_buffer(fa, t, handles, offsets)
+
+        def get_graph_buffer_ipc_meta(fa: int) -> Tuple[torch.Tensor, List[int]]:
+            return aiter_custom_ar.get_graph_buffer_ipc_meta(fa)
+
+        def register_graph_buffers(
+            fa: int, handles: List[str], offsets: List[List[int]]
+        ) -> None:
+            aiter_custom_ar.register_graph_buffers(fa, handles, offsets)
+
+        def allocate_meta_buffer(size: int) -> torch.Tensor:
+            return aiter_custom_ar.allocate_meta_buffer(size)
+
+        def get_meta_buffer_ipc_handle(inp: torch.Tensor) -> torch.Tensor:
+            return aiter_custom_ar.get_meta_buffer_ipc_handle(inp)
     else:
         # TRTLLM custom allreduce
         def init_custom_ar(
