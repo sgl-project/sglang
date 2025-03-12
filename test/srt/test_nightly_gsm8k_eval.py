087e0df0904317cdd82462fa2979f19ad81cb314
Lianmin Zheng <lianminzheng@gmail.com>
2025-03-12 15:35:45 -0700

    Update test_nightly_gsm8k_eval.py

--- test/srt/test_nightly_gsm8k_eval.py
+++ test/srt/test_nightly_gsm8k_eval.py
@@ -31,7 +31,7 @@ MODEL_SCORE_THRESHOLDS = {
     "neuralmagic/Meta-Llama-3.1-8B-Instruct-FP8": 0.83,
     "neuralmagic/Mistral-7B-Instruct-v0.3-FP8": 0.54,
     "neuralmagic/DeepSeek-Coder-V2-Lite-Instruct-FP8": 0.84,
-    "neuralmagic/gemma-2-2b-it-FP8": 0.50,
+    "neuralmagic/gemma-2-2b-it-FP8": 0.60,
     "neuralmagic/Meta-Llama-3.1-70B-Instruct-FP8": 0.94,
     "neuralmagic/Mixtral-8x7B-Instruct-v0.1-FP8": 0.65,
     "neuralmagic/Qwen2-72B-Instruct-FP8": 0.94,
