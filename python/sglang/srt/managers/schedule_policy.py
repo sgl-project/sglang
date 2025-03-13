92fc23c7c40226a7dfef4bc23e26e23db6300c28
Lianmin Zheng <lianminzheng@gmail.com>
2025-03-12 20:50:57 -0700

    Fix policy

--- python/sglang/srt/managers/schedule_policy.py
+++ python/sglang/srt/managers/schedule_policy.py
@@ -113,7 +113,7 @@ class SchedulePolicy:
             else:
                 raise ValueError(f"Unknown CacheAware Policy: {policy=}")
         else:
-            if self.policy == CacheAgnosticPolicy.FCFS:
+            if policy == CacheAgnosticPolicy.FCFS:
                 pass
             elif policy == CacheAgnosticPolicy.LOF:
                 SchedulePolicy._sort_by_longest_output(waiting_queue)
