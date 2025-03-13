e25acc86a054cd9a0b7c580f085c5ea7b2c48601
xihuai18 <leoxhwang@sjtu.edu.cn>
2025-03-13 21:31:21 +0800

    fix(finish_reason): :bug:

--- python/sglang/srt/openai_api/adapter.py
+++ python/sglang/srt/openai_api/adapter.py
@@ -635,7 +635,7 @@ def v1_generate_response(request, ret, tokenizer_manager, to_file=False):
                 "index": 0,
                 "text": text,
                 "logprobs": logprobs,
-                "finish_reason": (finish_reason["type"] if finish_reason else ""),
+                "finish_reason": finish_reason["type"],
                 "matched_stop": (
                     finish_reason["matched"]
                     if finish_reason and "matched" in finish_reason
@@ -1303,23 +1303,11 @@ async def v1_chat_completions(tokenizer_manager, raw_request: Request):
                     if is_first:
                         # First chunk with role
                         is_first = False
-                        if (
-                            tokenizer_manager.server_args.reasoning_parser
-                            and request.separate_reasoning
-                        ):
-                            delta = DeltaMessage(
-                                role="assistant", reasoning_content=None
-                            )
-                        else:
-                            delta = DeltaMessage(role="assistant", content=None)
+                        delta = DeltaMessage(role="assistant")
                         choice_data = ChatCompletionResponseStreamChoice(
                             index=index,
                             delta=delta,
-                            finish_reason=(
-                                None
-                                if finish_reason_type and len(finish_reason_type) == 0
-                                else finish_reason_type
-                            ),
+                            finish_reason=finish_reason_type,
                             matched_stop=(
                                 finish_reason["matched"]
                                 if finish_reason and "matched" in finish_reason
@@ -1359,12 +1347,7 @@ async def v1_chat_completions(tokenizer_manager, raw_request: Request):
                                         reasoning_text if reasoning_text else None
                                     )
                                 ),
-                                finish_reason=(
-                                    None
-                                    if finish_reason_type
-                                    and len(finish_reason_type) == 0
-                                    else finish_reason_type
-                                ),
+                                finish_reason=finish_reason_type,
                             )
                             chunk = ChatCompletionStreamResponse(
                                 id=content["meta_info"]["id"],
@@ -1395,12 +1378,7 @@ async def v1_chat_completions(tokenizer_manager, raw_request: Request):
                                 delta=DeltaMessage(
                                     content=normal_text if normal_text else None
                                 ),
-                                finish_reason=(
-                                    None
-                                    if finish_reason_type
-                                    and len(finish_reason_type) == 0
-                                    else finish_reason_type
-                                ),
+                                finish_reason=finish_reason_type,
                             )
                             chunk = ChatCompletionStreamResponse(
                                 id=content["meta_info"]["id"],
@@ -1413,11 +1391,7 @@ async def v1_chat_completions(tokenizer_manager, raw_request: Request):
                         for call_item in calls:
                             # transform call_item -> FunctionResponse + ToolCall
 
-                            if (
-                                content["meta_info"]["finish_reason"]
-                                and content["meta_info"]["finish_reason"]["type"]
-                                == "stop"
-                            ):
+                            if finish_reason_type == "stop":
                                 latest_delta_len = 0
                                 if isinstance(call_item.parameters, str):
                                     latest_delta_len = len(call_item.parameters)
@@ -1438,6 +1412,8 @@ async def v1_chat_completions(tokenizer_manager, raw_request: Request):
                                 )
                                 call_item.parameters = remaining_call
 
+                                finish_reason_type = "tool_calls"
+
                             tool_call = ToolCall(
                                 id=str(call_item.tool_index),
                                 function=FunctionResponse(
@@ -1450,7 +1426,12 @@ async def v1_chat_completions(tokenizer_manager, raw_request: Request):
                                 delta=DeltaMessage(
                                     role="assistant", tool_calls=[tool_call]
                                 ),
-                                finish_reason="tool_call",
+                                finish_reason=(
+                                    None
+                                    if request.stream_options
+                                    and request.stream_options.include_usage
+                                    else finish_reason_type
+                                ),  # additional chunk will be return
                             )
                             chunk = ChatCompletionStreamResponse(
                                 id=content["meta_info"]["id"],
@@ -1469,7 +1450,8 @@ async def v1_chat_completions(tokenizer_manager, raw_request: Request):
                             delta=DeltaMessage(content=delta if delta else None),
                             finish_reason=(
                                 None
-                                if finish_reason_type and len(finish_reason_type) == 0
+                                if request.stream_options
+                                and request.stream_options.include_usage
                                 else finish_reason_type
                             ),
                             matched_stop=(
@@ -1487,6 +1469,14 @@ async def v1_chat_completions(tokenizer_manager, raw_request: Request):
                         yield f"data: {chunk.model_dump_json()}\n\n"
                         stream_buffers[index] = new_stream_buffer
                         is_firsts[index] = is_first
+                if (
+                    finish_reason_type == "stop"
+                    and request.tool_choice != "none"
+                    and any([i in new_stream_buffer for i in TOOLS_TAG_LIST])
+                ):
+                    # if the stream ends with empty string after tool calls
+                    finish_reason_type = "tool_calls"
+
                 if request.stream_options and request.stream_options.include_usage:
                     total_prompt_tokens = sum(
                         tokens
@@ -1502,16 +1492,21 @@ async def v1_chat_completions(tokenizer_manager, raw_request: Request):
                         total_tokens=total_prompt_tokens + total_completion_tokens,
                     )
 
-                    final_usage_chunk = ChatCompletionStreamResponse(
-                        id=content["meta_info"]["id"],
-                        choices=[],
-                        model=request.model,
-                        usage=usage,
-                    )
-                    final_usage_data = final_usage_chunk.model_dump_json(
-                        exclude_none=True
-                    )
-                    yield f"data: {final_usage_data}\n\n"
+                else:
+                    usage = None
+                final_usage_chunk = ChatCompletionStreamResponse(
+                    id=content["meta_info"]["id"],
+                    choices=[
+                        ChatCompletionResponseStreamChoice(
+                            index=index,
+                            delta=DeltaMessage(),
+                            finish_reason=finish_reason_type,
+                        )
+                    ],
+                    model=request.model,
+                    usage=usage,
+                )
+                yield f"data: {final_usage_chunk.model_dump_json()}\n\n"
             except ValueError as e:
                 error = create_streaming_error_response(str(e))
                 yield f"data: {error}\n\n"
