Fix precision issue and fix style

sgl-kernel/benchmark/bench_per_token_group_quant_int8.py
@@ -3,6 +3,7 @@ import itertools
 import torch
 import triton
 from sgl_kernel import sgl_per_token_group_quant_int8
+
 from sglang.srt.layers.quantization.int8_kernel import per_token_group_quant_int8
 
 
@@ -43,7 +44,6 @@ def calculate_diff(batch_size, seq_len, group_size):
     x_q_triton, x_s_triton = per_token_group_quant_int8(x.clone(), group_size)
     x_q_sglang, x_s_sglang = sglang_per_token_group_quant_int8(x.clone(), group_size)
 
-    print(x_q_triton.to(torch.float32) - x_q_sglang.to(torch.float32))
     if torch.allclose(
         x_q_triton.to(torch.float32), x_q_sglang.to(torch.float32), rtol=1e-3, atol=1e-5
     ) and torch.allclose(x_s_triton, x_s_sglang, rtol=1e-3, atol=1e-5):