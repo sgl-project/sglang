c1781be72c6280078fd48380533389d41e9136ab
Chunyuan WU <chunyuan.wu@intel.com>
2025-03-14 10:21:07 +0800

    switch to weight_packed_linear if cpu_has_amx_support (#11)
    * switch to weight_packed_linear if cpu_has_amx_support
    
    * add self.use_intel_amx_backend

--- python/sglang/srt/layers/linear.py
+++ python/sglang/srt/layers/linear.py
@@ -30,7 +30,11 @@ from sglang.srt.layers.quantization.base_config import (
     QuantizationConfig,
     QuantizeMethodBase,
 )
-from sglang.srt.utils import set_weight_attrs
+from sglang.srt.utils import (
+    cpu_has_amx_support,
+    prepack_weight_if_needed,
+    set_weight_attrs,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -165,12 +169,28 @@ class UnquantizedLinearMethod(LinearMethodBase):
         layer.register_parameter("weight", weight)
         set_weight_attrs(weight, extra_weight_attrs)
 
+    def process_weights_after_loading(self, layer: torch.nn.Module) -> None:
+        # Pack weight for get better performance on CPU
+        layer.weight = torch.nn.Parameter(
+            prepack_weight_if_needed(layer.weight.data),
+            requires_grad=False,
+        )
+
+        self.use_intel_amx_backend = (
+            layer.weight.device == torch.device("cpu") and cpu_has_amx_support()
+        )
+
     def apply(
         self,
         layer: torch.nn.Module,
         x: torch.Tensor,
         bias: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
+
+        if self.use_intel_amx_backend:
+            return torch.ops.sgl_kernel.weight_packed_linear(
+                x, layer.weight, bias, True  # is_vnni
+            )
 
         return F.linear(x, layer.weight, bias)
 
