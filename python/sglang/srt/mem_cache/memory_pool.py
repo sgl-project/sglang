53bcdadb525516b51fe20d3e83c5c972678f606e
Zhiqiang Xie <xiezhq@stanford.edu>
2025-03-15 22:10:39 +0000

    revoke change on sync

--- python/sglang/srt/mem_cache/memory_pool.py
+++ python/sglang/srt/mem_cache/memory_pool.py
@@ -165,6 +165,8 @@ class TokenToKVPoolAllocator:
             return None
 
         select_index = self.free_slots[:need_size]
+        # ensure select_index is ready before returning
+        torch.cuda.current_stream().synchronize()
         self.free_slots = self.free_slots[need_size:]
         return select_index
 
