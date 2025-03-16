ac218abb916e7e63e3beeec11a1953621417e231
alcanderian <alcanderian@gmail.com>
2025-03-16 06:16:51 +0000

    [fix] fix lint

--- python/sglang/test/test_dynamic_grad_mode.py
+++ python/sglang/test/test_dynamic_grad_mode.py
@@ -1,22 +1,28 @@
+import unittest
+
 import torch
+
 from sglang.srt.utils import DynamicGradMode
 
-import unittest
 
 class TestDynamicGradMode(unittest.TestCase):
     def test_grad_mode(self):
 
         DynamicGradMode.set_inference_mode(True)
+
         @DynamicGradMode()
         def create_tensor_x():
             return torch.empty(0)
+
         X = create_tensor_x()
         self.assertTrue(not X.requires_grad and X.is_inference())
 
         DynamicGradMode.set_inference_mode(False)
+
         @DynamicGradMode()
         def create_tensor_y():
             return torch.empty(0)
+
         Y = create_tensor_y()
         self.assertTrue(not Y.requires_grad and not Y.is_inference())
 
