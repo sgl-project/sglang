33ff5656667a71a6edc0b7cedbe8a5bc3f7accf7
alcanderian <alcanderian@gmail.com>
2025-03-16 08:41:13 +0000

    [test] add nested grad mode test

--- python/sglang/test/test_dynamic_grad_mode.py
+++ python/sglang/test/test_dynamic_grad_mode.py
@@ -6,8 +6,8 @@ from sglang.srt.utils import DynamicGradMode
 
 
 class TestDynamicGradMode(unittest.TestCase):
-    def test_grad_mode(self):
-
+    def test_inference(self):
+        # Test inference_mode
         DynamicGradMode.set_inference_mode(True)
 
         @DynamicGradMode()
@@ -17,6 +17,8 @@ class TestDynamicGradMode(unittest.TestCase):
         X = create_tensor_x()
         self.assertTrue(not X.requires_grad and X.is_inference())
 
+    def test_no_grad(self):
+        # Test no_grad
         DynamicGradMode.set_inference_mode(False)
 
         @DynamicGradMode()
@@ -25,6 +27,30 @@ class TestDynamicGradMode(unittest.TestCase):
 
         Y = create_tensor_y()
         self.assertTrue(not Y.requires_grad and not Y.is_inference())
+
+    def test_nested_inference(self):
+        # Test no_grad nested inference_mode, inference_mode should has higher priority
+        DynamicGradMode.set_inference_mode(False)
+
+        @DynamicGradMode()
+        def create_tensor_z():
+            with torch.inference_mode():
+                return torch.empty(0)
+
+        Z = create_tensor_z()
+        self.assertTrue(not Z.requires_grad and Z.is_inference())
+
+    def test_nested_no_grad(self):
+        # Test inference_mode nested no_grad, inference_mode should has higher priority
+        DynamicGradMode.set_inference_mode(True)
+
+        @DynamicGradMode()
+        def create_tensor_w():
+            with torch.no_grad():
+                return torch.empty(0)
+
+        W = create_tensor_w()
+        self.assertTrue(not W.requires_grad and W.is_inference())
 
 
 if __name__ == "__main__":
