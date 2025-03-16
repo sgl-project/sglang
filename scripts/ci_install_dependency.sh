1b859295f422fa242ec77243bab2f39bcdb2a7e6
Ying Sheng <sqy1415@gmail.com>
2025-03-16 02:48:55 -0700

    [Eagle] Remove the greedy branch and some redundant code (#4363)
    Co-authored-by: Sehoon Kim <sehoon@x.ai>

--- scripts/ci_install_dependency.sh
+++ scripts/ci_install_dependency.sh
@@ -24,6 +24,3 @@ pip install transformers==4.48.3 sentence_transformers accelerate==1.4.0 peft pa
 
 # For compling xgrammar kernels
 pip install cuda-python nvidia-cuda-nvrtc-cu12
-
-# reinstall sgl-kernel
-pip install sgl-kernel==0.0.5.post1 --force-reinstall --no-deps
