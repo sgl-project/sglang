33a70091945d0eac720a1a62006ae3fae44ccb33
mingfeima <mingfei.ma@intel.com>
2025-03-16 20:02:14 -0700

    add int8_scaled_mm for int8 W8A8 on CPU

--- sgl-kernel/src/sgl-kernel/csrc/cpu/gemm.h
+++ sgl-kernel/src/sgl-kernel/csrc/cpu/gemm.h
@@ -6,15 +6,61 @@
 #define TILE_M 16
 #define TILE_N 16
 #define TILE_K 32
-#define VNNI_BLK 2
-#define TILE_SIZE 512
 
 // block size for AMX gemm
 constexpr int block_size_m() { return 2 * TILE_M; }
 constexpr int block_size_n() { return 2 * TILE_N; }
 
+//template <typename T> constexpr int vnni_blk();
+//template <> constexpr int vnni_blk<at::BFloat16>() { return 2; }
+//template <> constexpr int vnni_blk<at::Half>() { return 2; }
+//template <> constexpr int vnni_blk<int8_t>() { return 4; }
+
 // work around compiler internal error
 #define BLOCK_K 128 // 4 * TILE_K
 
+// adjust leading dimension size for K
+template <typename T>
+inline int get_row_size(int K) {
+  return K;
+}
+
+template <>
+inline int get_row_size<int8_t>(int K) {
+  return K + sizeof(int32_t);
+}
+
 // pack weight to vnni format
 at::Tensor convert_weight_packed(at::Tensor& weight);
+
+// TODO: debug print, remove me later
+inline void print_16x32i(const __m512i x) {
+  int32_t a[16];
+  _mm512_storeu_si512((__m512i *)a, x);
+
+  for (int i = 0; i < 16; i++){
+    std::cout << a[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+inline void print_16x32(const __m512 x) {
+  float a[16];
+  _mm512_storeu_ps((__m512 *)a, x);
+
+  for (int i = 0; i < 16; i++){
+    std::cout << a[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+
+inline void print_32x8u(const __m256i x) {
+  uint8_t a[32];
+  _mm256_storeu_si256((__m256i *)a, x);
+
+  for (int i = 0; i < 32; ++i) {
+    std::cout << int32_t(a[i]) << " ";
+  }
+  std::cout << std::endl;
+}
--- sgl-kernel/src/sgl-kernel/csrc/cpu/vec.h
+++ sgl-kernel/src/sgl-kernel/csrc/cpu/vec.h
@@ -53,7 +53,7 @@ inline float vec_reduce_max(const Vectorized<float>& a) {
 }
 #endif
 
-// debug
+// TODO: debug print, remove me later
 template<typename scalar_t>
 void print_array(scalar_t* ptr, int size) {
   for (int d = 0; d < size; ++d) {
