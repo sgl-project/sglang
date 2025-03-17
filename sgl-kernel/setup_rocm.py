6864fea89054f064fa90dc4b1f8ee88fb209d40f
yigex <yigex@amd.com>
2025-03-17 15:46:52 +0800

    fix rocm verify greedy

--- sgl-kernel/setup_rocm.py
+++ sgl-kernel/setup_rocm.py
@@ -43,7 +43,7 @@ sources = [
     "csrc/moe/moe_align_kernel.cu",
     "csrc/moe/moe_topk_softmax_kernels.cu",
     "csrc/torch_extension_rocm.cc",
-    "csrc/speculative/eagle_utils.cu",
+    "csrc/speculative/eagle_utils.hip",
 ]
 
 cxx_flags = ["-O3"]
