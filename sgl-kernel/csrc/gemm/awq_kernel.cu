a995a6b32ace7ea92cfc2db870b2ff90e8bff5bd
laixinn <xielx@shanghaitech.edu.cn>
2025-03-18 22:37:21 +0800

    support bf16 awq_dequant

--- sgl-kernel/csrc/gemm/awq_kernel.cu
+++ sgl-kernel/csrc/gemm/awq_kernel.cu
@@ -2,7 +2,97 @@
 // https://github.com/vllm-project/vllm/blob/eb59b5a6cba6727d3727c0372258db9002f687c1/csrc/quantization/awq/gemm_kernels.cu#L350
 #include <c10/cuda/CUDAGuard.h>
 #include <cuda_fp16.h>
+#include <cuda_bf16.h>
 #include <torch/all.h>
+
+
+__device__ uint4 dequantize_s4_to_bf16x2(uint32_t const& source) {
+#if (defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 900)
+  uint4 result;
+
+  uint32_t* h = reinterpret_cast<uint32_t*>(&result);
+  uint32_t const i4s = reinterpret_cast<uint32_t const&>(source);
+
+  // First, we extract the i4s and construct an intermediate bf16 number.
+  static constexpr uint32_t immLut = (0xf0 & 0xcc) | 0xaa;
+  static constexpr uint32_t BOTTOM_MASK = 0x000f000f;
+  static constexpr uint32_t I4s_TO_BF16s_MAGIC_NUM = 0x43004300;
+
+  // Issue first to hide RAW dependency if we issue immediately before required.
+  // Shift once is not feasible because bf16 has only 7 bits for the mantissa.
+  const uint32_t i4s_23 = i4s >> 4;
+  const uint32_t i4s_45 = i4s >> 8;
+  const uint32_t i4s_67 = i4s >> 12;
+  // Extract elt_01 (i4s & 0x000f000f) | 0x43004300
+  asm volatile("lop3.b32 %0, %1, %2, %3, %4;\n"
+                : "=r"(h[0])
+                : "r"(i4s), "n"(BOTTOM_MASK), "n"(I4s_TO_BF16s_MAGIC_NUM), "n"(immLut));
+  // Extract elt_23 (i4s & 0x000f000f) | 0x43004300
+  asm volatile("lop3.b32 %0, %1, %2, %3, %4;\n"
+                : "=r"(h[1])
+                : "r"(i4s_23), "n"(BOTTOM_MASK), "n"(I4s_TO_BF16s_MAGIC_NUM), "n"(immLut));
+  // Extract elt_45 (top_i4s & 0x000f000f) | 0x43004300
+  asm volatile("lop3.b32 %0, %1, %2, %3, %4;\n"
+                : "=r"(h[2])
+                : "r"(i4s_45), "n"(BOTTOM_MASK), "n"(I4s_TO_BF16s_MAGIC_NUM), "n"(immLut));
+  // Extract elt_67 (top_i4s & 0x000f000f) | 0x43004300
+  asm volatile("lop3.b32 %0, %1, %2, %3, %4;\n"
+                : "=r"(h[3])
+                : "r"(i4s_67), "n"(BOTTOM_MASK), "n"(I4s_TO_BF16s_MAGIC_NUM), "n"(immLut));
+
+  // This is the half2 {1024, 1024} represented as an integer.
+  static constexpr uint32_t BF16_TOP_MAGIC_NUM = 0x43004300;
+
+  // Finally, we construct the output numbers.
+  // Convert elt_01
+  asm volatile("sub.bf16x2 %0, %1, %2;\n" : "=r"(h[0]) : "r"(h[0]), "r"(BF16_TOP_MAGIC_NUM));
+  // Convert elt_23
+  asm volatile("sub.bf16x2 %0, %1, %2;\n" : "=r"(h[1]) : "r"(h[1]), "r"(BF16_TOP_MAGIC_NUM));
+  // Convert elt_45
+  asm volatile("sub.bf16x2 %0, %1, %2;\n" : "=r"(h[2]) : "r"(h[2]), "r"(BF16_TOP_MAGIC_NUM));
+  // Convert elt_67
+  asm volatile("sub.bf16x2 %0, %1, %2;\n" : "=r"(h[3]) : "r"(h[3]), "r"(BF16_TOP_MAGIC_NUM));
+
+  return result;
+#else
+  assert(false);
+  return {};
+#endif
+}
+
+__global__ void __launch_bounds__(256) dequantize_weights_bf16(
+    int* __restrict__ qweight,
+    __nv_bfloat16* __restrict__ scales,
+    int* __restrict__ qzeros,
+    __nv_bfloat16* __restrict__ output,
+    int group_size,
+    int qweight_cols) {
+#if (defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 900)
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+  uint4 zeros = dequantize_s4_to_bf16x2(qzeros[col + (row / group_size) * qweight_cols]);
+  uint4 loaded_scale = *(uint4*)(scales + 8 * col + (row / group_size) * qweight_cols * 8);
+
+  uint4 weight_bf16 = dequantize_s4_to_bf16x2(qweight[col + row * qweight_cols]);
+
+  asm volatile("sub.bf16x2 %0, %1, %2;\n" : "=r"(weight_bf16.x) : "r"(weight_bf16.x), "r"(zeros.x));
+  asm volatile("mul.rn.bf16x2 %0, %1, %2;\n" : "=r"(weight_bf16.x) : "r"(weight_bf16.x), "r"(loaded_scale.x));
+  asm volatile("sub.bf16x2 %0, %1, %2;\n" : "=r"(weight_bf16.y) : "r"(weight_bf16.y), "r"(zeros.y));
+  asm volatile("mul.rn.bf16x2 %0, %1, %2;\n" : "=r"(weight_bf16.y) : "r"(weight_bf16.y), "r"(loaded_scale.y));
+  asm volatile("sub.bf16x2 %0, %1, %2;\n" : "=r"(weight_bf16.z) : "r"(weight_bf16.z), "r"(zeros.z));
+  asm volatile("mul.rn.bf16x2 %0, %1, %2;\n" : "=r"(weight_bf16.z) : "r"(weight_bf16.z), "r"(loaded_scale.z));
+  asm volatile("sub.bf16x2 %0, %1, %2;\n" : "=r"(weight_bf16.w) : "r"(weight_bf16.w), "r"(zeros.w));
+  asm volatile("mul.rn.bf16x2 %0, %1, %2;\n" : "=r"(weight_bf16.w) : "r"(weight_bf16.w), "r"(loaded_scale.w));
+
+  __nv_bfloat16* output_ptr = output + 8 * col + 8 * row * qweight_cols;
+  *(uint4*)output_ptr = weight_bf16;
+#else
+  assert(false);
+  return {};
+#endif
+}
+
 
 __device__ uint4 dequantize_s4_to_fp16x2(uint32_t const& source) {
 #if defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 750
@@ -68,7 +158,7 @@ __device__ uint4 dequantize_s4_to_fp16x2(uint32_t const& source) {
 #endif
 }
 
-__global__ void __launch_bounds__(256) dequantize_weights(
+__global__ void __launch_bounds__(256) dequantize_weights_fp16(
     int* __restrict__ qweight,
     half* __restrict__ scales,
     int* __restrict__ qzeros,
@@ -111,17 +201,39 @@ torch::Tensor awq_dequantize(torch::Tensor qweight, torch::Tensor scales, torch:
   auto output_tensor_options = torch::TensorOptions().dtype(scales.dtype()).device(scales.device());
   at::Tensor output = torch::empty({qweight_rows, qweight_cols * 8}, output_tensor_options);
 
-  auto _qweight = reinterpret_cast<int*>(qweight.data_ptr<int>());
-  auto _scales = reinterpret_cast<half*>(scales.data_ptr<at::Half>());
-  auto _zeros = reinterpret_cast<int*>(qzeros.data_ptr<int>());
-  auto _output = reinterpret_cast<half*>(output.data_ptr<at::Half>());
+  if(scales.scalar_type() == at::ScalarType::Half){
+    auto _qweight = reinterpret_cast<int*>(qweight.data_ptr<int>());
+    auto _zeros = reinterpret_cast<int*>(qzeros.data_ptr<int>());
+    auto _scales = reinterpret_cast<half*>(scales.data_ptr<at::Half>());
+    auto _output = reinterpret_cast<half*>(output.data_ptr<at::Half>());
 
-  dim3 num_blocks(x_blocks, y_blocks);
-  dim3 threads_per_block(x_num_threads, y_num_threads);
+    dim3 num_blocks(x_blocks, y_blocks);
+    dim3 threads_per_block(x_num_threads, y_num_threads);
 
-  const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-  dequantize_weights<<<num_blocks, threads_per_block, 0, stream>>>(
-      _qweight, _scales, _zeros, _output, group_size, qweight_cols);
+    const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+
+    dequantize_weights_fp16<<<num_blocks, threads_per_block, 0, stream>>>(
+        _qweight, _scales, _zeros, _output, group_size, qweight_cols);
+  }
+#if (defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 900)
+  else if(scales.scalar_type() == at::ScalarType::BFloat16){
+    auto _qweight = reinterpret_cast<int*>(qweight.data_ptr<int>());
+    auto _zeros = reinterpret_cast<int*>(qzeros.data_ptr<int>());
+    auto _scales = reinterpret_cast<__nv_bfloat16*>(scales.data_ptr<at::BFloat16>());
+    auto _output = reinterpret_cast<__nv_bfloat16*>(output.data_ptr<at::BFloat16>());
+
+    dim3 num_blocks(x_blocks, y_blocks);
+    dim3 threads_per_block(x_num_threads, y_num_threads);
+
+    const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+
+    dequantize_weights_bf16<<<num_blocks, threads_per_block, 0, stream>>>(
+        _qweight, _scales, _zeros, _output, group_size, qweight_cols);
+  }
+#endif
+  else{
+    assert(false);
+  }
 
   return output;
 }
