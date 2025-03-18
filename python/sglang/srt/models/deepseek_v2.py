a4615ac0fedcff40232ea6a4a18fdba8b20db216
leoneo <1320612015@qq.com>
2025-03-19 00:45:22 +0800

    fix some nuts

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -69,7 +69,6 @@ from sglang.srt.managers.schedule_batch import global_server_args_dict
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch
 from sglang.srt.model_loader.weight_utils import default_weight_loader
 from sglang.srt.utils import add_prefix, is_cuda, is_cuda_available, is_hip
-from sgl_kernel import awq_dequantize
 
 _is_hip = is_hip()
 _is_cuda = is_cuda()
