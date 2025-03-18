74fccc04bb9c978511955497008985a24a7bc5f3
adarshxs <adarsh.shirawalmath@gmail.com>
2025-03-18 04:50:49 +0000

    resolve conflicts

--- python/sglang/srt/configs/__init__.py
+++ python/sglang/srt/configs/__init__.py
@@ -2,7 +2,6 @@ from sglang.srt.configs.chatglm import ChatGLMConfig
 from sglang.srt.configs.dbrx import DbrxConfig
 from sglang.srt.configs.deepseekvl2 import DeepseekVL2Config
 from sglang.srt.configs.exaone import ExaoneConfig
-
 from sglang.srt.configs.gemma3 import Gemma3Config, Gemma3TextConfig
 from sglang.srt.configs.janus_pro import MultiModalityConfig
 
