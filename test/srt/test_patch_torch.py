de3fadf660084baa1d277e601eba45104ff0dab2
fzyzcjy <ch271828n@outlook.com>
2025-03-19 11:01:49 +0800

    empty

---
+++ test/srt/test_patch_torch.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestReleaseMemoryOccupation(unittest.TestCase):
+    def test_monkey_patch_torch_reductions(self):
+        TDOO
+
+
+if __name__ == "__main__":
+    unittest.main()
