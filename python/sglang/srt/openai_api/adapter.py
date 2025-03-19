afe481e2e412a27fa7c81f9e8615f8fbeb699507
minleminzui <2969413251@qq.com>
2025-03-19 14:06:11 +0000

    more

--- python/sglang/srt/openai_api/adapter.py
+++ python/sglang/srt/openai_api/adapter.py
@@ -19,25 +19,13 @@ import logging
 import os
 import time
 import uuid
-# from http import HTTPStatus
-# from typing import Dict, List, Type, Union
-
-# from fastapi import HTTPException, Request, UploadFile
-# from fastapi.responses import ORJSONResponse, StreamingResponse
-# from pydantic import BaseModel, ValidationError
-
-from typing import Dict, List
+from http import HTTPStatus
+from typing import Dict, List, Type, Union
 
 from fastapi import HTTPException, Request, UploadFile
 from fastapi.responses import ORJSONResponse, StreamingResponse
-from pydantic import ValidationError
+from pydantic import BaseModel, ValidationError
 
-try:
-    from outlines.fsm.json_schema import convert_json_schema_to_str
-except ImportError:
-    # Before outlines 0.0.47, convert_json_schema_to_str is under
-    # outlines.integrations.utils
-    from outlines.integrations.utils import convert_json_schema_to_str
 from sglang.srt.code_completion_parser import (
     generate_completion_prompt_from_request,
     is_completion_template_defined,
