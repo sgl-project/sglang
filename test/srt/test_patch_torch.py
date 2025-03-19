058b0db26c9b951a230d394f6cb3d9cf19099a54
fzyzcjy <ch271828n@outlook.com>
2025-03-19 11:06:48 +0800

    more

--- test/srt/test_patch_torch.py
+++ test/srt/test_patch_torch.py
@@ -1,5 +1,7 @@
+import os
 import traceback
 import unittest
+from typing import List
 
 import torch.multiprocessing as mp
 
@@ -9,10 +11,15 @@ class TestReleaseMemoryOccupation(unittest.TestCase):
         self._test_monkey_patch_torch_reductions_core()
 
     def _test_monkey_patch_torch_reductions_core(self):
+        print(f'test_monkey_patch_torch_reductions_core {os.environ.get("CUDA_VISIBLE_DEVICES")=}')
+        cuda_visible_devices_list: List[int] = \
+            [int(x) for x in os.environ.get('CUDA_VISIBLE_DEVICES', '0,1,2,3,4,5,6,7').split(',')]
+
         processes = []
         output_reader, output_writer = mp.Pipe(duplex=False)
         queue = mp.Queue()
         for rank in range(2):
+            os.environ['CUDA_VISIBLE_DEVICES'] = TODO
             p = mp.Process(
                 target=_run_subprocess,
                 kwargs=dict(rank=rank, queue=queue, output_writer=output_writer),
@@ -28,6 +35,7 @@ class TestReleaseMemoryOccupation(unittest.TestCase):
 
 
 def _run_subprocess(rank: int, queue: mp.Queue, output_writer):
+    print(f'subprocess[{rank}] start {os.environ.get("CUDA_VISIBLE_DEVICES")=}', flush=True)
     try:
         TODO
         execution_ok = True
