fix aiter mha causal mask

python/sglang/srt/layers/attention/triton_backend.py
@@ -194,10 +194,14 @@ class TritonAttnBackend(AttentionBackend):
             mask_indptr = None
             attn_logits = None
             max_extend_len = torch.max(forward_batch.extend_seq_lens).item()
+            max_prefix_extend_len = torch.max(
+                forward_batch.extend_seq_lens + forward_batch.extend_prefix_lens
+            ).item()
 
         self.forward_metadata = (
             attn_logits,
             max_extend_len,
+            max_prefix_extend_len,
             kv_indptr,
             kv_indices,
             qo_indptr,
@@ -397,6 +401,7 @@ class TritonAttnBackend(AttentionBackend):
         (
             _,
             max_extend_len,
+            max_prefix_extend_len,
             kv_indptr,
             kv_indices,
             qo_indptr,
@@ -404,34 +409,93 @@ class TritonAttnBackend(AttentionBackend):
             mask_indptr,
         ) = self.forward_metadata
 
-        if _is_hip and kv_indices.shape[0] == 0 and get_bool_env_var("CK_MOE"):
-            o, *_ = flash_attn_varlen_func(
-                q,
-                k,
-                v,
-                qo_indptr,
-                qo_indptr,
-                max_extend_len,
-                max_extend_len,
-                softmax_scale=layer.scaling,
-            )
-        else:
-            self.extend_attention_fwd(
-                q.view(-1, layer.tp_q_head_num, layer.qk_head_dim),
-                k.contiguous(),
-                v.contiguous(),
-                o.view(-1, layer.tp_q_head_num, layer.v_head_dim),
-                forward_batch.token_to_kv_pool.get_key_buffer(layer.layer_id),
-                forward_batch.token_to_kv_pool.get_value_buffer(layer.layer_id),
-                qo_indptr,
-                kv_indptr,
-                kv_indices,
-                custom_mask,
-                mask_indptr,
-                max_extend_len,
-                layer.scaling,
-                layer.logit_cap,
-            )
+        if _is_hip and get_bool_env_var("CK_MOE"):
+            K_Buffer = forward_batch.token_to_kv_pool.get_key_buffer(layer.layer_id)
+            V_Buffer = forward_batch.token_to_kv_pool.get_value_buffer(layer.layer_id)
+            kv_lora_rank = V_Buffer.shape[-1]
+            qk_rope_head_dim = K_Buffer.shape[-1] - kv_lora_rank
+            qk_nope_head_dim = k.shape[-1] - qk_rope_head_dim
+            assert len(q.shape) == 3
+            assert len(k.shape) == 3
+            assert len(v.shape) == 3
+            if _is_hip and kv_indices.shape[0] == 0:
+                o, *_ = flash_attn_varlen_func(
+                    q,
+                    k,
+                    v,
+                    qo_indptr,
+                    qo_indptr,
+                    max_extend_len,
+                    max_extend_len,
+                    softmax_scale=layer.scaling,
+                    causal=True,
+                )
+                return o
+            elif layer.qk_head_dim != (kv_lora_rank + qk_rope_head_dim):
+                cu_seqlens = qo_indptr + kv_indptr
+                max_seqlen = max_prefix_extend_len
+                K_Buffer = torch.index_select(K_Buffer, 0, kv_indices)
+                kvc, k_pe = torch.split(
+                    K_Buffer, [kv_lora_rank, qk_rope_head_dim], dim=-1
+                )
+                kvprefix = layer.kv_b_proj(kvc.contiguous())[0]
+
+                kvprefix = kvprefix.view(
+                    -1, layer.tp_k_head_num, qk_nope_head_dim + layer.v_head_dim
+                )
+                k_prefix, v_prefix = torch.split(
+                    kvprefix, [qk_nope_head_dim, layer.v_head_dim], dim=-1
+                )
+                k_prefix = torch.cat(
+                    [
+                        k_prefix,
+                        torch.broadcast_to(
+                            k_pe, (k_pe.shape[0], layer.tp_k_head_num, k_pe.shape[2])
+                        ),
+                    ],
+                    dim=-1,
+                )
+                assert (
+                    forward_batch.extend_prefix_lens.shape
+                    == forward_batch.extend_seq_lens.shape
+                )
+                k_prefix = torch.split(k_prefix, forward_batch.extend_prefix_lens_cpu)
+                k_extend = torch.split(k, forward_batch.extend_seq_lens_cpu)
+                assert len(k_prefix) == len(forward_batch.extend_prefix_lens_cpu)
+                k = torch.cat([x for el in zip(k_prefix, k_extend) for x in el])
+                v_prefix = torch.split(v_prefix, forward_batch.extend_prefix_lens_cpu)
+                v_extend = torch.split(v, forward_batch.extend_seq_lens_cpu)
+                v = torch.cat([x for el in zip(v_prefix, v_extend) for x in el])
+
+                o, *_ = flash_attn_varlen_func(
+                    q,
+                    k,
+                    v,
+                    qo_indptr,
+                    cu_seqlens,
+                    max_extend_len,
+                    max_seqlen,
+                    softmax_scale=layer.scaling,
+                    causal=True,
+                )
+                return o
+
+        self.extend_attention_fwd(
+            q.view(-1, layer.tp_q_head_num, layer.qk_head_dim),
+            k.contiguous(),
+            v.contiguous(),
+            o.view(-1, layer.tp_q_head_num, layer.v_head_dim),
+            forward_batch.token_to_kv_pool.get_key_buffer(layer.layer_id),
+            forward_batch.token_to_kv_pool.get_value_buffer(layer.layer_id),
+            qo_indptr,
+            kv_indptr,
+            kv_indices,
+            custom_mask,
+            mask_indptr,
+            max_extend_len,
+            layer.scaling,
+            layer.logit_cap,
+        )
         return o
 
     def forward_decode(