1f858db4e3b527dead7f4c25bc32bca78584a869
fzyzcjy <ch271828n@outlook.com>
2025-03-20 09:02:46 +0800

    more

--- python/sglang/srt/layers/attention/composed_backend.py
+++ python/sglang/srt/layers/attention/composed_backend.py
@@ -1,7 +1,9 @@
-from __future__ import annotations
+from typing import Optional, Union
 
+import torch
 from sglang.srt.layers.attention.base_attn_backend import AttentionBackend
-from sglang.srt.model_executor.forward_batch_info import ForwardBatch
+from sglang.srt.model_executor.forward_batch_info import ForwardBatch, ForwardMode
+from sglang.srt.speculative.eagle_utils import EagleDraftInput, EagleVerifyInput
 
 
 class ComposedAttnBackend(AttentionBackend):
@@ -9,6 +11,38 @@ class ComposedAttnBackend(AttentionBackend):
         super().__init__()
 
     def init_forward_metadata(self, forward_batch: ForwardBatch):
+        TODO
+
+    def init_cuda_graph_state(self, max_bs: int):
+        TODO
+
+    def init_forward_metadata_capture_cuda_graph(
+        self,
+        bs: int,
+        num_tokens: int,
+        req_pool_indices: torch.Tensor,
+        seq_lens: torch.Tensor,
+        encoder_lens: Optional[torch.Tensor],
+        forward_mode: ForwardMode,
+        spec_info: Optional[Union[EagleDraftInput, EagleVerifyInput]],
+    ):
+        TODO
+
+    def init_forward_metadata_replay_cuda_graph(
+        self,
+        bs: int,
+        num_kv_heads: int,
+        req_pool_indices: torch.Tensor,
+        seq_lens: torch.Tensor,
+        seq_lens_sum: int,
+        encoder_lens: Optional[torch.Tensor],
+        forward_mode: ForwardMode,
+        spec_info: Optional[Union[EagleDraftInput, EagleVerifyInput]],
+        seq_lens_cpu: Optional[torch.Tensor],
+    ):
+        TODO
+
+    def get_cuda_graph_seq_len_fill_value(self):
         TODO
 
     def forward_extend(self, *args, **kwargs):
