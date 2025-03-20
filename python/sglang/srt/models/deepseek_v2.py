a83ff60f7a54ff5ffa15059667da87f68eab3d12
fzyzcjy <ch271828n@outlook.com>
2025-03-21 07:49:15 +0800

    more

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -375,7 +375,7 @@ class DeepseekV2MoE(nn.Module):
         shared_output = self._forward_deepep_shared_output(
             state["forward_batch"].forward_mode, state["hidden_states_for_moe_input"]
         )
-        state |= self._forward_tbo_substage_combine_wait(state)
+        self._forward_tbo_substage_combine_wait(state)
         output_hidden_states = state["hidden_states_from_combine"] + shared_output
         output = self._forward_tbo_substage_compute_layer_output(state, output_hidden_states)
         state.clear()
@@ -393,7 +393,7 @@ class DeepseekV2MoE(nn.Module):
         self._forward_tbo_substage_combine_start(state)
 
     def _forward_tbo_stage_decode_extra(self, state):
-        state |= self._forward_tbo_substage_combine_wait(state)
+        self._forward_tbo_substage_combine_wait(state)
         output_hidden_states = (
             state["hidden_states_from_combine"] + state["shared_output"]
         )
@@ -434,11 +434,8 @@ class DeepseekV2MoE(nn.Module):
         )
 
     def _forward_tbo_substage_combine_wait(self, state):
-        hidden_states_from_combine = self.tbo_deepep_dispatchers[
+        state['hidden_states_from_combine'] = self.tbo_deepep_dispatchers[
             state["tbo_subbatch_index"]].combine_stage_wait(state['state_combine'])
-        return dict(
-            hidden_states_from_combine=hidden_states_from_combine,
-        )
 
     def _forward_tbo_substage_compute_layer_output(self, state, output_hidden_states):
         return dict(
