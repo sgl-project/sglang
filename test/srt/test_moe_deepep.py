888d7c8638cc6eb034009c68fb3ee3271b559da5
fzyzcjy <ch271828n@outlook.com>
2025-03-20 14:29:26 +0800

    fmt

--- test/srt/test_moe_deepep.py
+++ test/srt/test_moe_deepep.py
@@ -16,7 +16,7 @@ class TestDeepEPMoE(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.model = DEFAULT_MLA_MODEL_NAME_FOR_TEST
-        cls.base_url = "http://127.0.0.1:" + os.environ.get('HACK_PORT', '2157')
+        cls.base_url = "http://127.0.0.1:" + os.environ.get("HACK_PORT", "2157")
         cls.process = popen_launch_server(
             cls.model,
             cls.base_url,
