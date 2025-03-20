0e5dda1778fcfd4758cbe845349c975633327dcc
AniZpZ <zhuangsen.zp@antgroup.com>
2025-03-20 18:37:53 +0800

    update

--- python/sglang/srt/layers/quantization/moe_wna16.py
+++ python/sglang/srt/layers/quantization/moe_wna16.py
@@ -1,4 +1,5 @@
-# SPDX-License-Identifier: Apache-2.0
+# Adapted from https://github.com/vllm-project/vllm/blob/main/vllm/model_executor/layers/quantization/moe_wna16.py
+
 import logging
 from typing import Any, Callable, Dict, List, Optional
 
