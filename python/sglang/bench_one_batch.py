cb83c8a2f292bd9654a43509f8eee37ef9e8c184
fzyzcjy <ch271828n@outlook.com>
2025-03-21 08:45:36 +0800

    more

--- python/sglang/bench_one_batch.py
+++ python/sglang/bench_one_batch.py
@@ -267,7 +267,7 @@ def _maybe_prepare_dp_attn_batch(batch: ScheduleBatch, model_runner):
             tp_cpu_group=model_runner.tp_group.cpu_group,
             get_idle_batch=None,
             disable_cuda_graph=model_runner.server_args.disable_cuda_graph,
-            spec_algorithm=None,
+            spec_algorithm=SpeculativeAlgorithm.NONE,
             speculative_num_draft_tokens=None,
         )
 
