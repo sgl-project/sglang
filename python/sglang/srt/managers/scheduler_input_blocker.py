cf1d1dc6f6dc7db38a923b4fcf2bd241d82c1274
fzyzcjy <ch271828n@outlook.com>
2025-03-23 18:09:58 +0800

    more

--- python/sglang/srt/managers/scheduler_input_blocker.py
+++ python/sglang/srt/managers/scheduler_input_blocker.py
@@ -11,6 +11,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+from typing import List
+
 
 class SchedulerInputBlocker:
-    pass
+    def handle(self, recv_reqs: List):
+        TODO
