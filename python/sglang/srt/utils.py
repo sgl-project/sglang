29929bf4ab2b8bf0de26422a84ba1e9e4e4affc0
vikram singh shekhawat <vshekhawat@habana.ai>
2025-03-24 14:18:25 +0530

    Update utils.py

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -1179,9 +1179,7 @@ def get_distributed_backend() -> str:
     device = get_device().lower()
 
     backend_map = {"cuda": "nccl", "xpu": "ccl", "hpu": "hccl", "cpu": "gloo"}
-
-    backend = backend_map.get(device, "nccl")
-    return backend
+    return backend_map[device]
 
 
 @lru_cache(maxsize=8)
