8bd799507e5651ce4b6f555f96f23aae611c57f3
Yubo Wang <yubowang2019@gmail.com>
2025-03-25 16:31:43 +0000

    set datapath to None

--- test/srt/test_flash_attention3.py
+++ test/srt/test_flash_attention3.py
@@ -7,6 +7,7 @@ import torch
 from sglang.srt.utils import kill_process_tree
 from sglang.test.few_shot_gsm8k import run_eval as run_eval_few_shot_gsm8k
 from sglang.test.test_utils import (
+    DEFAULT_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     popen_launch_server,
@@ -16,7 +17,9 @@ from sglang.test.test_utils import (
 Integration test for python/sglang/srt/layers/attention/flashattention_backend.py
 """
 # Change to your own model if testing model is not public.
-MODEL_USED_FOR_TEST = "lmsys/sglang-ci-dsv3-test"
+MODEL_USED_FOR_TEST = DEFAULT_MODEL_NAME_FOR_TEST
+# Setting data path to None uses default data path in few_shot_gsm8k eval test.
+DATA_PATH = None
 
 
 class TestFlashAttention3(unittest.TestCase):
@@ -51,6 +54,7 @@ class TestFlashAttention3(unittest.TestCase):
             parallel=128,
             host="http://127.0.0.1",
             port=int(self.base_url.split(":")[-1]),
+            data_path=DATA_PATH,
         )
         metrics = run_eval_few_shot_gsm8k(args)
         print(metrics)
@@ -98,6 +102,7 @@ class TestFlashAttention3DisableCudaGraph(unittest.TestCase):
             parallel=128,
             host="http://127.0.0.1",
             port=int(self.base_url.split(":")[-1]),
+            data_path=DATA_PATH,
         )
         metrics = run_eval_few_shot_gsm8k(args)
         print(metrics)
