d63f77f1ddc4253fbc702c6180d722e430434877
Yineng Zhang <me@zhyncs.com>
2025-03-24 21:56:04 -0700

    upd

--- test/srt/test_mla_int8_deepseek_v3.py
+++ test/srt/test_mla_int8_deepseek_v3.py
@@ -16,7 +16,7 @@ from sglang.test.test_utils import (
 class TestMLADeepseekV3ChannelInt8(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.model = "lmsys/sglang-ci-dsv3-channel-int8-test"
+        cls.model = "sgl-project/sglang-ci-dsv3-channel-int8-test"
         cls.base_url = DEFAULT_URL_FOR_TEST
         other_args = ["--trust-remote-code"]
         if torch.cuda.is_available() and torch.version.cuda:
