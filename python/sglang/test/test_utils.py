30f65cada0190426aa3a5cd0abf460e8cef2c4ec
fzyzcjy <ch271828n@outlook.com>
2025-03-25 17:38:13 +0800

    more

--- python/sglang/test/test_utils.py
+++ python/sglang/test/test_utils.py
@@ -1004,7 +1004,8 @@ def run_logprob_check(self: unittest.TestCase, arg: Tuple):
 class CustomTestCase(unittest.TestCase):
     def _callTestMethod(self, method):
         _retry_execution(
-            lambda: super(CustomTestCase, self)._callTestMethod(method), max_retry=_get_max_retry()
+            lambda: super(CustomTestCase, self)._callTestMethod(method),
+            max_retry=_get_max_retry(),
         )
 
 
