dc35be020ebf6eda7cdae8785767be3785a6f0d0
fzyzcjy <ch271828n@outlook.com>
2025-03-28 22:14:50 +0800

    more

--- scripts/warmup_deepgemm_analyzer.py
+++ scripts/warmup_deepgemm_analyzer.py
@@ -4,7 +4,7 @@ from sglang.srt import warmup_deepgemm
 df_raw = pl.DataFrame(warmup_deepgemm.read_output())
 print(df_raw)
 
-df = df_raw.group_by('k', 'n').agg(pl.col('m').unique().sort())
+df = df_raw.group_by('k', 'n').agg(pl.col('m').unique().sort()).sort('k', 'n')
 with pl.Config(fmt_str_lengths=1000, fmt_table_cell_list_len=1000, tbl_cols=-1, tbl_rows=-1):
     print(df)
 
