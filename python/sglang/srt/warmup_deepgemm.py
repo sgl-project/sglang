f6dfcc4830cd8039b61f52de6db4d883bd1b8dab
fzyzcjy <ch271828n@outlook.com>
2025-03-28 20:02:02 +0800

    more

--- python/sglang/srt/warmup_deepgemm.py
+++ python/sglang/srt/warmup_deepgemm.py
@@ -8,8 +8,24 @@ _ENABLE_CAPTURE = get_bool_env_var("SGLANG_ENABLE_WARMUP_DEEPGEMM_CAPTURE")
 def on_execution(lhs, rhs, out):
     if not _ENABLE_CAPTURE:
         return
+    _capturer.on_execution(lhs=lhs, rhs=rhs)
 
-    info = _compute_shape_from_args(lhs=lhs, rhs=rhs)
+
+class _Capturer:
+    def __init__(self):
+        self._seen_infos = set()
+
+    def on_execution(self, lhs, rhs):
+        info = _compute_shape_from_args(lhs=lhs, rhs=rhs)
+
+        if info in self._seen_infos:
+            return
+
+        self._seen_infos.add(info)
+        TODO_print
+
+
+_capturer = _Capturer() if _ENABLE_CAPTURE else None
 
 
 def _compute_shape_from_args(lhs, rhs):
@@ -17,14 +33,3 @@ def _compute_shape_from_args(lhs, rhs):
     n, k_ = rhs[0].shape
     assert k == k_
     return m, k, n
-
-
-class _Capturer:
-    def __init__(self):
-        self._seen_infos = set()
-
-    def on_execution(self, info):
-        if info in self._seen_infos:
-            return
-        self._seen_infos.add(info)
-        TODO_print
