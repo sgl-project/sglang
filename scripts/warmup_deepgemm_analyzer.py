f0be069a6a7e5bd9041fdb078e9ef41d8a9d6d82
fzyzcjy <ch271828n@outlook.com>
2025-03-28 21:06:25 +0800

    more

--- scripts/warmup_deepgemm_analyzer.py
+++ scripts/warmup_deepgemm_analyzer.py
@@ -7,3 +7,9 @@ print(df_raw)
 df = df_raw.group_by('k', 'n').agg(pl.col('m').unique().sort())
 with pl.Config(fmt_str_lengths=1000, fmt_table_cell_list_len=1000, tbl_cols=-1, tbl_rows=-1):
     print(df)
+
+output_text = '\n'.join([
+    f'dict(n={row["n"]}, k={row["k"]}),'
+    for row in df.iter_rows(named=True)
+])
+print(output_text)
