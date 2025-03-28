ff7d846cb97aff7bded78385e31d80cc477940a5
fzyzcjy <ch271828n@outlook.com>
2025-03-28 20:56:55 +0800

    more

---
+++ scripts/warmup_deepgemm_analyzer.py
@@ -0,0 +1,11 @@
+import polars as pl
+from python.sglang.srt import warmup_deepgemm
+
+
+def run():
+    df = pl.DataFrame(warmup_deepgemm.read_output())
+    print(df)
+
+
+if __name__ == '__main__':
+    run()
