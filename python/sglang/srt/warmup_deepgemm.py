52f22dc5edbd92f0c16ec7ae7d83df0204cb45a9
fzyzcjy <ch271828n@outlook.com>
2025-03-28 19:58:37 +0800

    more

--- python/sglang/srt/warmup_deepgemm.py
+++ python/sglang/srt/warmup_deepgemm.py
@@ -1,4 +1,12 @@
 # TODO shall we put this file elsewhere?
 
+from sglang.srt.utils import get_bool_env_var
+
+_ENABLE_CAPTURE = get_bool_env_var("SGLANG_ENABLE_WARMUP_DEEPGEMM_CAPTURE")
+
+
 def on_execution(lhs, rhs, out):
+    if not _ENABLE_CAPTURE:
+        return
+
     TODO
