f6d99314bbf5b76acd91761d729e99b1d1788fde
adarshxs <adarsh.shirawalmath@gmail.com>
2025-03-30 12:29:05 +0530

    fix

--- sgl-kernel/tests/test_cublas_grouped_gemm.py
+++ sgl-kernel/tests/test_cublas_grouped_gemm.py
@@ -13,36 +13,27 @@ skip_condition = not torch.cuda.is_available() or (
 )
 
 
-m_values = [1, 16, 32, 256, 1024]
-n_values = [2, 16, 128, 256, 4096]
-k_values = [3, 16, 32, 512, 8192]
-
-
 @pytest.mark.skipif(
     skip_condition, reason="CUDA not available or CUDA version lower than 12.5"
 )
 @pytest.mark.parametrize("out_dtype", [torch.float16, torch.bfloat16])
-@pytest.mark.parametrize("M", m_values)
-@pytest.mark.parametrize("N", n_values)
-@pytest.mark.parametrize("K", k_values)
+@pytest.mark.parametrize("M", [1, 16, 32, 256, 1024])
+@pytest.mark.parametrize("N", [2, 16, 128, 256, 4096])
+@pytest.mark.parametrize("K", [3, 16, 32, 512, 8192])
 def test_grouped_gemm_accuracy(out_dtype, M, N, K):
-    try:
-        a = torch.randn((M, K), device="cuda", dtype=out_dtype) * 5
-        b = torch.randn((N, K), device="cuda", dtype=out_dtype) * 5
-        expected = torch.matmul(a, b.t()).to(out_dtype)
+    a = torch.randn((M, K), device="cuda", dtype=out_dtype) * 5
+    b = torch.randn((N, K), device="cuda", dtype=out_dtype) * 5
+    expected = torch.matmul(a, b.t()).to(out_dtype)
 
-        a_array = [a]
-        b_array = [b]
-        c_array = [torch.empty((M, N), device="cuda", dtype=out_dtype)]
+    a_array = [a]
+    b_array = [b]
+    c_array = [torch.empty((M, N), device="cuda", dtype=out_dtype)]
 
-        result_torch = torch_grouped_gemm(a_array, b_array, out_dtype)[0]
-        cublas_grouped_gemm(a_array, b_array, c_array, out_dtype)
+    result_torch = torch_grouped_gemm(a_array, b_array, out_dtype)[0]
+    cublas_grouped_gemm(a_array, b_array, c_array, out_dtype)
 
-        torch.testing.assert_close(result_torch, expected)
-        torch.testing.assert_close(c_array[0], expected)
-
-    except torch.cuda.OutOfMemoryError:
-        pytest.skip(f"Skipping M={M}, N={N}, K={K} due to OOM")
+    torch.testing.assert_close(result_torch, expected)
+    torch.testing.assert_close(c_array[0], expected)
 
 
 if __name__ == "__main__":
