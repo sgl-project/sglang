b09d4e2e21cd7fdfba33136fcf1175c35f3b92c2
hebiao064 <hebiaobuaa@gmail.com>
2025-03-31 20:15:10 +0000

    Use sglkernel's fa3

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -28,6 +28,7 @@ suites = {
         TestFile("test_chunked_prefill.py", 336),
         TestFile("test_eagle_infer.py", 447),
         TestFile("test_ebnf_constrained.py"),
+        TestFile("test_fa3.py", 5),
         TestFile("test_fp8_kernel.py", 2),
         TestFile("test_embedding_openai_server.py", 36),
         TestFile("test_expert_distribution.py", 31),
