90f723d2dc2e6eb795b68e3f6ee0b8cce73ca798
fzyzcjy <ch271828n@outlook.com>
2025-04-01 10:11:41 +0800

    more

--- test/srt/test_expert_distribution.py
+++ test/srt/test_expert_distribution.py
@@ -3,6 +3,7 @@ import os
 import unittest
 
 import requests
+import torch
 from sglang.srt.utils import kill_process_tree
 from sglang.test.test_utils import (
     DEFAULT_SMALL_MOE_MODEL_NAME_FOR_TEST,
@@ -84,29 +85,9 @@ class TestExpertDistribution(CustomTestCase):
             if mode_detail:
                 self.assertGreater(len(data), 0, "Should contain data rows")
             else:
-                TODO
-
-            for row in rows:
-                # Verify each row has 3 columns
-                self.assertEqual(
-                    len(row),
-                    3,
-                    "Each row should have layer_id, expert_id and count",
-                )
-
-                # Verify data types
-                layer_id, expert_id, count = row
-                self.assertTrue(
-                    layer_id.isdigit(),
-                    f"layer_id should be an integer {row=} {rows=}",
-                )
-                self.assertTrue(
-                    expert_id.isdigit(),
-                    f"expert_id should be an integer {row=} {rows=}",
-                )
-                self.assertTrue(
-                    count.isdigit(), f"count should be an integer {row=} {rows=}"
-                )
+                logical_count = torch.tensor(data['logical_count'])
+                print(f"{logical_count=}")
+                self.assertTrue(logical_count.sum() > 0)
 
         finally:
             kill_process_tree(process.pid)
