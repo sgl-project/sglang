9f67203c36d5ef033b86b9638f4c21c484a671fe
fzyzcjy <ch271828n@outlook.com>
2025-04-01 10:21:34 +0800

    more

--- python/sglang/srt/managers/expert_location.py
+++ python/sglang/srt/managers/expert_location.py
@@ -23,6 +23,7 @@ class ExpertLocationMetadata:
     def init_new(num_layers: int, num_logical_experts: int):
         # TODO handle more complex cases like duplicating experts on different GPUs
         num_local_physical_experts = num_logical_experts // get_tensor_model_parallel_world_size()
+        num_physical_experts = num_logical_experts
 
         return ExpertLocationMetadata(
             num_layers=num_layers,
@@ -30,7 +31,7 @@ class ExpertLocationMetadata:
             num_local_physical_experts=num_local_physical_experts,
             physical_to_logical_map=_create_vanilla_physical_to_logical_map(
                 num_layers=num_layers,
-                num_physical_experts=num_local_physical_experts * TODO,
+                num_physical_experts=num_physical_experts,
             ),
         )
 
