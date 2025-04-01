acdc853761491ac1506cde037be023f51a27221a
Yubo Wang <yubowang2019@gmail.com>
2025-04-01 01:36:25 +0000

    add mla integration tes

--- test/srt/test_fa3.py
+++ test/srt/test_fa3.py
@@ -7,6 +7,8 @@ import torch
 from sglang.srt.utils import kill_process_tree
 from sglang.test.few_shot_gsm8k import run_eval as run_eval_few_shot_gsm8k
 from sglang.test.test_utils import (
+    DEFAULT_MODEL_NAME_FOR_TEST,
+    DEFAULT_MLA_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     popen_launch_server,
@@ -16,14 +18,18 @@ from sglang.test.test_utils import (
 Integration test for python/sglang/srt/layers/attention/flashattention_backend.py
 """
 # Change to your own model if testing model is not public.
-MODEL_USED_FOR_TEST = "lmsys/sglang-ci-dsv3-test"
+MODEL_USED_FOR_TEST = DEFAULT_MODEL_NAME_FOR_TEST
+MODEL_USED_FOR_TEST_MLA = DEFAULT_MLA_MODEL_NAME_FOR_TEST
+# Setting data path to None uses default data path in few_shot_gsm8k eval test.
+DATA_PATH = None
+# DATA_PATH = None
 
 
 class TestFlashAttention3(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.model = MODEL_USED_FOR_TEST
+        cls.model = MODEL_USED_FOR_TEST_MLA
         cls.base_url = DEFAULT_URL_FOR_TEST
         other_args = ["--trust-remote-code"]
         if torch.cuda.is_available() and torch.version.cuda:
@@ -51,11 +57,12 @@ class TestFlashAttention3(unittest.TestCase):
             parallel=128,
             host="http://127.0.0.1",
             port=int(self.base_url.split(":")[-1]),
+            data_path=DATA_PATH,
         )
         metrics = run_eval_few_shot_gsm8k(args)
         print(metrics)
 
-        self.assertGreater(metrics["accuracy"], 0.62)
+        self.assertGreater(metrics["score"], 0.5)
 
     @classmethod
     def tearDownClass(cls):
@@ -73,8 +80,6 @@ class TestFlashAttention3DisableCudaGraph(unittest.TestCase):
                 [
                     "--enable-torch-compile",
                     "--disable-cuda-graph",
-                    "--cuda-graph-max-bs",
-                    "4",
                     "--attention-backend",
                     "fa3",
                 ]
@@ -98,6 +103,51 @@ class TestFlashAttention3DisableCudaGraph(unittest.TestCase):
             parallel=128,
             host="http://127.0.0.1",
             port=int(self.base_url.split(":")[-1]),
+            data_path=DATA_PATH,
+        )
+        metrics = run_eval_few_shot_gsm8k(args)
+        print(metrics)
+
+        self.assertGreater(metrics["accuracy"], 0.62)
+
+
+class TestFlashAttention3DisableMLA(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = MODEL_USED_FOR_TEST
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        other_args = ["--trust-remote-code"]
+        if torch.cuda.is_available() and torch.version.cuda:
+            other_args.extend(
+                [
+                    "--enable-torch-compile",
+                    "--cuda-graph-max-bs",
+                    "2",
+                    "--disable-mla",
+                    "--attention-backend",
+                    "fa3",
+                ]
+            )
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            num_shots=5,
+            num_questions=200,
+            max_new_tokens=512,
+            parallel=128,
+            host="http://127.0.0.1",
+            port=int(self.base_url.split(":")[-1]),
+            data_path=DATA_PATH,
         )
         metrics = run_eval_few_shot_gsm8k(args)
         print(metrics)
