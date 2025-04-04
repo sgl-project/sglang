fceb2a9ec9b6edf21121c223eb06798efc8c943a
Brayden Zhong <b8zhong@uwaterloo.ca>
2025-04-04 14:34:39 -0400

    feat: initial Arctic imp
    Co-authored-by: vincent-4 <vincentzhongy+githubvincent4@gmail.com>

--- python/sglang/srt/configs/__init__.py
+++ python/sglang/srt/configs/__init__.py
@@ -1,3 +1,4 @@
+from sglang.srt.configs.arctic import ArcticConfig
 from sglang.srt.configs.chatglm import ChatGLMConfig
 from sglang.srt.configs.dbrx import DbrxConfig
 from sglang.srt.configs.deepseekvl2 import DeepseekVL2Config
@@ -5,6 +6,7 @@ from sglang.srt.configs.exaone import ExaoneConfig
 from sglang.srt.configs.janus_pro import MultiModalityConfig
 
 __all__ = [
+    "ArcticConfig",
     "ExaoneConfig",
     "ChatGLMConfig",
     "DbrxConfig",
