f4dd95d1746fb52d70701bbcab3e94ec599a5b37
Kyle Huang <kylhuang@nvidia.com>
2025-04-04 01:53:10 +0000

    add test

---
+++ python/sglang/launch_server_test.py
@@ -0,0 +1,21 @@
+"""Launch the inference server."""
+
+import os
+import sys
+
+from sglang.srt.entrypoints.http_server import launch_server
+from sglang.srt.server_args import prepare_server_args, ServerArgs
+from sglang.srt.utils import kill_process_tree
+
+if __name__ == "__main__":
+    #server_args = prepare_server_args(sys.argv[1:])
+    server_args =  ServerArgs(model_path="nvidia/Llama-3_3-Nemotron-Super-49B-v1",
+                    tp_size=2,
+                    base_gpu_id=4,
+                    disable_cuda_graph=True,
+                    trust_remote_code=True)      
+
+    try:
+        launch_server(server_args)
+    finally:
+        kill_process_tree(os.getpid(), include_parent=False)
