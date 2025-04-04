1aba01aac05bd2d42e7c060fd4fb9e200c7f7e80
fzyzcjy <ch271828n@outlook.com>
2025-04-04 10:39:11 +0800

    more

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -1006,7 +1006,8 @@ class ModelRunner:
     def _forward_raw(
         self, forward_batch: ForwardBatch, skip_attn_backend_init: bool
     ) -> LogitsProcessorOutput:
-        print(f'hi forward_raw {forward_batch.forward_mode=} {forward_batch.tbo_split_seq_index=}')
+        print(
+            f'hi forward_raw {forward_batch.forward_mode=} {forward_batch.batch_size=} {forward_batch.tbo_split_seq_index=}')
         if (
             forward_batch.forward_mode.is_cuda_graph()
             and self.cuda_graph_runner
