b7b4b457b3cbf63e9bfb5f12daec0ac76e344204
fecet <xiezej@gmail.com>
2025-04-04 23:05:46 +0800

    fix cmakefile and build script

--- sgl-kernel/build.sh
+++ sgl-kernel/build.sh
@@ -4,24 +4,31 @@ set -ex
 PYTHON_VERSION=$1
 CUDA_VERSION=$2
 PYTHON_ROOT_PATH=/opt/python/cp${PYTHON_VERSION//.}-cp${PYTHON_VERSION//.}
-ENABLE_SM90A=$(( ${CUDA_VERSION%.*} >= 12 ? ON : OFF ))
-
-if [ ${CUDA_VERSION} = "12.8" ]; then
-   DOCKER_IMAGE="pytorch/manylinux2_28-builder:cuda${CUDA_VERSION}"
-   TORCH_INSTALL="pip install --no-cache-dir --pre torch --index-url https://download.pytorch.org/whl/nightly/cu${CUDA_VERSION//.}"
+DOCKER_IMAGE="pytorch/manylinux2_28-builder:cuda${CUDA_VERSION}"
+if (( ${CUDA_VERSION%.*} >= 12 )); then
+    ENABLE_SM90A="ON"
 else
-   DOCKER_IMAGE="pytorch/manylinux-builder:cuda${CUDA_VERSION}"
+    ENABLE_SM90A="OFF"
+fi
+
+if [ "${CUDA_VERSION}" = "12.8" ]; then
+   TORCH_INSTALL="pip install --no-cache-dir --pre torch --index-url https://download.pytorch.org/whl/nightly/cu${CUDA_VERSION//.}"
+   ENABLE_SM100A=ON
+else
    TORCH_INSTALL="pip install --no-cache-dir torch==2.5.1 --index-url https://download.pytorch.org/whl/cu${CUDA_VERSION//.}"
+   ENABLE_SM100A=OFF
 fi
 
 docker run --rm \
-   -v $(pwd):/sgl-kernel \
-   ${DOCKER_IMAGE} \
+   -v "$(pwd)":/sgl-kernel \
+   "${DOCKER_IMAGE}" \
    bash -c "
+   /sgl-kernel/replace_ptxas.sh ${CUDA_VERSION} && \
    ${PYTHON_ROOT_PATH}/bin/${TORCH_INSTALL} && \
    ${PYTHON_ROOT_PATH}/bin/pip install --no-cache-dir ninja setuptools==75.0.0 wheel==0.41.0 numpy uv && \
    export TORCH_CUDA_ARCH_LIST='7.5 8.0 8.9 9.0+PTX' && \
    export CUDA_VERSION=${CUDA_VERSION} && \
+   export CMAKE_ARGS='-DSGL_KERNEL_ENABLE_SM90A=${ENABLE_SM90A} -DBUILD_FA3=${ENABLE_SM90A} -DSGL_KERNEL_ENABLE_SM100A=$ENABLE_SM100A' && \
    mkdir -p /usr/lib/x86_64-linux-gnu/ && \
    ln -s /usr/local/cuda-${CUDA_VERSION}/targets/x86_64-linux/lib/stubs/libcuda.so /usr/lib/x86_64-linux-gnu/libcuda.so && \
    cd /sgl-kernel && \
---
+++ sgl-kernel/replace_ptxas.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -ex
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <CUDA_VERSION>"
+    exit 1
+fi
+
+CUDA_VERSION=$1
+
+if awk "BEGIN {exit !($CUDA_VERSION >= 12.6 && $CUDA_VERSION < 12.8)}"; then
+    NVCC_ARCHIVE_VERSION="12.8.93"
+    NVCC_ARCHIVE_NAME="cuda_nvcc-linux-x86_64-${NVCC_ARCHIVE_VERSION}-archive"
+    NVCC_ARCHIVE_TAR="${NVCC_ARCHIVE_NAME}.tar.xz"
+    NVCC_ARCHIVE_URL="https://developer.download.nvidia.com/compute/cuda/redist/cuda_nvcc/linux-x86_64/${NVCC_ARCHIVE_TAR}"
+    wget "$NVCC_ARCHIVE_URL"
+    tar -xf "$NVCC_ARCHIVE_TAR"
+    mkdir -p /usr/local/cuda/bin
+    cp "${NVCC_ARCHIVE_NAME}/bin/ptxas" /usr/local/cuda/bin/
+fi
