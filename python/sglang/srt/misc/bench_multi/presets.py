d3ee28bef9096eb2de9f5f9fb68a95dc119caae0
fzyzcjy <ch271828n@outlook.com>
2025-04-05 09:30:03 +0800

    more

--- python/sglang/srt/misc/bench_multi/presets.py
+++ python/sglang/srt/misc/bench_multi/presets.py
@@ -4,4 +4,9 @@ from sglang.srt.misc.bench_multi.configs import Config
 
 
 def get_configs(preset_name: str) -> List[Config]:
-    return TODO
+    if preset_name == "scan_8xH200":
+        return TODO
+    if preset_name == "scan_2x8xH100":
+        return TODO
+    if preset_name == "scan_4x8xH100":
+        return TODO
