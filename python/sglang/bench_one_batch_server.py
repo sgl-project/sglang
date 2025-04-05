3e01700e56c26c5f2d3b81723769d2d9fd3e946c
fzyzcjy <ch271828n@outlook.com>
2025-04-05 09:08:20 +0800

    more

--- python/sglang/bench_one_batch_server.py
+++ python/sglang/bench_one_batch_server.py
@@ -19,7 +19,7 @@ from typing import Tuple
 
 import numpy as np
 import requests
-from sglang.srt.entrypoints.http_server import launch_server
+from sglang.srt.entrypoints.http_server import launch_server_process
 from sglang.srt.server_args import ServerArgs
 from sglang.srt.utils import kill_process_tree
 
