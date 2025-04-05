dbeeaa7d9a11c4046263dc106f283c832c620f9c
fzyzcjy <ch271828n@outlook.com>
2025-04-05 09:04:51 +0800

    more

---
+++ python/sglang/srt/misc/bench_multi/configs.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    pass
--- python/sglang/srt/misc/bench_multi/runner.py
+++ python/sglang/srt/misc/bench_multi/runner.py
@@ -1,2 +1,11 @@
+from typing import List
+
+from sglang.srt.misc.bench_multi.configs import Config
+
+
 def run_bench_multi():
     TODO
+
+
+def _get_configs() -> List[Config]:
+    return TODO
