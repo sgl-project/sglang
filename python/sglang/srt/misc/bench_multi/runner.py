8250d64c851bd597c3f6c50afed710a68172c84c
fzyzcjy <ch271828n@outlook.com>
2025-04-05 09:12:49 +0800

    more

--- python/sglang/srt/misc/bench_multi/runner.py
+++ python/sglang/srt/misc/bench_multi/runner.py
@@ -2,6 +2,8 @@ from contextlib import contextmanager
 from typing import List
 
 from sglang.srt.misc.bench_multi.configs import Config
+from sglang.srt.server_args import ServerArgs
+from sglang.test.test_utils import launch_server_process
 
 
 def run_bench_multi():
@@ -21,8 +23,8 @@ def _run_one_config(config: Config):
 
 
 @contextmanager
-def _with_server():
-    _start_server()
+def _with_server(server_args: ServerArgs):
+    proc, base_url = launch_server_process(server_args)
     try:
         yield
     finally:
