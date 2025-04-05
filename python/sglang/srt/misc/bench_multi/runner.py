1202023167d68f42157f9a9248de557ae012f2e8
fzyzcjy <ch271828n@outlook.com>
2025-04-05 09:14:06 +0800

    more

--- python/sglang/srt/misc/bench_multi/runner.py
+++ python/sglang/srt/misc/bench_multi/runner.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 from typing import List
 
+from sglang import bench_serving
 from sglang.srt.misc.bench_multi.configs import Config
 from sglang.srt.server_args import ServerArgs
 from sglang.srt.utils import kill_process_tree
@@ -18,9 +19,10 @@ def _get_configs() -> List[Config]:
 
 
 def _run_one_config(config: Config):
-    with _with_server():
+    with _with_server(server_args):
         for _ in range(TODO):
-            _run_bench_serving()
+            bench_serving_output = bench_serving.run_benchmark(TODO)
+            _write_output()
 
 
 @contextmanager
@@ -32,5 +34,5 @@ def _with_server(server_args: ServerArgs):
         kill_process_tree(proc.pid)
 
 
-def _run_bench_serving():
+def _write_output():
     TODO
