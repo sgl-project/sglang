600d1f95741f76552feba4e95c75307268cf5234
fzyzcjy <ch271828n@outlook.com>
2025-04-05 09:05:13 +0800

    more

--- python/sglang/srt/misc/bench_multi/runner.py
+++ python/sglang/srt/misc/bench_multi/runner.py
@@ -4,8 +4,14 @@ from sglang.srt.misc.bench_multi.configs import Config
 
 
 def run_bench_multi():
-    TODO
+    configs = _get_configs()
+    for config in configs:
+        _run_one_config(config)
 
 
 def _get_configs() -> List[Config]:
     return TODO
+
+
+def _run_one_config(config: Config):
+    TODO
