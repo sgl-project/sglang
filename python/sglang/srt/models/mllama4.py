8de5eb86a3ddfae44e1ccbab199e8462ee13a181
fzyzcjy <ch271828n@outlook.com>
2025-04-06 16:47:28 +0800

    more

--- python/sglang/srt/models/mllama4.py
+++ python/sglang/srt/models/mllama4.py
@@ -112,11 +112,11 @@ class Llama4ForConditionalGeneration(nn.Module):
         weights = compute_shared_experts_fusion_weights(
             weights,
             n_share_experts_fusion=self.n_share_experts_fusion,
-            n_routed_experts=self.config.num_local_experts,
+            n_routed_experts=self.config.text_config.num_local_experts,
             moe_layer_ids=range(
                 1,
-                self.config.num_hidden_layers,
-                self.config.interleave_moe_layer_step,
+                self.config.text_config.num_hidden_layers,
+                self.config.text_config.interleave_moe_layer_step,
             ),
             suffix_list=[
                 "down_proj",
