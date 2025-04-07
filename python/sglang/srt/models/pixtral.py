5f9ddf63fd99db10ecb63f3b0ffefc3c395ff0d6
KivenChen <sdckivenchen@gmail.com>
2025-04-08 01:51:09 +0800

    fix import issue

--- python/sglang/srt/models/pixtral.py
+++ python/sglang/srt/models/pixtral.py
@@ -36,14 +36,14 @@ from transformers.models.pixtral.modeling_pixtral import (
 )
 from transformers.models.pixtral.modeling_pixtral import position_ids_in_meshgrid
 
-from python.sglang.srt.managers.schedule_batch import Modality, MultimodalInputs
-from python.sglang.srt.utils import flatten_nested_list
 from sglang.srt.layers.activation import SiluAndMul
 from sglang.srt.layers.attention.vision import VisionAttention
 from sglang.srt.layers.layernorm import RMSNorm
 from sglang.srt.layers.linear import MergedColumnParallelLinear, RowParallelLinear
 from sglang.srt.layers.quantization.base_config import QuantizationConfig
+from sglang.srt.managers.schedule_batch import Modality, MultimodalInputs
 from sglang.srt.model_loader.weight_utils import default_weight_loader
+from sglang.srt.utils import flatten_nested_list
 
 
 class PixtralHFInputPatcher:
