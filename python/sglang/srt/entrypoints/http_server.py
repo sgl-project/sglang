1055a02b0aa669f3fd71a35446e1de1c453ee78f
fzyzcjy <ch271828n@outlook.com>
2025-04-08 18:12:33 +0800

    more

--- python/sglang/srt/entrypoints/http_server.py
+++ python/sglang/srt/entrypoints/http_server.py
@@ -771,14 +771,7 @@ def _wait_and_warmup(
         if server_args.dp_size == 1:
             json_data["input_ids"] = json_data["input_ids"][0]
     else:
-        # TODO
-        # TODO hack
-        # TODO
-        json_data["text"] = [
-            "1+1=",
-            "The capital city of France is",
-        ][: server_args.dp_size]
-        # json_data["text"] = ["The capital city of France is"] * server_args.dp_size
+        json_data["text"] = ["The capital city of France is"] * server_args.dp_size
         # TODO Workaround the bug that embedding errors for list of size 1
         if server_args.dp_size == 1:
             json_data["text"] = json_data["text"][0]
