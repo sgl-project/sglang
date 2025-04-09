7e8eca7872d46c93de104559c66dbc3c86dd2e4e
ch-wan <cwan39@gatech.edu>
2025-04-09 04:52:55 +0000

    fix ci

--- python/sglang/srt/layers/moe/ep_moe/token_dispatcher.py
+++ python/sglang/srt/layers/moe/ep_moe/token_dispatcher.py
@@ -28,7 +28,7 @@ class DeepEPDispatchMode(IntEnum):
 
 class DeepEPBuffer:
 
-    _buffer: Optional[Buffer] = None
+    _buffer = None
     _dispatch_mode: Optional[DeepEPDispatchMode] = None
     _hidden_size: Optional[int] = None
     _num_max_dispatch_tokens_per_rank: Optional[int] = None
