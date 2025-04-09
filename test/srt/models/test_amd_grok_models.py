f6fad0f45b3869fb48e63e7c271c0471e076a0fc
saienduri <saimanas.enduri@amd.com>
2025-04-08 17:47:22 -0700

    try python wrapper

---
+++ test/srt/models/test_amd_grok_models.py
@@ -0,0 +1,32 @@
+import unittest
+
+from sglang.test.test_utils import CustomTestCase, is_in_ci, run_bench_one_batch
+
+
+class TestDummyGrok1(CustomTestCase):
+    def test_dummy_grok_1(self):
+        output_throughput = run_bench_one_batch(
+            "/dummy-grok",
+            [
+                "--batch-size",
+                "32",
+                "--input",
+                "1024",
+                "--output",
+                "8",
+                "--tokenizer-path",
+                "Xenova/grok-1-tokenizer",
+                "--tp",
+                "8",
+                "--quantization",
+                "fp8",
+                "--load-format dummy",
+            ],
+        )
+
+        if is_in_ci():
+            assert output_throughput > 0, f"{output_throughput=}"
+
+
+if __name__ == "__main__":
+    unittest.main()
