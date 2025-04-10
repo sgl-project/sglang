1114ab3be33c7b08dfab7d7646f7a4003fe4133c
fzyzcjy <ch271828n@outlook.com>
2025-04-10 21:05:38 +0800

    more

--- python/sglang/srt/managers/expert_distribution_storage.py
+++ python/sglang/srt/managers/expert_distribution_storage.py
@@ -1,12 +1,19 @@
 from pathlib import Path
 
+from sglang.srt.managers.tokenizer_manager import TokenizerManager
+
 
 class ExpertDistributionStorage:
-    def __init__(self, dir_data):
+    def __init__(self, dir_data, tokenizer_manager: TokenizerManager):
         self._dir_data = Path(dir_data)
+        self._tokenizer_manager = tokenizer_manager
 
-    def save_current(self):
-        TODO
+    async def initialize(self):
+        await self._tokenizer_manager.start_expert_distribution_record()
 
-    def get_last_snapshot(self):
-        return TODO
+    async def save_current(self):
+        data = await self._tokenizer_manager.dump_expert_distribution_record()
+        TODO_write_data
+
+    async def get_last_snapshot(self):
+        return TODO_read_data
