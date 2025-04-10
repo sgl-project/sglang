2e0ca4fbff55246806394b6e6a860edfedcf77a9
fzyzcjy <ch271828n@outlook.com>
2025-04-10 15:09:03 +0800

    more

--- python/sglang/srt/managers/expert_distribution.py
+++ python/sglang/srt/managers/expert_distribution.py
@@ -5,7 +5,6 @@ from copy import deepcopy
 from typing import Any, List, Optional, Type
 
 import torch
-
 from sglang.srt.managers.expert_location import ExpertLocationMetadata
 from sglang.srt.server_args import ServerArgs
 from sglang.srt.utils import Withable, get_bool_env_var
@@ -188,11 +187,12 @@ class _SelectExpertsSinglePassGatherer(_LayerBasedSinglePassGatherer):
         torch.cuda.synchronize()
 
         num_recv_tokens_per_expert_list = [
-            0
-        ] * self._expert_location_metadata.num_local_physical_experts
+                                              0
+                                          ] * self._expert_location_metadata.num_local_physical_experts
         for token_record in topk_ids_list:
             for global_physical_expert_idx in token_record:
-                local_physical_expert_idx = self._expert_location_metadata.global_physical_to_local_physical(global_physical_expert_idx)
+                local_physical_expert_idx = self._expert_location_metadata.global_physical_to_local_physical(
+                    global_physical_expert_idx)
                 num_recv_tokens_per_expert_list[local_physical_expert_idx] += 1
 
         self._on_layer_data(layer_idx, num_recv_tokens_per_expert_list)
@@ -329,10 +329,9 @@ class _StatAccumulator(_Accumulator):
                 for local_physical_expert_index in range(
                     expert_location_metadata.num_local_physical_experts
                 ):
-                    global_physical_expert_index = (
-                        expert_location_metadata.num_local_physical_experts
-                        * physical_dump["rank"]
-                        + local_physical_expert_index
+                    global_physical_expert_index = expert_location_metadata.local_physical_to_global_physical(
+                        rank=physical_dump["rank"],
+                        local_physical_expert_index=local_physical_expert_index
                     )
                     logical_expert_index = (
                         expert_location_metadata.physical_to_logical_map[
