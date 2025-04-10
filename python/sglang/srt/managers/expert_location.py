3dfd35923bba6f77abcf97836d967edeb00a14bb
fzyzcjy <ch271828n@outlook.com>
2025-04-10 16:09:47 +0800

    more

--- python/sglang/srt/managers/expert_location.py
+++ python/sglang/srt/managers/expert_location.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 import torch
-
 from sglang.srt.configs.model_config import ModelConfig
 from sglang.srt.distributed import get_tensor_model_parallel_world_size
 from sglang.srt.model_loader import get_model_architecture
@@ -9,7 +8,6 @@ from sglang.srt.model_loader import get_model_architecture
 
 @dataclass
 class ExpertLocationMetadata:
-    is_dummy: bool
     num_layers: int
     num_local_physical_experts: int
     num_logical_experts: int
@@ -24,15 +22,14 @@ class ExpertLocationMetadata:
         return ExpertLocationMetadata.init_dummy()
 
     @staticmethod
-    def init_new(num_layers: int, num_logical_experts: int, is_dummy: bool = False):
+    def init_new(num_layers: int, num_logical_experts: int):
         # TODO handle more complex cases like duplicating experts on different GPUs
         num_local_physical_experts = (
-            num_logical_experts // get_tensor_model_parallel_world_size()
+                num_logical_experts // get_tensor_model_parallel_world_size()
         )
         num_physical_experts = num_logical_experts
 
         return ExpertLocationMetadata(
-            is_dummy=is_dummy,
             num_layers=num_layers,
             num_logical_experts=num_logical_experts,
             num_local_physical_experts=num_local_physical_experts,
@@ -44,12 +41,10 @@ class ExpertLocationMetadata:
 
     @staticmethod
     def init_dummy():
-        return ExpertLocationMetadata.init_new(
-            num_layers=1, num_logical_experts=1, is_dummy=True
-        )
+        return ExpertLocationMetadata.init_new(num_layers=1, num_logical_experts=1)
 
     def local_physical_to_global_physical(
-        self, rank: int, local_physical_expert_index: int
+            self, rank: int, local_physical_expert_index: int
     ):
         return self.num_local_physical_experts * rank + local_physical_expert_index
 
@@ -57,18 +52,6 @@ class ExpertLocationMetadata:
         return global_physical_expert_index % self.num_local_physical_experts
 
     def update(self, other: "ExpertLocationMetadata"):
-        if self.is_dummy:
-            self._update_unconditionally(other)
-        else:
-            self._update_partial(other)
-
-    def _update_unconditionally(self, other: "ExpertLocationMetadata"):
-        for field in _UPDATE_FIELDS_TRIVIAL:
-            setattr(self, field, getattr(other, field))
-        for field in _UPDATE_FIELDS_TENSOR:
-            setattr(self, field, getattr(other, field).detach().clone())
-
-    def _update_partial(self, other: "ExpertLocationMetadata"):
         for field in _UPDATE_FIELDS_TRIVIAL:
             assert getattr(self, field) == getattr(other, field)
         for field in _UPDATE_FIELDS_TENSOR:
