b74588d7b315c38dace03b64e86a00c818ee0a5c
fzyzcjy <ch271828n@outlook.com>
2025-04-10 22:34:48 +0800

    more

--- test/srt/test_eplb.py
+++ test/srt/test_eplb.py
@@ -30,11 +30,11 @@ class TestEPLB(CustomTestCase):
             print(f"Action: start engine")
             engine = sgl.Engine(**engine_kwargs)
             ref_output = self._engine_generate(engine)
-            self._assert_behavior(engine, ref_output)
+            self._assert_behavior(engine, ref_output, "equal_trivial")
 
             print(f"Action: eplb_rebalance")
-            engine.eplb_rebalance()
-            self._assert_behavior(engine, ref_output)
+            physical_to_logical_map_layer_0_after_first_rebalance = engine.eplb_rebalance()
+            self._assert_behavior(engine, ref_output, "not_equal_trivial")
 
             print(f"Action: shutdown engine")
             engine.shutdown()
@@ -42,13 +42,13 @@ class TestEPLB(CustomTestCase):
 
             print(f"Action: start engine")
             engine = sgl.Engine(**engine_kwargs)
-            self._assert_behavior(engine, ref_output)
+            self._assert_behavior(engine, ref_output, physical_to_logical_map_layer_0_after_first_rebalance)
 
             print(f"Action: shutdown engine")
             engine.shutdown()
             del engine
 
-    def _assert_behavior(self, engine: sgl.Engine, ref_output: List[str]):
+    def _assert_behavior(self, engine: sgl.Engine, ref_output: List[str], expect_physical_to_local_map):
         ret = engine.flush_cache()
         assert ret.success
 
@@ -58,6 +58,15 @@ class TestEPLB(CustomTestCase):
         physical_to_logical_map = engine.tokenizer_manager.expert_location_metadata.physical_to_logical_map
         physical_to_logical_map_layer_0 = physical_to_logical_map[0, :].tolist()
         print(f"{physical_to_logical_map_layer_0=}")
+
+        if expect_physical_to_local_map == "equal_trivial":
+            self.assertEqual(physical_to_logical_map_layer_0, list(range(TODO)))
+        elif expect_physical_to_local_map == "not_equal_trivial":
+            self.assertNotEqual(physical_to_logical_map_layer_0, list(range(TODO)))
+        else:
+            self.assertEqual(physical_to_logical_map_layer_0, expect_physical_to_local_map)
+
+        return physical_to_logical_map_layer_0
 
     def _engine_generate(self, engine: sgl.Engine):
         output = engine.generate(prompt=["1+1=2, 2+2=4", "One plus one is two, two plus two is four"],
