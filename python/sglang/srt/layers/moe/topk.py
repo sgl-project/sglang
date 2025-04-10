8e8dd6c67e41df89d1cfa5716d9fdf8a5ce5e601
fzyzcjy <ch271828n@outlook.com>
2025-04-10 16:07:49 +0800

    fmt

--- python/sglang/srt/layers/moe/topk.py
+++ python/sglang/srt/layers/moe/topk.py
@@ -16,8 +16,12 @@ from typing import Callable, Optional
 
 import torch
 import torch.nn.functional as F
+
 from sglang.srt.managers.expert_distribution import expert_distribution_recorder
-from sglang.srt.managers.schedule_batch import global_server_args_dict, global_expert_location_metadata
+from sglang.srt.managers.schedule_batch import (
+    global_expert_location_metadata,
+    global_server_args_dict,
+)
 from sglang.srt.utils import get_compiler_backend, is_cuda, is_hip
 
 _is_cuda = is_cuda()
