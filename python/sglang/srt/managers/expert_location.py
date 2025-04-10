5767e79c951a0f70e56811c6d7cd474f646cb11a
fzyzcjy <ch271828n@outlook.com>
2025-04-10 16:50:06 +0800

    cherry pick

--- python/sglang/srt/managers/expert_location.py
+++ python/sglang/srt/managers/expert_location.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 import torch
 
@@ -9,94 +10,82 @@ from sglang.srt.model_loader import get_model_architecture
 
 @dataclass
 class ExpertLocationMetadata:
-    is_dummy: bool
     num_layers: int
     num_local_physical_experts: int
     num_logical_experts: int
-    # will have a `logical_to_physical_map` later
     physical_to_logical_map: torch.Tensor  # (layers, num_physical_experts)
-    chosen_logical_to_physical_map: torch.Tensor  # (layers, num_logical_experts)
+    logical_to_all_physical_map: torch.Tensor  # (layers, num_logical_experts, X)
+    logical_to_rank_chosen_physical_map: (
+        torch.Tensor
+    )  # (num_gpus, layers, num_logical_experts)
+
+    # -------------------------------- construction and mutation ------------------------------------
 
     @staticmethod
     def from_model_config(model_config: ModelConfig):
         model_class, _ = get_model_architecture(model_config)
         if hasattr(model_class, "get_expert_location_metadata"):
             return model_class.get_expert_location_metadata(model_config.hf_config)
-        return ExpertLocationMetadata.init_dummy()
+        return ExpertLocationMetadata._init_dummy()
 
     @staticmethod
-    def init_new(num_layers: int, num_logical_experts: int, is_dummy: bool = False):
+    def init_new(num_layers: int, num_logical_experts: int):
         # TODO handle more complex cases like duplicating experts on different GPUs
         num_local_physical_experts = (
             num_logical_experts // get_tensor_model_parallel_world_size()
         )
         num_physical_experts = num_logical_experts
 
-        output = ExpertLocationMetadata(
-            is_dummy=is_dummy,
+        return ExpertLocationMetadata(
             num_layers=num_layers,
             num_logical_experts=num_logical_experts,
             num_local_physical_experts=num_local_physical_experts,
-            physical_to_logical_map=_create_vanilla_physical_to_logical_map(
-                num_layers=num_layers,
-                num_physical_experts=num_physical_experts,
+            physical_to_logical_map=torch.arange(0, num_physical_experts).repeat(
+                num_layers, 1
             ),
-            chosen_logical_to_physical_map=TODO,
+            logical_to_all_physical_map=torch.arange(0, num_physical_experts).repeat(
+                num_layers, 1
+            )[..., None],
+            logical_to_rank_chosen_physical_map=torch.arange(
+                0, num_physical_experts
+            ).repeat(num_layers, 1)[..., None],
         )
-        output._rebuild()
-        return output
 
     @staticmethod
-    def init_dummy():
-        return ExpertLocationMetadata.init_new(
-            num_layers=1, num_logical_experts=1, is_dummy=True
-        )
-
-    def local_physical_to_global_physical(
-        self, rank: int, local_physical_expert_index: int
-    ):
-        return self.num_local_physical_experts * rank + local_physical_expert_index
-
-    def global_physical_to_local_physical(self, global_physical_expert_index: int):
-        return global_physical_expert_index % self.num_local_physical_experts
-
-    def logical_to_global_physical(self, logical_expert_id: int):
-        return [logical_expert_id]  # TODO add a logical_to_physical_map
-
-    def _rebuild(self):
-        self.chosen_logical_to_physical_map[...] = TODO
+    def _init_dummy():
+        return ExpertLocationMetadata.init_new(num_layers=1, num_logical_experts=1)
 
     def update(self, other: "ExpertLocationMetadata"):
-        if self.is_dummy:
-            self._update_unconditionally(other)
-        else:
-            self._update_partial(other)
-        self._rebuild()
-
-    def _update_unconditionally(self, other: "ExpertLocationMetadata"):
-        for field in _UPDATE_FIELDS_TRIVIAL:
-            setattr(self, field, getattr(other, field))
-        for field in _UPDATE_FIELDS_TENSOR:
-            setattr(self, field, getattr(other, field).detach().clone())
-
-    def _update_partial(self, other: "ExpertLocationMetadata"):
-        for field in _UPDATE_FIELDS_TRIVIAL:
+        for field in [
+            "num_layers",
+            "num_local_physical_experts",
+            "num_logical_experts",
+        ]:
             assert getattr(self, field) == getattr(other, field)
-        for field in _UPDATE_FIELDS_TENSOR:
+
+        for field in [
+            "physical_to_logical_map",
+            "logical_to_all_physical_map",
+            "logical_to_rank_chosen_physical_map",
+        ]:
             # Cannot update address to avoid breaking CUDA graph
             getattr(self, field)[...] = getattr(other, field)
 
+    # -------------------------------- usage ------------------------------------
 
-_UPDATE_FIELDS_TRIVIAL = [
-    "is_dummy",
-    "num_layers",
-    "num_local_physical_experts",
-    "num_logical_experts",
-]
-_UPDATE_FIELDS_TENSOR = [
-    "physical_to_logical_map",
-]
+    def local_physical_to_physical(self, rank: int, local_physical_expert_index: int):
+        return self.num_local_physical_experts * rank + local_physical_expert_index
 
+    def physical_to_local_physical(self, global_physical_expert_index: int):
+        return global_physical_expert_index % self.num_local_physical_experts
 
-def _create_vanilla_physical_to_logical_map(num_layers: int, num_physical_experts: int):
-    return torch.arange(0, num_physical_experts).repeat(num_layers, 1)
+    def logical_to_all_physical(
+        self, layer_id: int, logical_expert_id: int
+    ) -> List[int]:
+        return [
+            physical_expert_id
+            for physical_expert_id in self.logical_to_all_physical_map[
+                layer_id, logical_expert_id
+            ].tolist()
+            if physical_expert_id != -1
+        ]
