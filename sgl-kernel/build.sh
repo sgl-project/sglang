36dc67a6ed9505e502a385d5354a1b66062f6c9d
Elfie Guo <elfieg@nvidia.com>
2025-04-10 19:50:41 +0000

    build..script...elfie...

--- sgl-kernel/build.sh
+++ sgl-kernel/build.sh
@@ -19,7 +19,7 @@ docker run --rm \
    ${DOCKER_IMAGE} \
    bash -c "
    ${PYTHON_ROOT_PATH}/bin/${TORCH_INSTALL} && \
-   ${PYTHON_ROOT_PATH}/bin/pip install --no-cache-dir ninja setuptools==75.0.0 wheel==0.41.0 numpy uv && \
+   ${PYTHON_ROOT_PATH}/bin/pip install --no-cache-dir ninja setuptools==75.0.0 wheel==0.41.0 numpy uv scikit-build-core && \
    export TORCH_CUDA_ARCH_LIST='7.5 8.0 8.9 9.0+PTX' && \
    export CUDA_VERSION=${CUDA_VERSION} && \
    mkdir -p /usr/lib/x86_64-linux-gnu/ && \
