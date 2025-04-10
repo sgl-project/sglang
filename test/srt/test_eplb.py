f6c5c3858284e4749eed214f1f859a57acc124c0
fzyzcjy <ch271828n@outlook.com>
2025-04-10 22:07:23 +0800

    more

--- test/srt/test_eplb.py
+++ test/srt/test_eplb.py
@@ -2,17 +2,19 @@ import unittest
 
 from sglang.srt.utils import kill_process_tree
 from sglang.test.test_utils import (
-    DEFAULT_SMALL_MOE_MODEL_NAME_FOR_TEST,
+    DEFAULT_MLA_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
     popen_launch_server,
 )
+import sglang as sgl
 
 
 class TestEPLB(CustomTestCase):
     def test_eplb_e2e(self):
-        TODO
+        engine = sgl.Engine(model_path=DEFAULT_MLA_MODEL_NAME_FOR_TEST)
+        engine.shutdown()
 
 
 if __name__ == "__main__":
