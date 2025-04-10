db52e916984a921d0054f115fe53db7f8b0969ad
fzyzcjy <ch271828n@outlook.com>
2025-04-10 17:41:40 +0800

    more

--- python/sglang/srt/managers/expert_location.py
+++ python/sglang/srt/managers/expert_location.py
@@ -23,8 +23,9 @@ class ExpertLocationMetadata:
     @staticmethod
     def from_model_config(model_config: ModelConfig):
         model_class, _ = get_model_architecture(model_config)
-        if hasattr(model_class, "get_expert_location_metadata"):
-            return model_class.get_expert_location_metadata(model_config.hf_config)
+        if hasattr(model_class, "get_model_config_for_expert_location"):
+            model_config_for_expert_location = model_class.get_model_config_for_expert_location(model_config.hf_config)
+            return TODO
         return ExpertLocationMetadata._init_dummy()
 
     @staticmethod
