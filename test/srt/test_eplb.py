77a986b8f1e43eaf9672e07d1f1723f3fdc5241f
fzyzcjy <ch271828n@outlook.com>
2025-04-11 17:12:17 +0800

    more

--- test/srt/test_eplb.py
+++ test/srt/test_eplb.py
@@ -145,7 +145,7 @@ class TestEPLB(CustomTestCase):
         physical_to_logical_map = (
                 (offset + torch.arange(0, _NUM_ROUTED_EXPERTS + ep_num_redundant_experts).repeat(_NUM_HIDDEN_LAYERS, 1))
                 % _NUM_ROUTED_EXPERTS
-        )
+        ).tolist()
         init_expert_location = dict(physical_to_logical_map=physical_to_logical_map.tolist())
 
         engine = sgl.Engine(**engine_kwargs, init_expert_location=json.dumps(init_expert_location))
