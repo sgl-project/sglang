9b981f9a126029b735f28d95321cfaf636cf822a
ch-wan <cwan39@gatech.edu>
2025-04-11 01:07:31 +0000

    format

--- python/sglang/srt/layers/attention/flashattention_backend.py
+++ python/sglang/srt/layers/attention/flashattention_backend.py
@@ -976,14 +976,12 @@ class FlashAttentionBackend(AttentionBackend):
                 max_seq_pages = (
                     metadata.max_seq_len_k + self.page_size - 1
                 ) // self.page_size
-                page_indices = (
-                    self.req_to_token[
-                        req_pool_indices[:, None],
-                        self.decode_cuda_graph_metadata["strided_indices"][
-                            :max_seq_pages
-                        ][None, :],
-                    ]
-                )
+                page_indices = self.req_to_token[
+                    req_pool_indices[:, None],
+                    self.decode_cuda_graph_metadata["strided_indices"][:max_seq_pages][
+                        None, :
+                    ],
+                ]
                 page_indices //= self.page_size
                 metadata.page_table[:, :max_seq_pages].copy_(page_indices)
                 metadata.page_table[:, max_seq_pages:].fill_(0)
