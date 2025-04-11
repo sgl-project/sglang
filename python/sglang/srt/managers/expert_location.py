8fbd02678917f4cc19b41c9b7b396419681b6612
fzyzcjy <ch271828n@outlook.com>
2025-04-11 11:33:08 +0800

    more

--- python/sglang/srt/managers/expert_location.py
+++ python/sglang/srt/managers/expert_location.py
@@ -186,14 +186,15 @@ def _compute_logical_to_all_physical_map(physical_to_logical_map: torch.Tensor, 
     logical_to_all_physical_map = [
         [
             []
-            for physical_expert_id in range(num_logical_experts)
+            for _logical_expert_id in range(num_logical_experts)
         ]
-        for layer_id in range(num_layers)
+        for _layer_id in range(num_layers)
     ]
 
     for layer_id in range(num_layers):
         for physical_expert_id in range(num_physical_experts):
-            TODO
+            logical_expert_id = physical_to_logical_map[layer_id, physical_expert_id].item()
+            logical_to_all_physical_map[layer_id][logical_expert_id].append(physical_expert_id)
 
     return TODO
 
