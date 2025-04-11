d62bafb5cc811d145d2921db0af9aca16e616dd0
fecet <xiezej@gmail.com>
2025-04-11 10:25:03 +0800

    apply change

--- sgl-kernel/build.sh
+++ sgl-kernel/build.sh
@@ -36,6 +36,7 @@ docker run --rm \
    which cmake
    cmake --version
 
+   /sgl-kernel/replace_ptxas.sh ${CUDA_VERSION} && \
    ${PYTHON_ROOT_PATH}/bin/${TORCH_INSTALL} && \
    ${PYTHON_ROOT_PATH}/bin/pip install --no-cache-dir ninja setuptools==75.0.0 wheel==0.41.0 numpy uv scikit-build-core && \
    export TORCH_CUDA_ARCH_LIST='7.5 8.0 8.9 9.0+PTX' && \
