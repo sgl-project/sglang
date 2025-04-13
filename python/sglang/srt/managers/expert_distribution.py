217236775cae4c77f0ecff08af8c3d8370de54d7
fzyzcjy <ch271828n@outlook.com>
2025-04-13 14:31:50 +0800

    more

--- python/sglang/srt/managers/expert_distribution.py
+++ python/sglang/srt/managers/expert_distribution.py
@@ -227,8 +227,13 @@ class _DeepepNormalSinglePassGatherer(_LayerBasedSinglePassGatherer):
 # e.g. use naive tensor copying
 # need to consider CUDA graph, e.g. add initialization and after-end
 class _DeepepLowLatencySinglePassGatherer(_SinglePassGatherer):
-    def __init__(self):
-        self._data = torch.zeros((num_layers, num_local_physical_experts), dtype=torch.int)
+    def __init__(self, expert_location_metadata: "ExpertLocationMetadata"):
+        super().__init__(expert_location_metadata)
+        self._data = torch.zeros(
+            (expert_location_metadata.num_layers, expert_location_metadata.num_local_physical_experts),
+            dtype=torch.int,
+            device=TODO,
+        )
 
     def on_deepep_dispatch_low_latency(self, layer_idx: int, recv_count: torch.Tensor):
         self._data[layer_idx, :] = packed_recv_count
