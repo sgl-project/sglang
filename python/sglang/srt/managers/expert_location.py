c493293d46152beaea40c56b23d3950b649dee35
fzyzcjy <ch271828n@outlook.com>
2025-04-13 18:41:01 +0800

    more

--- python/sglang/srt/managers/expert_location.py
+++ python/sglang/srt/managers/expert_location.py
@@ -140,6 +140,7 @@ class ExpertLocationMetadata:
             getattr(self, field)[...] = getattr(other, field)
 
         for field in [
+            # TODO maybe make last dim size const to simplify the logic
             "logical_to_all_physical_map",
         ]:
             setattr(self, field, getattr(other, field).to(getattr(self, field).device))
