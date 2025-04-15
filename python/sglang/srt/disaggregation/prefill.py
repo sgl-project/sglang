2fafad39b15331b0f378f00cd0688eb3946ea329
ch-wan <cwan39@gatech.edu>
2025-04-15 06:02:04 +0000

    fix

--- python/sglang/srt/disaggregation/prefill.py
+++ python/sglang/srt/disaggregation/prefill.py
@@ -210,7 +210,7 @@ class SchedulerDisaggregationPrefillMixin:
 
         polls = poll_and_all_reduce(
             [req.disagg_kv_sender for req in self.disagg_prefill_inflight_queue],
-            self.tp_worker.get_tp_cpu_group(),
+            self.attn_tp_cpu_group,
         )
 
         undone_reqs: List[Req] = []
