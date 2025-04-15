2e48a4f63482b90c6ab4a70b45af8cf7d556778c
Chunyuan WU <chunyuan.wu@intel.com>
2025-04-15 15:09:32 +0800

    FP8: update MoE padding value and support TP=6 (#53)
    * port TP = 6 support into vLLmParameter for FP8
    
    * set moe padding size to weight_block_size for FP8
    
    * reuse the _get_quantization_config function and move update_config to be after load_config defined
    
    * remove unused variable
    
    * fix the start value in load_merged_column_weight
    
    * refactor param_data narrow in load_merged_column_weight

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -88,6 +88,7 @@ from triton.runtime.cache import (
 
 if TYPE_CHECKING:
     from sglang.srt.configs.model_config import ModelConfig
+    from sglang.srt.configs.load_config import LoadConfig
 
 from sglang.srt.layers.vocab_parallel_embedding import pad_vocab_size
 
@@ -2179,6 +2180,27 @@ class Withable(Generic[T]):
             self._value = None
 
 
+def get_moe_padding_size(model_config, load_config):
+    from sglang.srt.model_loader.loader import _get_quantization_config
+
+    quant_config = _get_quantization_config(model_config, load_config)
+
+    if quant_config is not None and hasattr(quant_config, "weight_block_size"):
+        # See NOTE(HandH1998): To ensure proper alignment of the block-wise quantization scales, the output_size of the weights for both the gate and up layers must be divisible by block_n.
+        weight_block_size = getattr(quant_config, "weight_block_size")
+
+        assert (
+            len(weight_block_size) == 2
+        ), "Only len(weight_block_size) == 2 is supported"
+        assert (
+            weight_block_size[0] == weight_block_size[1]
+        ), "Only weight_block_size[0] == weight_block_size[1] is supported"
+
+        return weight_block_size[0]
+
+    return DEFAULT_MOE_PADDING_SIZE
+
+
 def update_intermediate_size(model_config, attr_name, intermediate_padding_size):
     if hasattr(model_config.hf_config, attr_name):
         attr_value = getattr(model_config.hf_config, attr_name)
@@ -2189,7 +2211,9 @@ def update_intermediate_size(model_config, attr_name, intermediate_padding_size)
     return model_config
 
 
-def update_config(model_config: ModelConfig, tp_size: int) -> ModelConfig:
+def update_config(
+    model_config: ModelConfig, load_config: LoadConfig, tp_size: int
+) -> ModelConfig:
     # Support the case where the num_attention_heads is not divisible by the TP size.
     if model_config.num_attention_heads % tp_size != 0:
         query_heads_per_kv = (
@@ -2206,7 +2230,9 @@ def update_config(model_config: ModelConfig, tp_size: int) -> ModelConfig:
         model_config.hf_config.num_attention_heads = num_attention_heads
         model_config.hf_text_config.num_attention_heads = num_attention_heads
 
-    intermediate_padding_size = tp_size * DEFAULT_MOE_PADDING_SIZE
+    intermediate_padding_size = tp_size * get_moe_padding_size(
+        model_config, load_config
+    )
     model_config = update_intermediate_size(
         model_config, "moe_intermediate_size", intermediate_padding_size
     )
