d5598bbc2e28abd75ef9c3926d616b3a55b9b24e
AinL <gmlwns5176@gmail.com>
2025-04-16 04:13:53 +0000

    fix

--- python/sglang/srt/models/llama4.py
+++ python/sglang/srt/models/llama4.py
@@ -525,7 +525,7 @@ class Llama4ForCausalLM(LlamaForCausalLM):
         "qkv_proj": ["q_proj", "k_proj", "v_proj"],
         "gate_up_proj": ["gate_proj", "up_proj"],
     }
-    hip_attention_supported = False
+    hip_attention_supported = True
 
     def __init__(
         self,
