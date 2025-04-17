384db827c321cbc147fe9cc30e3316f1d1448af9
fzyzcjy <ch271828n@outlook.com>
2025-04-17 11:18:24 +0800

    more

--- python/sglang/srt/model_executor/model_weight_updater.py
+++ python/sglang/srt/model_executor/model_weight_updater.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from dataclasses import dataclass
 from typing import Iterator, Tuple
 
 import torch
@@ -7,11 +8,31 @@ from sglang.srt.model_executor.memory_transfer import AsyncToCudaManager, Combin
 
 class ModelWeightUpdater:
     def __init__(self, init_pin_memory: bool):
+        self._state: _State = _StateIdle()
         self._manager_transfer_manager = AsyncToCudaManager() if init_pin_memory else CombinedManager.init_pin_memory_and_to_cuda()
         self._model_weight_source = _ModelWeightSourcePinnedMemory() if init_pin_memory else _ModelWeightSourceVanilla()
 
     def start_prepare(self):
         TODO
+
+
+class _State(ABC):
+    pass
+
+
+@dataclass
+class _StateIdle(_State):
+    pass
+
+
+@dataclass
+class _StateAwaitMemoryTransfer(_State):
+    pass
+
+
+@dataclass
+class _StatePrepared(_State):
+    named_tensors: List[Tuple[str, torch.Tensor]]
 
 
 class _ModelWeightSourceBase(ABC):
