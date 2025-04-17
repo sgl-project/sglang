687647c6054deb187f0e46e2d12e1f700bb1fab5
fzyzcjy <ch271828n@outlook.com>
2025-04-17 11:15:22 +0800

    more

--- python/sglang/srt/model_executor/model_weight_updater.py
+++ python/sglang/srt/model_executor/model_weight_updater.py
@@ -1,4 +1,19 @@
+from abc import ABC
+
+
 class ModelWeightUpdater:
-    def __init__(self):
+    def __init__(self, init_pin_memory: bool):
         self._manager_transfer_manager = TODO
-        self._model_weight_source = TODO
+        self._model_weight_source = _ModelWeightSourcePinnedMemory() if init_pin_memory else _ModelWeightSourceVanilla()
+
+
+class _ModelWeightSourceBase(ABC):
+    pass
+
+
+class _ModelWeightSourceVanilla(ABC):
+    TODO
+
+
+class _ModelWeightSourcePinnedMemory(ABC):
+    TODO
