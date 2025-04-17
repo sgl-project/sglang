68f104788a01733cf75d36a6c76d0f97968dedc6
liwenju <liwenjudetiankong@126.com>
2025-04-17 14:04:59 +0800

    add test_vision_openai_server of kimi-vl

--- test/srt/test_vision_openai_server.py
+++ test/srt/test_vision_openai_server.py
@@ -731,6 +731,32 @@ class TestGemma3itServer(TestOpenAIVisionServer):
     def test_video_chat_completion(self):
         pass
 
+class TestKimiVLServer(TestOpenAIVisionServer):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = "moonshotai/Kimi-VL-A3B-Instruct"
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.api_key = "sk-123456"
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--trust-remote-code",
+                "--chat-template",
+                "kimi-vl",
+                "--context-length",
+                "4096",
+                "--tensor-parallel-size",
+                "2",
+                "--dtype",
+                "bfloat16",
+            ],
+        )
+        cls.base_url += "/v1"
+
+    def test_video_chat_completion(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
