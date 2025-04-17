ffd4aecf1f88646c79e2c4970894c5666e5c98c7
luoyuan.luo <luoyuan.luo@antgroup.com>
2025-04-17 22:22:15 +0800

    Refine get local ip for closed network environment

--- python/sglang/srt/disaggregation/mooncake/conn.py
+++ python/sglang/srt/disaggregation/mooncake/conn.py
@@ -27,7 +27,7 @@ from sglang.srt.disaggregation.base.conn import (
 from sglang.srt.disaggregation.mooncake.transfer_engine import MooncakeTransferEngine
 from sglang.srt.disaggregation.utils import DisaggregationMode
 from sglang.srt.server_args import ServerArgs
-from sglang.srt.utils import get_free_port, get_ip, get_local_ip_by_remote
+from sglang.srt.utils import get_free_port, get_ip, get_local_ip
 
 logger = logging.getLogger(__name__)
 
@@ -202,7 +202,7 @@ class MooncakeKVManager(BaseKVManager):
 
     def start_prefill_thread(self):
         self.rank_port = get_free_port()
-        self.server_socket.bind(f"tcp://{get_local_ip_by_remote()}:{self.rank_port}")
+        self.server_socket.bind(f"tcp://{get_local_ip()}:{self.rank_port}")
 
         def bootstrap_thread():
             """This thread recvs pre-alloc notification from the decode engine"""
@@ -266,7 +266,7 @@ class MooncakeKVManager(BaseKVManager):
 
     def start_decode_thread(self):
         self.rank_port = get_free_port()
-        self.server_socket.bind(f"tcp://{get_local_ip_by_remote()}:{self.rank_port}")
+        self.server_socket.bind(f"tcp://{get_local_ip()}:{self.rank_port}")
 
         def decode_thread():
             while True:
@@ -321,13 +321,13 @@ class MooncakeKVManager(BaseKVManager):
         if self.dist_init_addr:
             ip_address = socket.gethostbyname(self.dist_init_addr.split(":")[0])
         else:
-            ip_address = get_ip()
+            ip_address = get_local_ip()
 
         bootstrap_server_url = f"{ip_address}:{self.bootstrap_port}"
         url = f"http://{bootstrap_server_url}/route"
         payload = {
             "role": "Prefill",
-            "rank_ip": get_local_ip_by_remote(),
+            "rank_ip": get_local_ip(),
             "rank_port": self.rank_port,
             "engine_rank": self.kv_args.engine_rank,
         }
@@ -459,7 +459,7 @@ class MooncakeKVReceiver(BaseKVReceiver):
         self._connect("tcp://" + self.prefill_server_url).send_multipart(
             [
                 str(self.bootstrap_room).encode("ascii"),
-                get_local_ip_by_remote().encode("ascii"),
+                get_local_ip().encode("ascii"),
                 str(self.kv_mgr.rank_port).encode("ascii"),
                 self.session_id.encode("ascii"),
                 packed_kv_data_ptrs,
