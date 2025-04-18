8e1ffb69d7413d654a7c670d1f8a6a098caffd32
Zijian Zhang <zijzhang@nvidia.com>
2025-04-18 05:03:20 -0700

    feat: support refactored VILA remote code

--- python/sglang/srt/managers/multimodal_processors/vila.py
+++ python/sglang/srt/managers/multimodal_processors/vila.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Tuple, Type, TypedDict, Union, Unpack, cast
+from typing import List, Optional, Tuple, Type, Union, Unpack, cast
 
 import numpy as np
 import torch
@@ -6,13 +6,7 @@ import torch.nn as nn
 import transformers.image_utils as image_utils
 from numpy.typing import NDArray
 from torch import Tensor
-from transformers import (
-    BatchFeature,
-    PretrainedConfig,
-    PreTrainedTokenizer,
-    ProcessorMixin,
-    TensorType,
-)
+from transformers import BatchFeature, PretrainedConfig, ProcessorMixin, TensorType
 from transformers.image_utils import ImageInput, VideoInput
 from transformers.processing_utils import ProcessingKwargs
 from transformers.tokenization_utils_base import TextInput
@@ -23,6 +17,49 @@ from sglang.srt.managers.multimodal_processors.base_processor import (
 )
 from sglang.srt.models.vila import VILAForConditionalGeneration
 from sglang.srt.server_args import ServerArgs
+
+
+class VILAProcessorKwargsHF(ProcessingKwargs, total=False):
+    _defaults = {}  # type: ignore
+
+
+class VILAProcessorOutputHF(BatchFeature):
+    input_ids: List[List[int]] | NDArray[np.int64] | Tensor
+    attention_mask: List[List[int]] | NDArray[np.int64] | Tensor
+    pixel_values: Optional[List[NDArray[np.float32]] | NDArray[np.float32] | Tensor]
+
+
+class VILAProcessorHF(ProcessorMixin):
+    def __call__(
+        self,
+        images: Optional[ImageInput] = None,
+        text: Optional[TextInput | List[TextInput]] = None,
+        audio: None = None,
+        videos: Optional[VideoInput] = None,
+        **kwargs: Unpack[VILAProcessorKwargsHF],
+    ) -> VILAProcessorOutputHF:
+        raise NotImplementedError
+
+    def _pad_image_tokens_by_num_crops(
+        self,
+        text: List[TextInput],
+        *,
+        num_cropped_images: List[int],
+    ) -> List[TextInput]:
+        raise NotImplementedError
+
+    def _pad_image_tokens_by_num_embeddings(
+        self,
+        text: List[TextInput],
+    ) -> List[TextInput]:
+        raise NotImplementedError
+
+    def _process_images(
+        self,
+        images: ImageInput,
+        **kwargs: Unpack[VILAProcessorKwargsHF],
+    ) -> Tuple[BatchFeature, List[int]]:
+        raise NotImplementedError
 
 
 class VILAProcessorOutput(BatchFeature):
@@ -40,13 +77,13 @@ class VILAProcessorOutput(BatchFeature):
 class VILAProcessor(BaseMultimodalProcessor):
     models: List[Type[nn.Module]] = [VILAForConditionalGeneration]
 
-    _processor: "_VILAProcessorHF"
+    _processor: VILAProcessorHF
 
     def __init__(
         self,
         hf_config: PretrainedConfig,
         server_args: ServerArgs,
-        _processor: "_VILAProcessorHF",
+        _processor: VILAProcessorHF,
     ) -> None:
         super().__init__(hf_config, server_args, _processor)
 
@@ -64,88 +101,38 @@ class VILAProcessor(BaseMultimodalProcessor):
         ), "input_text must be a string or a list of integers as input_ids. Batch input is not supported."
         input_text = cast(Union[str, List[int]], input_text)
 
-        # Handle empty image input.
+        # If no image provided, no need to process.
         if image_data is None:
             return None
 
-        # Process images.
-        # TODO: Use self.load_mm_data
-        if isinstance(image_data, str):
-            image_data = [image_data]
+        # Normalize input data.
+        image_data = [image_data] if isinstance(image_data, str) else image_data
 
         images = [image_utils.load_image(image_file) for image_file in image_data]
 
-        image_inputs, num_cropped_images = self._processor._process_images(
-            images,
-            return_tensors=TensorType.PYTORCH,
+        input_text = (
+            input_text
+            if isinstance(input_text, str)
+            else cast(
+                List[str],
+                self._processor.post_process_image_text_to_text(
+                    [input_text], skip_special_tokens=False
+                ),
+            )[0]
         )
 
-        # Process text.
-        if not isinstance(input_text, str):
-            input_text = self._processor.tokenizer.decode(input_text)
-
-        input_text = self._processor._pad_image_tokens_by_num_crops(
-            [input_text],
-            num_cropped_images=num_cropped_images,
-        )[0]
-
-        input_text = self._processor._pad_image_tokens_by_num_embeddings(
-            [input_text],
-        )[0]
-
-        input_ids: List[int] = self._processor.tokenizer.__call__(
-            text=[input_text],
-        ).input_ids[0]
+        inputs = self._processor.__call__(
+            images=images,
+            text=input_text,
+            max_length=max_req_input_len,
+            return_tensors=TensorType.PYTORCH,
+            truncation=True,
+        )
 
         return VILAProcessorOutput(
             data=dict(
-                input_ids=input_ids,
-                pixel_values=image_inputs.pixel_values,
+                input_ids=list(inputs.input_ids[0]),
+                pixel_values=inputs.pixel_values,
                 data_hashes=[hash(image_file) for image_file in image_data],
             )
         )
-
-
-class _VILAProcessorKwargsHF(ProcessingKwargs, total=False):
-    _defaults = {}  # type: ignore
-
-
-class _VILAProcessorOutputHF(BatchFeature):
-    input_ids: List[List[int]] | NDArray[np.int64] | Tensor
-    attention_mask: List[List[int]] | NDArray[np.int64] | Tensor
-    pixel_values: Optional[List[NDArray[np.float32]] | NDArray[np.float32] | Tensor]
-
-
-class _VILAProcessorHF(ProcessorMixin):
-    tokenizer: PreTrainedTokenizer
-
-    def __call__(
-        self,
-        images: Optional[ImageInput] = None,
-        text: Optional[TextInput | List[TextInput]] = None,
-        audio: None = None,
-        videos: Optional[VideoInput] = None,
-        **kwargs: Unpack[_VILAProcessorKwargsHF],
-    ) -> _VILAProcessorOutputHF:
-        raise NotImplementedError
-
-    def _process_images(
-        self,
-        images: ImageInput,
-        **kwargs: Unpack[_VILAProcessorKwargsHF],
-    ) -> Tuple[BatchFeature, List[int]]:
-        raise NotImplementedError
-
-    def _pad_image_tokens_by_num_crops(
-        self,
-        text: List[TextInput],
-        *,
-        num_cropped_images: List[int],
-    ) -> List[TextInput]:
-        raise NotImplementedError
-
-    def _pad_image_tokens_by_num_embeddings(
-        self,
-        text: List[TextInput],
-    ) -> List[TextInput]:
-        raise NotImplementedError
--- python/sglang/srt/models/vila.py
+++ python/sglang/srt/models/vila.py
@@ -1,21 +1,18 @@
 import logging
-from typing import Iterable, List, Optional, Tuple, cast
+from typing import Dict, Iterable, List, Optional, Tuple, cast
 
 import torch
 import torch.nn as nn
 from torch import Tensor
-from transformers import (
-    AutoModelForImageTextToText,
-    GenerationMixin,
-    PretrainedConfig,
-    PreTrainedModel,
-    Qwen2Config,
-)
+from transformers import GenerationMixin, PretrainedConfig, PreTrainedModel, Qwen2Config
+from transformers import Qwen2ForCausalLM as Qwen2ForCausalLMHF
+from transformers import SiglipVisionConfig, SiglipVisionModel
+from transformers.modeling_outputs import BaseModelOutputWithPooling
 
-import sglang.srt.utils
+import sglang.srt.managers.mm_utils as mm_utils
+import sglang.srt.utils as utils
 from sglang.srt.layers.logits_processor import LogitsProcessorOutput
 from sglang.srt.layers.quantization.base_config import QuantizationConfig
-from sglang.srt.managers import mm_utils
 from sglang.srt.managers.mm_utils import MultiModalityDataPaddingPatternImageTokens
 from sglang.srt.managers.schedule_batch import MultimodalInputs
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch
@@ -28,22 +25,113 @@ logger = logging.getLogger(__name__)
 
 class VILAConfig(PretrainedConfig):
     text_config: Qwen2Config
+    vision_config: SiglipVisionConfig
+
+    hidden_size: int
+    image_token_id: int
+    mm_projector_type: str
+    mm_vision_select_feature: str
+    mm_vision_select_layer: int
+
+
+##### Copy from remote code. #####
+
+
+class DownSample3x3BlockFix(nn.Module):
+    @staticmethod
+    def flat_square_3x3(x: Tensor) -> Tensor:
+        n, w, h, c = x.size()
+        if w % 3 != 0:
+            x = torch.concat(
+                [
+                    x,
+                    torch.zeros((n, 3 - (w % 3), h, c), device=x.device, dtype=x.dtype),
+                ],
+                dim=1,
+            ).contiguous()
+            n, w, h, c = x.size()
+        x = x.contiguous()
+        if h % 3 != 0:
+            x = torch.concat(
+                [
+                    x,
+                    torch.zeros((n, w, 3 - (h % 3), c), device=x.device, dtype=x.dtype),
+                ],
+                dim=2,
+            ).contiguous()
+            n, w, h, c = x.size()
+        x = x.view(n, w, int(h / 3), int(c * 3))
+        x = x.permute(0, 2, 1, 3).contiguous()
+        x = x.view(n, int(h / 3), int(w / 3), int(c * 9))
+        x = x.permute(0, 2, 1, 3).contiguous()
+        return x
+
+    def forward(self, x: Tensor) -> Tensor:
+        vit_embeds = x
+        h = w = int(vit_embeds.shape[1] ** 0.5)
+        vit_embeds = vit_embeds.reshape(vit_embeds.shape[0], h, w, -1)
+        vit_embeds = self.flat_square_3x3(vit_embeds)
+        vit_embeds = vit_embeds.reshape(vit_embeds.shape[0], -1, vit_embeds.shape[-1])
+        return vit_embeds
+
+
+class MultimodalProjector(nn.Module):
+    layers: nn.Sequential
+
+    def __init__(
+        self,
+        config: VILAConfig,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        match config.mm_projector_type:
+            case "linear":
+                self.layers = nn.Sequential(
+                    nn.Linear(config.vision_config.hidden_size, config.hidden_size),
+                )
+            case "mlp_downsample_3x3_fix":
+                self.layers = nn.Sequential(
+                    DownSample3x3BlockFix(),
+                    nn.LayerNorm(config.vision_config.hidden_size * 9),
+                    nn.Linear(
+                        config.vision_config.hidden_size * 9,
+                        config.vision_config.hidden_size * 3,
+                    ),
+                    nn.GELU(),
+                    nn.LayerNorm(config.vision_config.hidden_size * 3),
+                    nn.Linear(config.vision_config.hidden_size * 3, config.hidden_size),
+                    nn.GELU(),
+                    nn.Linear(config.hidden_size, config.hidden_size),
+                )
+            case _:
+                raise NotImplementedError(
+                    f"mm_projector_type={config.mm_projector_type} not implemented."
+                )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.layers(x)
+
+
+##### End of copy. #####
 
 
 class VILAForConditionalGenerationHF(PreTrainedModel, GenerationMixin):
-    mm_projector: PreTrainedModel
-    llm: PreTrainedModel
-    vision_tower: PreTrainedModel
+    config: VILAConfig
+
+    llm: Qwen2ForCausalLMHF
+    mm_projector: MultimodalProjector
+    vision_tower: SiglipVisionModel
 
 
 class VILAForConditionalGeneration(nn.Module):
     config: VILAConfig
     quant_config: Optional[QuantizationConfig]
 
-    language_model: Qwen2ForCausalLM
-
-    mm_projector: PreTrainedModel
-    vision_tower: PreTrainedModel
+    llm: Qwen2ForCausalLM
+    mm_projector: MultimodalProjector
+    vision_tower: SiglipVisionModel
 
     def __init__(
         self,
@@ -56,21 +144,13 @@ class VILAForConditionalGeneration(nn.Module):
         self.config = config
         self.quant_config = quant_config
 
-        hf_model: VILAForConditionalGenerationHF = (
-            AutoModelForImageTextToText.from_pretrained(
-                config.name_or_path,
-                trust_remote_code=True,
-            )
-        )
-        self.mm_projector = hf_model.mm_projector
-        self.vision_tower = hf_model.vision_tower
-
-        self.language_model = Qwen2ForCausalLM(
+        self.llm = Qwen2ForCausalLM(
             config=config.text_config,
             quant_config=quant_config,
-            prefix=sglang.srt.utils.add_prefix("llm", prefix),
+            prefix=utils.add_prefix("llm", prefix),
         )
-        self.language_model.load_weights(hf_model.llm.named_parameters())
+        self.mm_projector = MultimodalProjector(config)
+        self.vision_tower = SiglipVisionModel(config.vision_config)
 
     def forward(
         self,
@@ -81,11 +161,11 @@ class VILAForConditionalGeneration(nn.Module):
         input_embeds = mm_utils.general_mm_embed_routine(
             input_ids=input_ids,
             forward_batch=forward_batch,
-            embed_tokens=cast(nn.Embedding, self.language_model.model.embed_tokens),
+            embed_tokens=cast(nn.Embedding, self.llm.model.embed_tokens),
             mm_data_embedding_func=self._embed_mm_data,
         )
 
-        return self.language_model.__call__(
+        return self.llm.__call__(
             input_ids=None,
             positions=positions,
             forward_batch=forward_batch,
@@ -93,10 +173,17 @@ class VILAForConditionalGeneration(nn.Module):
         )
 
     def load_weights(self, weights: Iterable[Tuple[str, Tensor]]) -> None:
-        # VILA stores weights in sub-directories, so SGLang cannot load them directly.
-        logger.warning(
-            "VILA does not support SGLang weight loading. Its weights are directly loaded from HuggingFace on initialization."
-        )
+        llm_state_dict: Dict[str, Tensor] = {}
+        other_state_dict: Dict[str, Tensor] = {}
+
+        for name, param in weights:
+            if name.startswith("llm."):
+                llm_state_dict[name[len("llm.") :]] = param
+            else:
+                other_state_dict[name] = param
+
+        self.load_state_dict(other_state_dict, strict=False)
+        self.llm.load_weights(llm_state_dict.items())
 
     def pad_input_ids(
         self,
@@ -111,15 +198,38 @@ class VILAForConditionalGeneration(nn.Module):
 
     def _embed_mm_data(self, mm_input: MultimodalInputs) -> Tensor:
         assert isinstance(mm_input.pixel_values, Tensor)
-
         pixel_values = mm_input.pixel_values.to(
             device=self.vision_tower.device, dtype=self.vision_tower.dtype
         )
 
-        image_features: Tensor = self.vision_tower.__call__(pixel_values)
-        image_features = self.mm_projector.__call__(image_features)
+        ##### Copy from remote code. #####
 
-        return image_features
+        image_features: BaseModelOutputWithPooling = self.vision_tower.__call__(
+            pixel_values,
+            output_hidden_states=True,
+        )
+        assert image_features.hidden_states is not None
+
+        # Select image feature.
+        selected_layer_output = image_features.hidden_states[
+            self.config.mm_vision_select_layer
+        ]
+        match self.config.mm_vision_select_feature:
+            case "cls_patch":
+                selected_feature = selected_layer_output
+            case _:
+                raise NotImplementedError(
+                    f"mm_vision_select_feature={self.config.mm_vision_select_feature} not implemented."
+                )
+
+        image_embedding: Tensor = self.mm_projector.__call__(selected_feature)
+
+        n_images, n_feature, dim_feature = image_embedding.shape
+        image_embedding = image_embedding.view(n_images * n_feature, dim_feature)
+
+        ##### End of copy. #####
+
+        return image_embedding
 
 
 EntryClass = [VILAForConditionalGeneration]
