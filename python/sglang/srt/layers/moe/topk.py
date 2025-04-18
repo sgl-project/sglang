f9e63fe18944dba9a26d280232377c7dc78c5bfa
BBuf <1182563586@qq.com>
2025-04-18 14:17:20 +0800

    refine

--- python/sglang/srt/layers/moe/topk.py
+++ python/sglang/srt/layers/moe/topk.py
@@ -221,7 +221,7 @@ def biased_grouped_topk(
     if (
         _is_cuda
         and gating_output.shape[1] // num_expert_group
-        <= 32  # assert check3 in moe_fused_gate cuda kernel. When kernel can handle gating_output.shape[1] // num_expert_group > 32, we can remove this check.
+        <= 32  # moe_fused_gate kernel ensure that num_experts/num_expert_group does not exceed MAX_VPT=32 now. And when kernel can handle MAX_VPT > 32, we can remove this assertion.
         and n_share_experts_fusion == 0
         and is_power_of_two(correction_bias.shape[0])
     ):
