35f603c214f3ca66b56a3ac81f56dbff7d1fb30e
Yubo Wang <yubowang2019@gmail.com>
2025-04-19 04:08:37 +0000

    remove comments

--- python/sglang/srt/layers/sampler.py
+++ python/sglang/srt/layers/sampler.py
@@ -112,10 +112,6 @@ class Sampler(nn.Module):
                         check_nan=self.use_nan_detection,
                     )
 
-                    # if self.use_nan_detection and not torch.all(success):
-                    #     logger.warning("Detected errors during sampling!")
-                    #     batch_next_token_ids = torch.zeros_like(batch_next_token_ids)
-
             elif global_server_args_dict["sampling_backend"] == "pytorch":
                 # A slower fallback implementation with torch native operations.
                 batch_next_token_ids = top_k_top_p_min_p_sampling_from_probs_torch(
