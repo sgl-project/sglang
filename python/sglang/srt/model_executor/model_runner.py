fa59d3133f0d9e2b6a3857fb0a6f80c492f035e1
ch-wan <cwan39@gatech.edu>
2025-04-20 03:26:55 +0000

    Revert "remove debug info"
    This reverts commit 234cf14b1ea98ea24406a2a62f5ada98167c2141.

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -1123,6 +1123,9 @@ class ModelRunner:
     def _forward_raw(
         self, forward_batch: ForwardBatch, skip_attn_backend_init: bool
     ) -> LogitsProcessorOutput:
+        print(
+            f"hi forward_raw tp_rank={get_tensor_model_parallel_rank()} {forward_batch.forward_mode=} {forward_batch.batch_size=} {forward_batch.tbo_split_seq_index=}"
+        )
         if (
             forward_batch.forward_mode.is_cuda_graph()
             and self.cuda_graph_runner
