83e12f769c250b95267a53320eb8ce8e7fbe3039
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-04-20 12:06:59 +0800

    Update io_struct.py

--- python/sglang/srt/managers/io_struct.py
+++ python/sglang/srt/managers/io_struct.py
@@ -397,8 +397,8 @@ class GenerateReqInput:
                 else None
             ),
             return_hidden_states=self.return_hidden_states,
-            bootstrap_host=self.bootstrap_host,
-            bootstrap_room=self.bootstrap_room,
+            bootstrap_host=self.bootstrap_host[i],
+            bootstrap_room=self.bootstrap_room[i],
         )
 
 
