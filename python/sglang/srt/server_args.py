190c981050b2df791755fe4c975bfbede2cf65a2
fzyzcjy <ch271828n@outlook.com>
2025-04-20 10:04:12 +0800

    more

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -172,7 +172,7 @@ class ServerArgs:
     enable_eplb: bool = False
     eplb_storage_dir: str = "/tmp/eplb_storage"
     eplb_rebalance_period: Optional[int] = None
-    enable_expert_distribution_recorder: bool = False
+    expert_distribution_recorder_mode: Optional[Literal["stat", "detail", "state_and_ut"]] = None
     enable_torch_compile: bool = False
     torch_compile_max_bs: int = 32
     cuda_graph_max_bs: Optional[int] = None
@@ -343,11 +343,11 @@ class ServerArgs:
             )
 
         if self.enable_eplb:
-            self.enable_expert_distribution_recorder = True
+            self.expert_distribution_recorder_mode = "stat"
             if self.expert_location_updater_mode is None:
                 self.expert_location_updater_mode = "pageable_memory"
             logger.info(
-                f"EPLB is enabled. The enable_expert_distribution_recorder and expert_location_updater_mode are automatically set."
+                f"EPLB is enabled. The expert_distribution_recorder_mode and expert_location_updater_mode are automatically set."
             )
         if self.expert_location_updater_mode is not None:
             self.disable_overlap_schedule = True
@@ -459,8 +459,8 @@ class ServerArgs:
             default=ServerArgs.tokenizer_mode,
             choices=["auto", "slow"],
             help="Tokenizer mode. 'auto' will use the fast "
-            "tokenizer if available, and 'slow' will "
-            "always use the slow tokenizer.",
+                 "tokenizer if available, and 'slow' will "
+                 "always use the slow tokenizer.",
         )
         parser.add_argument(
             "--skip-tokenizer-init",
@@ -484,21 +484,21 @@ class ServerArgs:
                 "remote",
             ],
             help="The format of the model weights to load. "
-            '"auto" will try to load the weights in the safetensors format '
-            "and fall back to the pytorch bin format if safetensors format "
-            "is not available. "
-            '"pt" will load the weights in the pytorch bin format. '
-            '"safetensors" will load the weights in the safetensors format. '
-            '"npcache" will load the weights in pytorch format and store '
-            "a numpy cache to speed up the loading. "
-            '"dummy" will initialize the weights with random values, '
-            "which is mainly for profiling."
-            '"gguf" will load the weights in the gguf format. '
-            '"bitsandbytes" will load the weights using bitsandbytes '
-            "quantization."
-            '"layered" loads weights layer by layer so that one can quantize a '
-            "layer before loading another to make the peak memory envelope "
-            "smaller.",
+                 '"auto" will try to load the weights in the safetensors format '
+                 "and fall back to the pytorch bin format if safetensors format "
+                 "is not available. "
+                 '"pt" will load the weights in the pytorch bin format. '
+                 '"safetensors" will load the weights in the safetensors format. '
+                 '"npcache" will load the weights in pytorch format and store '
+                 "a numpy cache to speed up the loading. "
+                 '"dummy" will initialize the weights with random values, '
+                 "which is mainly for profiling."
+                 '"gguf" will load the weights in the gguf format. '
+                 '"bitsandbytes" will load the weights using bitsandbytes '
+                 "quantization."
+                 '"layered" loads weights layer by layer so that one can quantize a '
+                 "layer before loading another to make the peak memory envelope "
+                 "smaller.",
         )
         parser.add_argument(
             "--trust-remote-code",
@@ -511,13 +511,13 @@ class ServerArgs:
             default=ServerArgs.dtype,
             choices=["auto", "half", "float16", "bfloat16", "float", "float32"],
             help="Data type for model weights and activations.\n\n"
-            '* "auto" will use FP16 precision for FP32 and FP16 models, and '
-            "BF16 precision for BF16 models.\n"
-            '* "half" for FP16. Recommended for AWQ quantization.\n'
-            '* "float16" is the same as "half".\n'
-            '* "bfloat16" for a balance between precision and range.\n'
-            '* "float" is shorthand for FP32 precision.\n'
-            '* "float32" for FP32 precision.',
+                 '* "auto" will use FP16 precision for FP32 and FP16 models, and '
+                 "BF16 precision for BF16 models.\n"
+                 '* "half" for FP16. Recommended for AWQ quantization.\n'
+                 '* "float16" is the same as "half".\n'
+                 '* "bfloat16" for a balance between precision and range.\n'
+                 '* "float" is shorthand for FP32 precision.\n'
+                 '* "float32" for FP32 precision.',
         )
         parser.add_argument(
             "--kv-cache-dtype",
@@ -552,9 +552,9 @@ class ServerArgs:
             type=nullable_str,
             default=None,
             help="Path to the JSON file containing the KV cache "
-            "scaling factors. This should generally be supplied, when "
-            "KV cache dtype is FP8. Otherwise, KV cache scaling factors "
-            "default to 1.0, which may cause accuracy issues. ",
+                 "scaling factors. This should generally be supplied, when "
+                 "KV cache dtype is FP8. Otherwise, KV cache scaling factors "
+                 "default to 1.0, which may cause accuracy issues. ",
         )
         parser.add_argument(
             "--context-length",
@@ -596,8 +596,8 @@ class ServerArgs:
             type=str,
             default=None,
             help="The specific model version to use. It can be a branch "
-            "name, a tag name, or a commit id. If unspecified, will use "
-            "the default version.",
+                 "name, a tag name, or a commit id. If unspecified, will use "
+                 "the default version.",
         )
         # Memory and scheduling
         parser.add_argument(
@@ -617,7 +617,7 @@ class ServerArgs:
             type=int,
             default=ServerArgs.max_total_tokens,
             help="The maximum number of tokens in the memory pool. If not specified, it will be automatically calculated based on the memory usage fraction. "
-            "This option is typically used for development and debugging purposes.",
+                 "This option is typically used for development and debugging purposes.",
         )
         parser.add_argument(
             "--chunked-prefill-size",
@@ -1046,9 +1046,10 @@ class ServerArgs:
             help="Enabling expert parallelism for moe. The ep size is equal to the tp size.",
         )
         parser.add_argument(
-            "--enable-expert-distribution-recorder",
-            action="store_true",
-            help="Enable expert distribution recorder",
+            "--expert-distribution-recorder-mode",
+            type=str,
+            default=ServerArgs.expert_distribution_recorder_mode,
+            help="Expert distribution recorder mode (TODO doc)",
         )
         parser.add_argument(
             "--enable-torch-compile",
@@ -1093,7 +1094,7 @@ class ServerArgs:
             "--triton-attention-reduce-in-fp32",
             action="store_true",
             help="Cast the intermidiate attention results to fp32 to avoid possible crashes related to fp16."
-            "This only affects Triton attention kernels.",
+                 "This only affects Triton attention kernels.",
         )
         parser.add_argument(
             "--triton-attention-num-kv-splits",
@@ -1106,8 +1107,8 @@ class ServerArgs:
             type=int,
             default=ServerArgs.num_continuous_decode_steps,
             help="Run multiple continuous decoding steps to reduce scheduling overhead. "
-            "This can potentially increase throughput but may also increase time-to-first-token latency. "
-            "The default value is 1, meaning only run one decoding step at a time.",
+                 "This can potentially increase throughput but may also increase time-to-first-token latency. "
+                 "The default value is 1, meaning only run one decoding step at a time.",
         )
         parser.add_argument(
             "--delete-ckpt-after-loading",
@@ -1213,7 +1214,7 @@ class ServerArgs:
             type=int,
             default=0,
             help="The number of shared_experts need to be replica to fuse with normal experts in deepseek v3/r1 "
-            "we use tp_size by default.",
+                 "we use tp_size by default.",
         )
         parser.add_argument(
             "--disable-shared-experts-fusion",
@@ -1242,7 +1243,7 @@ class ServerArgs:
             type=str,
             required=False,
             help="Specify custom warmup functions (csv) to run before server starts eg. --warmups=warmup_name1,warmup_name2 "
-            "will run the functions `warmup_name1` and `warmup_name2` specified in warmup.py before the server starts listening for requests",
+                 "will run the functions `warmup_name1` and `warmup_name2` specified in warmup.py before the server starts listening for requests",
         )
 
         # Debug tensor dumps
