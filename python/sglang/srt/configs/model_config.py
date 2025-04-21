fc96b00040a5cc3e1e2d0d8360971ad6a1c73bf2
Yineng Zhang <me@zhyncs.com>
2025-04-20 17:00:20 -0700

    upd

--- python/sglang/srt/configs/model_config.py
+++ python/sglang/srt/configs/model_config.py
@@ -75,7 +75,7 @@ class ModelConfig:
         if enable_multimodal is None:
             if self.hf_config.architectures[0] == "Llama4ForConditionalGeneration":
                 enable_multimodal = False
-                logger.warning(
+                logger.info(
                     "Multimodal is disabled for Llama4. To enable it, set --enable-llama4-multimodal."
                 )
             else:
