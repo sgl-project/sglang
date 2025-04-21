80fa5dfc6b01cf6a8e9137046d34e9531ed027af
fzyzcjy <ch271828n@outlook.com>
2025-04-21 17:53:59 +0800

    more

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -464,11 +464,11 @@ class DeepseekV2MoE(nn.Module):
 
     def _forward_tbo_op_mlp(self, state):
         state.expert_output_hidden_states = self.experts(
-            hidden_states=state.hidden_states_from_dispatch,
-            reorder_topk_ids=state.reorder_topk_ids_from_dispatch,
-            seg_indptr=state.seg_indptr_from_dispatch,
-            masked_m=state.masked_m_from_dispatch,
-            expected_m=state.expected_m_from_dispatch,
+            hidden_states=state.pop("hidden_states_from_dispatch"),
+            reorder_topk_ids=state.pop("reorder_topk_ids_from_dispatch"),
+            seg_indptr=state.pop("seg_indptr_from_dispatch"),
+            masked_m=state.pop("masked_m_from_dispatch"),
+            expected_m=state.pop("expected_m_from_dispatch"),
             forward_mode=state.forward_batch.forward_mode,
         )
 
