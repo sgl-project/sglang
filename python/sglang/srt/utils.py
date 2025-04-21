a2564c0d55318279c85b7c0eadf46ab098c0ac4f
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-04-21 14:11:29 +0800

    Tiny add warning when cannot recognize bool env var (#5348)

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -78,10 +78,24 @@ time_infos = {}
 
 HIP_FP8_E4M3_FNUZ_MAX = 224.0
 
+_warned_bool_env_var_keys = set()
+
 
 def get_bool_env_var(name: str, default: str = "false") -> bool:
     value = os.getenv(name, default)
-    return value.lower() in ("true", "1")
+    value = value.lower()
+
+    truthy_values = ("true", "1")
+    falsy_values = ("false", "0")
+
+    if (value not in truthy_values) and (value not in falsy_values):
+        if value not in _warned_bool_env_var_keys:
+            logger.warning(
+                f"get_bool_env_var({name}) see non-understandable value={value} and treat as false"
+            )
+        _warned_bool_env_var_keys.add(value)
+
+    return value in truthy_values
 
 
 # https://pytorch.org/docs/stable/notes/hip.html#checking-for-hip
