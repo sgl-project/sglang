d365cfdd786fcb315bcc6aa352c1aa87f79a72a0
hebiao064 <hebiaobuaa@gmail.com>
2025-04-22 17:36:08 +0000

    fix

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -1101,6 +1101,8 @@ def get_amdgpu_memory_capacity():
 
 
 def get_device_sm():
+    # print python interpreter path
+    print(f"python interpreter path: {sys.executable}")
     print(f"torch.cuda.is_available(): {torch.cuda.is_available()}")
     if torch.cuda.is_available():
         major, minor = torch.cuda.get_device_capability()
