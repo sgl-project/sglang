4eba6e38a7ca8f32f0ae8a4c5543dbb5b23a517f
fzyzcjy <ch271828n@outlook.com>
2025-04-23 15:23:48 +0800

    more

--- python/sglang/srt/managers/expert_distribution.py
+++ python/sglang/srt/managers/expert_distribution.py
@@ -624,7 +624,7 @@ class _StatAndUtilizationRateAccumulator(_StatAccumulator):
                 f"forward_pass_id={forward_pass_id} "
                 f"current_pass_value={utilization_rate:.03f} "
                 f"{''.join(f'last_{size}_value={value:.03f} ' for size, value in self._history.mean().items())} "
-                f"gpu_physical_count_sum={gpu_physical_count_sum}"
+                f"gpu_physical_count_sum={gpu_physical_count_sum} "
                 f"current_pass_per_layer={utilization_rate_tensor.cpu().tolist()}"
             )
 
