b689283705ae3319df883554ebae90fd55f34632
Yang Wang <yang3.wang@intel.com>
2025-04-23 02:44:02 +0000

    add type hint

--- python/sglang/srt/layers/attention/hpu_attn_backend.py
+++ python/sglang/srt/layers/attention/hpu_attn_backend.py
@@ -51,9 +51,9 @@ class HPUAttnBackend(AttentionBackend):
 
     def forward_extend(
         self,
-        q,
-        k,
-        v,
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
         layer: RadixAttention,
         forward_batch: ForwardBatch,
         save_kv_cache=True,
@@ -88,9 +88,9 @@ class HPUAttnBackend(AttentionBackend):
 
     def forward_decode(
         self,
-        q,
-        k,
-        v,
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
         layer: RadixAttention,
         forward_batch: ForwardBatch,
         save_kv_cache=True,
