edd48bf2f35e1de09d95497d06f02887ca85ab01
fzyzcjy <ch271828n@outlook.com>
2025-04-24 21:53:59 +0800

    more

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1059,7 +1059,7 @@ class Scheduler(
             f"#cached-token: {adder.log_hit_tokens}, "
             f"token usage: {num_used / self.max_total_num_tokens:.2f}, "
             # NOTE MODIFIED
-            f"input throughput (token/s): {self.last_input_throughput:.2f}, "
+            f"gap_latency: {gap_latency:.2f}, "
             f"#running-req: {running_bs}, "
             f"#queue-req: {len(self.waiting_queue)}, "
         )
