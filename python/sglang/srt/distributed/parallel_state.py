5bd30dd3b4f5024b78580743fb2de560a37b928a
Edenzzzz <wtan45@wisc.edu>
2025-04-25 14:03:45 +0000

    fix

--- python/sglang/srt/distributed/parallel_state.py
+++ python/sglang/srt/distributed/parallel_state.py
@@ -1055,7 +1055,9 @@ def init_distributed_environment(
             world_size=world_size,
             rank=rank,
             timeout=timeout,
-            device_id=f"cuda:{torch.cuda.current_device()}",  # Allow NCCL to eagerly init communicator
+            device_id=torch.device(
+                f"cuda:{torch.cuda.current_device()}"
+            ),  # Allow NCCL to eagerly init communicator
         )
 
     # set the local rank
