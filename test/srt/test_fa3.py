414f3115265e51203fd1b5f6ff21af827900bd37
hebiao064 <hebiaobuaa@gmail.com>
2025-04-25 23:33:54 +0000

    adjust context len for llama 4 to avoid oom

--- test/srt/test_fa3.py
+++ test/srt/test_fa3.py
@@ -136,7 +136,8 @@ class TestFlashAttention3LocalAttn(BaseFlashAttentionTest):
     @classmethod
     def get_server_args(cls):
         cloned_args = DEFAULT_SERVER_ARGS.copy()
-        cloned_args.extend(["--tp", "4"])
+        # we cannot use scout's 10m context due to this bug: https://github.com/sgl-project/sglang/issues/5755
+        cloned_args.extend(["--tp", "4", "--context-length", "1000000"])
         return cloned_args
 
 
