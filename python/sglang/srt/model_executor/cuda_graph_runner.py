21514ff5bdad2ae598bf3070d3a583aa4fa35ae7
Lianmin Zheng <lianminzheng@gmail.com>
2025-04-25 15:54:39 -0700

    Disable flaky eagle tests (#5753)

--- python/sglang/srt/model_executor/cuda_graph_runner.py
+++ python/sglang/srt/model_executor/cuda_graph_runner.py
@@ -279,9 +279,9 @@ class CudaGraphRunner:
                 f"Capture cuda graph failed: {e}\n"
                 "Possible solutions:\n"
                 "1. set --mem-fraction-static to a smaller value (e.g., 0.8 or 0.7)\n"
-                "2. set --cuda-graph-max-bs to a smaller value (e.g., 32)\n"
+                "2. set --cuda-graph-max-bs to a smaller value (e.g., 16)\n"
                 "3. disable torch compile by not using --enable-torch-compile\n"
-                "4. disable cuda graph by --disable-cuda-graph\n"
+                "4. disable cuda graph by --disable-cuda-graph. (Not recommonded. Huge perf loss)\n"
                 "Open an issue on GitHub https://github.com/sgl-project/sglang/issues/new/choose \n"
             )
 
