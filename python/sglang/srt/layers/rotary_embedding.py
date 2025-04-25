2971beb293693670efa459f392429081fb85c026
ispobock <ispobaoke@163.com>
2025-04-25 02:55:42 +0000

    update rotary logic

--- python/sglang/srt/layers/rotary_embedding.py
+++ python/sglang/srt/layers/rotary_embedding.py
@@ -665,6 +665,10 @@ class DeepseekScalingRotaryEmbedding(RotaryEmbedding):
         offsets: Optional[torch.Tensor] = None,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """PyTorch-native implementation equivalent to forward()."""
+        if not query.is_contiguous():
+            query = query.contiguous()
+        if not key.is_contiguous():
+            key = key.contiguous()
         dtype = query.dtype
         query_rot = query[..., : self.rotary_dim]
         key_rot = key[..., : self.rotary_dim]
