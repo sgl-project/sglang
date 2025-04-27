c9b1d22808270fda7d4550b0632686e24eff03a4
fzyzcjy <ch271828n@outlook.com>
2025-04-27 11:57:26 +0800

    more

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -2046,8 +2046,7 @@ class Scheduler(
             # The caller will be notified when reaching profiler_target_forward_ct
         else:
             self.profiler_target_forward_ct = None
-            # TODO fix?
-            # return ProfileReqOutput(success=True, message="Succeeded")
+            return ProfileReqOutput(success=True, message="Succeeded")
 
     def stop_profile(self) -> None:
         if self.profiler_activities is None:
