272404dd43dd35ec93ae4a910ccccf735e7db244
saienduri <saimanas.enduri@amd.com>
2025-04-29 08:35:15 -0700

    debug log

--- test/srt/test_nightly_gsm8k_eval_amd.py
+++ test/srt/test_nightly_gsm8k_eval_amd.py
@@ -38,18 +38,29 @@ failing_models = {
     "google/gemma-2-27b-it",
     "neuralmagic/DeepSeek-Coder-V2-Lite-Instruct-FP8",
     "neuralmagic/gemma-2-2b-it-FP8",
-    "neuralmagic/Mixtral-8x7B-Instruct-v0.1-FP8"
+    "neuralmagic/Mixtral-8x7B-Instruct-v0.1-FP8",
 }
+
 
 def remove_failing_models(model_str):
     models = model_str.split(",")
     filtered = [m for m in models if m not in failing_models]
     return ",".join(filtered)
 
-DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_TP1 = remove_failing_models(DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_TP1)
-DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_TP2 = remove_failing_models(DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_TP2)
-DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_FP8_TP1 = remove_failing_models(DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_FP8_TP1)
-DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_FP8_TP2 = remove_failing_models(DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_FP8_TP2)
+
+DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_TP1 = remove_failing_models(
+    DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_TP1
+)
+DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_TP2 = remove_failing_models(
+    DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_TP2
+)
+DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_FP8_TP1 = remove_failing_models(
+    DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_FP8_TP1
+)
+DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_FP8_TP2 = remove_failing_models(
+    DEFAULT_MODEL_NAME_FOR_NIGHTLY_EVAL_FP8_TP2
+)
+
 
 def parse_models(model_string):
     return [model.strip() for model in model_string.split(",") if model.strip()]
@@ -141,6 +152,11 @@ class TestNightlyGsm8KEval(unittest.TestCase):
         )
         is_first = True
         all_results = []
+
+        if is_in_ci():
+            print("IN CI IS TRUE")
+        else:
+            print("IN CI IS FALSE")
 
         for model_group, is_fp8, is_tp2 in self.model_groups:
             for model in model_group:
