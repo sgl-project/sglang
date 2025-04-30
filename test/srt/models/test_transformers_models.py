d123cdfe7a4284e89e868d243eb054390dfcf304
Marc Sun <marc@huggingface.co>
2025-04-30 20:44:00 +0200

    update

--- test/srt/models/test_transformers_models.py
+++ test/srt/models/test_transformers_models.py
@@ -3,7 +3,6 @@ import unittest
 from types import SimpleNamespace
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
     DEFAULT_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -49,13 +48,12 @@ class TestTransformersFallbackEndpoint(CustomTestCase):
             num_examples=64,
             num_threads=32,
         )
-
+        from sglang.test.run_eval import run_eval
         metrics = run_eval(args)
         self.assertGreaterEqual(metrics["score"], 0.65)
 
     def test_gsm8k(self):
         args = SimpleNamespace(
-            base_url=self.base_url,
             num_shots=5,
             data_path=None,
             num_questions=200,
@@ -64,6 +62,7 @@ class TestTransformersFallbackEndpoint(CustomTestCase):
             host="http://127.0.0.1",
             port=int(self.base_url.split(":")[-1]),
         )
+        from sglang.test.few_shot_gsm8k import run_eval
         metrics = run_eval(args)
         print(f"{metrics=}")
         self.assertGreater(metrics["accuracy"], 0.78)
