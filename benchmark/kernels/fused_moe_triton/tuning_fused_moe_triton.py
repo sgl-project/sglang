4eea281dc3245c992e412379fc51dd0a990b92ee
fzyzcjy <ch271828n@outlook.com>
2025-05-06 14:22:52 +0800

    fmt

--- benchmark/kernels/fused_moe_triton/tuning_fused_moe_triton.py
+++ benchmark/kernels/fused_moe_triton/tuning_fused_moe_triton.py
@@ -9,6 +9,8 @@ import ray
 import torch
 import triton
 from ray.experimental.tqdm_ray import tqdm
+from transformers import AutoConfig
+
 from sglang.srt.layers.moe.fused_moe_triton.fused_moe import (
     fused_moe,
     get_config_dtype_str,
@@ -17,7 +19,6 @@ from sglang.srt.layers.moe.fused_moe_triton.fused_moe import (
     get_moe_configs,
 )
 from sglang.srt.utils import is_hip
-from transformers import AutoConfig
 
 _is_hip = is_hip()
 
