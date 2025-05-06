14cfed0d5757616a2dea6e6ad0343d7d9a1b7416
Qiaolin-Yu <liin1211@outlook.com>
2025-05-06 22:01:52 +0000

    fix ci

--- sgl-kernel/build.sh
+++ sgl-kernel/build.sh
@@ -8,11 +8,15 @@ PYTHON_ROOT_PATH=/opt/python/cp${PYTHON_VERSION//.}-cp${PYTHON_VERSION//.}
 ARCH=$(uname -i)
 echo "ARCH:  $ARCH"
 if [ ${ARCH} = "aarch64" ]; then
-   BUILDER_NAME="pytorch/manylinuxaarch64-builder"
    LIBCUDA_ARCH="sbsa"
+   BUILDER_NAME="pytorch/manylinuxaarch64-builder"
 else
-   BUILDER_NAME="pytorch/manylinux-builder"
    LIBCUDA_ARCH=${ARCH}
+   if [ ${CUDA_VERSION} = "12.8" ]; then
+      BUILDER_NAME="pytorch/manylinux2_28-builder"
+   else
+      BUILDER_NAME="pytorch/manylinux-builder"
+   fi
 fi
 
 if [ ${CUDA_VERSION} = "12.8" ]; then
