4cee26e95adffa160cec89716316a263bca016aa
woodx9 <woodx9@163.com>
2025-05-06 22:22:32 +0800

    add roberta cls model

--- python/sglang/srt/configs/model_config.py
+++ python/sglang/srt/configs/model_config.py
@@ -553,6 +553,8 @@ def is_generation_model(model_architectures: List[str], is_embedding: bool = Fal
         or "BertModel" in model_architectures
         or "Contriever" in model_architectures
         or "BertForSequenceClassification" in model_architectures
+        or "XLMRobertaModel" in model_architectures
+        or "XLMRobertaForSequenceClassification" in model_architectures
     ):
         return False
     else:
