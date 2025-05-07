ce585799ccdda784f126053c451907b4f709e3d8
Song Zhang <70674731+botieking98@users.noreply.github.com>
2025-05-07 11:32:53 +0800

    [Feature] Support for Ascend NPU backend (#3853)
    Signed-off-by: Song Zhang <gepin.zs@antgroup.com>
    Co-authored-by: 22dimensions <waitingwind@foxmail.com>

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -556,7 +556,7 @@ class ServerArgs:
             "--device",
             type=str,
             default=ServerArgs.device,
-            help="The device to use ('cuda', 'xpu', 'hpu', 'cpu'). Defaults to auto-detection if not specified.",
+            help="The device to use ('cuda', 'xpu', 'hpu', 'npu', 'cpu'). Defaults to auto-detection if not specified.",
         )
         parser.add_argument(
             "--served-model-name",
