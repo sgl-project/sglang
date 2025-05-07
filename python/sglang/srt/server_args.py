35940cf78daa2f182dd885f4eb7a23b16b3d689f
Zhiyu Cheng <zhiyuc@nvidia.com>
2025-05-07 07:54:55 +0000

    specify modelopt_fp8, add modelopt_quant in ServerArgs

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -20,7 +20,7 @@ import logging
 import os
 import random
 import tempfile
-from typing import List, Literal, Optional
+from typing import Dict, List, Literal, Optional, Union
 
 from sglang.srt.hf_transformers_utils import check_gguf_file, get_config
 from sglang.srt.reasoning_parser import ReasoningParser
@@ -51,6 +51,7 @@ class ServerArgs:
     dtype: str = "auto"
     kv_cache_dtype: str = "auto"
     quantization: Optional[str] = None
+    modelopt_quant: Optional[Union[str, Dict]] = None
     quantization_param_path: Optional[str] = None
     context_length: Optional[int] = None
     device: Optional[str] = None
@@ -633,11 +634,8 @@ class ServerArgs:
                 "awq_marlin",
                 "bitsandbytes",
                 "gguf",
-                "modelopt_fp8",  # TODO: set default for fp8 quantization, use fp8_e4m3
+                "modelopt_fp8",  # TODO: set default for modelopt quantization,
                 "modelopt_fp4",
-                "modelopt_int4_awq",  # TODO: add support in sglang runtime
-                "modelopt_w4a8_awq",  # TODO: add support in sglang runtime
-                "modelopt_fp4_awq",  # TODO: add support in sglang runtime
                 "w8a8_int8",
                 "w8a8_fp8",
                 "moe_wna16",
