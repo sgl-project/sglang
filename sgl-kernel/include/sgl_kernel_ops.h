38a5237028be45eb34b3b59c336905472a649735
Pavani Majety <pmajety@nvidia.com>
2025-05-07 11:13:49 -0700

    Initial FP4 MoE commit
    Signed-off-by: Pavani Majety <pmajety@nvidia.com>

--- sgl-kernel/include/sgl_kernel_ops.h
+++ sgl-kernel/include/sgl_kernel_ops.h
@@ -240,6 +240,26 @@ void prepare_moe_input(
     const int64_t n,
     const int64_t k);
 
+void cutlass_fp4_group_mm(
+    torch::Tensor& output,
+    const torch::Tensor& a,
+    const torch::Tensor& b,
+    const torch::Tensor& a_blockscale,
+    const torch::Tensor& b_blockscales,
+    const torch::Tensor& alphas,
+    const torch::Tensor& problem_sizes,
+    const torch::Tensor& expert_offsets,
+    const torch::Tensor& sf_offsets);
+
+void scaled_fp4_experts_quant(
+    torch::Tensor& output,
+    torch::Tensor& output_scale,
+    torch::Tensor const& input,
+    torch::Tensor const& input_global_scale,
+    torch::Tensor const& input_offset_by_experts,
+    torch::Tensor const& output_scale_offset_by_experts);
+
+
 /*
  * From csrc/speculative
  */
