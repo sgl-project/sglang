f0d160d0a25786b24037c7a906cf7f6d52ddc7b4
quinnrong <quinnrong@tencent.com>
2025-05-08 14:33:49 +0800

    fix flasinfer mla kv cache dtype

--- python/sglang/srt/layers/attention/flashinfer_mla_backend.py
+++ python/sglang/srt/layers/attention/flashinfer_mla_backend.py
@@ -364,7 +364,7 @@ class FlashInferMLAAttnBackend(AttentionBackend):
             )
         else:
             # mla paged prefill
-            k_buf = forward_batch.token_to_kv_pool.get_key_buffer(layer.layer_id).to(torch.bfloat16)
+            k_buf = forward_batch.token_to_kv_pool.get_key_buffer(layer.layer_id).to(q.dtype)
             o = prefill_wrapper_paged.run(
                 qall[:, :, : layer.v_head_dim],
                 qall[:, :, layer.v_head_dim :],
@@ -398,8 +398,7 @@ class FlashInferMLAAttnBackend(AttentionBackend):
 
         # Reshape inputs
         reshaped_q = q.view(-1, layer.tp_q_head_num, layer.head_dim)
-        k_buffer = forward_batch.token_to_kv_pool.get_key_buffer(layer.layer_id)
-        reshaped_k = k_buffer.view(-1, 1, layer.head_dim).to(torch.bfloat16)
+        k_buffer = forward_batch.token_to_kv_pool.get_key_buffer(layer.layer_id).to(q.dtype)
         # Direct call to run without the wrapper
         o = decode_wrapper.run(
             reshaped_q[:, :, : layer.v_head_dim],
