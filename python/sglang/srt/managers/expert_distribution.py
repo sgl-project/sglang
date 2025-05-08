1dc624e470f55fd39614f1cadc3c08b4017499dd
fzyzcjy <ch271828n@outlook.com>
2025-05-08 09:42:40 +0800

    more

--- python/sglang/srt/managers/expert_distribution.py
+++ python/sglang/srt/managers/expert_distribution.py
@@ -317,6 +317,7 @@ class _DetailSinglePassGatherer(_SinglePassGatherer):
     def on_forward_pass_start(self, forward_batch: ForwardBatch):
         assert self._metadata is None
         self._metadata = dict(
+            rids=forward_batch.rids,
             input_ids=forward_batch.input_ids.cpu().tolist(),
             positions=forward_batch.positions.cpu().tolist(),
             extend_seq_lens=forward_batch.extend_seq_lens_cpu,
