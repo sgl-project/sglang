d81cb67313fe8b55aa4c2cebed044e030c6d82b2
fzyzcjy <ch271828n@outlook.com>
2025-05-09 07:59:30 +0800

    more

--- python/sglang/srt/memory_saver_tensors.py
+++ python/sglang/srt/memory_saver_tensors.py
@@ -4,21 +4,6 @@ from torch.utils._pytree import tree_map
 
 class _WrapperTensor(torch.Tensor):
     @classmethod
-    def __new__(cls, inner: torch.Tensor):
-        r = torch.Tensor._make_wrapper_subclass(
-            cls,
-            inner.shape,
-            strides=inner.stride(),
-            storage_offset=inner.storage_offset(),
-            dtype=inner.dtype,
-            device=inner.device,
-            layout=inner.layout,
-            requires_grad=inner.requires_grad,
-        )
-        r._inner = inner
-        return r
-
-    @classmethod
     def __torch_dispatch__(cls, func, types, args=(), kwargs=None):
         def unwrap(e):
             if isinstance(e, cls):
@@ -33,6 +18,21 @@ class _WrapperTensor(torch.Tensor):
 
 
 class DisposableTensor(_WrapperTensor):
+    @classmethod
+    def __new__(cls, inner: torch.Tensor):
+        r = torch.Tensor._make_wrapper_subclass(
+            cls,
+            inner.shape,
+            strides=inner.stride(),
+            storage_offset=inner.storage_offset(),
+            dtype=inner.dtype,
+            device=inner.device,
+            layout=inner.layout,
+            requires_grad=inner.requires_grad,
+        )
+        r._inner = inner
+        return r
+
     def _unwrap(self):
         assert self._inner is not None, "Cannot use a DisposableTensor that is already disposed"
         return self._inner
