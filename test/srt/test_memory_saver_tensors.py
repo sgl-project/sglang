f81e03f9bc4a110994c247899afb7ae35370072c
fzyzcjy <ch271828n@outlook.com>
2025-05-09 08:04:50 +0800

    more

--- test/srt/test_memory_saver_tensors.py
+++ test/srt/test_memory_saver_tensors.py
@@ -1,17 +1,19 @@
 import unittest
 
 import torch
+from python.sglang.srt.memory_saver_tensors import DisposableTensor
 
 
 class TestPPAccuracy(unittest.TestCase):
     def test_disposable_tensor(self):
-        self._common_test(TODO)
+        self._common_test(DisposableTensor(torch.tensor([3.0, 4.0, 5.0])))
 
     def test_lazy_tensor(self):
         self._common_test(TODO)
 
     def _common_test(self, x: torch.Tensor):
-        TODO
+        self.assertEqual(torch.max(x).item(), 5.0)
+        self.assertTrue(torch.allclose(x + torch.tensor([2.0, 2.0, 2.0]), torch.tensor([5.0, 6.0, 7.0])))
 
 
 if __name__ == "__main__":
