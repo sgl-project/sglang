113097f02e2246a6608cb80ac585badae3628c6f
fzyzcjy <ch271828n@outlook.com>
2025-05-09 21:28:52 +0800

    more

--- python/sglang/srt/eplb_simulator/simulator.py
+++ python/sglang/srt/eplb_simulator/simulator.py
@@ -4,6 +4,7 @@ from sglang.srt.managers.expert_distribution import (
     compute_gpu_physical_count,
     compute_utilization_rate,
 )
+from sglang.srt.managers.expert_location import ExpertLocationMetadata
 
 
 def simulate_execution_given_logical_count_of_batch(
@@ -30,3 +31,33 @@ def simulate_execution_given_logical_count_of_batch(
         mean_utilization_rate=mean_utilization_rate,
         num_simulated_batches=logical_count_of_batch.shape[0],
     )
+
+
+def simulate_logical_to_physical_by_random_dispatching(
+    logical_count_of_whatever: torch.Tensor,  # (..., num_layer, num_logical_expert)
+    logical_to_all_physical_map: torch.Tensor,  # (num_layer, num_logical_experts, X)
+    num_physical_expert: int,
+):
+    """output: (..., num_layer, num_physical_expert)"""
+    *prefix_dims, num_layer, num_logical_expert = logical_count_of_whatever.shape
+
+    physical_count_of_whatever = torch.zeros(
+        (*prefix_dims, num_layer, num_physical_expert),
+        dtype=torch.float32,
+    )
+
+    for layer_id in range(num_layer):
+        for logical_expert_id in range(num_logical_expert):
+            all_physical_expert_ids = (
+                ExpertLocationMetadata.logical_to_all_physical_raw(
+                    logical_to_all_physical_map, layer_id, logical_expert_id
+                )
+            )
+            for physical_expert_id in all_physical_expert_ids:
+                physical_count_of_whatever[
+                :, layer_id, physical_expert_id
+                ] += logical_count_of_whatever[:, layer_id, logical_expert_id] / len(
+                    all_physical_expert_ids
+                )
+
+    return physical_count_of_whatever
--- python/sglang/srt/eplb_simulator/to_be_migrated.py
+++ python/sglang/srt/eplb_simulator/to_be_migrated.py
@@ -233,35 +233,6 @@ def simulate_batching(
     )
 
 
-def simulate_logical_to_physical(
-    logical_count_of_whatever: torch.Tensor,  # (*, num_layer, num_logical_expert)
-    logical_to_all_physical_map: torch.Tensor,  # (num_layer, num_logical_experts, X)
-    num_physical_expert: int,
-):
-    """output: (*, num_layer, num_physical_expert)"""
-    num_whatever, num_layer, num_logical_expert = logical_count_of_whatever.shape
-
-    physical_count_of_whatever = torch.zeros(
-        (num_whatever, num_layer, num_physical_expert),
-        dtype=torch.float32,
-    )
-
-    for layer_id in range(num_layer):
-        for logical_expert_id in range(num_logical_expert):
-            all_physical_expert_ids = (
-                ExpertLocationMetadata.logical_to_all_physical_raw(
-                    logical_to_all_physical_map, layer_id, logical_expert_id
-                )
-            )
-            for physical_expert_id in all_physical_expert_ids:
-                physical_count_of_whatever[
-                :, layer_id, physical_expert_id
-                ] += logical_count_of_whatever[:, layer_id, logical_expert_id] / len(
-                    all_physical_expert_ids
-                )
-
-    return physical_count_of_whatever
-
 
 def compute_num_token(whatever_with_num_layer_and_num_expert: torch.Tensor):
     num_token_mul_num_experts = whatever_with_num_layer_and_num_expert[..., -1, :].sum(
