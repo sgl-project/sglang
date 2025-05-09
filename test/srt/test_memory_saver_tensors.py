a77aef3b2c40772aaa395a757f81f33c84744968
fzyzcjy <ch271828n@outlook.com>
2025-05-09 08:11:30 +0800

    temp

--- test/srt/test_memory_saver_tensors.py
+++ test/srt/test_memory_saver_tensors.py
@@ -1,3 +1,4 @@
+import gc
 import unittest
 
 import torch
@@ -11,16 +12,28 @@ _TRITON_DEVICE = triton.runtime.driver.active.get_active_torch_device()
 
 class TestMemorySaverTensors(unittest.TestCase):
     def test_disposable_tensor(self):
+        memory_before = torch.cuda.memory_allocated()
+
         x = DisposableTensor(torch.tensor([3.0, 4.0, 5.0], device=_DEVICE))
-        self._common_test(x)
+
+        self._common_test_operations(x)
+
+        x.dispose()
+
+        gc.collect()
+        torch.cuda.ipc_collect()
+        torch.cuda.empty_cache()
+        memory_after = torch.cuda.memory_allocated()
+
+        print(f"{x=}")  # ensure there are still references to the object
 
     def test_lazy_tensor(self):
         x = LazyTensor((3,), device=_DEVICE)
         x[0] = 3.0
         x[1:3] = [4.0, 5.0]
-        self._common_test(x)
+        self._common_test_operations(x)
 
-    def _common_test(self, x: torch.Tensor):
+    def _common_test_operations(self, x: torch.Tensor):
         print(f"common_test {type(x)=} {x=}")
         self.assertEqual(torch.max(x).item(), 5.0)
         self.assertTrue(torch.allclose(x + torch.tensor([2.0, 2.0, 2.0], device=_DEVICE),
