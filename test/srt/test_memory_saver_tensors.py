3216884dac9e5f771e7d15fda8b7143bfc776498
fzyzcjy <ch271828n@outlook.com>
2025-05-09 08:02:56 +0800

    more

---
+++ test/srt/test_memory_saver_tensors.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestPPAccuracy(unittest.TestCase):
+    def test_disposable_tensor(self):
+        TODO
+
+    def test_lazy_tensor(self):
+        TODO
+
+
+if __name__ == "__main__":
+    unittest.main()
