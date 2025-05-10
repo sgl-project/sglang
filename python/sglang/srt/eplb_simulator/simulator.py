2fff98e7eee9fc9a7ba448867777b71d3422a9a3
fzyzcjy <ch271828n@outlook.com>
2025-05-10 10:35:36 +0800

    more

--- python/sglang/srt/eplb_simulator/simulator.py
+++ python/sglang/srt/eplb_simulator/simulator.py
@@ -78,9 +78,7 @@ def _simulate_scheduled_pack_indices_given_seq_metadata(
 
     if phase == "decode":
         # loose bound
-        num_steps_upper_bound = math.ceil(math.ceil(
-            df_metadata["pack_end_index"].max() / num_tokens_in_batch_overall
-        ) * 2.5)
+        num_steps_upper_bound = df_metadata["pack_end_index"].max()
 
         pack_indices_of_step = torch.full(
             (num_steps_upper_bound, num_tokens_in_batch_overall), fill_value=-1, dtype=torch.int32
