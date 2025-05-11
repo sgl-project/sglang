197b44dbcb293980fbca9d79fb2268f84c48c62b
Lianmin Zheng <lianminzheng@gmail.com>
2025-05-11 02:58:26 -0700

    Fix

--- python/sglang/srt/constrained/outlines_backend.py
+++ python/sglang/srt/constrained/outlines_backend.py
@@ -170,7 +170,7 @@ class OutlinesGrammarBackend(BaseGrammarBackend):
                 whitespace_pattern=self.whitespace_pattern,
             )
         except (NotImplementedError, json.decoder.JSONDecodeError, ValueError) as e:
-            logger.warning(f"Skip invalid json_schema: json_schema={key_string}, {e=}")
+            logger.warning(f"Skip invalid json_schema: {key_string=}, {e=}")
             return None
         return self._compile_regex(regex)
 
