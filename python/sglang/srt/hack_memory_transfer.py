ccd63a1b5dbf1f26c6c82f870df33c05c94bede5
fzyzcjy <ch271828n@outlook.com>
2025-05-11 10:42:10 +0800

    more

--- python/sglang/srt/hack_memory_transfer.py
+++ python/sglang/srt/hack_memory_transfer.py
@@ -12,4 +12,8 @@ def _thread_entrypoint():
     tensor_size = 1024 ** 3
     tensor_cpu_pinned = torch.rand((tensor_size,), dtype=torch.uint8, device="cpu", pin_memory=True)
     print(f"{tensor_cpu_pinned.nbytes=}")
-    TODO
+
+    alt_stream = torch.cuda.Stream()
+    with torch.cuda.stream(alt_stream):
+        while True:
+            TODO
