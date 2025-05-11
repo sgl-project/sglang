b2f7bf0818bfdd47b933dec474f5b19ccd773c6e
fzyzcjy <ch271828n@outlook.com>
2025-05-11 10:43:34 +0800

    more

--- python/sglang/srt/hack_memory_transfer.py
+++ python/sglang/srt/hack_memory_transfer.py
@@ -1,4 +1,5 @@
 import threading
+from datetime import datetime
 
 import torch
 
@@ -14,13 +15,12 @@ def _thread_entrypoint():
         tensor_size = 1024 ** 3
         tensor_cpu_pinned = torch.rand((tensor_size,), dtype=torch.uint8, device="cpu", pin_memory=True)
         tensor_output = torch.empty((tensor_size,), dtype=torch.uint8, device="cuda")
-        print(
-            f"[memory_transfer_experiment] {tensor_cpu_pinned.nbytes=} {tensor_output.nbytes=} {tensor_output.device=}")
+        _log(f"{tensor_cpu_pinned.nbytes=} {tensor_output.nbytes=} {tensor_output.device=}")
 
         while True:
             output_tensor.copy_(input_tensor, non_blocking=True)
             TODO
 
 
-def _log():
-    TODO
+def _log(msg):
+    print(f"[memory_transfer_experiment, {datetime.now().isoformat()}] {msg}")
