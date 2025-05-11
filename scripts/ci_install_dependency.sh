d9df1732510f065757be5a75d1ca2176ad97f399
Lianmin Zheng <lianminzheng@gmail.com>
2025-05-11 10:55:06 -0700

    [CI] Reorganize the 8 gpu tests (#6192)

--- scripts/ci_install_dependency.sh
+++ scripts/ci_install_dependency.sh
@@ -5,25 +5,22 @@ set -euxo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bash "${SCRIPT_DIR}/killall_sglang.sh"
 
+# Update pip
+pip install --upgrade pip
+
 # Clean up existing installations
-pip uninstall -y flashinfer flashinfer_python sgl-kernel sglang vllm || true
+pip uninstall -y flashinfer flashinfer_python sgl-kernel sglang vllm
 pip cache purge
 rm -rf /root/.cache/flashinfer
 rm -rf /usr/local/lib/python3.10/dist-packages/flashinfer*
 rm -rf /usr/local/lib/python3.10/dist-packages/sgl_kernel*
-
-# Update pip
-pip install --upgrade pip
-
-# Install sgl-kernel
-pip install sgl-kernel==0.1.2.post1 --no-cache-dir
 
 # Install the main package
 pip install -e "python[all]"
 
 # Install additional dependencies
 pip install torch_memory_saver
-pip install transformers==4.51.0 sentence_transformers accelerate peft pandas datasets timm torchaudio==2.6.0
+pip install transformers==4.51.0 timm torchaudio==2.6.0 sentence_transformers accelerate peft pandas datasets mooncake-transfer-engine
 
 # For compiling xgrammar kernels
 pip install cuda-python nvidia-cuda-nvrtc-cu12
