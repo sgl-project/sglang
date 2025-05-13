c6c53df3d65c1b14653c28ee6bc55c38e89b5bfc
fzyzcjy <ch271828n@outlook.com>
2025-05-13 21:02:50 +0800

    more

--- python/sglang/srt/managers/eplb_manager.py
+++ python/sglang/srt/managers/eplb_manager.py
@@ -1,17 +1,10 @@
-import asyncio
 import logging
 from typing import TYPE_CHECKING
 
-import torch
-from sglang.srt.configs.model_config import ModelConfig
-from sglang.srt.managers import deepseek_eplb
 from sglang.srt.managers.expert_distribution import get_global_expert_distribution_recorder
-from sglang.srt.managers.expert_distribution_storage import ExpertDistributionStorage
 from sglang.srt.managers.expert_location import (
     ExpertLocationMetadata,
-    ModelConfigForExpertLocation,
 )
-from sglang.srt.server_args import ServerArgs
 
 if TYPE_CHECKING:
     from sglang.srt.model_executor.model_runner import ModelRunner
@@ -39,50 +32,3 @@ class EPLBManager:
         logical_count = get_global_expert_distribution_recorder().dump_record()["logical_count"]
         expert_location_metadata = ExpertLocationMetadata.init_by_eplb(self._server_args, logical_count)
         self._model_runner.update_expert_location(expert_location_metadata)
-
-
-class _TODO_REMOVE_EPLBManager:
-    def bind(self, tokenizer_manager: "TokenizerManager"):
-        self._tokenizer_manager = tokenizer_manager
-        self._expert_distribution_storage.bind(tokenizer_manager)
-
-    async def handle_loop(self):
-        await self._expert_distribution_storage.start()
-        TODO_remove_eplb_rebalance_period
-        while True:
-            sleep_time = self._server_args.eplb_rebalance_period or 1000000000
-            logger.info(
-                f"EPLBManager: Sleep {sleep_time} seconds before next automatic rebalancing"
-            )
-            await asyncio.sleep(sleep_time)
-            await self.rebalance(EplbRebalanceReqInput())
-
-    async def rebalance(self, obj: EplbRebalanceReqInput):
-        await self.save_expert_distribution()
-        expert_location_metadata = self.compute_expert_location_metadata(
-            debug_use_random_stat=obj.debug_use_random_stat
-        )
-        await self._tokenizer_manager.update_expert_location(
-            UpdateExpertLocationReqInput(
-                expert_location_metadata=expert_location_metadata
-            )
-        )
-
-    async def save_expert_distribution(self):
-        await self._expert_distribution_storage.save_current()
-
-    def compute_expert_location_metadata(self, debug_use_random_stat: bool = False):
-        snapshot = self._expert_distribution_storage.get_last_snapshot()
-        if snapshot is None:
-            return ExpertLocationMetadata.init_trivial(self._server_args)
-
-        if debug_use_random_stat:
-            logger.warning(
-                "EPLBManager.compute_expert_location_metadata use random stat for debugging."
-            )
-            original_logical_count = torch.tensor(snapshot["logical_count"])
-            snapshot = {
-                "logical_count": torch.randint_like(original_logical_count, high=100000)
-            }
-
-        return ExpertLocationMetadata.init_by_eplb(self._server_args, **snapshot)
