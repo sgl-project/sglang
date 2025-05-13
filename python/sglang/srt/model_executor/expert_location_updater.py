155620493c8bd68551dfc7a9bfdbd3ea53438a7a
fzyzcjy <ch271828n@outlook.com>
2025-05-13 21:10:52 +0800

    more

--- python/sglang/srt/model_executor/expert_location_updater.py
+++ python/sglang/srt/model_executor/expert_location_updater.py
@@ -21,5 +21,5 @@ def _update_expert_weights(
     old_expert_location_metadata: ExpertLocationMetadata,
     new_expert_location_metadata: ExpertLocationMetadata,
 ):
-    temp_buffers = [torch.empty_like(tensor) for tensor in routed_experts_weights_of_layer[0]]
+    temp_buffers = [torch.empty_like(tensor) for tensor in next(iter(routed_experts_weights_of_layer.values()))]
     TODO
