8d7406b5da261567f4343e00d80bcde6eeeb6fbd
fzyzcjy <ch271828n@outlook.com>
2025-05-13 10:35:00 +0800

    more

--- python/sglang/srt/layers/moe/ep_moe/kernels.py
+++ python/sglang/srt/layers/moe/ep_moe/kernels.py
@@ -658,9 +658,9 @@ def grouped_gemm_triton(
     if use_fp8_w8a8 and block_shape is None:
         assert scale_a is not None and scale_b is not None
 
-    a_original = a
-
     if block_shape is not None:
+        a_original = a
+
         assert len(block_shape) == 2
         block_n, block_k = block_shape[0], block_shape[1]
         a, scale_a = per_token_group_quant_fp8(a, block_k)
@@ -669,7 +669,7 @@ def grouped_gemm_triton(
         assert triton.cdiv(b.shape[-2], block_n) == scale_b.shape[-2]
         assert triton.cdiv(b.shape[-1], block_k) == scale_b.shape[-1]
 
-    dispose_tensor(a_original)
+        dispose_tensor(a_original)
 
     # TODO: adjust config or tune kernel
     # Reduce block size to prevent L40 shared memory overflow.
