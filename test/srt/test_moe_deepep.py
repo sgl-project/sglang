04a78e9389034f8e6b8e34a68695ee9084f08b2e
fzyzcjy <ch271828n@outlook.com>
2025-05-13 14:22:13 +0800

    fmt

--- test/srt/test_moe_deepep.py
+++ test/srt/test_moe_deepep.py
@@ -71,22 +71,24 @@ class TestDPAttn(unittest.TestCase):
                 "--disable-cuda-graph",
                 # Test custom config
                 "--deepep-config",
-                json.dumps({
-                    "normal_dispatch": {
-                        "num_sms": 20,
-                        "num_max_nvl_chunked_send_tokens": 16,
-                        "num_max_nvl_chunked_recv_tokens": 256,
-                        "num_max_rdma_chunked_send_tokens": 6,
-                        "num_max_rdma_chunked_recv_tokens": 128,
-                    },
-                    "normal_combine": {
-                        "num_sms": 20,
-                        "num_max_nvl_chunked_send_tokens": 6,
-                        "num_max_nvl_chunked_recv_tokens": 256,
-                        "num_max_rdma_chunked_send_tokens": 6,
-                        "num_max_rdma_chunked_recv_tokens": 128,
-                    },
-                }),
+                json.dumps(
+                    {
+                        "normal_dispatch": {
+                            "num_sms": 20,
+                            "num_max_nvl_chunked_send_tokens": 16,
+                            "num_max_nvl_chunked_recv_tokens": 256,
+                            "num_max_rdma_chunked_send_tokens": 6,
+                            "num_max_rdma_chunked_recv_tokens": 128,
+                        },
+                        "normal_combine": {
+                            "num_sms": 20,
+                            "num_max_nvl_chunked_send_tokens": 6,
+                            "num_max_nvl_chunked_recv_tokens": 256,
+                            "num_max_rdma_chunked_send_tokens": 6,
+                            "num_max_rdma_chunked_recv_tokens": 128,
+                        },
+                    }
+                ),
             ],
             env={
                 "SGL_ENABLE_JIT_DEEPGEMM": "0",
