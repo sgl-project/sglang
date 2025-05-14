5946fab3d86de8bad851b8e09b8b6c6f6a402ef9
fzyzcjy <ch271828n@outlook.com>
2025-05-14 10:26:52 +0800

    more

--- test/srt/test_expert_location_updater.py
+++ test/srt/test_expert_location_updater.py
@@ -11,6 +11,10 @@ from tqdm import tqdm
 
 
 class TestExpertLocationUpdater(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        mp.set_start_method("spawn", force=True)
+
     def test_cpu(self):
         self._test_core(num_gpus=32, nnodes=4, num_logical_experts=256, num_physical_experts=288, device="cpu")
         self._test_core(num_gpus=144, nnodes=18, num_logical_experts=256, num_physical_experts=288, device="cpu")
