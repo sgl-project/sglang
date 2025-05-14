96313c440705bb8c48a8deb08e2fb7de1339b03d
fzyzcjy <ch271828n@outlook.com>
2025-05-14 10:35:56 +0800

    more

--- test/srt/test_expert_location_updater.py
+++ test/srt/test_expert_location_updater.py
@@ -71,7 +71,8 @@ def _run_subprocess(
 ):
     try:
         torch.random.manual_seed(42)
-        torch.distributed.init_process_group(rank=rank, world_size=num_gpus)
+        torch.distributed.init_process_group(rank=rank, world_size=num_gpus,
+                                             backend={"cpu": "gloo", "gpu": "nccl"}[device])
         if device == "cuda":
             torch.cuda.set_device(f"cuda:{rank}")
 
