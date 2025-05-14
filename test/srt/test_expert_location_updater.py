e9a2902b995645bb6236c5fe0244b89fca8a5c31
fzyzcjy <ch271828n@outlook.com>
2025-05-14 11:50:26 +0800

    more

--- test/srt/test_expert_location_updater.py
+++ test/srt/test_expert_location_updater.py
@@ -49,28 +49,15 @@ class TestExpertLocationUpdater(CustomTestCase):
     def _test_common(self, device):
         infos = []
 
-        # TODO
-        # TODO
-        # TODO
-        for nnodes in [4]:
-            for num_logical_experts in [2]:
-                for num_physical_experts in [256]:
+        for nnodes in [1, 2, 4]:
+            for num_logical_experts in [2, 5, 20, 200]:
+                for num_physical_experts in [8, 16, 256]:
                     if num_logical_experts > num_physical_experts: continue
                     infos.append(_TestInfo(
                         nnodes=nnodes,
                         num_logical_experts=num_logical_experts,
                         num_physical_experts=num_physical_experts,
                     ))
-
-        # for nnodes in [1, 2, 4]:
-        #     for num_logical_experts in [2, 5, 20, 200]:
-        #         for num_physical_experts in [8, 16, 256]:
-        #             if num_logical_experts > num_physical_experts: continue
-        #             infos.append(_TestInfo(
-        #                 nnodes=nnodes,
-        #                 num_logical_experts=num_logical_experts,
-        #                 num_physical_experts=num_physical_experts,
-        #             ))
 
         self._test_core(num_gpus=8, device=device, infos=infos)
 
