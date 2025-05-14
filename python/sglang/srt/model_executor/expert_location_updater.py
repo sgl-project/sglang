a297edd5d5055d385a60004de9f8f0e2dd08d3d2
fzyzcjy <ch271828n@outlook.com>
2025-05-14 08:56:15 +0800

    more

--- python/sglang/srt/model_executor/expert_location_updater.py
+++ python/sglang/srt/model_executor/expert_location_updater.py
@@ -109,6 +109,13 @@ def update_expert_weights_single_layer(
         all_src_nodes = [x // num_gpu_per_node for x in all_src_ranks]
         self_node_src_ranks = [x for x in all_src_ranks if x // num_gpu_per_node == self_node_id]
 
+        need_p2p_dst_ranks = _deduplicate_ordered([
+            x // num_local_physical_experts
+            for x in range(num_physical_experts)
+            if new_physical_to_logical_map[x] == logical_expert_id
+            and x // num_local_physical_experts not in all_src_ranks
+        ])
+
         # case 4: same-node
         TODO
 
