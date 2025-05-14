dccfd4007388185fad71432974714db12894b418
quinnrong <quinnrong@tencent.com>
2025-05-14 19:36:47 +0800

    update avg_spec_accept_length

--- test/srt/test_flashmla.py
+++ test/srt/test_flashmla.py
@@ -142,9 +142,12 @@ class TestFlashMLAMTP(CustomTestCase):
 
         server_info = requests.get(self.base_url + "/get_server_info")
         print(f"{server_info=}")
+        avg_spec_accept_length = server_info.json()["internal_states"][0][
+            "avg_spec_accept_length"
+        ]
         avg_spec_accept_length = server_info.json()["avg_spec_accept_length"]
         print(f"{avg_spec_accept_length=}")
-        self.assertGreater(avg_spec_accept_length, 2.5)
+        self.assertGreater(avg_spec_accept_length, 1.8)
 
 
 if __name__ == "__main__":
