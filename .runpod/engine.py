d5e93ea5bc372d388e0954ce982f1650f4e46a35
Tim Pietrusky <timpietrusky@gmail.com>
2025-05-14 11:48:39 +0200

    feat: making sure that sglang is working as a worker

--- .runpod/engine.py
+++ .runpod/engine.py
@@ -6,61 +6,82 @@ import asyncio
 import aiohttp
 import os
 
+
 class SGlangEngine:
-    def __init__(self, model=os.getenv('MODEL_NAME'), host="0.0.0.0", port=30000):
+    def __init__(
+        self,
+        model=os.getenv("MODEL_NAME"),
+        host=os.getenv("HOST", "0.0.0.0"),
+        port=int(os.getenv("PORT", 30000)),
+    ):
         self.model = model
         self.host = host
         self.port = port
-        self.base_url = f"http://{host}:{port}"
+        self.base_url = f"http://{self.host}:{self.port}"
         self.process = None
-        
+
     def start_server(self):
         command = [
-            "python3", "-m", "sglang.launch_server",
-            "--host", self.host,
-            "--port", str(self.port)
+            "python3",
+            "-m",
+            "sglang.launch_server",
+            "--host",
+            self.host,
+            "--port",
+            str(self.port),
         ]
 
         # Dictionary of all possible options and their corresponding env var names
         options = {
-            'MODEL_NAME': '--model-path',
-            'TOKENIZER_PATH': '--tokenizer-path',
-            'HOST': '--host',
-            'PORT': '--port',
-            'ADDITIONAL_PORTS': '--additional-ports',
-            'TOKENIZER_MODE': '--tokenizer-mode',
-            'LOAD_FORMAT': '--load-format',
-            'DTYPE': '--dtype',
-            'CONTEXT_LENGTH': '--context-length',
-            'QUANTIZATION': '--quantization',
-            'SERVED_MODEL_NAME': '--served-model-name',
-            'CHAT_TEMPLATE': '--chat-template',
-            'MEM_FRACTION_STATIC': '--mem-fraction-static',
-            'MAX_RUNNING_REQUESTS': '--max-running-requests',
-            'MAX_NUM_REQS': '--max-num-reqs',
-            'MAX_TOTAL_TOKENS': '--max-total-tokens',
-            'CHUNKED_PREFILL_SIZE': '--chunked-prefill-size',
-            'MAX_PREFILL_TOKENS': '--max-prefill-tokens',
-            'SCHEDULE_POLICY': '--schedule-policy',
-            'SCHEDULE_CONSERVATIVENESS': '--schedule-conservativeness',
-            'TENSOR_PARALLEL_SIZE': '--tensor-parallel-size',
-            'STREAM_INTERVAL': '--stream-interval',
-            'RANDOM_SEED': '--random-seed',
-            'LOG_LEVEL': '--log-level',
-            'LOG_LEVEL_HTTP': '--log-level-http',
-            'API_KEY': '--api-key',
-            'FILE_STORAGE_PTH': '--file-storage-pth',
-            'DATA_PARALLEL_SIZE': '--data-parallel-size',
-            'LOAD_BALANCE_METHOD': '--load-balance-method',
+            "MODEL_NAME": "--model-path",
+            "TOKENIZER_PATH": "--tokenizer-path",
+            "HOST": "--host",
+            "ADDITIONAL_PORTS": "--additional-ports",
+            "TOKENIZER_MODE": "--tokenizer-mode",
+            "LOAD_FORMAT": "--load-format",
+            "DTYPE": "--dtype",
+            "CONTEXT_LENGTH": "--context-length",
+            "QUANTIZATION": "--quantization",
+            "SERVED_MODEL_NAME": "--served-model-name",
+            "CHAT_TEMPLATE": "--chat-template",
+            "MEM_FRACTION_STATIC": "--mem-fraction-static",
+            "MAX_RUNNING_REQUESTS": "--max-running-requests",
+            "MAX_NUM_REQS": "--max-num-reqs",
+            "MAX_TOTAL_TOKENS": "--max-total-tokens",
+            "CHUNKED_PREFILL_SIZE": "--chunked-prefill-size",
+            "MAX_PREFILL_TOKENS": "--max-prefill-tokens",
+            "SCHEDULE_POLICY": "--schedule-policy",
+            "SCHEDULE_CONSERVATIVENESS": "--schedule-conservativeness",
+            "TENSOR_PARALLEL_SIZE": "--tensor-parallel-size",
+            "STREAM_INTERVAL": "--stream-interval",
+            "RANDOM_SEED": "--random-seed",
+            "LOG_LEVEL": "--log-level",
+            "LOG_LEVEL_HTTP": "--log-level-http",
+            "API_KEY": "--api-key",
+            "FILE_STORAGE_PTH": "--file-storage-pth",
+            "DATA_PARALLEL_SIZE": "--data-parallel-size",
+            "LOAD_BALANCE_METHOD": "--load-balance-method",
+            "ATTENTION_BACKEND": "--attention-backend",
+            "SAMPLING_BACKEND": "--sampling-backend",
         }
 
         # Boolean flags
         boolean_flags = [
-            'SKIP_TOKENIZER_INIT', 'TRUST_REMOTE_CODE', 'LOG_REQUESTS',
-            'SHOW_TIME_COST', 'DISABLE_FLASHINFER', 'DISABLE_FLASHINFER_SAMPLING',
-            'DISABLE_RADIX_CACHE', 'DISABLE_REGEX_JUMP_FORWARD', 'DISABLE_CUDA_GRAPH',
-            'DISABLE_DISK_CACHE', 'ENABLE_TORCH_COMPILE', 'ENABLE_P2P_CHECK',
-            'ENABLE_MLA', 'ATTENTION_REDUCE_IN_FP32', 'EFFICIENT_WEIGHT_LOAD'
+            "SKIP_TOKENIZER_INIT",
+            "TRUST_REMOTE_CODE",
+            "LOG_REQUESTS",
+            "SHOW_TIME_COST",
+            "DISABLE_FLASHINFER",
+            "DISABLE_FLASHINFER_SAMPLING",
+            "DISABLE_RADIX_CACHE",
+            "DISABLE_REGEX_JUMP_FORWARD",
+            "DISABLE_CUDA_GRAPH",
+            "DISABLE_DISK_CACHE",
+            "ENABLE_TORCH_COMPILE",
+            "ENABLE_P2P_CHECK",
+            "ENABLE_MLA",
+            "ATTENTION_REDUCE_IN_FP32",
+            "EFFICIENT_WEIGHT_LOAD",
         ]
 
         # Add options from environment variables only if they are set
@@ -71,12 +92,12 @@ class SGlangEngine:
 
         # Add boolean flags only if they are set to true
         for flag in boolean_flags:
-            if os.getenv(flag, '').lower() in ('true', '1', 'yes'):
+            if os.getenv(flag, "").lower() in ("true", "1", "yes"):
                 command.append(f"--{flag.lower().replace('_', '-')}")
 
         self.process = subprocess.Popen(command, stdout=None, stderr=None)
         print(f"Server started with PID: {self.process.pid}")
-    
+
     def wait_for_server(self, timeout=900, interval=5):
         start_time = time.time()
         while time.time() - start_time < timeout:
@@ -96,35 +117,59 @@ class SGlangEngine:
             self.process.wait()
             print("Server shut down.")
 
+
 class OpenAIRequest:
     def __init__(self, base_url="http://0.0.0.0:30000/v1", api_key="EMPTY"):
         self.client = openai.Client(base_url=base_url, api_key=api_key)
-    
-    async def request_chat_completions(self, model="default", messages=None, max_tokens=100, stream=False, frequency_penalty=0.0, n=1, stop=None, temperature=1.0, top_p=1.0):
+
+    async def request_chat_completions(
+        self,
+        model="default",
+        messages=None,
+        max_tokens=100,
+        stream=False,
+        frequency_penalty=0.0,
+        n=1,
+        stop=None,
+        temperature=1.0,
+        top_p=1.0,
+    ):
         if messages is None:
             messages = [
                 {"role": "system", "content": "You are a helpful AI assistant"},
                 {"role": "user", "content": "List 3 countries and their capitals."},
             ]
-        
+
         response = self.client.chat.completions.create(
             model=model,
             messages=messages,
             max_tokens=max_tokens,
-            stream=stream,frequency_penalty=frequency_penalty,
+            stream=stream,
+            frequency_penalty=frequency_penalty,
             n=n,
             stop=stop,
             temperature=temperature,
-            top_p=top_p
+            top_p=top_p,
         )
-        
+
         if stream:
             async for chunk in response:
                 yield chunk.to_dict()
         else:
             yield response.to_dict()
-    
-    async def request_completions(self, model="default", prompt="The capital of France is", max_tokens=100, stream=False, frequency_penalty=0.0, n=1, stop=None, temperature=1.0, top_p=1.0):
+
+    async def request_completions(
+        self,
+        model="default",
+        prompt="The capital of France is",
+        max_tokens=100,
+        stream=False,
+        frequency_penalty=0.0,
+        n=1,
+        stop=None,
+        temperature=1.0,
+        top_p=1.0,
+    ):
         response = self.client.completions.create(
             model=model,
             prompt=prompt,
@@ -134,15 +179,15 @@ class OpenAIRequest:
             n=n,
             stop=stop,
             temperature=temperature,
-            top_p=top_p
+            top_p=top_p,
         )
-        
+
         if stream:
             async for chunk in response:
                 yield chunk.to_dict()
         else:
             yield response.to_dict()
-    
+
     async def get_models(self):
         response = await self.client.models.list()
         return response
--- .runpod/handler.py
+++ .runpod/handler.py
@@ -22,17 +22,17 @@ def get_max_concurrency(default=300):
     Returns:
         int: The maximum concurrency value.
     """
-    return int(os.getenv('MAX_CONCURRENCY', default))
-
+    return int(os.getenv("MAX_CONCURRENCY", default))
 
 
 async def async_handler(job):
     """Handle the requests asynchronously."""
     job_input = job["input"]
-    print(f"JOB_INPUT: {job_input}")
-    
+
     if job_input.get("openai_route"):
-        openai_route, openai_input = job_input.get("openai_route"), job_input.get("openai_input")
+        openai_route, openai_input = job_input.get("openai_route"), job_input.get(
+            "openai_input"
+        )
 
         openai_url = f"{engine.base_url}" + openai_route
         headers = {"Content-Type": "application/json"}
@@ -45,16 +45,27 @@ async def async_handler(job):
         else:
             for chunk in response.iter_lines():
                 if chunk:
-                    decoded_chunk = chunk.decode('utf-8')
-                    yield decoded_chunk        
+                    decoded_chunk = chunk.decode("utf-8")
+                    yield decoded_chunk
     else:
         generate_url = f"{engine.base_url}/generate"
         headers = {"Content-Type": "application/json"}
         # Directly pass `job_input` to `json`. Can we tell users the possible fields of `job_input`?
         response = requests.post(generate_url, json=job_input, headers=headers)
+
         if response.status_code == 200:
             yield response.json()
         else:
-            yield {"error": f"Generate request failed with status code {response.status_code}", "details": response.text}
+            yield {
+                "error": f"Generate request failed with status code {response.status_code}",
+                "details": response.text,
+            }
 
-runpod.serverless.start({"handler": async_handler, "concurrency_modifier": get_max_concurrency, "return_aggregate_stream": True})
+
+runpod.serverless.start(
+    {
+        "handler": async_handler,
+        "concurrency_modifier": get_max_concurrency,
+        "return_aggregate_stream": True,
+    }
+)
