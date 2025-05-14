630133f6edfa806aef5e9e701588520d2d40733c
fzyzcjy <ch271828n@outlook.com>
2025-05-14 17:49:23 +0800

    more

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -52,6 +52,7 @@ from sglang.srt.layers.quantization.deep_gemm import (
 from sglang.srt.layers.sampler import Sampler
 from sglang.srt.layers.torchao_utils import apply_torchao_config_to_model
 from sglang.srt.lora.lora_manager import LoRAManager
+from sglang.srt.managers.expert_distribution import set_global_expert_distribution_recorder, ExpertDistributionRecorder
 from sglang.srt.managers.schedule_batch import global_server_args_dict
 from sglang.srt.mem_cache.memory_pool import (
     DoubleSparseTokenToKVPool,
@@ -200,6 +201,14 @@ class ModelRunner:
         server_args = self.server_args
         self.memory_saver_adapter = TorchMemorySaverAdapter.create(
             enable=self.server_args.enable_memory_saver
+        )
+
+        set_global_expert_distribution_recorder(
+            ExpertDistributionRecorder.init_new(
+                server_args,
+                get_global_expert_location_metadata(),
+                rank=self.tp_rank,
+            )
         )
 
         # Load the model
