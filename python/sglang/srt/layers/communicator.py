8677ea27d5d0d910e45ed9b5648f5b7d9909a854
fzyzcjy <ch271828n@outlook.com>
2025-05-15 16:22:01 +0800

    more

--- python/sglang/srt/layers/communicator.py
+++ python/sglang/srt/layers/communicator.py
@@ -234,7 +234,7 @@ def _communicate_summable_tensor_pair(
         return hidden_states, residual
 
     if hidden_states_input_mode == ScatterMode.FULL:
-        assert residual_input_mode == WHAT
+        assert residual_input_mode == ScatterMode.TP_ATTN_FULL
         assert output_mode == ScatterMode.TP_ATTN_FULL
         TODO_add_more_guards
         # TODO(ch-wan): use reduce-scatter in MLP to avoid this scatter
@@ -249,7 +249,7 @@ def _communicate_summable_tensor_pair(
         return hidden_states, residual
 
     if hidden_states_input_mode == ScatterMode.SCATTERED:
-        assert residual_input_mode == WHAT
+        assert residual_input_mode == ScatterMode.SCATTERED
         TODO_add_more_guards
         if output_mode == ScatterMode.TP_ATTN_FULL:
             if context.attn_tp_size != 1:
