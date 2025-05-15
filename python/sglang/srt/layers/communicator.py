e0abc402ae475352dbe25ec410906a43418a799c
fzyzcjy <ch271828n@outlook.com>
2025-05-15 15:05:22 +0800

    more

--- python/sglang/srt/layers/communicator.py
+++ python/sglang/srt/layers/communicator.py
@@ -44,7 +44,12 @@ class LayerScatterModes:
 
     @classmethod
     def _compute_ffn_mode(cls, layer_id: int, context: _LayerModeComputationContext):
-        return TODO
+        return (
+            _FFNInputMode.SCATTERED
+            if (global_server_args_dict["enable_deepep_moe"] and is_sparse)
+               or (DeepseekV2DecoderLayer._enable_moe_dense_fully_dp() and not is_sparse)
+            else _FFNInputMode.FULL
+        )
 
     @classmethod
     def _compute_layer_output_mode(cls, layer_id: int, context: _LayerModeComputationContext):
