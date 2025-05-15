3701c94b19792b9468515968449a486d9da2c010
fzyzcjy <ch271828n@outlook.com>
2025-05-15 18:10:46 +0800

    more

--- python/sglang/srt/operations_strategy.py
+++ python/sglang/srt/operations_strategy.py
@@ -1,7 +1,15 @@
 import torch
 
 
-def compute_layer_operations(layer: torch.nn.Module):
+def compute_layer_operations(
+    layer: torch.nn.Module,
+):
+    if not layer.is_layer_sparse:
+        return [
+            TODO,
+        ]
+
+    # Will add TBO operation orders here
     return [
         TODO,
     ]
