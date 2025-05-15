63c9e2f7279a570115965c7b7ed61b14c1a47ba1
fzyzcjy <ch271828n@outlook.com>
2025-05-15 20:59:10 +0800

    fix

--- python/sglang/srt/layers/communicator.py
+++ python/sglang/srt/layers/communicator.py
@@ -200,8 +200,8 @@ def _compute_num_tokens_of_mode(
         ScatterMode.TP_ATTN_FULL: tp_attn_full_num_tokens,
         ScatterMode.FULL: (
             forward_batch.gathered_buffer.shape[0]
-            if forward_batch.gathered_buffer is not None
-            else None
+            if global_server_args_dict["enable_dp_attention"]
+            else forward_batch.input_ids.shape[0]
         ),
     }
 
