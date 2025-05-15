8a20ef4b893d4cd12ca60b1c4e1547222413ad97
fzyzcjy <ch271828n@outlook.com>
2025-05-15 16:33:40 +0800

    more

--- python/sglang/srt/layers/communicator.py
+++ python/sglang/srt/layers/communicator.py
@@ -239,14 +239,13 @@ def _communicate_summable_tensor_pair(
         and (output_mode == ScatterMode.TP_ATTN_FULL)
     ):
         # TODO(ch-wan): use reduce-scatter in MLP to avoid this scatter
-        if local_attn_dp_size != 1:
-            # important: forward batch.gathered_buffer is used both after scatter and after gather.
-            # be careful about this!
-            hidden_states, global_hidden_states = (
-                forward_batch.gathered_buffer[: forward_batch.input_ids.shape[0]],
-                hidden_states,
-            )
-            dp_scatter(hidden_states, global_hidden_states, forward_batch)
+        # important: forward batch.gathered_buffer is used both after scatter and after gather.
+        # be careful about this!
+        hidden_states, global_hidden_states = (
+            forward_batch.gathered_buffer[: forward_batch.input_ids.shape[0]],
+            hidden_states,
+        )
+        dp_scatter(hidden_states, global_hidden_states, forward_batch)
         return hidden_states, residual
 
     if (
