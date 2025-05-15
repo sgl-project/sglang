43f342b8e8cf28174d1df911de4891d86488ed75
Chang Su <chang.s.su@oracle.com>
2025-05-14 22:23:04 -0700

    Fix mllama4 input_ids
    - Fix wrong input_ids in mllama4.py

--- python/sglang/srt/managers/multimodal_processors/mllama4.py
+++ python/sglang/srt/managers/multimodal_processors/mllama4.py
@@ -135,9 +135,9 @@ class Mllama4ImageProcessor(BaseMultimodalProcessor):
         processor_output["im_end_id"] = self.eoi_token_index
         processor_output["im_token_id"] = self.image_token_index
 
-        # input_ids = processor_output["input_ids"].flatten()
         image_offsets = self.get_mm_items_offset(
-            input_ids=input_ids, mm_token_id=self.image_token_index
+            input_ids=torch.tensor(processor_output["input_ids"]),
+            mm_token_id=self.image_token_index,
         )
 
         # Add metadata for image processing
