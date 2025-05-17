736a38f858de32512adf37d155d41b264a5d55ff
fzyzcjy <ch271828n@outlook.com>
2025-05-17 19:32:53 +0800

    more

--- python/sglang/srt/hacks.py
+++ python/sglang/srt/hacks.py
@@ -1,7 +1,20 @@
 import subprocess
+import time
 
 
 def kill_other_memory_occupying_processes():
     cmd = "pkill -f demo_another_task"
     print(f"kill_other_memory_occupying_processes {cmd=}")
     subprocess.run(cmd, shell=True)
+
+
+if __name__ == '__main__':
+    import torch
+
+    print(f"{time.time()=} {torch.cuda.mem_get_info()=}")
+    print(f"{time.time()=} kill start")
+    kill_other_memory_occupying_processes()
+    print(f"{time.time()=} kill end")
+
+    while True:
+        print(f"{time.time()=} {torch.cuda.mem_get_info()=}")
