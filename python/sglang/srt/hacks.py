42bb20e292c7e47f948c1816a524be358d7b9484
fzyzcjy <ch271828n@outlook.com>
2025-05-17 19:38:50 +0800

    more

--- python/sglang/srt/hacks.py
+++ python/sglang/srt/hacks.py
@@ -4,8 +4,17 @@ import time
 
 def kill_other_memory_occupying_processes():
     cmd = "pkill -f demo_another_task"
-    print(f"kill_other_memory_occupying_processes {cmd=}")
+    print(f"[{time.time()=:.3f}] kill_other_memory_occupying_processes start {cmd=}")
     subprocess.run(cmd, shell=True)
+    print(f"[{time.time()=:.3f}] kill_other_memory_occupying_processes subprocess end")
+
+    while True:
+        free_memory, _ = torch.cuda.mem_get_info()
+        if free_memory > 50_000_000_000:
+            break
+        time.sleep(0.001)
+
+    print(f"[{time.time()=:.3f}] kill_other_memory_occupying_processes see free memory")
 
 
 if __name__ == '__main__':
