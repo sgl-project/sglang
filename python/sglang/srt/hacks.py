62c154df2b2f877d19dcb856bca09f5d82b606b3
fzyzcjy <ch271828n@outlook.com>
2025-05-17 17:48:21 +0800

    more

--- python/sglang/srt/hacks.py
+++ python/sglang/srt/hacks.py
@@ -2,6 +2,6 @@ import subprocess
 
 
 def kill_other_memory_occupying_processes():
-    cmd = "pkill -f demo_another_task.py"
+    cmd = "pkill -f demo_another_task"
     print(f"kill_other_memory_occupying_processes {cmd=}")
     subprocess.run(cmd, shell=True)
