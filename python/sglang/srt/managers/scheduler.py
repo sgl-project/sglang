38ca1ffdb204e788a03e411dabd88f12f89cd6b3
fzyzcjy <ch271828n@outlook.com>
2025-05-17 17:26:38 +0800

    more

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -2027,7 +2027,8 @@ class Scheduler(
 
     def resume_memory_occupation(self, recv_req: ResumeMemoryOccupationReqInput):
         print(f"[Scheduler TP{self.tp_rank}] resume kill others {time.time()=:.3f}")
-        if self.tp_rank % TODO == 0:
+        tp_size_per_node = self.tp_size // self.server_args.nnodes
+        if self.tp_rank % tp_size_per_node == 0:
             kill_other_memory_occupying_processes()
 
         print(f"[Scheduler TP{self.tp_rank}] memory saver resume {time.time()=:.3f}")
