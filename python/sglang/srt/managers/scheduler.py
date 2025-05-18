38ac9e9b5a52b6ea00348e449ba12a5731a33236
fzyzcjy <ch271828n@outlook.com>
2025-05-18 11:49:54 +0800

    more

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -32,8 +32,6 @@ import setproctitle
 import torch
 import torch_memory_saver
 import zmq
-from torch.distributed import barrier
-
 from sglang.global_config import global_config
 from sglang.srt.configs.model_config import ModelConfig
 from sglang.srt.constrained.base_grammar_backend import create_grammar_backend
@@ -147,6 +145,7 @@ from sglang.srt.utils import (
     suppress_other_loggers,
 )
 from sglang.utils import TypeBasedDispatcher, get_exception_traceback
+from torch.distributed import barrier
 
 expert_distribution_recorder = ExpertDistributionRecorder()
 
@@ -403,8 +402,8 @@ class Scheduler(
             1.0,
         )
         self.new_token_ratio_decay = (
-            self.init_new_token_ratio - self.min_new_token_ratio
-        ) / global_config.default_new_token_ratio_decay_steps
+                                         self.init_new_token_ratio - self.min_new_token_ratio
+                                     ) / global_config.default_new_token_ratio_decay_steps
         self.new_token_ratio = self.init_new_token_ratio
 
         # Init watchdog thread
@@ -1379,10 +1378,10 @@ class Scheduler(
             if (
                 self.lora_paths
                 and len(
-                    lora_set
-                    | set([req.lora_path for req in adder.can_run_list])
-                    | set([req.lora_path])
-                )
+                lora_set
+                | set([req.lora_path for req in adder.can_run_list])
+                | set([req.lora_path])
+            )
                 > self.max_loras_per_batch
             ):
                 self.running_batch.batch_is_full = True
@@ -1656,8 +1655,8 @@ class Scheduler(
                     # We should have at least 1 token for sample in every case.
                     max(extend_len - logprob_start_len, 1)
                     for logprob_start_len, extend_len in zip(
-                        local_batch.extend_logprob_start_lens, local_batch.extend_lens
-                    )
+                    local_batch.extend_logprob_start_lens, local_batch.extend_lens
+                )
                 ]
             )
 
@@ -2072,10 +2071,10 @@ class Scheduler(
         # self.memory_saver_adapter.check_validity(caller_name="resume_memory_occupation")
         # self.memory_saver_adapter.resume()
 
-        print(
-            f"[Scheduler, TP{self.tp_rank}, {time.time()}] torch cuda synchronize start"
-        )
-        torch.cuda.synchronize()
+        # print(
+        #     f"[Scheduler, TP{self.tp_rank}, {time.time()}] torch cuda synchronize start"
+        # )
+        # torch.cuda.synchronize()
 
         print(
             f"[Scheduler, TP{self.tp_rank}, {time.time()}] tms_copy_host_to_device start"
