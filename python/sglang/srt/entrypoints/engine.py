7ab2db6e55c355d5fb12ffa90e6a38add8877489
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-05-18 08:06:15 +0800

    Fix stop_profile does not wait for finishing (#4741)

--- python/sglang/srt/entrypoints/engine.py
+++ python/sglang/srt/entrypoints/engine.py
@@ -321,7 +321,8 @@ class Engine(EngineBase):
         loop.run_until_complete(self.tokenizer_manager.start_profile())
 
     def stop_profile(self):
-        self.tokenizer_manager.stop_profile()
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.tokenizer_manager.stop_profile())
 
     def get_server_info(self):
         loop = asyncio.get_event_loop()
