7ec6530a9255c12f074054b365fee54c00f3dd4c
Shangming Cai <caishangming@linux.alibaba.com>
2025-05-20 01:39:36 +0800

    tmp fix ci
    Signed-off-by: Shangming Cai <caishangming@linux.alibaba.com>

--- scripts/killall_sglang.sh
+++ scripts/killall_sglang.sh
@@ -4,10 +4,10 @@
 nvidia-smi
 
 # Clean SGLang processes
-kill -9 $(ps aux | grep 'sglang::' | grep -v 'grep' | awk '{print $2}') 2>/dev/null
-kill -9 $(ps aux | grep 'sglang.launch_server' | grep -v 'grep' | awk '{print $2}') 2>/dev/null
-kill -9 $(ps aux | grep 'sglang.bench' | grep -v 'grep' | awk '{print $2}') 2>/dev/null
-kill -9 $(ps aux | grep 'sglang.data_parallel' | grep -v 'grep' | awk '{print $2}') 2>/dev/null
+sudo kill -9 $(ps aux | grep 'sglang::' | grep -v 'grep' | awk '{print $2}') 2>/dev/null
+sudo kill -9 $(ps aux | grep 'sglang.launch_server' | grep -v 'grep' | awk '{print $2}') 2>/dev/null
+sudo kill -9 $(ps aux | grep 'sglang.bench' | grep -v 'grep' | awk '{print $2}') 2>/dev/null
+sudo kill -9 $(ps aux | grep 'sglang.data_parallel' | grep -v 'grep' | awk '{print $2}') 2>/dev/null
 
 # Clean all GPU processes if any argument is provided
 if [ $# -gt 0 ]; then
@@ -19,8 +19,8 @@ if [ $# -gt 0 ]; then
         apt-get update
         apt-get install -y lsof
     fi
-    kill -9 $(nvidia-smi | sed -n '/Processes:/,$p' | grep "   [0-9]" | awk '{print $5}') 2>/dev/null
-    lsof /dev/nvidia* | awk '{print $2}' | xargs kill -9 2>/dev/null
+    sudo kill -9 $(nvidia-smi | sed -n '/Processes:/,$p' | grep "   [0-9]" | awk '{print $5}') 2>/dev/null
+    sudo lsof /dev/nvidia* | awk '{print $2}' | xargs kill -9 2>/dev/null
 fi
 
 
