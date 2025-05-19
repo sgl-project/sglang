7ce892e5e794851b74c17af300a874fef8fa6e79
fzyzcjy <ch271828n@outlook.com>
2025-05-19 08:42:37 +0800

    more

--- python/sglang/srt/managers/data_parallel_controller.py
+++ python/sglang/srt/managers/data_parallel_controller.py
@@ -235,7 +235,8 @@ class DataParallelController:
                 ctx = (
                     memory_saver_adapter.change_env(
                         'LD_PRELOAD',
-                        TODO + ":" + str(memory_saver_adapter.get_binary_path())
+                        "/host_home/primary_synced/tom_sglang_server/misc/cupti_memory_profiler.so" + ":" + str(
+                            memory_saver_adapter.get_binary_path())
                     )
                     if get_bool_env_var("SGLANG_HACK_ENABLE_CUPTI_MEMORY_PROFILER")
                     else memory_saver_adapter.configure_subprocess():
