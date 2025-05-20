2e77753ee9c30fb79a4193284c7549f8e4e394fe
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-05-20 08:39:53 +0800

    Update bench_serving.py

--- python/sglang/bench_serving.py
+++ python/sglang/bench_serving.py
@@ -1591,6 +1591,7 @@ def run_benchmark(args_: argparse.Namespace):
             profile=args.profile,
             pd_separated=args.pd_separated,
             flush_cache=args.flush_cache,
+            warmup_requests=args.warmup_requests,
         )
     )
 
