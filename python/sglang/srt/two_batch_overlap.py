c269e50ca4ef7502aa45363fd797e116ddc73606
fzyzcjy <ch271828n@outlook.com>
2025-05-20 17:26:39 +0800

    more

--- python/sglang/srt/two_batch_overlap.py
+++ python/sglang/srt/two_batch_overlap.py
@@ -171,7 +171,7 @@ def model_forward_tbo_layers(
 
     with configure_deep_gemm_num_sms(deep_gemm_num_sms):
         output_a, output_b = execute_overlapped_operations(
-            inputs_a, inputs_b, operations, operations, delta_stages=delta_stages
+            [inputs_a, inputs_b], [operations] * 2, [0, delta_stages],
         )
 
     return _model_forward_merge_outputs(output_a, output_b)
