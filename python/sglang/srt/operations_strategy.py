f3e8e4e5a3df4f296f8d433dffb612c167a958fb
fzyzcjy <ch271828n@outlook.com>
2025-05-20 16:41:38 +0800

    more

--- python/sglang/srt/operations_strategy.py
+++ python/sglang/srt/operations_strategy.py
@@ -1,5 +1,8 @@
 import torch
 
+from sglang.srt import two_batch_overlap, operations
+from sglang.srt.model_executor.forward_batch_info import ForwardMode
+
 
 def compute_layer_operations(
     layer: torch.nn.Module,
@@ -13,7 +16,50 @@ def compute_layer_operations(
             layer.op_comm_postprocess_layer,
         ]
 
-    # Will add TBO operation orders here
+    if enable_two_batch_overlap:
+        if forward_mode == ForwardMode.EXTEND:
+            return [
+                layer.op_comm_prepare_attn,
+                layer.op_attn,
+                layer.op_comm_prepare_mlp,
+                layer.mlp.op_gate,
+                layer.mlp.op_select_experts,
+                layer.mlp.op_dispatch_a,
+                operations.YieldOperation(),
+                layer.mlp.op_dispatch_b,
+                layer.mlp.op_experts,
+                layer.mlp.op_combine_a,
+                operations.YieldOperation(),
+                layer.mlp.op_shared_experts,
+                layer.mlp.op_combine_b,
+                layer.mlp.op_output,
+                layer.op_comm_postprocess_layer,
+            ]
+        elif forward_mode == ForwardMode.DECODE:
+            return [
+                layer.op_comm_prepare_attn,
+                layer.op_decode_attn_0, # TODO
+                operations.YieldOperation(),
+                layer.op_decode_attn_1, # TODO
+                layer.op_comm_prepare_mlp,
+                layer.mlp.op_gate,
+                layer.mlp.op_select_experts,
+                operations.YieldOperation(),
+                layer.mlp.op_dispatch_a,
+                layer.mlp.op_shared_experts,
+                operations.YieldOperation(),
+                layer.mlp.op_dispatch_b,
+                layer.mlp.op_experts,
+                layer.mlp.op_combine_a,
+                operations.YieldOperation(),
+                layer.mlp.op_combine_b,
+                layer.mlp.op_output,
+                layer.op_comm_postprocess_layer,
+                operations.YieldOperation(),
+            ]
+        else:
+            raise NotImplementedError(f"Unsupported {forward_mode=}")
+
     return [
         layer.op_comm_prepare_attn,
         layer.op_attn,
@@ -30,45 +76,3 @@ def compute_layer_operations(
         layer.op_comm_postprocess_layer,
     ]
 
-    if forward_mode == ForwardMode.EXTEND:
-        return [
-            layer.op_comm_prepare_attn,
-            layer.op_attn,
-            layer.op_comm_prepare_mlp,
-            layer.mlp.op_gate,
-            layer.mlp.op_select_experts,
-            layer.mlp.op_dispatch_a,
-            two_batch_overlap.YieldOperation(),
-            layer.mlp.op_dispatch_b,
-            layer.mlp.op_experts,
-            layer.mlp.op_combine_a,
-            two_batch_overlap.YieldOperation(),
-            layer.mlp.op_shared_experts,
-            layer.mlp.op_combine_b,
-            layer.mlp.op_output,
-            layer.op_comm_postprocess_layer,
-        ]
-    elif forward_mode == ForwardMode.DECODE:
-        return [
-            layer.op_comm_prepare_attn,
-            layer.op_decode_attn_0, # TODO
-            two_batch_overlap.YieldOperation(),
-            layer.op_decode_attn_1, # TODO
-            layer.op_comm_prepare_mlp,
-            layer.mlp.op_gate,
-            layer.mlp.op_select_experts,
-            two_batch_overlap.YieldOperation(),
-            layer.mlp.op_dispatch_a,
-            layer.mlp.op_shared_experts,
-            two_batch_overlap.YieldOperation(),
-            layer.mlp.op_dispatch_b,
-            layer.mlp.op_experts,
-            layer.mlp.op_combine_a,
-            two_batch_overlap.YieldOperation(),
-            layer.mlp.op_combine_b,
-            layer.mlp.op_output,
-            layer.op_comm_postprocess_layer,
-            two_batch_overlap.YieldOperation(),
-        ]
-    else:
-        raise NotImplementedError(f"Unsupported {forward_mode=}")
