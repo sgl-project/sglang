244140d96c78e65e0a43d9c5badd38a1d3ef413c
fzyzcjy <ch271828n@outlook.com>
2025-05-20 16:35:20 +0800

    more

--- python/sglang/srt/operations_strategy.py
+++ python/sglang/srt/operations_strategy.py
@@ -29,3 +29,48 @@ def compute_layer_operations(
         layer.mlp.op_output,
         layer.op_comm_postprocess_layer,
     ]
+
+    if forward_mode == ForwardMode.EXTEND:
+        return [
+            self._forward_tbo_op_input_layernorm,
+            self._forward_tbo_op_prefill_attn,
+            self._forward_tbo_op_post_attn_layernorm,
+            self.mlp._forward_tbo_op_gate,
+            self.mlp._forward_tbo_op_dispatch_a_part_one,
+            self.mlp._forward_tbo_op_dispatch_a_part_two,
+            two_batch_overlap.YieldOperation(),
+            partial(
+                self.mlp._forward_tbo_op_dispatch_b, tbo_child_index=tbo_child_index
+            ),
+            self.mlp._forward_tbo_op_mlp,
+            self.mlp._forward_tbo_op_combine_a,
+            two_batch_overlap.YieldOperation(),
+            self.mlp._forward_tbo_op_shared,
+            self.mlp._forward_tbo_op_combine_b,
+            self._forward_tbo_op_compute_layer_output,
+        ]
+    elif forward_mode == ForwardMode.DECODE:
+        return [
+            self._forward_tbo_op_input_layernorm,
+            self._forward_tbo_op_decode_attn_0,
+            two_batch_overlap.YieldOperation(),
+            self._forward_tbo_op_decode_attn_1,
+            self._forward_tbo_op_post_attn_layernorm,
+            self.mlp._forward_tbo_op_gate,
+            self.mlp._forward_tbo_op_dispatch_a_part_one,
+            two_batch_overlap.YieldOperation(),
+            self.mlp._forward_tbo_op_dispatch_a_part_two,
+            self.mlp._forward_tbo_op_shared,
+            two_batch_overlap.YieldOperation(),
+            partial(
+                self.mlp._forward_tbo_op_dispatch_b, tbo_child_index=tbo_child_index
+            ),
+            self.mlp._forward_tbo_op_mlp,
+            self.mlp._forward_tbo_op_combine_a,
+            two_batch_overlap.YieldOperation(),
+            self.mlp._forward_tbo_op_combine_b,
+            self._forward_tbo_op_compute_layer_output,
+            two_batch_overlap.YieldOperation(),
+        ]
+    else:
+        raise NotImplementedError(f"Unsupported {forward_mode=}")
