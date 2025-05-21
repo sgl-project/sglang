9c992423f51cd37a53e9eaaa3789912c6a927b42
fzyzcjy <ch271828n@outlook.com>
2025-05-21 20:00:17 +0800

    more

--- python/sglang/srt/hacks.py
+++ python/sglang/srt/hacks.py
@@ -81,6 +81,7 @@ class CuptiMemoryProfiler:
         os.environ[
             'CUPTI_MEMORY_PROFILER_OUTPUT_PATH'] = f"/host_home/temp_sglang_server2local/cupti_memory_profiler_{time.time()}.log"
         self._cdll = ctypes.CDLL("/host_home/primary_synced/tom_sglang_server/misc/cupti_memory_profiler.so")
+        print("hi call cuptiMemoryProfilerInit")
         self._cdll.cuptiMemoryProfilerInit()
 
     def shutdown(self):
