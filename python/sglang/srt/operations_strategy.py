915e5af5c5275dcdc1ea629887f09bafb30848ed
fzyzcjy <ch271828n@outlook.com>
2025-05-21 09:17:23 +0800

    more

--- python/sglang/srt/operations_strategy.py
+++ python/sglang/srt/operations_strategy.py
@@ -45,45 +45,9 @@ def _compute_layer_operations(
 
     if these_layers_of_this_batch_needs_tbo:
         if forward_mode == ForwardMode.EXTEND:
-            return [
-                layer.op_comm_prepare_attn,
-                layer.op_attn,
-                layer.op_comm_prepare_mlp,
-                layer.mlp.op_gate,
-                layer.mlp.op_select_experts,
-                layer.mlp.op_dispatch_a,
-                operations.YieldOperation(),
-                layer.mlp.op_dispatch_b,
-                layer.mlp.op_experts,
-                layer.mlp.op_combine_a,
-                operations.YieldOperation(),
-                layer.mlp.op_shared_experts,
-                layer.mlp.op_combine_b,
-                layer.mlp.op_output,
-                layer.op_comm_postprocess_layer,
-            ]
+            return _compute_moe_deepseek_blog_prefill()
         elif forward_mode == ForwardMode.DECODE:
-            return [
-                layer.op_comm_prepare_attn,
-                layer.op_decode_attn_0,  # TODO
-                operations.YieldOperation(),
-                layer.op_decode_attn_1,  # TODO
-                layer.op_comm_prepare_mlp,
-                layer.mlp.op_gate,
-                layer.mlp.op_select_experts,
-                operations.YieldOperation(),
-                layer.mlp.op_dispatch_a,
-                layer.mlp.op_shared_experts,
-                operations.YieldOperation(),
-                layer.mlp.op_dispatch_b,
-                layer.mlp.op_experts,
-                layer.mlp.op_combine_a,
-                operations.YieldOperation(),
-                layer.mlp.op_combine_b,
-                layer.mlp.op_output,
-                layer.op_comm_postprocess_layer,
-                operations.YieldOperation(),
-            ]
+            return _compute_moe_deepseek_blog_decode()
         else:
             raise NotImplementedError(f"Unsupported {forward_mode=}")
 
@@ -115,4 +79,48 @@ def _compute_moe_normal():
         layer.mlp.op_combine_b,
         layer.mlp.op_output,
         layer.op_comm_postprocess_layer,
+    ]
+
+
+def _compute_moe_deepseek_blog_prefill():
+    return [
+        layer.op_comm_prepare_attn,
+        layer.op_attn,
+        layer.op_comm_prepare_mlp,
+        layer.mlp.op_gate,
+        layer.mlp.op_select_experts,
+        layer.mlp.op_dispatch_a,
+        operations.YieldOperation(),
+        layer.mlp.op_dispatch_b,
+        layer.mlp.op_experts,
+        layer.mlp.op_combine_a,
+        operations.YieldOperation(),
+        layer.mlp.op_shared_experts,
+        layer.mlp.op_combine_b,
+        layer.mlp.op_output,
+        layer.op_comm_postprocess_layer,
+    ]
+
+
+def _compute_moe_deepseek_blog_decode():
+    return [
+        layer.op_comm_prepare_attn,
+        layer.op_decode_attn_0,  # TODO
+        operations.YieldOperation(),
+        layer.op_decode_attn_1,  # TODO
+        layer.op_comm_prepare_mlp,
+        layer.mlp.op_gate,
+        layer.mlp.op_select_experts,
+        operations.YieldOperation(),
+        layer.mlp.op_dispatch_a,
+        layer.mlp.op_shared_experts,
+        operations.YieldOperation(),
+        layer.mlp.op_dispatch_b,
+        layer.mlp.op_experts,
+        layer.mlp.op_combine_a,
+        operations.YieldOperation(),
+        layer.mlp.op_combine_b,
+        layer.mlp.op_output,
+        layer.op_comm_postprocess_layer,
+        operations.YieldOperation(),
     ]
