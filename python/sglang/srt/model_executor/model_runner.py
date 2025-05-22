4500ff2f2418c48537c810c5bed2b0110557fb2f
root <root@dell300x-pla-t10-17.pla.dcgpu>
2025-05-22 08:30:28 +0000

    Fix CI OOM issue

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -405,6 +405,11 @@ class ModelRunner:
         if not server_args.disable_chunked_prefix_cache:
             logger.info("Chunked prefix cache is turned on.")
 
+        if server_args.attention_backend == "aiter":
+            print(f"self.model_config.context_len: {self.model_config.context_len}", flush=True)
+            if self.tp_size == 1 and self.model_config.context_len > 8192:
+                self.mem_fraction_static *= 0.85
+
     def init_torch_distributed(self):
         logger.info("Init torch distributed begin.")
 
