7513558074adc4c4015b68e2ae7cf719d3401d5d
Byron Hsu <byronhsu1230@gmail.com>
2025-05-21 21:22:21 -0700

    [PD] Add doc and simplify sender.send (#6019)

--- python/sglang/srt/managers/io_struct.py
+++ python/sglang/srt/managers/io_struct.py
@@ -407,6 +407,7 @@ class GenerateReqInput:
                 else None
             ),
             return_hidden_states=self.return_hidden_states,
+            # if `__getitem__` is called, the bootstrap_host, bootstrap_port, bootstrap_room must be a list
             bootstrap_host=(
                 self.bootstrap_host[i] if self.bootstrap_host is not None else None
             ),
