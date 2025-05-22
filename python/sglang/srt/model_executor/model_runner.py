1cc9ba59e858204673414a0386aeb8184372a0ea
root <root@dell300x-pla-t10-17.pla.dcgpu>
2025-05-22 08:31:47 +0000

    remove debug message

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -406,7 +406,6 @@ class ModelRunner:
             logger.info("Chunked prefix cache is turned on.")
 
         if server_args.attention_backend == "aiter":
-            print(f"self.model_config.context_len: {self.model_config.context_len}", flush=True)
             if self.tp_size == 1 and self.model_config.context_len > 8192:
                 self.mem_fraction_static *= 0.85
 
