bdce5d7df6607bc0ad91354fd26e82cd505c6c93
PanJason <pyyjason@gmail.com>
2025-05-23 14:26:31 +0000

    Disable stack due to a pytorch bug

--- python/sglang/srt/managers/tokenizer_manager.py
+++ python/sglang/srt/managers/tokenizer_manager.py
@@ -808,7 +808,7 @@ class TokenizerManager:
             output_dir=output_dir,
             num_steps=num_steps,
             activities=activities,
-            with_stack=with_stack,
+            with_stack=False,
             record_shapes=record_shapes,
             profile_by_stage=profile_by_stage,
             profile_id=str(time.time()),
