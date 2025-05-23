62e0ed7d58540d12adb1bcbb6e0a4206d9f042c3
ByronHsu <byron@x.ai>
2025-05-23 18:58:07 +0000

    fix test

--- test/srt/test_disaggregation.py
+++ test/srt/test_disaggregation.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import time
 import unittest
@@ -28,10 +29,16 @@ class TestDisaggregationMooncake(CustomTestCase):
         cls.lb_port = base_port
         cls.prefill_port = f"{int(base_port) + 100}"
         cls.decode_port = f"{int(base_port) + 200}"
+        cls.prefill_url = f"http://{cls.base_host}:{cls.prefill_port}"
+        cls.decode_url = f"http://{cls.base_host}:{cls.decode_port}"
+        cls.lb_url = f"http://{cls.base_host}:{cls.lb_port}"
         print(f"{cls.base_host=} {cls.lb_port=} {cls.prefill_port=} {cls.decode_port=}")
-        run_with_timeout(cls.start_prefill, timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH)
-        run_with_timeout(cls.start_decode, timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH)
 
+        # Non blocking start servers
+        cls.start_prefill()
+        cls.start_decode()
+
+        # Block until both
         cls.wait_server_ready(cls.prefill_url + "/health")
         cls.wait_server_ready(cls.decode_url + "/health")
 
@@ -46,7 +53,7 @@ class TestDisaggregationMooncake(CustomTestCase):
             "--host",
             cls.base_host,
             "--port",
-            str(cls.base_port),
+            cls.lb_port,
         ]
 
         print("Starting load balancer:", " ".join(lb_command))
@@ -62,13 +69,13 @@ class TestDisaggregationMooncake(CustomTestCase):
             "--disaggregation-mode",
             "prefill",
             "--tp",
-            "4",
+            "1",
             "--disaggregation-ib-device",
-            "mlx5_roce0,mlx5_roce1,mlx5_roce2,mlx5_roce3",
+            "mlx5_roce0",
         ]
         cls.process_prefill = popen_launch_pd_server(
             cls.model,
-            f"http://{cls.base_host}:{cls.prefill_port}",
+            cls.prefill_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=prefill_args,
         )
@@ -80,15 +87,15 @@ class TestDisaggregationMooncake(CustomTestCase):
             "--disaggregation-mode",
             "decode",
             "--tp",
-            "4",
+            "1",
             "--base-gpu-id",
-            "4",
+            "1",
             "--disaggregation-ib-device",
-            "mlx5_roce4,mlx5_roce5,mlx5_roce6,mlx5_roce7",
+            "mlx5_roce1",
         ]
         cls.process_decode = popen_launch_pd_server(
             cls.model,
-            f"http://{cls.base_host}:{cls.decode_port}",
+            cls.decode_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=decode_args,
         )
@@ -118,25 +125,28 @@ class TestDisaggregationMooncake(CustomTestCase):
                 except Exception as e:
                     print(f"Error killing process {process.pid}: {e}")
 
-    # def test_gsm8k(self):
-    #     args = SimpleNamespace(
-    #         num_shots=5,
-    #         data_path=None,
-    #         num_questions=200,
-    #         max_new_tokens=512,
-    #         parallel=128,
-    #         host="http://127.0.0.1",
-    #         port=int(self.lb_url.split(":")[-1]),
-    #     )
-    #     metrics = run_eval_few_shot_gsm8k(args)
-    #     print(f"Evaluation metrics: {metrics}")
+        # wait for 5 seconds
+        time.sleep(5)
 
-    #     self.assertGreater(metrics["accuracy"], 0.62)
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            num_shots=5,
+            data_path=None,
+            num_questions=200,
+            max_new_tokens=512,
+            parallel=128,
+            host=f"http://{self.base_host}",
+            port=int(self.lb_port),
+        )
+        metrics = run_eval_few_shot_gsm8k(args)
+        print(f"Evaluation metrics: {metrics}")
+
+        self.assertGreater(metrics["accuracy"], 0.62)
 
     def test_logprob(self):
         prompt = "The capital of taiwan is "
         response = requests.post(
-            f"http://{self.base_host}:{self.lb_port}/generate",
+            self.lb_url + "/generate",
             json={
                 "text": prompt,
                 "sampling_params": {"temperature": 0},
@@ -147,14 +157,146 @@ class TestDisaggregationMooncake(CustomTestCase):
         )
 
         j = response.json()
+        completion_tokens = j["meta_info"]["completion_tokens"]
         input_logprobs = j["meta_info"]["input_token_logprobs"]
         output_logprobs = j["meta_info"]["output_token_logprobs"]
 
-        print(len(input_logprobs), len(output_logprobs))
+        assert (
+            len(output_logprobs) == completion_tokens
+        ), f"output_logprobs and completion_tokens should have the same length, but got {len(output_logprobs)} and {completion_tokens}"
+        assert (
+            len(input_logprobs) > 0
+        ), f"input_logprobs should have at least one token, but got {len(input_logprobs)}"
 
 
-# class TestDisaggregationFailure(CustomTestCase):
-#     ...
+class TestDisaggregationMooncakeFailure(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        # set DISAGGREGATION_TEST_FAILURE_PROB to simulate failure
+        os.environ["DISAGGREGATION_TEST_FAILURE_PROB"] = "0.05"
+
+        cls.model = DEFAULT_MODEL_NAME_FOR_TEST
+        parsed_url = urlparse(DEFAULT_URL_FOR_TEST)
+        cls.base_host = parsed_url.hostname
+        base_port = str(parsed_url.port)
+        cls.lb_port = base_port
+        cls.prefill_port = f"{int(base_port) + 100}"
+        cls.decode_port = f"{int(base_port) + 200}"
+        cls.prefill_url = f"http://{cls.base_host}:{cls.prefill_port}"
+        cls.decode_url = f"http://{cls.base_host}:{cls.decode_port}"
+        cls.lb_url = f"http://{cls.base_host}:{cls.lb_port}"
+        print(f"{cls.base_host=} {cls.lb_port=} {cls.prefill_port=} {cls.decode_port=}")
+
+        # Non blocking start servers
+        cls.start_prefill()
+        cls.start_decode()
+
+        # Block until both
+        cls.wait_server_ready(cls.prefill_url + "/health")
+        cls.wait_server_ready(cls.decode_url + "/health")
+
+        lb_command = [
+            "python3",
+            "-m",
+            "sglang.srt.disaggregation.mini_lb",
+            "--prefill",
+            cls.prefill_url,
+            "--decode",
+            cls.decode_url,
+            "--host",
+            cls.base_host,
+            "--port",
+            cls.lb_port,
+        ]
+
+        print("Starting load balancer:", " ".join(lb_command))
+        cls.process_lb = subprocess.Popen(
+            lb_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        cls.wait_server_ready(cls.lb_url + "/health")
+
+    @classmethod
+    def start_prefill(cls):
+        prefill_args = [
+            "--trust-remote-code",
+            "--disaggregation-mode",
+            "prefill",
+            "--tp",
+            "1",
+            "--disaggregation-ib-device",
+            "mlx5_roce0",
+        ]
+        cls.process_prefill = popen_launch_pd_server(
+            cls.model,
+            cls.prefill_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=prefill_args,
+        )
+
+    @classmethod
+    def start_decode(cls):
+        decode_args = [
+            "--trust-remote-code",
+            "--disaggregation-mode",
+            "decode",
+            "--tp",
+            "1",
+            "--base-gpu-id",
+            "1",
+            "--disaggregation-ib-device",
+            "mlx5_roce1",
+        ]
+        cls.process_decode = popen_launch_pd_server(
+            cls.model,
+            cls.decode_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=decode_args,
+        )
+
+    @classmethod
+    def wait_server_ready(cls, url, timeout=60):
+        start_time = time.perf_counter()
+        while True:
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    print(f"Server {url} is ready")
+                    return
+            except Exception:
+                pass
+
+            if time.perf_counter() - start_time > timeout:
+                raise RuntimeError(f"Server {url} failed to start in {timeout}s")
+            time.sleep(1)
+
+    @classmethod
+    def tearDownClass(cls):
+        # unset DISAGGREGATION_TEST_FAILURE_PROB
+        os.environ.pop("DISAGGREGATION_TEST_FAILURE_PROB")
+        for process in [cls.process_lb, cls.process_decode, cls.process_prefill]:
+            if process:
+                try:
+                    kill_process_tree(process.pid)
+                except Exception as e:
+                    print(f"Error killing process {process.pid}: {e}")
+
+        # wait for 5 seconds
+        time.sleep(5)
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            num_shots=5,
+            data_path=None,
+            num_questions=200,
+            max_new_tokens=512,
+            parallel=128,
+            host=f"http://{self.base_host}",
+            port=int(self.lb_port),
+        )
+        metrics = run_eval_few_shot_gsm8k(args)
+        print(f"Evaluation metrics: {metrics}")
+        # Expect lots of failure but the server cannot crash
+
 
 if __name__ == "__main__":
     unittest.main()
