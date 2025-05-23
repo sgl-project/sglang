e04a387e1b6c9668b5f0c507564726d73e18474d
ocss884 <ocss.lin@gmail.com>
2025-05-22 21:39:19 -0700

    more

--- test/srt/test_verl_engine_2_gpu.py
+++ test/srt/test_verl_engine_2_gpu.py
@@ -47,10 +47,7 @@ ALL_MODELS = [
         model_path="ibm-granite/granite-3.0-2b-instruct",
         prefill_tolerance=0.22,
         decode_tolerance=0.22,
-    ),
-    # Fail to run these models in test_generation_models.py, need to fix that first
-    # dict(model_path="openai-community/gpt2"),
-    # dict(model_path="microsoft/Phi-3-small-8k-instruct"),
+    )
 ]
 
 
