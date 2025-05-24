b0d6e42225565647be864ea86df6dfbc1094fe82
saienduri <saimanas.enduri@amd.com>
2025-05-24 02:21:18 -0700

    try oom fix

--- scripts/amd_ci_install_dependency.sh
+++ scripts/amd_ci_install_dependency.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 docker exec ci_sglang pip install --upgrade pip
 docker exec ci_sglang pip uninstall sgl-kernel -y || true
 docker exec -w /sglang-checkout/sgl-kernel ci_sglang bash -c "rm -f pyproject.toml && mv pyproject_rocm.toml pyproject.toml && python3 setup_rocm.py install"
+docker exec ci_sglang pip install -e "python[dev_hip]"
 
 docker exec -w / ci_sglang git clone https://github.com/merrymercy/human-eval.git
 docker exec -w /human-eval ci_sglang pip install -e .
