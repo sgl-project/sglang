492062b91269efa3582f06fa317a1c803579f343
fzyzcjy <ch271828n@outlook.com>
2025-05-27 19:41:26 +0800

    more

--- python/sglang/srt/model_executor/cuda_graph_runner.py
+++ python/sglang/srt/model_executor/cuda_graph_runner.py
@@ -579,8 +579,9 @@ class CudaGraphRunner:
         self.seq_lens[:raw_bs].copy_(forward_batch.seq_lens)
         self.out_cache_loc[:raw_num_token].copy_(forward_batch.out_cache_loc)
         self.positions[:raw_num_token].copy_(forward_batch.positions)
-        self.num_token_non_padded[...] = len(forward_batch.input_ids)
-        self.tbo_plugin.replay_prepare()
+        num_token_non_padded = len(forward_batch.input_ids)
+        self.num_token_non_padded[...] = num_token_non_padded
+        self.tbo_plugin.replay_prepare(num_token_non_padded=num_token_non_padded)
         if forward_batch.seq_lens_cpu is not None:
             if bs != raw_bs:
                 self.seq_lens_cpu.fill_(1)
--- python/sglang/srt/two_batch_overlap.py
+++ python/sglang/srt/two_batch_overlap.py
@@ -103,9 +103,12 @@ class TboCudaGraphRunnerPlugin:
             tbo_children_num_token_non_padded=self.tbo_children_num_token_non_padded,
         )
 
-    def replay_prepare(self):
-        self.tbo_children_num_token_non_padded[...] = TboForwardBatchPreparer.compute_tbo_children_num_token_non_padded(
-            TODO)
+    def replay_prepare(self, num_token_non_padded: int):
+        self.tbo_children_num_token_non_padded[
+            ...] = TboForwardBatchPreparer.compute_tbo_children_num_token_non_padded_raw(
+            tbo_split_token_index=TODO,
+            num_token_non_padded=num_token_non_padded,
+        )
 
     @staticmethod
     def _compute_tbo_split_seq_index(num_tokens: int, forward_mode):
