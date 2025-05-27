0d47f141087f6a55e8accc435ef28e39d7386b62
kyle <kyle.pena@kuzco.xyz>
2025-05-27 04:33:10 +0000

    test suite cleanup

--- test/srt/test_openai_server_hidden_states.py
+++ test/srt/test_openai_server_hidden_states.py
@@ -24,66 +24,51 @@ HS_TEST_CASES = [True]
 ENABLE_HS_ARGS = ["--enable-return-hidden-states"]  # ["--enable-return-hidden-states"]
 
 
-class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
-    """
+class BaseTestOpenAIServerWithHiddenStates(ABC):
+
     def test_completion(self):
-        for return_hidden_states in HS_TEST_CASES:
-            for echo in [False]: # echo=True broken for EAGLE (probably also EAGLE3)
-                for logprobs in [None, 5]:
-                    for use_list_input in [True, False]:
-                        for parallel_sample_num in [1, 2]:
-                            for token_input in [False, True]:
-                                self.run_completion(
-                                    echo,
-                                    logprobs,
-                                    use_list_input,
-                                    parallel_sample_num,
-                                    token_input,
-                                    return_hidden_states,
-                                )
+        for return_hidden_states in [False, True]:
+            for use_list_input in [True, False]:
+                for parallel_sample_num in [1]:
+                    for token_input in [False, True]:
+                        self.run_completion(
+                            use_list_input,
+                            parallel_sample_num,
+                            token_input,
+                            return_hidden_states,
+                        )
 
     def test_completion_stream(self):
         # parallel sampling and list input are not supported in streaming mode
-        for return_hidden_states in HS_TEST_CASES:
-            for echo in [False]: # echo=True broken for EAGLE (probably also EAGLE3)
-                for logprobs in [None, 5]:
-                    for use_list_input in [True]:
-                        for parallel_sample_num in [1, 2]:
-                            for token_input in [False, True]:
-                                self.run_completion_stream(
-                                    echo,
-                                    logprobs,
-                                    use_list_input,
-                                    parallel_sample_num,
-                                    token_input,
-                                    return_hidden_states,
-                                )
-    """
+        for return_hidden_states in [False, True]:
+            for use_list_input in [True]:
+                for parallel_sample_num in [1]:
+                    for token_input in [False, True]:
+                        self.run_completion_stream(
+                            use_list_input,
+                            parallel_sample_num,
+                            token_input,
+                            return_hidden_states,
+                        )
 
     def test_chat_completion(self):
-        for return_hidden_states in HS_TEST_CASES:
-            for logprobs in [None]:  # logprobs 5 breaks with EAGLE (only returns 4)
-                for parallel_sample_num in [
-                    1
-                ]:  # parallel sample num 2 breaks in the adapter with a 400 for EAGLE
-                    self.run_chat_completion(
-                        logprobs, parallel_sample_num, return_hidden_states
-                    )
+        for return_hidden_states in [False, True]:
+            for parallel_sample_num in [
+                1
+            ]:  # parallel sample num 2 breaks in the adapter with a 400 for EAGLE
+                self.run_chat_completion(parallel_sample_num, return_hidden_states)
 
-    """
     def test_chat_completion_stream(self):
-        for return_hidden_states in HS_TEST_CASES:
-            for logprobs in [None]: # logprobs 5 breaks with EAGLE (only returns 4)
-                for parallel_sample_num in [1]:  # parallel sample num > 1 breaks in the adapter with a 400 for EAGLE
-                    self.run_chat_completion_stream(
-                        logprobs, parallel_sample_num, return_hidden_states
-                    )
-    """
+        for return_hidden_states in [False, True]:
+            for parallel_sample_num in [
+                1
+            ]:  # parallel sample num > 1 breaks in the adapter with a 400 for EAGLE
+                self.run_chat_completion_stream(
+                    parallel_sample_num, return_hidden_states
+                )
 
     def run_completion(
         self,
-        echo,
-        logprobs,
         use_list_input,
         parallel_sample_num,
         token_input,
@@ -111,32 +96,11 @@ class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
             prompt=prompt_arg,
             temperature=0,
             max_tokens=32,
-            echo=echo,
-            logprobs=logprobs,
             n=parallel_sample_num,
             extra_body=dict(return_hidden_states=return_hidden_states),
         )
 
         assert len(response.choices) == num_choices * parallel_sample_num
-
-        if echo:
-            text = response.choices[0].text
-            print("text", text)
-            assert text.startswith(prompt)
-
-        if logprobs:
-            assert response.choices[0].logprobs
-            assert isinstance(response.choices[0].logprobs.tokens[0], str)
-            assert isinstance(response.choices[0].logprobs.top_logprobs[1], dict)
-            ret_num_top_logprobs = len(response.choices[0].logprobs.top_logprobs[1])
-
-            # FIXME: Sometimes, some top_logprobs are missing in the return value. The reason is that some output id maps to the same output token and duplicate in the map
-            # assert ret_num_top_logprobs == logprobs, f"{ret_num_top_logprobs} vs {logprobs}"
-            assert ret_num_top_logprobs > 0
-
-            # when echo=True and request.logprobs>0, logprob_start_len is 0, so the first token's logprob would be None.
-            if not echo:
-                assert response.choices[0].logprobs.token_logprobs[0]
 
         assert response.id
         assert response.created
@@ -160,8 +124,6 @@ class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
 
     def run_completion_stream(
         self,
-        echo,
-        logprobs,
         use_list_input,
         parallel_sample_num,
         token_input,
@@ -189,8 +151,6 @@ class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
             prompt=prompt_arg,
             temperature=0,
             max_tokens=32,
-            echo=echo,
-            logprobs=logprobs,
             stream=True,
             stream_options={"include_usage": True},
             n=parallel_sample_num,
@@ -217,27 +177,7 @@ class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
             index = response.choices[0].index
             is_first = is_firsts.get(index, True)
 
-            if logprobs:
-                assert response.choices[0].logprobs, f"no logprobs in response"
-                assert isinstance(
-                    response.choices[0].logprobs.tokens[0], str
-                ), f"{response.choices[0].logprobs.tokens[0]} is not a string"
-                if not (is_first and echo):
-                    assert isinstance(
-                        response.choices[0].logprobs.top_logprobs[0], dict
-                    ), f"top_logprobs was not a dictionary"
-                    ret_num_top_logprobs = len(
-                        response.choices[0].logprobs.top_logprobs[0]
-                    )
-                    # FIXME: Sometimes, some top_logprobs are missing in the return value. The reason is that some output id maps to the same output token and duplicate in the map
-                    # assert ret_num_top_logprobs == logprobs, f"{ret_num_top_logprobs} vs {logprobs}"
-                    assert ret_num_top_logprobs > 0, f"ret_num_top_logprobs was 0"
-
             if is_first:
-                if echo:
-                    assert response.choices[0].text.startswith(
-                        prompt
-                    ), f"{response.choices[0].text} and all args {echo} {logprobs} {token_input} {is_first}"
                 is_firsts[index] = False
             assert response.id, f"no id in response"
             assert response.created, f"no created in response"
@@ -261,9 +201,9 @@ class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
                 hidden_states is None
             ), "hidden_states was returned and should not have been"
 
-    def run_chat_completion(self, logprobs, parallel_sample_num, return_hidden_states):
+    def run_chat_completion(self, parallel_sample_num, return_hidden_states):
         print(
-            f"run_chat_completion: logprobs={logprobs}, parallel_sample_num={parallel_sample_num}, return_hidden_states={return_hidden_states}"
+            f"run_chat_completion: parallel_sample_num={parallel_sample_num}, return_hidden_states={return_hidden_states}"
         )
         client = openai.Client(api_key=self.api_key, base_url=self.base_url)
         response = client.chat.completions.create(
@@ -276,23 +216,9 @@ class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
                 },
             ],
             temperature=0,
-            logprobs=logprobs is not None and logprobs > 0,
-            top_logprobs=logprobs,
             n=parallel_sample_num,
             extra_body=dict(return_hidden_states=return_hidden_states),
         )
-
-        if logprobs:
-            assert isinstance(
-                response.choices[0].logprobs.content[0].top_logprobs[0].token, str
-            )
-
-            ret_num_top_logprobs = len(
-                response.choices[0].logprobs.content[0].top_logprobs
-            )
-            assert (
-                ret_num_top_logprobs == logprobs
-            ), f"{ret_num_top_logprobs} vs {logprobs}"
 
         assert len(response.choices) == parallel_sample_num
         assert response.choices[0].message.role == "assistant"
@@ -316,7 +242,7 @@ class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
             ), "hidden_states was returned and should not have been"
 
     def run_chat_completion_stream(
-        self, logprobs, parallel_sample_num=1, return_hidden_states=False
+        self, parallel_sample_num=1, return_hidden_states=False
     ):
         client = openai.Client(api_key=self.api_key, base_url=self.base_url)
         generator = client.chat.completions.create(
@@ -326,8 +252,6 @@ class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
                 {"role": "user", "content": "What is the capital of France?"},
             ],
             temperature=0,
-            logprobs=logprobs is not None and logprobs > 0,
-            top_logprobs=logprobs,
             stream=True,
             stream_options={"include_usage": True},
             n=parallel_sample_num,
@@ -336,7 +260,6 @@ class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
 
         is_firsts = {}
         hidden_states = None
-        top_logprob_tokens = []
         for response in generator:
             usage = response.usage
             if usage is not None:
@@ -359,27 +282,6 @@ class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
                 is_firsts[index] = False
                 continue
 
-            if logprobs:
-                assert response.choices[0].logprobs, f"logprobs was not returned"
-                assert isinstance(
-                    response.choices[0].logprobs.content[0].top_logprobs[0].token, str
-                ), f"top_logprobs token was not a string"
-                assert isinstance(
-                    response.choices[0].logprobs.content[0].top_logprobs, list
-                ), f"top_logprobs was not a list"
-                ret_num_top_logprobs = len(
-                    response.choices[0].logprobs.content[0].top_logprobs
-                )
-                assert (
-                    ret_num_top_logprobs == logprobs
-                ), f"{ret_num_top_logprobs} vs {logprobs}"
-                top_logprob_tokens.append(
-                    response.choices[0].logprobs.content[0].top_logprobs[0].token
-                )
-
-            # assert (
-            #    len(top_logprob_tokens) <= 2 or len(set(top_logprob_tokens)) > 1
-            # ), "Top Logprob tokens should not consistent of the same token repeated"
             assert (
                 isinstance(data.content, str)
                 or isinstance(data.reasoning_content, str)
@@ -409,8 +311,9 @@ class BaseTestOpenAIServerWithHiddenStatesEnabled(ABC):
             ), "hidden_states was returned and should not have been"
 
 
-"""
-class TestOpenAIServerWithHiddenStatesEnabled(unittest.TestCase, BaseTestOpenAIServerWithHiddenStatesEnabled):
+class TestOpenAIServerWithHiddenStatesEnabled(
+    unittest.TestCase, BaseTestOpenAIServerWithHiddenStates
+):
     @classmethod
     def setUpClass(cls):
         cls.model = DEFAULT_SMALL_MODEL_NAME_FOR_TEST
@@ -421,7 +324,7 @@ class TestOpenAIServerWithHiddenStatesEnabled(unittest.TestCase, BaseTestOpenAIS
             cls.base_url,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             api_key=cls.api_key,
-            other_args=ENABLE_HS_ARGS
+            other_args=ENABLE_HS_ARGS,
         )
         cls.base_url += "/v1"
         cls.tokenizer = get_tokenizer(DEFAULT_SMALL_MODEL_NAME_FOR_TEST)
@@ -429,11 +332,10 @@ class TestOpenAIServerWithHiddenStatesEnabled(unittest.TestCase, BaseTestOpenAIS
     @classmethod
     def tearDownClass(cls):
         kill_process_tree(cls.process.pid)
-"""
 
 
 class TestOpenAIServerWithEAGLEAndHiddenStatesEnabled(
-    unittest.TestCase, BaseTestOpenAIServerWithHiddenStatesEnabled
+    unittest.TestCase, BaseTestOpenAIServerWithHiddenStates
 ):
     @classmethod
     def setUpClass(cls):
@@ -475,8 +377,9 @@ class TestOpenAIServerWithEAGLEAndHiddenStatesEnabled(
         kill_process_tree(cls.process.pid)
 
 
-"""
-class TestOpenAIServerWithEAGLE3AndHiddenStatesEnabled(unittest.TestCase, BaseTestOpenAIServerWithHiddenStatesEnabled):
+class TestOpenAIServerWithEAGLE3AndHiddenStatesEnabled(
+    unittest.TestCase, BaseTestOpenAIServerWithHiddenStates
+):
     @classmethod
     def setUpClass(cls):
         print("STARTING EAGLE 3 WITH HIDDEN STATES TEST SUITE")
@@ -514,11 +417,10 @@ class TestOpenAIServerWithEAGLE3AndHiddenStatesEnabled(unittest.TestCase, BaseTe
         cls.base_url += "/v1"
         cls.tokenizer = get_tokenizer(cls.model)
 
-
     @classmethod
     def tearDownClass(cls):
         kill_process_tree(cls.process.pid)
-"""
+
 
 if __name__ == "__main__":
     unittest.main()
