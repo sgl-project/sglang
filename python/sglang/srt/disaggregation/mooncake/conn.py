68e38896ac1ba28e27f424555b55306e438b626d
Trevor Morris <tmorris@nvidia.com>
2025-05-27 19:35:01 +0000

    precommit

--- python/sglang/srt/disaggregation/mooncake/conn.py
+++ python/sglang/srt/disaggregation/mooncake/conn.py
@@ -29,7 +29,10 @@ from sglang.srt.disaggregation.base.conn import (
     KVPoll,
 )
 from sglang.srt.disaggregation.mooncake.transfer_engine import MooncakeTransferEngine
-from sglang.srt.disaggregation.utils import DisaggregationMode, group_concurrent_contiguous
+from sglang.srt.disaggregation.utils import (
+    DisaggregationMode,
+    group_concurrent_contiguous,
+)
 from sglang.srt.server_args import ServerArgs
 from sglang.srt.utils import (
     get_free_port,
