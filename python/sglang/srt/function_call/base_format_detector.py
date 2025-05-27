6ad3b1804f59f352b1fc53668668055b1c4d56db
seongjiko <soc06202@naver.com>
2025-05-27 11:08:46 +0900

    fix: use correct tool index in streaming tool parse

--- python/sglang/srt/function_call/base_format_detector.py
+++ python/sglang/srt/function_call/base_format_detector.py
@@ -182,14 +182,20 @@ class BaseFormatDetector(ABC):
                         sent = len(self.streamed_args_for_tool[self.current_tool_id])
                         argument_diff = cur_args_json[sent:]
 
+                        cur_name = current_tool_call.get("name")
+                        true_index = self._tool_indices.get(cur_name, -1)
+                        if true_index == -1:
+                            logger.warning("Unknown tool name %s", cur_name)
+                            return StreamingParseResult()
+
                         res = StreamingParseResult(
                             calls=[
                                 ToolCallItem(
-                                    tool_index=self.current_tool_id,
+                                    tool_index=true_index,
                                     name="",
                                     parameters=argument_diff,
                                 )
-                            ],
+                            ]
                         )
                         self.streamed_args_for_tool[
                             self.current_tool_id
@@ -248,10 +254,16 @@ class BaseFormatDetector(ABC):
                             argument_diff = prefix[sent:]
 
                     if argument_diff is not None:
+                        function_name = current_tool_call.get("name")
+                        true_index = self._tool_indices.get(function_name, -1)
+                        if true_index == -1:
+                            logger.warning("Unknown tool name %s", function_name)
+                            return StreamingParseResult()
+
                         res = StreamingParseResult(
                             calls=[
                                 ToolCallItem(
-                                    tool_index=self.current_tool_id,
+                                    tool_index=true_index,
                                     parameters=argument_diff,
                                 )
                             ],
