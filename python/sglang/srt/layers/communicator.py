c80f333f6ed20dd6137fa7032a8419682ca7bdac
Cheng Wan <54331508+ch-wan@users.noreply.github.com>
2025-05-28 02:23:32 -0700

    Update communicator.py

--- python/sglang/srt/layers/communicator.py
+++ python/sglang/srt/layers/communicator.py
@@ -377,7 +377,7 @@ class CommunicateWithAllReduceAndLayerNormFn:
     ):
         if residual_input_mode == ScatterMode.SCATTERED:
             residual, local_residual = (
-                forward_batch.gathered_buffer[: forward_batch.input_ids.shape[0]],
+                forward_batch.gathered_buffer[: forward_batch.input_ids.shape[0]].clone(),
                 residual,
             )
             attn_tp_all_gather(
