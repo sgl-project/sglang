90366a8413ed8474a6cca1904640dd414f5b1a89
fzyzcjy <ch271828n@outlook.com>
2025-05-31 07:34:48 +0800

    more

--- python/sglang/srt/debug_utils.py
+++ python/sglang/srt/debug_utils.py
@@ -1,20 +1,23 @@
 import time
 from pathlib import Path
 
+import torch
+
 
 class _Dumper:
     def __init__(self):
         self._partial_name = str(time.time())
         self.forward_pass_id = None
 
-    def dump(self, name, value, **kwargs):
+    def dump(self, name, value):
         from sglang.srt.distributed import get_tensor_model_parallel_rank
 
         rank = get_tensor_model_parallel_rank()
-        full_filename = TODO
-        path = Path('/tmp') / f'sglang_dump_{self._partial_name}_{rank}' / f'{full_filename}.pt'
+        full_filename = f'F{self.forward_pass_id}__{name}.pt'
+        path = Path('/tmp') / f'sglang_dump_{self._partial_name}_{rank}' / full_filename
 
-        TODO
+        path.mkdir(parents=True, exist_ok=True)
+        torch.save(value, str(name))
 
 
 dumper = _Dumper()
