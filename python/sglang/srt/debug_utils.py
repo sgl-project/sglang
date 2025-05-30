ee16a5ee7c79e96cdacac3cd16574e1b8dc4dd3a
fzyzcjy <ch271828n@outlook.com>
2025-05-31 07:31:40 +0800

    more

--- python/sglang/srt/debug_utils.py
+++ python/sglang/srt/debug_utils.py
@@ -1,12 +1,17 @@
+import time
 from pathlib import Path
 
 
 class _Dumper:
     def __init__(self):
-        self._dir_base = Path('/tmp') / TODO
+        self._partial_name = str(time.time())
 
-    def dump(self, name, value):
+    def dump(self, name, value, **kwargs):
         from sglang.srt.distributed import get_tensor_model_parallel_rank
+
+        rank = get_tensor_model_parallel_rank()
+        full_filename = TODO
+        path = Path('/tmp') / f'sglang_dump_{self._partial_name}_{rank}' / f'{full_filename}.pt'
 
         TODO
 
