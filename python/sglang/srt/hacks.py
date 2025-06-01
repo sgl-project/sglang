a3c1a42d9cf50e2a0c19e8c7ac46d20bba5200ae
fzyzcjy <ch271828n@outlook.com>
2025-06-01 10:17:00 +0800

    more

--- python/sglang/srt/hacks.py
+++ python/sglang/srt/hacks.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import torch
 from sglang.srt.layers.quantization.fp8_utils import block_quant_dequant
 from tqdm import tqdm
@@ -52,4 +54,40 @@ def _requant_moe_weight(that, weight: torch.Tensor, weight_scale_inv: torch.Tens
         model_dtype,
     )
 
-    return TODO, TODO
+    return per_block_cast_to_fp8(weight_dequant)
+
+
+def ceil_to_ue8m0(x: torch.Tensor):
+    assert x.view(-1).amax().item() > 0
+    # TODO: stronger tests
+    return torch.pow(2.0, torch.ceil(torch.log2(x.abs())))
+
+
+def per_block_cast_to_fp8(x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    assert x.dim() == 2
+    m, n = x.shape
+    x_padded = torch.zeros((align(m, 128), align(n, 128)), dtype=x.dtype, device=x.device)
+    x_padded[:m, :n] = x
+    x_view = x_padded.view(-1, 128, x_padded.size(1) // 128, 128)
+    x_amax = x_view.abs().float().amax(dim=(1, 3), keepdim=True).clamp(1e-4)
+    sf = ceil_to_ue8m0(x_amax / 448.0)
+    x_scaled = (x_view * (1.0 / sf)).to(torch.float8_e4m3fn)
+    return x_scaled.view_as(x_padded)[:m, :n].contiguous(), sf.view(x_view.size(0), x_view.size(2))
+
+
+def ceil_div(x: int, y: int) -> int:
+    """
+    Perform ceiling division of two integers.
+
+    Args:
+        x: the dividend.
+        y: the divisor.
+
+    Returns:
+        The result of the ceiling division.
+    """
+    return (x + y - 1) // y
+
+
+def align(x: int, y: int) -> int:
+    return ceil_div(x, y) * y
