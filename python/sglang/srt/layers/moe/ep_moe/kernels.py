117f87402219e3ec38dfc4fc8205a8a6df9c36e6
fzyzcjy <ch271828n@outlook.com>
2025-06-01 14:41:21 +0800

    more

--- python/sglang/srt/layers/moe/ep_moe/kernels.py
+++ python/sglang/srt/layers/moe/ep_moe/kernels.py
@@ -251,8 +251,8 @@ def silu_and_mul_triton_kernel(
             tl.store(down_input_ptr + offset, silu_mul_output, mask=mask)
 
 
-HACK_SILU_AND_MUL_POST_QUANT_KERNEL_EXTRA_CEIL = get_bool_env_var(
-    'SGLANG_HACK_SILU_AND_MUL_POST_QUANT_KERNEL_EXTRA_CEIL')
+CONSTEXPR_HACK_SILU_AND_MUL_POST_QUANT_KERNEL_EXTRA_CEIL = tl.constexpr(get_bool_env_var(
+    'SGLANG_HACK_SILU_AND_MUL_POST_QUANT_KERNEL_EXTRA_CEIL'))
 
 
 # copy from https://github.com/ModelTC/lightllm/blob/a000ab69098654df4731f5b12587dd4e7f0a4f41/lightllm/common/fused_moe/moe_silu_and_mul_mix_quant_ep.py
@@ -317,7 +317,7 @@ def _silu_and_mul_post_quant_kernel(
         gate_up = up * gate
         _absmax = tl.maximum(tl.max(tl.abs(gate_up)), 1e-10)
         output_s = _absmax / fp8_max
-        if HACK_SILU_AND_MUL_POST_QUANT_KERNEL_EXTRA_CEIL:
+        if CONSTEXPR_HACK_SILU_AND_MUL_POST_QUANT_KERNEL_EXTRA_CEIL:
             output_s = tl.exp2(tl.ceil(tl.log2(tl.abs(output_s))))
         output_q = tl.clamp(gate_up / output_s, fp8_min, fp8_max).to(
             output_ptr.dtype.element_ty
