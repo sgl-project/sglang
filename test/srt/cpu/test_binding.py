fe07fbc1fbfcb6ba8052d536bcebd982ee0135a7
Wu, Chunyuan <chunyuan.wu@intel.com>
2025-06-03 09:53:35 +0000

    fix UT format

--- test/srt/cpu/test_binding.py
+++ test/srt/cpu/test_binding.py
@@ -3,6 +3,7 @@ import unittest
 
 import sgl_kernel
 import torch
+
 kernel = torch.ops.sgl_kernel
 
 from sglang.test.test_utils import CustomTestCase
@@ -15,12 +16,13 @@ class TestGemm(CustomTestCase):
         end_id = start_id + n_cpu - 1
         cpu_ids = f"{start_id}-{end_id}"
         output = kernel.init_cpu_threads_env(cpu_ids)
-       
+
         bindings = re.findall(r"OMP tid: \d+, core (\d+)", output)
         self.assertEqual(len(bindings), n_cpu)
 
         expected_cores = list(map(str, range(start_id, n_cpu)))
         self.assertEqual(bindings, expected_cores)
+
 
 if __name__ == "__main__":
     unittest.main()
