96a145c56456583cf24bb79d39c99d958e687020
Zheng, Beilei <beilei.zheng@intel.com>
2025-06-02 22:44:05 -0700

    add dtype check for shm_allreduce and shm_allgather

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -2612,3 +2612,7 @@ def get_cpu_ids_by_node():
 
     # ['0,1,2,3', '4,5,6,7', '8,9,10,11', '12,13,14,15', '16,17,18,19', '20,21,22,23']
     return cpu_ids
+
+
+def is_shm_available(dtype):
+    return cpu_has_amx_support() and dtype in [torch.bfloat16, torch.float]
