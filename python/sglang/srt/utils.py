867ba9611dfd56f7c21307b2a69940644455bc25
Zheng, Beilei <beilei.zheng@intel.com>
2025-06-04 01:11:18 -0700

    add world_size and local_size check for shm_allreduce and shm_allgather

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -2614,5 +2614,10 @@ def get_cpu_ids_by_node():
     return cpu_ids
 
 
-def is_shm_available(dtype):
-    return cpu_has_amx_support() and dtype in [torch.bfloat16, torch.float]
+def is_shm_available(dtype, world_size, local_size):
+    return (
+        cpu_has_amx_support()
+        and dtype in [torch.bfloat16, torch.float]
+        and world_size >= 1
+        and world_size == local_size
+    )
