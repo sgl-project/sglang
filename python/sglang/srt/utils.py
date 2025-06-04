0c29018608074466bd5b7c5c2e8918ca34ff1bcf
Zheng, Beilei <beilei.zheng@intel.com>
2025-06-04 01:11:18 -0700

    add world_size and local_size check for shm_allreduce and shm_allgather

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -2547,5 +2547,10 @@ def ceil_div(x: int, y: int) -> int:
     return (x + y - 1) // y
 
 
-def is_shm_available(dtype):
-    return cpu_has_amx_support() and dtype in [torch.bfloat16, torch.float]
+def is_shm_available(dtype, world_size, local_size):
+    return (
+        cpu_has_amx_support()
+        and dtype in [torch.bfloat16, torch.float]
+        and world_size >= 1
+        and world_size == local_size
+    )
