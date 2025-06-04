035bc6e3d531053611e5dc1444f8e273ae5194c6
Zheng, Beilei <beilei.zheng@intel.com>
2025-06-04 01:11:18 -0700

    add world_size and local_size check for shm_allreduce and shm_allgather

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -2342,5 +2342,10 @@ class LazyValue:
         return self._value
 
 
-def is_shm_available(dtype):
-    return cpu_has_amx_support() and dtype in [torch.bfloat16, torch.float]
+def is_shm_available(dtype, world_size, local_size):
+    return (
+        cpu_has_amx_support()
+        and dtype in [torch.bfloat16, torch.float]
+        and world_size >= 1
+        and world_size == local_size
+    )
