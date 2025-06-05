f8825f461e74bd8c5fb808541e87541bb43eba92
ser163tlt <ser163tlt@gmail.com>
2025-06-05 20:08:41 +0300

    fix_qwen2_eagle_map

--- python/sglang/srt/models/qwen2_eagle.py
+++ python/sglang/srt/models/qwen2_eagle.py
@@ -126,7 +126,7 @@ class Qwen2ForCausalLMEagle(Qwen2ForCausalLM):
             self.lm_head = self.model.embed_tokens
         else:
             self.lm_head = ParallelLMHead(
-                config.vocab_size,
+                getattr(config, "hot_vocab_size", config.vocab_size),
                 config.hidden_size,
                 quant_config=quant_config,
                 prefix=add_prefix("lm_head", prefix),
