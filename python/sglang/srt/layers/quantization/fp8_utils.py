74508e992d9ade0e73fb2a67990dc3e7402d1867
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-06-05 17:51:46 +0800

    Update fp8_utils.py

--- python/sglang/srt/layers/quantization/fp8_utils.py
+++ python/sglang/srt/layers/quantization/fp8_utils.py
@@ -227,7 +227,7 @@ def deepgemm_w8a8_block_fp8_linear_with_fallback(
     output_dtype = input.dtype
     dtype_supported = output_dtype == torch.bfloat16
 
-    # TODO: design shape check here
+    # TODO: https://github.com/sgl-project/sglang/pull/6890#issuecomment-2943395737
     shape_supported = weight.shape[0] % 64 == 0 and weight.shape[1] % 128 == 0
 
     if not (shape_supported and dtype_supported):
