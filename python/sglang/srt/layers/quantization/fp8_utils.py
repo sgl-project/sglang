3618f951254e895721677091410d39f7f6622cdc
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-06-05 16:12:34 +0800

    Update fp8_utils.py

--- python/sglang/srt/layers/quantization/fp8_utils.py
+++ python/sglang/srt/layers/quantization/fp8_utils.py
@@ -227,8 +227,9 @@ def deepgemm_w8a8_block_fp8_linear_with_fallback(
     output_dtype = input.dtype
     dtype_supported = output_dtype == torch.bfloat16
 
-    # TODO: add more robust shape check here
-    shape_supported = weight.shape[0] % 128 == 0 and weight.shape[1] % 128 == 0
+    # TODO: design shape check here
+    # shape_supported = weight.shape[0] % 128 == 0 and weight.shape[1] % 128 == 0
+    shape_supported = True
 
     if not (shape_supported and dtype_supported):
         # fall back to triton
