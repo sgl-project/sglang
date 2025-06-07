b62caeb72eaa4fd8ffe65e91c6785b6c7deeec48
Jayon02 <12012211@mail.sustech.edu.cn>
2025-06-07 18:23:06 +0000

    update

--- python/sglang/srt/layers/moe/ep_moe/layer.py
+++ python/sglang/srt/layers/moe/ep_moe/layer.py
@@ -369,6 +369,12 @@ class EPMoE(torch.nn.Module):
                 ),
             )
 
+        ones_tensor = torch.ones(
+                self.num_experts_per_partition,
+                dtype=torch.float32,
+                device=hidden_states_device,
+        )
+
         self.w2_input_scale = torch.nn.Parameter(
             ones_tensor,
             requires_grad=False,
