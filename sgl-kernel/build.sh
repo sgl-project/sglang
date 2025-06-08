e00383ec80e773b9d5cb1c8724e2ca127525af7b
zhyncs <me@zhyncs.com>
2025-06-07 23:10:51 -0700

    upd

--- sgl-kernel/build.sh
+++ sgl-kernel/build.sh
@@ -17,11 +17,7 @@ if [ ${ARCH} = "aarch64" ]; then
    BUILDER_NAME="pytorch/manylinuxaarch64-builder"
 else
    LIBCUDA_ARCH=${ARCH}
-   if [ ${CUDA_VERSION} = "12.8" ]; then
-      BUILDER_NAME="pytorch/manylinux2_28-builder"
-   else
-      BUILDER_NAME="pytorch/manylinux-builder"
-   fi
+   BUILDER_NAME="pytorch/manylinux2_28-builder"
 fi
 
 if [ ${CUDA_VERSION} = "12.8" ]; then
