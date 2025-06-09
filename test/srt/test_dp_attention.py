e15db54c8f349be9f2787621bb95d9471fb246a0
austindeng <austindeng@tencent.com>
2025-06-10 01:22:36 +0800

    Update mtp+dp-attention test cases (#6081)

--- test/srt/test_dp_attention.py
+++ test/srt/test_dp_attention.py
@@ -13,6 +13,7 @@ from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
+    is_in_amd_ci,
     popen_launch_server,
 )
 
@@ -77,8 +78,6 @@ class TestDPAttentionDP2TP2DeepseekV3MTP(CustomTestCase):
         cls.base_url = DEFAULT_URL_FOR_TEST
         other_args = [
             "--trust-remote-code",
-            "--cuda-graph-max-bs",
-            "4",
             "--disable-radix",
             "--speculative-algorithm",
             "EAGLE",
@@ -96,6 +95,8 @@ class TestDPAttentionDP2TP2DeepseekV3MTP(CustomTestCase):
             "--dp-size",
             "2",
         ]
+        if not is_in_amd_ci():
+            other_args += ["--mem-frac", "0.7"]
         cls.process = popen_launch_server(
             cls.model,
             cls.base_url,
