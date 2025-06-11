6806ed4eb3d1b44cc8319c1e593a48d2d27cc4e3
Lianmin Zheng <lianminzheng@gmail.com>
2025-06-10 19:01:39 -0700

    Simplify the heuristics for setting --mem-fraction-static (#7054)

--- python/sglang/srt/model_executor/cuda_graph_runner.py
+++ python/sglang/srt/model_executor/cuda_graph_runner.py
@@ -153,7 +153,7 @@ def get_batch_sizes_to_capture(model_runner: ModelRunner):
 
         gpu_mem = get_device_memory_capacity()
         if gpu_mem is not None:
-            if gpu_mem > 90 * 1024:  # H200
+            if gpu_mem > 90 * 1024:  # H200, H20
                 capture_bs += list(range(160, 257, 8))
             if gpu_mem > 160 * 1000:  # B200, MI300
                 capture_bs += list(range(256, 513, 16))
