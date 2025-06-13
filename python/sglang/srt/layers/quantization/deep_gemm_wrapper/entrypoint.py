bd9e256c1d0291c2c89ca3baa66951704c511351
fzyzcjy <ch271828n@outlook.com>
2025-06-13 14:12:05 +0800

    more

---
+++ python/sglang/srt/layers/quantization/deep_gemm_wrapper/entrypoint.py
@@ -0,0 +1,27 @@
+import torch
+from typing import Tuple
+
+
+def grouped_gemm_nt_f8f8bf16_masked(
+    lhs: Tuple[torch.Tensor, torch.Tensor],
+    rhs: Tuple[torch.Tensor, torch.Tensor],
+    out: torch.Tensor,
+    masked_m: torch.Tensor,
+    expected_m: int,
+):
+    TODO
+
+def grouped_gemm_nt_f8f8bf16_contig(
+    lhs: Tuple[torch.Tensor, torch.Tensor],
+    rhs: Tuple[torch.Tensor, torch.Tensor],
+    out: torch.Tensor,
+    m_indices: torch.Tensor,
+):
+    TODO
+
+def gemm_nt_f8f8bf16(
+    lhs: Tuple[torch.Tensor, torch.Tensor],
+    rhs: Tuple[torch.Tensor, torch.Tensor],
+    out: torch.Tensor,
+):
+    TODO
