68774130eaeb42a1288d5eae4f0ba9d541bf2d63
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-06-13 16:58:29 +0800

    Remove 200us slow concat kernel (part 1: kernel) (#7145)

--- sgl-kernel/include/sgl_kernel_ops.h
+++ sgl-kernel/include/sgl_kernel_ops.h
@@ -105,7 +105,8 @@ void merge_state_v2(
     at::Tensor v_a, at::Tensor s_a, at::Tensor v_b, at::Tensor s_b, at::Tensor v_merged, at::Tensor s_merged);
 void cutlass_mla_decode(
     torch::Tensor const& out,
-    torch::Tensor const& q_nope_and_q_pe,
+    torch::Tensor const& q_nope,
+    torch::Tensor const& q_pe,
     torch::Tensor const& kv_c_and_k_pe_cache,
     torch::Tensor const& seq_lens,
     torch::Tensor const& page_table,
