1d5b0943d0ce736f2aaa176eb22364f99060da25
fzyzcjy <ch271828n@outlook.com>
2025-06-13 13:52:26 +0800

    rm unused

--- python/sglang/srt/hacks.py
+++ python/sglang/srt/hacks.py
@@ -7,64 +7,6 @@ from tqdm import trange
 from sglang.srt.layers.moe.ep_moe.layer import DeepEPMoE
 from sglang.srt.layers.quantization.fp8_utils import block_quant_dequant
 
-# def hack_requant_moe_weight(that, weights):
-#     print("hi hack_requant_moe_weight")
-#
-#     weights_dict = dict(list(weights))
-#     del weights
-#
-#     moe_layers = range(
-#         that.config.first_k_dense_replace,
-#         that.config.num_hidden_layers,
-#         that.config.moe_layer_freq,
-#     )
-#
-#     module_names = [
-#         "down_proj",
-#         "gate_proj",
-#         "up_proj",
-#     ]
-#
-#     for moe_layer in moe_layers:
-#         for expert_index in trange(
-#             that.config.n_routed_experts, desc=f"layer={moe_layer}"
-#         ):
-#             for module_name in module_names:
-#                 partial_name = (
-#                     f"model.layers.{moe_layer}.mlp.experts.{expert_index}.{module_name}"
-#                 )
-#                 name_weight = partial_name + ".weight"
-#                 name_weight_scale_inv = partial_name + ".weight_scale_inv"
-#
-#                 weight_new, weight_scale_inv_new = _requant_moe_weight(
-#                     that, weights_dict[name_weight], weights_dict[name_weight_scale_inv]
-#                 )
-#
-#                 weights_dict[name_weight] = weight_new
-#                 weights_dict[name_weight_scale_inv] = weight_scale_inv_new
-#
-#     return list(weights_dict.items())
-#
-#
-# def _requant_moe_weight(that, weight: torch.Tensor, weight_scale_inv: torch.Tensor):
-#     weight_block_size = that.quant_config.weight_block_size
-#
-#     assert weight_block_size == [128, 128]
-#
-#     weight_dequant = block_quant_dequant(
-#         weight,
-#         # TODO does "inv" have trouble?
-#         weight_scale_inv,
-#         weight_block_size,
-#         # TODO correct?
-#         torch.float32,
-#     )
-#
-#     return per_block_cast_to_fp8(weight_dequant)
-#
-
-
-
 
 def requant_weight_ue8m0_inplace(that, weight, weight_scale_inv):
     assert isinstance(weight, torch.nn.Parameter)
@@ -75,7 +17,7 @@ def requant_weight_ue8m0_inplace(that, weight, weight_scale_inv):
 
 
 def _requant_weight_ue8m0(
-    that, weight: torch.Tensor, weight_scale_inv: torch.Tensor
+        that, weight: torch.Tensor, weight_scale_inv: torch.Tensor
 ):
     weight_block_size = that.quant_config.weight_block_size
     assert weight_block_size == [128, 128]
@@ -90,32 +32,13 @@ def _requant_weight_ue8m0(
         torch.bfloat16,
     )
 
-    # weight_dequant_flat = einops.rearrange(weight_dequant, "num_group n k -> (num_group n) k")
     weight_dequant_flat = weight_dequant.view((-1, k))
     out_w_flat, out_s_flat = per_block_cast_to_fp8(weight_dequant_flat)
-
-    # def _unflatten(x):
-    #     return einops.rearrange(
-    #         x,
-    #         "(num_group n_div_128) whatever_div_128 -> num_group n_div_128 whatever_div_128",
-    #         num_group=num_group,
-    #     )
-    #
-    # return _unflatten(out_w_flat), _unflatten(out_s_flat)
 
     out_w = out_w_flat.view(weight.shape)
     out_s = out_s_flat.view(weight_scale_inv.shape)
 
     out_s = _transform_scale(out_s, mn=out_w.shape[-2])
-
-    # print(
-    #     f"requant_grouped_moe_weight "
-    #     f"{type(weight)=} {type(weight_scale_inv)=} "
-    #     f"{weight.shape=} {weight.dtype=} "
-    #     f"{weight_scale_inv.shape=} {weight_scale_inv.dtype=} "
-    #     f"{out_w.shape=} {out_w.dtype=} "
-    #     f"{out_s.shape=} {out_s.dtype=} "
-    # )
 
     return out_w, out_s
 
