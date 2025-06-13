5e59edae8bd8f1d4d051121a4afe33979b453d41
fzyzcjy <ch271828n@outlook.com>
2025-06-13 14:47:49 +0800

    more

--- python/sglang/srt/layers/quantization/deep_gemm_wrapper/entrypoint.py
+++ python/sglang/srt/layers/quantization/deep_gemm_wrapper/entrypoint.py
@@ -4,9 +4,7 @@ from typing import Tuple
 
 import torch
 
-from sglang.srt.layers.quantization.deep_gemm_wrapper.compile_utils import (
-    DeepGemmKernelType, deep_gemm_execution_hook,
-)
+from sglang.srt.layers.quantization.deep_gemm_wrapper import compile_utils
 from sglang.srt.server_args import ServerArgs
 from sglang.srt.utils import get_bool_env_var, get_device_sm
 
@@ -63,9 +61,9 @@ def grouped_gemm_nt_f8f8bf16_masked(
 ):
     num_groups, _, k = lhs[0].shape
     _, n, _ = rhs[0].shape
-    kernel_type = DeepGemmKernelType.GROUPED_GEMM_NT_F8F8BF16_MASKED
+    kernel_type = compile_utils.DeepGemmKernelType.GROUPED_GEMM_NT_F8F8BF16_MASKED
 
-    with deep_gemm_execution_hook(expected_m, n, k, num_groups, kernel_type):
+    with compile_utils.deep_gemm_execution_hook(expected_m, n, k, num_groups, kernel_type):
         _grouped_gemm_nt_f8f8bf16_masked_raw(lhs, rhs, out, masked_m, expected_m)
 
 
@@ -77,9 +75,9 @@ def grouped_gemm_nt_f8f8bf16_contig(
 ):
     m, k = lhs[0].shape
     num_groups, n, _ = rhs[0].shape
-    kernel_type = DeepGemmKernelType.GROUPED_GEMM_NT_F8F8BF16_CONTIG
+    kernel_type = compile_utils.DeepGemmKernelType.GROUPED_GEMM_NT_F8F8BF16_CONTIG
 
-    with deep_gemm_execution_hook(m, n, k, num_groups, kernel_type):
+    with compile_utils.deep_gemm_execution_hook(m, n, k, num_groups, kernel_type):
         _grouped_gemm_nt_f8f8bf16_contig_raw(lhs, rhs, out, m_indices)
 
 
@@ -91,9 +89,9 @@ def gemm_nt_f8f8bf16(
     m, k = lhs[0].shape
     n, _ = rhs[0].shape
     num_groups = 1
-    kernel_type = DeepGemmKernelType.GEMM_NT_F8F8BF16
+    kernel_type = compile_utils.DeepGemmKernelType.GEMM_NT_F8F8BF16
 
-    with deep_gemm_execution_hook(m, n, k, num_groups, kernel_type):
+    with compile_utils.deep_gemm_execution_hook(m, n, k, num_groups, kernel_type):
         _gemm_nt_f8f8bf16_raw(
             lhs,
             rhs,
