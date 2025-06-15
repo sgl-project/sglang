3253de60922e2f5bc2b0cc939add98b2a739e881
hebiao064 <hebiaobuaa@gmail.com>
2025-06-15 23:22:24 +0000

    update tms usage

--- python/sglang/srt/torch_memory_saver_adapter.py
+++ python/sglang/srt/torch_memory_saver_adapter.py
@@ -8,7 +8,7 @@ try:
     import torch_memory_saver
 
     # Use the global singleton instance
-    _memory_saver = torch_memory_saver.memory_saver
+    _memory_saver = torch_memory_saver.torch_memory_saver
     import_error = None
 except ImportError as e:
     import_error = e
