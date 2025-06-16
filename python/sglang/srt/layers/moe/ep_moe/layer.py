b51f324ec3a9f39ba579d1accfb5f17e2cee1c45
TianQiLin666666 <1834987979@qq.com>
2025-06-16 17:09:03 +0800

    assert act=="silu" in epmoe forward_deepgemm

--- python/sglang/srt/layers/moe/ep_moe/layer.py
+++ python/sglang/srt/layers/moe/ep_moe/layer.py
@@ -254,6 +254,7 @@ class EPMoE(torch.nn.Module):
         self, hidden_states: torch.Tensor, router_logits: torch.Tensor
     ):
         assert self.quant_method is not None
+        assert self.activation == "silu"
         hidden_states_shape = hidden_states.shape
         hidden_states_dtype = hidden_states.dtype
         hidden_states_device = hidden_states.device
