a97ba11ec8f13e18ac57b6a6b54a7abd143b0753
Shangming Cai <caishangming@linux.alibaba.com>
2025-06-16 21:18:18 +0800

    fix isort
    Signed-off-by: Shangming Cai <caishangming@linux.alibaba.com>

--- python/sglang/srt/disaggregation/mooncake/conn.py
+++ python/sglang/srt/disaggregation/mooncake/conn.py
@@ -35,12 +35,7 @@ from sglang.srt.disaggregation.common.utils import (
 from sglang.srt.disaggregation.mooncake.transfer_engine import MooncakeTransferEngine
 from sglang.srt.disaggregation.utils import DisaggregationMode
 from sglang.srt.server_args import ServerArgs
-from sglang.srt.utils import (
-    get_free_port,
-    get_int_env_var,
-    get_ip,
-    get_local_ip_auto,
-)
+from sglang.srt.utils import get_free_port, get_int_env_var, get_ip, get_local_ip_auto
 
 logger = logging.getLogger(__name__)
 
