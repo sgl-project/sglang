7d511837885433b5b4c3e5d8bfd17850b713e47a
Mick <mickjagger19@icloud.com>
2025-06-17 15:15:49 +0800

    cleanup

--- python/sglang/srt/hf_transformers_utils.py
+++ python/sglang/srt/hf_transformers_utils.py
@@ -220,8 +220,7 @@ def get_tokenizer(
         if kwargs.get("use_fast", False):
             raise ValueError("Cannot use the fast tokenizer in slow tokenizer mode.")
         kwargs["use_fast"] = False
-    # print(f"{tokenizer_name=}")
-    # print(f"{tokenizer_mode=}")
+
     # TODO(Xinyuan): Remove this once we have a proper tokenizer for Devstral
     if tokenizer_name == "mistralai/Devstral-Small-2505":
         tokenizer_name = "mistralai/Mistral-Small-3.1-24B-Instruct-2503"
