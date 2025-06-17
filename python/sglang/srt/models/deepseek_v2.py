1505dd5cc51e18ad0602dfb62e37bf69405890ac
yiakwy-xpu-ml-framework-team <961186938@qq.com>
2025-06-17 17:46:16 +0800

    remove debug info

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -608,20 +608,12 @@ class DeepseekV2AttentionMLA(nn.Module):
 
         # For tensor parallel attention
         if self.q_lora_rank is not None:
-
-            # DEBUG (yiakwy) : remove
-            # if layer_id == 52:
-            #     from remote_pdb import set_trace
-            #     set_trace()
-            #     pass
-
             self.fused_qkv_a_proj_with_mqa = ReplicatedLinear(
                 self.hidden_size,
                 self.q_lora_rank + self.kv_lora_rank + self.qk_rope_head_dim,
                 bias=False,
                 quant_config=quant_config,
                 prefix=add_prefix("fused_qkv_a_proj_with_mqa", prefix),
-                layer_id=layer_id,
                 fused_parameters=2,
             )
             self.q_a_layernorm = RMSNorm(self.q_lora_rank, eps=config.rms_norm_eps)
@@ -2015,7 +2007,7 @@ class DeepseekV2ForCausalLM(nn.Module):
                         "up_proj.weight_scale_2",
                         "up_proj.input_scale",
                     ]
-                elif self.quant_config.get_name() == "compressed_tensors": 
+                elif self.quant_config.get_name() == "compressed_tensors":
                     # Note (yiakwy) see supported quantiztion methods in https://github.com/neuralmagic/compressed-tensors/blob/main/src/compressed_tensors/quantization/quant_scheme.py
                     suffix_list = [
                         "down_proj.weight",
