f3f8d15fbfa6ae83109d4bcb2dc4b7ee88a4cb10
ch-tiger1 <xyz@ch-tech.ip-ddns.com>
2025-06-18 16:50:52 +0800

    fix chunk too big
    Signed-off-by: ch-tiger1 <xyz@ch-tech.ip-ddns.com>

--- python/sglang/srt/disaggregation/mini_lb.py
+++ python/sglang/srt/disaggregation/mini_lb.py
@@ -20,7 +20,7 @@ from sglang.srt.disaggregation.utils import PDRegistryRequest
 
 AIOHTTP_STREAM_READ_CHUNK_SIZE = (
     1024 * 64
-)   # 64KB, to prevent aiohttp's "Chunk too big" error
+)  # 64KB, to prevent aiohttp's "Chunk too big" error
 
 def setup_logger():
     logger = logging.getLogger("pdlb")
