debc33d2e25f71b22b971c6fd946c4b07bf5f28a
Bumsik Kim <k.bumsik@gmail.com>
2025-06-18 22:26:22 +0000

    fix

--- python/sglang/srt/layers/attention/hip_attention.py
+++ python/sglang/srt/layers/attention/hip_attention.py
@@ -306,7 +306,7 @@ class HiPAttentionBackend(AttentionBackend):
         
         is_decode = False
         need_dense_prefill = using_chunked_sw or using_dense_prefill
-        need_dense_decode = using_chunked_sw or delta_dense_decode or force_dense_decode
+        need_dense_decode = using_chunked_sw # or delta_dense_decode or force_dense_decode
         
         if need_dense_decode:
             o = self.flashattention_backend.forward_decode(
