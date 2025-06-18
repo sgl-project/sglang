ca084b14131710a63fc2cdec2ff524d1cc22ccae
johnnynunez <johnnynuca14@gmail.com>
2025-06-18 15:18:14 +0200

    remove 12.1 not presented in cuda 12.8

--- sgl-kernel/build.sh
+++ sgl-kernel/build.sh
@@ -62,7 +62,7 @@ docker run --rm \
    ln -sv /usr/lib64/libibverbs.so.1 /usr/lib64/libibverbs.so && \
    ${PYTHON_ROOT_PATH}/bin/${TORCH_INSTALL} && \
    ${PYTHON_ROOT_PATH}/bin/pip install --no-cache-dir ninja setuptools==75.0.0 wheel==0.41.0 numpy uv scikit-build-core && \
-   export TORCH_CUDA_ARCH_LIST='7.5 8.0 8.9 9.0 10.0 12.0 12.1+PTX' && \
+   export TORCH_CUDA_ARCH_LIST='7.5 8.0 8.9 9.0 10.0+PTX' && \
    export CUDA_VERSION=${CUDA_VERSION} && \
    export MAX_JOBS=${MAX_JOBS} && \
    export NVCC_THREADS=${NVCC_THREADS} && \
