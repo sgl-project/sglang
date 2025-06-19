064f5187d59f2baf259df024246ee7e3454cf164
Atream <zhangbx24@mails.tsinghua.edu.cn>
2025-06-19 12:35:21 +0000

    fix bug

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1696,13 +1696,15 @@ class Scheduler(
             # These 2 values are needed for processing the output, but the values can be
             # modified by overlap schedule. So we have to copy them here so that
             # we can use the correct values in output processing.
-            if batch.return_logprob:
+            if batch.return_logprob or self.spec_algorithm.is_eagle():
                 extend_input_len_per_req = [req.extend_input_len for req in batch.reqs]
+            else:
+                extend_input_len_per_req = None
+            if batch.return_logprob:
                 extend_logprob_start_len_per_req = [
                     req.extend_logprob_start_len for req in batch.reqs
                 ]
             else:
-                extend_input_len_per_req = None
                 extend_logprob_start_len_per_req = None
 
             ret = GenerationBatchResult(
