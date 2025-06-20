3410cc68139f72395f69d75c1695ccf65bead275
keru <rukeyang@gmail.com>
2025-06-20 16:27:24 +0000

    Remove unused method

--- python/sglang/srt/entrypoints/openai/protocol.py
+++ python/sglang/srt/entrypoints/openai/protocol.py
@@ -593,8 +593,3 @@ OpenAIServingRequest = Union[
     ScoringRequest,
     V1RerankReqInput,
 ]
-
-
-def exclude_if_none(obj, field_names: List[str]):
-    omit_if_none_fields = {k for k, v in obj.model_fields.items() if k in field_names}
-    return {k: v for k, v in obj if k not in omit_if_none_fields or v is not None}
