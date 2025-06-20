5fc9d97b3dcc6992595276ea55db49726ec71f63
Ata Fatahi <immrata@gmail.com>
2025-06-19 23:55:16 -0700

    rewrite gpu memory usage
    Signed-off-by: Ata Fatahi <immrata@gmail.com>

--- test/srt/test_multi_instance_release_memory_occupation.py
+++ test/srt/test_multi_instance_release_memory_occupation.py
@@ -19,23 +19,73 @@ from sglang.test.test_utils import (
     find_available_port,
 )
 
-MODEL_INFO = dict(
+TEST_SUITE = dict(
     model_path=DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
-    mem_fraction_static=0.7,
+    mem_fraction_static=0.85,
     dp_size=2,
     tp_size=2,
 )
 
 
+class EngineWrapper:
+    """
+    A wrapper around Sglang engine to mock multi instance cases such as RL traing.
+
+    """
+
+    def __init__(
+        self, model_path, random_seed, mem_fraction_static, device_mesh_cpu, base_gpu_id
+    ):
+        self._device_mesh_cpu = device_mesh_cpu
+        self._tp_rank = device_mesh_cpu.get_local_rank()
+        self._rank = device_mesh_cpu.get_rank()
+        self._tp_size = device_mesh_cpu.size()
+        tp_size_per_node = self._tp_size
+        node_rank = self._tp_rank // tp_size_per_node
+        first_rank_in_node = self._tp_rank % tp_size_per_node == 0
+        engine_kwargs = dict(
+            model_path=model_path,
+            random_seed=random_seed,
+            mem_fraction_static=mem_fraction_static,
+            base_gpu_id=base_gpu_id,
+            enable_memory_saver=True,
+            tp_size=self._tp_size,
+            node_rank=node_rank,
+            nnodes=1,
+        )
+        self._engine = None
+        if first_rank_in_node:
+            os.environ["SGLANG_BLOCK_NONZERO_RANK_CHILDREN"] = "0"
+            self._engine = SglangEngine(**engine_kwargs)
+
+        dist.barrier(group=self._device_mesh_cpu.get_group())
+
+    def update_weights_from_tensor(
+        self, named_tensors: Iterable[Tuple[str, torch.Tensor]]
+    ):
+        if self._tp_rank == 0:
+            self._engine.update_weights_from_tensor(list(named_tensors))
+            self._engine.flush_cache()
+        dist.barrier(group=self._device_mesh_cpu.get_group())
+
+    def release_memory_occupation(self, tags):
+        if self._tp_rank == 0:
+            self._engine.release_memory_occupation(tags)
+        dist.barrier(group=self._device_mesh_cpu.get_group())
+
+    def resume_memory_occupation(self, tags):
+        if self._tp_rank == 0:
+            self._engine.resume_memory_occupation(tags)
+        dist.barrier(group=self._device_mesh_cpu.get_group())
+
+    def shutdown(self):
+        if self._tp_rank == 0:
+            self._engine.shutdown()
+        dist.barrier(group=self._device_mesh_cpu.get_group())
+
+
 def get_gpu_memory_gb(gpu_id=0):
-    cmd_list = [
-        "nvidia-smi",
-        "--query-gpu=memory.used",
-        "--format=csv,noheader,nounits",
-        f"--id={gpu_id}",
-    ]
-    result = subprocess.check_output(cmd_list, text=True, shell=False).strip()
-    return int(result) / 1024
+    return torch.cuda.device_memory_used() / 1024**3
 
 
 class TestMultiInstanceReleaseMemoryOccupation(CustomTestCase):
@@ -46,8 +96,8 @@ class TestMultiInstanceReleaseMemoryOccupation(CustomTestCase):
     def test_multi_instance_release_memory_occupation(self):
         master_port = find_available_port(23456)
 
-        dp_size = MODEL_INFO["dp_size"]
-        tp_size = MODEL_INFO["tp_size"]
+        dp_size = TEST_SUITE["dp_size"]
+        tp_size = TEST_SUITE["tp_size"]
         world_size = dp_size * tp_size
         processes = []
         output_reader, output_writer = multiprocessing.Pipe(duplex=False)
@@ -58,10 +108,10 @@ class TestMultiInstanceReleaseMemoryOccupation(CustomTestCase):
                     rank=rank,
                     dp_size=dp_size,
                     tp_size=tp_size,
-                    model_path=MODEL_INFO["model_path"],
+                    model_path=TEST_SUITE["model_path"],
                     master_port=master_port,
                     output_writer=output_writer,
-                    mem_fraction_static=MODEL_INFO["mem_fraction_static"],
+                    mem_fraction_static=TEST_SUITE["mem_fraction_static"],
                 ),
             )
             p.start()
@@ -194,63 +244,6 @@ def _run_sglang_subprocess(
 
     if engine:
         engine.shutdown()
-
-
-class EngineWrapper:
-    """
-    A wrapper around Sglang engine to mock multi instance cases such as RL traing.
-
-    """
-
-    def __init__(
-        self, model_path, random_seed, mem_fraction_static, device_mesh_cpu, base_gpu_id
-    ):
-        self._device_mesh_cpu = device_mesh_cpu
-        self._tp_rank = device_mesh_cpu.get_local_rank()
-        self._rank = device_mesh_cpu.get_rank()
-        self._tp_size = device_mesh_cpu.size()
-        tp_size_per_node = self._tp_size
-        node_rank = self._tp_rank // tp_size_per_node
-        first_rank_in_node = self._tp_rank % tp_size_per_node == 0
-        engine_kwargs = dict(
-            model_path=model_path,
-            random_seed=random_seed,
-            mem_fraction_static=mem_fraction_static,
-            base_gpu_id=base_gpu_id,
-            enable_memory_saver=True,
-            tp_size=self._tp_size,
-            node_rank=node_rank,
-            nnodes=1,
-        )
-        self._engine = None
-        if first_rank_in_node:
-            os.environ["SGLANG_BLOCK_NONZERO_RANK_CHILDREN"] = "0"
-            self._engine = SglangEngine(**engine_kwargs)
-
-        dist.barrier(group=self._device_mesh_cpu.get_group())
-
-    def update_weights_from_tensor(
-        self, named_tensors: Iterable[Tuple[str, torch.Tensor]]
-    ):
-        if self._tp_rank == 0:
-            self._engine.update_weights_from_tensor(list(named_tensors))
-            self._engine.flush_cache()
-        dist.barrier(group=self._device_mesh_cpu.get_group())
-
-    def release_memory_occupation(self, tags):
-        if self._tp_rank == 0:
-            self._engine.release_memory_occupation(tags)
-        dist.barrier(group=self._device_mesh_cpu.get_group())
-
-    def resume_memory_occupation(self, tags):
-        if self._tp_rank == 0:
-            self._engine.resume_memory_occupation(tags)
-        dist.barrier(group=self._device_mesh_cpu.get_group())
-
-    def shutdown(self):
-        if self._tp_rank == 0:
-            self._engine.shutdown()
-        dist.barrier(group=self._device_mesh_cpu.get_group())
 
 
 if __name__ == "__main__":
