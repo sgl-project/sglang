b4b01ddc4fe575ee2aa0d9bc8467c817e26764c5
fzyzcjy <ch271828n@outlook.com>
2025-06-21 08:11:03 +0800

    cherry pick

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -141,6 +141,7 @@ from sglang.srt.utils import (
     DeepEPMode,
     DynamicGradMode,
     broadcast_pyobj,
+    configure_gc_logger,
     configure_logger,
     disable_request_logging,
     get_available_gpu_memory,
@@ -508,6 +509,9 @@ class Scheduler(
             self.server_args.disaggregation_mode
         )
         self.init_disaggregation()
+
+        if get_bool_env_var("SGLANG_GC_LOG"):
+            configure_gc_logger()
 
     def maybe_sleep_on_idle(self):
         if self.idle_sleeper is not None:
