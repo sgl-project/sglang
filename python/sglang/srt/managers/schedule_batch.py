3cb79d53f6edf9500539fdddf1e43ac78ef5a9f0
Lianmin Zheng <lianminzheng@gmail.com>
2025-06-23 00:02:47 -0700

    update sync

--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -786,6 +786,7 @@ class Req:
         self.multimodal_inputs = None
         self.grammar = None
         self.origin_input_ids = [0]  # set it to one token to skip the long prefill
+        self.return_logprob = False
         self.finished_reason = FINISH_ABORT(
             error_msg, HTTPStatus.BAD_REQUEST, "BadRequestError"
         )
