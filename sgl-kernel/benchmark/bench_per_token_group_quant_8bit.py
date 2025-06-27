bae3049265f48269e322e4640fea152518efdd5e
fzyzcjy <ch271828n@outlook.com>
2025-06-27 17:29:28 +0800

    more

--- sgl-kernel/benchmark/bench_per_token_group_quant_8bit.py
+++ sgl-kernel/benchmark/bench_per_token_group_quant_8bit.py
@@ -13,29 +13,6 @@ _is_hip = is_hip()
 fp8_type_ = torch.float8_e4m3fnuz if _is_hip else torch.float8_e4m3fn
 
 
-def calculate_diff(batch_size, seq_len, group_size, dst_dtype):
-    device = torch.device("cuda")
-    hidden_dim = 7168
-
-    x = torch.randn(
-        batch_size * seq_len, hidden_dim, device=device, dtype=torch.float16
-    )
-
-    x_q_triton, x_s_triton = triton_per_token_group_quant_8bit(
-        x.clone(), group_size, dst_dtype
-    )
-    x_q_sglang, x_s_sglang = sglang_per_token_group_quant_8bit(
-        x.clone(), group_size, dst_dtype
-    )
-
-    if torch.allclose(
-        x_q_triton.to(torch.float32), x_q_sglang.to(torch.float32), rtol=1e-3, atol=1e-5
-    ) and torch.allclose(x_s_triton, x_s_sglang, rtol=1e-3, atol=1e-5):
-        print(f"✅ {dst_dtype} implementations match")
-    else:
-        print("❌ Implementations differ")
-
-
 batch_size_range = [1, 2, 4, 8, 16, 32, 64]
 seq_len_range = [64, 128, 256, 512, 1024, 2048]
 group_size_range = [128]  # For DeepSeek V3/R1
@@ -82,8 +59,4 @@ def benchmark(batch_size, seq_len, group_size, dst_dtype, provider):
 
 
 if __name__ == "__main__":
-
-    calculate_diff(batch_size=4, seq_len=128, group_size=64, dst_dtype=torch.int8)
-    calculate_diff(batch_size=4, seq_len=128, group_size=64, dst_dtype=fp8_type_)
-
     benchmark.run(print_data=True)
