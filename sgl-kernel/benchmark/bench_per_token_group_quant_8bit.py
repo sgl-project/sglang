28b9d00e9089f41d2ab572d8d34ae3a5a4fc6910
fzyzcjy <ch271828n@outlook.com>
2025-06-27 20:10:31 +0800

    more

--- sgl-kernel/benchmark/bench_per_token_group_quant_8bit.py
+++ sgl-kernel/benchmark/bench_per_token_group_quant_8bit.py
@@ -17,16 +17,7 @@ num_tokens_range = [1, 4, 16, 64, 256, 768, 2048, 8192, 16384]
 group_size_range = [128]  # For DeepSeek V3/R1
 dst_dtype_range = [torch.int8, fp8_type_]
 flags_range = [
-    dict(
-        column_major_scales=False,
-        scale_tma_aligned=False,
-        scale_ue8m0=False,
-    ),
-    dict(
-        column_major_scales=True,
-        scale_tma_aligned=False,
-        scale_ue8m0=False,
-    ),
+    # TODO support more combinations
     dict(
         column_major_scales=True,
         scale_tma_aligned=True,
