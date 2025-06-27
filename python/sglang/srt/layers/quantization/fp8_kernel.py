fcae3938bd38f7d16b9deb9541c17c674cc06eac
fzyzcjy <ch271828n@outlook.com>
2025-06-27 17:51:15 +0800

    fmt

--- python/sglang/srt/layers/quantization/fp8_kernel.py
+++ python/sglang/srt/layers/quantization/fp8_kernel.py
@@ -267,7 +267,8 @@ def per_token_group_quant_fp8(
         assert group_size == 128
         x_s = transform_sf_into_required_layout(
             x_s,
-            mn=x_q.shape[1], k=x_q.shape[2],
+            mn=x_q.shape[1],
+            k=x_q.shape[2],
             recipe=(1, 128, 128),
             num_groups=x_q.shape[0],
             is_sfa=True,
