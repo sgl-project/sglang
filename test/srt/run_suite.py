b980095c60d6a9f5fed78ae46f0c3b0e9a330d61
Hubert Lu <Hubert.Lu@amd.com>
2025-06-27 19:47:42 +0000

    Revert "Add test_rope_rocm.py to AMD CI"
    This reverts commit ec7429e00112433eb53313b0eceb89c37bedcc08.

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -142,7 +142,6 @@ suites = {
         TestFile("test_vertex_endpoint.py", 31),
         TestFile("test_vision_chunked_prefill.py", 175),
         TestFile("test_reasoning_parser.py", 5),
-        TestFile("test_rope_rocm.py", 3),
         TestFile("test_awq_dequant.py", 2),
     ],
     "per-commit-2-gpu": [
