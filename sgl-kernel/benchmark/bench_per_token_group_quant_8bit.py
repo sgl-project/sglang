693c7c656f00625a7c488db4f4c46373981ad1e8
fzyzcjy <ch271828n@outlook.com>
2025-06-27 17:55:00 +0800

    more

--- sgl-kernel/benchmark/bench_per_token_group_quant_8bit.py
+++ sgl-kernel/benchmark/bench_per_token_group_quant_8bit.py
@@ -17,17 +17,18 @@ batch_size_range = [1, 2, 4, 8, 16, 32, 64]
 seq_len_range = [64, 128, 256, 512, 1024, 2048]
 group_size_range = [128]  # For DeepSeek V3/R1
 dst_dtype_range = [torch.int8, fp8_type_]
+flags_range = [TODO]
 
 configs = list(
     itertools.product(
-        batch_size_range, seq_len_range, group_size_range, dst_dtype_range
+        batch_size_range, seq_len_range, group_size_range, dst_dtype_range, flags_range
     )
 )
 
 
 @triton.testing.perf_report(
     triton.testing.Benchmark(
-        x_names=["batch_size", "seq_len", "group_size", "dst_dtype"],
+        x_names=["batch_size", "seq_len", "group_size", "dst_dtype", "flags"],
         x_vals=configs,
         line_arg="provider",
         line_vals=["triton", "sglang"],
@@ -38,7 +39,7 @@ configs = list(
         args={},
     )
 )
-def benchmark(batch_size, seq_len, group_size, dst_dtype, provider):
+def benchmark(batch_size, seq_len, group_size, dst_dtype, flags, provider):
     device = torch.device("cuda")
     hidden_dim = 7168
 
@@ -49,9 +50,9 @@ def benchmark(batch_size, seq_len, group_size, dst_dtype, provider):
     quantiles = [0.5, 0.2, 0.8]
 
     if provider == "triton":
-        fn = lambda: triton_per_token_group_quant_8bit(x.clone(), group_size, dst_dtype)
+        fn = lambda: triton_per_token_group_quant_8bit(x.clone(), group_size, dst_dtype, **flags)
     elif provider == "sglang":
-        fn = lambda: sglang_per_token_group_quant_8bit(x.clone(), group_size, dst_dtype)
+        fn = lambda: sglang_per_token_group_quant_8bit(x.clone(), group_size, dst_dtype, **flags)
 
     ms, min_ms, max_ms = triton.testing.do_bench(fn, quantiles=quantiles)
 
