12f85c2931d88cd093d6c4b619441c4a42888de2
fzyzcjy <ch271828n@outlook.com>
2025-06-28 17:10:50 +0800

    more

--- sgl-kernel/csrc/gemm/per_token_group_quant_8bit.cu
+++ sgl-kernel/csrc/gemm/per_token_group_quant_8bit.cu
@@ -150,8 +150,10 @@ __global__ void per_token_group_quant_8bit_kernel(
   }
 
   for (int32_t i = lane_id; i < num_vec_elems; i += THREADS_PER_GROUP) {
-    vec_t input_vec;
-    input_vec.cast_load(group_input + i * vec_size);
+    int4 input_int4;
+    T* input_vec = reinterpret_cast<T*>(&input_int4);
+    static_assert(sizeof(input_vec) * vec_size = sizeof(input_int4));
+    input_int4 = ld_global_nc(reinterpret_cast<int4*>(group_input + i * vec_size));
 
     int2 output_buf;
 
