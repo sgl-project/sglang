a0312f624c27223192cccec832a36a86c2dfec11
fzyzcjy <ch271828n@outlook.com>
2025-06-28 17:14:14 +0800

    more

--- sgl-kernel/csrc/gemm/per_token_group_quant_8bit.cu
+++ sgl-kernel/csrc/gemm/per_token_group_quant_8bit.cu
@@ -119,14 +119,13 @@ __global__ void per_token_group_quant_8bit_kernel(
 
   constexpr uint32_t vec_num_bytes = 16;
   constexpr uint32_t vec_size = vec_num_bytes / sizeof(T);
-  using vec_t = flashinfer::vec_t<T, vec_size>;
 
   const int32_t num_vec_elems = group_size / vec_size;
 
   for (int32_t i = lane_id; i < num_vec_elems; i += THREADS_PER_GROUP) {
     int4 input_int4;
     T* input_vec = reinterpret_cast<T*>(&input_int4);
-    static_assert(sizeof(input_vec) * vec_size == sizeof(input_int4));
+    static_assert(sizeof(input_vec[0]) * vec_size == sizeof(input_int4));
     input_int4 = ld_global_nc(reinterpret_cast<const int4*>(group_input + i * vec_size));
 
 #pragma unroll
@@ -152,7 +151,7 @@ __global__ void per_token_group_quant_8bit_kernel(
   for (int32_t i = lane_id; i < num_vec_elems; i += THREADS_PER_GROUP) {
     int4 input_int4;
     T* input_vec = reinterpret_cast<T*>(&input_int4);
-    static_assert(sizeof(input_vec) * vec_size == sizeof(input_int4));
+    static_assert(sizeof(input_vec[0]) * vec_size == sizeof(input_int4));
     input_int4 = ld_global_nc(reinterpret_cast<const int4*>(group_input + i * vec_size));
 
     int2 output_buf;
