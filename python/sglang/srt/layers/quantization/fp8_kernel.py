a5f351d05af59fa6f386d25ae17cbe3e36ea3b49
fzyzcjy <ch271828n@outlook.com>
2025-06-30 21:56:20 +0800

    more

--- python/sglang/srt/layers/quantization/fp8_kernel.py
+++ python/sglang/srt/layers/quantization/fp8_kernel.py
@@ -430,11 +430,12 @@ def sglang_per_token_group_quant_fp8(
     ), "the last dimension of `x` cannot be divisible by `group_size`"
     assert x.is_contiguous(), "`x` is not contiguous"
 
+    out_shape = tuple([*x.shape[:-1], x.shape[-1] // (2 if fuse_silu_and_mul else 1)])
+
     if scale_ue8m0:
         # TODO: handle this case by fixing the (token=4, dim=256, group_size=128) UT case
-        assert x.shape[-1] % (group_size * 4) == 0
+        assert out_shape[-1] % (group_size * 4) == 0
 
-    out_shape = tuple([*x.shape[:-1], x.shape[-1] // (2 if fuse_silu_and_mul else 1)])
     x_q = torch.empty(out_shape, device=x.device, dtype=fp8_dtype)
     x_s = create_per_token_group_quant_fp8_output_scale(
         x_shape=x.shape,
