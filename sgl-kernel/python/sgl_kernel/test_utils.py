f189ccef564fd6920e00a85e3319de43a98316f6
fzyzcjy <ch271828n@outlook.com>
2025-06-30 11:01:56 +0800

    fmt

--- sgl-kernel/python/sgl_kernel/test_utils.py
+++ sgl-kernel/python/sgl_kernel/test_utils.py
@@ -19,11 +19,23 @@ def create_per_token_group_quant_test_data(num_tokens, hidden_dim, flags):
         assert num_local_experts * num_ranks == num_global_experts
 
         # mimic DeepEP low_latency_dispatch output
-        x = torch.randn(num_local_experts, num_max_dispatch_tokens_per_rank * num_ranks, effective_hidden_dim, device=device, dtype=dtype)
+        x = torch.randn(
+            num_local_experts,
+            num_max_dispatch_tokens_per_rank * num_ranks,
+            effective_hidden_dim,
+            device=device,
+            dtype=dtype,
+        )
 
-        masked_data_generation_mode = flags.get("masked_data_generation_mode", "default")
+        masked_data_generation_mode = flags.get(
+            "masked_data_generation_mode", "default"
+        )
         if masked_data_generation_mode == "default":
-            masked_m = torch.tensor(_split_evenly(num_tokens, num_local_experts), device='cuda', dtype=torch.int)
+            masked_m = torch.tensor(
+                _split_evenly(num_tokens, num_local_experts),
+                device="cuda",
+                dtype=torch.int,
+            )
         elif masked_data_generation_mode == "imbalanced":
             masked_m = TODO
         else:
@@ -31,7 +43,11 @@ def create_per_token_group_quant_test_data(num_tokens, hidden_dim, flags):
 
         return x, masked_m
     else:
-        return torch.randn(num_tokens, effective_hidden_dim, device=device, dtype=dtype), None
+        return (
+            torch.randn(num_tokens, effective_hidden_dim, device=device, dtype=dtype),
+            None,
+        )
+
 
 def _split_evenly(num: int, arr_len: int) -> list[int]:
     base = num // arr_len
