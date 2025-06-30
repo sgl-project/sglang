7c0c6ae671044555f64608d78da5c071cb06f0de
fzyzcjy <ch271828n@outlook.com>
2025-06-30 10:35:11 +0800

    more

--- sgl-kernel/tests/test_per_token_group_quant_8bit.py
+++ sgl-kernel/tests/test_per_token_group_quant_8bit.py
@@ -18,8 +18,8 @@ fp8_type_ = torch.float8_e4m3fnuz if _is_hip else torch.float8_e4m3fn
     "num_tokens, hidden_dim, group_size, dst_dtype, flags",
     list(
         itertools.product(
-            [127, 128, 512, 1024, 4096, 8192],  # num_tokens
-            [256, 512, 1024, 2048, 4096],  # hidden_dim
+            [1, 4, 16, 64, 127, 128, 512, 1024, 4096, 8192],  # num_tokens
+            [256, 512, 1024, 1536, 2048, 4096, 7168, 16384],  # hidden_dim
             # TODO support group size != 128
             # [8, 16, 32, 64, 128],  # group_size
             [128],  # group_size
