16e3e230d4ad43ee3d0f665d55884c6f384542e4
ykcombat <chenyukang@sjtu.edu.cn>
2025-06-30 06:09:15 +0000

    add greenctx stream to sgl-kernel

---
+++ sgl-kernel/csrc/spatial/greenctx_stream.h
@@ -0,0 +1,4 @@
+#include <vector>
+
+std::vector<int64_t> create_greenctx_stream_by_percent(float smA, float smB, int device);
+std::vector<int64_t> create_greenctx_stream_by_value(int64_t smA, int64_t smB, int64_t device);
