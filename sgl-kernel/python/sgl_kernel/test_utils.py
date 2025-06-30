d1e90384f7122ac3f278d757cfa4dc09b594c800
fzyzcjy <ch271828n@outlook.com>
2025-06-30 10:46:14 +0800

    more

---
+++ sgl-kernel/python/sgl_kernel/test_utils.py
@@ -0,0 +1,7 @@
+def create_per_token_group_quant_test_data(
+
+):
+    if TODO:
+        return TODO
+    else:
+        return torch.randn(num_tokens, hidden_dim, device=device, dtype=torch.bfloat16)
