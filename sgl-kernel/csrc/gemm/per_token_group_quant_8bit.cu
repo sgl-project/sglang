82cab61c365af8b1c7625ed013b1b52f6c905c48
fzyzcjy <ch271828n@outlook.com>
2025-06-30 13:46:22 +0800

    simp

--- sgl-kernel/csrc/gemm/per_token_group_quant_8bit.cu
+++ sgl-kernel/csrc/gemm/per_token_group_quant_8bit.cu
@@ -235,47 +235,39 @@ void sgl_per_token_group_quant_8bit(
   const int scale_num_rows = output_s.size(1);
   const int scale_stride = output_s.stride(1);
 
-#define LAUNCH_KERNEL(T, DST_DTYPE)                                                               \
-  do {                                                                                            \
-    /* TODO do not copy paste */                                                                  \
-    constexpr uint32_t VEC_TYPED_SIZE = VEC_NUM_BYTES / sizeof(T);                                \
-    TORCH_CHECK(THREADS_PER_GROUP == group_size / VEC_TYPED_SIZE);                                \
-                                                                                                  \
-    using dst_dtype_info = DtypeInfo<DST_DTYPE>;                                                  \
-    CHECK_EQ(dst_dtype_info::MIN, min_8bit);                                                      \
-    CHECK_EQ(dst_dtype_info::MAX, max_8bit);                                                      \
-                                                                                                  \
-    dim3 grid(num_blocks);                                                                        \
-    dim3 block(num_threads);                                                                      \
-    if (is_column_major) {                                                                        \
-      if (scale_ue8m0) {                                                                          \
-        per_token_group_quant_8bit_kernel<T, DST_DTYPE, true, true><<<grid, block, 0, stream>>>(  \
-            static_cast<T*>(input.data_ptr()),                                                    \
-            static_cast<DST_DTYPE*>(output_q.data_ptr()),                                         \
-            static_cast<uint32_t*>(output_s.data_ptr()),                                          \
-            group_size,                                                                           \
-            groups_per_block,                                                                     \
-            scale_num_rows,                                                                       \
-            scale_stride);                                                                        \
-      } else {                                                                                    \
-        per_token_group_quant_8bit_kernel<T, DST_DTYPE, true, false><<<grid, block, 0, stream>>>( \
-            static_cast<T*>(input.data_ptr()),                                                    \
-            static_cast<DST_DTYPE*>(output_q.data_ptr()),                                         \
-            static_cast<float*>(output_s.data_ptr()),                                             \
-            group_size,                                                                           \
-            groups_per_block,                                                                     \
-            scale_num_rows,                                                                       \
-            scale_stride);                                                                        \
-      }                                                                                           \
-    } else {                                                                                      \
-      TORCH_CHECK(!scale_ue8m0);                                                                  \
-      per_token_group_quant_8bit_kernel<T, DST_DTYPE, false><<<grid, block, 0, stream>>>(         \
-          static_cast<T*>(input.data_ptr()),                                                      \
-          static_cast<DST_DTYPE*>(output_q.data_ptr()),                                           \
-          static_cast<float*>(output_s.data_ptr()),                                               \
-          group_size,                                                                             \
-          groups_per_block);                                                                      \
-    }                                                                                             \
+  auto launch = [&](auto kernel) {
+    dim3 grid(num_blocks);
+    dim3 block(num_threads);
+    kernel<<<grid, block, 0, stream>>>(
+        static_cast<T*>(input.data_ptr()),
+        static_cast<DST_DTYPE*>(output_q.data_ptr()),
+        static_cast<uint32_t*>(output_s.data_ptr()),
+        group_size,
+        groups_per_block,
+        scale_num_rows,
+        scale_stride);
+  };
+
+#define LAUNCH_KERNEL(T, DST_DTYPE)                                           \
+  do {                                                                        \
+    /* TODO do not copy paste */                                              \
+    constexpr uint32_t VEC_TYPED_SIZE = VEC_NUM_BYTES / sizeof(T);            \
+    TORCH_CHECK(THREADS_PER_GROUP == group_size / VEC_TYPED_SIZE);            \
+                                                                              \
+    using dst_dtype_info = DtypeInfo<DST_DTYPE>;                              \
+    CHECK_EQ(dst_dtype_info::MIN, min_8bit);                                  \
+    CHECK_EQ(dst_dtype_info::MAX, max_8bit);                                  \
+                                                                              \
+    if (is_column_major) {                                                    \
+      if (scale_ue8m0) {                                                      \
+        launch(per_token_group_quant_8bit_kernel<T, DST_DTYPE, true, true>);  \
+      } else {                                                                \
+        launch(per_token_group_quant_8bit_kernel<T, DST_DTYPE, true, false>); \
+      }                                                                       \
+    } else {                                                                  \
+      TORCH_CHECK(!scale_ue8m0);                                              \
+      launch(per_token_group_quant_8bit_kernel<T, DST_DTYPE, false>);         \
+    }                                                                         \
   } while (0)
 
   DISPATCH_PYTORCH_DTYPE_TO_CTYPE_FP16(input.scalar_type(), scalar_t, [&] {
