65ea1c6a527065f8d636346d94ac2ad695908dee
fzyzcjy <ch271828n@outlook.com>
2025-06-30 10:50:22 +0800

    more

--- sgl-kernel/python/sgl_kernel/test_utils.py
+++ sgl-kernel/python/sgl_kernel/test_utils.py
@@ -4,8 +4,15 @@ def create_per_token_group_quant_test_data(
     num_tokens, hidden_dim, group_size, flags
 ):
     device = torch.device("cuda")
+    dtype = torch.bfloat16
+
+    if flags["fuse_silu_and_mul"]:
+        effective_hidden_dim = hidden_dim * 2
+    else:
+        effective_hidden_dim = hidden_dim
+    del hidden_dim
 
     if TODO:
         return TODO
     else:
-        return torch.randn(num_tokens, hidden_dim, device=device, dtype=torch.bfloat16)
+        return torch.randn(num_tokens, effective_hidden_dim, device=device, dtype=dtype)
