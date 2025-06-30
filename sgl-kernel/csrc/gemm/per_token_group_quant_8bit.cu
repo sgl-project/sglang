d9840bbbaa7af71ae4fdb53295f30306cb2650ee
fzyzcjy <ch271828n@outlook.com>
2025-06-30 18:16:56 +0800

    more

--- sgl-kernel/csrc/gemm/per_token_group_quant_8bit.cu
+++ sgl-kernel/csrc/gemm/per_token_group_quant_8bit.cu
@@ -132,6 +132,7 @@ __global__ void per_token_group_quant_8bit_kernel(
     const T* __restrict__ input,
     DST_DTYPE* __restrict__ output_q,
     scale_packed_t* __restrict__ output_s,
+    const int32_t* __restrict__ masked_m,
     const int group_size,
     const int subwarps_per_block,
     const int hidden_size_num_groups,
@@ -280,6 +281,7 @@ void sgl_per_token_group_quant_8bit(
         static_cast<T*>(input.data_ptr()),                                                               \
         static_cast<DST_DTYPE*>(output_q.data_ptr()),                                                    \
         static_cast<output_s_dtype*>(output_s.data_ptr()),                                               \
+        static_cast<int32_t*>(masked_m.has_value() ? masked_m->data_ptr() : 0),                          \
         group_size,                                                                                      \
         subwarps_per_block,                                                                              \
         hidden_size_num_groups,                                                                          \
