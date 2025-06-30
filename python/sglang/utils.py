35b2af556a1ed08eb1dc3a0329b6fa74a83a3825
Arist12 <ykzhang@cs.wisc.edu>
2025-06-30 11:03:02 +0000

    rename the misleading  variable in  class

--- python/sglang/utils.py
+++ python/sglang/utils.py
@@ -289,21 +289,21 @@ class LazyImport:
     def __init__(self, module_name: str, class_name: str):
         self.module_name = module_name
         self.class_name = class_name
-        self._module = None
+        self._class = None
 
     def _load(self):
-        if self._module is None:
+        if self._class is None:
             module = importlib.import_module(self.module_name)
-            self._module = getattr(module, self.class_name)
-        return self._module
+            self._class = getattr(module, self.class_name)
+        return self._class
 
     def __getattr__(self, name: str):
-        module = self._load()
-        return getattr(module, name)
+        target_class = self._load()
+        return getattr(target_class, name)
 
     def __call__(self, *args, **kwargs):
-        module = self._load()
-        return module(*args, **kwargs)
+        target_class = self._load()
+        return target_class(*args, **kwargs)
 
 
 def download_and_cache_file(url: str, filename: Optional[str] = None):
