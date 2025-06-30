519552ce49d7a3ad69162afeedac5b580d798a84
fzyzcjy <ch271828n@outlook.com>
2025-06-30 14:28:59 +0800

    more

--- sgl-kernel/csrc/gemm/per_token_group_quant_8bit.cu
+++ sgl-kernel/csrc/gemm/per_token_group_quant_8bit.cu
@@ -143,10 +143,10 @@ __global__ void per_token_group_quant_8bit_kernel(
       // TODO unify w/ other places?
       const int token_idx = group_id / scale_hidden_size_unpacked;
       const int hidden_idx_unpacked = group_id % scale_hidden_size_unpacked;
-      const int hidden_idx = hidden_idx_unpacked / num_elems_per_pack;
+      const int hidden_idx_packed = hidden_idx_unpacked / num_elems_per_pack;
       const int pack_idx = hidden_idx_unpacked % num_elems_per_pack;
       scale_output = reinterpret_cast<scale_element_t*>(output_s) +
-                     (hidden_idx * scale_hidden_stride * num_elems_per_pack +
+                     (hidden_idx_packed * scale_hidden_stride * num_elems_per_pack +
                       token_idx * scale_token_stride * num_elems_per_pack + pack_idx);
     } else {
       static_assert(!SCALE_UE8M0);
