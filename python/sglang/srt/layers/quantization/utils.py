c369dfd9e1daf1b48a7090162da48fa0db1931ca
fzyzcjy <ch271828n@outlook.com>
2025-06-30 12:06:06 +0800

    more

--- python/sglang/srt/layers/quantization/utils.py
+++ python/sglang/srt/layers/quantization/utils.py
@@ -163,7 +163,7 @@ def assert_fp8_all_close(a: torch.Tensor, b: torch.Tensor):
         (count_diff_sign == 0)
         and (
             (count_tiny_diff / numel < 0.005)
-            or ((count_tiny_diff / numel < 0.015) and (numel <= 2048))
+            or ((count_tiny_diff / numel < 0.04) and (numel <= 2048))
         )
         and (count_large_diff == 0)
     ), f"{count_diff_sign=} {count_tiny_diff=} {count_large_diff=} {numel=} {a=} {b=}"
