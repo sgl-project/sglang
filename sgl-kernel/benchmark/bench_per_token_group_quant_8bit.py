76f94a2314e376921bde6baeb03db82d98c9b0cb
fzyzcjy <ch271828n@outlook.com>
2025-07-01 10:41:17 +0800

    more

--- sgl-kernel/benchmark/bench_per_token_group_quant_8bit.py
+++ sgl-kernel/benchmark/bench_per_token_group_quant_8bit.py
@@ -36,6 +36,21 @@ if int(os.environ.get("SGLANG_NSYS_PROFILING", "0")):
     #         masked_layout_mode=None,
     #     ),
     # ]]
+    # configs = [
+    #     [
+    #         768 * 8,
+    #         2048,
+    #         128,
+    #         fp8_type_,
+    #         dict(
+    #             column_major_scales=True,
+    #             scale_tma_aligned=True,
+    #             scale_ue8m0=True,
+    #             fuse_silu_and_mul=True,
+    #             masked_layout_mode=None,
+    #         ),
+    #     ]
+    # ]
     configs = [
         [
             768 * 8,
@@ -47,7 +62,7 @@ if int(os.environ.get("SGLANG_NSYS_PROFILING", "0")):
                 scale_tma_aligned=True,
                 scale_ue8m0=True,
                 fuse_silu_and_mul=True,
-                masked_layout_mode=None,
+                masked_layout_mode="balanced",
             ),
         ]
     ]
