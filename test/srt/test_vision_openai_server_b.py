a09d8112ed42071c23bfc0833afc96786b86e1ce
Xinyuan Tong <justinning0323@outlook.com>
2025-06-30 17:35:06 -0700

    Moves MiMo-VL test
    Signed-off-by: Xinyuan Tong <justinning0323@outlook.com>

--- test/srt/test_vision_openai_server_b.py
+++ test/srt/test_vision_openai_server_b.py
@@ -243,23 +243,5 @@ class TestVILAServer(TestOpenAIVisionServer):
         cls.base_url += "/v1"
 
 
-class TestMimoVLServer(TestOpenAIVisionServer):
-    @classmethod
-    def setUpClass(cls):
-        cls.model = "XiaomiMiMo/MiMo-VL-7B-RL"
-        cls.base_url = DEFAULT_URL_FOR_TEST
-        cls.api_key = "sk-123456"
-        cls.process = popen_launch_server(
-            cls.model,
-            cls.base_url,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            api_key=cls.api_key,
-            other_args=[
-                "--trust-remote-code",
-            ],
-        )
-        cls.base_url += "/v1"
-
-
 if __name__ == "__main__":
     unittest.main()
