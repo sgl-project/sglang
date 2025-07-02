3d28fd012f2cf0f587847bfa706be611507a5327
Baizhou Zhang <sobereddiezhang@gmail.com>
2025-07-01 23:14:25 -0700

    fix

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -111,9 +111,16 @@ _is_fp8_fnuz = is_fp8_fnuz()
 _use_aiter = get_bool_env_var("SGLANG_USE_AITER") and _is_hip
 _is_cpu_amx_available = cpu_has_amx_support()
 _is_cpu = is_cpu()
+_device_sm = get_device_sm()
 
 if _is_cuda:
-    from sgl_kernel import awq_dequantize, bmm_fp8, dsv3_fused_a_gemm, dsv3_router_gemm, merge_state_v2
+    from sgl_kernel import (
+        awq_dequantize,
+        bmm_fp8,
+        dsv3_fused_a_gemm,
+        dsv3_router_gemm,
+        merge_state_v2,
+    )
 elif _is_cpu and _is_cpu_amx_available:
     pass
 else:
@@ -229,7 +236,7 @@ class MoEGate(nn.Module):
             hidden_states.shape[0] < 4
             and hidden_states.shape[1] == 7168
             and self.weight.shape[0] == 256
-            and get_device_sm() >= 90
+            and _device_sm >= 90
         ):
             logits = dsv3_router_gemm(hidden_states, self.weight).to(
                 hidden_states.dtype
@@ -893,7 +900,7 @@ class DeepseekV2AttentionMLA(nn.Module):
             and self.fused_qkv_a_proj_with_mqa.weight.shape[0] == 2112
             and self.fused_qkv_a_proj_with_mqa.weight.shape[1] == 7168
             and is_cuda
-            and get_device_sm() >= 90
+            and _device_sm >= 90
         )
 
         self.qkv_proj_with_rope_is_int8 = (
