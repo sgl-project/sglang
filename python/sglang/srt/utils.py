16ddb68d96ddfcf950a7b78712ffd79f8c443248
Chunyuan WU <chunyuan.wu@intel.com>
2025-07-04 00:51:38 +0800

    [CPU] support the case where num_attention_heads or intermediate_size is not divisible by the TP size (#6771)

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -13,6 +13,8 @@
 # ==============================================================================
 """Common utilities."""
 
+from __future__ import annotations
+
 import base64
 import builtins
 import ctypes
