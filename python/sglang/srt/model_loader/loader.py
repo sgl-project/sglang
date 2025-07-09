cb93c667ecd6e01af8bad5ab914720abd1b48ed3
ronnie_zheng <zl19940307@163.com>
2025-07-09 12:04:19 +0300

    Update python/sglang/srt/model_loader/loader.py
    Co-authored-by: Yineng Zhang <me@zhyncs.com>

--- python/sglang/srt/model_loader/loader.py
+++ python/sglang/srt/model_loader/loader.py
@@ -161,7 +161,7 @@ def _initialize_model(
     """Initialize a model with the given configurations."""
     model_class, _ = get_model_architecture(model_config)
     packed_modules_mapping = getattr(model_class, "packed_modules_mapping", {})
-    if is_npu():
+    if _is_npu:
         packed_modules_mapping["fused_qkv_a_proj_with_mqa"] = [
             "q_a_proj",
             "kv_a_proj_with_mqa",
