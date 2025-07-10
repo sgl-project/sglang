98a8c4893ccc83315ef378c8ef047877492a585a
Hanming Lu <hanming@x.ai>
2025-07-10 00:54:08 +0000

    running; incorrect output

--- python/sglang/srt/mem_cache/memory_pool.py
+++ python/sglang/srt/mem_cache/memory_pool.py
@@ -597,6 +597,20 @@ class SWAKVPool(KVCache):
                 v_scale,
                 layer_id_override=layer_id_pool,
             )
+    
+    def load_from_host_per_layer(
+        self, host_pool, host_indices, device_indices, layer_id, io_backend
+    ):
+        raise NotImplementedError(
+            "HiCache not supported for SWAKVPool."
+        )
+
+    def backup_to_host_all_layer(
+        self, host_pool, host_indices, device_indices, io_backend
+    ):
+        raise NotImplementedError(
+            "HiCache not supported for SWAKVPool."
+        )
 
 
 class AscendTokenToKVPool(MHATokenToKVPool):
