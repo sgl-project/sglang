92fd6aee4ab51c919579f01eec3cdd6ff66ea2b2
mywaaagh_admin <pkwarcraft@gmail.com>
2025-07-10 22:35:14 +0800

    fix lint
    Signed-off-by: mywaaagh_admin <pkwarcraft@gmail.com>

--- scripts/npu_ci_install_dependency.sh
+++ scripts/npu_ci_install_dependency.sh
@@ -41,4 +41,3 @@ pip install -e "python[srt_npu]"
 ### Modify PyTorch TODO: to be removed later
 TORCH_LOCATION=$(pip show torch | grep Location | awk -F' ' '{print $2}')
 sed -i 's/from triton.runtime.autotuner import OutOfResources/from triton.runtime.errors import OutOfResources/' "${TORCH_LOCATION}/torch/_inductor/runtime/triton_heuristics.py"
-
