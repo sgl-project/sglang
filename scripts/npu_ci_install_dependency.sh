f3d094df31b4579de341ccafcf0d0190bb738b3d
mywaaagh_admin <pkwarcraft@gmail.com>
2025-07-10 21:20:49 +0800

    remove unused code
    Signed-off-by: mywaaagh_admin <pkwarcraft@gmail.com>

--- scripts/npu_ci_install_dependency.sh
+++ scripts/npu_ci_install_dependency.sh
@@ -42,29 +42,3 @@ pip install -e "python[srt_npu]"
 TORCH_LOCATION=$(pip show torch | grep Location | awk -F' ' '{print $2}')
 sed -i 's/from triton.runtime.autotuner import OutOfResources/from triton.runtime.errors import OutOfResources/' "${TORCH_LOCATION}/torch/_inductor/runtime/triton_heuristics.py"
 
-
-
-# official PPA comes with ffmpeg 2.8, which lacks tons of features, we use ffmpeg 4.0 here
-# add-apt-repository -y ppa:jonathonf/ffmpeg-4 # for ubuntu20.04 official PPA is already version 4.2, you may skip this step
-# apt-get update -y
-# apt-get install -y build-essential python3-dev python3-setuptools make cmake
-# apt-get install -y ffmpeg libavcodec-dev libavfilter-dev libavformat-dev libavutil-dev
-# # build decord
-# git clone --recursive https://github.com/dmlc/decord
-# cd decord
-# mkdir build && cd build
-# cmake .. -DUSE_CUDA=0 -DCMAKE_BUILD_TYPE=Release
-# make
-# cd ../python
-# sed -i 's/maintainer_email.*/&\n    dependency_links=[\n        "https:\/\/mirrors.tuna.tsinghua.edu.cn\/pypi\/web\/simple"\n    ],/g' setup.py
-# python3 setup.py install --user
-# cd ../..
-# pwd
-# note: make sure you have cmake 3.8 or later, you can install from cmake official website if it's too old
-
-
-
-# pip install -e "python[all_npu]"
-
-# pip install modelscope
-# pip install pytest
