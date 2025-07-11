43e18611bb710e852c8af10955da2b7eeb5ef846
guanyewang <guanyewang@tencent.com>
2025-07-11 21:51:59 +0800

    use mp.manager to do load balancing every 40 forward_cts

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1538,7 +1538,7 @@ class Scheduler(
         if need_dp_attn_preparation:
             if (
                 self.server_args.load_balance_method == "minimum_tokens"
-                and self.forward_ct % 10 == 0
+                and self.forward_ct % 40 == 0
             ):
                 self.handle_dp_balance_data(ret)
             ret = self.prepare_mlp_sync_batch(ret)
