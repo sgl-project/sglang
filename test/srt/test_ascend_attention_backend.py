3875608d215b626386e5479d439fade8a9ce9d5c
Even Zhou <14368888+iforgetmyname@users.noreply.github.com>
2025-07-11 19:56:08 +0800

    fix ci download model issue

--- test/srt/test_ascend_attention_backend.py
+++ test/srt/test_ascend_attention_backend.py
@@ -20,7 +20,7 @@ from sglang.test.test_utils import (
     run_bench_offline_throughput,
 )
 
-DEFAULT_MODEL_NAME_FOR_TEST = "AI-ModelScope/Llama-3.1-8B-Instruct"
+DEFAULT_MODEL_NAME_FOR_TEST = "Qwen/Qwen2.5-7B-Instruct"
 
 
 class TestAscendAttnBackend(CustomTestCase):
