fix bug: fix precision bug, new one query precision right: 1. kv_b wight is bf16 fomat; 2. npu rope fusion kernel is not ready; 3. dpsk smooth quant scale is all one; 4. no need to init w2_weight_scale and w13_weight_scale; 5. npu_dequant_swiglu_quant needs activate_left=True for dpsk; 6. routed_scaling_factor is used for twice; 7. optimize dispatch padding len;

python/sglang/srt/layers/moe/ep_moe/token_dispatcher.py
@@ -785,9 +785,9 @@ class NpuDeepEPDispatcher:
         else:
             self.shared_expert_rank_num = self.n_shared_experts * self.experts_share_num_copy
         epsilon = 1e-2
-        self.experts_scale = torch.nn.Parameter(
-            torch.rand(size=(self.num_experts, self.hidden_size), dtype=torch.float32) * (1 - epsilon) + epsilon
-        )
+        self.smooth_scale = torch.nn.Parameter(
+            torch.ones(size=(self.num_experts, self.hidden_size), dtype=torch.float32) * (1 - epsilon) + epsilon
+        ) # smooth scale, now dpsk use smooth_scale == 1
 
     def dispatch(self,
         hidden_states: torch.Tensor,
@@ -798,7 +798,7 @@ class NpuDeepEPDispatcher:
         hidden_states = hidden_states.view(-1, hidden_states.shape[-1])
         topk_ids = topk_idx.to(torch.int)
         if forward_mode.is_extend():
-            return self.dispatch_prefill(hidden_states, topk_ids)
+            return self.dispatch_decode(hidden_states, topk_ids) #TODO: dispatch_prefill precision is wrong
         else:
             return self.dispatch_decode(hidden_states, topk_ids)
 
@@ -811,7 +811,7 @@ class NpuDeepEPDispatcher:
             hidden_states,
             expert_idx=topk_ids,
             active_num=topk_ids.shape[0] * topk_ids.shape[1],
-            scale=self.experts_scale,  # None: non-quant; tensor with shape [num_rows,]: quant
+            scale=self.smooth_scale,  # None: non-quant; tensor with shape [num_rows,]: quant
             expert_num=self.num_experts,
             expert_tokens_num_type=1,  # 0: cumsum mode(not supported now); 1: count mode
             expert_tokens_num_flag=True,
@@ -848,7 +848,7 @@ class NpuDeepEPDispatcher:
             "shared_expert_rank_num": self.shared_expert_rank_num,
             "moe_expert_num": self.num_experts,
             "global_bs": 0,
-            "scales": self.experts_scale,
+            "scales": self.smooth_scale,
             "quant_mode": 2,
             "group_ep": self.group_name,
             "ep_world_size": self.world_size,
@@ -883,8 +883,10 @@ class NpuDeepEPDispatcher:
         if forward_mode.is_extend():
             input_splits = ep_send_counts
             output_splits = tp_send_counts
-            return self.combine_prefill(hidden_states, topk_ids, topk_idx, topk_weights, input_splits, output_splits,
-                                        shared_output, expanded_x, expanded_row_idx)
+            return self.combine_decode(hidden_states, topk_ids, topk_idx, topk_weights, ep_send_counts, tp_send_counts,
+                                       shared_output) # combine_prefill precision is wrong;
+            # return self.combine_prefill(hidden_states, topk_ids, topk_idx, topk_weights, input_splits, output_splits,
+            #                             shared_output, expanded_x, expanded_row_idx)
         else:
             return self.combine_decode(hidden_states, topk_ids, topk_idx, topk_weights, ep_send_counts, tp_send_counts,
                                        shared_output)

python/sglang/srt/models/deepseek_v2.py
@@ -607,9 +607,11 @@ class DeepseekV2MoE(nn.Module):
             self, hidden_states: torch.Tensor, forward_batch: ForwardBatch
     ) -> torch.Tensor:
         pad_size = None
-        if hidden_states.size(0) == 0 or hidden_states.size(0) % get_attention_tp_size() != 0:
-            pad_size = get_attention_tp_size() - (hidden_states.size(0) % get_attention_tp_size())
-            hidden_states = torch.nn.functional.pad(hidden_states, [0, 0, 0, pad_size], "constant", 0)
+        if forward_batch.forward_mode.is_extend():
+            pad_size = (forward_batch.seq_lens_sum // get_attention_tp_size() + 1) - hidden_states.size(0)
+        else:
+            pad_size = (forward_batch.batch_size // get_attention_tp_size() + 1) - hidden_states.size(0)
+        hidden_states = F.pad(hidden_states, [0, 0, 0, pad_size], "constant", 0)
         forward_mode = forward_batch.forward_mode
         shared_output = None
         if is_non_idle_and_non_empty(forward_mode, hidden_states):
@@ -681,14 +683,7 @@ class DeepseekV2MoE(nn.Module):
                 expanded_row_idx=expanded_row_idx,
             )
 
-        if shared_output is not None:
-            x = shared_output
-            x.add_(final_hidden_states, alpha=self.routed_scaling_factor)
-            final_hidden_states = x
-        else:
-            final_hidden_states *= self.routed_scaling_factor
-
-        if pad_size is not None:
+        if pad_size > 0:
             final_hidden_states = final_hidden_states[:-pad_size, :]
 
         return final_hidden_states
@@ -909,7 +904,7 @@ class DeepseekV2AttentionMLA(nn.Module):
             self.kv_lora_rank,
             self.num_heads * (self.qk_nope_head_dim + self.v_head_dim),
             bias=False,
-            quant_config=quant_config,
+            quant_config=None,
             prefix=add_prefix("kv_b_proj", prefix),
             tp_rank=attn_tp_rank,
             tp_size=attn_tp_size,