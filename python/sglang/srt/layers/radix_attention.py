4cb8621e93e2ae5715561590e93d25d71231b7d0
Cheng Wan <cwan@x.ai>
2025-07-14 13:34:29 -0700

    revert unnecessary changes

--- python/sglang/srt/layers/radix_attention.py
+++ python/sglang/srt/layers/radix_attention.py
@@ -13,17 +13,13 @@
 # ==============================================================================
 """Radix attention."""
 
-from __future__ import annotations
-
 from enum import Enum
-from typing import TYPE_CHECKING, Optional
+from typing import Optional
 
 from torch import nn
 
+from sglang.srt.layers.quantization.base_config import QuantizationConfig
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch
-
-if TYPE_CHECKING:
-    from sglang.srt.layers.quantization.base_config import QuantizationConfig
 
 
 class AttentionType(Enum):
