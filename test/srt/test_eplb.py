ba55fc9cc816718fbf158059ee791869164bdecb
Han Han <hanhan7630@outlook.com>
2025-07-14 09:29:55 +0000

    fix

--- test/srt/test_eplb.py
+++ test/srt/test_eplb.py
@@ -22,7 +22,7 @@ class _BaseTestDynamicEPLB(CustomTestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.model = try_cached_model(DEFAULT_MLA_MODEL_NAME_FOR_TEST)
+        cls.model = DEFAULT_MLA_MODEL_NAME_FOR_TEST
         cls.base_url = DEFAULT_URL_FOR_TEST
         cls.process = popen_launch_server(
             cls.model,
