edc8a42763dcf0b244a591d3d3aa6c7dd1a17e37
Arist12 <ykzhang@cs.wisc.edu>
2025-07-14 07:58:54 +0000

    fix qwen3 context length resolving bug

--- python/sglang/srt/hf_transformers_utils.py
+++ python/sglang/srt/hf_transformers_utils.py
@@ -191,7 +191,9 @@ def get_context_length(config):
     if rope_scaling:
         rope_scaling_factor = rope_scaling.get("factor", 1)
         if "original_max_position_embeddings" in rope_scaling:
-            rope_scaling_factor = 1
+            return (
+                rope_scaling["original_max_position_embeddings"] * rope_scaling_factor
+            )
         if rope_scaling.get("rope_type", None) == "llama3":
             rope_scaling_factor = 1
     else:
