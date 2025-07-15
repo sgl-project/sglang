f45e0662145bc0a45ee4a3764cbd145c5db3aab9
Baizhou Zhang <sobereddiezhang@gmail.com>
2025-07-14 23:23:17 -0700

    Apply updated dsv3_router_gemm kernel

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -243,14 +243,13 @@ class MoEGate(nn.Module):
         # NOTE: For some unknown reason, router_gemm seems degrade accept length.
         if (
             _is_cuda
-            and not self.is_nextn
-            and hidden_states.shape[0] < 4
+            and hidden_states.shape[0] <= 16
             and hidden_states.shape[1] == 7168
             and self.weight.shape[0] == 256
             and _device_sm >= 90
         ):
-            logits = dsv3_router_gemm(hidden_states, self.weight).to(
-                hidden_states.dtype
+            logits = dsv3_router_gemm(
+                hidden_states, self.weight, out_dtype=torch.bfloat16
             )
         else:
             logits = F.linear(hidden_states, self.weight, None)
