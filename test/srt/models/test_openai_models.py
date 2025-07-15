2401d39cc50559ca811078fb86e51bf3d69a9920
Jinyan Chen <jinyanc@nvidia.com>
2025-07-15 04:23:11 -0700

    Add key tokens into promt for wrong detokenizing

--- test/srt/models/test_openai_models.py
+++ test/srt/models/test_openai_models.py
@@ -63,7 +63,7 @@ class TestOpenAIMoE(CustomTestCase):
     #     # self.assertGreaterEqual(metrics["score"], 0.759) # target
 
     def test_bs_1_speed(self):
-        args = BenchArgs(port=int(self.base_url.split(":")[-1]), max_new_tokens=10, prompt="How are you?") # What is the capital of France?
+        args = BenchArgs(port=int(self.base_url.split(":")[-1]), max_new_tokens=10, prompt="Human: How are you?\n\nAssistant:") # What is the capital of France?
         acc_length, speed = send_one_prompt(args)
 
         print(f"{speed=:.2f}")
