0f8f70079d8c24c9ca1db41ca7bd8b1096275520
fzyzcjy <ch271828n@outlook.com>
2025-07-15 08:01:50 +0800

    more

--- python/sglang/srt/offloader.py
+++ python/sglang/srt/offloader.py
@@ -6,39 +6,41 @@ from torch.func import functional_call
 from sglang.srt.utils import is_pin_memory_available
 
 
+class _StatelessOffloaderUtil:
+    @staticmethod
+    def offload_module_to_cpu(module):
+        pin_memory = is_pin_memory_available()
+        for p in module.parameters():
+            cpu_data = torch.empty_strided(
+                size=p.data.size(),
+                stride=p.data.stride(),
+                dtype=p.data.dtype,
+                layout=p.data.layout,
+                device="cpu",
+                pin_memory=pin_memory,
+            )
+            cpu_data.copy_(p.data)
+            p.data = cpu_data
+
+    @staticmethod
+    def create_onload_params(module, device):
+        return {
+            k: v.to(device, non_blocking=True)
+            for k, v in module.state_dict().items()
+        }
+
+
 class _ModuleOffloader:
     def __init__(self, module: torch.nn.Module):
         self.device = next(module.parameters()).device
         assert self.device != torch.device("cpu"), "not handled device=cpu case yet (should skip this tensor)"
 
-        _offload_module_to_cpu(module)
+        _StatelessOffloaderUtil.offload_module_to_cpu(module)
 
         def _create_parameter_and_buffer_dicts():
-            return _create_onload_params(module, self.device)
+            return _StatelessOffloaderUtil.create_onload_params(module, self.device)
 
         _hook_module_forward(module, _create_parameter_and_buffer_dicts)
-
-
-def _offload_module_to_cpu(module):
-    pin_memory = is_pin_memory_available()
-    for p in module.parameters():
-        cpu_data = torch.empty_strided(
-            size=p.data.size(),
-            stride=p.data.stride(),
-            dtype=p.data.dtype,
-            layout=p.data.layout,
-            device="cpu",
-            pin_memory=pin_memory,
-        )
-        cpu_data.copy_(p.data)
-        p.data = cpu_data
-
-
-def _create_onload_params(module, device):
-    return {
-        k: v.to(device, non_blocking=True)
-        for k, v in module.state_dict().items()
-    }
 
 
 def _hook_module_forward(module, create_parameter_and_buffer_dicts):
