e4b6679e924288e44961f73c16d564b21d0ac545
Pengyu Chen <pychen96@gmail.com>
2025-07-15 13:25:28 -0700

    Kernel parts of PR #7702.
    Support relaxed acceptance for thinking tokens in speculative decoding.

--- sgl-kernel/include/sgl_kernel_ops.h
+++ sgl-kernel/include/sgl_kernel_ops.h
@@ -347,8 +347,8 @@ void tree_speculative_sampling_target_only(
     at::Tensor uniform_samples_for_final_sampling,
     at::Tensor target_probs,
     at::Tensor draft_probs,
-    double threshold_single = 1,
-    double threshold_acc = 1,
+    at::Tensor threshold_singles,
+    at::Tensor threshold_accs,
     bool deterministic = true,
     int64_t cuda_stream = 0);
 
