3b5c9458f69d42042223ea0c6244316e60c4f57f
fzyzcjy <ch271828n@outlook.com>
2025-07-15 08:42:32 +0800

    more

--- python/sglang/srt/offloader.py
+++ python/sglang/srt/offloader.py
@@ -6,6 +6,58 @@ from torch.func import functional_call
 from sglang.srt.utils import is_pin_memory_available, get_int_env_var
 
 
+def wrap_layers_for_offload(layers: List[torch.nn.Module]):
+    layer_interval = get_int_env_var("SGLANG_OFFLOAD_LAYER_INTERVAL", 5)
+    offload_layers = layers[layer_interval - 1::layer_interval]
+    offloaders = [_ModuleOffloader(layer) for layer in offload_layers]
+
+    offloaders[0].start_onload()
+
+    for index, layer in enumerate(offload_layers):
+        _hook_module_forward_for_offloader(index=index, layer=layer, offloaders=offloaders)
+
+    return layers
+
+
+def _hook_module_forward_for_offloader(index, layer, offloaders):
+    _hook_module_forward_raw(
+        layer,
+        on_forward_start=lambda: offloaders[(index + 1) % len(offloaders)].start_onload(),
+        on_forward_end=lambda: offloaders[index].offload(),
+        get_parameter_and_buffer_dicts=lambda: offloaders[index].device_tensors,
+    )
+
+
+def _hook_module_forward_raw(module, on_forward_start, on_forward_end, get_parameter_and_buffer_dicts):
+    original_forward = module.forward
+
+    def forward(*args, **kwargs):
+        module.forward = original_forward
+        on_forward_start()
+        output = functional_call(module, get_parameter_and_buffer_dicts(), args=args, kwargs=kwargs)
+        on_forward_end()
+        module.forward = forward
+        return output
+
+    module.forward = forward
+
+
+class _ModuleOffloader:
+    def __init__(self, module: torch.nn.Module):
+        self.module = module
+        self.device = next(module.parameters()).device
+        assert self.device != torch.device("cpu"), "not handled device=cpu case yet (should skip this tensor)"
+
+        self.device_tensors = None
+        _StatelessOffloaderUtil.offload(module)
+
+    def start_onload(self):
+        self.device_tensors = _StatelessOffloaderUtil.create_onload_tensors(self.module, self.device)
+
+    def offload(self):
+        self.device_tensors = None
+
+
 class _StatelessOffloaderUtil:
     @staticmethod
     def offload(module):
@@ -29,52 +81,3 @@ class _StatelessOffloaderUtil:
             k: v.to(device, non_blocking=True)
             for k, v in module.state_dict().items()
         }
-
-
-class _ModuleOffloader:
-    def __init__(self, module: torch.nn.Module):
-        self.module = module
-        self.device = next(module.parameters()).device
-        assert self.device != torch.device("cpu"), "not handled device=cpu case yet (should skip this tensor)"
-
-        self.device_tensors = None
-        _StatelessOffloaderUtil.offload(module)
-
-    def start_onload(self):
-        self.device_tensors = _StatelessOffloaderUtil.create_onload_tensors(self.module, self.device)
-
-    def offload(self):
-        self.device_tensors = None
-
-
-def _hook_module_forward(module, on_forward_start, on_forward_end, get_parameter_and_buffer_dicts):
-    original_forward = module.forward
-
-    def forward(*args, **kwargs):
-        module.forward = original_forward
-        on_forward_start()
-        output = functional_call(module, get_parameter_and_buffer_dicts(), args=args, kwargs=kwargs)
-        on_forward_end()
-        module.forward = forward
-        return output
-
-    module.forward = forward
-
-
-def wrap_layers_for_offload(layers: List[torch.nn.Module]):
-    layer_interval = get_int_env_var("SGLANG_OFFLOAD_LAYER_INTERVAL", 5)
-    offload_layers = layers[layer_interval - 1::layer_interval]
-    offloaders = [_ModuleOffloader(layer) for layer in offload_layers]
-
-    offloaders[0].start_onload()
-
-    for index, layer in enumerate(offload_layers):
-        TODO_closure_index
-        _hook_module_forward(
-            layer,
-            on_forward_start=lambda: offloaders[(index + 1) % len(offloaders)].start_onload(),
-            on_forward_end=lambda: offloaders[index].offload(),
-            get_parameter_and_buffer_dicts=lambda: offloaders[index].device_tensors,
-        )
-
-    return layers
