3bd115fa1aed08515f394424c24f34c516d845e5
luoyuan.luo <luoyuan.luo@antgroup.com>
2025-07-16 21:03:56 +0800

    Refactor per_token_quant_fp8

--- sgl-kernel/csrc/gemm/per_token_quant_fp8.cu
+++ sgl-kernel/csrc/gemm/per_token_quant_fp8.cu
@@ -1,10 +1,13 @@
 #include <ATen/cuda/CUDAContext.h>
 
 #include <cmath>
-#include <cub/block/block_reduce.cuh>
+#include <cute/arch/copy_sm90.hpp>
+#include <cute/tensor.hpp>
 #include <flashinfer/vec_dtypes.cuh>
 
 #include "utils.h"
+
+using namespace cute;
 
 template <typename T>
 __global__ void per_token_quant_fp8_kernel(
@@ -17,23 +20,23 @@ __global__ void per_token_quant_fp8_kernel(
   if (token_idx >= num_tokens) return;
 
   const int tid = threadIdx.x;
-  const int block_dim = blockDim.x;
 
-  const T* token_input = input + token_idx * hidden_dim;
-  FP8_TYPE* token_output = output_q + token_idx * hidden_dim;
+  float max_value = 0.f;
 
-  float max_value = 0.0f;
+  // input[token, hidden_dim]
+  auto gmem_in = make_tensor(const_cast<T*>(input) + token_idx * hidden_dim, make_shape(hidden_dim));
+  auto gmem_out = make_tensor(output_q + token_idx * hidden_dim, make_shape(hidden_dim));
+  auto gmem_s = make_tensor(output_s + token_idx, make_shape(1));
 
-  // We want to store 128 bits of data at a time. 16 = 128 / 8 bits
   // Load is already vectorized, so 16 elements work for T.
+  const uint32_t BLOCK_SIZE = 256;
   const uint32_t VEC_SIZE = 16;
   using vec_t = flashinfer::vec_t<T, VEC_SIZE>;
   const int32_t num_vec_elems = hidden_dim / VEC_SIZE;
 
-  // Find max using vectorized loads
-  for (int32_t i = tid; i < num_vec_elems; i += block_dim) {
+  for (int32_t i = tid; i < num_vec_elems; i += BLOCK_SIZE) {
     vec_t input_vec;
-    input_vec.cast_load(token_input + i * VEC_SIZE);
+    input_vec.cast_load(gmem_in.data() + i * VEC_SIZE);
 
 #pragma unroll
     for (uint32_t j = 0; j < VEC_SIZE; ++j) {
@@ -47,16 +50,16 @@ __global__ void per_token_quant_fp8_kernel(
   __shared__ float scale;
   if (tid == 0) {
     scale = max_value / FP8_E4M3_MAX;
-    output_s[token_idx] = scale;
+    gmem_s(0) = scale;
   }
   __syncthreads();
 
-  const float scale_inv = 1.0f / scale;
+  const float scale_inv = (scale == 0.f) ? 0.f : 1.0f / scale;
 
   // Quantize using vectorized loads
-  for (int32_t i = tid; i < num_vec_elems; i += block_dim) {
+  for (int32_t i = tid; i < num_vec_elems; i += BLOCK_SIZE) {
     vec_t input_vec;
-    input_vec.cast_load(token_input + i * VEC_SIZE);
+    input_vec.cast_load(gmem_in.data() + i * VEC_SIZE);
 
     FP8_TYPE output_arr[VEC_SIZE];
 #pragma unroll
@@ -71,7 +74,7 @@ __global__ void per_token_quant_fp8_kernel(
 #endif
     }
 
-    *(uint4*)(token_output + i * VEC_SIZE) = *(uint4*)output_arr;
+    *(uint4*)(gmem_out.data() + i * VEC_SIZE) = *(uint4*)output_arr;
   }
 }
 
