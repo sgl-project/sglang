116c10208f027f3b65a6c76e9f2ce651e464f283
Cheng Wan <cwan@x.ai>
2025-07-16 20:41:17 -0700

    add throughput test for 8-gpu deepep

--- test/srt/test_deepep_large.py
+++ test/srt/test_deepep_large.py
@@ -11,6 +11,9 @@ from sglang.test.test_utils import (
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
     popen_launch_server,
+    run_bench_one_batch,
+    is_in_ci,
+    write_github_step_summary,
 )
 
 
@@ -45,6 +48,7 @@ class TestDeepseek(CustomTestCase):
                 "256",
                 "--max-running-requests",
                 "2048",
+                "--disable-radix-cache",
             ],
         )
 
@@ -54,10 +58,10 @@ class TestDeepseek(CustomTestCase):
 
     def test_gsm8k(self):
         args = SimpleNamespace(
-            num_shots=8,
+            num_shots=5,
             data_path=None,
-            num_questions=1250,
-            parallel=1250,
+            num_questions=200,
+            parallel=200,
             max_new_tokens=512,
             host="http://127.0.0.1",
             port=int(self.base_url.split(":")[-1]),
@@ -66,6 +70,22 @@ class TestDeepseek(CustomTestCase):
         print(f"Eval accuracy of GSM8K: {metrics=}")
 
         self.assertGreater(metrics["accuracy"], 0.93)
+    
+    def test_throughput(self):
+        requests.get(self.base_url + "/flush_cache")
+        res = run_bench_one_batch(
+            model=DEFAULT_DEEPPEP_MODEL_NAME_FOR_TEST,
+            num_prompts=2048,
+            request_rate=float("inf"),
+            port=int(self.base_url.split(":")[-1]),
+        )
+        print(res)
+        if is_in_ci():
+            write_github_step_summary(
+                f"### test_throughput\n"
+                f'Output throughput: {res["output_throughput"]:.2f} token/s\n'
+            )
+            self.assertGreater(res["output_throughput"], 3500)
 
 
 class TestDeepseekMTP(CustomTestCase):
@@ -107,6 +127,7 @@ class TestDeepseekMTP(CustomTestCase):
                 "1",
                 "--speculative-num-draft-tokens",
                 "2",
+                "--disable-radix-cache",
             ],
         )
 
@@ -116,10 +137,10 @@ class TestDeepseekMTP(CustomTestCase):
 
     def test_gsm8k(self):
         args = SimpleNamespace(
-            num_shots=8,
+            num_shots=5,
             data_path=None,
-            num_questions=1250,
-            parallel=1250,
+            num_questions=200,
+            parallel=200,
             max_new_tokens=512,
             host="http://127.0.0.1",
             port=int(self.base_url.split(":")[-1]),
@@ -139,6 +160,22 @@ class TestDeepseekMTP(CustomTestCase):
             f"{avg_spec_accept_length=:.3f}\n"
         )
         self.assertGreater(avg_spec_accept_length, 1.9)
+    
+    def test_throughput(self):
+        requests.get(self.base_url + "/flush_cache")
+        res = run_bench_one_batch(
+            model=DEFAULT_DEEPPEP_MODEL_NAME_FOR_TEST,
+            num_prompts=2048,
+            request_rate=float("inf"),
+            port=int(self.base_url.split(":")[-1]),
+        )
+        print(res)
+        if is_in_ci():
+            write_github_step_summary(
+                f"### test_throughput_mtp\n"
+                f'Output throughput: {res["output_throughput"]:.2f} token/s\n'
+            )
+            self.assertGreater(res["output_throughput"], 3500)
 
 
 if __name__ == "__main__":
