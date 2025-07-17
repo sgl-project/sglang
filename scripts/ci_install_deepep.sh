c5cc5d95bf69e169af74d967511358d6f68f9404
Cheng Wan <cwan@x.ai>
2025-07-16 18:00:58 -0700

    [ci] recover 8-gpu deepep test

--- scripts/ci_install_deepep.sh
+++ scripts/ci_install_deepep.sh
@@ -4,16 +4,16 @@ set -euxo pipefail
 
 bash scripts/ci_install_dependency.sh
 
-if python3 -c "import deep_ep" >/dev/null 2>&1; then
-    echo "deep_ep is already installed or importable. Skipping installation."
-    exit 0
-fi
-
 export GDRCOPY_HOME=/usr/src/gdrdrv-2.4.4/
 export NVSHMEM_DIR=/opt/nvshmem/install
 export LD_LIBRARY_PATH="${NVSHMEM_DIR}/lib:$LD_LIBRARY_PATH"
 export PATH="${NVSHMEM_DIR}/bin:$PATH"
 export CUDA_HOME=/usr/local/cuda
+
+if python3 -c "import deep_ep" >/dev/null 2>&1; then
+    echo "deep_ep is already installed or importable. Skipping installation."
+    exit 0
+fi
 
 # Install system dependencies
 apt install -y curl wget git sudo libibverbs-dev rdma-core infiniband-diags openssh-server perftest ibverbs-providers libibumad3 libibverbs1 libnl-3-200 libnl-route-3-200 librdmacm1 build-essential cmake
@@ -27,7 +27,7 @@ git checkout v2.4.4
 apt update
 apt install -y nvidia-dkms-535
 apt install -y build-essential devscripts debhelper fakeroot pkg-config dkms
-apt install -y check libsubunit0 libsubunit-dev
+apt install -y check libsubunit0 libsubunit-dev python3-venv
 cd packages
 CUDA=/usr/local/cuda ./build-deb-packages.sh
 dpkg -i gdrdrv-dkms_*.deb
@@ -41,12 +41,12 @@ fi
 apt-get update && apt-get install -y libfabric-dev
 
 # Clone DeepEP
-rm -rf /root/.cache/deepep && git clone https://github.com/deepseek-ai/DeepEP.git /root/.cache/deepep && cd /root/.cache/deepep && git checkout eef7ab50fa5cf0ab1dd3fce4c6493c90bdf290ac
+rm -rf /root/.cache/deepep && git clone https://github.com/deepseek-ai/DeepEP.git /root/.cache/deepep && cd /root/.cache/deepep && git checkout b6ce310bb0b75079682d09bc2ebc063a074fbd58
 
 # Install NVSHMEM
 cd /opt/nvshmem
-wget https://developer.download.nvidia.com/compute/redist/nvshmem/3.2.5/source/nvshmem_src_3.2.5-1.txz
-tar -xf nvshmem_src_3.2.5-1.txz
+wget https://developer.download.nvidia.com/compute/redist/nvshmem/3.3.9/source/nvshmem_src_cuda12-all-all-3.3.9.tar.gz
+tar -xf nvshmem_src_cuda12-all-all-3.3.9.tar.gz
 rm -rf nvshmem && mv nvshmem_src nvshmem
 cd nvshmem
 git apply /root/.cache/deepep/third-party/nvshmem.patch
@@ -66,9 +66,6 @@ make -j$(nproc) install
 cd /root/.cache/deepep && python3 setup.py install
 
 # Verify configuration
-echo "=== NCCL Configuration ==="
-nvidia-smi topo -m
-nvidia-smi nvlink -s
 echo "=== Verify GDRCOPY ==="
 gdrcopy_copybw
 echo "=== Verify NVSHMEM ==="
