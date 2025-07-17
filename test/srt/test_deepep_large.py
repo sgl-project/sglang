ee0f93e72c32ac16e1057798d872c44c35b40c59
Cheng Wan <cwan@x.ai>
2025-07-17 00:43:24 -0700

    remove unused import

--- test/srt/test_deepep_large.py
+++ test/srt/test_deepep_large.py
@@ -10,10 +10,7 @@ from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
-    is_in_ci,
     popen_launch_server,
-    run_bench_one_batch,
-    write_github_step_summary,
 )
 
 
