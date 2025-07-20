f48c313e6379bda5b5a08ebfe17895e8088af0b4
liyouli <2084318355@qq.com>
2025-07-20 17:11:48 +0800

    Create test_score_qwen3_rerank.py

---
+++ python/sglang/test/test_score_qwen3_rerank.py
@@ -0,0 +1,26 @@
+import requests
+
+# 1.starting a server: python3 -m sglang.launch_server --model-path /root/paddlejob/workspace/liuli44/Qwen/Qwen3-Reranker-0___6B/ --port 8191 --host 0.0.0.0
+# 2.then: python test_score_qwen3_rerank.py
+
+# url = "http://127.0.0.1:30000/v1/score"
+url = "http://0.0.0.0:8191/v1/score"
+
+payload = {
+    "model": "Qwen/Qwen3-Reranker-0.6B",
+    "query": "what is panda?",
+    "items": [
+        # "hi",
+        "The giant panda (Ailuropoda melanoleuca), sometimes called a panda bear or simply panda, is a bear species endemic to China."
+    ],
+    "instruction": "Given a web search query, retrieve relevant passages that answer the query",
+    "rerank_type": "qwen3-rerank",
+    "label_token_ids": [9693, 2152],
+}
+
+response = requests.post(url, json=payload)
+response_json = response.json()
+
+for item in response_json:
+    print(item, response_json[item])
+    # print(f"Score: {item['score']:.2f} - Document: '{item['document']}'")
