707e0ecaa5b3a5f3026b1e16ea829ce20a6a05ff
Maksim <makcum888e@mail.ru>
2025-07-23 10:25:38 +0300

    alloc_decode optimized

--- python/sglang/srt/mem_cache/allocator.py
+++ python/sglang/srt/mem_cache/allocator.py
@@ -585,27 +585,22 @@ def alloc_extend_kernel_ascend(
             out_indices[end_pos[i] - num3 : end_pos[i]] = (
                 free_pages[end_new_pages[i] - 1] * page_size + pos_in_page[:num3]
             ).view(-1)
-    return num_new_pages
+    return num_new_pages.sum()
 
 
 def alloc_decode_kernel_ascend(
     seq_lens,
     last_loc,
     free_pages,
-    out_indices,
     page_size,
 ):
-    num_new_pages = (seq_lens + page_size - 1) // page_size - (
-        seq_lens - 1 + page_size - 1
-    ) // page_size
+    num_new_pages = (seq_lens % page_size == 1).int()
     end_new_pages = torch.cumsum(num_new_pages, 0)
     start_new_pages = end_new_pages - num_new_pages
-    for i in range(len(seq_lens)):
-        if num_new_pages[i]:
-            out_indices[i] = free_pages[start_new_pages[i]] * page_size
-        else:
-            out_indices[i] = last_loc[i] + 1
-    return num_new_pages
+    out_indices = (last_loc + 1) * (1 - num_new_pages) + free_pages[
+        start_new_pages
+    ] * page_size * num_new_pages
+    return out_indices, num_new_pages.sum()
 
 
 class AscendPagedTokenToKVPoolAllocator(PagedTokenToKVPoolAllocator):
@@ -651,7 +646,7 @@ class AscendPagedTokenToKVPoolAllocator(PagedTokenToKVPoolAllocator):
         if self.debug_mode:
             assert len(torch.unique(out_indices)) == len(out_indices)
 
-        num_new_pages = self.ret_values.sum()
+        num_new_pages = self.ret_values.item()
         if num_new_pages > len(self.free_pages):
             return None
 
@@ -668,21 +663,17 @@ class AscendPagedTokenToKVPoolAllocator(PagedTokenToKVPoolAllocator):
                 (last_loc + 2) % self.page_size == seq_lens % self.page_size
             )
 
-        bs = len(seq_lens)
-        out_indices = torch.empty((bs,), dtype=torch.int32, device=self.device)
-
-        self.ret_values = alloc_decode_kernel_ascend(
+        out_indices, self.ret_values = alloc_decode_kernel_ascend(
             seq_lens,
             last_loc,
             self.free_pages,
-            out_indices,
             self.page_size,
         )
 
         if self.debug_mode:
             assert len(torch.unique(out_indices)) == len(out_indices)
 
-        num_new_pages = self.ret_values.sum()
+        num_new_pages = self.ret_values.item()
         if num_new_pages > len(self.free_pages):
             return None
 
