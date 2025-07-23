6910fff7a2b827b64f50bb6214f481c7dc675e61
MahmoudAshraf97 <hassouna97.ma@gmail.com>
2025-07-23 18:41:48 +0300

    fix data loading

--- python/sglang/srt/multimodal/processors/whisper.py
+++ python/sglang/srt/multimodal/processors/whisper.py
@@ -16,19 +16,17 @@ class WhisperProcessor(BaseMultimodalProcessor):
         self,
         audio_data,
         input_text,
-        request_obj,
-        max_req_input_len,
         **kwargs,
     ) -> Optional[Dict[str, Any]]:
-        audios = [load_audio(audio)[0] for audio in audio_data]
+        audios = [load_audio(audio) for audio in audio_data]
 
         processor_output = self.process_mm_data(input_text=input_text, audio=audios)
         output = {}
-        output["data_hashes"] = [hash(str(audio_data)) for audio_data in audio_data]
+        output["data_hashes"] = [hash(audio_data) for audio_data in audio_data]
         output["input_ids"] = processor_output["labels"].tolist()
         output["mm_items"] = [
             MultimodalDataItem(
-                audio_features=processor_output["input_features"],
+                feature=processor_output["input_features"],
                 modality=Modality.AUDIO,
             )
         ]
