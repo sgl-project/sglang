095c817b8465a9076ad1f96c99eb1e6790df8d34
zuoyuan <zhangzuo21@mails.tsinghua.edu.cn>
2025-07-24 15:50:15 +0800

    modify for review

--- python/sglang/bench_serving.py
+++ python/sglang/bench_serving.py
@@ -734,6 +734,7 @@ class BenchmarkMetrics:
 
 SHAREGPT_URL = "https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json"
 
+
 def download_and_cache_file(url: str, filename: Optional[str] = None):
     """Read and cache a file from a url."""
     if filename is None:
--- python/sglang/srt/mem_cache/hiradix_cache.py
+++ python/sglang/srt/mem_cache/hiradix_cache.py
@@ -260,7 +260,7 @@ class HiRadixCache(RadixCache):
             for k, v in x.parent.children.items():
                 if v == x:
                     break
-            
+            del x.parent.children[k]
 
             if len(x.parent.children) == 0 and x.parent.evicted:
                 heapq.heappush(leaves, x.parent)
