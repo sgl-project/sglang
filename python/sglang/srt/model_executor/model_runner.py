f12f4b1c06eaa48828540c97070ff79a94f97941
ZhengWG <zwg0606@gmail.com>
2025-07-25 20:41:53 +0800

    feat: hack allocate kv for MHA in Embedding disggregation mode

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -1252,6 +1252,7 @@ class ModelRunner:
                     enable_memory_saver=self.server_args.enable_memory_saver,
                     start_layer=self.start_layer,
                     end_layer=self.end_layer,
+                    disable_allocate_kvcache=(self.server_args.disaggregation_mode == "embedding")
                 )
 
         if self.token_to_kv_pool_allocator is None:
