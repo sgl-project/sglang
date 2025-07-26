cf83217fe024f5348529565e4fe10c318a6dd635
Mick <mickjagger19@icloud.com>
2025-07-26 19:52:09 +0800

    remove warning

--- python/sglang/srt/managers/mm_utils.py
+++ python/sglang/srt/managers/mm_utils.py
@@ -92,9 +92,7 @@ class TransportProxyTensor(torch.Tensor):
                 }
                 state["tensor_data"] = None
             except Exception as e:
-                print_warning_once(
-                    f"Warning: Failed to get CUDA IPC handle ({e}). Falling back to default transport."
-                )
+                # Failed to get CUDA IPC handle ({e}). Falling back to default transport."
                 state["metadata"]["transport_mode"] = "default"
                 state["tensor_data"] = self.as_subclass(torch.Tensor)
         else:
@@ -751,7 +749,11 @@ def tensor_hash(tensor_list) -> int:
         ]
         tensor = torch.concat(tensor_list)
     if tensor.is_cuda:
+        print(f"{tensor.device.index=}")
+        print(f"{tensor.device=}")
+        print(f"{tensor.get_device=}")
         return gpu_tensor_hash(tensor.cuda())
+        # return gpu_tensor_hash(tensor.contiguous())
     tensor = tensor.detach().contiguous()
 
     if tensor.dtype == torch.bfloat16:
