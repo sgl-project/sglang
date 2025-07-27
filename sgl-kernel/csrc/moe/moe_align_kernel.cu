e858bea372ac86d7815702ba00f3b88a24537104
BBuf <1182563586@qq.com>
2025-07-27 21:48:20 +0800

    fallback

--- sgl-kernel/csrc/moe/moe_align_kernel.cu
+++ sgl-kernel/csrc/moe/moe_align_kernel.cu
@@ -53,7 +53,7 @@ __device__ __forceinline__ int warp_exclusive_scan(int v, unsigned mask = 0xffff
 #pragma unroll
   for (int offset = 1; offset < WARP_SIZE; offset <<= 1) {
     int n = SHFL_UP(mask, v, offset);
-#ifdef __HIP_PLATFORM_HCC__
+#ifndef __CUDA_ARCH__ 
     asm volatile("s_barrier" : : : "memory");
 #endif
     if ((threadIdx.x & (WARP_SIZE - 1)) >= offset) v += n;
