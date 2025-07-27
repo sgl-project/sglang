3c61463e196d64514b4172776213209132d1fbf1
fzyzcjy <ch271828n@outlook.com>
2025-07-27 21:20:55 +0800

    more

--- python/sglang/srt/cuda_memory.py
+++ python/sglang/srt/cuda_memory.py
@@ -1,5 +1,17 @@
 from cuda import cuda
 
+
+class SharedMemoryAllocator:
+    def malloc(self):
+        return TODO
+
+    def get_mem_handle(self):
+        return TODO
+
+    def open_mem_handle(self):
+        return TODO
+
+
 # copied from TensorRT-LLM
 def _check_cu_result(cu_func_ret):
     # TODO optimize code
