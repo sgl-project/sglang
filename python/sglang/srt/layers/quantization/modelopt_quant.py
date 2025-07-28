e402589b7d7d353e328d894f67f43153cd6aa29e
fzyzcjy <ch271828n@outlook.com>
2025-07-28 09:01:21 +0800

    more

--- python/sglang/srt/layers/quantization/modelopt_quant.py
+++ python/sglang/srt/layers/quantization/modelopt_quant.py
@@ -29,7 +29,7 @@ from sglang.srt.layers.quantization.utils import (
     requantize_with_max_scale,
 )
 from sglang.srt.layers.radix_attention import RadixAttention
-from sglang.srt.utils import is_cuda, next_power_of_2
+from sglang.srt.utils import is_cuda, next_power_of_2, dispose_tensor
 
 if TYPE_CHECKING:
     from sglang.srt.layers.moe.topk import TopKOutput
@@ -894,6 +894,7 @@ class ModelOptNvFp4FusedMoEMethod(FusedMoEMethodBase):
         layer.w13_blockscale_swizzled = Parameter(
             w13_blockscale_swizzled, requires_grad=False
         )
+        dispose_tensor(layer.w13_weight_scale.data)
         del layer.w13_weight_scale
 
         # This is for quantization, so we need to invert it.
@@ -930,6 +931,7 @@ class ModelOptNvFp4FusedMoEMethod(FusedMoEMethodBase):
         layer.w2_blockscale_swizzled = Parameter(
             w2_blockscale_swizzled, requires_grad=False
         )
+        dispose_tensor(layer.w2_weight_scale.data)
         del layer.w2_weight_scale
         layer.w2_weight = Parameter(layer.w2_weight.data, requires_grad=False)
 
