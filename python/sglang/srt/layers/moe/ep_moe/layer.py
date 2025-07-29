f04ac141a3893a31d370cab566c42d4c8a6f041a
ayrnb <641876696@qq.com>
2025-07-29 19:21:21 +0800

    clean code

--- python/sglang/srt/layers/moe/ep_moe/layer.py
+++ python/sglang/srt/layers/moe/ep_moe/layer.py
@@ -482,7 +482,6 @@ class DeepEPMoE(EPMoE):
             dispatch_output.topk_weights,
         )
         local_topk_ids = topk_idx
-        # hidden_states,hidden_states_scale=hidden_states
         local_topk_ids = (
             torch.where(local_topk_ids == -1, self.num_experts, topk_idx)
             .to(torch.int32)
