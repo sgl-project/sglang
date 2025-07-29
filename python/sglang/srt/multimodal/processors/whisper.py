a35063b1d83d72ed70537829c2ac3cbf1659af73
MahmoudAshraf97 <hassouna97.ma@gmail.com>
2025-07-29 16:33:21 +0300

    update processor init

--- python/sglang/srt/multimodal/processors/whisper.py
+++ python/sglang/srt/multimodal/processors/whisper.py
@@ -9,8 +9,8 @@ from sglang.srt.utils import load_audio
 class WhisperProcessor(BaseMultimodalProcessor):
     models = [WhisperForConditionalGeneration]
 
-    def __init__(self, hf_config, server_args, _processor):
-        super().__init__(hf_config, server_args, _processor)
+    def __init__(self, hf_config, server_args, _processor, *args, **kwargs):
+        super().__init__(hf_config, server_args, _processor, *args, **kwargs)
 
     async def process_mm_data_async(
         self,
@@ -25,6 +25,7 @@ class WhisperProcessor(BaseMultimodalProcessor):
             audio=audios,
             return_attention_mask=True,
             pad_to_multiple_of=320,
+            sampling_rate=16000,
         )
         input_ids = processor_output["labels"][0]
         input_ids_mask = input_ids != self._processor.tokenizer.pad_token_id
