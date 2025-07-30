9480077a4e87c9b4ec463efb2fbfa2e69df3132c
wangwenming <295323587@qq.com>
2025-07-30 18:28:13 +0800

    feat: add support for nvfp4 (w4a16fp4) quantization method

--- python/sglang/srt/layers/quantization/compressed_tensors/compressed_tensors.py
+++ python/sglang/srt/layers/quantization/compressed_tensors/compressed_tensors.py
@@ -31,6 +31,7 @@ from sglang.srt.layers.quantization.compressed_tensors.schemes import (
     CompressedTensorsScheme,
     CompressedTensorsW8A8Fp8,
     CompressedTensorsW8A16Fp8,
+    CompressedTensorsW4A16Fp4
 )
 from sglang.srt.layers.quantization.compressed_tensors.utils import (
     find_matched_target,
@@ -249,6 +250,21 @@ class CompressedTensorsConfig(QuantizationConfig):
             return supported
         else:
             return False
+    
+    def _is_fp4a16_nvfp4(self, weight_quant: BaseModel,
+                         input_quant: BaseModel):
+
+        is_weight_only = weight_quant is not None and input_quant is None
+        is_tensor_group_quant = (
+            weight_quant.strategy == QuantizationStrategy.TENSOR_GROUP.value)
+        is_symmetric = weight_quant.symmetric
+
+        is_group_size_16 = weight_quant.group_size == 16
+        is_float_type = weight_quant.type == QuantizationType.FLOAT
+        is_4_bits = weight_quant.num_bits == 4
+
+        return (is_weight_only and is_tensor_group_quant and is_float_type
+                and is_4_bits and is_group_size_16 and is_symmetric)
 
     def _is_static_tensor_w8a8(
         self, weight_quant: BaseModel, input_quant: BaseModel
@@ -362,6 +378,9 @@ class CompressedTensorsConfig(QuantizationConfig):
     ) -> CompressedTensorsScheme:
 
         # Detect If Mixed Precision
+        if self._is_fp4a16_nvfp4(weight_quant, input_quant):
+            return CompressedTensorsW4A16Fp4()
+
         if self._is_wNa16_group_channel(weight_quant, input_quant):
             if not VLLM_AVAILABLE:
                 raise ImportError(
