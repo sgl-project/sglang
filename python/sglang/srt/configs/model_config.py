8f48ed6febb644c22f425d4c19c177e3a2f4add7
nnnobody-code <nnnobody@foxmail.com>
2025-07-30 19:05:04 +0800

    support image input

--- python/sglang/srt/configs/model_config.py
+++ python/sglang/srt/configs/model_config.py
@@ -326,10 +326,10 @@ class ModelConfig:
                 "kv_n_heads",
                 self.hf_config.num_attention_heads,
             )
-        
+
         # For Step3v
         if self.hf_config.model_type in ["step3v"]:
-            return getattr(self.hf_text_config, "num_attention_groups", 1)
+            return getattr(self.text_config, "num_attention_groups", 1)
 
         attributes = [
             # For Falcon:
@@ -648,6 +648,8 @@ multimodal_model_archs = [
     "InternS1ForConditionalGeneration",
     "Phi4MMForCausalLM",
     "VILAForConditionalGeneration",
+    "Step3VLForConditionalGeneration",
+    "MMGPTStep3vForCausalLM",
 ]
 
 
