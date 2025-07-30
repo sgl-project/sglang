f86a0746588a626b60ff2a599e65765bc23f2a3d
jianan-gu <jianan.gu@intel.com>
2025-07-30 02:59:36 -0400

    Revert "add fp8 support for graph runner (#46)"
    This reverts commit 6e036b7d660f05184931ed7dbf3576bfa4b4ae30.

--- sgl-kernel/python/sgl_kernel/cpu_register_fake.py
+++ sgl-kernel/python/sgl_kernel/cpu_register_fake.py
@@ -105,6 +105,131 @@ def _(
 ):
     return torch.empty_like(x)
 
+
+
+# # @torch.library.register_fake("sgl_kernel::forward_moe_fused")
+# # def _(
+# #     hidden_states,
+# #     MoEGate_weight,
+# #     MoEGate_bias,
+# #     fused_experts_w13_weight,
+# #     fused_experts_w2_weight,
+# #     shared_expert_w1,
+# #     shared_expert_w2,
+# #     top_k,
+# #     use_grouped_topk,
+# #     renormalize,
+# #     fused_experts_use_int8_w8a8,
+# #     fused_experts_use_fp8_w8a16,
+# #     fused_experts_inplace,
+# #     routed_scaling_factor,
+# #     shared_expert_inplace,
+# #     shared_expert_use_int8_w8a8,
+# #     shared_expert_use_fp8_w8a16,
+# #     tp_size,
+# #     topk_group,
+# #     num_expert_group,
+# #     correction_bias,
+# #     fused_experts_w1_scale,
+# #     fused_experts_w2_scale,
+# #     fused_experts_a1_scale,
+# #     fused_experts_a2_scale,
+# #     fused_experts_block_size,
+# #     shared_expert_w1_scale,
+# #     shared_expert_w2_scale,
+# #     shared_expert_block_size,
+# #     shared_expert_a1_scale,
+# #     shared_expert_a2_scale,
+# #     process_group,
+# #     op,
+# #     is_vnni,
+# # ):
+# #     num_tokens = hidden_states.shape[0]
+# #     hidden_dim = hidden_states.shape[1]
+# #     return hidden_states.new_empty(num_tokens, hidden_dim)
+
+
+# # @torch.library.register_fake("sgl_kernel::forward_absorb_decode_fused_cpu")
+# # def _(
+# #     hidden_states,
+# #     q_a_proj_weight,
+# #     q_b_proj_weight,
+# #     kv_a_proj_weight,
+# #     w_kc,
+# #     q_a_layernorm_weight,
+# #     kv_a_layernorm_weight,
+# #     positions,
+# #     cos_sin_cache,
+# #     k_cache,
+# #     v_cache,
+# #     loc,
+# #     attn_logits,
+# #     req_to_token,
+# #     req_pool_indices,
+# #     seq_lens,
+# #     w_vc,
+# #     o_proj_weight,
+# #     o_proj_bias,
+# #     eps,
+# #     use_int8_w8a8,
+# #     use_fp8_w8a16,
+# #     sm_scale,
+# #     logit_cap,
+# #     tp_k_head_num,
+# #     qk_head_dim,
+# #     tp_v_head_num,
+# #     v_head_dim,
+# #     tp_q_head_num,
+# #     num_local_heads,
+# #     kv_lora_rank,
+# #     tp_size,
+# #     tp_rank,
+# #     o_proj_use_int8_w8a8,
+# #     o_proj_use_fp8_w8a16,
+# #     o_proj_out_dtype,
+# #     q_a_proj_scale,
+# #     q_b_proj_scale,
+# #     kv_a_proj_scale,
+# #     block_size,
+# #     bmm_scale,
+# #     process_group,
+# #     op,
+# #     o_proj_scale,
+# #     o_proj_block_size,
+# #     is_vnni):
+# #     num_seqs = hidden_states.shape[0]
+# #     num_heads = w_kc.shape[0]
+# #     kv_lora_rank = w_kc.shape[1]
+# #     qk_rope_head_dim = kv_a_proj_weight.shape[0] - kv_lora_rank
+# #     query = hidden_states.new_empty(num_seqs, num_heads, kv_lora_rank + qk_rope_head_dim)
+    
+# #     query= query.reshape(-1, tp_q_head_num * qk_head_dim)
+# #     if qk_head_dim != v_head_dim:
+# #         attn_output = query.new_empty(query.shape[0], tp_q_head_num * v_head_dim)
+# #     else:
+# #         attn_output = query
+# #     attn_output = attn_output.reshape(-1, num_local_heads, kv_lora_rank)
+# #     M = attn_output.shape[0]
+# #     N = o_proj_weight.shape[0]
+# #     return attn_output.new_empty(M, N)
+
+
+# @torch.library.register_fake("sgl_kernel::grouped_topk_cpu")
+# def _(
+#     hidden_states,
+#     router_logits,
+#     top_k,
+#     renormalize,
+#     num_expert_group,
+#     topk_group,
+# ):
+#     shape = (hidden_states.shape[0], top_k)
+#     device = hidden_states.device
+#     topk_weights = torch.empty(shape, device=device, dtype=torch.float32)
+#     topk_ids = torch.empty(shape, device=device, dtype=torch.int)
+#     return topk_weights, topk_ids
+
+
 @torch.library.register_fake("sgl_kernel::biased_grouped_topk_cpu")
 def _(
     hidden_states,
@@ -126,9 +251,28 @@ def _(
     topk_ids = torch.empty(shape, device=device, dtype=torch.int)
     return topk_weights, topk_ids
 
+
 @torch.library.register_fake("sgl_kernel::rmsnorm_cpu")
 def _(input, weight, eps):
     return torch.empty_like(input)
+
+
+# @torch.library.register_fake("sgl_kernel::int8_scaled_mm_with_quant")
+# def _(
+#     mat1,
+#     mat2,
+#     scales2,
+#     bias,
+#     out_dtype,
+#     is_vnni):
+#     M = mat1.shape[0]
+#     N = mat2.shape[0]
+#     return mat1.new_empty(M, N, dtype=out_dtype)
+
+
+# # @torch.library.register_fake("sgl_kernel::rotary_position_embedding_cpu")
+# # def _(t_pos, q_pe, k_pe, t_emb_pos):
+# #     return torch.empty_like(q_pe), torch.empty_like(k_pe)
 
 @torch.library.register_fake("sgl_kernel::silu_and_mul_cpu")
 def _(input):
@@ -139,20 +283,6 @@ def _(
     mat1,
     mat2,
     scales2,
-    bias,
-    out_dtype,
-    is_vnni,
-):
-    M = mat1.shape[0]
-    N = mat2.shape[0]
-    return mat1.new_empty(M, N, dtype=out_dtype)
-
-@torch.library.register_fake("sgl_kernel::fp8_scaled_mm_cpu")
-def _(
-    mat1,
-    mat2,
-    scales2,
-    block_size,
     bias,
     out_dtype,
     is_vnni,
