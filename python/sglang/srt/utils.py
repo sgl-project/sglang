cef1d5442949397295be01205837d605890c2583
Jiang, Yanbing <yanbing.jiang@intel.com>
2025-07-30 12:04:50 +0000

    Enable torch_native backend UT on AMX CPUs

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -216,8 +216,15 @@ except:
     is_intel_amx_backend_available = False
 
 
+@lru_cache(maxsize=1)
 def cpu_has_amx_support():
-    return torch._C._cpu._is_amx_tile_supported() and is_intel_amx_backend_available
+    return (
+        # Env DISABLE_AMX is to check whether AMX is disabled by user.
+        # Current scenario is to enable torch_native UT on AMX CPUs.
+        os.environ.get("DISABLE_AMX") != "1"
+        and torch._C._cpu._is_amx_tile_supported()
+        and is_intel_amx_backend_available
+    )
 
 
 def use_intel_amx_backend(layer):
