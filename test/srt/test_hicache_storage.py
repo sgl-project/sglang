bd700932e0569dc4649d8973cd2d1fdff2c17447
Zhiqiang Xie <xiezhq@stanford.edu>
2025-07-31 00:22:01 -0700

    cleaning

--- test/srt/test_hicache_storage.py
+++ test/srt/test_hicache_storage.py
@@ -26,7 +26,7 @@ class TestHiCache(CustomTestCase):
                 "--mem-fraction-static",
                 0.7,
                 "--hicache-size",
-                50,
+                100,
                 "--page-size",
                 "64",
                 "--hicache-storage-backend",
