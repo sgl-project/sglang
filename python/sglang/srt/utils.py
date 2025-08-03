2b7d6838fd900e03f523363a071aec979a363be7
ybyang <10629930+whybeyoung@users.noreply.github.com>
2025-08-03 16:59:06 +0800

    [Improvements] Merge health check route (#8444)
    Signed-off-by: ybyang <ybyang7@iflytek.com>
    Co-authored-by: Lianmin Zheng <lianminzheng@gmail.com>
    Co-authored-by: Kan Wu <wukanustc@gmail.com>

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -44,7 +44,6 @@ import traceback
 import warnings
 from collections import OrderedDict, defaultdict
 from contextlib import contextmanager
-from enum import Enum
 from functools import lru_cache
 from importlib.metadata import PackageNotFoundError, version
 from importlib.util import find_spec
@@ -92,6 +91,7 @@ logger = logging.getLogger(__name__)
 
 show_time_cost = False
 time_infos = {}
+
 
 HIP_FP8_E4M3_FNUZ_MAX = 224.0
 
