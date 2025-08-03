7180864a6541094014015ff7a1b3174dd6493800
leoneo <1320612015@qq.com>
2025-08-03 10:20:19 +0800

    add scalar_types

--- sgl-kernel/tests/test_marlin_repack.py
+++ sgl-kernel/tests/test_marlin_repack.py
@@ -3,7 +3,7 @@ import pytest
 import torch
 from sgl_kernel import awq_marlin_repack
 
-from sglang.srt.layers.quantization.scalar_type import scalar_types
+from sgl_kernel.scalar_type import scalar_types
 from sglang.srt.layers.quantization.utils import (
     get_pack_factor,
     pack_cols,
