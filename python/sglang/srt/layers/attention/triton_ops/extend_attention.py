065ac55a0bcf4a4bd62f56e3493f0587007e730b
ispobock <ispobaoke@gmail.com>
2025-08-06 02:39:03 -0700

    upd

--- python/sglang/srt/layers/attention/triton_ops/extend_attention.py
+++ python/sglang/srt/layers/attention/triton_ops/extend_attention.py
@@ -263,7 +263,7 @@ def _fwd_kernel(
             final_mask &= window_mask
 
         SKIP_TILE = False
-        if (USE_CUSTOM_MASK and not SKIP_PREFIX_CUSTOM_MASK) or SLIDING_WINDOW_SIZE > 0:
+        if USE_CUSTOM_MASK or SLIDING_WINDOW_SIZE > 0:
             SKIP_TILE = tl.max(tl.max(final_mask.to(tl.int32), axis=1), axis=0) == 0
 
         if not SKIP_TILE:
