e3f862b4451f5f4d19757fe33708a41b6af12ba2
fzyzcjy <ch271828n@outlook.com>
2025-08-06 18:47:09 +0800

    more

--- scripts/ci_install_dependency.sh
+++ scripts/ci_install_dependency.sh
@@ -11,7 +11,7 @@ fi
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bash "${SCRIPT_DIR}/killall_sglang.sh"
 
-if [ "$MODE_BLACKWELL" = "1" ]; then
+if ! command -v git >/dev/null 2>&1; then
     sudo apt update
     sudo apt install -y git
 fi
