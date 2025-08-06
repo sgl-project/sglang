77b84d11a395cb04243371d6de812f454f5485ac
fzyzcjy <ch271828n@outlook.com>
2025-08-06 14:11:37 +0800

    more
    (cherry picked from commit 7e9dce6cd45bf34a87a355cdae5d4d584903db14)

--- test/srt/test_gpt_oss_1gpu.py
+++ test/srt/test_gpt_oss_1gpu.py
@@ -6,8 +6,10 @@ from test_gpt_oss_common import BaseTestGptOss
 class TestGptOss1Gpu(BaseTestGptOss):
     def test_mxfp4_20b(self):
         self.run_test(
-            model=TODO,
-
+            model_variant="20b",
+            quantization="mxfp4",
+            # TODO
+            expected_score=0.50,
         )
 
 
