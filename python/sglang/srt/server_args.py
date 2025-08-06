f90f4d26e14f428053528a98f81b37c9a1365fcd
Co-author fzyzcjy <ch271828n@outlook.com>
2025-08-05 20:55:30 -0700

    mxfp4

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -463,6 +463,11 @@ class ServerArgs:
             self.enable_triton_kernel_moe = True
             self.disable_hybrid_swa_memory = True
 
+            quantization_config = getattr(self.get_hf_config(), "quantization_config", None)
+            if quantization_config is not None and quantization_config.get("quant_method") == "mxfp4":
+                # use bf16 for mxfp4 triton kernels
+                self.dtype = "bfloat16"
+
         # Set page size
         if self.page_size is None:
             self.page_size = 1
