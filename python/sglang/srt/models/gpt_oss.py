a2f7758f1d58f6d1f987b2d27d2d2ec3953e01ad
eigen <52445717+yyihuang@users.noreply.github.com>
2025-08-06 22:18:27 -0400

    feat: openai oss attention sink support with trtllm-gen backend #8825 (#8834)
    Co-authored-by: averyhuang <averyh@nvidia.com>

--- python/sglang/srt/models/gpt_oss.py
+++ python/sglang/srt/models/gpt_oss.py
@@ -301,7 +301,7 @@ class GptOssAttention(nn.Module):
         hidden_states, forward_batch, inner_state = intermediate_state
         if inner_state is None:
             return hidden_states
-        attn_output = self.attn(*inner_state, sinks=self.sinks)
+        attn_output = self.attn(*inner_state, sinks=self.sinks.to(torch.float32))
         output, _ = self.o_proj(attn_output)
         return output
 
