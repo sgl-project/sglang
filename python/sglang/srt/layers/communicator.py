8ad20b6f4cb5f337ea6d7d2b1d4f80c647898fc7
Elfie Guo <elfieg@nvidia.com>
2025-08-07 06:48:15 +0000

    address comment.

--- python/sglang/srt/layers/communicator.py
+++ python/sglang/srt/layers/communicator.py
@@ -541,10 +541,6 @@ class CommunicateSummableTensorPairFn:
             forward_batch.gathered_buffer[: forward_batch.input_ids.shape[0]],
             hidden_states,
         )
-        if hidden_states.untyped_storage() is global_hidden_states.untyped_storage():
-            # dp_scatter is an in-place operation and requires input and output tensors to not be aliased.
-            # so we create a separate buffer if they share the same storage.
-            hidden_states = torch.empty_like(hidden_states)
         dp_scatter(hidden_states, global_hidden_states, forward_batch)
         return hidden_states, residual
 
