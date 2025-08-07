e7c33269d0defccb444e4992d2e3ea4891c7d6ce
VDV1985 <vladdv85@mail.ru>
2025-08-07 16:24:11 +0300

    graph tests

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -156,9 +156,11 @@ suites = {
     ],
     "per-commit-1-ascend-npu": [
         TestFile("test_ascend_tp1_bf16.py", 400),
+        TestFile("test_ascend_graph_tp1_bf16.py", 400),
     ],
     "per-commit-2-ascend-npu": [
         TestFile("test_ascend_tp2_bf16.py", 400),
+        TestFile("test_ascend_graph_tp2_bf16.py", 400),
     ],
     "per-commit-4-ascend-npu": [
         TestFile("test_ascend_mla_w8a8int8.py", 400),
