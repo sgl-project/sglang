d6fa5aad08d7a5bb378fbc397ab143d56f2e3fc7
fzyzcjy <ch271828n@outlook.com>
2025-08-08 21:39:47 +0800

    Revert "hack use few layers"
    This reverts commit bb460951f841a2dd9b0e7575f1e8394a59eca7e8.

--- python/sglang/srt/models/gpt_oss.py
+++ python/sglang/srt/models/gpt_oss.py
@@ -497,9 +497,7 @@ class GptOssModel(nn.Module):
             residual = pp_proxy_tensors["residual"]
 
         aux_hidden_states = []
-        # HACK
-        # for i in range(self.start_layer, self.end_layer):
-        for i in range(self.start_layer, 3):
+        for i in range(self.start_layer, self.end_layer):
             with get_global_expert_distribution_recorder().with_current_layer(i):
                 if i in self.layers_to_capture:
                     aux_hidden_states.append(hidden_states + residual)
