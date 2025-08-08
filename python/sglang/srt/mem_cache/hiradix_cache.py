dde52a9e580ad307281c860d94ef76d1eae7536d
Zhiqiang Xie <xiezhq@stanford.edu>
2025-08-08 13:52:56 -0700

    lint

--- python/sglang/srt/mem_cache/hiradix_cache.py
+++ python/sglang/srt/mem_cache/hiradix_cache.py
@@ -470,7 +470,7 @@ class HiRadixCache(RadixCache):
         last_host_node, token_ids, host_indices, operation = self.ongoing_prefetch[
             req_id
         ]
-        
+
         if operation.host_indices is None:
             # prefetch has not been issued due to insufficient host memory
             return True
