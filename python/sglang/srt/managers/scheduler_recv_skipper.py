0cc650fb77f35cd4105e3a9f861e123e8089a675
fzyzcjy <ch271828n@outlook.com>
2025-08-08 11:51:56 +0800

    more

--- python/sglang/srt/managers/scheduler_recv_skipper.py
+++ python/sglang/srt/managers/scheduler_recv_skipper.py
@@ -3,6 +3,10 @@ from sglang.srt.server_args import ServerArgs
 
 
 class SchedulerRecvSkipper:
+    @staticmethod
+    def maybe_create(server_args: ServerArgs):
+        return TODO
+
     def __init__(self, server_args: ServerArgs):
         # Can be supported if needed, but may need e.g. `global_forward_mode`
         assert not server_args.enable_dp_attention
