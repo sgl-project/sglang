d5984ff09d7e9e0690edaa7e59b94b84cdc21a4b
GeLee <leege233@gmail.com>
2025-08-10 01:04:20 +0800

    rm monkey_patch_torch_reductions in _unwarp_tensor

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -1797,7 +1797,6 @@ def _model_load_weights_direct(model, named_tensors: List[Tuple[str, torch.Tenso
 
 def _unwrap_tensor(tensor, tp_rank, device):
     if isinstance(tensor, LocalSerializedTensor):
-        monkey_patch_torch_reductions()
         tensor = tensor.get(tp_rank)
     return tensor.to(device)
 
