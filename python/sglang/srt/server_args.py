ac5765d1d5fd03f31857f22f84ded962819c2095
fzyzcjy <ch271828n@outlook.com>
2025-08-09 20:46:26 +0800

    more

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -2109,8 +2109,16 @@ class ServerArgs:
             )
 
             if is_sm100_supported() and is_mxfp4_quant_format:
-                self.enable_flashinfer_mxfp4_moe = True
-                self.enable_triton_kernel_moe = False
+                # TODO we should use --moe-backend
+                num_enabled_backends = (
+                    int(self.enable_flashinfer_mxfp4_moe)
+                    + int(self.enable_flashinfer_mxfp4_bf16_moe)
+                    + int(self.enable_triton_kernel_moe)
+                )
+                assert num_enabled_backends <= 1
+                if num_enabled_backends == 0:
+                    # TODO shall we use mxfp4 or mxfp4_bf16 by default?
+                    self.enable_flashinfer_mxfp4_moe = True
                 logger.warning(
                     "Detected SM100 and MXFP4 quantization format for GPT-OSS model, enabling FlashInfer MXFP4 MOE kernel."
                 )
