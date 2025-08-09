d9b03936bf68c35f932bba5d2f682389b3f98c1f
Lianmin Zheng <lianminzheng@gmail.com>
2025-08-09 12:32:58 -0700

    Fix imports warnings

--- python/sglang/srt/entrypoints/engine.py
+++ python/sglang/srt/entrypoints/engine.py
@@ -627,7 +627,6 @@ def _set_envs_and_config(server_args: ServerArgs):
     os.environ["NCCL_CUMEM_ENABLE"] = str(int(server_args.enable_symm_mem))
     if not server_args.enable_symm_mem:
         os.environ["NCCL_NVLS_ENABLE"] = str(int(server_args.enable_nccl_nvls))
-    os.environ["TORCH_NCCL_AVOID_RECORD_STREAMS"] = "1"
     os.environ["CUDA_DEVICE_MAX_CONNECTIONS"] = "4"
     os.environ["CUDA_MODULE_LOADING"] = "AUTO"
 
