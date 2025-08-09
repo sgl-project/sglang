f8697ba956b2428a5e7c50b5cd0ce4f9a9348b52
Lianmin Zheng <lianminzheng@gmail.com>
2025-08-08 17:25:08 -0700

    Use uv instead

--- scripts/ci_install_dependency.sh
+++ scripts/ci_install_dependency.sh
@@ -16,37 +16,39 @@ bash "${SCRIPT_DIR}/killall_sglang.sh"
 # Install apt packages
 apt install -y git libnuma-dev
 
-# Clean up existing installations
+# Install uv
 pip install --upgrade pip
-pip uninstall -y flashinfer_python sgl-kernel sglang vllm || true
-pip cache purge || true
+pip install uv
+
+# Clean up existing installations
+uv pip uninstall -y flashinfer_python sgl-kernel sglang vllm || true
 
 # Install the main package
-pip install -e "python[dev]" --extra-index-url https://download.pytorch.org/whl/${CU_VERSION}
+uv pip install -e "python[dev]" --extra-index-url https://download.pytorch.org/whl/${CU_VERSION}
 
 if [ "$IS_BLACKWELL" = "1" ]; then
     # TODO auto determine sgl-kernel version
     SGL_KERNEL_VERSION=0.3.2
-    pip3 install https://github.com/sgl-project/whl/releases/download/v${SGL_KERNEL_VERSION}/sgl_kernel-${SGL_KERNEL_VERSION}-cp39-abi3-manylinux2014_x86_64.whl --force-reinstall
+    uv pip install https://github.com/sgl-project/whl/releases/download/v${SGL_KERNEL_VERSION}/sgl_kernel-${SGL_KERNEL_VERSION}-cp39-abi3-manylinux2014_x86_64.whl --force-reinstall
 fi
 
 # Show current packages
-pip list
+uv pip list
 
 # Install additional dependencies
-pip install mooncake-transfer-engine==0.3.5 nvidia-cuda-nvrtc-cu12 py-spy huggingface_hub[hf_xet]
+uv pip install mooncake-transfer-engine==0.3.5 nvidia-cuda-nvrtc-cu12 py-spy huggingface_hub[hf_xet]
 
 if [ "$IS_BLACKWELL" != "1" ]; then
     # For lmms_evals evaluating MMMU
     git clone --branch v0.3.3 --depth 1 https://github.com/EvolvingLMMs-Lab/lmms-eval.git
-    pip install -e lmms-eval/
+    uv pip install -e lmms-eval/
 
     # Install xformers
-    pip install -U xformers --index-url https://download.pytorch.org/whl/${CU_VERSION} --no-deps
+    uv pip install -U xformers --index-url https://download.pytorch.org/whl/${CU_VERSION} --no-deps
 fi
 
 # Install FlashMLA for attention backend tests
 # pip install git+https://github.com/deepseek-ai/FlashMLA.git
 
 # Show current packages
-pip list
+uv pip list
