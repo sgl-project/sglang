a09b3245e28bfd4ca2a1441a14dfbe50033876cf
fzyzcjy <ch271828n@outlook.com>
2025-08-11 14:09:11 +0800

    more

--- sgl-kernel/benchmark/bench_rotary_embedding.py
+++ sgl-kernel/benchmark/bench_rotary_embedding.py
@@ -1,82 +1,34 @@
 import itertools
-import time
-from functools import partial
-from pathlib import Path
 
 import torch
 import triton
 
 from sglang.srt.bench_utils import bench_kineto
-from sglang.srt.layers.quantization.fp8_kernel import (
-    create_per_token_group_quant_fp8_output_scale,
-)
-from sglang.srt.layers.quantization.fp8_kernel import (
-    per_token_group_quant_8bit as triton_per_token_group_quant_8bit,
-)
-from sglang.srt.layers.quantization.fp8_kernel import sglang_per_token_group_quant_8bit
-from sglang.srt.utils import is_hip
 
-_is_hip = is_hip()
-fp8_type_ = torch.float8_e4m3fnuz if _is_hip else torch.float8_e4m3fn
-
-
-num_tokens_range = [1, 4, 16, 64, 256, 768, 2048, 8192, 16384]
-hidden_dim_range = [1536, 7168, 18432]  # For DeepSeek V3/R1
-group_size_range = [128]  # For DeepSeek V3/R1
-# TODO test int8
-dst_dtype_range = [fp8_type_]
-flags_range = [
-    dict(
-        column_major_scales=False,
-        scale_tma_aligned=False,
-        scale_ue8m0=False,
-    ),
-    dict(
-        column_major_scales=True,
-        scale_tma_aligned=False,
-        scale_ue8m0=False,
-    ),
-    dict(
-        column_major_scales=True,
-        scale_tma_aligned=True,
-        scale_ue8m0=False,
-    ),
-    dict(
-        column_major_scales=True,
-        scale_tma_aligned=True,
-        scale_ue8m0=True,
-    ),
-]
-
-
-configs = list(
-    itertools.product(
-        num_tokens_range,
-        hidden_dim_range,
-        group_size_range,
-        dst_dtype_range,
-        flags_range,
+configs = [
+    (batch_size, seq_len, save_kv_cache)
+    for save_kv_cache in (False, True)
+    for batch_size, seq_len in (
+        (1, 1),
+        (32, 1),
+        (2, 512),
     )
-)
+]
 
 
 @triton.testing.perf_report(
     triton.testing.Benchmark(
-        x_names=["num_tokens", "hidden_dim", "group_size", "dst_dtype", "flags"],
+        x_names=["batch_size", "seq_len", "save_kv_cache"],
         x_vals=configs,
         line_arg="provider",
-        line_vals=["triton", "sglang"],
-        line_names=["Triton", "SGL Kernel"],
-        styles=[("blue", "-"), ("green", "-")],
+        line_vals=["sglang"],
+        line_names=["SGL Kernel"],
+        styles=[("green", "-")],
         ylabel="us",
-        plot_name="per-token-group-quant-8bit-performance",
         args={},
     )
 )
-def benchmark(num_tokens, hidden_dim, group_size, dst_dtype, flags, provider):
-    if flags["scale_ue8m0"] and group_size != 128:
-        return
-
+def benchmark(batch_size, seq_len, save_kv_cache, provider):
     device = torch.device("cuda")
 
     x = torch.randn(num_tokens, hidden_dim, device=device, dtype=torch.bfloat16)
