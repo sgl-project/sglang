48546d24818f860de6e97dcd8221caa71ae1e26c
Lianmin Zheng <lianminzheng@gmail.com>
2025-08-13 12:10:32 -0700

    Fix triton

--- python/sglang/srt/layers/attention/triton_backend.py
+++ python/sglang/srt/layers/attention/triton_backend.py
@@ -330,7 +330,7 @@ class TritonAttnBackend(AttentionBackend):
             mask_indptr = None
             attn_logits = None
             attn_lse = None
-            max_extend_len = torch.max(forward_batch.extend_seq_lens_cpu).item()
+            max_extend_len = max(forward_batch.extend_seq_lens_cpu)
             num_kv_splits = None
 
         self.forward_metadata = ForwardMetadata(
