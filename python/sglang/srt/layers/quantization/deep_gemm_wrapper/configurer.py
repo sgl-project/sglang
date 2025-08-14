bd1722fbe8c786dc2d508fbfb31877071781ad17
fzyzcjy <ch271828n@outlook.com>
2025-08-14 09:19:58 +0800

    more
    (cherry picked from commit df0a6fd820023da6a10a9abce30041326037c550)

--- python/sglang/srt/layers/quantization/deep_gemm_wrapper/configurer.py
+++ python/sglang/srt/layers/quantization/deep_gemm_wrapper/configurer.py
@@ -22,12 +22,5 @@ def _compute_enable_deep_gemm():
 
 ENABLE_JIT_DEEPGEMM = _compute_enable_deep_gemm()
 
-try:
-    from deep_gemm import fp8_gemm_nt
-
-    # They have not given a name to this breaking change
-    DEEPGEMM_BLACKWELL = True
-except ImportError:
-    DEEPGEMM_BLACKWELL = False
-
+DEEPGEMM_BLACKWELL = TODO_is_blackwell
 DEEPGEMM_SCALE_UE8M0 = DEEPGEMM_BLACKWELL
