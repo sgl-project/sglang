3027c13d206859d6c79a00b0a8d7e648046c220f
fzyzcjy <ch271828n@outlook.com>
2025-08-16 09:11:58 +0800

    more

--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -86,6 +86,7 @@ GLOBAL_SERVER_ARGS_KEYS = [
     "disable_chunked_prefix_cache",
     "disable_radix_cache",
     "enable_dp_lm_head",
+    "flashinfer_mxfp4_moe_precision",
     "enable_flashinfer_allreduce_fusion",
     "moe_dense_tp_size",
     "ep_dispatch_algorithm",
