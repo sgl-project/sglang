60239da6c589d472f0cd2a3081fca8c59b8a372f
jianan-gu <jianan.gu@intel.com>
2025-08-17 21:35:02 -0400

    [compile] add more bs captured for compile

--- python/sglang/srt/model_executor/cpu_graph_runner.py
+++ python/sglang/srt/model_executor/cpu_graph_runner.py
@@ -96,7 +96,7 @@ def get_batch_sizes_to_capture(model_runner: ModelRunner):
 
     # cpu torch compile only speeds up decoding by
     # reducing python overhead when bs is small
-    capture_bs = list(range(1, 9)) + list(range(10, 17, 2))
+    capture_bs = list(range(1, 9)) + list(range(10, 17, 2)) + list([32, 64])
 
     capture_bs = [bs for bs in capture_bs if bs <= server_args.torch_compile_max_bs]
     capture_bs = [bs for bs in capture_bs if bs <= model_runner.req_to_token_pool.size]
