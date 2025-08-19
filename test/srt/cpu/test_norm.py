fbc1e1d7f1c3c1fd8af6505181da6b56ad50b2a1
Zheng, Beilei <beilei.zheng@intel.com>
2025-08-18 19:33:13 -0700

    add gemma_rmsnorm and gemma_fused_add_rmsnorm

--- test/srt/cpu/test_norm.py
+++ test/srt/cpu/test_norm.py
@@ -47,6 +47,25 @@ class TestNorm(CustomTestCase):
         output = output * (1.0 + weight.float())
         return output.type_as(x)
 
+    def _gemma_rmsnorm_native(
+        self,
+        x: torch.Tensor,
+        weight: torch.Tensor,
+        variance_epsilon: float = 1e-6,
+        residual: Optional[torch.Tensor] = None,
+    ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
+        orig_dtype = x.dtype
+        if residual is not None:
+            x = x + residual
+            residual = x
+
+        x = x.float()
+        variance = x.pow(2).mean(dim=-1, keepdim=True)
+        x = x * torch.rsqrt(variance + variance_epsilon)
+        x = x * (1.0 + weight.float())
+        x = x.to(orig_dtype)
+        return x if residual is None else (x, residual)
+
     def _norm_test(self, m, n, dtype):
 
         x = torch.randn([m, n], dtype=dtype)
@@ -89,6 +108,35 @@ class TestNorm(CustomTestCase):
         atol = rtol = precision[ref_out.dtype]
         torch.testing.assert_close(ref_out, out, atol=atol, rtol=rtol)
 
+    def _gemma_rmsnorm_test(self, m, n, dtype):
+
+        x = torch.randn([m, n], dtype=dtype)
+        x = make_non_contiguous(x)
+        hidden_size = x.size(-1)
+        weight = torch.randn(hidden_size, dtype=dtype)
+        variance_epsilon = 1e-6
+
+        out = torch.ops.sgl_kernel.gemma_rmsnorm_cpu(x, weight, variance_epsilon)
+        ref_out = self._gemma_rmsnorm_native(x, weight, variance_epsilon)
+
+        atol = rtol = precision[ref_out.dtype]
+        torch.testing.assert_close(ref_out, out, atol=atol, rtol=rtol)
+
+        ref_x = x.clone()
+        residual = torch.randn([m, hidden_size], dtype=dtype)
+        ref_residual = residual.clone()
+
+        torch.ops.sgl_kernel.gemma_fused_add_rmsnorm_cpu(
+            x, residual, weight, variance_epsilon
+        )
+
+        ref_x, ref_residual = self._gemma_rmsnorm_native(
+            ref_x, weight, variance_epsilon, ref_residual
+        )
+
+        torch.testing.assert_close(x, ref_x, atol=atol, rtol=rtol)
+        torch.testing.assert_close(residual, ref_residual, atol=atol, rtol=rtol)
+
     def _gemma3_rmsnorm_test(self, m, n, dtype):
         x_list = [
             torch.randn([m, n], dtype=dtype),
@@ -110,6 +158,7 @@ class TestNorm(CustomTestCase):
             with self.subTest(m=params[0], n=params[1], dtype=params[2]):
                 self._norm_test(*params)
                 self._l2norm_test(*params)
+                self._gemma_rmsnorm_test(*params)
                 self._gemma3_rmsnorm_test(*params)
 
 
