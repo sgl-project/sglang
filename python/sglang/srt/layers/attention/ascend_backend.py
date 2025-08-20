a55910ddf5ce54bcf3aa7e6db54775c14ea0b8a8
zhoumenglei <zhoumenglei@meituan.com>
2025-08-20 18:04:43 +0800

    rebase main

--- python/sglang/srt/layers/attention/ascend_backend.py
+++ python/sglang/srt/layers/attention/ascend_backend.py
@@ -102,6 +102,7 @@ class AscendAttnBackend(AttentionBackend):
         encoder_lens: Optional[torch.Tensor],
         forward_mode: ForwardMode,
         spec_info: Optional[Union[EagleDraftInput, EagleVerifyInput]],
+        extend_prefix_lens: Optional[torch.Tensor] = None,
     ):
         metadata = ForwardMetadata()
 
@@ -123,6 +124,7 @@ class AscendAttnBackend(AttentionBackend):
         forward_mode: ForwardMode,
         spec_info: Optional[Union[EagleDraftInput, EagleVerifyInput]],
         seq_lens_cpu: Optional[torch.Tensor],
+        extend_prefix_lens: Optional[torch.Tensor] = None,
     ):
         metadata = self.graph_metadata[bs]
         max_len = seq_lens_cpu[:bs].max().item()
