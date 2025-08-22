1ea98e2fcf2eaff361ad4696394a1d6d761f3b67
Keyang Ru <rukeyang@gmail.com>
2025-08-22 14:18:47 -0700

    [router] Move all protocols to spec.rs file  (#9519)

--- sgl-router/src/protocols/generate/mod.rs
+++
@@ -1,8 +0,0 @@
-// SGLang native Generate API module (/generate)
-
-pub mod request;
-pub mod types;
-
-// Re-export main types for convenience
-pub use request::GenerateRequest;
-pub use types::{GenerateParameters, InputIds, SamplingParams};
