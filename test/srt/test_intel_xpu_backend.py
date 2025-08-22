883144df363acb6bfafaab389b15c3a9db4bc350
diwei sun <diwei.sun@intel.com>
2025-08-21 23:06:42 -0700

    enable xpu ci

---
+++ test/srt/test_intel_xpu_backend.py
@@ -0,0 +1,64 @@
+"""
+Usage:
+python3 -m unittest test_intel_amx_attention_backend.TestIntelAMXAttnBackend.test_mmlu
+"""
+
+import os
+import unittest
+from functools import wraps
+
+from sglang.test.test_utils import (
+    DEFAULT_SMALL_MODEL_NAME_FOR_TEST_QWEN,
+    DEFAULT_MODEL_NAME_FOR_TEST_AWQ_INT4, 
+    CustomTestCase,
+    is_in_ci,
+    run_bench_one_batch,
+)
+
+
+def intel_xpu_benchmark(extra_args=None, min_throughput=None):
+    def decorator(test_func):
+        @wraps(test_func)
+        def wrapper(self):
+            common_args = [
+                "--disable-radix",
+                "--trust-remote-code",
+                "--mem-fraction-static",
+                "0.3",
+                "--batch-size",
+                "1",
+                "--device",
+                "xpu"
+            ]
+            full_args = common_args + (extra_args or [])
+
+            model = test_func(self)
+            prefill_latency, decode_throughput, decode_latency = run_bench_one_batch(
+                model, full_args
+            )
+
+            print(f"{model=}")
+            print(f"{prefill_latency=}")
+            print(f"{decode_throughput=}")
+            print(f"{decode_latency=}")
+
+            if is_in_ci() and min_throughput is not None:
+                self.assertGreater(decode_throughput, min_throughput)
+
+        return wrapper
+
+    return decorator
+
+
+class TestIntelAMXAttnBackend(CustomTestCase):
+
+    @intel_xpu_benchmark(min_throughput=10)
+    def test_latency_qwen_model(self):
+        return DEFAULT_SMALL_MODEL_NAME_FOR_TEST_QWEN
+
+    @intel_xpu_benchmark(min_throughput=10)
+    def test_latency_awq_int4_default_model(self):
+        return DEFAULT_MODEL_NAME_FOR_TEST_AWQ_INT4
+
+if __name__ == "__main__":
+    unittest.main()
