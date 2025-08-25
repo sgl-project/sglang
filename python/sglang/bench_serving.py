fef8ca6267aa8f1fd01826fb434edbd1458db7ab
Mick <mickjagger19@icloud.com>
2025-08-25 16:17:21 +0800

    upd

--- python/sglang/bench_serving.py
+++ python/sglang/bench_serving.py
@@ -57,7 +57,7 @@ def _create_bench_client_session():
     # the content. We increase the read_bufsize from 64K to 10M.
     # Define constants for timeout and buffer size for clarity and maintainability
     BENCH_AIOHTTP_TIMEOUT_SECONDS = 6 * 60 * 60  # 6 hours
-    BENCH_AIOHTTP_READ_BUFSIZE_BYTES = 10 * 1024 ** 2  # 10 MB
+    BENCH_AIOHTTP_READ_BUFSIZE_BYTES = 10 * 1024**2  # 10 MB
 
     aiohttp_timeout = aiohttp.ClientTimeout(total=BENCH_AIOHTTP_TIMEOUT_SECONDS)
     return aiohttp.ClientSession(
@@ -96,7 +96,7 @@ class RequestFuncOutput:
 
 
 def remove_prefix(text: str, prefix: str) -> str:
-    return text[len(prefix):] if text.startswith(prefix) else text
+    return text[len(prefix) :] if text.startswith(prefix) else text
 
 
 def remove_suffix(text: str, suffix: str) -> str:
@@ -552,8 +552,8 @@ async def async_request_sglang_generate(
                                     if num_new_tokens == 0:
                                         continue
                                     adjust_itl = (
-                                                     timestamp - most_recent_timestamp
-                                                 ) / num_new_tokens
+                                        timestamp - most_recent_timestamp
+                                    ) / num_new_tokens
                                     output.itl.extend([adjust_itl] * num_new_tokens)
 
                                 most_recent_timestamp = timestamp
@@ -1427,9 +1427,9 @@ def calculate_metrics(
         output_throughput_retokenized=sum(retokenized_output_lens) / dur_s,
         total_throughput=(total_input + sum(output_lens)) / dur_s,
         total_throughput_retokenized=(total_input + sum(retokenized_output_lens))
-                                     / dur_s,
+        / dur_s,
         mean_ttft_ms=np.mean(ttfts or 0)
-                     * 1000,  # ttfts is empty if streaming is not supported by backend
+        * 1000,  # ttfts is empty if streaming is not supported by backend
         median_ttft_ms=np.median(ttfts or 0) * 1000,
         std_ttft_ms=np.std(ttfts or 0) * 1000,
         p99_ttft_ms=np.percentile(ttfts or 0, 99) * 1000,
@@ -2026,7 +2026,7 @@ if __name__ == "__main__":
         type=float,
         default=0.0,
         help="Range of sampled ratio of input/output length, "
-             "used only for random dataset.",
+        "used only for random dataset.",
     )
     # random-image dataset args
     parser.add_argument(
@@ -2049,20 +2049,20 @@ if __name__ == "__main__":
         type=float,
         default=float("inf"),
         help="Number of requests per second. If this is inf, then all the requests are sent at time 0. "
-             "Otherwise, we use Poisson process to synthesize the request arrival times. Default is inf.",
+        "Otherwise, we use Poisson process to synthesize the request arrival times. Default is inf.",
     )
     parser.add_argument(
         "--max-concurrency",
         type=int,
         default=None,
         help="Maximum number of concurrent requests. This can be used "
-             "to help simulate an environment where a higher level component "
-             "is enforcing a maximum number of concurrent requests. While the "
-             "--request-rate argument controls the rate at which requests are "
-             "initiated, this argument will control how many are actually allowed "
-             "to execute at a time. This means that when used in combination, the "
-             "actual request rate may be lower than specified with --request-rate, "
-             "if the server is not processing requests fast enough to keep up.",
+        "to help simulate an environment where a higher level component "
+        "is enforcing a maximum number of concurrent requests. While the "
+        "--request-rate argument controls the rate at which requests are "
+        "initiated, this argument will control how many are actually allowed "
+        "to execute at a time. This means that when used in combination, the "
+        "actual request rate may be lower than specified with --request-rate, "
+        "if the server is not processing requests fast enough to keep up.",
     )
     parser.add_argument("--output-file", type=str, help="Output JSONL file name.")
     parser.add_argument(
@@ -2094,7 +2094,7 @@ if __name__ == "__main__":
         metavar='{"key1": "value1", "key2": "value2"}',
         type=str,
         help="Append given JSON object to the request payload. You can use this to specify"
-             "additional generate params like sampling params.",
+        "additional generate params like sampling params.",
     )
     parser.add_argument(
         "--apply-chat-template",
@@ -2105,7 +2105,7 @@ if __name__ == "__main__":
         "--profile",
         action="store_true",
         help="Use Torch Profiler. The endpoint must be launched with "
-             "SGLANG_TORCH_PROFILER_DIR to enable profiler.",
+        "SGLANG_TORCH_PROFILER_DIR to enable profiler.",
     )
     parser.add_argument(
         "--lora-name",
