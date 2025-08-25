4f7c5aef94085412087d5a1ce589d7164ab1627a
vincentzed <207368749+vincentzed@users.noreply.github.com>
2025-08-25 14:52:04 -0400

    remove mip

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -1,5 +1,3 @@
-from sglang.srt.utils import get_bool_env_var, get_int_env_var
-
 # Copyright 2023-2024 SGLang Team
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
