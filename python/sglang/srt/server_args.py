b5388a196f268947741ae0b06793034726eaba7a
Yineng Zhang <me@zhyncs.com>
2025-08-25 01:29:06 -0700

    fix: revert #8593 (#9581)

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -639,10 +639,6 @@ class ServerArgs:
                     logger.warning(
                         "DeepSeek MTP does not require setting speculative_draft_model_path."
                     )
-                if self.page_size != 1 and self.attention_backend == "flashinfer":
-                    raise ValueError(
-                        "Speculative decoding with page_size != 1 is not supported. Please set page_size to 1."
-                    )
 
             # Auto choose parameters
             if self.speculative_num_steps is None:
