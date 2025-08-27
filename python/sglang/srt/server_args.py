cb04db7025bc1addc702994a04096d6534df7309
hzh0425 <hzh0425@apache.org>
2025-08-27 08:55:20 +0800

    refactor(hicache): Introduce generic HiCacheStorageConfig for improved configuration management (#9555)
    Co-authored-by: Teng Ma <805522925@qq.com>

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -216,6 +216,7 @@ class ServerArgs:
     hicache_mem_layout: str = "layer_first"
     hicache_storage_backend: Optional[str] = None
     hicache_storage_prefetch_policy: str = "best_effort"
+    hicache_storage_backend_extra_config: Optional[str] = None
 
     # Double Sparsity
     enable_double_sparsity: bool = False
@@ -1640,6 +1641,12 @@ class ServerArgs:
             choices=["best_effort", "wait_complete", "timeout"],
             default=ServerArgs.hicache_storage_prefetch_policy,
             help="Control when prefetching from the storage backend should stop.",
+        )
+        parser.add_argument(
+            "--hicache-storage-backend-extra-config",
+            type=str,
+            default=ServerArgs.hicache_storage_backend_extra_config,
+            help="A dictionary in JSON string format containing extra configuration for the storage backend.",
         )
 
         # Double Sparsity
