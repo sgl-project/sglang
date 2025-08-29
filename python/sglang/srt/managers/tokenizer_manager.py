e1c769bff4c64c2e2a80bf47e43f54c53c52b68c
huanglong <huanglong@linux.alibaba.com>
2025-08-29 10:13:15 +0800

    fix
    Signed-off-by: huanglong <huanglong@linux.alibaba.com>

--- python/sglang/srt/managers/tokenizer_manager.py
+++ python/sglang/srt/managers/tokenizer_manager.py
@@ -1100,6 +1100,8 @@ class TokenizerManager:
     async def _wait_for_model_update_from_disk(
         self, obj: UpdateWeightFromDiskReqInput
     ) -> Tuple[bool, str]:
+        if self.server_args.tokenizer_worker_num > 1:
+            obj = MultiTokenizerWarpper(self.worker_id, obj)
         self.send_to_scheduler.send_pyobj(obj)
         self.model_update_result = asyncio.Future()
         if self.server_args.dp_size == 1:
