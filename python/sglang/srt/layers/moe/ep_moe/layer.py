52368d6c0d04a0bb172f5d5f69906ba948bb45ae
Mohammad Miadh Angkad <mangkad.bsdsba2027@aim.edu>
2025-08-31 18:31:15 +0800

    Refactor scale handling in gateup and down inputs

--- python/sglang/srt/layers/moe/ep_moe/layer.py
+++ python/sglang/srt/layers/moe/ep_moe/layer.py
@@ -232,9 +232,7 @@ class EPMoE(FusedMoE):
             (
                 _cast_to_e8m0_with_rounding_up(gateup_input_scale)
                 if deep_gemm_wrapper.DEEPGEMM_SCALE_UE8M0
-                else deep_gemm_wrapper.get_mn_major_tma_aligned_tensor(
-                    gateup_input_scale
-                )
+                else deep_gemm_wrapper.get_mn_major_tma_aligned_tensor(gateup_input_scale)
             ),
         )
         num_groups, m, k = gateup_input_fp8[0].size()
@@ -289,9 +287,7 @@ class EPMoE(FusedMoE):
             (
                 down_input_scale
                 if deep_gemm_wrapper.DEEPGEMM_SCALE_UE8M0
-                else deep_gemm_wrapper.get_mn_major_tma_aligned_tensor(
-                    down_input_scale
-                )
+                else deep_gemm_wrapper.get_mn_major_tma_aligned_tensor(down_input_scale)
             ),
         )
         down_output = torch.empty(
