bd61cc701ba6b3b966b94aadc85d7620592c0c69
Mick <mickjagger19@icloud.com>
2025-09-02 16:50:54 +0100

    lang: support direct video inference

--- python/sglang/lang/backend/runtime_endpoint.py
+++ python/sglang/lang/backend/runtime_endpoint.py
@@ -104,6 +104,7 @@ class RuntimeEndpoint(BaseBackend):
     def commit_lazy_operations(self, s: StreamExecutor):
         data = {"text": s.text_, "sampling_params": {"max_new_tokens": 0}}
         self._add_images(s, data)
+        self._add_videos(s, data)
         res = http_request(
             self.base_url + "/generate",
             json=data,
@@ -112,9 +113,11 @@ class RuntimeEndpoint(BaseBackend):
         )
         self._assert_success(res)
 
+    # not used anywhere?
     def fill_image(self, s: StreamExecutor):
         data = {"text": s.text_, "sampling_params": {"max_new_tokens": 0}}
         self._add_images(s, data)
+
         res = http_request(
             self.base_url + "/generate",
             json=data,
@@ -181,6 +184,7 @@ class RuntimeEndpoint(BaseBackend):
                 data[item] = value
 
         self._add_images(s, data)
+        self._add_videos(s, data)
 
         res = http_request(
             self.base_url + "/generate",
@@ -222,6 +226,8 @@ class RuntimeEndpoint(BaseBackend):
 
         data["stream"] = True
         self._add_images(s, data)
+        self._add_videos(s, data)
+
 
         res = http_request(
             self.base_url + "/generate",
@@ -324,6 +330,8 @@ class RuntimeEndpoint(BaseBackend):
 
     def _generate_http_request(self, s: StreamExecutor, data):
         self._add_images(s, data)
+        self._add_videos(s, data)
+
         res = http_request(
             self.base_url + "/generate",
             json=data,
@@ -337,6 +345,11 @@ class RuntimeEndpoint(BaseBackend):
         if s.images_:
             assert len(s.images_) == 1, "Only support one image."
             data["image_data"] = s.images_[0][1]
+
+    def _add_videos(self, s: StreamExecutor, data):
+        if s.videos:
+            assert len(s.videos) == 1, "Only support one video."
+            data["video_data"] = s.videos
 
     def _assert_success(self, res):
         if res.status_code != 200:
