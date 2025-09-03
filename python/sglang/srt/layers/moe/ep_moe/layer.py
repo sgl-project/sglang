cce217c567b33d1dc2b51758909b275c662e0617
fzyzcjy <ch271828n@outlook.com>
2025-09-03 10:11:12 +0800

    more

--- python/sglang/srt/layers/moe/ep_moe/layer.py
+++ python/sglang/srt/layers/moe/ep_moe/layer.py
@@ -590,13 +590,15 @@ class DeepEPMoE(EPMoE):
         )
         output_index = torch.empty_like(topk_idx)
 
-        # num_recv_tokens_per_expert_gpu = torch.tensor(
-        #     num_recv_tokens_per_expert,
-        #     dtype=torch.int32,
-        #     pin_memory=True,
-        #     device="cpu",
-        # ).cuda(non_blocking=True)
-        num_recv_tokens_per_expert_gpu = copy_to_gpu_no_ce_wrapped(num_recv_tokens_per_expert)
+        if len(num_recv_tokens_per_expert) == 72:
+            num_recv_tokens_per_expert_gpu = copy_to_gpu_no_ce_wrapped(num_recv_tokens_per_expert)
+        else:
+            num_recv_tokens_per_expert_gpu = torch.tensor(
+                num_recv_tokens_per_expert,
+                dtype=torch.int32,
+                pin_memory=True,
+                device="cpu",
+            ).cuda(non_blocking=True)
         expert_start_loc = torch.empty_like(num_recv_tokens_per_expert_gpu)
 
         ep_scatter(
