e1977bc8f6f0f69d80b3b844080ce40ff4a4e94f
fzyzcjy <ch271828n@outlook.com>
2025-09-03 09:02:44 +0800

    more

--- sgl-kernel/csrc/elementwise/copy.cu
+++ sgl-kernel/csrc/elementwise/copy.cu
@@ -3,17 +3,35 @@ struct InputArray {
     int values[N];
 };
 
-__global__ void copy_to_gpu_kernel(InputArray input_array, const int* output) {
-    TODO;
+template <int N>
+__global__ void copy_to_gpu_kernel(InputArray<T> input_array, const int* output) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx < N) {
+        output[idx] = input_array.values[idx];
+    }
 }
 
-void copy_to_gpu(const std::vector<int>& input, at::Tensor& output) {
-  InputArray input_array;
-  TODO_fill;
+template <int N>
+void copy_to_gpu_impl(const std::vector<int>& input, at::Tensor& output) {
+  TORCH_CHECK(static_cast<int>(input.size()) == N, "input size");
+  TORCH_CHECK(output.numel() == N, "output size");
 
+  InputArray<N> input_array;
+  for (int i = 0; i < N; ++i) input_array.values[i] = input[i];
+
+  // TODO may use multi thread blocks?
   dim3 grid(1);
-  dim3 block(input.size();
+  dim3 block(input.size());
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
   copy_to_gpu_kernel<<<grid, block, 0, stream>>>(input_array, output.data_ptr();
   C10_CUDA_KERNEL_LAUNCH_CHECK();
+}
+
+void copy_to_gpu(const std::vector<int>& input, at::Tensor& output) {
+    const int N = input.size();
+    if (N == 72) {
+        copy_to_gpu_impl<72>(input, output);
+    } else {
+        TORCH_CHECK(false, "unexpected N");
+    }
 }
