d6c7fa5deeef8bd8dbde5126fca7f5a0f1d0c7aa
Even Zhou <even.y.zhou@outlook.com>
2025-09-06 16:10:27 +0800

    fix linting

--- python/sglang/srt/layers/quantization/w4afp8.py
+++ python/sglang/srt/layers/quantization/w4afp8.py
@@ -17,7 +17,7 @@ from sglang.srt.layers.quantization.base_config import (
 from sglang.srt.layers.quantization.fp8 import Fp8LinearMethod
 from sglang.srt.layers.quantization.unquant import UnquantizedLinearMethod
 from sglang.srt.layers.quantization.utils import is_layer_skipped
-from sglang.srt.utils import set_weight_attrs, is_npu
+from sglang.srt.utils import is_npu, set_weight_attrs
 
 _is_npu = is_npu()
 if not _is_npu:
