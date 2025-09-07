8e17a86e94096079da423668feecedd1c8e95700
root <root@hieu-openai-gpu.datacrunch.io>
2025-09-07 01:23:24 +0000

    fix format

--- python/sglang/srt/layers/attention/blackwell_prefill_attention_backend.py
+++ python/sglang/srt/layers/attention/blackwell_prefill_attention_backend.py
@@ -7,29 +7,13 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-# from sglang.srt.configs.model_config import AttentionArch
 from sglang.srt.layers.attention.base_attn_backend import AttentionBackend
-
-# from sglang.srt.managers.schedule_batch import global_server_args_dict
-# from sglang.srt.mem_cache.memory_pool import SWAKVPool
 from sglang.srt.speculative.eagle_utils import EagleDraftInput, EagleVerifyInput
 
 if TYPE_CHECKING:
     from sglang.srt.layers.radix_attention import RadixAttention
     from sglang.srt.model_executor.forward_batch_info import ForwardBatch, ForwardMode
     from sglang.srt.model_executor.model_runner import ModelRunner
-
-
-def _green(x: str) -> str:
-    return f"\033[1;32m{x}\033[0m"
-
-
-def _red(x: str) -> str:
-    return f"\033[1;31m{x}\033[0m"
-
-
-def _yellow(x: str) -> str:
-    return f"\033[1;33m{x}\033[0m"
 
 
 @dataclass
