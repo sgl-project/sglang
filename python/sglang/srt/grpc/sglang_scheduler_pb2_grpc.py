c1f3d10377c13ce410f7be5ebcc1e8edcf35051d
Chang Su <chang.s.su@oracle.com>
2025-09-10 21:24:35 +0000

    Fix import

--- python/sglang/srt/grpc/sglang_scheduler_pb2_grpc.py
+++ python/sglang/srt/grpc/sglang_scheduler_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import sglang_scheduler_pb2 as sglang__scheduler__pb2
+from . import sglang_scheduler_pb2 as sglang__scheduler__pb2
 
 GRPC_GENERATED_VERSION = '1.74.0'
 GRPC_VERSION = grpc.__version__
