cfc459d7a60a9a554419927d146545691351670d
Yineng Zhang <me@zhyncs.com>
2025-09-11 04:10:06 -0700

    upd

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -124,7 +124,7 @@ suites = {
         TestFile("test_data_parallelism.py", 73),
         TestFile("test_dp_attention.py", 277),
         TestFile("test_patch_torch.py", 19),
-        # TestFile("test_release_memory_occupation.py", 127),
+        TestFile("test_release_memory_occupation.py", 127),
         TestFile("hicache/test_hicache_storage_file_backend.py", 400),
         TestFile("hicache/test_hicache_storage_3fs_backend.py", 400),
     ],
