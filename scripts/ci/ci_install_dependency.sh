1007f94cb900dc275e0ba595d2efe626c0c56679
fzyzcjy <ch271828n@outlook.com>
2025-09-11 17:58:50 +0800

    more

--- scripts/ci/ci_install_dependency.sh
+++ scripts/ci/ci_install_dependency.sh
@@ -52,8 +52,12 @@ pwd
 SGL_KERNEL_VERSION=$(grep -Po '(?<=^version = ")[^"]*' sgl-kernel/pyproject.toml)
 echo "SGL_KERNEL_VERSION=${SGL_KERNEL_VERSION}"
 
-if [ "$IS_BLACKWELL" = "1" ]; then
-    $PIP_CMD install https://github.com/sgl-project/whl/releases/download/v${SGL_KERNEL_VERSION}/sgl_kernel-${SGL_KERNEL_VERSION}+cu128-cp310-abi3-manylinux2014_x86_64.whl --force-reinstall $PIP_INSTALL_SUFFIX
+if [ "$CUSTOM_BUILD_SGL_KERNEL" = "true" ]; then
+    $PIP_CMD install TODO --force-reinstall $PIP_INSTALL_SUFFIX
+else
+    if [ "$IS_BLACKWELL" = "1" ]; then
+        $PIP_CMD install https://github.com/sgl-project/whl/releases/download/v${SGL_KERNEL_VERSION}/sgl_kernel-${SGL_KERNEL_VERSION}+cu128-cp310-abi3-manylinux2014_x86_64.whl --force-reinstall $PIP_INSTALL_SUFFIX
+    fi
 fi
 
 # Show current packages
