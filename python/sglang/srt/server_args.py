1d8ca38199cb72bcf292312571643d87d29d86b4
晟海 <huangtingwei.htw@antgroup.com>
2025-09-11 10:16:08 +0800

    check in server args

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -710,6 +710,13 @@ class ServerArgs:
             self.hicache_io_backend = "kernel"
             self.hicache_mem_layout = "page_first"
 
+        if self.hicache_mem_layout == "page_first_direct":
+            if self.hicache_io_backend != "direct":
+                self.hicache_io_backend = "direct"
+                logger.warning(
+                    "Page first direct layout only support direct io backend"
+                )
+
         # Speculative Decoding
         if self.speculative_algorithm == "NEXTN":
             # NEXTN shares the same implementation of EAGLE
