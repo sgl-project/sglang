216090494c4674bf5ca8483c5a58dea87ddd5aef
yizhang2077 <1109276519@qq.com>
2025-09-12 04:19:25 +0000

    add qwen3-next ut

--- test/srt/models/test_qwen_models.py
+++ test/srt/models/test_qwen_models.py
@@ -73,5 +73,39 @@ class TestQwen2FP8(CustomTestCase):
         self.assertGreater(metrics["accuracy"], 0.78)
 
 
+class TestQwen3Next(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = "Qwen/Qwen3-Next-80B-A3B-Instruct"
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--tp-size",
+                "4",
+            ],
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            num_shots=5,
+            data_path=None,
+            num_questions=200,
+            max_new_tokens=512,
+            parallel=128,
+            host="http://127.0.0.1",
+            port=int(self.base_url.split(":")[-1]),
+        )
+        metrics = run_eval(args)
+        print(f"{metrics=}")
+        self.assertGreater(metrics["accuracy"], 0.93)
+
+
 if __name__ == "__main__":
     unittest.main()
