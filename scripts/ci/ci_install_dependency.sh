b8749684666827393d4fa7cafe00dd505e9c9c60
fzyzcjy <ch271828n@outlook.com>
2025-09-13 08:09:01 +0800

    debug flag

--- scripts/ci/ci_install_dependency.sh
+++ scripts/ci/ci_install_dependency.sh
@@ -43,7 +43,7 @@ else
 fi
 
 # Install the main package
-$PIP_CMD install -e "python[dev]" --extra-index-url https://download.pytorch.org/whl/${CU_VERSION} $PIP_INSTALL_SUFFIX
+$PIP_CMD install -vvv -e "python[dev]" --extra-index-url https://download.pytorch.org/whl/${CU_VERSION} $PIP_INSTALL_SUFFIX
 
 # Install router for pd-disagg test
 SGLANG_ROUTER_BUILD_NO_RUST=1 $PIP_CMD install -e "sgl-router" $PIP_INSTALL_SUFFIX
