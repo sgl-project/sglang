c321305e26877931d559e4a0aed6154a159f79bd
Cheng Wan <cwan@x.ai>
2025-09-14 22:00:09 +0000

    update threshold

--- test/srt/quant/test_w8a8_quantization.py
+++ test/srt/quant/test_w8a8_quantization.py
@@ -90,14 +90,14 @@ class TestW8A8Int8(BaseW8A8Test):
     model = "neuralmagic/Meta-Llama-3-8B-Instruct-quantized.w8a8"
     quantization = "w8a8_int8"
     gsm8k_accuracy_threshold = 0.69
-    throughput_threshold = 140
+    throughput_threshold = 230
 
 
 class TestW8A8Fp8(BaseW8A8Test):
     model = "neuralmagic/Meta-Llama-3.1-8B-Instruct-FP8-dynamic"
     quantization = "w8a8_fp8"
     gsm8k_accuracy_threshold = 0.69
-    throughput_threshold = 140
+    throughput_threshold = 230
 
 
 class TestW8A8Fp8MoE(BaseW8A8Test):
