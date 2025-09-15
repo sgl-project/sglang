43585c2716e399960ba3e473af7f0ebbd4e3249d
fzyzcjy <ch271828n@outlook.com>
2025-09-15 16:52:09 +0800

    revert

--- python/sglang/srt/layers/moe/ep_moe/layer.py
+++ python/sglang/srt/layers/moe/ep_moe/layer.py
@@ -29,7 +29,7 @@ from sglang.srt.layers.quantization.base_config import QuantizationConfig
 from sglang.srt.layers.quantization.fp8 import Fp8Config
 from sglang.srt.layers.quantization.fp8_kernel import (
     is_fp8_fnuz,
-    sglang_per_token_group_quant_fp8, sglang_per_token_group_quant_8bit,
+    sglang_per_token_group_quant_fp8,
 )
 from sglang.srt.managers.schedule_batch import global_server_args_dict
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch
@@ -714,17 +714,32 @@ class DeepEPMoE(EPMoE):
         dispose_tensor(hidden_states_fp8[0])
 
         # Act
+        down_input = torch.empty(
+            (
+                gateup_output.shape[0],
+                gateup_output.shape[1],
+                gateup_output.shape[2] // 2,
+            ),
+            device=gateup_output.device,
+            dtype=self.fp8_dtype,
+        )
         scale_block_size = 128
-        down_input, down_input_scale = sglang_per_token_group_quant_8bit(
-            x=gateup_output,
-            dst_dtype=self.fp8_dtype,
-            group_size=scale_block_size,
-            masked_m=masked_m,
-            column_major_scales=True,
-            scale_tma_aligned=True,
+        down_input_scale = torch.empty(
+            (
+                gateup_output.shape[0],
+                gateup_output.shape[1],
+                gateup_output.shape[2] // 2 // scale_block_size,
+            ),
+            device=gateup_output.device,
+            dtype=torch.float32,
+        )
+        silu_and_mul_masked_post_quant_fwd(
+            gateup_output,
+            down_input,
+            down_input_scale,
+            scale_block_size,
+            masked_m,
             scale_ue8m0=deep_gemm_wrapper.DEEPGEMM_SCALE_UE8M0,
-            fuse_silu_and_mul=True,
-            enable_v2=True,
         )
         del gateup_output
 
