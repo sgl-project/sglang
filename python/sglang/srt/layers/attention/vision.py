5f6463b8589c70e335793c172875a9e364c09ac7
yoctta <zhq2015@mail.ustc.edu.cn>
2025-09-16 02:23:50 +0800

    Update vision.py
    fixing "SingletonCache" has no attribute "cpu" when serving InternVL-3.5 with --enable-multimodal

--- python/sglang/srt/layers/attention/vision.py
+++ python/sglang/srt/layers/attention/vision.py
@@ -141,7 +141,7 @@ class VisionSdpaAttention(nn.Module):
     def generate_patch_attention_mask(
         self,
         s: int,
-        cu_seqlens: Optional[torch.Tensor],
+        cu_seqlens: Optional[Union[SingletonCache, torch.Tensor]],
         flatten_batch: bool = False,
     ) -> Optional[torch.Tensor]:
         r"""
@@ -155,7 +155,10 @@ class VisionSdpaAttention(nn.Module):
         """
         if cu_seqlens is None:
             return None
-
+        if isinstance(cu_seqlens, SingletonCache):
+            if cu_seqlens.empty():
+                return None
+            cu_seqlens = cu_seqlens.get_data()
         cu_seqlens_tuple = tuple(cu_seqlens.cpu().tolist())
 
         return self._generate_mask_cache(s, flatten_batch, cu_seqlens_tuple)
