c5a764dd2890e1dfb240adddadee487a06277b0d
fzyzcjy <ch271828n@outlook.com>
2025-09-17 08:23:53 +0800

    fmt

--- python/sglang/srt/slow_rank_detector.py
+++ python/sglang/srt/slow_rank_detector.py
@@ -1,10 +1,9 @@
-import triton
 import logging
+from typing import Any, Dict, List
 
 import torch
-from typing import Any, List, Dict
-
 import torch.distributed as dist
+import triton
 
 logger = logging.getLogger(__name__)
 
@@ -13,7 +12,9 @@ def execute():
     if dist.get_rank() == 0:
         logger.info(f"[slow_rank_detector] Start benchmarking...")
 
-    local_metrics = {bench_name: _compute_local_metric(bench_name) for bench_name in _BENCH_NAMES}
+    local_metrics = {
+        bench_name: _compute_local_metric(bench_name) for bench_name in _BENCH_NAMES
+    }
 
     all_metrics = [None for _ in range(dist.get_world_size())]
     dist.gather_object(local_metrics, all_metrics if dist.get_rank() == 0 else None)
@@ -24,8 +25,8 @@ def execute():
 
 class _GemmExecutor:
     def __init__(self):
-        self.lhs = torch.randn((8192, 8192), dtype=torch.bfloat16, device='cuda')
-        self.rhs = torch.randn((8192, 8192), dtype=torch.bfloat16, device='cuda')
+        self.lhs = torch.randn((8192, 8192), dtype=torch.bfloat16, device="cuda")
+        self.rhs = torch.randn((8192, 8192), dtype=torch.bfloat16, device="cuda")
 
     def __call__(self):
         self.lhs @ self.rhs
@@ -33,7 +34,9 @@ class _GemmExecutor:
 
 class _ElementwiseExecutor:
     def __init__(self):
-        self.value = torch.randint(0, 10000, (128 * 1024 ** 2,), dtype=torch.int32, device='cuda')
+        self.value = torch.randint(
+            0, 10000, (128 * 1024**2,), dtype=torch.int32, device="cuda"
+        )
 
     def __call__(self):
         self.value += 1
@@ -59,6 +62,10 @@ def _analyze_metrics(all_metrics: List[Dict[str, Any]]):
         speed_of_rank = 1 / time_of_rank
         rel_speed_of_rank = speed_of_rank / speed_of_rank.max()
         slowest_rel_speed = rel_speed_of_rank.min().item()
-        logger.info(f"[slow_rank_detector] {bench_name=} {slowest_rel_speed=} {rel_speed_of_rank=} {time_of_rank=}")
+        logger.info(
+            f"[slow_rank_detector] {bench_name=} {slowest_rel_speed=} {rel_speed_of_rank=} {time_of_rank=}"
+        )
         if slowest_rel_speed < 0.9:
-            logger.warning("[slow_rank_detector] Some ranks are too slow compared with others")
+            logger.warning(
+                "[slow_rank_detector] Some ranks are too slow compared with others"
+            )
