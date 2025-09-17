9f3cf28f2002b1f431f05e9ceb9f394857fa7e75
有禾 <zhangdonghao.zdh@antgroup.com>
2025-09-17 15:43:32 +0800

    add moe config

--- benchmark/kernels/fused_moe_triton/tuning_fused_moe_triton.py
+++ benchmark/kernels/fused_moe_triton/tuning_fused_moe_triton.py
@@ -13,8 +13,8 @@ from ray.experimental.tqdm_ray import tqdm
 from transformers import AutoConfig
 
 from sglang.srt.layers.moe.fused_moe_triton import override_config
-from sglang.srt.layers.moe.fused_moe_triton.fused_moe import (
-    fused_moe,
+from sglang.srt.layers.moe.fused_moe_triton.fused_moe import fused_moe
+from sglang.srt.layers.moe.fused_moe_triton.fused_moe_triton_config import (
     get_config_dtype_str,
     get_config_file_name,
     get_default_config,
@@ -443,6 +443,15 @@ def main(args: argparse.Namespace):
         shard_intermediate_size = 2 * intermediate_size // args.tp_size
     elif config.architectures[0] in ["Glm4MoeForCausalLM"]:
         E = config.n_routed_experts
+        topk = config.num_experts_per_tok
+        intermediate_size = config.moe_intermediate_size
+        shard_intermediate_size = 2 * intermediate_size // args.tp_size
+    elif config.architectures[0] in [
+        "BailingMoELinearForCausalLM",
+        "BailingMoeLinearForCausalLM",
+        "BailingMoeLinearV2ForCausalLM",
+    ]:
+        E = config.num_experts
         topk = config.num_experts_per_tok
         intermediate_size = config.moe_intermediate_size
         shard_intermediate_size = 2 * intermediate_size // args.tp_size
