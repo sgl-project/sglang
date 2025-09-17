117cb491f5506fe0d7aa4e58b80f698e48aae4c6
Philo Lu <lulie@linux.alibaba.com>
2025-09-17 18:22:53 +0800

    afd: Add server args
    Add afd server args and checks for current un-supported options.
    
    A new ipc_name is initialized for AFD, using afd_ffn_addr. This address is
    used for A-F scheduler synchronization.
    
    Signed-off-by: Philo Lu <lulie@linux.alibaba.com>
    Co-authored-by: D. Wythe <alibuda@linux.alibaba.com>

--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -112,6 +112,9 @@ GLOBAL_SERVER_ARGS_KEYS = [
     "enable_custom_logit_processor",
     "disaggregation_mode",
     "enable_deterministic_inference",
+    "afd_role",
+    "afd_ffn_addr",
+    "afd_transfer_backend",
 ]
 
 # Put some global args for easy access
