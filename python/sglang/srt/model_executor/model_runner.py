94255a2c1cae1a8e11603c94377c2da4ba77f295
ckvermaAI <ckverma@habana.ai>
2025-09-18 11:16:12 +0300

    Add support for XPUGraphRunner

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -101,6 +101,7 @@ from sglang.srt.model_executor.cpu_graph_runner import CPUGraphRunner
 from sglang.srt.model_executor.cuda_graph_runner import CudaGraphRunner
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch, PPProxyTensors
 from sglang.srt.model_executor.npu_graph_runner import NPUGraphRunner
+from sglang.srt.model_executor.xpu_graph_runner import XPUGraphRunner
 from sglang.srt.model_loader import get_model
 from sglang.srt.model_loader.loader import DefaultModelLoader, get_model_loader
 from sglang.srt.model_loader.utils import set_default_torch_dtype
@@ -396,7 +397,7 @@ class ModelRunner:
             self.init_cublas()
             self.init_attention_backend()
             self.init_device_graphs()
-        elif self.device in ["npu", "cpu"]:
+        elif self.device in ["npu", "cpu", "xpu"]:
             self.init_attention_backend()
             self.init_device_graphs()
         else:
@@ -1906,6 +1907,7 @@ class ModelRunner:
             {
                 "cpu": CPUGraphRunner,
                 "npu": NPUGraphRunner,
+                "xpu": XPUGraphRunner,
             },
         )
         self.graph_runner = graph_runners[self.device](self)
