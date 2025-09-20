4f58baad7bc78e8c77d129a2ef6eb64136aa81ba
Yineng Zhang <me@zhyncs.com>
2025-09-20 01:47:01 -0700

    fix: remove awq_dequantize deps (#10686)

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -164,7 +164,7 @@ elif _is_hip:
         awq_dequantize_triton as awq_dequantize,
     )
 else:
-    from vllm._custom_ops import awq_dequantize
+    pass
 
 if _is_hip:
     from sglang.srt.layers.attention.triton_ops.rocm_mla_decode_rope import (
