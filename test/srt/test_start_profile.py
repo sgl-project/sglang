a32ea973111ecb442899b5fb7340b44f5f836bb2
shubham singhal <ssinghal@habana.ai>
2025-09-24 08:54:53 +0530

    Update test_start_profile.py

--- test/srt/test_start_profile.py
+++ test/srt/test_start_profile.py
@@ -9,8 +9,8 @@ import unittest
 
 import requests
 
-from sglang.srt.utils import kill_process_tree
 from sglang.srt.environ import envs
+from sglang.srt.utils import kill_process_tree
 from sglang.test.test_utils import (
     DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
