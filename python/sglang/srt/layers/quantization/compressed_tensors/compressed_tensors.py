00438afbe8ca501019a28bfeab441495efea58b5
岱初 <xulianhao.xlh@antgroup.com>
2025-09-24 14:53:23 +0800

    clean code

--- python/sglang/srt/layers/quantization/compressed_tensors/compressed_tensors.py
+++ python/sglang/srt/layers/quantization/compressed_tensors/compressed_tensors.py
@@ -40,11 +40,13 @@ from sglang.srt.layers.quantization.compressed_tensors.utils import (
 from sglang.srt.layers.quantization.unquant import UnquantizedLinearMethod
 
 try:
+    from vllm.model_executor.layers.quantization.compressed_tensors.schemes import (
+        CompressedTensorsW4A16Fp4,
+    )
     from vllm.model_executor.layers.quantization.compressed_tensors.schemes.compressed_tensors_wNa16 import (
         WNA16_SUPPORTED_BITS,
         CompressedTensorsWNA16,
     )
-    from vllm.model_executor.layers.quantization.compressed_tensors.schemes import CompressedTensorsW4A16Fp4
 
     VLLM_AVAILABLE = True
 except ImportError:
@@ -345,19 +347,27 @@ class CompressedTensorsConfig(QuantizationConfig):
         # All conditions satisfied.
         return True
 
-    def _is_fp4a16_nvfp4(self, weight_quant: QuantizationArgs,
-                         input_quant: QuantizationArgs):
+    def _is_fp4a16_nvfp4(
+        self, weight_quant: QuantizationArgs, input_quant: QuantizationArgs
+    ):
         is_weight_only = weight_quant is not None and input_quant is None
         is_tensor_group_quant = (
-            weight_quant.strategy == QuantizationStrategy.TENSOR_GROUP.value)
+            weight_quant.strategy == QuantizationStrategy.TENSOR_GROUP.value
+        )
         is_symmetric = weight_quant.symmetric
 
         is_group_size_16 = weight_quant.group_size == 16
         is_float_type = weight_quant.type == QuantizationType.FLOAT
         is_4_bits = weight_quant.num_bits == 4
 
-        return (is_weight_only and is_tensor_group_quant and is_float_type
-                and is_4_bits and is_group_size_16 and is_symmetric)
+        return (
+            is_weight_only
+            and is_tensor_group_quant
+            and is_float_type
+            and is_4_bits
+            and is_group_size_16
+            and is_symmetric
+        )
 
     def _is_wNa16_group_channel(
         self, weight_quant: BaseModel, input_quant: BaseModel
