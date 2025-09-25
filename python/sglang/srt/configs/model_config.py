9866dfb816db3942ee23cdfdb3e45cb980025f39
Zhiyu Cheng <zhiyuc@nvidia.com>
2025-09-25 22:48:21 +0000

    resolve conflict
    Signed-off-by: Zhiyu Cheng <zhiyuc@nvidia.com>

--- python/sglang/srt/configs/model_config.py
+++ python/sglang/srt/configs/model_config.py
@@ -91,6 +91,7 @@ class ModelConfig:
             remote_instance_weight_loader_send_weights_group_ports
         )
         self.modelopt_quant = modelopt_quant
+        self.impl = impl
 
         # Get hf config
         self._maybe_pull_model_tokenizer_from_remote()
