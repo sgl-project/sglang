f890bae7cb0f0c94146e450156e0433537f2df5d
Maksim <makcum888e@mail.ru>
2025-09-25 11:54:43 +0300

    use implementation from base class

--- python/sglang/srt/layers/rotary_embedding.py
+++ python/sglang/srt/layers/rotary_embedding.py
@@ -1087,30 +1087,21 @@ class MRotaryEmbedding(RotaryEmbedding):
         key = torch.cat((key_rot, key_pass), dim=-1).reshape(key_shape)
         return query, key
 
-    def forward_npu(
+    def forward_cpu(
         self,
         positions: torch.Tensor,
         query: torch.Tensor,
         key: torch.Tensor,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        if query.shape[1] > 4096:
-            return self.forward_native(positions, query, key)
-        rotary_mode = "half"
-        if self.is_neox_style:
-            rotary_mode = "half"
-        else:
-            rotary_mode = "interleave"
-        mrope_section = [0, 0, 0]
-        query_out, key_out = torch_npu.npu_mrope(
-            positions,
-            query,
-            key,
-            self.cos_sin_cache,
-            self.head_size,
-            mrope_section=mrope_section,
-            rotary_mode=rotary_mode,
-        )
-        return query_out, key_out
+        return self.forward_native(positions, query, key)
+
+    def forward_cuda(
+        self,
+        positions: torch.Tensor,
+        query: torch.Tensor,
+        key: torch.Tensor,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        return self.forward_native(positions, query, key)
 
     # Copied from https://github.com/huggingface/transformers/blob/c8e0e603de9b3d49161a15fe6e8ea84badfb5d02/src/transformers/models/qwen2_vl/modeling_qwen2_vl.py#L1439
     @staticmethod
