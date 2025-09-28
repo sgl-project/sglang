7a069fbb10be22b1da152bb59a1a7317068015f4
FlamingoPg <1106310035@qq.com>
2025-09-28 10:22:24 +0000

    fix kerenl compile

--- sgl-kernel/csrc/quantization/gguf/gguf_kernel.cu
+++ sgl-kernel/csrc/quantization/gguf/gguf_kernel.cu
@@ -1,3 +1,5 @@
+// Adatped from
+// https://github.com/vllm-project/vllm/blob/755ed7b05be4743237d3339c4ff8c22bcaae04f4/csrc/quantization/gguf/gguf_kernel.cu
 #include <c10/cuda/CUDAGuard.h>
 #include <cuda_fp16.h>
 #include <cuda_runtime.h>
