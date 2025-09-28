da471b27db4076064b5943eed64161d2b73f7d28
晟海 <huangtingwei.htw@antgroup.com>
2025-09-28 16:49:54 +0800

    Prevent assert from causing sglang to fail to start & fix ci

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -931,10 +931,15 @@ class ServerArgs:
 
     def _handle_hicache(self):
         if self.hicache_storage_backend == "mooncake":
-            assert self.hicache_mem_layout in [
-                "page_first",
-                "page_first_direct",
-            ], f"Mooncake storage backend does not support {self.hicache_mem_layout} layout"
+            if self.hicache_mem_layout == "layer_first":
+                if self.hicache_io_backend == "direct":
+                    self.hicache_mem_layout = "page_first_direct"
+                elif self.hicache_io_backend == "kernel":
+                    self.hicache_mem_layout = "page_first"
+                logger.warning(
+                    f"Mooncake storage backend does not support layer_first layout, "
+                    f"switching to {self.hicache_mem_layout} layout for {self.hicache_io_backend} io backend"
+                )
 
         if self.hicache_mem_layout == "page_first_direct":
             if self.hicache_io_backend != "direct":
