44732a99fb2b6b929e846f8ba8fbaa5aa832f7b3
Zhengda Qin <zhengdqin@gmail.com>
2025-09-28 14:48:51 +0800

    ci bug fix

--- python/sglang/srt/model_loader/loader.py
+++ python/sglang/srt/model_loader/loader.py
@@ -511,7 +511,7 @@ class DefaultModelLoader(BaseModelLoader):
                 # parameters onto device for processing and back off after.
                 with device_loading_context(module, target_device):
                     quant_method.process_weights_after_loading(module)
-                if is_npu:
+                if _is_npu:
                     torch.npu.empty_cache()
 
 
