4f7d605652a8f43426770d2c353a13e9b86f73ad
ilyasch2 <ilyas.chahed.2001@gmail.com>
2025-09-29 08:11:02 +0000

    added more tests for TP4 and no gated RMS

--- test/srt/models/test_falcon_h1_models.py
+++ test/srt/models/test_falcon_h1_models.py
@@ -11,7 +11,7 @@ from sglang.test.test_utils import (
 )
 
 
-class TestFalconH1Next(CustomTestCase):
+class TestFalconH1(CustomTestCase):
     @classmethod
     def setUpClass(cls):
         cls.model = "tiiuae/Falcon-H1-0.5B-Instruct"
@@ -43,3 +43,107 @@ class TestFalconH1Next(CustomTestCase):
         metrics = run_eval(args)
         print(f"{metrics=}")
         self.assertGreater(metrics["accuracy"], 0.75)
+
+
+class TestFalconH1TP4(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = "tiiuae/Falcon-H1-0.5B-Instruct"
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--tensor-parallel-size",
+                "4",
+            ],
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            num_shots=5,
+            data_path=None,
+            num_questions=200,
+            max_new_tokens=512,
+            parallel=128,
+            host="http://127.0.0.1",
+            port=int(self.base_url.split(":")[-1]),
+        )
+        metrics = run_eval(args)
+        print(f"{metrics=}")
+        self.assertGreater(metrics["accuracy"], 0.75)
+
+
+class TestFalconH1NoGatedRMS(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = "tiiuae/Falcon-H1-1.5B-Instruct"
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--tensor-parallel-size",
+                "1",
+            ],
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            num_shots=5,
+            data_path=None,
+            num_questions=200,
+            max_new_tokens=512,
+            parallel=128,
+            host="http://127.0.0.1",
+            port=int(self.base_url.split(":")[-1]),
+        )
+        metrics = run_eval(args)
+        print(f"{metrics=}")
+        self.assertGreater(metrics["accuracy"], 0.80)
+
+
+
+class TestFalconH1NoGatedTP4(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = "tiiuae/Falcon-H1-1.5B-Instruct"
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--tensor-parallel-size",
+                "4",
+            ],
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            num_shots=5,
+            data_path=None,
+            num_questions=200,
+            max_new_tokens=512,
+            parallel=128,
+            host="http://127.0.0.1",
+            port=int(self.base_url.split(":")[-1]),
+        )
+        metrics = run_eval(args)
+        print(f"{metrics=}")
+        self.assertGreater(metrics["accuracy"], 0.80)
+
