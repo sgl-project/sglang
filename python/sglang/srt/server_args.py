a9beec689d3412be4b7343853de3b34470f15b34
xiafang.zhanxia <zhanxia.zx@antgroup.com>
2025-09-29 19:22:47 +0800

    fix bug

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -296,7 +296,15 @@ class ServerArgs:
     speculative_lookahead_match_type: Literal["BFS", "PROB"] = "BFS"
     speculative_lookahead_branch_length: int = 18
     speculative_lookahead_capacity: int = 10 * 1000 * 1000
-
+    
+    # For ngram only
+    speculative_ngram_min_match_window_size: int = 1
+    speculative_ngram_max_match_window_size: int = 12
+    speculative_ngram_min_bfs_breadth: int = 1
+    speculative_ngram_max_bfs_breadth: int = 10
+    speculative_ngram_match_type: Literal["BFS", "PROB"] = "BFS"
+    speculative_ngram_branch_length: int = 18
+    speculative_ngram_capacity: int = 10 * 1000 * 1000
     # Expert parallelism
     ep_size: int = 1
     moe_a2a_backend: Literal["none", "deepep"] = "none"
