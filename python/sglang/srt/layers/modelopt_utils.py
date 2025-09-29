c21b4806ddee276ec43a4fdd3dd7a7f3b0ad58a2
Zhiyu Cheng <zhiyuc@nvidia.com>
2025-09-29 23:42:25 +0000

    resolve conflict
    Signed-off-by: Zhiyu Cheng <zhiyuc@nvidia.com>

---
+++ python/sglang/srt/layers/modelopt_utils.py
@@ -0,0 +1,11 @@
+"""
+ModelOpt related constants
+"""
+
+QUANT_CFG_CHOICES = {
+    "fp8": "FP8_DEFAULT_CFG",
+    "int4_awq": "INT4_AWQ_CFG",
+    "w4a8_awq": "W4A8_AWQ_BETA_CFG",
+    "nvfp4": "NVFP4_DEFAULT_CFG",
+    "nvfp4_awq": "NVFP4_AWQ_LITE_CFG",
+}
