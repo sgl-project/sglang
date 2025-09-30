4ac6dddf7d5c9b43c458964ef2f4f779bd735158
Chang Su <chang.s.su@oracle.com>
2025-09-30 13:06:25 -0700

    Remove unsued import in proto

--- python/sglang/srt/grpc/sglang_scheduler_pb2.pyi
+++ python/sglang/srt/grpc/sglang_scheduler_pb2.pyi
@@ -2,7 +2,6 @@ import datetime
 
 from google.protobuf import timestamp_pb2 as _timestamp_pb2
 from google.protobuf import struct_pb2 as _struct_pb2
-from google.protobuf import wrappers_pb2 as _wrappers_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
