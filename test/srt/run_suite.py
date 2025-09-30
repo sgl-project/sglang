3f74c1dbc6aba51ccb0ac7726c5e4433142d6665
Shangming Cai <csmthu@gmail.com>
2025-09-30 22:31:30 +0800

    Update nixl estimated time

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -148,7 +148,7 @@ suites = {
         TestFile("test_disaggregation.py", 499),
         TestFile("test_disaggregation_different_tp.py", 155),
         TestFile("test_disaggregation_pp.py", 60),
-        TestFile("test_disaggregation_nixl.py", 35 * 60),
+        TestFile("test_disaggregation_nixl.py", 1174),
         TestFile("test_full_deepseek_v3.py", 333),
     ],
     "per-commit-4-gpu-b200": [
