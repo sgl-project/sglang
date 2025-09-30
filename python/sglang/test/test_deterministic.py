dc725e2e315a9514d2306fa64602ee3b8b4e8705
Alex Chi Z <iskyzh@gmail.com>
2025-09-30 04:50:45 +0000

    fix sampling_seed cannot be None
    Signed-off-by: Alex Chi Z <iskyzh@gmail.com>

--- python/sglang/test/test_deterministic.py
+++ python/sglang/test/test_deterministic.py
@@ -99,11 +99,14 @@ def send_single(
             "max_new_tokens": args.max_new_tokens,
             "frequency_penalty": args.frequency_penalty,
             "presence_penalty": args.presence_penalty,
-            "sampling_seed": args.sampling_seed,
         },
         "return_logprob": args.return_logprob,
         "stream": args.stream,
     }
+
+    if args.sampling_seed is not None:
+        # sglang server cannot parse None value for sampling_seed
+        json_data["sampling_params"]["sampling_seed"] = args.sampling_seed
 
     if profile:
         run_profile(
@@ -148,11 +151,13 @@ def send_mixed(args, batch_size: int):
             "max_new_tokens": args.max_new_tokens,
             "frequency_penalty": args.frequency_penalty,
             "presence_penalty": args.presence_penalty,
-            "sampling_seed": args.sampling_seed,
         },
         "return_logprob": args.return_logprob,
         "stream": args.stream,
     }
+
+    if args.sampling_seed is not None:
+        json_data["sampling_params"]["sampling_seed"] = args.sampling_seed
 
     response = requests.post(
         f"http://{args.host}:{args.port}/generate",
@@ -195,11 +200,13 @@ def send_prefix(args, batch_size: int, prompts: List[str]):
             "max_new_tokens": args.max_new_tokens,
             "frequency_penalty": args.frequency_penalty,
             "presence_penalty": args.presence_penalty,
-            "sampling_seed": args.sampling_seed,
         },
         "return_logprob": args.return_logprob,
         "stream": args.stream,
     }
+
+    if args.sampling_seed is not None:
+        json_data["sampling_params"]["sampling_seed"] = args.sampling_seed
 
     response = requests.post(
         f"http://{args.host}:{args.port}/generate",
