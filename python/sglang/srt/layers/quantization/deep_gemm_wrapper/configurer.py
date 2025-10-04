5c850615291124165436f8a5a3da4759f9b61830
Baizhou Zhang <sobereddiezhang@gmail.com>
2025-10-04 11:32:36 -0700

    update

--- python/sglang/srt/layers/quantization/deep_gemm_wrapper/configurer.py
+++ python/sglang/srt/layers/quantization/deep_gemm_wrapper/configurer.py
@@ -3,6 +3,7 @@ import logging
 import torch
 
 from sglang.srt.utils import get_bool_env_var, get_device_sm, is_blackwell
+from sglang.utils import is_in_ci
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +12,9 @@ def _compute_enable_deep_gemm():
     sm_version = get_device_sm()
     if sm_version < 90:
         return False
-    if sm_version == 90 and torch.version.cuda is not None:
+    if sm_version == 90 and torch.version.cuda is not None and is_in_ci():
+        # Temporarily disable DeepGEMM for non-12.8/12.9 CUDA versions in CI
+        # Should be removed after this issue if fixed
         if torch.version.cuda != "12.8" and torch.version.cuda != "12.9":
             return False
     try:
