d1aef971793b71c36c52f169ab39ee98beb62915
sunxxuns <126995791+sunxxuns@users.noreply.github.com>
2025-10-05 19:03:53 -0400

    [Fix AMD CI] VRAM cleanup  (#11174)
    Co-authored-by: root <root@smci350-zts-gtu-e17-15.zts-gtu.dcgpu>

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -2,6 +2,7 @@ import argparse
 import glob
 from dataclasses import dataclass
 
+from sglang.srt.utils import is_hip
 from sglang.test.test_utils import run_unittest_files
 
 
@@ -368,7 +369,7 @@ if __name__ == "__main__":
     arg_parser.add_argument(
         "--timeout-per-file",
         type=int,
-        default=1200,
+        default=1500 if is_hip() else 1200,
         help="The time limit for running one file in seconds.",
     )
     arg_parser.add_argument(
