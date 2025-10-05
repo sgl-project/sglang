e2937bb827f7129d6b86489c7c81406d566cb39c
Netanel Haber <58652339+netanel-haber@users.noreply.github.com>
2025-10-05 08:45:10 +0000

    fix circular imports

--- python/sglang/srt/configs/qwen3_next.py
+++ python/sglang/srt/configs/qwen3_next.py
@@ -16,7 +16,6 @@
 
 import enum
 
-import numpy as np
 from transformers.configuration_utils import PretrainedConfig
 from transformers.modeling_rope_utils import rope_config_validation
 from transformers.utils import logging
