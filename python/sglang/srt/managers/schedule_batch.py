f35564e52a57881f2f676bb4dad187e44ec4a707
Lianmin Zheng <lianminzheng@gmail.com>
2025-10-06 15:50:56 -0700

    Rename max_micro_batch_size -> pp_max_micro_batch_size (#11279)

--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -97,7 +97,7 @@ GLOBAL_SERVER_ARGS_KEYS = [
     "ep_num_redundant_experts",
     "enable_nan_detection",
     "flashinfer_mla_disable_ragged",
-    "max_micro_batch_size",
+    "pp_max_micro_batch_size",
     "disable_shared_experts_fusion",
     "sampling_backend",
     "speculative_accept_threshold_single",
