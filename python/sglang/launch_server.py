1cf346d116c19b76164c96bd1bb6b589700ec503
Lianmin Zheng <lianminzheng@gmail.com>
2025-10-06 10:09:17 -0700

    Remove env var warnings for release (#11262)

--- python/sglang/launch_server.py
+++ python/sglang/launch_server.py
@@ -7,22 +7,8 @@ from sglang.srt.entrypoints.http_server import launch_server
 from sglang.srt.server_args import prepare_server_args
 from sglang.srt.utils import kill_process_tree
 
-MOVE_ENVS_WARN = """
-########################################################################
-# For contributors and developers:                                    #
-# Please move environment variable definitions to sglang.srt.environ  #
-# using the following pattern:                                        #
-#     SGLANG_XXX = EnvBool(False)                                     #
-#                                                                     #
-########################################################################
-"""
-
 if __name__ == "__main__":
     server_args = prepare_server_args(sys.argv[1:])
-
-    from sglang.srt.server_args import print_deprecated_warning
-
-    print_deprecated_warning(MOVE_ENVS_WARN)
 
     try:
         launch_server(server_args)
