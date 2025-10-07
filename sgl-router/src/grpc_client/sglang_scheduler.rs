3d5671cf0c1d4535020c735e107de497ecf9bb8a
Simo Lin <simo.lin@oracle.com>
2025-10-07 10:04:04 -0700

    [router] fix grpc connect conversion

--- sgl-router/src/grpc_client/sglang_scheduler.rs
+++ sgl-router/src/grpc_client/sglang_scheduler.rs
@@ -36,6 +36,14 @@ impl SglangSchedulerClient {
 
         let channel = Channel::from_shared(http_endpoint)?
             .timeout(Duration::from_secs(30))
+            .http2_keep_alive_interval(Duration::from_secs(30))
+            .keep_alive_timeout(Duration::from_secs(10))
+            .keep_alive_while_idle(true)
+            .tcp_keepalive(Some(Duration::from_secs(60)))
+            .tcp_nodelay(true)
+            .http2_adaptive_window(true)
+            .initial_stream_window_size(Some(16 * 1024 * 1024))  // 16MB
+            .initial_connection_window_size(Some(32 * 1024 * 1024))  // 32MB
             .connect()
             .await?;
 
