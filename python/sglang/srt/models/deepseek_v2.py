a2c34a51dac8c303f9eb0467dd191b9010ff0767
Liu-congo <1502632128@qq.com>
2025-10-09 10:23:48 +0800

    modify the fp8quant method for ds2mla head to adapt to bmm_fp8 for cublas  12.9
    Signed-off-by: Liu-congo <1502632128@qq.com>

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -208,6 +208,14 @@ def add_forward_absorb_core_attention_backend(backend_name):
         FORWARD_ABSORB_CORE_ATTENTION_BACKENDS.append(backend_name)
         logger.info(f"Added {backend_name} to FORWARD_ABSORB_CORE_ATTENTION_BACKENDS.")
 
+def to_float8(x, dtype=torch.float8_e4m3fn):
+    finfo = torch.finfo(dtype)
+    min_val, max_val = x.aminmax()
+    amax = torch.maximum(min_val.abs(), max_val.abs()).clamp(min=1e-12)
+    scale = finfo.max / amax
+    x_scl_sat = (x * scale).clamp(min=finfo.min, max=finfo.max)
+    return x_scl_sat.to(dtype), scale.float().reciprocal()
+
 
 class AttnForwardMethod(IntEnum):
     # Use multi-head attention
@@ -1572,10 +1580,10 @@ class DeepseekV2AttentionMLA(nn.Module):
                     self.w_kc.to(torch.bfloat16) * self.w_scale,
                 )
         elif self.w_kc.dtype == torch.float8_e4m3fn:
-            q_nope_val, q_nope_scale = per_tensor_quant_mla_fp8(
-                q_nope.transpose(0, 1),
-                zero_allocator.allocate(1),
+            q_nope_val, q_nope_scale = to_float8(
+                q_nope.transpose(0, 1)
             )
+            q_nope_val = q_nope_val.contiguous()
             q_nope_out = bmm_fp8(
                 q_nope_val, self.w_kc, q_nope_scale, self.w_scale, torch.bfloat16
             )
@@ -1716,10 +1724,10 @@ class DeepseekV2AttentionMLA(nn.Module):
                 attn_bmm_output = attn_bmm_output.transpose(0, 1).flatten(1, 2)
 
         elif self.w_vc.dtype == torch.float8_e4m3fn:
-            attn_output_val, attn_output_scale = per_tensor_quant_mla_fp8(
-                attn_output.transpose(0, 1),
-                zero_allocator.allocate(1),
+            attn_output_val, attn_output_scale = to_float8(
+                attn_output.transpose(0, 1)
             )
+            attn_output_val = attn_output_val.contiguous()
             attn_bmm_output = bmm_fp8(
                 attn_output_val,
                 self.w_vc,
