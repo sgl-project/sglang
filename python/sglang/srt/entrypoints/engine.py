e1a66ab35c5e01f2bfc64ab50e52f80b15977999
Zhiyu Cheng <zhiyuc@nvidia.com>
2025-10-09 20:09:54 +0000

    fix ci
    Signed-off-by: Zhiyu Cheng <zhiyuc@nvidia.com>

--- python/sglang/srt/entrypoints/engine.py
+++ python/sglang/srt/entrypoints/engine.py
@@ -78,7 +78,6 @@ from sglang.srt.utils import (
     kill_process_tree,
     launch_dummy_health_check_server,
     prepare_model_and_tokenizer,
-    rank0_log,
     set_prometheus_multiproc_dir,
     set_ulimit,
 )
@@ -127,7 +126,7 @@ class Engine(EngineBase):
         self.port_args = PortArgs.init_new(server_args)
         logger.info(f"{server_args=}")
 
-        rank0_log("Launching subprocesses for distributed inference...")
+        logger.info("Launching subprocesses for distributed inference...")
 
         # Launch subprocesses
         tokenizer_manager, template_manager, scheduler_info = _launch_subprocesses(
