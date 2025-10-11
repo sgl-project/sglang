afd3cb5a0165f051c1b8c29e02563f0866c12f80
fzyzcjy <ch271828n@outlook.com>
2025-10-11 12:29:27 +0800

    more

--- sgl-router/src/metrics_aggregator.rs
+++ sgl-router/src/metrics_aggregator.rs
@@ -1,15 +1,33 @@
+use openmetrics_parser::{PrometheusType, PrometheusValue};
+use prometheus::Encoder;
 use std::collections::HashMap;
+use std::string::FromUtf8Error;
 
-pub(crate) struct MetricPack {
-    pub(crate) labels: HashMap<String, String>,
-    pub(crate) metrics_text: String,
+pub struct MetricPack {
+    pub labels: HashMap<String, String>,
+    pub metrics_text: String,
 }
 
 /// Aggregate Prometheus metrics scraped from multiple sources into a unified one
-pub(crate) fn aggregate_metrics(metric_packs: Vec<MetricPack>) -> anyhow::Result<String> {
+pub fn aggregate_metrics(metric_packs: Vec<MetricPack>) -> anyhow::Result<String> {
     for metric_pack in metric_packs {
-        let metrics = openmetrics_parser::prometheus::parse_prometheus(&metric_pack.metrics_text)?;
-        todo!()
+        let exposition =
+            match openmetrics_parser::prometheus::parse_prometheus(&metric_pack.metrics_text) {
+                Ok(e) => e,
+                Err(_) => continue,
+            };
+
+        TODO
     }
-    todo!()
+
+    encode_metric_families(output_metric_families)
+}
+
+fn encode_metric_families(
+    metric_families: &[prometheus::proto::MetricFamily],
+) -> anyhow::Result<String> {
+    let mut buffer = vec![];
+    let encoder = prometheus::TextEncoder::new();
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+    Ok(String::from_utf8(buffer)?)
 }
