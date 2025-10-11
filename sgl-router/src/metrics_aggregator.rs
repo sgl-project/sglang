d614c2072331b4643b39b5574699fdf8f98083d5
fzyzcjy <ch271828n@outlook.com>
2025-10-11 14:02:56 +0800

    more

--- sgl-router/src/metrics_aggregator.rs
+++ sgl-router/src/metrics_aggregator.rs
@@ -1,5 +1,5 @@
-use std::collections::HashMap;
 use openmetrics_parser::{MetricsExposition, PrometheusType, PrometheusValue};
+use std::collections::HashMap;
 
 pub struct MetricPack {
     pub labels: HashMap<String, String>,
@@ -10,26 +10,30 @@ type PrometheusExposition = MetricsExposition<PrometheusType, PrometheusValue>;
 
 /// Aggregate Prometheus metrics scraped from multiple sources into a unified one
 pub fn aggregate_metrics(metric_packs: Vec<MetricPack>) -> anyhow::Result<String> {
+    let mut expositions = vec![];
+
     for metric_pack in metric_packs {
         let exposition =
             match openmetrics_parser::prometheus::parse_prometheus(&metric_pack.metrics_text) {
-                Ok(e) => e,
+                Ok(x) => x,
                 Err(err) => {
                     eprintln!("hi err={err:?}");
                     continue;
-                },
+                }
             };
-        eprintln!("exposition={}", exposition);
-        let exposition = transform_metrics(exposition);
+        let exposition = transform_metrics(exposition, &metric_pack.labels);
+        expositions.push(exposition);
     }
 
     Ok("hi".into())
 }
 
-fn transform_metrics(mut exposition: PrometheusExposition) -> PrometheusExposition {
+fn transform_metrics(
+    mut exposition: PrometheusExposition,
+    extra_labels: &HashMap<String, String>,
+) -> PrometheusExposition {
     for (_, family) in &mut exposition.families {
-        eprintln!("family={}", family);
+        *family = family.with_labels(extra_labels);
     }
-
     exposition
 }
