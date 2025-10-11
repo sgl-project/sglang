6ecd2b31f29b632a3540145cdeb95e4868eaf026
fzyzcjy <ch271828n@outlook.com>
2025-10-11 12:32:14 +0800

    more

--- sgl-router/src/metrics_aggregator.rs
+++ sgl-router/src/metrics_aggregator.rs
@@ -10,8 +10,6 @@ pub struct MetricPack {
 
 /// Aggregate Prometheus metrics scraped from multiple sources into a unified one
 pub fn aggregate_metrics(metric_packs: Vec<MetricPack>) -> anyhow::Result<String> {
-    let mut output_metric_families = vec![];
-
     for metric_pack in metric_packs {
         let exposition =
             match openmetrics_parser::prometheus::parse_prometheus(&metric_pack.metrics_text) {
@@ -20,25 +18,9 @@ pub fn aggregate_metrics(metric_packs: Vec<MetricPack>) -> anyhow::Result<String
             };
 
         for (_, family) in exposition.families {
-            let output_metric_family = prometheus::proto::MetricFamily {
-                name: Some(TODO),
-                help: Some(TODO),
-                type_: Some(TODO),
-                metric: vec![TODO],
-                special_fields: Default::default(),
-            };
-            TODO
+            println!("family={}", family);
         }
     }
 
-    encode_metric_families(output_metric_families)
-}
-
-fn encode_metric_families(
-    metric_families: &[prometheus::proto::MetricFamily],
-) -> anyhow::Result<String> {
-    let mut buffer = vec![];
-    let encoder = prometheus::TextEncoder::new();
-    encoder.encode(&metric_families, &mut buffer).unwrap();
-    Ok(String::from_utf8(buffer)?)
+    Ok("hi".into())
 }
