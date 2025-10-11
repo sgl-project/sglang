ef5c9c4cf295c7b96d11369861d4350cc5c5b840
fzyzcjy <ch271828n@outlook.com>
2025-10-11 14:37:27 +0800

    more

--- sgl-router/src/metrics_aggregator.rs
+++ sgl-router/src/metrics_aggregator.rs
@@ -19,7 +19,7 @@ pub fn aggregate_metrics(metric_packs: Vec<MetricPack>) -> anyhow::Result<String
             match openmetrics_parser::prometheus::parse_prometheus(&metric_pack.metrics_text) {
                 Ok(x) => x,
                 Err(err) => {
-                    warn!(
+                    eprintln!(
                         "aggregate_metrics error when parsing text: pack={:?} err={:?}",
                         metric_pack, err
                     );
