962b063b1489f724952bb71d480283d2d7481bf0
fzyzcjy <ch271828n@outlook.com>
2025-10-11 12:13:59 +0800

    more

--- sgl-router/src/metrics_aggregator.rs
+++ sgl-router/src/metrics_aggregator.rs
@@ -5,6 +5,7 @@ pub(crate) struct MetricPack {
     pub(crate) metrics_text: String,
 }
 
+/// Aggregate Prometheus metrics scraped from multiple sources into a unified one
 pub(crate) fn aggregate_metrics(metric_packs: Vec<MetricPack>) -> anyhow::Result<String> {
     for metric_pack in metric_packs {
         let metrics = openmetrics_parser::prometheus::parse_prometheus(&metric_pack.metrics_text)?;
