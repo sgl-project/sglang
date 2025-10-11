908898b226ce5bd0b336d54246e25f861dbb00fb
wxsm <wxsms@foxmail.com>
2025-10-11 10:18:19 +0800

    fix: Change dsv32 hack temporary path to use system temp directory

--- python/sglang/srt/utils/hf_transformers_utils.py
+++ python/sglang/srt/utils/hf_transformers_utils.py
@@ -19,6 +19,7 @@ import os
 import warnings
 from pathlib import Path
 from typing import Any, Dict, Optional, Type, Union
+import tempfile
 
 import torch
 from huggingface_hub import snapshot_download
@@ -145,7 +146,7 @@ def _load_deepseek_v32_model(
     config_json["architectures"] = ["DeepseekV3ForCausalLM"]
     config_json["model_type"] = "deepseek_v3"
 
-    tmp_path = os.path.join(local_path, "_tmp_config_folder")
+    tmp_path = os.path.join(tempfile.gettempdir(), "_tmp_config_folder")
     os.makedirs(tmp_path, exist_ok=True)
 
     unique_path = os.path.join(tmp_path, f"deepseek_v32_{os.getpid()}")
