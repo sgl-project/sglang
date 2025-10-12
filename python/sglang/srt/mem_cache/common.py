28ded0cfd927a3e03206c00a6c6288dde2562ddc
Even Zhou <even.y.zhou@outlook.com>
2025-10-12 23:53:20 +0800

    fix memcache alloc device

--- python/sglang/srt/mem_cache/common.py
+++ python/sglang/srt/mem_cache/common.py
@@ -354,7 +354,7 @@ def alloc_for_extend(
             (
                 t[-1:]
                 if len(t) > 0
-                else torch.tensor([-1], device=batch.tree_cache.device)
+                else torch.tensor([-1], device=batch.device)
             )
             for t in prefix_tensors
         ]
