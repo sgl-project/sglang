84e4d9dd1b60fc848c5e0fa2001cf789a4f2d29a
晟海 <huangtingwei.htw@antgroup.com>
2025-10-13 18:40:03 +0800

    fix

--- sgl-kernel/csrc/kvcacheio/transfer.cu
+++ sgl-kernel/csrc/kvcacheio/transfer.cu
@@ -96,8 +96,8 @@ __device__ __forceinline__ T* get_global_offset_lf_tbl_hfrg(
     int64_t head_fragment_num,
     int64_t head_fragment_id,
     int64_t /*unused*/) {
-  return reinterpret_cast<T*>(layer_base_tbl[layer_id]) + page_id * item_size_bytes
-        + item_size_bytes / head_fragment_num * head_fragment_id;
+  return reinterpret_cast<T*>(layer_base_tbl[layer_id]) + page_id * item_size_bytes;
+//         + item_size_bytes / head_fragment_num * head_fragment_id;
 }
 
 template <typename T>
@@ -114,7 +114,7 @@ __device__ __forceinline__ T* get_global_offset_phf(
   // page head first
   return base + page_id / page_size * page_size * page_dim
          + page_dim / head_fragment_num * head_fragment_id * page_size
-         + (page_id % page_size) * page_dim / head_fragment_num
+         + page_id % page_size * page_dim / head_fragment_num
          + layer_id * item_size_bytes / head_fragment_num;
 }
 
