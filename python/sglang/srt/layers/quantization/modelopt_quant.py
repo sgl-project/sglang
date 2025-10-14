98defbf9f510ffc25e46627f1cd28bcb609ba8ab
jiahanc <173873397+jiahanc@users.noreply.github.com>
2025-10-14 10:25:49 -0700

    update flashinfer to 0.4.1
    Signed-off-by: jiahanc <173873397+jiahanc@users.noreply.github.com>

--- python/sglang/srt/layers/quantization/modelopt_quant.py
+++ python/sglang/srt/layers/quantization/modelopt_quant.py
@@ -1070,8 +1070,8 @@ class ModelOptNvFp4FusedMoEMethod(FusedMoEMethodBase):
             shuffle_matrix_sf_a,
         )
         from flashinfer.fused_moe.core import (
-            _maybe_get_cached_w2_permute_indices,
             _maybe_get_cached_w3_w1_permute_indices,
+            get_w2_permute_indices_with_cache,
         )
 
         """Prepare quantized weights for kernel (done offline with weights)."""
@@ -1132,7 +1132,7 @@ class ModelOptNvFp4FusedMoEMethod(FusedMoEMethodBase):
                 )
             )
 
-            permute_indices = _maybe_get_cached_w2_permute_indices(
+            permute_indices = get_w2_permute_indices_with_cache(
                 self._cache_permute_indices,
                 gemm2_weights_fp4[i].view(torch.uint8),
                 epilogue_tile_m,
@@ -1143,7 +1143,7 @@ class ModelOptNvFp4FusedMoEMethod(FusedMoEMethodBase):
                 .contiguous()
             )
 
-            permute_sf_indices = _maybe_get_cached_w2_permute_indices(
+            permute_sf_indices = get_w2_permute_indices_with_cache(
                 self._cache_permute_indices,
                 gemm2_scales_linear_fp4[i].view(torch.uint8),
                 epilogue_tile_m,
