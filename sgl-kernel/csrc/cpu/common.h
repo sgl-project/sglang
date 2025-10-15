401ca72712876ac703732182f5a763ab147f61ed
mingfeima <mingfei.ma@intel.com>
2025-10-14 19:58:13 -0700

    add casaul_conv1d support on CPU

--- sgl-kernel/csrc/cpu/common.h
+++ sgl-kernel/csrc/cpu/common.h
@@ -321,4 +321,10 @@ struct Unroll<1> {
   }
 };
 
+// conditional data ptr for optional tensor
+template <typename T>
+inline T* conditional_data_ptr(const std::optional<at::Tensor>& opt) {
+  return opt.has_value() ? opt.value().data_ptr<T>() : nullptr;
+}
+
 }  // anonymous namespace
