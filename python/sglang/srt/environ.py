596703e81d0ad6e38e9f3761027ded2b58822470
zejunchen-zejun <zejun.chen@amd.com>
2025-10-15 09:29:15 +0800

    [Profiler] Add SGLANG_PROFILE_RECORD_SHAPES for recording shapes when profiling
    Signed-off-by: zejunchen-zejun <zejun.chen@amd.com>

--- python/sglang/srt/environ.py
+++ python/sglang/srt/environ.py
@@ -117,6 +117,7 @@ class Envs:
     SGLANG_TEST_RETRACT = EnvBool(False)
     SGLANG_SET_CPU_AFFINITY = EnvBool(False)
     SGLANG_PROFILE_WITH_STACK = EnvBool(True)
+    SGLANG_PROFILE_RECORD_SHAPES = EnvBool(True)
     SGLANG_RECORD_STEP_TIME = EnvBool(False)
     SGLANG_GC_LOG = EnvBool(False)
     SGLANG_FORCE_SHUTDOWN = EnvBool(False)
