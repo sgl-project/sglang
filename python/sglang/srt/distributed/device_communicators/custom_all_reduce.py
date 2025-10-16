8b17fc6cbbd5ff330699eeef4a401d92e6ee05c3
Lianmin Zheng <lianminzheng@gmail.com>
2025-10-15 18:09:56 -0700

    fix error messages

--- python/sglang/srt/distributed/device_communicators/custom_all_reduce.py
+++ python/sglang/srt/distributed/device_communicators/custom_all_reduce.py
@@ -301,15 +301,11 @@ class CustomAllreduce:
         if _is_hip:
             handle, offset = ops.get_graph_buffer_ipc_meta(self._ptr)
             handles, offsets = self._gather_ipc_meta((bytes(handle), offset))
-            log_info_on_rank0(
-                logger, "Registering %d cuda graph addresses", len(offset)
-            )
+            log_info_on_rank0(logger, f"Registering {len(offset)} cuda graph addresses")
             ops.register_graph_buffers(self._ptr, handles, offsets)
         else:
             handle, offset = ops.get_graph_buffer_ipc_meta(self._ptr)
-            log_info_on_rank0(
-                logger, "Registering %d cuda graph addresses", len(offset)
-            )
+            log_info_on_rank0(logger, f"Registering {len(offset)} cuda graph addresses")
             # We cannot directly use `dist.all_gather_object` here
             # because it is incompatible with `gloo` backend under inference mode.
             # see https://github.com/pytorch/pytorch/issues/126032 for details.
