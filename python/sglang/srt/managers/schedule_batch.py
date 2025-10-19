be057f03055a16f41c3fca57bce4a931c16c2229
Liangsheng Yin <lsyincs@gmail.com>
2025-10-19 15:40:02 +0800

    remove wrong cache

--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import enum
-from functools import cache
 
 # Copyright 2023-2024 SGLang Team
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -655,7 +654,6 @@ class Req:
         return self.sampling_params.max_new_tokens == 0 and spec_alg is None
 
     @property
-    @cache
     def output_ids_through_stop(self) -> List[int]:
         """Get the output ids through the stop condition. Stop position is included."""
         if self.finished_len is not None:
@@ -780,6 +778,12 @@ class Req:
         # Check stop token ids
         matched_eos = False
 
+        self.sampling_params.stop_token_ids = [279, 7227]  # The, API
+        stop_token_ids = self.sampling_params.stop_token_ids
+        stop_token = self.tokenizer.convert_ids_to_tokens(stop_token_ids)
+        print(f"[V2 Spec] {stop_token_ids=}")
+        print(f"[V2 Spec] {stop_token=}")
+
         for i, token_id in enumerate(new_accepted_tokens):
             if self.sampling_params.stop_token_ids:
                 matched_eos |= token_id in self.sampling_params.stop_token_ids
@@ -793,6 +797,9 @@ class Req:
                 self.finished_reason = FINISH_MATCHED_TOKEN(matched=token_id)
                 matched_pos = len(self.output_ids) - len(new_accepted_tokens) + i
                 self.finished_len = matched_pos + 1
+                print(
+                    f"[V2 Spec] Finished token ids: {self.output_ids[:self.finished_len]}"
+                )
                 return True
 
         return False
