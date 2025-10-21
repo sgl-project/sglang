91f7ae40f27d265fa7297a76cb5e2b1b4506c870
zRzRzRzRzRzRzR <2448370773@qq.com>
2025-10-21 08:50:25 +0800

    Update glm4_moe.py

--- python/sglang/srt/models/glm4_moe.py
+++ python/sglang/srt/models/glm4_moe.py
@@ -56,11 +56,9 @@ from sglang.srt.layers.linear import (
 )
 from sglang.srt.layers.logits_processor import LogitsProcessor
 from sglang.srt.layers.moe import (
-    get_deepep_mode,
     get_moe_a2a_backend,
     should_use_flashinfer_cutlass_moe_fp4_allgather,
 )
-from sglang.srt.layers.moe import get_moe_a2a_backend
 from sglang.srt.layers.moe.ep_moe.layer import get_moe_impl_class
 from sglang.srt.layers.moe.fused_moe_triton.layer import FusedMoE
 from sglang.srt.layers.moe.topk import TopK
