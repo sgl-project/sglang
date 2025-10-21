6b211b8d31c4ceac9a99eb31488ca4c8e6d4b7aa
ZLkanyo009 <407120045@qq.com>
2025-10-21 06:51:23 +0000

    [Feature] Add fuse shared expert feature in EP

--- python/sglang/srt/managers/tokenizer_communicator_mixin.py
+++ python/sglang/srt/managers/tokenizer_communicator_mixin.py
@@ -310,6 +310,8 @@ class TokenizerCommunicatorMixin:
         self.auto_create_handle_loop()
         env_with_stack: bool = get_bool_env_var("SGLANG_PROFILE_WITH_STACK", "true")
         with_stack = False if with_stack is False or env_with_stack is False else True
+        env_record_shapes: bool = get_bool_env_var("SGLANG_PROFILE_RECORD_SHAPES", "true")
+        record_shapes = False if record_shapes is False or env_record_shapes is False else True
         req = ProfileReq(
             type=ProfileReqType.START_PROFILE,
             output_dir=output_dir,
