bcbb24c8cbb0ba0ad515505e917b868b3193ecae
Wangzheee <634486483@qq.com>
2025-10-22 16:01:06 +0800

    fix lint

--- python/sglang/srt/layers/quantization/compressed_tensors/schemes/__init__.py
+++ python/sglang/srt/layers/quantization/compressed_tensors/schemes/__init__.py
@@ -3,7 +3,6 @@
 
 from .compressed_tensors_scheme import CompressedTensorsScheme
 from .compressed_tensors_w8a8_fp8 import CompressedTensorsW8A8Fp8
-from .compressed_tensors_w8a8_int8 import CompressedTensorsW8A8Int8
 from .compressed_tensors_w8a16_fp8 import CompressedTensorsW8A16Fp8
 from .compressed_tensors_wNa16 import WNA16_SUPPORTED_BITS, CompressedTensorsWNA16
 
