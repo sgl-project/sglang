8af3418c3c5f4fdbf09c3500e177559f75cb1327
Bowen Bao <bowenbao@amd.com>
2025-10-22 22:39:43 +0000

    Move aiter imports under hip check

--- python/sglang/srt/layers/quantization/quark/quark_moe.py
+++ python/sglang/srt/layers/quantization/quark/quark_moe.py
@@ -6,9 +6,6 @@ import logging
 from typing import TYPE_CHECKING, Any
 
 import torch
-from aiter import ActivationType, QuantType
-from aiter.fused_moe import fused_moe
-from aiter.utility.fp4_utils import e8m0_shuffle
 
 from sglang.srt.layers.moe import MoeRunner, MoeRunnerBackend, MoeRunnerConfig
 from sglang.srt.layers.moe.moe_runner.triton import TritonMoeQuantInfo
@@ -36,6 +33,9 @@ _is_hip = is_hip()
 _use_aiter = get_bool_env_var("SGLANG_USE_AITER") and _is_hip
 if _use_aiter:
     from aiter.ops.shuffle import shuffle_weight
+    from aiter import ActivationType, QuantType
+    from aiter.fused_moe import fused_moe
+    from aiter.utility.fp4_utils import e8m0_shuffle
 
     from sglang.srt.layers.moe.rocm_moe_utils import rocm_fused_experts_tkw1
 
