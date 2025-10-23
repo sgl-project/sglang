38b5b6b1e094189a382ccd61b19b6ac638928cf6
Hubert Lu <Hubert.Lu@amd.com>
2025-10-23 19:05:15 +0000

    Update AMD CI

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -272,15 +272,14 @@ suite_amd = {
         # TestFile("test_triton_attention_kernels.py", 4),
         TestFile("test_triton_attention_backend.py", 150),
         TestFile("test_triton_sliding_window.py", 250),
-        # TestFile("test_vision_chunked_prefill.py", 175), # Disabled temporarily and track in #7701
+        TestFile("test_vision_chunked_prefill.py", 175),
         TestFile("test_wave_attention_kernels.py", 2),
-        TestFile("test_vlm_input_format.py", 300),
         # Disabled temporarily
+        # TestFile("test_vlm_input_format.py", 300),
         # TestFile("models/test_embedding_models.py", 73), # Disabled temporarily, see https://github.com/sgl-project/sglang/issues/11127
         # TestFile("openai_server/features/test_openai_server_hidden_states.py", 240),
         # TestFile("rl/test_update_weights_from_tensor.py", 48),
         # TestFile("test_no_overlap_scheduler.py", 234), # Disabled temporarily and track in #7703
-        # TestFile("test_vision_chunked_prefill.py", 175), # Disabled temporarily and track in #7701
         # TestFile("test_wave_attention_backend.py", 150), # Disabled temporarily, see https://github.com/sgl-project/sglang/issues/11127
     ],
     "per-commit-amd-mi35x": [
