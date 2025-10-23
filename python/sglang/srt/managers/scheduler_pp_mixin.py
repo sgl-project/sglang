2efd976958bd255d95fc83f3a292c07b810c2055
bluecoffee8 <jasperli2002@gmail.com>
2025-10-23 15:35:14 -0700

    cleanup PR

--- python/sglang/srt/managers/scheduler_pp_mixin.py
+++ python/sglang/srt/managers/scheduler_pp_mixin.py
@@ -203,7 +203,6 @@ class SchedulerPPMixin:
         if mbs[next_mb_id] is not None:
             with torch.profiler.record_function("recv_res_dict_from_prev_stage"):
                 next_pp_outputs = PPProxyTensors(self._pp_recv_dict_from_prev_stage())
-        # self._pp_commit_comm_work(work=send_output_work)
         if mbs[next_mb_id] is not None:
             with self.copy_stream_ctx:
                 self.copy_stream.wait_stream(self.default_stream)
@@ -291,7 +290,6 @@ class SchedulerPPMixin:
                 next_mb_id = (mb_id + 1) % self.pp_loop_size
                 with torch.profiler.record_function("recv_requests"):
                     recv_reqs = self.recv_requests()
-                    # self._pp_commit_comm_work(send_req_work)
                     self.process_input_requests(recv_reqs)
                 if not self.pp_group.is_last_rank:
                     self._pp_commit_comm_work(send_req_work)
@@ -307,7 +305,6 @@ class SchedulerPPMixin:
                 if self.cur_batch:
                     server_is_idle = False
                     pp_proxy_tensors = self._pp_recv_proxy_tensors()
-                # self._pp_commit_comm_work(send_proxy_work)
                 next_pp_outputs = None
                 next_batch_result = None
                 d2h_event = None
@@ -349,12 +346,6 @@ class SchedulerPPMixin:
                         )
                     last_mbs[next_mb_id] = mbs[next_mb_id]
                 if not self.pp_group.is_last_rank:
-                    # self._pp_commit_comm_work(send_req_work)
-                    # with torch.profiler.record_function("send_reqs_to_next_stage"):
-                    #     send_req_work = self._pp_send_pyobj_to_next_stage(
-                    #         recv_reqs,
-                    #         async_send=True,
-                    #     )
                     if self.cur_batch:
                         torch.cuda.current_stream().wait_event(event)
                         self._pp_commit_comm_work(send_proxy_work)
