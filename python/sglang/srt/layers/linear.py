353a8fad26b19dee027c83b05f857b371e7cf5a0
artetaout <lulala341@gmail.com>
2025-10-27 11:42:33 +0000

    fix lint
    Signed-off-by: artetaout <lulala341@gmail.com>

--- python/sglang/srt/layers/linear.py
+++ python/sglang/srt/layers/linear.py
@@ -32,7 +32,7 @@ from sglang.srt.layers.parameter import (
 )
 from sglang.srt.layers.quantization.unquant import UnquantizedLinearMethod
 from sglang.srt.layers.utils import pad_or_narrow_weight
-from sglang.srt.utils import get_bool_env_var, is_cpu, is_hip, is_npu, set_weight_attrs
+from sglang.srt.utils import get_bool_env_var, get_int_env_var, is_cpu, is_hip, is_npu, set_weight_attrs
 
 if TYPE_CHECKING:
     from sglang.srt.layers.quantization.base_config import (
