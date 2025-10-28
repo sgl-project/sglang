418c789417981c5d57274f8c96f92c7992965ff2
Vladimir221 <xashev2013@gmail.com>
2025-10-28 19:21:19 +0300

    NPU only support prefill compilation with 'eager' backend, added checking of piecewise_cuda_graph_compiler option, changed device arg to PrivateUse1 in direct_register_custom_op function if platform is NPU

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -558,6 +558,9 @@ class ServerArgs:
         self._handle_hpu_backends()
         self._handle_cpu_backends()
 
+        # Handle compilation config
+        self._handle_compilation_cfg()
+
         # Apply model-specific adjustments.
         self._handle_model_specific_adjustments()
 
@@ -860,6 +863,15 @@ class ServerArgs:
             if self.attention_backend is None:
                 self.attention_backend = "intel_amx"
             self.sampling_backend = "pytorch"
+
+    def _handle_compilation_cfg(self):
+        # NPU platform
+        if is_npu() and self.piecewise_cuda_graph_compiler != "eager":
+            logger.warning(
+                "At this moment Ascend platform only support prefill graph compilation with "
+                "piecewise_cuda_graph_compiler='eager', change piecewise_cuda_graph_compiler to 'eager'."
+            )
+            self.piecewise_cuda_graph_compiler = "eager"
 
     def _handle_model_specific_adjustments(self):
         from sglang.srt.configs.model_config import is_deepseek_nsa
