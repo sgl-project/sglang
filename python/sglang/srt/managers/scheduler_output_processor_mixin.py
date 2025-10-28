6f482dbd8afed68b7346e5ecda0982fe063ed244
Liangsheng Yin <lsyincs@gmail.com>
2025-10-29 02:06:39 +0800

    fix mixed

--- python/sglang/srt/managers/scheduler_output_processor_mixin.py
+++ python/sglang/srt/managers/scheduler_output_processor_mixin.py
@@ -83,17 +83,8 @@ class SchedulerOutputProcessorMixin:
             logprob_pt = 0
 
             for i, (req, next_token_id) in enumerate(zip(batch.reqs, next_token_ids)):
-                if (
-                    self.is_mixed_chunk
-                    and self.enable_overlap
-                    and (req.finished() or req.is_retracted)
-                ):
-                    # Free the one delayed token for the mixed decode batch
-                    j = len(batch.out_cache_loc) - len(batch.reqs) + i
-                    self.token_to_kv_pool_allocator.free(batch.out_cache_loc[j : j + 1])
-                    continue
-
-                if req.is_retracted:
+                if req.finished() or req.is_retracted:
+                    # decode req in mixed batch or retracted req
                     continue
 
                 if req.is_chunked <= 0:
