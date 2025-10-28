bc15288bf292978c7ad5e460d893d08b08440797
AichenF <aichenf@nvidia.com>
2025-10-28 14:12:38 +0800

    fix format

--- python/sglang/srt/layers/quantization/modelopt_quant.py
+++ python/sglang/srt/layers/quantization/modelopt_quant.py
@@ -39,7 +39,7 @@ from sglang.srt.layers.quantization.utils import (
     requantize_with_max_scale,
 )
 from sglang.srt.layers.radix_attention import RadixAttention
-from sglang.srt.utils import get_bool_env_var, is_blackwell, is_cuda, next_power_of_2
+from sglang.srt.utils import get_bool_env_var, is_cuda, next_power_of_2
 
 if TYPE_CHECKING:
     from sglang.srt.layers.moe.fused_moe_triton.layer import FusedMoE
