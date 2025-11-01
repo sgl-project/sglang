1082bf33aa56e40cc679e58c3888154dc0d77e27
wanghanpei <wanghanpei@bytedance.com>
2025-11-01 05:44:07 +0000

    fix lint

--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -81,8 +81,8 @@ from sglang.srt.sampling.sampling_batch_info import SamplingBatchInfo
 from sglang.srt.sampling.sampling_params import SamplingParams
 from sglang.srt.server_args import ServerArgs, get_global_server_args
 from sglang.srt.utils import flatten_nested_list
-from sglang.srt.utils.cuda_ipc_transport_utils import CudaIpcTensorTransportProxy
 from sglang.srt.utils.common import is_npu
+from sglang.srt.utils.cuda_ipc_transport_utils import CudaIpcTensorTransportProxy
 
 _is_npu = is_npu()
 
