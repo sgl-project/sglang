d154b1265beb2693c45ee4c9229c2f0acedd8425
Zheng, Beilei <beilei.zheng@intel.com>
2025-11-02 21:41:12 -0800

    apply mrope kernel

--- python/sglang/srt/layers/rotary_embedding.py
+++ python/sglang/srt/layers/rotary_embedding.py
@@ -36,6 +36,9 @@ if _is_cuda:
 else:
     FusedSetKVBufferArg = None
 
+if _is_cpu and _is_cpu_amx_available:
+    import sgl_kernel  # noqa: F401
+
 if _use_aiter:
     from aiter.rotary_embedding import get_rope as aiter_get_rope
 
@@ -1364,6 +1367,26 @@ class MRotaryEmbedding(RotaryEmbedding):
         ):
             self.cos_sin_cache = self.cos_sin_cache.to(query.device, dtype=query.dtype)
 
+    def _forward_cpu(
+        self,
+        positions: torch.Tensor,
+        query: torch.Tensor,
+        key: torch.Tensor,
+        fused_set_kv_buffer_arg: Optional[FusedSetKVBufferArg] = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        if _is_cpu_amx_available:
+            return torch.ops.sgl_kernel.multimodal_rotary_embedding_cpu(
+                positions,
+                query,
+                key,
+                self.head_size,
+                self.cos_sin_cache,
+                self.mrope_section if self.mrope_section else None,
+                self.mrope_interleaved,
+                self.is_neox_style,
+            )
+        return self._forward_native(positions, query, key, fused_set_kv_buffer_arg)
+
     @torch.compile(dynamic=True, backend=get_compiler_backend())
     def _forward_native(
         self,
@@ -1438,6 +1461,8 @@ class MRotaryEmbedding(RotaryEmbedding):
 
         if positions.ndim == 2 and self.mrope_section and _is_cuda:
             return self._forward_triton(positions, query, key)
+        elif _is_cpu:
+            return self._forward_cpu(positions, query, key)
         else:
             return self._forward_native(positions, query, key)
 
