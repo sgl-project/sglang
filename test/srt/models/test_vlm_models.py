95bd89ec95a17d67db11fb102427a5ffddfbc608
wanghanpei <wanghanpei@bytedance.com>
2025-11-05 08:56:35 +0000

    fix ut

--- test/srt/models/test_vlm_models.py
+++ test/srt/models/test_vlm_models.py
@@ -146,8 +146,6 @@ class TestVLMModels(CustomTestCase):
             process_env = os.environ.copy()
             if custom_env:
                 process_env.update(custom_env)
-            # if test vlm with cuda_ipc feature, open this env_var
-            process_env["SGLANG_USE_CUDA_IPC_TRANSPORT"] = "1"
 
             # Prepare stdout/stderr redirection if needed
             stdout_file = None
@@ -167,6 +165,7 @@ class TestVLMModels(CustomTestCase):
                     "--cuda-graph-max-bs",
                     "32",
                     "--enable-multimodal",
+                    "--keep-mm-feature-on-device",
                     "--mem-fraction-static",
                     str(self.parsed_args.mem_fraction_static),  # Use class variable
                     "--log-level",
