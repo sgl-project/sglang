c8547ecddd8ebf5095b8ee3b825166b5cf94ad89
Morpheus Guo <yuechao.guo@amd.com>
2025-11-06 05:01:23 +0800

    Enable Aiter Attention for VL model (#12699)
    Co-authored-by: yuechguo <yuechguo@amd.com>

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -2644,7 +2644,7 @@ class ServerArgs:
         parser.add_argument(
             "--mm-attention-backend",
             type=str,
-            choices=["sdpa", "fa3", "triton_attn", "ascend_attn"],
+            choices=["sdpa", "fa3", "triton_attn", "ascend_attn", "aiter_attn"],
             default=ServerArgs.mm_attention_backend,
             help="Set multimodal attention backend.",
         )
