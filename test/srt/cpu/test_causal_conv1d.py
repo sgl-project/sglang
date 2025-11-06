4283bf64aaad9fea7ab85e9129c6981123b90ff9
mingfeima <mingfei.ma@intel.com>
2025-11-06 15:50:36 +0000

    update format

--- test/srt/cpu/test_causal_conv1d.py
+++ test/srt/cpu/test_causal_conv1d.py
@@ -2,12 +2,13 @@ import itertools
 import unittest
 from typing import Optional
 
+import sgl_kernel
 import torch
 import torch.nn.functional as F
-from sglang.test.test_utils import CustomTestCase
 from utils import parametrize, precision
 
-import sgl_kernel
+from sglang.test.test_utils import CustomTestCase
+
 causal_conv1d_weight_pack = torch.ops.sgl_kernel.causal_conv1d_weight_pack
 causal_conv1d_fwd = torch.ops.sgl_kernel.causal_conv1d_fwd_cpu
 causal_conv1d_update = torch.ops.sgl_kernel.causal_conv1d_update_cpu
@@ -16,6 +17,7 @@ causal_conv1d_update = torch.ops.sgl_kernel.causal_conv1d_update_cpu
 torch.manual_seed(1234)
 
 PAD_SLOT_ID = -1
+
 
 def causal_conv1d_ref(
     x: torch.Tensor,
@@ -57,6 +59,7 @@ def causal_conv1d_ref(
             final_states_out = final_states
     out = (out if activation is None else F.silu(out)).to(dtype=dtype_in)
     return (out, None) if not return_final_states else (out, final_states_out)
+
 
 def causal_conv1d_update_ref(
     x, conv_state, weight, bias=None, activation=None, cache_seqlens=None
@@ -112,7 +115,7 @@ class TestCausalConv1d(CustomTestCase):
         has_bias,
         has_initial_state,
         dtype=torch.bfloat16,
-        prepack=True
+        prepack=True,
     ):
         x = torch.randn(batch, seqlen, dim).to(dtype).transpose_(-1, -2)
         weight = torch.randn(dim, width).to(dtype)
@@ -133,7 +136,8 @@ class TestCausalConv1d(CustomTestCase):
             bias,
             initial_states,
             return_final_states=has_initial_state,
-            activation=self.activation)
+            activation=self.activation,
+        )
 
         out = causal_conv1d_fwd(
             x,
@@ -145,11 +149,14 @@ class TestCausalConv1d(CustomTestCase):
             has_initial_state_tensor,
             self.activation in ["silu"],
             PAD_SLOT_ID,
-            prepack)
+            prepack,
+        )
 
         atol = rtol = precision[dtype]
         torch.testing.assert_close(out_ref, out, atol=atol, rtol=rtol)
-        torch.testing.assert_close(final_states_ref, initial_states, atol=atol, rtol=rtol)
+        torch.testing.assert_close(
+            final_states_ref, initial_states, atol=atol, rtol=rtol
+        )
 
     @parametrize(
         batch=[11],
@@ -165,7 +172,7 @@ class TestCausalConv1d(CustomTestCase):
         width,
         has_bias=False,
         dtype=torch.bfloat16,
-        prepack=False
+        prepack=False,
     ):
         total_entries = batch + 3
 
@@ -184,7 +191,9 @@ class TestCausalConv1d(CustomTestCase):
         final_states = torch.randn(total_entries, dim, width - 1, dtype=dtype)
         final_states_ref = final_states.clone()
 
-        has_initial_states = torch.randint(0, 2, (batch,), dtype=torch.bool).fill_(False)
+        has_initial_states = torch.randint(0, 2, (batch,), dtype=torch.bool).fill_(
+            False
+        )
         state_indices = torch.randperm(total_entries, dtype=torch.int32)[:batch]
 
         out_ref = []
@@ -195,23 +204,23 @@ class TestCausalConv1d(CustomTestCase):
         for i, x_s in enumerate(splits):
             out_ref_b.append(
                 causal_conv1d_ref(
-                x_s.unsqueeze(0),
-                weight,
-                bias,
-                activation=self.activation,
-                return_final_states=return_final_states,
-                final_states_out=(
-                    final_states_ref[state_indices[i]].unsqueeze(0)
-                    if return_final_states
-                    else None
-                ),
-                initial_states=(
-                    final_states_ref[state_indices[i]].unsqueeze(0)
-                    if has_initial_states[i]
-                    else None
+                    x_s.unsqueeze(0),
+                    weight,
+                    bias,
+                    activation=self.activation,
+                    return_final_states=return_final_states,
+                    final_states_out=(
+                        final_states_ref[state_indices[i]].unsqueeze(0)
+                        if return_final_states
+                        else None
+                    ),
+                    initial_states=(
+                        final_states_ref[state_indices[i]].unsqueeze(0)
+                        if has_initial_states[i]
+                        else None
+                    ),
                 )
             )
-        )
         out_ref.append(torch.cat([t[0] for t in out_ref_b], dim=2))
         out_ref_tensor = torch.cat(out_ref, dim=0).squeeze(0)
 
@@ -225,7 +234,8 @@ class TestCausalConv1d(CustomTestCase):
             has_initial_states,
             self.activation in ["silu"],
             PAD_SLOT_ID,
-            prepack)
+            prepack,
+        )
 
         atol = rtol = precision[dtype]
         torch.testing.assert_close(out_ref_tensor, out, atol=atol, rtol=rtol)
@@ -237,13 +247,7 @@ class TestCausalConv1d(CustomTestCase):
         width=[4],
     )
     def test_causal_conv1d_update(
-        self,
-        batch,
-        dim,
-        width,
-        has_bias=False,
-        dtype=torch.bfloat16,
-        prepack=True
+        self, batch, dim, width, has_bias=False, dtype=torch.bfloat16, prepack=True
     ):
         x = torch.randn(batch, dim).to(dtype)
         conv_state = torch.randn(batch, dim, width - 1, dtype=dtype)
@@ -254,11 +258,7 @@ class TestCausalConv1d(CustomTestCase):
 
         conv_state_ref = conv_state.clone()
         out_ref = causal_conv1d_update_ref(
-            x,
-            conv_state_ref,
-            weight,
-            bias,
-            activation=self.activation
+            x, conv_state_ref, weight, bias, activation=self.activation
         )
 
         cache_seqlens = None
@@ -272,7 +272,8 @@ class TestCausalConv1d(CustomTestCase):
             cache_seqlens,
             conv_state_indices,
             PAD_SLOT_ID,
-            prepack)
+            prepack,
+        )
 
         atol = rtol = precision[dtype]
         torch.testing.assert_close(out_ref, out, atol=atol, rtol=rtol)
@@ -284,13 +285,7 @@ class TestCausalConv1d(CustomTestCase):
         width=[4],
     )
     def test_causal_conv1d_update_with_batch_gather(
-        self,
-        batch,
-        dim,
-        width,
-        has_bias=False,
-        dtype=torch.bfloat16,
-        prepack=True
+        self, batch, dim, width, has_bias=False, dtype=torch.bfloat16, prepack=True
     ):
         total_entries = batch + 3
 
@@ -306,11 +301,7 @@ class TestCausalConv1d(CustomTestCase):
         packed_weight = causal_conv1d_weight_pack(weight) if prepack else weight
 
         out_ref = causal_conv1d_update_ref(
-            x,
-            conv_state_ref,
-            weight,
-            bias,
-            activation=self.activation
+            x, conv_state_ref, weight, bias, activation=self.activation
         )
 
         cache_seqlens = None
@@ -323,12 +314,14 @@ class TestCausalConv1d(CustomTestCase):
             cache_seqlens,
             conv_state_indices,
             PAD_SLOT_ID,
-            prepack
+            prepack,
         )
 
         atol = rtol = precision[dtype]
         torch.testing.assert_close(out_ref, out, atol=atol, rtol=rtol)
-        torch.testing.assert_close(conv_state_ref, conv_state[conv_state_indices, :], atol=atol, rtol=rtol)
+        torch.testing.assert_close(
+            conv_state_ref, conv_state[conv_state_indices, :], atol=atol, rtol=rtol
+        )
 
 
 if __name__ == "__main__":
