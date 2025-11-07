9a788bb161f28c611b3e0377b656002763cbc013
Vladimir221 <46907221+Vladimir221@users.noreply.github.com>
2025-11-07 12:48:13 +0300

    Update run_suite.py
    Added piecewise graph test on NPU into test suite for ascend

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -513,6 +513,7 @@ suite_xpu = {
 suite_ascend = {
     "per-commit-1-ascend-npu": [
         TestFile("ascend/test_ascend_graph_tp1_bf16.py", 400),
+        TestFile("ascend/test_ascend_piecewise_graph_prefill.py", 400),
         TestFile("ascend/test_ascend_tp1_bf16.py", 400),
     ],
     "per-commit-2-ascend-npu": [
