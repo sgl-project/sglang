1110d8351ea6bd21f3ea88669f6c58b4823a39eb
Zheng, Beilei <beilei.zheng@intel.com>
2025-11-09 17:07:14 -0800

    Fix typo and only change bias initialization to use torch.zeros on CPU

--- python/sglang/srt/layers/linear.py
+++ python/sglang/srt/layers/linear.py
@@ -226,9 +226,14 @@ class ReplicatedLinear(LinearBase):
         )
 
         if bias:
-            self.bias = Parameter(
-                torch.zeros(self.output_size, dtype=self.params_dtype)
-            )
+            if _is_cpu:
+                self.bias = Parameter(
+                    torch.zeros(self.output_size, dtype=self.params_dtype)
+                )
+            else:
+                self.bias = Parameter(
+                    torch.empty(self.output_size, dtype=self.params_dtype)
+                )
             set_weight_attrs(
                 self.bias,
                 {
@@ -355,9 +360,14 @@ class ColumnParallelLinear(LinearBase):
             ),
         )
         if bias:
-            self.bias = Parameter(
-                torch.zeros(self.output_size_per_partition, dtype=params_dtype)
-            )
+            if _is_cpu:
+                self.bias = Parameter(
+                    torch.zeros(self.output_size_per_partition, dtype=params_dtype)
+                )
+            else:
+                self.bias = Parameter(
+                    torch.empty(self.output_size_per_partition, dtype=params_dtype)
+                )
             set_weight_attrs(
                 self.bias,
                 {
@@ -1359,7 +1369,10 @@ class RowParallelLinear(LinearBase):
         )
 
         if bias:
-            self.bias = Parameter(torch.zeros(self.output_size, dtype=params_dtype))
+            if _is_cpu:
+                self.bias = Parameter(torch.zeros(self.output_size, dtype=params_dtype))
+            else:
+                self.bias = Parameter(torch.empty(self.output_size, dtype=params_dtype))
             set_weight_attrs(
                 self.bias,
                 {
