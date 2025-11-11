1eac215436c63bd0b9d062956c5e88f75b1f6b82
Shiyang Chen <shiyang@x.ai>
2025-11-11 10:21:25 -0800

    change to nightly

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -15,7 +15,6 @@ class TestFile:
 # NOTE: please sort the test cases alphabetically by the test file name
 suites = {
     "per-commit-1-gpu": [
-        TestFile("test_cpp_radix_cache.py", 60),
         TestFile("debug_utils/test_tensor_dump_forward_hook.py", 15),
         TestFile("function_call/test_json_schema_constraint.py", 1),
         TestFile("hicache/test_hicache.py", 116),
@@ -218,6 +217,7 @@ suites = {
         TestFile("openai_server/features/test_lora_openai_compatible.py", 150),
         TestFile("batch_invariant/test_batch_invariant_ops.py", 10),
         TestFile("test_deepseek_v3_deterministic.py", 240),
+        TestFile("test_cpp_radix_cache.py", 60),
     ],
     "nightly-4-gpu-b200": [
         TestFile("test_fp4_moe.py", 300),
