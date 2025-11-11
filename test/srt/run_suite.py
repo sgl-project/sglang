4a78031a71ddbce7b1e0fc8d2a1e9105eb1e145b
yctseng0211 <yctseng@amd.com>
2025-11-11 18:59:10 +0800

    [ROCM] Optimized deepseek-r1 model with rmsnorm + fp8 quant fusion (#12689)
    should be clean after https://github.com/sgl-project/sglang/pull/13017 landed

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -421,6 +421,7 @@ suite_amd = {
         TestFile("openai_server/validation/test_request_length_validation.py", 31),
         TestFile("quant/test_awq_dequant.py", 2),
         TestFile("quant/test_block_int8.py", 22),
+        TestFile("quant/test_fused_rms_fp8_group_quant.py", 10),
         TestFile("rl/test_update_weights_from_disk.py", 210),
         TestFile("test_abort.py", 51),
         TestFile("test_chunked_prefill.py", 410),
