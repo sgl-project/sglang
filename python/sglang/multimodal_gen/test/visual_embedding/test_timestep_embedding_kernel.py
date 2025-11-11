23c6fd71cf201def9cdf32f453b65a3874cdb581
66RING <travisturing@qq.com>
2025-11-11 10:36:56 +0800

    format

--- python/sglang/multimodal_gen/test/visual_embedding/test_timestep_embedding_kernel.py
+++ python/sglang/multimodal_gen/test/visual_embedding/test_timestep_embedding_kernel.py
@@ -3,10 +3,13 @@
 """
 
 import unittest
+
 import tabulate
 import torch
-from sglang.multimodal_gen.runtime.layers.visual_embedding import timestep_embedding
+
 from sglang.multimodal_gen.runtime.layers.triton_ops import timestep_embedding_triton
+from sglang.multimodal_gen.runtime.layers.visual_embedding import timestep_embedding
+
 
 class TestTimestepEmbed(unittest.TestCase):
     NUM_BATCH = [1, 2, 8, 63, 256, 512, 613, 1024, 1536]
@@ -19,13 +22,12 @@ class TestTimestepEmbed(unittest.TestCase):
                 t = torch.randn((B,), device=device)
                 torch_output = timestep_embedding(t, dim)
                 triton_output = timestep_embedding_triton(t, dim)
-                assert torch.allclose(torch_output, triton_output, atol=1e-6), f"{(torch_output - triton_output).abs().max()}"
-
+                assert torch.allclose(
+                    torch_output, triton_output, atol=1e-6
+                ), f"{(torch_output - triton_output).abs().max()}"
 
     def test_dtype(self):
         pass
-
-
 
     def test_perf(self):
         def perf_kernel_fn(kernel_fn: callable, *args, **kwargs):
@@ -60,21 +62,26 @@ class TestTimestepEmbed(unittest.TestCase):
                 time_triton = perf_kernel_fn(timestep_embedding_triton, t, dim)
                 speedup = time_torch / time_triton
 
-                results.append({
-                    "Batch Size": B,
-                    "Dimension": dim,
-                    "Torch Time (ms)": time_torch,
-                    "Triton Time (ms)": time_triton,
-                    "Speedup (Torch/Triton)": speedup
-                })
+                results.append(
+                    {
+                        "Batch Size": B,
+                        "Dimension": dim,
+                        "Torch Time (ms)": time_torch,
+                        "Triton Time (ms)": time_triton,
+                        "Speedup (Torch/Triton)": speedup,
+                    }
+                )
 
         print("=== Timestep Embedding Benchmark Results ===")
-        print(tabulate.tabulate(
-            results,
-            headers="keys",
-            tablefmt="fancy_grid",
-            floatfmt=(".0f", ".0f", ".6f", ".6f", ".5f")
-        ))
+        print(
+            tabulate.tabulate(
+                results,
+                headers="keys",
+                tablefmt="fancy_grid",
+                floatfmt=(".0f", ".0f", ".6f", ".6f", ".5f"),
+            )
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
-
