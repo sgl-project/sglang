47b74a4cf4b8ab49f3adf54ef0fe087dc9a2329f
Alex Hu <superalexanderhu@gmail.com>
2025-11-11 01:17:00 +0000

    fix

--- python/sglang/srt/configs/jet_nemotron.py
+++ python/sglang/srt/configs/jet_nemotron.py
@@ -255,7 +255,7 @@ class JetNemotronStateShape(Mamba2StateShape):
 @dataclass
 class JetBlockConfig:
     mode: str = "chunk"
-    expand_v: int = 2.0
+    expand_v: float = 2.0
     num_heads: int = 6
     head_dim: int = 256
     norm_eps: float = 1e-5
