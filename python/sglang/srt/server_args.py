4bb16c7080a80408f07e08f01f60577cd36da040
Nicholas <45984215+liusy58@users.noreply.github.com>
2025-11-12 14:36:49 +0800

    Update python/sglang/srt/server_args.py
    Co-authored-by: Shangming Cai <csmthu@gmail.com>

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -3615,11 +3615,10 @@ class ServerArgs:
             help="The path of the decrypted draft config file.",
         )
         parser.add_argument(
-            "--mm-encoder-tp-mode",
-            type=str,
-            choices=["weight", "data"],
-            default=ServerArgs.mm_encoder_tp_mode,
-            help="The mode to run encoder. ",
+            "--mm-enable-dp-encoder",
+            action="store_true",
+            default=ServerArgs.mm_enable_dp_encoder,
+            help="Enabling data parallelism for mm encoder. The dp size will be set to the tp size automatically."
         )
 
     @classmethod
