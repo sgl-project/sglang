20bf9ec6982fe3de5a30b2350c3b614790f355e5
陈虎 <chenhu3@xiaomi.com>
2025-11-13 09:59:26 +0800

    [PD] Adjustable dispatch node count in fake mode for ideal expert distribution

--- python/sglang/srt/eplb/expert_location_dispatch.py
+++ python/sglang/srt/eplb/expert_location_dispatch.py
@@ -12,6 +12,7 @@
 # limitations under the License.
 # ==============================================================================
 
+import random
 from dataclasses import dataclass
 from typing import Literal, Optional
 
@@ -19,6 +20,11 @@ import torch
 
 from sglang.srt.eplb.expert_location import get_global_expert_location_metadata
 from sglang.srt.server_args import get_global_server_args
+
+import os
+
+max_batch_size = int(os.getenv("MAX_BATCH_SIZE", 256))
+DUMMY_MAX_TOPK_SHAPE = (max_batch_size, 8)  # (max batch size, topk)
 
 
 @dataclass
@@ -31,6 +37,7 @@ class ExpertLocationDispatchInfo:
     # (num_logical_experts,)
     partial_logical_to_all_physical_map_num_valid: torch.Tensor
     num_physical_experts: int
+    precomputed_balanced_tensor_map: dict
 
     @classmethod
     def init_new(cls, layer_id: int):
@@ -40,6 +47,24 @@ class ExpertLocationDispatchInfo:
 
         if ep_dispatch_algorithm is None:
             return None
+
+        precomputed_balanced_tensor_map = {}
+        if ep_dispatch_algorithm == "fake":
+            nnodes = get_global_server_args().nnodes
+            node_rank = get_global_server_args().node_rank
+            device = get_global_server_args().device
+            num_physical_experts = expert_location_metadata.num_physical_experts
+            dispatch_node = get_global_server_args().ep_dispatch_fake_num_node
+            dummy_topk_ids_shape = DUMMY_MAX_TOPK_SHAPE
+            balanced_tensor = generate_balanced_expert_selection(
+                dummy_topk_ids_shape,
+                node_rank=node_rank,
+                nnodes=nnodes,
+                total_experts=num_physical_experts,
+                dispatch_node=dispatch_node,
+                device=device,
+            )
+            precomputed_balanced_tensor_map[dummy_topk_ids_shape] = balanced_tensor
 
         return cls(
             ep_dispatch_algorithm=ep_dispatch_algorithm,
@@ -58,19 +83,8 @@ class ExpertLocationDispatchInfo:
                 layer_id, :
             ],
             num_physical_experts=expert_location_metadata.num_physical_experts,
+            precomputed_balanced_tensor_map=precomputed_balanced_tensor_map,
         )
-
-
-def transform_select_experts_inputs(
-    router_logits: torch.Tensor,
-    correction_bias: Optional[torch.Tensor],
-    info: Optional[ExpertLocationDispatchInfo],
-):
-    if (info is not None) and (info.ep_dispatch_algorithm == "fake"):
-        router_logits.uniform_(5, 10)
-        if correction_bias is not None:
-            correction_bias = torch.zeros_like(correction_bias)
-    return router_logits, correction_bias
 
 
 def topk_ids_logical_to_physical(
@@ -81,8 +95,10 @@ def topk_ids_logical_to_physical(
 
     if info.ep_dispatch_algorithm == "static":
         return _topk_ids_logical_to_physical_static(topk_ids, info)
-    if info.ep_dispatch_algorithm in ["dynamic", "fake"]:
+    if info.ep_dispatch_algorithm == "dynamic":
         return _topk_ids_logical_to_physical_dynamic(topk_ids, info)
+    if info.ep_dispatch_algorithm == "fake":
+        return _topk_ids_logical_to_physical_fake(topk_ids, info)
     raise NotImplementedError(f"Unknown algorithm {info.ep_dispatch_algorithm}")
 
 
@@ -107,3 +123,63 @@ def _topk_ids_logical_to_physical_dynamic(
 
     topk_ids = topk_ids.view(topk_ids_original_shape)
     return topk_ids
+
+
+def _topk_ids_logical_to_physical_fake(
+    topk_ids: torch.Tensor, info: Optional[ExpertLocationDispatchInfo]
+) -> torch.Tensor:
+
+    topk_ids_original_shape = topk_ids.shape
+    target_len = topk_ids_original_shape[0]
+    base_shape, base_tensor = next(iter(info.precomputed_balanced_tensor_map.items()))
+    base_len = base_shape[0]
+
+    repeat_n = target_len // base_len
+    remaining = target_len % base_len
+
+    balanced_tensor = (
+        torch.cat([base_tensor.repeat((repeat_n, 1)), base_tensor[:remaining]], dim=0)
+        if target_len > 0
+        else base_tensor[:0]
+    )
+
+    topk_ids = balanced_tensor.view(topk_ids_original_shape)
+
+    return topk_ids
+
+
+def generate_balanced_expert_selection(
+    topk_ids_shape,
+    node_rank=0,
+    nnodes=1,
+    total_experts=288,
+    dispatch_node=1,
+    device="cpu",
+):
+    num_elements = topk_ids_shape[0] * topk_ids_shape[1]
+    # Number of experts per node
+    assert total_experts % nnodes == 0
+    experts_per_node = total_experts // nnodes
+
+    # Number of tokens processed by each expert
+    tokens_per_expert = num_elements // experts_per_node // dispatch_node
+    remaining_tokens_per_expert = num_elements // experts_per_node % dispatch_node
+
+    repeat = []
+    remaining_list = []
+    # Select experts from multiple nodes
+    for i in range(dispatch_node):
+        start = (node_rank + i) * experts_per_node
+        end = (node_rank + i + 1) * experts_per_node
+        base = torch.arange(start, end, device=device) % total_experts
+        if remaining_tokens_per_expert != 0:
+            remaining_list.append(base)
+        repeat.append(base.repeat(tokens_per_expert))
+    selected_experts = torch.cat(repeat)
+
+    if remaining_tokens_per_expert != 0:
+        sampled = random.sample(remaining_list, remaining_tokens_per_expert)
+        random_remaining = torch.cat(sampled)
+        selected_experts = torch.cat([selected_experts, random_remaining])
+
+    return selected_experts.view(topk_ids_shape)
