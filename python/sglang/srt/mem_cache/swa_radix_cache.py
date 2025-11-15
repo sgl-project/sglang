d9ebb930f466c3651a29e90c2c55e47d612366a3
CLFutureX <chenyongqyl@163.com>
2025-11-15 20:56:35 +0800

    Optimize the Time Complexity of Node Retrieval Operation
    Signed-off-by: CLFutureX <chenyongqyl@163.com>

--- python/sglang/srt/mem_cache/swa_radix_cache.py
+++ python/sglang/srt/mem_cache/swa_radix_cache.py
@@ -1002,10 +1002,11 @@ class SWARadixCache(BasePrefixCache):
             not node.swa_tombstone
         ), f"Invariant violated: leaf node is a tombstone, {node.id=}"
         assert len(node.children) == 0, f"leaf node has children, {node.id=}"
-        for k, v in node.parent.children.items():
-            if v == node:
-                break
-        del node.parent.children[k]
+        key = self.get_child_key_fn(node.key)
+        v = node.parent.children.get(key, None)
+        assert v == node, f"parent does not have child key, {key}"
+
+        del node.parent.children[key]
         self.full_evictable_size_ -= len(node.key)
         self.swa_evictable_size_ -= len(node.key)
 
