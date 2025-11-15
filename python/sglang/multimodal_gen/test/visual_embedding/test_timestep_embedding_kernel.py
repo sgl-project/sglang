89c92e3927181d040cb35de17b6e4cb8983aa05f
66RING <travisturing@qq.com>
2025-11-15 21:33:00 +0800

    fix dtype

--- python/sglang/multimodal_gen/test/visual_embedding/test_timestep_embedding_kernel.py
+++ python/sglang/multimodal_gen/test/visual_embedding/test_timestep_embedding_kernel.py
@@ -18,24 +18,23 @@ from sglang.multimodal_gen.runtime.layers.visual_embedding import (
 class TestTimestepEmbed(unittest.TestCase):
     NUM_BATCH = [1, 2, 8, 63, 256, 512, 613, 1024, 1536]
     NUM_DIM = [32, 64, 128, 256, 259, 512, 613, 1024, 2048, 4096]
+    DTYPE = [torch.float32, torch.bfloat16]
 
     def test_correctness(self):
         device = "cuda"
-        for B in self.NUM_BATCH:
-            for dim in self.NUM_DIM:
-                t = torch.randint(-1000, 1000, (B,), device=device, dtype=torch.int32)
-                torch_output = timestep_embedding(t, dim)
-                triton_output = timestep_embedding_triton(t, dim)
-                cuda_output = timestep_embedding_cuda(t, dim)
-                assert torch.allclose(
-                    torch_output, triton_output, atol=1e-4
-                ), f"triton ({B=}, {dim=}), Max diff {(torch_output - triton_output).abs().max()}"
-                assert torch.allclose(
-                    torch_output, cuda_output, atol=1e-4
-                ), f"cuda ({B=}, {dim=}), Max diff {(torch_output - cuda_output).abs().max()}"
-
-    def test_dtype(self):
-        pass
+        for dtype in self.DTYPE:
+            for B in self.NUM_BATCH:
+                for dim in self.NUM_DIM:
+                    t = torch.randn((B,), device=device, dtype=dtype)
+                    torch_output = timestep_embedding(t, dim)
+                    triton_output = timestep_embedding_triton(t, dim)
+                    cuda_output = timestep_embedding_cuda(t, dim)
+                    assert torch.allclose(
+                        torch_output, triton_output, atol=1e-4
+                    ), f"triton ({B=}, {dim=}), Max diff {(torch_output - triton_output).abs().max()}"
+                    assert torch.allclose(
+                        torch_output, cuda_output, atol=1e-4
+                    ), f"cuda ({B=}, {dim=}), Max diff {(torch_output - cuda_output).abs().max()}"
 
     def test_perf(self):
         def perf_kernel_fn(kernel_fn: callable, *args, **kwargs):
@@ -66,7 +65,7 @@ class TestTimestepEmbed(unittest.TestCase):
         cuda_speedups = []
         for B in self.NUM_BATCH:
             for dim in self.NUM_DIM:
-                t = torch.randint(-1000, 1000, (B,), device=device, dtype=torch.int32)
+                t = torch.randn((B,), device=device)
 
                 time_torch = perf_kernel_fn(timestep_embedding, t, dim)
                 time_triton = perf_kernel_fn(timestep_embedding_triton, t, dim)
@@ -81,8 +80,8 @@ class TestTimestepEmbed(unittest.TestCase):
                         "Torch Time (ms)": time_torch,
                         "Triton Time (ms)": time_triton,
                         "CUDA Time (ms)": time_cuda,
-                        "Speedup (Torch/Triton)": speedup_triton,
-                        "Speedup (Torch/CUDA)": speedup_cuda,
+                        "Speedup (Triton)": speedup_triton,
+                        "Speedup (CUDA)": speedup_cuda,
                     }
                 )
                 triton_speedups.append(speedup_triton)
