d368c7451a48f2c58aa19e6f20a5725296094551
lixiaolx <lixiao31@baidu.com>
2025-11-17 12:12:25 +0800

    (1/n)support context parallel with deepseekv3.2-DSA (#12065)

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -380,6 +380,7 @@ suites = {
         TestFile("test_w8a8_quantization.py"),
         TestFile("test_wave_attention_backend.py"),
         TestFile("test_weight_version.py"),
+        TestFile("test_deepseek_v32_cp_single_node.py", 275),
     ],
 }
 
