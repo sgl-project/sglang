317690a832d0518bb22118a81a1b9b8c64b95048
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2025-11-18 16:27:30 +0800

    Update run_suite.py

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -520,6 +520,7 @@ suite_ascend = {
         TestFile("ascend/test_ascend_tp1_bf16.py", 400),
         TestFile("ascend/test_ascend_hicache_mha.py", 400),
         TestFile("ascend/test_ascend_sampling_backend.py", 400),
+        TestFile("models/test_embedding_models.py", 400),
     ],
     "per-commit-2-ascend-npu": [
         TestFile("ascend/test_ascend_graph_tp2_bf16.py", 400),
@@ -530,6 +531,7 @@ suite_ascend = {
     "per-commit-4-ascend-npu": [
         TestFile("ascend/test_ascend_mla_w8a8int8.py", 400),
         TestFile("ascend/test_ascend_tp4_bf16.py", 400),
+        TestFile("ascend/test_vlm_models_deepseek_vl2.py", 400),
     ],
     "per-commit-16-ascend-a3": [
         TestFile("ascend/test_ascend_deepep.py", 400),
