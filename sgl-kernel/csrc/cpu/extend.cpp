3c0c0d0c90849c7b0792a14633dde974e95351f2
Zheng, Beilei <beilei.zheng@intel.com>
2025-11-18 23:16:15 -0800

    refactor pack_vnni and pack_vnni2

--- sgl-kernel/csrc/cpu/extend.cpp
+++ sgl-kernel/csrc/cpu/extend.cpp
@@ -22,6 +22,7 @@ template <typename scalar_t, typename index_t>
 inline void pack_vnni_Nx32(
     scalar_t* __restrict__ dst,
     const scalar_t* __restrict__ src,
+    const float* __restrict__ src_scale,
     const index_t* __restrict__ ind,
     int N,
     int ld_src,
@@ -91,6 +92,7 @@ template <typename scalar_t, typename index_t>
 inline void pack_vnni_Kx32(
     scalar_t* __restrict__ dst,
     const scalar_t* __restrict__ src,
+    const float* __restrict__ src_scale,
     const index_t* __restrict__ ind,
     int K,
     int ld_src,
@@ -155,50 +157,10 @@ inline void pack_vnni_Kx32(
 
 // convert to vnni format
 // from [N, K/2, 2] to [K/2, N, 2] for bfloat16 and float16
-template <typename scalar_t, typename index_t>
+template <typename scalar_t, typename kvcache_t, typename index_t>
 void pack_vnni(
     scalar_t* __restrict__ dst,
-    const scalar_t* __restrict__ src,
-    const float* __restrict__ src_scale,
-    const index_t* __restrict__ ind,
-    int N,
-    int K,
-    int ld_src,
-    int ld_dst) {
-#if defined(CPU_CAPABILITY_AVX512)
-  const int NB = div_up(N, 16);
-  const int KB = K / 32;  // no remainder
-  const bool is_indexed = ind != nullptr;
-
-  for (int nb = 0; nb < NB; ++nb) {
-    for (int kb = 0; kb < KB; ++kb) {
-      // handle 16x512bits each block
-      int nb_size = std::min(N - nb * 16, 16);
-      pack_vnni_Nx32<scalar_t, index_t>(
-          /*    dst */ dst + ((kb * 32) >> 1) * ld_dst * 2 + nb * 16 * 2,
-          /*    src */ src + kb * 32 + (is_indexed ? 0 : nb * 16 * ld_src),
-          /*    ind */ is_indexed ? ind + nb * 16 : nullptr,
-          /*      N */ nb_size,
-          /* ld_src */ ld_src,
-          /* ld_dst */ ld_dst);
-    }
-  }
-#else
-  for (int n = 0; n < N; ++n) {
-    index_t index = get_index(ind, n);
-    for (int k = 0; k < K / 2; ++k) {
-      for (int d = 0; d < 2; ++d) {
-        dst[k * ld_dst * 2 + n * 2 + d] = src[index * ld_src + k * 2 + d];
-      }
-    }
-  }
-#endif
-}
-
-template <typename scalar_t, typename index_t>
-void pack_vnni(
-    scalar_t* __restrict__ dst,
-    const at::Float8_e4m3fn* __restrict__ src,
+    const kvcache_t* __restrict__ src,
     const float* __restrict__ src_scale,
     const index_t* __restrict__ ind,
     int N,
@@ -227,9 +189,10 @@ void pack_vnni(
 #else
   for (int n = 0; n < N; ++n) {
     index_t index = get_index(ind, n);
+    float scale = src_scale != nullptr ? src_scale[index] : 1.0f;
     for (int k = 0; k < K / 2; ++k) {
       for (int d = 0; d < 2; ++d) {
-        dst[k * ld_dst * 2 + n * 2 + d] = src[index * ld_src + k * 2 + d] * src_scale[index];
+        dst[k * ld_dst * 2 + n * 2 + d] = src[index * ld_src + k * 2 + d] * scale;
       }
     }
   }
@@ -238,59 +201,10 @@ void pack_vnni(
 
 // convert to vnni format
 // from [K/2, 2, N] to [K/2, N, 2] for bfloat16 and float16
-template <typename scalar_t, typename index_t>
+template <typename scalar_t, typename kvcache_t, typename index_t>
 void pack_vnni2(
     scalar_t* __restrict__ dst,
-    const scalar_t* __restrict__ src,
-    const float* __restrict__ src_scale,
-    const index_t* __restrict__ ind,
-    int K,
-    int N,
-    int ld_src,
-    int ld_dst) {
-#if defined(CPU_CAPABILITY_AVX512)
-  const int KB = div_up(K, 2);
-  const int NB = N / 32;  // no remainder
-  const bool is_indexed = ind != nullptr;
-
-  for (int kb = 0; kb < KB; ++kb) {
-    for (int nb = 0; nb < NB; ++nb) {
-      // handle 2x512bits each block
-      int kb_size = std::min(K - kb * 2, 2);
-      pack_vnni_Kx32<scalar_t, index_t>(
-          /*    dst */ dst + ((kb * 2) >> 1) * ld_dst * 2 + nb * 32 * 2,
-          /*    src */ src + (is_indexed ? 0 : kb * 2 * ld_src) + nb * 32,
-          /*    ind */ is_indexed ? ind + kb * 2 : nullptr,
-          /*      K */ kb_size,
-          /* ld_src */ ld_src,
-          /* ld_dst */ ld_dst);
-    }
-  }
-#else
-  int k = 0;
-  for (; k < (K >> 1) * 2; k += 2) {
-    index_t index0 = get_index(ind, k + 0);
-    index_t index1 = get_index(ind, k + 1);
-    for (int n = 0; n < N; ++n) {
-      dst[(k >> 1) * ld_dst * 2 + n * 2 + 0] = src[index0 * ld_src + n];
-      dst[(k >> 1) * ld_dst * 2 + n * 2 + 1] = src[index1 * ld_src + n];
-    }
-  }
-  if (K % 2 != 0) {
-    index_t index = get_index(ind, K - 1);
-    for (int n = 0; n < N; ++n) {
-      dst[(K >> 1) * ld_dst * 2 + n * 2 + 0] = src[index * ld_src + n];
-      dst[(K >> 1) * ld_dst * 2 + n * 2 + 1] = 0;
-    }
-    k += 2;
-  }
-#endif
-}
-
-template <typename scalar_t, typename index_t>
-void pack_vnni2(
-    scalar_t* __restrict__ dst,
-    const at::Float8_e4m3fn* __restrict__ src,
+    const kvcache_t* __restrict__ src,
     const float* __restrict__ src_scale,
     const index_t* __restrict__ ind,
     int K,
@@ -321,15 +235,18 @@ void pack_vnni2(
   for (; k < (K >> 1) * 2; k += 2) {
     index_t index0 = get_index(ind, k + 0);
     index_t index1 = get_index(ind, k + 1);
+    float scale0 = src_scale != nullptr ? src_scale[index0] : 1.0f;
+    float scale1 = src_scale != nullptr ? src_scale[index1] : 1.0f;
     for (int n = 0; n < N; ++n) {
-      dst[(k >> 1) * ld_dst * 2 + n * 2 + 0] = src[index0 * ld_src + n] * src_scale[index0];
-      dst[(k >> 1) * ld_dst * 2 + n * 2 + 1] = src[index1 * ld_src + n] * src_scale[index1];
+      dst[(k >> 1) * ld_dst * 2 + n * 2 + 0] = src[index0 * ld_src + n] * scale0;
+      dst[(k >> 1) * ld_dst * 2 + n * 2 + 1] = src[index1 * ld_src + n] * scale1;
     }
   }
   if (K % 2 != 0) {
     index_t index = get_index(ind, K - 1);
+    float scale = src_scale != nullptr ? src_scale[index] : 1.0f;
     for (int n = 0; n < N; ++n) {
-      dst[(K >> 1) * ld_dst * 2 + n * 2 + 0] = src[index * ld_src + n] * src_scale[index];
+      dst[(K >> 1) * ld_dst * 2 + n * 2 + 0] = src[index * ld_src + n] * scale;
       dst[(K >> 1) * ld_dst * 2 + n * 2 + 1] = 0;
     }
     k += 2;
@@ -515,7 +432,7 @@ void extend_attention_kernel_impl(
         const int padded_n_size = div_up(n_size, TILE_K) * TILE_K;
 
         // get key and pack
-        pack_vnni<scalar_t, index_t>(
+        pack_vnni<scalar_t, kvcache_t, index_t>(
             /*    dst */ Btmp,
             /*    src */ k_buffer + head_kv_id * k_strideH,
             /* src_scale*/ k_buf_scale,
@@ -585,7 +502,7 @@ void extend_attention_kernel_impl(
         }
 
         // get value and pack
-        pack_vnni2<scalar_t, index_t>(
+        pack_vnni2<scalar_t, kvcache_t, index_t>(
             /*    dst */ Btmp,
             /*    src */ v_buffer + head_kv_id * v_strideH,
             /* src_scale*/ v_buf_scale,
@@ -618,7 +535,7 @@ void extend_attention_kernel_impl(
         const int padded_n_size = div_up(n_size, TILE_K) * TILE_K;
 
         // get key and pack
-        pack_vnni<scalar_t, index_t>(
+        pack_vnni<scalar_t, scalar_t, index_t>(
             /*    dst */ Btmp,
             /*    src */ k_extend + (seq_extend_start_loc + n) * ke_strideN + head_kv_id * ke_strideH,
             /* src_scale*/ nullptr,
@@ -698,7 +615,7 @@ void extend_attention_kernel_impl(
         }
 
         // get value and pack
-        pack_vnni2<scalar_t, index_t>(
+        pack_vnni2<scalar_t, scalar_t, index_t>(
             /*    dst */ Btmp,
             /*    src */ v_extend + (seq_extend_start_loc + n) * ve_strideN + head_kv_id * ve_strideH,
             /* src_scale*/ nullptr,
