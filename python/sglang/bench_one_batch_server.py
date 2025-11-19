a3aec4f1cc60875be8b5b0ab76c4769466ca41e5
Kumar, Kalyan <kalyan.kumar@intel.com>
2025-11-19 04:53:26 +0000

    Added pytorch profiling to bench_one_batch_server and also to capture required number of steps

--- python/sglang/bench_one_batch_server.py
+++ python/sglang/bench_one_batch_server.py
@@ -56,6 +56,8 @@ class BenchArgs:
     skip_warmup: bool = False
     show_report: bool = False
     profile: bool = False
+    profile_activities: Tuple[str] = ("CPU", "GPU")
+    profile_start_step: Optional[int] = None
     profile_steps: int = 5
     profile_by_stage: bool = False
     profile_prefix: Optional[str] = None
@@ -96,6 +98,20 @@ class BenchArgs:
         parser.add_argument("--skip-warmup", action="store_true")
         parser.add_argument("--show-report", action="store_true")
         parser.add_argument("--profile", action="store_true")
+        parser.add_argument(
+            "--profile-activities",
+            type=str,
+            nargs="+",
+            default=("CPU", "GPU"),
+            choices=["CPU", "GPU", "XPU"],
+            help="Profiler activities: CPU, GPU, XPU. use torch profiler.",
+        )
+        parser.add_argument(
+            "--profile-start-step",
+            type=int,
+            default=BenchArgs.profile_start_step,
+            help="Start profiling after this many forward steps. Useful for warmup.",
+        )
         parser.add_argument(
             "--profile-steps", type=int, default=BenchArgs.profile_steps
         )
@@ -225,6 +241,8 @@ def run_one_case(
     result_filename: str,
     tokenizer: PreTrainedTokenizer | AutoProcessor,
     profile: bool = False,
+    profile_activities: Tuple[str] = ("CPU", "GPU"),
+    profile_start_step: Optional[int] = None,
     profile_steps: int = BenchArgs.profile_steps,
     profile_by_stage: bool = False,
     profile_prefix: Optional[str] = BenchArgs.profile_prefix,
@@ -302,10 +320,11 @@ def run_one_case(
         profile_link: str = run_profile(
             url=url,
             num_steps=profile_steps,
-            activities=["CPU", "GPU"],
+            profile_activities=profile_activities,
             output_dir=profile_output_dir,
             profile_by_stage=profile_by_stage,
             profile_prefix=profile_prefix,
+            start_step=profile_start_step,
         )
 
     # Run the request
@@ -562,6 +581,8 @@ def run_benchmark(server_args: ServerArgs, bench_args: BenchArgs):
                             dataset_path=bench_args.dataset_path,
                             parallel_batch=bench_args.parallel_batch,
                             profile=bench_args.profile,
+                            profile_activities=bench_args.profile_activities,
+                            profile_start_step=bench_args.profile_start_step,
                             profile_steps=bench_args.profile_steps,
                             profile_by_stage=bench_args.profile_by_stage,
                             profile_prefix=profile_prefix,
