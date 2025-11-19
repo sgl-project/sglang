5e8dcdf1003aaddcbcc5eba9eab7fcc428a669d9
Tianyu Guo <guoty9@mail2.sysu.edu.cn>
2025-11-19 14:20:27 +0000

    Revert changes

--- python/sglang/srt/multimodal/processors/gemma3n.py
+++ python/sglang/srt/multimodal/processors/gemma3n.py
@@ -31,7 +31,6 @@ class Gemma3nSGLangProcessor(SGLangBaseProcessor):
 
         self.IM_START_TOKEN_ID = hf_config.boi_token_id
         self.IM_END_TOKEN_ID = hf_config.eoi_token_id
-        self.IM_TOKEN_ID = hf_config.image_token_id
 
         self.AUDIO_START_TOKEN_ID = hf_config.boa_token_id
         self.AUDIO_END_TOKEN_ID = hf_config.eoa_token_id
