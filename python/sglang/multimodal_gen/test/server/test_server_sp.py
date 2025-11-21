c9374c69602df97b84b41bdfd498604e6ea06cc9
yhyang201 <yhyang201@gmail.com>
2025-11-21 14:02:23 +0000

    upd

--- python/sglang/multimodal_gen/test/server/test_server_sp.py
+++ python/sglang/multimodal_gen/test/server/test_server_sp.py
@@ -20,74 +20,13 @@ from sglang.multimodal_gen.test.server.test_server_utils import (
     WarmupRunner,
     download_image_from_url,
 )
-from sglang.multimodal_gen.test.server.testcase_configs import DiffusionTestCase
+from sglang.multimodal_gen.test.server.testcase_configs import (
+    SP_CASES,
+    DiffusionTestCase,
+)
 from sglang.multimodal_gen.test.test_utils import get_dynamic_server_port
 
 logger = init_logger(__name__)
-
-SP_CASES = [
-    DiffusionTestCase(
-        id="wan2_2_i2v_a14b",
-        model_path="Wan-AI/Wan2.2-I2V-A14B-Diffusers",
-        modality="video",
-        prompt="generate",  # passing in something since failing if no prompt is passed
-        warmup_text=0,  # warmups only for image gen models
-        warmup_edit=0,
-        output_size="832x1104",
-        edit_prompt="generate",
-        image_path="https://github.com/Wan-Video/Wan2.2/blob/990af50de458c19590c245151197326e208d7191/examples/i2v_input.JPG?raw=true",
-        custom_validator="video",
-        seconds=1,
-    ),
-    DiffusionTestCase(
-        id="wan2_2_t2v_a14b",
-        model_path="Wan-AI/Wan2.2-T2V-A14B-Diffusers",
-        modality="video",
-        prompt="A curious raccoon",
-        output_size="720x480",
-        seconds=4,
-        warmup_text=0,
-        warmup_edit=0,
-        custom_validator="video",
-    ),
-    DiffusionTestCase(
-        id="wan2_1_t2v_14b",
-        model_path="Wan-AI/Wan2.1-T2V-14B-Diffusers",
-        modality="video",
-        prompt="A curious raccoon",
-        output_size="720x480",
-        seconds=4,
-        warmup_text=0,
-        warmup_edit=0,
-        custom_validator="video",
-    ),
-    DiffusionTestCase(
-        id="wan2_1_i2v_14b_480P",
-        model_path="Wan-AI/Wan2.1-I2V-14B-480P-Diffusers",
-        output_size="832x1104",
-        modality="video",
-        prompt="Animate this image",
-        edit_prompt="Add dynamic motion to the scene",
-        image_path="https://github.com/lm-sys/lm-sys.github.io/releases/download/test/TI2I_Qwen_Image_Edit_Input.jpg",
-        warmup_text=0,  # warmups only for image gen models
-        warmup_edit=0,
-        custom_validator="video",
-        seconds=1,
-    ),
-    DiffusionTestCase(
-        id="wan2_1_i2v_14b_720P",
-        model_path="Wan-AI/Wan2.1-I2V-14B-720P-Diffusers",
-        modality="video",
-        prompt="Animate this image",
-        edit_prompt="Add dynamic motion to the scene",
-        image_path="https://github.com/lm-sys/lm-sys.github.io/releases/download/test/TI2I_Qwen_Image_Edit_Input.jpg",
-        output_size="832x1104",
-        warmup_text=0,  # warmups only for image gen models
-        warmup_edit=0,
-        custom_validator="video",
-        seconds=1,
-    ),
-]
 
 
 @pytest.fixture(params=SP_CASES, ids=lambda c: c.id)
@@ -104,7 +43,7 @@ def diffusion_server(case: DiffusionTestCase):
 
     # Append the required arguments
     extra_args = os.environ.get("SGLANG_TEST_SERVE_ARGS", "")
-    extra_args += " --num-gpus 2 --ulysses-degree 2"
+    extra_args += f" --num-gpus {case.num_gpus} --ulysses-degree {case.num_gpus}"
 
     # start server
     manager = ServerManager(
