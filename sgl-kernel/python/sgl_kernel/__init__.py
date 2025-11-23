c89bdda6a434bfee1425bc45809b96e1d23f44d3
BBuf <1182563586@qq.com>
2025-11-23 16:30:20 +0800

    ud

--- sgl-kernel/python/sgl_kernel/__init__.py
+++ sgl-kernel/python/sgl_kernel/__init__.py
@@ -83,6 +83,7 @@ from sgl_kernel.marlin import (
 from sgl_kernel.memory import set_kv_buffer_kernel, weak_ref_tensor
 from sgl_kernel.moe import (
     apply_shuffle_mul_sum,
+    batched_moe_align_block_size,
     cutlass_fp4_group_mm,
     fp8_blockwise_scaled_grouped_mm,
     kimi_k2_moe_fused_gate,
