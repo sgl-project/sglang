13efca8eb3221b93c7278b29bdfd9bb46808e919
Cherry_ming <136634645@qq.com>
2025-11-24 18:27:30 +0800

    Create test_vlm_models_janus_pro_1b.py

---
+++ test/srt/ascend/test_vlm_models_janus_pro_1b.py
@@ -0,0 +1,29 @@
+import argparse
+import glob
+import json
+import os
+import random
+import subprocess
+import sys
+import unittest
+from types import SimpleNamespace
+
+from sglang.test.test_vlm_utils import TestVLMModels
+
+
+class TestJanus1Models(TestVLMModels):
+    models = [
+        SimpleNamespace(
+            model="/root/.cache/modelscope/hub/models/deepseek-ai/Janus-Pro-1B",
+            mmmu_accuracy=0.2,
+        ),
+    ]
+    tp_size = 4
+    mem_fraction_static = 0.35
+
+    def test_vlm_mmmu_benchmark(self):
+        self.vlm_mmmu_benchmark()
+
+
+if __name__ == "__main__":
+    unittest.main()
