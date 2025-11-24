7ab6babc7ad7771845f0ff8f2195b051a481f62b
Cherry_ming <136634645@qq.com>
2025-11-24 18:30:29 +0800

    Create test_vlm_models_gemma_3_4b_it.py

---
+++ test/srt/ascend/test_vlm_models_gemma_3_4b_it.py
@@ -0,0 +1,29 @@
+import argparse
+import glob
+import json
+import os
+import random
+import subprocess
+import sys
+import unittest
+from types import SimpleNamespace
+
+from sglang.test.test_vlm_utils import TestVLMModels
+
+
+class TestGemmaModels(TestVLMModels):
+    models = [
+        SimpleNamespace(
+            model="/root/.cache/modelscope/hub/models/google/gemma-3-4b-it",
+            mmmu_accuracy=0.2,
+        ),
+    ]
+    tp_size = 4
+    mem_fraction_static = 0.35
+
+    def test_vlm_mmmu_benchmark(self):
+        self.vlm_mmmu_benchmark()
+
+
+if __name__ == "__main__":
+    unittest.main()
