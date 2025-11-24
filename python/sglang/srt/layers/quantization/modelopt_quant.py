701c03993ea84f252817f748983ea12d298111b3
ErsongWang <Eason.Wang@amd.com>
2025-11-24 17:19:21 +0800

    Check SM120 only if the device is CUDA

--- python/sglang/srt/layers/quantization/modelopt_quant.py
+++ python/sglang/srt/layers/quantization/modelopt_quant.py
@@ -59,7 +59,7 @@ if TYPE_CHECKING:
     from sglang.srt.single_batch_overlap import DownGemmOverlapArgs
 
 try:
-    if is_sm120_supported():
+    if is_cuda() and is_sm120_supported():
         from flashinfer import fp4_quantize
     else:
         from sgl_kernel import scaled_fp4_quant as fp4_quantize
