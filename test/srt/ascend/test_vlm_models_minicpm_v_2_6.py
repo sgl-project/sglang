08c808d68b746081c44ce4c1339d5938fe73f129
Cherry_ming <136634645@qq.com>
2025-11-24 18:29:44 +0800

    Create test_vlm_models_minicpm_v_2_6.py

---
+++ test/srt/ascend/test_vlm_models_minicpm_v_2_6.py
@@ -0,0 +1,29 @@
+import argparse
+import glob
+import json
+import os
+import random
+import subprocess
+import sys
+import unittest
+from types import SimpleNamespace
+
+from sglang.test.test_vlm_utils import TestVLMModels
+
+
+class TestMiniCPMvModels(TestVLMModels):
+    models = [
+        SimpleNamespace(
+            model="/root/.cache/modelscope/hub/models/openbmb/MiniCPM-V-2_6",
+            mmmu_accuracy=0.2,
+        ),
+    ]
+    tp_size = 4
+    mem_fraction_static = 0.35
+
+    def test_vlm_mmmu_benchmark(self):
+        self.vlm_mmmu_benchmark()
+
+
+if __name__ == "__main__":
+    unittest.main()
