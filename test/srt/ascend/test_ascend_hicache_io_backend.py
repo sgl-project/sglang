4c05d1b436b607e1cc9755c1d55c2dc6acffa5d9
Cherry_ming <136634645@qq.com>
2025-11-26 23:29:08 +0800

    Create test_ascend_hicache_io_backend.py

---
+++ test/srt/ascend/test_ascend_hicache_io_backend.py
@@ -0,0 +1,64 @@
+import unittest
+
+import requests
+
+from sglang.srt.utils import is_npu, kill_process_tree
+from sglang.test.test_utils import (
+    DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+
+class TestHicacheIoBackend(CustomTestCase):
+    def test_hicache_io_backend(self):
+        other_args = (
+            [
+                "--hicache-io-backend",
+                "direct",
+                "--attention-backend",
+                "ascend",
+                "--disable-cuda-graph",
+            ]
+            if is_npu()
+            else [
+                "--hicache-io-backend",
+                "direct",
+            ]
+        )
+        process = popen_launch_server(
+            (
+                "/root/.cache/modelscope/hub/models/LLM-Research/Llama-3.2-1B"
+                if is_npu()
+                else DEFAULT_SMALL_MODEL_NAME_FOR_TEST
+            ),
+            DEFAULT_URL_FOR_TEST,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+        )
+        response = requests.get(f"{DEFAULT_URL_FOR_TEST}/health_generate")
+        self.assertEqual(response.status_code, 200)
+
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Paris", response.text)
+        response = requests.get(DEFAULT_URL_FOR_TEST + "/get_server_info")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["hicache_io_backend"], "direct")
+        kill_process_tree(process.pid)
+
+
+if __name__ == "__main__":
+
+    unittest.main()
