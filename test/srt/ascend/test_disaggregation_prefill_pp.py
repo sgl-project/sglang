3f8e20bae21384ddf848691623ee0ce5c8ca1ec0
Cherry_ming <136634645@qq.com>
2025-11-27 00:42:22 +0800

    Update test_disaggregation_prefill_pp.py

--- test/srt/ascend/test_disaggregation_prefill_pp.py
+++ test/srt/ascend/test_disaggregation_prefill_pp.py
@@ -1,19 +1,14 @@
-import json
 import os
-import time
 import unittest
-from types import SimpleNamespace
 
 import requests
 
-from sglang.test.few_shot_gsm8k import run_eval as run_eval_few_shot_gsm8k
 from sglang.test.test_disaggregation_utils import TestDisaggregationBase
 from sglang.test.test_utils import (
     DEFAULT_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     is_npu,
-    kill_process_tree,
     popen_launch_pd_server,
 )
 
