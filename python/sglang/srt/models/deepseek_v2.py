60283f2d8af9ba8aea370c7cd9b856d090f7f869
Kangyan Zhou <zky314343421@gmail.com>
2025-11-25 18:06:21 -0800

    Fix Deepseek v3.1 loading issue

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -3562,13 +3562,12 @@ class DeepseekV2ForCausalLM(nn.Module):
                         weight_scale = self_attn.kv_b_proj.weight_scale_inv
 
                     # In multiple weight loading scenarios (e.g. RL), we need to inverse the scale of the weights after the requantization happened at the first loading.
-                    if (
-                        should_deepgemm_weight_requant_ue8m0(
-                            weight_block_size=getattr(
-                                self.quant_config, "weight_block_size", None
-                            )
+                    if should_deepgemm_weight_requant_ue8m0(
+                        weight_block_size=getattr(
+                            self.quant_config, "weight_block_size", None
                         )
-                        and self_attn.kv_b_proj.executed_weight_requant_ue8m0
+                    ) and getattr(
+                        self_attn.kv_b_proj, "executed_weight_requant_ue8m0", False
                     ):
                         weight_scale = inverse_transform_scale_ue8m0(
                             weight_scale, mn=weight.shape[-2]
