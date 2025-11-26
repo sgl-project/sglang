68acd9125febc0986aa4d7de9cb6f86871ac3209
Cherry_ming <136634645@qq.com>
2025-11-26 11:17:04 +0800

    Update test_ascend_lora_backend.py

--- test/srt/ascend/test_ascend_lora_backend.py
+++ test/srt/ascend/test_ascend_lora_backend.py
@@ -4,7 +4,6 @@ import requests
 
 from sglang.srt.utils import is_npu, kill_process_tree
 from sglang.test.test_utils import (
-    DEFAULT_MODEL_NAME_FOR_TEST,
     DEFAULT_SMALL_MODEL_NAME_FOR_TEST
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
