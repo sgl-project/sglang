8f225d5c4f43bc0835fcb1b0c654a17f4b9505b2
Cherry_ming <136634645@qq.com>
2025-11-26 11:18:49 +0800

    Update test_enable_multimodal_non_mlm.py

--- test/srt/ascend/test_enable_multimodal_non_mlm.py
+++ test/srt/ascend/test_enable_multimodal_non_mlm.py
@@ -6,7 +6,6 @@ import requests
 from sglang.srt.utils import kill_process_tree
 from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
-    DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
