b2b98fb53d0d383aeb8dbcfc3d2ccede2f7c8c4d
Cherry_ming <136634645@qq.com>
2025-11-26 11:18:24 +0800

    Update test_ascend_warmups.py

--- test/srt/ascend/test_ascend_warmups.py
+++ test/srt/ascend/test_ascend_warmups.py
@@ -1,13 +1,10 @@
 import os
 import unittest
-from types import SimpleNamespace
 
 import requests
 
 from sglang.srt.utils import is_npu, kill_process_tree
-from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
-    DEFAULT_SMALL_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
