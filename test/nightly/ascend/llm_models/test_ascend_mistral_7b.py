a579a468f7055d5242babc7eaa435ad2e02937fa
Cherry_ming <136634645@qq.com>
2025-11-29 17:08:51 +0800

    Create test_ascend_mistral_7b.py

---
+++ test/nightly/ascend/llm_models/test_ascend_mistral_7b.py
@@ -0,0 +1,9 @@
+import unittest
+
+from gsm8k_ascend_mixin import GSM8KAscendMixin
+from sglang.test.test_utils import CustomTestCase
+
+
+class TestMistral7B(GSM8KAscendMixin, CustomTestCase):
+  model = "/root/.cache/modelscope/hub/models/mistralai/Mistral-7B-Instruct-v0.2"
+  accuracy = 0.00
