c3a850ae169e98a62411e931c5b11e3bf44fb798
Cherry_ming <136634645@qq.com>
2025-11-29 17:26:38 +0800

    Update test_ascend_glm4_9b_chat.py

--- test/nightly/ascend/llm_models/test_ascend_glm4_9b_chat.py
+++ test/nightly/ascend/llm_models/test_ascend_glm4_9b_chat.py
@@ -1,7 +1,10 @@
 import unittest
 
 from gsm8k_ascend_mixin import GSM8KAscendMixin
+from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
+
+register_npu_ci(est_time=400, suite="nightly-1-npu", nightly=True)
 
 
 class TestGLM49BChat(GSM8KAscendMixin, CustomTestCase):
