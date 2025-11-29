9b3566c6f6517f510074f1a66a298770d57f53b6
fzyzcjy <ch271828n@outlook.com>
2025-11-29 21:14:25 +0800

    cp

--- python/sglang/srt/environ.py
+++ python/sglang/srt/environ.py
@@ -143,6 +143,7 @@ class Envs:
     SGLANG_SET_CPU_AFFINITY = EnvBool(False)
     SGLANG_PROFILE_WITH_STACK = EnvBool(True)
     SGLANG_PROFILE_RECORD_SHAPES = EnvBool(True)
+    SGLANG_PROFILE_V2 = EnvBool(False)
     SGLANG_RECORD_STEP_TIME = EnvBool(False)
     SGLANG_FORCE_SHUTDOWN = EnvBool(False)
     SGLANG_DEBUG_MEMORY_POOL = EnvBool(False)
