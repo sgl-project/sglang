e8fbd0d501914a7e0c4c4bb8e71828295b0c4ae8
BBuf <1182563586@qq.com>
2025-11-29 13:57:58 +0800

    add moe_wna16_marlin_gemm_v2

--- sgl-kernel/csrc/moe/marlin_moe_wna16/ops.cu
+++ sgl-kernel/csrc/moe/marlin_moe_wna16/ops.cu
@@ -24,6 +24,7 @@
 #endif
 
 #include "kernel.h"
+#include "marlin_template.h"
 #include "kernel_marlin.cuh"
 
 #define STATIC_ASSERT_SCALAR_TYPE_VALID(scalar_t)                                        \
