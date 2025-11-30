f9322d54232158c909814c6ea94849a6b622f901
Cherry_ming <136634645@qq.com>
2025-11-30 22:15:29 +0800

    Update test_ascend_reward_models.py

--- test/nightly/ascend/reward_models/test_ascend_reward_models.py
+++ test/nightly/ascend/reward_models/test_ascend_reward_models.py
@@ -10,7 +10,11 @@ from sglang.test.test_utils import CustomTestCase
 register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 MODELS = [
-    ("/root/.cache/modelscope/hub/models/Shanghai_AI_Laboratory/internlm2-7b-reward", 1, 4e-2),
+    (
+        "/root/.cache/modelscope/hub/models/Shanghai_AI_Laboratory/internlm2-7b-reward",
+        1,
+        4e-2
+    ),
     ("/root/.cache/modelscope/hub/models/Howeee/Qwen2.5-1.5B-apeach", 1, 4e-2),
 ]
 TORCH_DTYPES = [torch.float16]
