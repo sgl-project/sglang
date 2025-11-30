9786923b71fb22502a4f3ea13b28ca6711895bbc
Cherry_ming <136634645@qq.com>
2025-11-30 22:11:54 +0800

    Update gsm8k_ascend_mixin.py

--- test/nightly/ascend/llm_models/gsm8k_ascend_mixin.py
+++ test/nightly/ascend/llm_models/gsm8k_ascend_mixin.py
@@ -1,15 +1,12 @@
-from abc import ABC
 import os
-import unittest
+from abc import ABC
 from types import SimpleNamespace
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.few_shot_gsm8k import run_eval
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
-    CustomTestCase,
     popen_launch_server,
 )
 
