a60e2ddcbf5b0990234176b4d4219f8ef0dafa1a
Tony Lu <tonylu@linux.alibaba.com>
2025-12-01 14:27:15 +0800

    [model-gateway] mesh: fix maturin build
    Signed-off-by: Tony Lu <tonylu@linux.alibaba.com>
    Signed-off-by: Kun(llfl) <i@imux.top>

--- sgl-model-gateway/bindings/python/src/lib.rs
+++ sgl-model-gateway/bindings/python/src/lib.rs
@@ -459,6 +459,8 @@ impl Router {
                 prefill_selector: self.prefill_selector.clone(),
                 decode_selector: self.decode_selector.clone(),
                 bootstrap_port_annotation: self.bootstrap_port_annotation.clone(),
+                router_selector: HashMap::new(),
+                router_mesh_port_annotation: "sglang.ai/mesh-port".to_string(),
             })
         } else {
             None
@@ -863,6 +865,8 @@ impl Router {
                 prefill_selector: self.prefill_selector.clone(),
                 decode_selector: self.decode_selector.clone(),
                 bootstrap_port_annotation: self.bootstrap_port_annotation.clone(),
+                router_selector: HashMap::new(),
+                router_mesh_port_annotation: "sglang.ai/mesh-port".to_string(),
             })
         } else {
             None
@@ -897,6 +901,7 @@ impl Router {
                     .control_plane_auth
                     .as_ref()
                     .map(|c| c.to_auth_control_plane_config()),
+                mesh_server_config: None,
             })
             .await
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))
