def9abac121ee1193d45ab78397abe8e80f6558d
Zheng, Beilei <beilei.zheng@intel.com>
2025-12-01 00:57:19 -0800

    fix ut

--- test/srt/cpu/test_mla.py
+++ test/srt/cpu/test_mla.py
@@ -107,6 +107,8 @@ class TestMLA(CustomTestCase):
             q,
             k_buffer2,
             v_buffer2,
+            None,
+            None,
             o,
             key,
             value,
