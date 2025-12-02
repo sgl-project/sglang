5063ee07258fd600a544a7bd1c5fc3f9659aae8c
Yutao Xu <xytpai@foxmail.com>
2025-12-03 00:14:58 +0800

    Support local rank in allreduce fusion kernel

--- python/sglang/srt/layers/aiter_comm_fusion.py
+++ python/sglang/srt/layers/aiter_comm_fusion.py
@@ -4,58 +4,37 @@ from typing import Tuple
 import aiter
 import torch
 import torch.distributed as dist
-from aiter.dist.communication_op import (
-    tensor_model_parallel_all_reduce,
-    tensor_model_parallel_fused_allreduce_rmsnorm,
-)
-
-from aiter.dist.parallel_state import (
-    ensure_model_parallel_initialized,
-    init_distributed_environment,
-    set_custom_all_reduce,
-    get_tp_group,
-    graph_capture,
-    destroy_model_parallel,
-    destroy_distributed_environment,
-)
 
 from sglang.srt.distributed import get_tensor_model_parallel_world_size
 from sglang.srt.utils import direct_register_custom_op
+from sglang.srt.distributed.parallel_state import get_tp_group
 
 logger = logging.getLogger(__name__)
 
 
 class AiterCommManager:
     def __init__(self):
-        self.world_size = None
-        self.rank = None
+        self.group = None
+        self.device_id = None
         self.dtype = None
         self.initialized = False
 
     def initialize(
         self,
-        world_size: int,
-        rank: int,
+        group,
+        device_id,
         dtype: torch.dtype,
     ):
         """Initialize workspace"""
-        if self.initialized and self.world_size == world_size:
+        if self.initialized and group == self.group and device_id == self.device_id:
             return
 
         self.cleanup()
 
-        self.world_size = world_size
-        self.rank = rank
+        self.group = group
+        self.device_id = device_id
         self.dtype = dtype
-        self.dist_env = aiter.TRTLLMDistEnv(rank, world_size, dtype=self.dtype)
-
-        set_custom_all_reduce(True)
-        init_distributed_environment(
-            world_size=self.world_size,
-            rank=rank,
-            distributed_init_method=None,
-        )
-        ensure_model_parallel_initialized(self.world_size, 1)
+        self.dist_env = aiter.AiterDistEnv(group=self.group, device_id=self.device_id, dtype=self.dtype)
 
         self.initialized = True
 
@@ -67,25 +46,23 @@ class AiterCommManager:
 _aiter_comm_manager = AiterCommManager()
 
 
-def ensure_workspace_initialized(dtype):
+def ensure_aiter_comm_initialized(dtype):
     """Ensure workspace is initialized"""
     if _aiter_comm_manager is None:
         return False
 
-    world_size = get_tensor_model_parallel_world_size()
-    if world_size <= 1:
-        return False
-
-    rank = dist.get_rank()
-
+    group = get_tp_group().device_group
+    device_id = get_tp_group().local_rank
+    
     if (
         not _aiter_comm_manager.initialized
-        or _aiter_comm_manager.world_size != world_size
+        or _aiter_comm_manager.group != group
+        or _aiter_comm_manager.device_id != device_id
         or _aiter_comm_manager.dtype != dtype
     ):
         _aiter_comm_manager.initialize(
-            world_size=world_size,
-            rank=rank,
+            group=group,
+            device_id=device_id,
             dtype=dtype,
         )
 
@@ -106,21 +83,16 @@ def aiter_allreduce_residual_rmsnorm(
     if world_size <= 1:
         logger.debug("Single GPU, no need for allreduce fusion")
         return None, None, None
-    if not ensure_workspace_initialized(allreduce_in.dtype):
+    if not ensure_aiter_comm_initialized(allreduce_in.dtype):
         logger.debug("Aiter workspace is not initialized")
         return None, None, None
-    num_tokens = allreduce_in.shape[0]
-    if num_tokens <= 1024:
-        residual_out, norm_out = tensor_model_parallel_fused_allreduce_rmsnorm(allreduce_in, residual_in, rms_weight, eps)
-    else:
-        return _aiter_comm_manager.dist_env.allreduce_add_rms_fused(
-            allreduce_in,
-            residual_in,
-            rms_weight,
-            eps,
-            fp8_out,
-        )
-    return residual_out, norm_out, None
+    return _aiter_comm_manager.dist_env.allreduce_add_rms_fused(
+        allreduce_in,
+        residual_in,
+        rms_weight,
+        eps,
+        fp8_out,
+    )
 
 
 def fake_aiter_allreduce_residual_rmsnorm(
