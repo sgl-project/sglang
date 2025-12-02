509f62397b36c123e203e74898fde9e500dd329e
d00662834 <dingshun@huawei.com>
2025-12-02 14:25:48 +0800

    Modify ascend nightly workflow script.

--- test/srt/ascend_k8s/run_ascend_deepep.sh
+++ test/srt/ascend_k8s/run_ascend_deepep.sh
@@ -9,6 +9,10 @@ pip install kubernetes
 pip3 install xgrammar==0.1.25
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 
+# copy or download required file
+cp /data/ascend-ci-share-pkking-sglang/huggingface/hub/datasets--anon8231489123--ShareGPT_Vicuna_unfiltered/snapshots/192ab2185289094fc556ec8ce5ce1e8e587154ca/ShareGPT_V3_unfiltered_cleaned_split.json /tmp
+curl -o /tmp/test.jsonl -L https://gh-proxy.test.osinfra.cn/https://raw.githubusercontent.com/openai/grade-school-math/master/grade_school_math/data/test.jsonl
+
 echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 sysctl -w vm.swappiness=0
 sysctl -w kernel.numa_balancing=0
