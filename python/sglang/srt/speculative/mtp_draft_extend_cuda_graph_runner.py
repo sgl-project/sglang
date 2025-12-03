e71cd89427936fa8063051b328b2477101ee24c5
jiangzihan <jiangzihan@xiaomi.com>
2025-12-03 17:56:50 +0000

    fix: set global_num_tokens_for_logprob_gpu to num tokens for MTP worker V2

--- python/sglang/srt/speculative/mtp_draft_extend_cuda_graph_runner.py
+++ python/sglang/srt/speculative/mtp_draft_extend_cuda_graph_runner.py
@@ -141,6 +141,7 @@ class MTPDraftExtendCudaGraphRunner:
                     )
                 else:
                     assert self.require_attn_tp_gather
+                    assert False, "assume not reached"
                     self.global_num_tokens_gpu = torch.zeros((1,), dtype=torch.int32)
                     self.global_num_tokens_for_logprob_gpu = torch.zeros(
                         (1,), dtype=torch.int32
@@ -253,7 +254,7 @@ class MTPDraftExtendCudaGraphRunner:
             )
             self.global_num_tokens_for_logprob_gpu.copy_(
                 torch.tensor(
-                    [bs] * self.dp_size,
+                    [num_tokens] * self.dp_size,
                     dtype=torch.int32,
                     device=self.input_ids.device,
                 )
@@ -433,11 +434,6 @@ class MTPDraftExtendCudaGraphRunner:
             self.accept_length[:raw_bs].copy_(forward_batch.spec_info.accept_length)
         self.req_pool_indices[:raw_bs].copy_(forward_batch.req_pool_indices)
 
-        # TODO(ch-wan): support num_token_non_padded
-        if self.require_gathered_buffer:
-            self.global_num_tokens_gpu.fill_(bs * self.num_tokens_per_bs)
-            self.global_num_tokens_for_logprob_gpu.fill_(bs)
-
         if forward_batch.seq_lens_cpu is not None:
             if bs != raw_bs:
                 self.seq_lens_cpu.fill_(self.seq_len_fill_value)
@@ -457,12 +453,7 @@ class MTPDraftExtendCudaGraphRunner:
         num_tokens = raw_bs * self.num_tokens_per_bs
         # num_tokens = forward_batch.input_ids.shape[0]
         if self.require_mlp_tp_gather:
-            max_num_tokens = max(forward_batch.global_num_tokens_cpu)
-            max_batch_size = (
-                max_num_tokens // self.num_tokens_per_bs
-                if self.model_runner.spec_algorithm.is_eagle()
-                else max_num_tokens
-            )
+            max_batch_size = max(forward_batch.original_global_num_tokens_cpu)
             index = bisect.bisect_left(self.capture_bs, max_batch_size)
         else:
             index = bisect.bisect_left(self.capture_bs, raw_bs)
@@ -474,7 +465,7 @@ class MTPDraftExtendCudaGraphRunner:
 
         if self.require_gathered_buffer:
             self.global_num_tokens_gpu.fill_(bs * self.num_tokens_per_bs)
-            self.global_num_tokens_for_logprob_gpu.fill_(bs)
+            self.global_num_tokens_for_logprob_gpu.fill_(bs * self.num_tokens_per_bs)
 
         forward_batch.spec_info.hidden_states = self.hidden_states[:num_tokens]
         forward_batch.spec_info.accept_length = self.accept_length[:bs]
