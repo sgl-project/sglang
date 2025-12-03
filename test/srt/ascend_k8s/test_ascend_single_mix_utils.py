9975c342923aaa9cc317aef1f3914742bf32875e
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2025-12-03 14:47:58 +0800

    Update test_ascend_single_mix_utils.py

--- test/srt/ascend_k8s/test_ascend_single_mix_utils.py
+++ test/srt/ascend_k8s/test_ascend_single_mix_utils.py
@@ -313,48 +313,6 @@ QWEN3_CODER_480B_A35B_INSTRUCT_W8A8_QUAROT_ENVS = {
     "STREAMS_PER_DEVICE": "32",
 }
 
-QWEN3_8B_OTHER_ARGS = (
-    [
-        "--trust-remote-code",
-        "--nnodes",
-        "1",
-        "--node-rank",
-        "0",
-        "--attention-backend",
-        "ascend",
-        "--device",
-        "npu",
-        "--quantization",
-        "w8a8_int8",
-        "--max-running-requests",
-        "16",
-        "--disable-radix-cache",
-        "--chunked-prefill-size",
-        "43008",
-        "--max-prefill-tokens",
-        "525000",
-        "--tp-size",
-        "4",
-        "--mem-fraction-static",
-        "0.8",
-        "--cuda-graph-bs",
-        "16",
-        "--dtype",
-        "bfloat16",    
-    ]
-)
-
-QWEN3_8B_ENVS = {
-    "SGLANG_SET_CPU_AFFINITY": "1",
-    "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
-    "SGLANG_DISAGGREGATION_BOOTSTRAP_TIMEOUT": "600",
-    "HCCL_BUFFSIZE": "400",
-    "HCCL_SOCKET_IFNAME": "lo",
-    "GLOO_SOCKET_IFNAME": "lo",     
-    "HCCL_OP_EXPANSION_MODE": "AIV",
-    
-}
-
 
 def run_command(cmd, shell=True):
     try:
