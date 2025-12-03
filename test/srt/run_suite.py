5f82152486ce5b233d88c305872389b950106db4
Zheng, Beilei <beilei.zheng@intel.com>
2025-12-03 01:02:56 -0800

    fix ut

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -329,6 +329,7 @@ suite_xeon = {
     "per-commit-cpu": [
         TestFile("cpu/test_activation.py"),
         TestFile("cpu/test_binding.py"),
+        TestFile("cpu/test_bmm.py"),
         TestFile("cpu/test_decode.py"),
         TestFile("cpu/test_extend.py"),
         TestFile("cpu/test_gemm.py"),
