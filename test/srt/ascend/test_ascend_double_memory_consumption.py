fd6999eb5fea85ceb4c6e911e2564f9a2964b1e6
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2025-12-03 14:13:55 +0300

    Remove unnecessary function

--- test/srt/ascend/test_ascend_double_memory_consumption.py
+++ test/srt/ascend/test_ascend_double_memory_consumption.py
@@ -29,45 +29,6 @@ STDERR_FILENAME = "./tmp/stderr.txt"
 STDOUT_FILENAME = "./tmp/stdout.txt"
 
 
-def kill_process_tree(parent_pid, include_parent: bool = True, skip_pid: int = None):
-    """Kill the process and all its child processes."""
-    # Remove sigchld handler to avoid spammy logs.
-    if threading.current_thread() is threading.main_thread():
-        signal.signal(signal.SIGCHLD, signal.SIG_DFL)
-
-    if parent_pid is None:
-        parent_pid = os.getpid()
-        include_parent = False
-
-    try:
-        itself = psutil.Process(parent_pid)
-    except psutil.NoSuchProcess:
-        return
-
-    children = itself.children(recursive=True)
-    for child in children:
-        if child.pid == skip_pid:
-            continue
-        try:
-            child.kill()
-        except psutil.NoSuchProcess:
-            pass
-
-    if include_parent:
-        try:
-            if parent_pid == os.getpid():
-                itself.kill()
-                sys.exit(0)
-
-            itself.kill()
-
-            # Sometime processes cannot be killed with SIGKILL (e.g, PID=1 launched by kubernetes),
-            # so we send an additional signal to kill them.
-            itself.send_signal(signal.SIGQUIT)
-        except psutil.NoSuchProcess:
-            pass
-
-
 class TestMemoryConsumptionAscend(CustomTestCase):
 
     def test_memory_consumption(self):
