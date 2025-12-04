1b5182b7f383722292441fedb5202a73a04e8b61
Kevin-XiongC <kevin_xiong1997@outlook.com>
2025-12-04 02:42:27 +0000

    1

--- python/sglang/srt/disaggregation/prefill.py
+++ python/sglang/srt/disaggregation/prefill.py
@@ -391,7 +391,7 @@ class SchedulerDisaggregationPrefillMixin:
     
     def notify_prefill_done(self: Scheduler,batch: ScheduleBatch,result: GenerationBatchResult) -> None:
         for i, req in enumerate(batch.reqs):
-            if isinstance(req.disagg_kv_sender, MooncakeKVSender):
+            if hasattr(req.disagg_kv_sender, "result"):
                 assert req.disagg_kv_sender.result is None
                 req.disagg_kv_sender.result = (batch.copy(), result)
                 self.send_kv_chunk(req, last_chunk=req.is_chunked <= 0)
