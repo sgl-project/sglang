7717b95cc438643103ffe783d37e7885c81032b8
Yihan Chen <yingluosanqian@example.com>
2025-12-04 16:17:55 +0000

    remote origin impl

--- python/sglang/multimodal_gen/test/test_scale_residual_norm_scale_shift.py
+++ python/sglang/multimodal_gen/test/test_scale_residual_norm_scale_shift.py
@@ -1,25 +1,12 @@
 
-from sglang.multimodal_gen.runtime.layers.triton_ops import fuse_scale_shift_kernel
-from sglang.multimodal_gen.test.utils import allclose_with_tolerance
 from sglang.test.test_utils import CustomTestCase
 from sglang.multimodal_gen.runtime.layers.layernorm import ScaleResidualNormScaleShift
-from sglang.multimodal_gen.runtime.layers.layernorm import ScaleResidualLayerNormScaleShift, LayerNorm
-import torch.nn as nn
 import random
 import unittest
-import itertools
-
 import torch
-import time
-import os
-print("CUDA_VISIBLE_DEVICES =", os.environ.get("CUDA_VISIBLE_DEVICES"))
-print("torch sees", torch.cuda.device_count(), "GPUs")
-print("Using device:", torch.cuda.get_device_name(0))
+from torch import Tensor
 
 
-################################################################################
-# Benchmark
-################################################################################
 def is_float(value):
     try:
         float(value)
@@ -28,36 +15,21 @@ def is_float(value):
         return False
 
 
-def benchmark(fn, *args, **kwargs):
-    warmup = 2
-    iters = 20
-    # Make sure everything is clean
-    with torch.inference_mode():
-        if torch.cuda.is_available():
-            torch.cuda.synchronize()
+def allclose_with_tolerance(x: Tensor, y: Tensor, atol: float, rtol: float, max_ratio=0.005) -> Tuple[bool, str]:
+    diff = torch.abs(x - y)
+    th = atol + rtol * torch.abs(y)
 
-        # --- Warmup ---
-        for _ in range(warmup):
-            fn(*args, **kwargs)
-        if torch.cuda.is_available():
-            torch.cuda.synchronize()
+    # out-of-tolerance mask
+    bad_mask = diff > th
+    bad_ratio = bad_mask.float().mean().item()
 
-        # --- Timed runs ---
-        t0 = time.time()
-        for _ in range(iters):
-            fn(*args, **kwargs)
-        if torch.cuda.is_available():
-            torch.cuda.synchronize()
-        t1 = time.time()
-
-    # Average time per iteration (ms)
-    avg_time_ms = (t1 - t0) * 1000 / iters
-    return avg_time_ms
-
+    return bad_ratio <= max_ratio, f"{bad_ratio:.6f} > {max_ratio}"
 
 ################################################################################
 # Accuracy Test
 ################################################################################
+
+
 class TestScaleResidualNormScaleShiftAccuracy(CustomTestCase):
     DTYPES = [torch.float32, torch.float16, torch.bfloat16]
     PARAM_DTYPES = [torch.float32, torch.float16, torch.bfloat16]
@@ -200,174 +172,6 @@ class TestScaleResidualNormScaleShiftAccuracy(CustomTestCase):
             with self.subTest(**params):
                 self._run_fused_test(**params)
                 torch.cuda.synchronize()
-
-
-################################################################################
-# Performance Test
-################################################################################
-class TestScaleResidualNormScaleShiftPerf(CustomTestCase):
-    DTYPES = [torch.float32, torch.float16]
-    PARAM_DTYPES = [torch.float32]
-    BATCH_NUM = [1, 2]
-    SEQ = [1024, 2048, 4096, 16380, 32760, 115200]
-    FRAME = [4]
-    HIDDEN_SIZES = [512, 1024, 1536, 2048, 3072]
-    USE_AFFINE = [True, False]
-    USE_BIAS = [True]
-    NORM_TYPE = ["layer"]
-    GATE_SHAPE = ["B1D"]
-    SCALE_SHIFT_SHAPE = ["11D"]
-    SEEDS = [0]
-
-    args = [
-        (DTYPES, "dtype"),
-        (PARAM_DTYPES, "param_type"),
-        (BATCH_NUM, "batch"),
-        (SEQ, "seq"),
-        (FRAME, "frame"),
-        (HIDDEN_SIZES, "hidden_size"),
-        (USE_AFFINE, "use_affine"),
-        (USE_BIAS, "use_bias"),
-        (NORM_TYPE, "norm_type"),
-        (GATE_SHAPE, "gate_shape"),
-        (SCALE_SHIFT_SHAPE, "scale_shift_shape"),
-        (SEEDS, "seed"),
-    ]
-
-    def gen_cases(self):
-        keys = [arg[1] for arg in self.args]
-        value_lists = [arg[0] for arg in self.args]
-
-        for values in itertools.product(*value_lists):
-            yield dict(zip(keys, values))
-
-    @classmethod
-    def setUpClass(cls):
-        if not torch.cuda.is_available():
-            raise unittest.SkipTest("CUDA is not available")
-        torch.set_default_device(f"cuda")
-
-    def _run_fused_test(
-        self, **params,
-    ):
-        dtype = params["dtype"]
-        param_type = params["param_type"]
-        batch = params["batch"]
-        seq = params["seq"]
-        frame = params["frame"]
-        hidden_size = params["hidden_size"]
-        use_affine = params["use_affine"]
-        use_bias = params["use_bias"]
-        norm_type = params["norm_type"]
-        gate_shape = params["gate_shape"]
-        scale_shift_shape = params["scale_shift_shape"]
-        seed = params["seed"]
-
-        torch.manual_seed(seed)
-
-        if norm_type == "layer" and use_bias == False:
-            return
-
-        ref_layer = ScaleResidualLayerNormScaleShift(
-            hidden_size, elementwise_affine=use_affine, dtype=param_type, norm_type=norm_type
-        )
-        layer = ScaleResidualNormScaleShift(
-            hidden_size, elementwise_affine=use_affine, bias=use_bias, dtype=param_type, norm_type=norm_type
-        )
-        if use_affine:
-            w = torch.empty_like(layer.norm.weight)
-            w.normal_(mean=1.0, std=0.1)
-            layer.norm.weight.data.copy_(w)
-            ref_layer.norm.weight.data.copy_(w)
-            if norm_type == "layer" and use_bias:
-                b = torch.empty_like(layer.norm.bias)
-                b.normal_(mean=1.0, std=0.1)
-                layer.norm.bias.data.copy_(b)
-                ref_layer.norm.bias.data.copy_(b)
-
-        residual = torch.randn(batch, seq, hidden_size, dtype=dtype)
-        x = torch.randn(batch, seq, hidden_size, dtype=dtype)
-
-        if gate_shape == "1":
-            gate = 1
-        elif gate_shape == "BF1D":
-            if seq % frame != 0:
-                return
-            gate = torch.randn(batch, frame, 1, hidden_size, dtype=dtype)
-        elif gate_shape == "B1D":
-            gate = torch.randn(batch, 1, hidden_size, dtype=dtype)
-        else:
-            raise ValueError("Unknown gate shape.")
-
-        if is_float(scale_shift_shape):
-            scale = torch.ones(1, dtype=dtype) * float(scale_shift_shape)
-            shift = torch.ones(1, dtype=dtype) * float(scale_shift_shape)
-        elif scale_shift_shape == "[1]":
-            scale = torch.ones(1, dtype=dtype) * torch.rand(1).item()
-            shift = torch.ones(1, dtype=dtype) * torch.rand(1).item()
-        elif scale_shift_shape == "BD":
-            scale = torch.randn(batch, hidden_size, dtype=dtype)
-            shift = torch.randn(batch, hidden_size, dtype=dtype)
-        elif scale_shift_shape == "1D":
-            scale = torch.randn(1, hidden_size, dtype=dtype)
-            shift = torch.randn(1, hidden_size, dtype=dtype)
-        elif scale_shift_shape == "BSD":
-            scale = torch.randn(batch, seq, hidden_size, dtype=dtype)
-            shift = torch.randn(batch, seq, hidden_size, dtype=dtype)
-        elif scale_shift_shape == "B1D":
-            scale = torch.randn(batch, 1, hidden_size, dtype=dtype)
-            shift = torch.randn(batch, 1, hidden_size, dtype=dtype)
-        elif scale_shift_shape == "1SD":
-            scale = torch.randn(1, seq, hidden_size, dtype=dtype)
-            shift = torch.randn(1, seq, hidden_size, dtype=dtype)
-        elif scale_shift_shape == "11D":
-            scale = torch.randn(1, 1, hidden_size, dtype=dtype)
-            shift = torch.randn(1, 1, hidden_size, dtype=dtype)
-        elif scale_shift_shape == "BF1D":
-            if seq % frame != 0:
-                return
-            scale = torch.randn(batch, frame, 1, hidden_size, dtype=dtype)
-            shift = torch.randn(batch, frame, 1, hidden_size, dtype=dtype)
-
-        with torch.inference_mode():
-            ref_out_mod, ref_out_resi = ref_layer(
-                residual, x, gate, shift, scale)
-            out_mod, out_resi = layer(residual, x, gate, shift, scale)
-
-        if dtype == torch.float32 and param_type == torch.float32:
-            self.assertTrue(*allclose_with_tolerance(
-                out_mod, ref_out_mod, atol=1e-6, rtol=1e-4))
-            self.assertTrue(*allclose_with_tolerance(
-                out_resi, ref_out_resi, atol=1e-6, rtol=1e-4))
-        else:
-            self.assertTrue(*allclose_with_tolerance(
-                out_mod, ref_out_mod, atol=5e-2, rtol=1e-2))
-            self.assertTrue(*allclose_with_tolerance(
-                out_resi, ref_out_resi, atol=5e-2, rtol=1e-2))
-
-        # Perf
-        fused_kernel_time = benchmark(layer, residual, x, gate, shift, scale)
-        naive_kernel_time = benchmark(
-            ref_layer, residual, x, gate, shift, scale)
-        speedup = naive_kernel_time / fused_kernel_time
-        print(
-            f"[speedup]={speedup:.2f}x ({naive_kernel_time:.3f}ms/{fused_kernel_time:.3f}ms), "
-            f"dtype={dtype}, param_type={param_type}, batch={batch}, seq={seq}, "
-            f"frame={frame}, hidden={hidden_size}, use_affine={use_affine}, "
-            f"use_bias={use_bias}, norm_type={norm_type}, gate_shape={gate_shape}, "
-            f"scale_shift_shape={scale_shift_shape}, seed={seed}"
-        )
-        return speedup
-
-    def test_fused(self):
-        speedup = []
-        for params in self.gen_cases():
-            with self.subTest(**params):
-                torch.cuda.synchronize()
-                speedup.append(self._run_fused_test(**params))
-                torch.cuda.synchronize()
-        avg_speedup = sum(speedup) / len(speedup)
-        print(f"Average Speedup = {avg_speedup}")
 
 
 if __name__ == "__main__":
