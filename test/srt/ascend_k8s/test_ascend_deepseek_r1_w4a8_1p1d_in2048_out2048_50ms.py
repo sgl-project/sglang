7bf616010c0b6f0fde2a505f80a8bea1159f546b
shun8686 <verve@vip.qq.com>
2025-12-05 10:40:00 +0800

    Update testcase for ascend test.

--- test/srt/ascend_k8s/test_ascend_deepseek_r1_w4a8_1p1d_in2048_out2048_50ms.py
+++ test/srt/ascend_k8s/test_ascend_deepseek_r1_w4a8_1p1d_in2048_out2048_50ms.py
@@ -135,7 +135,7 @@ class Test_DeepSeek_R1_W4A8_1P1D_In2048_Out2048(TestAscendDisaggregationUtils):
     model_config = MODEL_CONFIG
     dataset_name = "random"
     request_rate = 16
-    max_concurrency = 352
+    max_concurrency = 8
     num_prompts = int(max_concurrency) * 4
     input_len = 2048
     output_len = 2048
