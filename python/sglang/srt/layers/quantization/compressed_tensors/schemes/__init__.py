c82ae89d73334d6d353a1f72f7703bd649a09c34
elvischenv <219235043+elvischenv@users.noreply.github.com>
2025-12-09 11:11:40 +0800

    clean up w4a16 nvfp4 (#5)

--- python/sglang/srt/layers/quantization/compressed_tensors/schemes/__init__.py
+++ python/sglang/srt/layers/quantization/compressed_tensors/schemes/__init__.py
@@ -2,7 +2,6 @@
 
 from .compressed_tensors_scheme import CompressedTensorsScheme
 from .compressed_tensors_w4a4_nvfp4 import CompressedTensorsW4A4Fp4
-from .compressed_tensors_w4a16_nvfp4 import CompressedTensorsW4A16Fp4
 from .compressed_tensors_w8a8_fp8 import CompressedTensorsW8A8Fp8
 from .compressed_tensors_w8a8_int8 import CompressedTensorsW8A8Int8
 from .compressed_tensors_w8a16_fp8 import CompressedTensorsW8A16Fp8
@@ -15,6 +14,5 @@ __all__ = [
     "CompressedTensorsW8A8Int8",
     "CompressedTensorsWNA16",
     "WNA16_SUPPORTED_BITS",
-    "CompressedTensorsW4A16Fp4",
     "CompressedTensorsW4A4Fp4",
 ]
