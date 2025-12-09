9a35a9b1a04fc3ce8821a782a37f34e5735d5a20
Muqi Li <muqi1029@gmail.com>
2025-12-09 02:38:27 -0800

    add test

---
+++ test/manual/openai_server/features/test_parallel_tool_calls.py
@@ -0,0 +1,15 @@
+"""
+python3 -m unittest test.srt.openai_server.features.test_paralell_tool_calls
+"""
+
+import unittest
+
+from sglang.test.test_utils import CustomTestCase
+
+
+class TestParallelToolCall(CustomTestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
