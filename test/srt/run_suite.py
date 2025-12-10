3e717e150391cc6fe39fd18fb31edb7efbcd3618
Brayden Zhong <b8zhong@uwaterloo.ca>
2025-12-09 20:09:16 -0800

    more

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -171,8 +171,8 @@ suites = {
         TestFile("test_deepseek_v3_fp4_4gpu.py", 1800),
         TestFile("test_flash_attention_4.py", 300),
         TestFile("test_fp8_blockwise_gemm.py", 280),
-        TestFile("test_gpt_oss_4gpu.py", 600),
-        TestFile("test_llama31_fp4.py", 300),
+        TestFile("test_gpt_oss_1gpu.py", 300),
+        TestFile("test_llama31_fp4.py", 90),
         TestFile("test_eagle_infer_beta_dp_attention.py", 300),
     ],
     # "per-commit-8-gpu-b200": [
