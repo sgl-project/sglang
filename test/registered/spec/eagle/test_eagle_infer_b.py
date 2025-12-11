da404591d1294ed8348674f1884c58a16f5d5084
Liangsheng Yin <lsyincs@gmail.com>
2025-12-11 23:51:24 +0900

    simplify

--- test/registered/spec/eagle/test_eagle_infer_b.py
+++ test/registered/spec/eagle/test_eagle_infer_b.py
@@ -18,35 +18,14 @@ class TestEAGLEServerBasic(EagleServerBase):
     extra_args = ["--chunked-prefill-size", 128, "--max-running-requests", 8]
 
 
-class TestEAGLERetract(TestEAGLEServerBasic):
+class TestEAGLERetract(EagleServerBase):
+    extra_args = ["--chunked-prefill-size", 128, "--max-running-requests", 64]
+
     @classmethod
     def setUpClass(cls):
         # These config helps find a leak.
         envs.SGLANG_CI_SMALL_KV_SIZE.set(4500)
-        cls.base_url = DEFAULT_URL_FOR_TEST
-        cls.process = popen_launch_server(
-            DEFAULT_EAGLE_TARGET_MODEL_FOR_TEST,
-            cls.base_url,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=[
-                "--speculative-algorithm",
-                "EAGLE",
-                "--speculative-draft-model-path",
-                DEFAULT_EAGLE_DRAFT_MODEL_FOR_TEST,
-                "--speculative-num-steps",
-                5,
-                "--speculative-eagle-topk",
-                8,
-                "--speculative-num-draft-tokens",
-                64,
-                "--mem-fraction-static",
-                0.7,
-                "--chunked-prefill-size",
-                128,
-                "--max-running-requests",
-                64,
-            ],
-        )
+        super().setUpClass()
 
 
 class TestEAGLEServerTriton(TestEAGLEServerBasic):
