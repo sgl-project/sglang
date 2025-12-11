22848263581e2dd0b487d20dced6ff20341dc3ba
ppraneth <pranethparuchuri@gmail.com>
2025-12-11 21:35:19 +0530

    fix error ci/cd

--- sgl-model-gateway/benches/router_registry_bench.rs
+++ sgl-model-gateway/benches/router_registry_bench.rs
@@ -19,7 +19,7 @@ fn setup_registry(count: usize) -> Arc<WorkerRegistry> {
             WorkerType::Decode
         };
 
-        let worker = BasicWorkerBuilder::new(&format!("http://worker-{}:8000", i))
+        let worker = BasicWorkerBuilder::new(format!("http://worker-{}:8000", i))
             .worker_type(worker_type)
             .labels(labels)
             .circuit_breaker_config(CircuitBreakerConfig::default())
