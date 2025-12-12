c660d8dfd08029a5f0e4d58dc5cf64336b294992
Liangsheng Yin <hnyls2002@gmail.com>
2025-12-12 12:25:39 +0900

    Re-org eagle unit tests (#14909)

--- python/sglang/srt/environ.py
+++ python/sglang/srt/environ.py
@@ -173,6 +173,7 @@ class Envs:
     SGLANG_TEST_RETRACT_NO_PREFILL_BS = EnvInt(2 ** 31)
     SGLANG_ENABLE_STRICT_MEM_CHECK_DURING_BUSY = EnvInt(0)
     SGLANG_ENABLE_STRICT_MEM_CHECK_DURING_IDLE = EnvBool(True)
+    SGLANG_CI_SMALL_KV_SIZE = EnvInt(-1)
 
     # Scheduler: new token ratio hyperparameters
     SGLANG_INIT_NEW_TOKEN_RATIO = EnvFloat(0.7)
