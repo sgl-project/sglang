ab468f79b4eae977eef3634720057755a210f750
wplf <975761915@qq.com>
2025-12-12 07:59:58 +0000

    add some button

--- python/sglang/cli/webui.py
+++ python/sglang/cli/webui.py
@@ -46,31 +46,82 @@ def webui(args, extra_argv):
     )
     sampling_params_kwargs = SamplingParams.get_cli_args(args)
 
-    def gradio_generate(prompt_in):
+    def gradio_generate(
+        prompt,
+        negative_prompt,
+        seed,
+        width,
+        height,
+        num_inference_steps,
+        guidance_scale,
+        enable_teacache,
+    ):
         # use global variable sampling_params_kwargs to avoid pass this param, because gradio does not support this.
         # return PIL.Image.Image | np.ndarray
-        sampling_params_kwargs["prompt"] = prompt_in
+        sampling_params_kwargs["prompt"] = prompt
+        sampling_params_kwargs["negative_prompt"] = negative_prompt
+        sampling_params_kwargs["seed"] = seed
+        sampling_params_kwargs["width"] = width
+        sampling_params_kwargs["height"] = height
+        sampling_params_kwargs["guidance_scale"] = guidance_scale
+        sampling_params_kwargs["num_inference_steps"] = num_inference_steps
+        sampling_params_kwargs["enable_teacache"] = enable_teacache
+
         results = generator.generate(sampling_params_kwargs=sampling_params_kwargs)
         frames = results["frames"][0]
         return frames
 
+    default_negative_prompt = "Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards"
+
     with gr.Blocks() as demo:
-        gr.Markdown("# 🚀 Diffusion Generate Demo")
+        gr.Markdown("# 🚀 Sglang Diffusion Application")
 
         with gr.Row():
-            prompt_in = gr.Textbox(label="Prompt")
-            run_btn = gr.Button("Generate")
+            with gr.Column():
+                prompt = gr.Textbox(label="Prompt", value="A curious raccoon")
+                negative_prompt = gr.Textbox(
+                    label="Negative_prompt", value=default_negative_prompt
+                )
 
-        image_out = gr.Image(
-            label="Generated Image",
+            run_btn = gr.Button("Generate", variant="primary")
+
+        with gr.Row():
+            with gr.Column():
+                seed = gr.Number(label="seed", precision=0, value=1234)
+                width = gr.Number(label="width", precision=0, value=512)
+                height = gr.Number(label="height", precision=0, value=512)
+                num_inference_steps = gr.Slider(
+                    minimum=0, maximum=50, value=20, step=1, label="num_inference_steps"
+                )
+
+                guidance_scale = gr.Slider(
+                    minimum=0.0, maximum=10, value=5, step=0.01, label="guidance_scale"
+                )
+                enable_teacache = gr.Checkbox(label="enable_teacache", value=False)
+
+            image_out = gr.Image(
+                label="Generated Image",
+            )
+
+        run_btn.click(
+            fn=gradio_generate,
+            inputs=[
+                prompt,
+                negative_prompt,
+                seed,
+                width,
+                height,
+                num_inference_steps,
+                guidance_scale,
+                enable_teacache,
+            ],
+            outputs=[image_out],
         )
-
-        run_btn.click(fn=gradio_generate, inputs=[prompt_in], outputs=[image_out])
 
     demo.launch()
 
 
 """ example script
-sglang webui  --model-path black-forest-labs/FLUX.1-dev
+sglang webui --model-path black-forest-labs/FLUX.1-dev
 
 """
