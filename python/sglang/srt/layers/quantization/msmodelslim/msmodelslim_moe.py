ca4895ed635eaff99e23ceaf37274e564908d561
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2025-12-12 17:47:24 +0300

    Add w4a8 support

--- python/sglang/srt/layers/quantization/msmodelslim/msmodelslim_moe.py
+++ python/sglang/srt/layers/quantization/msmodelslim/msmodelslim_moe.py
@@ -17,6 +17,7 @@ from sglang.srt.layers.quantization.msmodelslim.schemes import (
 )
 from sglang.srt.hardware_backend.npu.utils import npu_format_cast
 from sglang.srt.hardware_backend.npu.quantization.fused_moe_method_npu import (
+    NPUW4A8Int8DynamicMoEMethod,
     NPUW8A8Int8DynamicMoEMethod,
 )
 
@@ -37,6 +38,7 @@ logger = logging.getLogger(__name__)
 
 __all__ = [
     "ModelSlimMoEMethod",
+    "ModelSlimW4A8Int8MoE",
     "ModelSlimW8A8Int8MoE",
 ]
 
@@ -56,24 +58,198 @@ class ModelSlimMoEMethod(FusedMoEMethodBase):
         # TODO: @dsikka: refactor this to use schemes as other kernels
         # are supported + check if the layer is being ignored.
 
-        return ModelSlimW8A8Int8MoE(quant_config)
-        # weight_quant = quant_config.target_scheme_map["Linear"].get("weights")
-        # input_quant = quant_config.target_scheme_map["Linear"].get("input_activations")
-        # is_moe_w4_dynamic = quant_config.is_dynamic_token_w4(weight_quant, input_quant)
-        # is_moe_input_quant = input_quant
+        prefix_in_quant_config = prefix + ".0.down_proj.weight"
+        is_moe_w4a8_dynamic = (
+            quant_config.quant_description.get(prefix_in_quant_config, "STATIC")
+            == "W4A8_DYNAMIC"
+        )
 
-        # if (
-        #     is_moe_w4_dynamic and is_moe_input_quant is not None
-        # ) or quant_config._is_moe_w4a8_dynamic(prefix, weight_quant, input_quant):
-        #     return NPUW4A8Int4DynamicMoEMethod(quant_config)
-        # elif is_moe_w4_dynamic and is_moe_input_quant is None:
-        #     return NPUW4A16Int4DynamicMoEMethod(quant_config)
-        # else:
-        #     return NPUW8A8Int8DynamicMoEMethod(quant_config)
-        # else:
-        #     raise RuntimeError(
-        #         f"Unsupported FusedMoe scheme: {weight_quant}, {input_quant}"
-        #     )
+        if is_moe_w4a8_dynamic:
+            return ModelSlimW4A8Int8MoE(quant_config)
+
+        return ModelSlimW8A8Int8MoE(quant_config)
+
+
+class ModelSlimW4A8Int8MoE(ModelSlimMoEMethod):
+
+    def __init__(
+            self, quant_config: Dict[str, Any], prefix: str = None,
+    ):
+        self.quant_config = quant_config
+        self.group_size = 0
+        self.tp_size = 1
+        self.is_per_channel_weight = self.group_size == 0
+
+    def create_weights(
+        self,
+        layer: torch.nn.Module,
+        num_experts: int,
+        hidden_size: int,
+        intermediate_size_per_partition: int,
+        params_dtype: torch.dtype,
+        **extra_weight_attrs,
+    ) -> None:
+        from sglang.srt.layers.moe.fused_moe_triton import FusedMoeWeightScaleSupported
+
+        self.num_experts = num_experts
+        extra_weight_attrs.update(
+            {"quant_method": FusedMoeWeightScaleSupported.CHANNEL.value}
+        )
+
+        # >> weight
+        w13_output_size = intermediate_size_per_partition
+        w2_output_size = hidden_size // 2
+        w13_weight = torch.nn.Parameter(
+            torch.empty(num_experts, w13_output_size, hidden_size, dtype=torch.int8),
+            requires_grad=False,
+        )
+        layer.register_parameter("w13_weight", w13_weight)
+        set_weight_attrs(w13_weight, extra_weight_attrs)
+        w2_weight = torch.nn.Parameter(
+            torch.empty(
+                num_experts,
+                w2_output_size,
+                intermediate_size_per_partition,
+                dtype=torch.int8,
+            ),
+            requires_grad=False,
+        )
+        layer.register_parameter("w2_weight", w2_weight)
+        set_weight_attrs(w2_weight, extra_weight_attrs)
+
+        # >> scale
+        w13_weight_scale = torch.nn.Parameter(
+            torch.empty(
+                num_experts, 2 * intermediate_size_per_partition, 1, dtype=torch.float32
+            ),
+            requires_grad=False,
+        )
+        layer.register_parameter("w13_weight_scale", w13_weight_scale)
+        set_weight_attrs(w13_weight_scale, extra_weight_attrs)
+
+        w2_weight_scale = torch.nn.Parameter(
+            torch.empty(num_experts, hidden_size, 1, dtype=torch.float32),
+            requires_grad=False,
+        )
+        layer.register_parameter("w2_weight_scale", w2_weight_scale)
+        set_weight_attrs(w2_weight_scale, extra_weight_attrs)
+
+        # >> offset
+        w13_weight_offset = torch.nn.Parameter(
+            torch.empty(
+                num_experts, 2 * intermediate_size_per_partition, 1, dtype=torch.float32
+            ),
+            requires_grad=False,
+        )
+        layer.register_parameter("w13_weight_offset", w13_weight_offset)
+        set_weight_attrs(w13_weight_offset, extra_weight_attrs)
+
+        w2_weight_offset = torch.nn.Parameter(
+            torch.empty(num_experts, hidden_size, 1, dtype=torch.float32),
+            requires_grad=False,
+        )
+        layer.register_parameter("w2_weight_offset", w2_weight_offset)
+        set_weight_attrs(w2_weight_offset, extra_weight_attrs)
+
+        if not self.is_per_channel_weight:
+            # >>> special param for w4a8
+            w13_weight_scale_second = torch.nn.Parameter(
+                torch.empty(
+                    num_experts,
+                    2 * intermediate_size_per_partition,
+                    hidden_size // self.group_size,
+                    dtype=torch.float32,
+                ),
+                requires_grad=False,
+            )
+            layer.register_parameter("w13_weight_scale_second", w13_weight_scale_second)
+            set_weight_attrs(w13_weight_scale_second, extra_weight_attrs)
+            w13_weight_offset_second = torch.nn.Parameter(
+                torch.empty(
+                    num_experts,
+                    2 * intermediate_size_per_partition,
+                    hidden_size // self.group_size,
+                    dtype=torch.float32,
+                ),
+                requires_grad=False,
+            )
+            layer.register_parameter("w13_weight_offset_second", w13_weight_offset_second)
+            set_weight_attrs(w13_weight_offset_second, extra_weight_attrs)
+
+            w2_weight_scale_second = torch.nn.Parameter(
+                torch.empty(
+                    num_experts,
+                    hidden_size,
+                    intermediate_size_per_partition // self.group_size,
+                    dtype=torch.float32,
+                ),
+                requires_grad=False,
+            )
+            layer.register_parameter("w2_weight_scale_second", w2_weight_scale_second)
+            set_weight_attrs(w2_weight_scale_second, extra_weight_attrs)
+
+            w2_weight_offset_second = torch.nn.Parameter(
+                torch.empty(
+                    num_experts,
+                    hidden_size,
+                    intermediate_size_per_partition // self.group_size,
+                    dtype=torch.float32,
+                ),
+                requires_grad=False,
+            )
+            layer.register_parameter("w2_weight_offset_second", w2_weight_offset_second)
+            set_weight_attrs(w2_weight_offset_second, extra_weight_attrs)
+
+        w13_scale_bias = torch.nn.Parameter(
+            torch.empty(
+                num_experts, 2 * intermediate_size_per_partition, 1, dtype=torch.float32
+            ),
+            requires_grad=False,
+        )
+        layer.register_parameter("w13_scale_bias", w13_scale_bias)
+        set_weight_attrs(w13_scale_bias, extra_weight_attrs)
+
+        w2_scale_bias = torch.nn.Parameter(
+            torch.empty(
+                num_experts, hidden_size, 16 // self.tp_size, dtype=torch.float32
+            ),
+            requires_grad=False,
+        )
+        layer.register_parameter("w2_scale_bias", w2_scale_bias)
+        set_weight_attrs(w2_scale_bias, extra_weight_attrs)
+
+    def process_weights_after_loading(self, layer: torch.nn.Module) -> None:
+        print(layer)
+        NPUW4A8Int8DynamicMoEMethod.process_weights_after_loading(layer)
+
+    def create_moe_runner(
+        self, layer: torch.nn.Module, moe_runner_config: "MoeRunnerConfig"
+    ):
+        self.moe_runner_config = moe_runner_config
+    
+    def apply(
+        self,
+        layer,
+        dispatch_output: "StandardDispatchOutput",
+    ) -> "CombineInput":
+        return NPUW4A8Int8DynamicMoEMethod.apply(layer, dispatch_output)
+    
+
+    def apply_without_routing_weights(
+        self,
+        layer,
+        hidden_states,
+        hidden_states_scale,
+        group_list_type,
+        group_list,
+        output_dtype,
+    ):
+        return NPUW4A8Int8DynamicMoEMethod.apply_without_routing_weights(layer,
+                                                                        hidden_states,
+                                                                        hidden_states_scale,
+                                                                        group_list_type,
+                                                                        group_list,
+                                                                        output_dtype,)
 
 
 class ModelSlimW8A8Int8MoE(ModelSlimMoEMethod):
