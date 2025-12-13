a097858292e8a5ec319efa33958995aa00ac41f8
fzyzcjy <ch271828n@outlook.com>
2025-12-13 21:23:02 +0800

    more

--- python/sglang/srt/managers/scheduler_metrics_mixin.py
+++ python/sglang/srt/managers/scheduler_metrics_mixin.py
@@ -317,14 +317,13 @@ class SchedulerMetricsMixin:
                 self.stats.swa_token_usage = swa_token_usage
             if self.is_ssm_model:
                 self.stats.mamba_usage = mamba_usage
+            self.stats.decode_sum_seq_lens = batch.seq_lens.sum().item()
             self.stats.gen_throughput = self.last_gen_throughput
             self.stats.num_queue_reqs = len(self.waiting_queue)
             self.stats.num_grammar_queue_reqs = len(self.grammar_queue)
             self.stats.cache_hit_rate = cache_hit_rate
 
             self.stats.max_total_num_tokens = self.max_total_num_tokens
-
-            self.stats.decode_sum_seq_lens = batch.seq_lens.sum().item()
 
             # Speculative decoding
             self.stats.spec_accept_rate = spec_accept_rate
