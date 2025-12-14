05a49b66c461cba0648305b15a6b1ad31b374002
Capronir <839972205@qq.com>
2025-12-14 16:47:16 +0800

    Add pre allocate page offsets
    Signed-off-by: Capronir <839972205@qq.com>

--- python/sglang/srt/mem_cache/allocator.py
+++ python/sglang/srt/mem_cache/allocator.py
@@ -429,6 +429,7 @@ class PagedTokenToKVPoolAllocator(BaseTokenToKVPoolAllocator):
     ):
         super().__init__(size, page_size, dtype, device, kvcache, need_sort)
         self.num_pages = size // page_size
+        self.page_offsets = torch.arange(self.page_size, device=self.device)
         self.debug_mode = get_bool_env_var("SGLANG_DEBUG_MEMORY_POOL")
         self.seen_max_num_extend_tokens_next_power_of_2 = 1
         self.clear()
@@ -451,7 +452,7 @@ class PagedTokenToKVPoolAllocator(BaseTokenToKVPoolAllocator):
 
         out_indices = (
             out_pages[:, None] * self.page_size
-            + torch.arange(self.page_size, device=self.device)
+            + self.page_offsets
         ).reshape(-1)
 
         return out_indices
