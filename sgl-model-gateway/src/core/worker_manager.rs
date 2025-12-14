0b9d512a3e0f365c2d0d9fd72111fcf2e4638f37
ppraneth <pranethparuchuri@gmail.com>
2025-12-15 00:56:24 +0530

    test-fix

--- sgl-model-gateway/src/core/worker_manager.rs
+++ sgl-model-gateway/src/core/worker_manager.rs
@@ -8,7 +8,7 @@ use std::{collections::HashMap, sync::Arc, time::Duration};
 use axum::response::{IntoResponse, Response};
 use futures::{
     future,
-    stream::{FuturesUnordered, StreamExt},
+    stream::{self, StreamExt},
 };
 use http::{Method, StatusCode};
 use serde_json::Value;
@@ -283,7 +283,7 @@ impl WorkerManager {
             return Err((StatusCode::SERVICE_UNAVAILABLE, "No available workers").into_response());
         }
 
-        let futures: FuturesUnordered<_> = workers
+        let futures: Vec<_> = workers
             .into_iter()
             .map(|worker| {
                 let client = client.clone();
@@ -320,7 +320,7 @@ impl WorkerManager {
             })
             .collect();
 
-        let responses: Vec<_> = futures
+        let responses: Vec<_> = stream::iter(futures)
             .buffer_unordered(MAX_CONCURRENT)
             .filter_map(|r| async { r })
             .collect()
