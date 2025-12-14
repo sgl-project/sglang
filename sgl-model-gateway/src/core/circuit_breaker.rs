267427b59c3ebed0f9a0447d4cf9d059be27f6a7
fzyzcjy <ch271828n@outlook.com>
2025-12-14 14:30:04 +0800

    more

--- sgl-model-gateway/src/core/circuit_breaker.rs
+++ sgl-model-gateway/src/core/circuit_breaker.rs
@@ -311,6 +311,7 @@ impl CircuitBreaker {
         }
     }
 
+    // TODO maybe publish whenever the value is changed
     fn publish_gauge_metrics(&self) {
         RouterMetrics::set_cb_consecutive_failures(&self.metric_label, self.failure_count());
         RouterMetrics::set_cb_consecutive_successes(&self.metric_label, self.success_count());
