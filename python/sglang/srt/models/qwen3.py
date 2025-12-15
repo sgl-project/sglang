a0f708e08885d9f0767647be124c18f87b208219
22dimensions <waitingwind@foxmail.com>
2025-12-16 00:42:59 +0800

    support GPTQ quantization on npu
    Signed-off-by: 22dimensions <waitingwind@foxmail.com>

--- python/sglang/srt/models/qwen3.py
+++ python/sglang/srt/models/qwen3.py
@@ -300,6 +300,10 @@ class Qwen3DecoderLayer(nn.Module):
             cache=(
                 [self.mlp.gate_up_proj.weight, self.mlp.down_proj.weight]
                 if _is_npu and not get_global_server_args().enable_piecewise_cuda_graph
+                and (
+                    hasattr(self.mlp.gate_up_proj, "weight")
+                    and hasattr(self.mlp.down_proj, "weight")
+                )
                 else None
             ),
         )
