d4d53e084e3f13cbc6c9d16e8412fe121a828aea
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2025-12-16 14:42:47 +0300

    Fix w4a4 weights loading

--- python/sglang/srt/layers/quantization/msmodelslim/schemes/msmodelslim_w4a4_int4.py
+++ python/sglang/srt/layers/quantization/msmodelslim/schemes/msmodelslim_w4a4_int4.py
@@ -34,6 +34,23 @@ class ModelSlimW4A4Int4(ModelSlimScheme):
             self.quant_config[prefix + ".weight"]
             == "W4A4_DYNAMIC"
         )
+        
+    @staticmethod
+    def get_weight(
+        input_size: int, output_size: int, params_dtype: torch.dtype
+    ) -> Dict[str, Any]:
+        params_dict = {"weight": torch.empty(output_size, input_size, dtype=torch.int8)}
+        return params_dict
+
+    @staticmethod
+    def get_perchannel_param(
+        output_size: int,
+        params_dtype: torch.dtype,
+    ) -> Dict[str, Any]:
+        params_dict = {}
+        params_dict["weight_scale"] = torch.empty(output_size, 1, dtype=params_dtype)
+        params_dict["weight_offset"] = torch.empty(output_size, 1, dtype=params_dtype)
+        return params_dict
 
     def create_weights(
         self,
@@ -47,17 +64,15 @@ class ModelSlimW4A4Int4(ModelSlimScheme):
     ) -> None:
         output_size_per_partition = sum(output_partition_sizes)
         weight_loader = extra_weight_attrs.get("weight_loader")
-
-        weight_dict = self.quant_method.get_weight(
-            input_size_per_partition, output_size_per_partition, params_dtype
-        )
+        
+        weight_dict = {"weight": torch.empty(output_size_per_partition, input_size_per_partition, dtype=torch.int8)}
         for weight_name, weight_param in weight_dict.items():
             param = torch.nn.Parameter(weight_param, requires_grad=False)
             set_weight_attrs(param, {"input_dim": 1, "output_dim": 0})
             layer.register_parameter(weight_name, param)
             set_weight_attrs(param, extra_weight_attrs)
 
-        pertensor_dict = self.quant_method.get_pertensor_param(params_dtype)
+        pertensor_dict = {}
         for pertensor_name, pertensor_param in pertensor_dict.items():
             param = PerTensorScaleParameter(
                 data=pertensor_param, weight_loader=weight_loader
@@ -65,10 +80,10 @@ class ModelSlimW4A4Int4(ModelSlimScheme):
             # disable warning
             param.ignore_warning = True
             layer.register_parameter(pertensor_name, param)
-
-        perchannel_dict = self.quant_method.get_perchannel_param(
-            output_size_per_partition, params_dtype
-        )
+        
+        perchannel_dict = {}
+        perchannel_dict["weight_scale"] = torch.empty(output_size_per_partition, 1, dtype=params_dtype)
+        perchannel_dict["weight_offset"] = torch.empty(output_size_per_partition, 1, dtype=params_dtype)
         for perchannel_name, perchannel_param in perchannel_dict.items():
             param = torch.nn.Parameter(perchannel_param, requires_grad=False)
             set_weight_attrs(param, {"output_dim": 0})
