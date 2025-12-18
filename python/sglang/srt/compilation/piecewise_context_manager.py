567ff393861d1a8075ffad66120f0c9e4fded832
Oasis-Git <ayw.sirius19@gmail.com>
2025-12-18 09:00:37 +0000

    update stream
    Signed-off-by: Oasis-Git <ayw.sirius19@gmail.com>

--- python/sglang/srt/compilation/piecewise_context_manager.py
+++ python/sglang/srt/compilation/piecewise_context_manager.py
@@ -1,8 +1,8 @@
-import torch
-
 from contextlib import contextmanager
 from dataclasses import dataclass
 from typing import Any, List, Optional
+
+import torch
 
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch
 
@@ -14,11 +14,14 @@ _pcg_capture_stream = None
 def is_in_piecewise_cuda_graph():
     return _in_piecewise_cuda_graph
 
+
 def is_in_pcg_torch_compile():
     return _in_pcg_torch_compile
 
+
 def get_pcg_capture_stream():
     return _pcg_capture_stream
+
 
 @contextmanager
 def enable_piecewise_cuda_graph_compile():
@@ -37,12 +40,14 @@ def enable_piecewise_cuda_graph():
 
     _in_piecewise_cuda_graph = False
 
+
 @contextmanager
 def set_pcg_capture_stream(stream: torch.cuda.Stream):
     global _pcg_capture_stream
     _pcg_capture_stream = stream
     yield
     _pcg_capture_stream = None
+
 
 @dataclass
 class ForwardContext:
