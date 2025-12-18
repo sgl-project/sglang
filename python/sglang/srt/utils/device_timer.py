e630e72890152aecd236ce1d1d4961fe6548384f
fzyzcjy <ch271828n@outlook.com>
2025-12-18 17:41:58 +0800

    more

--- python/sglang/srt/utils/device_timer.py
+++ python/sglang/srt/utils/device_timer.py
@@ -1,10 +1,12 @@
 from contextlib import contextmanager
 
+from sglang.srt.model_executor.forward_batch_info import ForwardMode
+
 
 class DeviceTimer:
     TODO
 
 
 @contextmanager
-def time_device_forward_pass(name: str):
+def time_device_forward_pass(forward_mode: ForwardMode):
     TODO
