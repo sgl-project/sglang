7c9d7d2106d7ea0f8403d386f728e63eb3984391
fzyzcjy <ch271828n@outlook.com>
2025-12-18 17:44:07 +0800

    more

--- python/sglang/srt/utils/device_timer.py
+++ python/sglang/srt/utils/device_timer.py
@@ -8,6 +8,10 @@ _ENABLE_METRICS_DEVICE_TIMER = envs.SGLANG_ENABLE_METRICS_DEVICE_TIMER.get()
 
 @contextmanager
 def time_device_forward_pass(forward_mode: ForwardMode):
+    if not _ENABLE_METRICS_DEVICE_TIMER:
+        yield
+        return
+
     TODO
 
 
