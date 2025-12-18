54537df352151934de586e374e5c9aabc4b45870
fzyzcjy <ch271828n@outlook.com>
2025-12-18 18:01:03 +0800

    more

--- python/sglang/srt/metrics/collector.py
+++ python/sglang/srt/metrics/collector.py
@@ -699,6 +699,9 @@ class SchedulerMetricsCollector:
         )
         self.realtime_decode_tokens_total.labels(**self.labels).inc(decode_tokens)
 
+    def increment_gpu_execution_seconds_total(self):
+        TODO
+
     def log_stats(self, stats: SchedulerStats) -> None:
         self._log_gauge(self.num_running_reqs, stats.num_running_reqs)
         self._log_gauge(self.num_used_tokens, stats.num_used_tokens)
