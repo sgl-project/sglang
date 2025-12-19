de341643bc5984e24bf6f3bf193fb6c9ec0b2611
Jincong Chen <chenjincong11@gmail.com>
2025-12-19 14:06:40 +0800

    Update test/srt/ep/test_deepep_large.py
    Co-authored-by: Baizhou Zhang <sobereddiezhang@gmail.com>

--- test/srt/ep/test_deepep_large.py
+++ test/srt/ep/test_deepep_large.py
@@ -197,6 +197,7 @@ class TestDeepseekV32TBO(CustomTestCase):
         )
         metrics = run_eval_few_shot_gsm8k(args)
         print(f"{metrics=}")
+self.assertGreater(metrics["accuracy"], 0.92)
 
     def test_bs_1_speed(self):
         args = BenchArgs(port=int(self.base_url.split(":")[-1]), max_new_tokens=2048)
