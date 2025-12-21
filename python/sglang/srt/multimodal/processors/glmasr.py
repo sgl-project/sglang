c8cf8c9b448cbc0229a3a9167962c73f01b8a3f8
zRzRzRzRzRzRzR <2448370773@qq.com>
2025-12-21 16:58:39 +0800

    reset

--- python/sglang/srt/multimodal/processors/glmasr.py
+++ python/sglang/srt/multimodal/processors/glmasr.py
@@ -1,18 +1,20 @@
 import re
 
 from sglang.srt.models.glmasr import GlmasrForConditionalGeneration
-from sglang.srt.multimodal.processors.base_processor import BaseMultimodalProcessor
-from sglang.srt.multimodal.processors.base_processor import MultimodalSpecialTokens
+from sglang.srt.multimodal.processors.base_processor import (
+    BaseMultimodalProcessor,
+    MultimodalSpecialTokens,
+)
 
 
-class GlmasrMultimodalProcessor(BaseMultimodalProcessor):
+class GlmasrProcessor(BaseMultimodalProcessor):
     models = [GlmasrForConditionalGeneration]
 
     def __init__(self, hf_config, server_args, _processor, *args, **kwargs):
         super().__init__(hf_config, server_args, _processor, *args, **kwargs)
         self.AUDIO_TOKEN = "<|begin_of_audio|><|pad|><|end_of_audio|>"
         self.AUDIO_TOKEN_REGEX = re.compile(
-            r"<\|begin_of_audio\|>(?:<\|pad\|>)+<\|end_of_audio\|>"
+            r"<\|begin_of_audio\|><\|pad\|><\|end_of_audio\|>"
         )
         # Collect special token ids
         tokenizer = self._processor.tokenizer
@@ -39,15 +41,21 @@ class GlmasrMultimodalProcessor(BaseMultimodalProcessor):
         )
         if base_output is None:
             return None
-
         mm_items, input_ids, ret = self.process_and_combine_mm_data(
             base_output, self.mm_tokens
         )
+        print(mm_items)
+        audio_lengths = ret.pop("attention_mask").sum(-1)
 
-        assert (
-            "feature_attention_mask" in ret
-        ), "feature_attention_mask not found in processor output"
-        input_lengths = ret["feature_attention_mask"].sum(dim=-1)
+        def _get_audio_token_length(audio_length: int, merge_factor: int = 4) -> int:
+            for padding, kernel_size, stride in [(1, 3, 1), (1, 3, 2)]:
+                audio_length = (
+                    audio_length + 2 * padding - (kernel_size - 1) - 1
+                ) // stride + 1
+            num_tokens = (audio_length - merge_factor) // merge_factor + 1
+            return min(num_tokens, 1500 // merge_factor)
+
+        input_lengths = _get_audio_token_length(audio_lengths)
         input_lengths = (input_lengths - 1) // 2 + 1
         output_lengths = (input_lengths - 2) // 2 + 1
 
