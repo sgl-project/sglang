a1bca48bb2dbbf466b958b429e3c47985c7167d2
Rohit Nagraj <rohitnagraj.99@gmail.com>
2025-12-22 16:26:12 +0800

    [AMD] Add wheel artifacts support for ROCm sgl-kernel
    Co-authored-by: Alan Kao <akao@amd.com>

--- scripts/update_kernel_whl_index.py
+++ scripts/update_kernel_whl_index.py
@@ -23,20 +23,31 @@ def check_wheel_cuda_version(path_name, target_cuda_version):
     return True
 
 
-def update_wheel_index(cuda_version=DEFAULT_CUDA_VERSION):
-    index_dir = pathlib.Path(f"sgl-whl/cu{cuda_version}/sgl-kernel")
-    index_dir.mkdir(exist_ok=True)
+def update_wheel_index(cuda_version=DEFAULT_CUDA_VERSION, rocm_version=None):
+    if rocm_version is None:
+        index_dir = pathlib.Path(f"sgl-whl/cu{cuda_version}/sgl-kernel")
+    else:
+        index_dir = pathlib.Path(f"sgl-whl/rocm{rocm_version}/sgl-kernel")
+    index_dir.mkdir(exist_ok=True, parents=True)
     base_url = "https://github.com/sgl-project/whl/releases/download"
 
     for path in sorted(pathlib.Path("sgl-kernel/dist").glob("*.whl")):
         # Skip the wheel if mismatches the passed in cuda_version
-        if not check_wheel_cuda_version(path.name, cuda_version):
+        if (
+            not check_wheel_cuda_version(path.name, cuda_version)
+            and rocm_version is None
+        ):
             continue
         with open(path, "rb") as f:
             sha256 = hashlib.sha256(f.read()).hexdigest()
-        ver = re.findall(
-            r"sgl_kernel-([0-9.]+(?:\.post[0-9]+)?)(?:\+cu[0-9]+)?-", path.name
-        )[0]
+        if rocm_version is None:
+            ver = re.findall(
+                r"sgl_kernel-([0-9.]+(?:\.post[0-9]+)?)(?:\+cu[0-9]+)?-", path.name
+            )[0]
+        else:
+            ver = re.findall(
+                r"sgl_kernel-([0-9.]+(?:\.post[0-9]+)?)(?:\+rocm[0-9]+)?-", path.name
+            )[0]
         full_url = f"{base_url}/v{ver}/{path.name}#sha256={sha256}"
         with (index_dir / "index.html").open("a") as f:
             f.write(f'<a href="{full_url}">{path.name}</a><br>\n')
@@ -44,9 +55,10 @@ def update_wheel_index(cuda_version=DEFAULT_CUDA_VERSION):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cuda", type=str, default="118")
+    parser.add_argument("--cuda", type=str, default=DEFAULT_CUDA_VERSION)
+    parser.add_argument("--rocm", type=str, default=None)
     args = parser.parse_args()
-    update_wheel_index(args.cuda)
+    update_wheel_index(args.cuda, args.rocm)
 
 
 if __name__ == "__main__":
