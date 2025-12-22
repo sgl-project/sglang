a0a83232eed693e63577a5a86596addd1982979c
Maksim <makcum888e@mail.ru>
2025-12-22 09:41:47 +0300

    is_npu

--- python/sglang/multimodal_gen/runtime/layers/lora/linear.py
+++ python/sglang/multimodal_gen/runtime/layers/lora/linear.py
@@ -31,11 +31,11 @@ from sglang.multimodal_gen.runtime.layers.linear import (
 from sglang.multimodal_gen.runtime.layers.vocab_parallel_embedding import (
     VocabParallelEmbedding,
 )
-from sglang.multimodal_gen.runtime.utils.common import is_cuda
+from sglang.multimodal_gen.runtime.utils.common import is_npu
 from sglang.multimodal_gen.utils import get_mixed_precision_state
 
-_is_cuda = is_cuda()
-if _is_cuda:
+_is_npu = is_npu()
+if not _is_npu:
     torch._dynamo.config.recompile_limit = 16
 
 
