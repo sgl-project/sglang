c0bd65d372d90ff1d9bd7ebacedcca5fabde9521
BBuf <1182563586@qq.com>
2025-12-24 00:02:09 +0800

    jit kernel support per-tensor quant

--- python/sglang/jit_kernel/include/sgl_kernel/fp8_utils.cuh
+++ python/sglang/jit_kernel/include/sgl_kernel/fp8_utils.cuh
@@ -59,15 +59,15 @@ namespace device {
 
 #ifndef USE_ROCM
 using FP8_TYPE = c10::Float8_e4m3fn;
-inline constexpr auto FP8_E4M3_MAX = std::numeric_limits<FP8_TYPE>::max();
+C10_HOST_DEVICE constexpr auto FP8_E4M3_MAX = std::numeric_limits<FP8_TYPE>::max();
 #else
 #if HIP_FP8_TYPE_FNUZ
 using FP8_TYPE = c10::Float8_e4m3fnuz;
-inline constexpr auto FP8_E4M3_MAX = 224.0f;
+constexpr auto FP8_E4M3_MAX = 224.0f;
 #else
 #if HIP_FP8_TYPE_E4M3
 using FP8_TYPE = c10::Float8_e4m3fn;
-inline constexpr auto FP8_E4M3_MAX = std::numeric_limits<FP8_TYPE>::max();
+C10_HOST_DEVICE constexpr auto FP8_E4M3_MAX = std::numeric_limits<FP8_TYPE>::max();
 #endif
 #endif
 #endif
