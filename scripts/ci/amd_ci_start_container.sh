5adab4580e700c7acf93e77ee260a0c47aac34dc
yctseng0211 <yctseng@amd.com>
2025-12-24 03:28:41 -0600

    remove temp-section

--- scripts/ci/amd_ci_start_container.sh
+++ scripts/ci/amd_ci_start_container.sh
@@ -172,8 +172,6 @@ docker run -dt --user root --device=/dev/kfd ${DEVICE_FLAG} \
   -e HF_HUB_DOWNLOAD_TIMEOUT=300 \
   -e MIOPEN_USER_DB_PATH=/sgl-data/miopen-cache \
   -e MIOPEN_CUSTOM_CACHE_DIR=/sgl-data/miopen-cache \
-  -e HF_HUB_DOWNLOAD_TIMEOUT=300 \
-  -e HF_HUB_ETAG_TIMEOUT=300 \
   --security-opt seccomp=unconfined \
   -w /sglang-checkout \
   --name ci_sglang \
