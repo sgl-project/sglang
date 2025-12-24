70863f1f7204972f14966026bae0d3cc0db1a68e
Xiake Sun <xisun@amd.com>
2025-12-24 23:01:18 +0800

    Enable Qwen3-Omni FP8 PTPC Model (#109)
    * Enable Qwen3-Omni model by forcing VIT run with BF16 precision
    
    * Add comments
    
    * Fix audio feature preprocess issue by force disable tokenizer truncation for audio input

--- python/sglang/srt/models/qwen3_omni_moe.py
+++ python/sglang/srt/models/qwen3_omni_moe.py
@@ -471,7 +471,8 @@ class Qwen3OmniMoeThinkerForConditionalGeneration(Qwen3VLMoeForConditionalGenera
         self.audio_tower = Qwen3OmniMoeAudioEncoder(config.audio_config, self.use_data_parallel)
         self.visual = Qwen3OmniMoeVisionEncoder(
             config.vision_config,
-            quant_config=quant_config,
+            #quant_config=quant_config,
+            quant_config=None,  # Workaround: pass None to quant_config to force Qwen3-Omni VIT with BF16 instead of FP8
             norm_eps=getattr(config, "rms_norm_eps", 1e-6),
             prefix=add_prefix("visual", prefix),
             use_data_parallel=self.use_data_parallel
