8196998a0315232e23923238c9ad4f84cc68824a
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-12-24 22:15:23 +0800

    Tiny add num retracted tokens metric (#15653)

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -2077,8 +2077,16 @@ class Scheduler(
             new_token_gained = new_available_tokens - old_available_tokens
 
             self.num_retracted_reqs = len(retracted_reqs)
-            if self.enable_metrics and (x := len(retracted_reqs)) > 0:
-                self.metrics_collector.increment_num_retracted_reqs(x)
+            if self.enable_metrics and len(retracted_reqs) > 0:
+                self.metrics_collector.increment_retracted_reqs(
+                    num_retracted_reqs=len(retracted_reqs),
+                    num_retracted_input_tokens=sum(
+                        len(r.origin_input_ids) for r in retracted_reqs
+                    ),
+                    num_retracted_output_tokens=sum(
+                        len(r.output_ids) for r in retracted_reqs
+                    ),
+                )
             self.new_token_ratio = new_token_ratio
             for req in reqs_to_abort:
                 abort_reason: FINISH_ABORT = req.to_finish
