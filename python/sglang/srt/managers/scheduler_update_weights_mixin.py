fc5e1becc68660a56c14a9d2391337716ce498bb
leihaodi <haodilei@gmail.com>
2025-12-24 12:28:18 +0800

    [Feat] Add --enable-draft-model-update to control online draft model updates(especially in RL)

--- python/sglang/srt/managers/scheduler_update_weights_mixin.py
+++ python/sglang/srt/managers/scheduler_update_weights_mixin.py
@@ -88,7 +88,10 @@ class SchedulerUpdateWeightsMixin:
         self: Scheduler, recv_req: UpdateWeightsFromTensorReqInput
     ):
         """Update the online model parameter from tensors."""
-        worker = self.draft_worker or self.tp_worker
+        if self.server_args.speculative_draft_model_update:
+            worker = self.tp_worker
+        else:
+            worker = self.draft_worker or self.tp_workerc
         success, message = worker.update_weights_from_tensor(recv_req)
         # TODO extract common code b/t update_weights_from_distributed and update_weights_from_tensor later
         if success:
