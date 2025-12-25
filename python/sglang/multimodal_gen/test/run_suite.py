b9df78d8d06eee3ca2d668e0164e34546e0f71cb
wangqi <2801406898@qq.com>
2025-12-26 00:29:47 +0800

    test-pr

--- python/sglang/multimodal_gen/test/run_suite.py
+++ python/sglang/multimodal_gen/test/run_suite.py
@@ -203,6 +203,7 @@ def main():
         sys.exit(0)
 
     print("suite_files_abs", suite_files_abs)
+    logger.info(suite_files_abs)
     # 3. collect all test items and partition by items (not files)
     all_test_items = collect_test_items(suite_files_abs, filter_expr=args.filter)
     logger.info(suite_files_rel)
