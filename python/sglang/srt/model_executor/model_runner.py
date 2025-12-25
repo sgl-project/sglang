67fa00bc3eb5d1e61d30cb636eda37c7bf1bf9eb
Liangsheng Yin <lsyincs@gmail.com>
2025-12-25 18:39:01 +0800

    keep log

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -1581,6 +1581,8 @@ class ModelRunner:
         )
         if self.mambaish_config is not None:
             rest_memory = self.handle_max_mamba_cache(rest_memory)
+
+        logger.info(f"The available memory for KV cache is {rest_memory:.2f} GB.")
         return int(int(rest_memory * (1 << 30)) // cell_size)
 
     def handle_max_mamba_cache(self, total_rest_memory):
