09d2a55587dcccbba350975cb3fcbafd94faa14b
wangqi <2801406898@qq.com>
2025-12-25 23:13:13 +0800

    test-pr

--- scripts/ci/npu_ci_install_dependency.sh
+++ scripts/ci/npu_ci_install_dependency.sh
@@ -51,11 +51,10 @@ wget -O "${BISHENG_NAME}" "${BISHENG_URL}" && chmod a+x "${BISHENG_NAME}" && "./
 
 ### Install sgl-kernel-npu
 mkdir sgl-kernel-npu
-cd sgl-kernel-npu
-wget https://github.com/sgl-project/sgl-kernel-npu/releases/download/2025.12.24/sgl-kernel-npu_2025.12.24_8.3.rc2_910b.zip
-unzip sgl-kernel-npu_2025.12.24_8.3.rc2_910b.zip
-${PIP_INSTALL} output/deep_ep*.whl output/sgl_kernel_npu*.whl
-cd "$(python3 -m pip show deep-ep | grep -E '^Location:' | awk '{print $2}')" && ln -s deep_ep/deep_ep_cpp*.so
+(cd sgl-kernel-npu && wget https://github.com/sgl-project/sgl-kernel-npu/releases/download/2025.12.24/sgl-kernel-npu_2025.12.24_8.3.rc2_910b.zip \
+&& unzip sgl-kernel-npu_2025.12.24_8.3.rc2_910b.zip \
+&& ${PIP_INSTALL} output/deep_ep*.whl output/sgl_kernel_npu*.whl \
+&& (cd "$(python3 -m pip show deep-ep | grep -E '^Location:' | awk '{print $2}')" && ln -s deep_ep/deep_ep_cpp*.so))
 
 
 
