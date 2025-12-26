6c79913c0b76b84419891d54508be451d0b11c3e
ZailiWang <zaili.wang@intel.com>
2025-12-26 10:50:16 +0800

    lint

--- python/sglang/srt/multimodal/processors/base_processor.py
+++ python/sglang/srt/multimodal/processors/base_processor.py
@@ -17,7 +17,15 @@ from sglang.srt.managers.schedule_batch import (
     MultimodalDataItem,
     MultimodalInputFormat,
 )
-from sglang.srt.utils import envs, is_npu, is_cpu, load_audio, load_image, load_video, logger
+from sglang.srt.utils import (
+    envs,
+    is_cpu,
+    is_npu,
+    load_audio,
+    load_image,
+    load_video,
+    logger,
+)
 from sglang.srt.utils.cuda_ipc_transport_utils import (
     MM_FEATURE_CACHE_SIZE,
     MM_ITEM_MEMORY_POOL_RECYCLE_INTERVAL,
