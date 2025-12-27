29ce7b3612da63ab8ed6ca2860a1ee351fbed2e1
Yuhao Yang <47235274+yhyang201@users.noreply.github.com>
2025-12-27 13:25:05 +0800

    [diffusion] chore: remove stepvideo code (#15918)

--- python/sglang/multimodal_gen/configs/pipeline_configs/__init__.py
+++ python/sglang/multimodal_gen/configs/pipeline_configs/__init__.py
@@ -12,7 +12,6 @@ from sglang.multimodal_gen.configs.pipeline_configs.hunyuan import (
     FastHunyuanConfig,
     HunyuanConfig,
 )
-from sglang.multimodal_gen.configs.pipeline_configs.stepvideo import StepVideoT2VConfig
 from sglang.multimodal_gen.configs.pipeline_configs.wan import (
     SelfForcingWanT2V480PConfig,
     WanI2V480PConfig,
@@ -33,7 +32,6 @@ __all__ = [
     "WanI2V480PConfig",
     "WanT2V720PConfig",
     "WanI2V720PConfig",
-    "StepVideoT2VConfig",
     "SelfForcingWanT2V480PConfig",
     "ZImagePipelineConfig",
 ]
