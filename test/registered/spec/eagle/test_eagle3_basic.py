9a9f9325c4701a35ec064634a2b4a6414e9bc437
Liangsheng Yin <lsyincs@gmail.com>
2025-12-27 23:26:28 +0800

    do not print useless server_info

--- test/registered/spec/eagle/test_eagle3_basic.py
+++ test/registered/spec/eagle/test_eagle3_basic.py
@@ -34,9 +34,8 @@ class TestEagle3Basic(EagleServerBase):
         metrics = run_eval(args)
         self.assertGreaterEqual(metrics["score"], 0.72)
 
-        server_info = requests.get(self.base_url + "/server_info")
-        print(f"{server_info=}")
-        avg_spec_accept_length = server_info.json()["internal_states"][0][
+        server_info = requests.get(self.base_url + "/server_info").json()
+        avg_spec_accept_length = server_info["internal_states"][0][
             "avg_spec_accept_length"
         ]
         print(f"{avg_spec_accept_length=}")
