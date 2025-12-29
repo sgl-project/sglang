e747a99de5f58f5c50333637dfc4da004b9cfa22
Zongyao Chen <ZongYao.Chen@linux.alibaba.com>
2025-12-29 19:13:03 +0800

    [sglang] Add gRPC support to encoder server for EPD mode

--- sgl-model-gateway/build.rs
+++ sgl-model-gateway/build.rs
@@ -13,6 +13,7 @@ macro_rules! set_env {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Rebuild triggers
     println!("cargo:rerun-if-changed=src/proto/sglang_scheduler.proto");
+    println!("cargo:rerun-if-changed=src/proto/sglang_encoder.proto");
     println!("cargo:rerun-if-changed=src/proto/vllm_engine.proto");
     println!("cargo:rerun-if-changed=Cargo.toml");
 
@@ -25,6 +26,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .compile_protos(
             &[
                 "src/proto/sglang_scheduler.proto",
+                "src/proto/sglang_encoder.proto",
                 "src/proto/vllm_engine.proto",
             ],
             &["src/proto"],
