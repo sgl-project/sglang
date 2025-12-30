308c9d2d38106f1e6c646d5b0d69be76957a46ff
Liangsheng Yin <lsyincs@gmail.com>
2025-12-30 13:01:56 +0800

    fix

--- python/sglang/srt/model_executor/model_runner_kv_cache_mixin.py
+++ python/sglang/srt/model_executor/model_runner_kv_cache_mixin.py
@@ -303,7 +303,7 @@ class ModelRunnerKVCacheMixin:
                 max_num_reqs, self.server_args.max_mamba_cache_size // ratio
             )
 
-        if self.spec_algorithm.supports_spec_v2():
+        if not self.spec_algorithm.is_none():
             if self.is_draft_worker:
                 self.max_total_num_tokens = self.server_args.draft_runner_cache_size
                 max_num_reqs = self.server_args.max_num_reqs
