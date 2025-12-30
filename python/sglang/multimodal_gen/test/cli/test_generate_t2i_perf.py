bb72e5287aab5df4c1b15ebf3b6826ca7d835e28
Mick <mickjagger19@icloud.com>
2025-12-30 22:21:34 +0800

    upd

--- python/sglang/multimodal_gen/test/cli/test_generate_t2i_perf.py
+++ python/sglang/multimodal_gen/test/cli/test_generate_t2i_perf.py
@@ -4,17 +4,17 @@ import unittest
 
 from sglang.multimodal_gen.configs.sample.sampling_params import DataType
 from sglang.multimodal_gen.runtime.utils.logging_utils import init_logger
-from sglang.multimodal_gen.test.cli.test_generate_common import GenerateBase
+from sglang.multimodal_gen.test.cli.test_generate_common import CLIBase
 
 logger = init_logger(__name__)
 
 
-class TestFlux_T2V(GenerateBase):
+class TestFlux_T2V(CLIBase):
     model_path = "black-forest-labs/FLUX.1-dev"
     extra_args = []
     data_type: DataType = DataType.IMAGE
 
 
 if __name__ == "__main__":
-    del GenerateBase
+    del CLIBase
     unittest.main()
