2031231e1ac3572db1c077f6e909f4edb84178f4
Marvin Tsai <62472426+mqhc2020@users.noreply.github.com>
2025-12-30 08:13:03 +0000

    MLA RoPE is now different

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -167,7 +167,6 @@ from sglang.srt.utils import (
     log_info_on_rank0,
     make_layers,
     use_intel_amx_backend,
-    disable_duck_shaping,
 )
 
 if _use_aiter_gfx95:
@@ -1883,23 +1882,22 @@ class DeepseekV2AttentionMLA(nn.Module):
                     fp8_dtype if self.kv_cache_dtype == "fp8_e4m3" else q_nope_out.dtype
                 )
 
-                with disable_duck_shaping():
-                    q, _, _, k, _ = fused_qk_rope_cat_and_cache_mla(
-                        q_nope_out,
-                        q_pe,
-                        k_nope,
-                        k_pe,
-                        forward_batch.token_to_kv_pool.get_key_buffer(
-                            self.attn_mqa.layer_id
-                        ),
-                        forward_batch.out_cache_loc,
-                        positions,
-                        cos,
-                        sin,
-                        self.attn_mqa.k_scale,
-                        self.rotary_emb.is_neox_style,
-                        q_out_dtype=kv_cache_dtype,
-                    )
+                q, _, _, k, _ = fused_qk_rope_cat_and_cache_mla(
+                    q_nope_out,
+                    q_pe,
+                    k_nope,
+                    k_pe,
+                    forward_batch.token_to_kv_pool.get_key_buffer(
+                        self.attn_mqa.layer_id
+                    ),
+                    forward_batch.out_cache_loc,
+                    positions,
+                    cos,
+                    sin,
+                    self.attn_mqa.k_scale,
+                    self.rotary_emb.is_neox_style,
+                    q_out_dtype=kv_cache_dtype,
+                )
 
                 save_kv_cache = False
             else:
--- python/sglang/srt/utils/common.py
+++ python/sglang/srt/utils/common.py
@@ -3795,13 +3795,3 @@ def get_torch_compile_disable_decorator(is_disable) -> Callable:
         return torch._dynamo.disable
     else:
         return lambda func: func
-
-
-@contextmanager
-def disable_duck_shaping():
-    prev = fx_config.use_duck_shape
-    fx_config.use_duck_shape = False
-    try:
-        yield
-    finally:
-        fx_config.use_duck_shape = prev
