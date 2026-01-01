3bca17aeebbe0436ab20f485dbef5f133deb0c8c
alphabetc1 <2508695655@qq.com>
2026-01-01 21:02:17 +0800

    fix

--- python/sglang/srt/environ.py
+++ python/sglang/srt/environ.py
@@ -226,7 +226,6 @@ class Envs:
     SGLANG_DISAGGREGATION_HEARTBEAT_MAX_FAILURE = EnvInt(2)
     SGLANG_DISAGGREGATION_WAITING_TIMEOUT = EnvInt(300)
 
-
     # Scheduler: others:
     SGLANG_EMPTY_CACHE_INTERVAL = EnvFloat(-1)  # in seconds. Set if you observe high memory accumulation over a long serving period.
     SGLANG_DISABLE_CONSECUTIVE_PREFILL_OVERLAP = EnvBool(False)
