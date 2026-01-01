ad4f45a76d76ee357a186a96a54388afeb36b586
fzyzcjy <ch271828n@outlook.com>
2026-01-01 22:13:31 +0800

    more

--- test/srt/test_prefill_delayer.py
+++ test/srt/test_prefill_delayer.py
@@ -27,7 +27,7 @@ class TestPrefillDelayerThroughput(CustomTestCase):
             "10",
         ]
 
-        with envs.SGLANG_SCHEDULER_DECREASE_PREFILL_IDLE.override(with_prefill_delayer):
+        with envs.SGLANG_SCHEDULER_DECREASE_PREFILL_IDLE.override(with_prefill_delayer), envs.SGLANG_PREFILL_DELAYER_MAX_DELAY_PASSES.override(100):
             process = popen_launch_server(
                 model,
                 base_url,
