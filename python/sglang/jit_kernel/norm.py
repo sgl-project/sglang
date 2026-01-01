f44b77a48cbc3b0fc6b97cb1ca44f5437904eaf0
DarkSharpness <2040703891@qq.com>
2026-01-02 02:22:53 +0800

    feat: integrate into srt

--- python/sglang/jit_kernel/norm.py
+++ python/sglang/jit_kernel/norm.py
@@ -17,8 +17,8 @@ if TYPE_CHECKING:
 
 
 @cache_once
-def _jit_qknorm_module(head_dims: int, dtype: torch.dtype) -> Module:
-    args = make_cpp_args(head_dims, is_arch_support_pdl(), dtype)
+def _jit_qknorm_module(head_dim: int, dtype: torch.dtype) -> Module:
+    args = make_cpp_args(head_dim, is_arch_support_pdl(), dtype)
     return load_jit(
         "qknorm",
         *args,
