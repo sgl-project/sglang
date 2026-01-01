b479f586d2392758f2042de38500259fd1d67b95
DarkSharpness <2040703891@qq.com>
2026-01-02 01:42:32 +0800

    feat: support jit set kv cache

---
+++ python/sglang/jit_kernel/kvcache.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import torch
+
+from sglang.jit_kernel.utils import (
+    cache_once,
+    is_arch_support_pdl,
+    load_jit,
+    make_cpp_args,
+)
+
+if TYPE_CHECKING:
+    from tvm_ffi.module import Module
+
+
+@cache_once
+def _jit_kvcache_module(element_bytes: int) -> Module:
+    args = make_cpp_args(element_bytes, is_arch_support_pdl())
+    return load_jit(
+        "kvcache",
+        *args,
+        cuda_files=["elementwise/kvcache.cuh"],
+        cuda_wrappers=[("store_cache", f"StoreKVCacheKernel<{args}>::run")],
+    )
+
+
+def store_cache(
+    k: torch.Tensor,
+    v: torch.Tensor,
+    k_cache: torch.Tensor,
+    v_cache: torch.Tensor,
+    indices: torch.Tensor,
+    *,
+    element_bytes: int = 0,
+    num_split: int = 0,  # can be tuned for performance
+) -> None:
+    """Store key and value tensors into KV cache at specified indices.
+
+    Args:
+        k (torch.Tensor): Key tensor of shape (batch_size, H * D).
+        v (torch.Tensor): Value tensor of shape (batch_size, H * D).
+        k_cache (torch.Tensor): Key cache tensor of shape (num_pages, H * D).
+        v_cache (torch.Tensor): Value cache tensor of shape (num_pages, H * D).
+        indices (torch.Tensor): Indices tensor of shape (batch_size,).
+    """
+    element_bytes = element_bytes or k.shape[-1] * k.element_size()
+    module = _jit_kvcache_module(element_bytes)
+    if num_split <= 0:
+        if element_bytes % 2048 == 0:
+            num_split = 4
+        elif element_bytes % 1024 == 0:
+            num_split = 2
+        else:
+            num_split = 1
+    module.store_cache(
+        k,
+        v,
+        k_cache,
+        v_cache,
+        indices,
+        num_split,
+    )
