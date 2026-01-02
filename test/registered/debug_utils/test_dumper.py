446826e668a21db14a51529db5b13e8420cc53b4
fzyzcjy <ch271828n@outlook.com>
2026-01-02 13:22:56 +0800

    more

--- test/registered/debug_utils/test_dumper.py
+++ test/registered/debug_utils/test_dumper.py
@@ -12,8 +12,7 @@ import torch.multiprocessing as mp
 from sglang.test.ci.ci_register import register_cuda_ci
 from sglang.test.test_utils import CustomTestCase
 
-# TODO it needs 0 gpu
-register_cuda_ci(est_time=60, suite="nightly-1-gpu", nightly=True)
+register_cuda_ci(est_time=30, suite="nightly-2-gpu", nightly=True)
 
 
 class TestDumperPureFunctions(CustomTestCase):
