7bde8dbd7ad9082642210f933d880d130806cfe3
fzyzcjy <ch271828n@outlook.com>
2026-01-04 09:59:13 +0800

    more

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1929,8 +1929,11 @@ class Scheduler(
             if self.prefill_delayer
             else True
         )
-        if (not delayer_allow_prefill) or (
-            (self.running_batch.batch_is_full or len(self.waiting_queue) == 0)
+        if (
+            (
+                (not delayer_allow_prefill)
+                or ((self.running_batch.batch_is_full or len(self.waiting_queue) == 0))
+            )
             and self.chunked_req is None
         ):
             return None
--- test/srt/test_prefill_delayer.py
+++ test/srt/test_prefill_delayer.py
@@ -32,13 +32,10 @@ class TestPrefillDelayerThroughput(CustomTestCase):
             "0.6",
         ]
 
-        # TODO further fix mem leak
         with envs.SGLANG_SCHEDULER_DECREASE_PREFILL_IDLE.override(
             with_prefill_delayer
         ), envs.SGLANG_PREFILL_DELAYER_MAX_DELAY_PASSES.override(
             100
-        ), envs.SGLANG_ENABLE_STRICT_MEM_CHECK_DURING_IDLE.override(
-            False
         ):
             process = popen_launch_server(
                 model,
