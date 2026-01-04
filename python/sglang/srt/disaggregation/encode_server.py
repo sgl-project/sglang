231edf1f545f78d35279e637bef389733a408086
liusy58 <liusy58@linux.alibaba.com>
2026-01-04 17:25:27 +0800

    clean

--- python/sglang/srt/disaggregation/encode_server.py
+++ python/sglang/srt/disaggregation/encode_server.py
@@ -1,5 +1,4 @@
 import asyncio
-import concurrent
 import concurrent.futures
 import ctypes
 import logging
@@ -44,7 +43,6 @@ from sglang.srt.utils import (
     load_audio,
     load_image,
     load_video,
-    logger,
     random_uuid,
 )
 
