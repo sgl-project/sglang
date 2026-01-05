a17be546f9ba8c3f69b2fcc6b3d3474ae1d4c674
Kangyan-Zhou <zky314343421@gmail.com>
2026-01-04 18:39:32 -0800

    Revert "[diffusion] feat: support warmup with resolutions (#16330)"
    This reverts commit 9a8ba3c189bfae88bdc40ddfd54bcb70c33be198.

--- python/sglang/multimodal_gen/runtime/pipelines_core/composed_pipeline_base.py
+++ python/sglang/multimodal_gen/runtime/pipelines_core/composed_pipeline_base.py
@@ -330,11 +330,10 @@ class ComposedPipelineBase(ABC):
         batch.log(server_args=server_args)
 
         # Execute each stage
-        if not batch.is_warmup:
-            logger.info(
-                "Running pipeline stages: %s",
-                list(self._stage_name_mapping.keys()),
-                main_process_only=True,
-            )
+        logger.info(
+            "Running pipeline stages: %s",
+            list(self._stage_name_mapping.keys()),
+            main_process_only=True,
+        )
 
         return self.executor.execute_with_profiling(self.stages, batch, server_args)
