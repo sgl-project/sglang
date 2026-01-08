353ffb36e398da262aab2e6b241f2f3132f5c4b8
fn <huangji.huang@antgroup.com>
2026-01-08 10:24:44 +0800

    set output size of Turbowan test

--- python/sglang/multimodal_gen/test/server/testcase_configs.py
+++ python/sglang/multimodal_gen/test/server/testcase_configs.py
@@ -297,7 +297,7 @@ TURBOWAN_I2V_sampling_params = DiffusionSamplingParams(
     prompt="The man in the picture slowly turns his head, his expression enigmatic and otherworldly. The camera performs a slow, cinematic dolly out, focusing on his face. Moody lighting, neon signs glowing in the background, shallow depth of field.",
     image_path="https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/5f/fa/56/5ffa56c2-ea1f-7a17-6bad-192ff9b6476d/825646124206.jpg/600x600bb.jpg",
     direct_url_test=True,
-    output_size="600x600",
+    output_size="960x960",
     num_frames=4,
     fps=4,
 )
