173cf51cacfb29058f4ce749d5c7e63ed454df88
Marvin Tsai <62472426+mqhc2020@users.noreply.github.com>
2026-01-08 07:48:15 +0000

    modify prototype

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -1882,14 +1882,16 @@ class DeepseekV2AttentionMLA(nn.Module):
                     fp8_dtype if self.kv_cache_dtype == "fp8_e4m3" else q_nope_out.dtype
                 )
 
-                q, _, _, k, _ = fused_qk_rope_cat_and_cache_mla(
+                k = forward_batch.token_to_kv_pool.get_key_buffer(
+                        self.attn_mqa.layer_id
+                    )
+
+                q = fused_qk_rope_cat_and_cache_mla(
                     q_nope_out,
                     q_pe,
                     k_nope,
                     k_pe,
-                    forward_batch.token_to_kv_pool.get_key_buffer(
-                        self.attn_mqa.layer_id
-                    ),
+                    k,
                     forward_batch.out_cache_loc,
                     positions,
                     cos,
@@ -1897,7 +1899,7 @@ class DeepseekV2AttentionMLA(nn.Module):
                     self.attn_mqa.k_scale,
                     self.rotary_emb.is_neox_style,
                     q_out_dtype=kv_cache_dtype,
-                )
+                )[0]
 
                 save_kv_cache = False
             else:
