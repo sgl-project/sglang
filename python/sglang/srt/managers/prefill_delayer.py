88addaf0f9f82a69073008818627662a101b2a3c
fzyzcjy <ch271828n@outlook.com>
2026-01-09 18:19:52 +0800

    more

--- python/sglang/srt/managers/prefill_delayer.py
+++ python/sglang/srt/managers/prefill_delayer.py
@@ -70,9 +70,15 @@ class PrefillDelayer:
             local_prefillable=local_prefillable,
             local_force_allow=local_force_allow,
         )
+        num_prefillable = global_prefillable.sum().item()
+        num_force_allow = global_force_allow.sum().item()
 
         if global_force_allow.max().item() > 0:
-            self._reset(outcome="token_usage_watermark")
+            self._reset(
+                outcome="token_usage_watermark",
+                debug_num_prefillable=num_prefillable,
+                debug_num_force_allow=num_force_allow,
+            )
             return True
 
         global_exists_not_prefillable = global_prefillable.min().item() == 0
@@ -89,20 +95,24 @@ class PrefillDelayer:
                 return False
 
         is_timeout = global_mixed_prefillable
-        self._reset(outcome="timeout" if is_timeout else TODO)
+        self._reset(
+            outcome="timeout" if is_timeout else TODO,
+            debug_num_prefillable=num_prefillable,
+            debug_num_force_allow=num_force_allow,
+        )
         return True
 
-    def _reset(self, outcome: str) -> None:
+    def _reset(self, outcome: str, debug_num_prefillable: int, debug_num_force_allow: int) -> None:
         if _DEBUG_LOG:
             if outcome == "timeout":
                 logger.info(
-                    f"PrefillDelayer timeout thus not forbid prefill (num_prefillable={global_prefillable.sum()})"
+                    f"PrefillDelayer timeout thus not forbid prefill (num_prefillable={debug_num_prefillable})"
                 )
             elif outcome == "token_usage_watermark":
                 logger.info(
                     f"PrefillDelayer force allow prefill due to low watermark. "
-                    f"(num_prefillable={global_prefillable.sum().item()}, "
-                    f"num_force_allow={global_force_allow.sum().item()})"
+                    f"(num_prefillable={debug_num_prefillable}, "
+                    f"num_force_allow={debug_num_force_allow})"
                 )
 
         TODO_should_this_info_is_not_none
