4f4eb445e6f65515bd1356a6dae25087b58aea69
liusy58 <liusy58@linux.alibaba.com>
2026-01-12 22:10:54 +0800

    fix

--- python/sglang/srt/disaggregation/mooncake/conn.py
+++ python/sglang/srt/disaggregation/mooncake/conn.py
@@ -32,6 +32,7 @@ from sglang.srt.disaggregation.mooncake.utils import (
     check_mooncake_custom_mem_pool_enabled,
 )
 from sglang.srt.disaggregation.utils import DisaggregationMode
+from sglang.srt.environ import envs
 from sglang.srt.server_args import ServerArgs
 from sglang.srt.utils import format_tcp_address, get_int_env_var, is_valid_ipv6_address
 
@@ -551,7 +552,9 @@ class MooncakeKVManager(CommonKVManager):
         dst_aux_ptrs: list[int],
     ):
         # TODO(shangming): Fix me when nvlink_transport of Mooncake is bug-free
-        if self.enable_custom_mem_pool and self.custom_mem_pool_type == "NVLINK":
+        if (
+            self.enable_custom_mem_pool and self.custom_mem_pool_type == "NVLINK"
+        ) or envs.SGLANG_SEND_AUX_TCP.get():
             return self.send_aux_tcp(req, prefill_aux_index, dst_aux_ptrs)
 
         transfer_blocks = []
