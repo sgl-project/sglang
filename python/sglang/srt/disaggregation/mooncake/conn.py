ef13a634a86c7f9fb65373418609d5a92aa5734c
liusy58 <liusy58@linux.alibaba.com>
2026-01-12 23:38:32 +0800

    rename

--- python/sglang/srt/disaggregation/mooncake/conn.py
+++ python/sglang/srt/disaggregation/mooncake/conn.py
@@ -554,7 +554,7 @@ class MooncakeKVManager(CommonKVManager):
         # TODO(shangming): Fix me when nvlink_transport of Mooncake is bug-free
         if (
             self.enable_custom_mem_pool and self.custom_mem_pool_type == "NVLINK"
-        ) or envs.SGLANG_SEND_AUX_TCP.get():
+        ) or envs.SGLANG_MOONCAKE_SEND_AUX_TCP.get():
             return self.send_aux_tcp(req, prefill_aux_index, dst_aux_ptrs)
 
         transfer_blocks = []
