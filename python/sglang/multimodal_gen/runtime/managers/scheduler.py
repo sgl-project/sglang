7ff8980b8176d5d9121b13bbb304bacb04db5215
fn <huangji.huang@antgroup.com>
2026-01-12 11:25:08 +0800

    rename

--- python/sglang/multimodal_gen/runtime/managers/scheduler.py
+++ python/sglang/multimodal_gen/runtime/managers/scheduler.py
@@ -170,7 +170,7 @@ class Scheduler:
                     input_path = asyncio.run(
                         save_image_to_path(
                             MINIMUM_PICTURE_BASE64_FOR_WARMUP,
-                            os.path.join(uploads_dir, "warmup_image"),
+                            os.path.join(uploads_dir, "warmup_image.jpg"),
                         )
                     )
                     req = Req(
