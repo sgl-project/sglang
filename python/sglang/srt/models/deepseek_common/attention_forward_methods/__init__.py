9b3b23d8d5f92b12fc89eb7d92daa5265062ae33
Baizhou Zhang <sobereddiezhang@gmail.com>
2026-01-12 22:22:44 +0800

    migrate forward batch methods

--- python/sglang/srt/models/deepseek_common/attention_forward_methods/__init__.py
+++ python/sglang/srt/models/deepseek_common/attention_forward_methods/__init__.py
@@ -1,7 +1,9 @@
 from .forward_methods import AttnForwardMethod
 from .forward_mha import DeepseekMHAForwardMixin
+from .forward_mha_batch_methods import ForwardBatchDeepSeekMHAMixin
 
 __all__ = [
     "AttnForwardMethod",
     "DeepseekMHAForwardMixin",
+    "ForwardBatchDeepSeekMHAMixin",
 ]
