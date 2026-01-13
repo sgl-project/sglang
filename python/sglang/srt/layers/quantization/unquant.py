4c096b6ec8f65a14c039b7343b0411928df97742
Michelle Wu <michellewu351@gmail.com>
2026-01-13 10:01:59 +0800

    Update unquant.py

--- python/sglang/srt/layers/quantization/unquant.py
+++ python/sglang/srt/layers/quantization/unquant.py
@@ -42,6 +42,7 @@ if TYPE_CHECKING:
 _is_cpu_amx_available = cpu_has_amx_support()
 _is_hip = is_hip()
 _is_cpu = is_cpu()
+_is_npu = is_npu()
 _use_aiter = get_bool_env_var("SGLANG_USE_AITER") and _is_hip
 
 if _use_aiter:
