47f829cf6c5df20d632c3580b14d8b6687b459a0
Jacob Gordon <Jacob.Gordon@amd.com>
2026-01-13 23:14:18 +0000

    refactor: puts japanese before chinese

--- python/sglang/test/simple_eval_mgsm.py
+++ python/sglang/test/simple_eval_mgsm.py
@@ -30,10 +30,10 @@ LATIN_SCRIPT_LANGUAGES = [
 
 NON_LATIN_SCRIPT_LANGUAGES = [
     "bn",
-    "ja",
     "ru",
     "te",
     "th",
+    "ja",
     "zh",
 ]
 
@@ -45,11 +45,11 @@ LANG_TO_FPATH = {
     "en": "https://openaipublic.blob.core.windows.net/simple-evals/mgsm_en.tsv",
     "es": "https://openaipublic.blob.core.windows.net/simple-evals/mgsm_es.tsv",
     "fr": "https://openaipublic.blob.core.windows.net/simple-evals/mgsm_fr.tsv",
-    "ja": "https://openaipublic.blob.core.windows.net/simple-evals/mgsm_ja.tsv",
     "ru": "https://openaipublic.blob.core.windows.net/simple-evals/mgsm_ru.tsv",
     "sw": "https://openaipublic.blob.core.windows.net/simple-evals/mgsm_sw.tsv",
     "te": "https://openaipublic.blob.core.windows.net/simple-evals/mgsm_te.tsv",
     "th": "https://openaipublic.blob.core.windows.net/simple-evals/mgsm_th.tsv",
+    "ja": "https://openaipublic.blob.core.windows.net/simple-evals/mgsm_ja.tsv",
     "zh": "https://openaipublic.blob.core.windows.net/simple-evals/mgsm_zh.tsv",
 }
 LANG_TO_INSTRUCTIONS = {
@@ -58,11 +58,11 @@ LANG_TO_INSTRUCTIONS = {
     "de": """Löse dieses Mathematikproblem. Gib die Schritte zur Begründung an, bevor du die endgültige Antwort in der letzten Zeile alleine im Format "Antwort:" gibst. Füge nichts anderes als die ganzzahlige Antwort nach "Antwort:" hinzu.\n\n{input}""",
     "es": """Resuelve este problema matemático. Proporciona los pasos de razonamiento antes de dar la respuesta final en la última línea por sí misma en el formato de "Respuesta:". No añadas nada más que la respuesta entera después de "Respuesta:".\n\n{input}""",
     "fr": """Résolvez ce problème de mathématiques. Donnez les étapes de raisonnement avant de fournir la réponse finale sur la dernière ligne elle-même dans le format de "Réponse:". N'ajoutez rien d'autre que la réponse entière après "Réponse:".\n\n{input}""",
-    "ja": """の数学の問題を解いてください。最終的な答えを出す前に、解答の推論過程を記述してください。そして最後の行には "答え:" の形式で答えを記述し、その後には整数の答え以外何も追加しないでください。\n\n{input}""",
     "ru": """Решите эту математическую задачу. Объясните шаги рассуждения перед тем, как дать окончательный ответ в последней строке сам по себе в формате "Ответ:". Не добавляйте ничего, кроме целочисленного ответа после "Ответ:".\n\n{input}""",
     "sw": """Suluhisha tatizo hili la hesabu. Toa hatua za mantiki kabla ya kutoa jibu la mwisho kwenye mstari wa mwisho peke yake katika muundo wa "Jibu:". Usiongeze chochote kingine isipokuwa jibu la integer baada ya "Jibu:".\n\n{input}""",
     "te": """ఈ గణిత సమస్యను పరిష్కరించండి. చివరి సమాధానాన్ని ఇవ్వదానికి ముందు తర్కాత్మక అదుగులను ఇవ్వండి. చివరి పంక్తిలో మాత్రమే 'సమాధానం:' అనే ఆకారంలో చివరి సమాధానాద్ని ఇవ్వండి సమాధానం: తర్వాత పూర్ణాంక సమాధానానికి తప్పించి ఎదేనా చేర్చవద్దు.\n\n{input}""",
     "th": """แก้ปัญหาคณิตศาสตร์นี้ ให้ให้ขั้นตอนการใช้เหตุผลก่อนที่จะให้คำตอบสุดท้ายในบรรทัดสุดท้ายโดยอยู่ในรูปแบบ "คำตอบ:" ไม่ควรเพิ่มอะไรนอกจากคำตอบที่เป็นจำนวนเต็มหลังจาก "คำตอบ:"\n\n{input}""",
+    "ja": """の数学の問題を解いてください。最終的な答えを出す前に、解答の推論過程を記述してください。そして最後の行には "答え:" の形式で答えを記述し、その後には整数の答え以外何も追加しないでください。\n\n{input}""",
     "zh": """解决这个数学问题。在最后一行给出答案前，请提供推理步骤。最后一行应该以 "答案: " 的形式独立给出答案。在 "答案：" 后不要添加除整数答案之外的任何内容。\n\n{input}""",
 }
 
@@ -72,11 +72,11 @@ LANG_TO_ANSWER_PREFIX = {
     "de": "Antwort",
     "es": "Respuesta",
     "fr": "Réponse",
-    "ja": "答え",
     "ru": "Ответ",
     "sw": "Jibu",
     "te": "సమాధానం",
     "th": "คำตอบ",
+    "ja": "答え",
     "zh": "答案",
 }
 
