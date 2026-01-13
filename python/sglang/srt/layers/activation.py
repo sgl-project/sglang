acae791afe75b865aee4a42cf1df32d72f53f806
McZyWu <zhuoyun.wu.23@ucl.ac.uk>
2026-01-13 21:41:58 +0800

    being concise for reuse

--- python/sglang/srt/layers/activation.py
+++ python/sglang/srt/layers/activation.py
@@ -131,6 +131,8 @@ class GeluAndMul(MultiPlatformOp):
         return self._forward_impl(x)
 
     def forward_npu(self, x: torch.Tensor) -> torch.Tensor:
+        if get_bool_env_var("SGLANG_ENABLE_TORCH_COMPILE"):
+            return self.forward_native(x)
         y_npu, gelu_npu = torch_npu.npu_geglu(
             x,
             dim=-1,
