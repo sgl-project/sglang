edbba1b185a116838e02d0ae5a7bb71a800efb31
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-01-15 15:26:33 +0300

    Fix lint issue

--- python/sglang/srt/hardware_backend/npu/quantization/fused_moe_method_npu.py
+++ python/sglang/srt/hardware_backend/npu/quantization/fused_moe_method_npu.py
@@ -153,7 +153,6 @@ class NPUW8A8Int8DynamicMoEMethod(_NPUFusedMoEMethodBase):
     def process_weights_after_loading(self, layer: torch.nn.Module) -> None:
         layer.w13_weight.data = npu_format_cast(layer.w13_weight.data.transpose(1, 2))
         layer.w2_weight.data = npu_format_cast(layer.w2_weight.data.transpose(1, 2))
-        
         layer.w13_weight_scale = torch.nn.Parameter(
             layer.w13_weight_scale.data.squeeze(-1), requires_grad=False
         )
