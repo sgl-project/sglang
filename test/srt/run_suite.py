9a974cc6ad189c78cf234ff3f8a830c1ab3a58b7
Shangming Cai <csmthu@gmail.com>
2026-01-15 19:39:26 +0800

    tmp

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -23,13 +23,13 @@ suites = {
         TestFile("test_deepseek_v32_basic.py", 360),
         TestFile("test_deepseek_v32_mtp.py", 360),
         TestFile("models/test_mimo_models.py", 200),
+        TestFile("quant/test_w4a8_deepseek_v3.py", 520),
+        TestFile("test_disaggregation_different_tp.py", 600),
+        TestFile("test_disaggregation_pp.py", 180),
     ],
     "per-commit-8-gpu-h20": [
-        # TestFile("quant/test_w4a8_deepseek_v3.py", 520),
-        # TestFile("test_disaggregation_different_tp.py", 600),
         TestFile("test_disaggregation_dp_attention.py", 155),
         TestFile("test_disaggregation_hybrid_attention.py", 400),
-        # TestFile("test_disaggregation_pp.py", 180),
     ],
     "per-commit-4-gpu-b200": [
         TestFile("test_deepseek_v3_fp4_4gpu.py", 1500),
