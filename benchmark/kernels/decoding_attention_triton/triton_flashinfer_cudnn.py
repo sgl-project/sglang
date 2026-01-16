0eac677a805f9fd1ba89b64d5bc81bd1e09f3fdd
Mohammad Miadh Angkad <MAngkad.BSDSBA2027@aim.edu>
2026-01-17 05:13:58 +0800

    Fix benchmark import for should_use_tensor_core

--- benchmark/kernels/decoding_attention_triton/triton_flashinfer_cudnn.py
+++ benchmark/kernels/decoding_attention_triton/triton_flashinfer_cudnn.py
@@ -4,10 +4,10 @@ import math
 import cudnn
 import torch
 import torch.utils.benchmark as benchmark
-from flashinfer import BatchDecodeWithPagedKVCacheWrapper
 
+from flashinfer import BatchDecodeWithPagedKVCacheWrapper
+from sglang.srt.layers.attention.flashinfer_backend import should_use_tensor_core
 from sglang.srt.layers.attention.triton_ops.decode_attention import decode_attention_fwd
-from sglang.srt.utils import should_use_tensor_core
 
 
 def benchmark_forward(
