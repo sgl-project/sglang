545187c1c63ad2a1afd51ccbc680666d10c28884
Mengni Wang <mengni.wang@intel.com>
2026-01-19 13:58:26 +0000

    add arg
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -115,6 +115,7 @@ QUANTIZATION_CHOICES = [
     "compressed-tensors",  # for Ktransformers
     "modelslim",  # for NPU
     "quark_int4fp8_moe",
+    "autoround_w4a16",
 ]
 
 SPECULATIVE_DRAFT_MODEL_QUANTIZATION_CHOICES = [*QUANTIZATION_CHOICES, "unquant"]
