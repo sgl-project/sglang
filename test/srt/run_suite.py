a07c34806fa5461cfbb833e1cb08000601e39f18
Baizhou Zhang <sobereddiezhang@gmail.com>
2026-01-19 14:43:00 +0800

    upd

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -74,7 +74,6 @@ suites = {
         TestFile(
             "models/test_qwen3_next_models_pcg.py"
         ),  # Disabled: intermittent failures, see #17039
-        TestFile("ep/test_deepep_large.py", 563),  # Disabled: see #17175
     ],
 }
 
