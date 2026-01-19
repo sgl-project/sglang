90b9da49f042266f5b6caff8a49cefa26e7c9b61
Xinyuan Tong <xinyuantong.cs@gmail.com>
2026-01-19 16:49:45 +0000

    remove dead config codes
    Signed-off-by: Xinyuan Tong <xinyuantong.cs@gmail.com>

--- python/sglang/srt/configs/model_config.py
+++ python/sglang/srt/configs/model_config.py
@@ -425,16 +425,6 @@ class ModelConfig:
             self.attention_arch = AttentionArch.MLA
             self.kv_lora_rank = self.hf_text_config.kv_lora_rank
             self.qk_rope_head_dim = self.hf_text_config.qk_rope_head_dim
-        elif "Glm4MoeLiteForCausalLM" in self.hf_config.architectures and getattr(
-            self.hf_text_config, "use_mla", True
-        ):
-            self.head_dim = 256
-            self.attention_arch = AttentionArch.MLA
-            self.kv_lora_rank = self.hf_config.kv_lora_rank
-            self.qk_nope_head_dim = self.hf_config.qk_nope_head_dim
-            self.qk_rope_head_dim = self.hf_config.qk_rope_head_dim
-            self.v_head_dim = self.hf_config.v_head_dim
-            self.scaling = 1 / math.sqrt(self.qk_nope_head_dim + self.qk_rope_head_dim)
         elif "KimiVLForConditionalGeneration" in self.hf_config.architectures:
             self.head_dim = 256
             self.attention_arch = AttentionArch.MLA
