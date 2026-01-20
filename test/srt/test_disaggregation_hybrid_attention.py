4e4f2db9a53ab895017fd5eeb85d8ce797af3a1c
xjx471258437 <151611486+xjx471258437@users.noreply.github.com>
2026-01-20 15:11:16 +0800

    bugfix

--- test/srt/test_disaggregation_hybrid_attention.py
+++ test/srt/test_disaggregation_hybrid_attention.py
@@ -17,7 +17,7 @@ class TestDisaggregationHybridAttentionMamba(PDDisaggregationServerBase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.model = "/Qwen3-Next-80B-A3B-Instruct"
+        cls.model = "Qwen/Qwen3-Next-80B-A3B-Instruct"
 
         # Non blocking start servers
         cls.start_prefill()
