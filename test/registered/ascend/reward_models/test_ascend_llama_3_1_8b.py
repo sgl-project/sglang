7cda2e09e07fb94c68417f0446b8ae37afb05d86
cy <chenyang08056032@163.com>
2026-01-20 16:22:43 +0800

    modify lint

--- test/registered/ascend/reward_models/test_ascend_llama_3_1_8b.py
+++ test/registered/ascend/reward_models/test_ascend_llama_3_1_8b.py
@@ -10,10 +10,11 @@ from sglang.test.test_utils import CustomTestCase
 register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 MODELS = [
-    ("/root/.cache/huggingface/hub/models--Skywork--Skywork-Reward-Llama-3.1-8B-v0.2/snapshots/d4117fbfd81b72f41b96341238baa1e3e90a4ce1/",
-     1,
-     4e-2,
-     ),
+    (
+        "/root/.cache/huggingface/hub/models--Skywork--Skywork-Reward-Llama-3.1-8B-v0.2/snapshots/d4117fbfd81b72f41b96341238baa1e3e90a4ce1/",
+        1,
+        4e-2,
+    ),
 ]
 TORCH_DTYPES = [torch.float16]
 
