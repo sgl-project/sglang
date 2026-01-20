a296f51d493638ca2564e7a2346afb96bc941405
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-01-20 17:43:03 +0300

    Remove unused code

--- python/sglang/srt/hardware_backend/npu/quantization/torch_npu_kernels.py
+++ python/sglang/srt/hardware_backend/npu/quantization/torch_npu_kernels.py
@@ -84,42 +84,16 @@ class TorchNpuKernelsRunnerCore(MoeRunnerCore):
         topk_ids = runner_input.topk_ids
         topk_weights = runner_input.topk_weights
 
-        '''common_kwargs = dict(
-            routing_data=runner_input.routing_data,
-            gather_indx=runner_input.gather_indx,
-            scatter_indx=None if self.config.no_combine else runner_input.scatter_indx,
-            inplace=False,
-            activation=self.config.activation,
-            apply_router_weight_on_input=self.config.apply_router_weight_on_input,
-            global_num_experts=quant_info.global_num_experts,
-        )'''
-
-
-        if True:
-            output = npu_fused_experts(
-                hidden_states=hidden_states,
-                w13=quant_info.w13_weight,
-                w13_scale=quant_info.w13_scale,
-                w2=quant_info.w2_weight,
-                w2_scale=quant_info.w2_scale,
-                topk_weights=topk_weights,
-                topk_ids=topk_ids,
-                top_k=topk_ids.shape[1],
-            )
-        else:
-            output = torch_npu_fused_moe_without_routing_weights_bf16(
-                hidden_states=hidden_states,
-                w1=quant_info.w13_weight,
-                w2=quant_info.w2_weight,
-                **common_kwargs,
-            )
-
-        '''if self.config.no_combine:
-            tokens = runner_input.hidden_states.shape[0]
-            hidden = runner_input.hidden_states.shape[-1]
-            total_rows = output.shape[0]
-            top_k = total_rows // tokens
-            output = output.view(tokens, top_k, hidden)'''
+        output = npu_fused_experts(
+            hidden_states=hidden_states,
+            w13=quant_info.w13_weight,
+            w13_scale=quant_info.w13_scale,
+            w2=quant_info.w2_weight,
+            w2_scale=quant_info.w2_scale,
+            topk_weights=topk_weights,
+            topk_ids=topk_ids,
+            top_k=topk_ids.shape[1],
+        )
 
         return TorchNpuKernelsRunnerOutput(hidden_states=output)
 
@@ -140,15 +114,9 @@ def pre_permute_standard_to_torch_npu_kernels(
     runner_config: MoeRunnerConfig,
     running_state: dict,
 ) -> TorchNpuKernelsRunnerInput:
-    from sglang.srt.layers.moe.topk import TopKOutputChecker
 
     hidden_states = dispatch_output.hidden_states
     topk_output = dispatch_output.topk_output
-
-    #assert TopKOutputChecker.format_is_triton_kernels(
-    #    topk_output
-    #), "Torch-npu-kernel runner expects TorchNpuKernelTopKOutput"
-
     topk_weights, topk_ids, _ = topk_output
     topk_ids = topk_ids.to(torch.int32)
     topk_weights = topk_weights.to(hidden_states.dtype)
