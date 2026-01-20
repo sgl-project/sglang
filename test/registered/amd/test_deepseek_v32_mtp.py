b165d5f899ebc93beb19232bcc4336670ebc5f06
yctseng0211 <yctseng@amd.com>
2026-01-19 23:43:29 -0600

    init commit

--- test/registered/amd/test_deepseek_v32_mtp.py
+++ test/registered/amd/test_deepseek_v32_mtp.py
@@ -44,7 +44,7 @@ class TestDeepseekV32DPMTP(CustomTestCase):
             "--mem-frac",
             "0.7",
             "--model-loader-extra-config",
-            '{"enable_multithread_load": true}',
+            '{"enable_multithread_load": false}',
         ]
         if is_in_amd_ci():
             other_args += [
@@ -137,7 +137,7 @@ class TestDeepseekV32TPMTP(CustomTestCase):
             "--mem-frac",
             "0.7",
             "--model-loader-extra-config",
-            '{"enable_multithread_load": true}',
+            '{"enable_multithread_load": false}',
         ]
         if is_in_amd_ci():
             other_args += [
