f9597f203813cfc0941f939ed8121ebf5414ce6e
fn <huangji.huang@antgroup.com>
2026-01-20 19:38:36 +0800

    add attribution to THU-ML/sla

--- python/sglang/multimodal_gen/runtime/layers/attention/backends/sparse_linear_attn.py
+++ python/sglang/multimodal_gen/runtime/layers/attention/backends/sparse_linear_attn.py
@@ -1,5 +1,18 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copy and Adapted from https://github.com/thu-ml/TurboDiffusion/blob/main/turbodiffusion/SLA/core.py and https://github.com/thu-ml/SLA/blob/main/SageSLA/core.py
+"""
+Copyright (c) 2025 by SLA team.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+
+This implementation is adapted from: from https://github.com/thu-ml/TurboDiffusion/blob/main/turbodiffusion/SLA/core.py and https://github.com/thu-ml/SLA/blob/main/SageSLA/core.py
+Citation (please cite if you use this code):
+
+@article{zhang2025sla,
+  title={SLA: Beyond Sparsity in Diffusion Transformers via Fine-Tunable Sparse-Linear Attention},
+  author={Jintao Zhang and Haoxu Wang and Kai Jiang and Shuo Yang and Kaiwen Zheng and Haocheng Xi and Ziteng Wang and Hongzhou Zhu and Min Zhao and Ion Stoica and Joseph E. Gonzalez and Jun Zhu and Jianfei Chen},
+  journal={arXiv preprint arXiv:2509.24006},
+  year={2025}
+}
+"""
 
 from collections.abc import Callable
 from dataclasses import dataclass
