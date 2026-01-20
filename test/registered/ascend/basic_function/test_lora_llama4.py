387159755d494508e7522efdcda6d991a1300858
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-01-20 17:31:56 +0800

    Update test_lora_llama4.py

--- test/registered/ascend/basic_function/test_lora_llama4.py
+++ test/registered/ascend/basic_function/test_lora_llama4.py
@@ -2,6 +2,7 @@ import unittest
 from types import SimpleNamespace
 
 from sglang.srt.utils import kill_process_tree
+from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
