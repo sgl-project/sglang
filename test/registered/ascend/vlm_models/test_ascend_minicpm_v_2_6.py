54c7b122854c1e10ce481b6ec50097705bd57b7d
cy <chenyang08056032@163.com>
2026-01-21 17:14:20 +0800

    add minicpm_o/v model testcase

--- test/registered/ascend/vlm_models/test_ascend_minicpm_v_2_6.py
+++ test/registered/ascend/vlm_models/test_ascend_minicpm_v_2_6.py
@@ -6,7 +6,7 @@ from sglang.test.ci.ci_register import register_npu_ci
 register_npu_ci(est_time=400, suite="nightly-4-npu-a3", nightly=True)
 
 
-class TestGemmaModels(TestVLMModels):
+class TestMiniCPMModelsV(TestVLMModels):
     model = "/root/.cache/modelscope/hub/models/openbmb/MiniCPM-V-2_6"
     mmmu_accuracy = 0.2
 
