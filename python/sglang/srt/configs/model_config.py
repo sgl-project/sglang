e41e650551b79563f22ce553d1ac9d697c96e9af
yhyang201 <yhyang201@gmail.com>
2026-01-21 15:01:00 +0000

    upd

--- python/sglang/srt/configs/model_config.py
+++ python/sglang/srt/configs/model_config.py
@@ -1151,6 +1151,7 @@ multimodal_model_archs = [
     "JetVLMForConditionalGeneration",
     "PaddleOCRVLForConditionalGeneration",
     "MiDashengLMModel",
+    "StepVLForConditionalGeneration",
 ]
 
 if external_mm_model_arch := envs.SGLANG_EXTERNAL_MM_MODEL_ARCH.get():
