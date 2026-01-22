b938e405ffa030566a1e72b3c3d8b37a8bd470e4
duzeyan <duzeyan@didiglobal.com>
2026-01-22 14:42:42 +0800

    new kernel

--- python/sglang/srt/mem_cache/allocator.py
+++ python/sglang/srt/mem_cache/allocator.py
@@ -178,135 +178,66 @@ def alloc_extend_kernel(
     last_loc_ptr,
     free_page_ptr,
     out_indices,
-    bs_upper: tl.constexpr,
-    page_size: tl.constexpr,
-    max_num_extend_tokens: tl.constexpr,
-):
-    pid = tl.program_id(0)
-
-    load_offset = tl.arange(0, bs_upper)
-    seq_lens = tl.load(seq_lens_ptr + load_offset, mask=load_offset <= pid)
-    pre_lens = tl.load(pre_lens_ptr + load_offset, mask=load_offset <= pid)
-    extend_lens = seq_lens - pre_lens
-
-    seq_len = tl.load(seq_lens_ptr + pid)
-    pre_len = tl.load(pre_lens_ptr + pid)
-    extend_len = seq_len - pre_len
-
-    sum_extend_lens = tl.sum(extend_lens)
-    output_start_loc = sum_extend_lens - extend_len
-
-    num_pages_after = (seq_lens + page_size - 1) // page_size
-    num_pages_before = (pre_lens + page_size - 1) // page_size
-    num_new_pages = num_pages_after - num_pages_before
-
-    num_page_start_loc_self = (seq_len + page_size - 1) // page_size - (
-        pre_len + page_size - 1
-    ) // page_size
-    sum_num_new_pages = tl.sum(num_new_pages)
-    new_page_start_loc = sum_num_new_pages - num_page_start_loc_self
-
-    # Part 1: fill the old partial page
-    last_loc = tl.load(last_loc_ptr + pid)
-    num_part1 = (
-        min(seq_len, (pre_len + page_size - 1) // page_size * page_size) - pre_len
-    )
-    offset_one_page = tl.arange(0, page_size)
-    tl.store(
-        out_indices + output_start_loc + offset_one_page,
-        last_loc + 1 + offset_one_page,
-        mask=offset_one_page < num_part1,
-    )
-    if pre_len + num_part1 == seq_len:
-        return
-
-    # Part 2: fill the new full pages
-    num_part2 = (
-        seq_len // page_size * page_size
-        - (pre_len + page_size - 1) // page_size * page_size
-    )
-
-    offset_many_page = tl.arange(0, max_num_extend_tokens)
-    page_start = tl.load(
-        free_page_ptr + new_page_start_loc + offset_many_page // page_size,
-        mask=offset_many_page < num_part2,
-    )
-    tl.store(
-        out_indices + output_start_loc + num_part1 + offset_many_page,
-        page_start * page_size + offset_many_page % page_size,
-        mask=offset_many_page < num_part2,
-    )
-    if pre_len + num_part1 + num_part2 == seq_len:
-        return
-
-    # Part 3: fill the new partial page
-    num_part3 = seq_len - seq_len // page_size * page_size
-    start_loc = tl.load(
-        free_page_ptr + new_page_start_loc + num_page_start_loc_self - 1
-    )
-    tl.store(
-        out_indices + output_start_loc + num_part1 + num_part2 + offset_one_page,
-        start_loc * page_size + offset_one_page,
-        mask=offset_one_page < num_part3,
-    )
-
-
-@triton.jit
-def alloc_extend_kernel_v2(
-    pre_lens_ptr,
-    seq_lens_ptr,
-    last_loc_ptr,
-    free_page_ptr,
-    out_indices,
-    bs_upper: tl.constexpr,
+    step_size_bs: tl.constexpr,
     page_size: tl.constexpr,
     step_size: tl.constexpr,
 ):
     pid = tl.program_id(0)
 
-    load_offset = tl.arange(0, bs_upper)
-    seq_lens = tl.load(seq_lens_ptr + load_offset, mask=load_offset <= pid)
-    pre_lens = tl.load(pre_lens_ptr + load_offset, mask=load_offset <= pid)
-    extend_lens = seq_lens - pre_lens
+    sum_extend_lens = 0
+    sum_num_new_pages = 0
+    sum_extend_lens = sum_extend_lens.to(tl.int64)
+    sum_num_new_pages = sum_num_new_pages.to(tl.int64)
 
     seq_len = tl.load(seq_lens_ptr + pid)
     pre_len = tl.load(pre_lens_ptr + pid)
     extend_len = seq_len - pre_len
 
-    sum_extend_lens = tl.sum(extend_lens)
+    num_loops_bs = (pid + 1 + step_size_bs - 1) // step_size_bs
+    for i in range(num_loops_bs):
+        start = i * step_size_bs
+        offset = tl.arange(0, step_size_bs)
+        mask = (start + offset) <= pid
+
+        s_lens = tl.load(seq_lens_ptr + start + offset, mask=mask, other=0)
+        p_lens = tl.load(pre_lens_ptr + start + offset, mask=mask, other=0)
+
+        e_lens = s_lens - p_lens
+        sum_extend_lens += tl.sum(e_lens)
+
+        pgs_after = (s_lens + page_size - 1) // page_size
+        pgs_before = (p_lens + page_size - 1) // page_size
+        new_pgs = pgs_after - pgs_before
+        sum_num_new_pages += tl.sum(new_pgs)
+
     output_start_loc = sum_extend_lens - extend_len
 
-    num_pages_after = (seq_lens + page_size - 1) // page_size
-    num_pages_before = (pre_lens + page_size - 1) // page_size
-    num_new_pages = num_pages_after - num_pages_before
+    num_pages_after = (seq_len + page_size - 1) // page_size
+    num_pages_before = (pre_len + page_size - 1) // page_size
+    num_page_start_loc_self = num_pages_after - num_pages_before
 
-    num_page_start_loc_self = (seq_len + page_size - 1) // page_size - (
-        pre_len + page_size - 1
-    ) // page_size
-    sum_num_new_pages = tl.sum(num_new_pages)
     new_page_start_loc = sum_num_new_pages - num_page_start_loc_self
 
     # Part 1: fill the old partial page
     last_loc = tl.load(last_loc_ptr + pid)
-    num_part1 = (
-        min(seq_len, (pre_len + page_size - 1) // page_size * page_size) - pre_len
-    )
+
+    boundary_pre = (pre_len + page_size - 1) // page_size * page_size
+    num_part1 = min(seq_len, boundary_pre) - pre_len
+
     offset_one_page = tl.arange(0, page_size)
     tl.store(
         out_indices + output_start_loc + offset_one_page,
         last_loc + 1 + offset_one_page,
         mask=offset_one_page < num_part1,
     )
+
     if pre_len + num_part1 == seq_len:
         return
 
     # Part 2: fill the new full pages
-    num_part2 = (
-        seq_len // page_size * page_size
-        - (pre_len + page_size - 1) // page_size * page_size
-    )
+    boundary_seq = seq_len // page_size * page_size
+    num_part2 = boundary_seq - boundary_pre
 
-    # handle new page chunk by chunk
     num_loops = (num_part2 + step_size - 1) // step_size
     for i in range(num_loops):
         start_idx = i * step_size
@@ -317,10 +248,9 @@ def alloc_extend_kernel_v2(
         valid_mask = offset_chunk < current_chunk_size
         global_offset = start_idx + offset_chunk
 
-        page_start = tl.load(
-            free_page_ptr + new_page_start_loc + global_offset // page_size,
-            mask=valid_mask,
-        )
+        page_idx_in_free_list = new_page_start_loc + (global_offset // page_size)
+
+        page_start = tl.load(free_page_ptr + page_idx_in_free_list, mask=valid_mask)
 
         tl.store(
             out_indices + output_start_loc + num_part1 + global_offset,
@@ -332,7 +262,7 @@ def alloc_extend_kernel_v2(
         return
 
     # Part 3: fill the new partial page
-    num_part3 = seq_len - seq_len // page_size * page_size
+    num_part3 = seq_len - boundary_seq
     start_loc = tl.load(
         free_page_ptr + new_page_start_loc + num_page_start_loc_self - 1
     )
@@ -402,7 +332,7 @@ class PagedTokenToKVPoolAllocator(BaseTokenToKVPoolAllocator):
         self.num_pages = size // page_size
         self.debug_mode = get_bool_env_var("SGLANG_DEBUG_MEMORY_POOL")
         self.seen_max_num_extend_tokens_next_power_of_2 = 1
-        self.max_bs = next_power_of_2(1024)
+        self.bs_step = next_power_of_2(512)
         self.page_process_step = next_power_of_2(512)
         self.clear()
 
@@ -443,13 +373,7 @@ class PagedTokenToKVPoolAllocator(BaseTokenToKVPoolAllocator):
                 (last_loc + 1) % self.page_size == prefix_lens % self.page_size
             )
 
-        self.seen_max_num_extend_tokens_next_power_of_2 = max(
-            self.seen_max_num_extend_tokens_next_power_of_2,
-            next_power_of_2(extend_num_tokens),
-        )
-
         bs = len(prefix_lens)
-        self.max_bs = next_power_of_2(max(bs, self.max_bs))
         if self.need_sort and extend_num_tokens // self.page_size + bs + 1 > len(
             self.free_pages
         ):
@@ -458,13 +382,13 @@ class PagedTokenToKVPoolAllocator(BaseTokenToKVPoolAllocator):
         out_indices = torch.empty(
             (extend_num_tokens,), dtype=torch.int64, device=self.device
         )
-        alloc_extend_kernel_v2[(bs,)](
+        alloc_extend_kernel[(bs,)](
             prefix_lens,
             seq_lens,
             last_loc,
             self.free_pages,
             out_indices,
-            self.max_bs,
+            self.bs_step,
             self.page_size,
             self.page_process_step,
         )
--- test/srt/test_triton_page_alloc_extend.py
+++ test/srt/test_triton_page_alloc_extend.py
@@ -2,8 +2,10 @@ import time
 import unittest
 
 import torch
+import triton
+import triton.language as tl
 
-from sglang.srt.mem_cache.allocator import alloc_extend_kernel, alloc_extend_kernel_v2
+from sglang.srt.mem_cache.allocator import alloc_extend_kernel
 from sglang.srt.utils import next_power_of_2
 
 # Test configurations
@@ -11,6 +13,86 @@ BATCH_SIZES = [1, 32, 64]
 SEQ_LENS = [1, 1000, 32000, 64000]
 PAGE_SIZE = 64
 NUM_PAGES = 100000
+
+
+@triton.jit
+def alloc_extend_kernel_old(
+    pre_lens_ptr,
+    seq_lens_ptr,
+    last_loc_ptr,
+    free_page_ptr,
+    out_indices,
+    bs_upper: tl.constexpr,
+    page_size: tl.constexpr,
+    max_num_extend_tokens: tl.constexpr,
+):
+    pid = tl.program_id(0)
+
+    load_offset = tl.arange(0, bs_upper)
+    seq_lens = tl.load(seq_lens_ptr + load_offset, mask=load_offset <= pid)
+    pre_lens = tl.load(pre_lens_ptr + load_offset, mask=load_offset <= pid)
+    extend_lens = seq_lens - pre_lens
+
+    seq_len = tl.load(seq_lens_ptr + pid)
+    pre_len = tl.load(pre_lens_ptr + pid)
+    extend_len = seq_len - pre_len
+
+    sum_extend_lens = tl.sum(extend_lens)
+    output_start_loc = sum_extend_lens - extend_len
+
+    num_pages_after = (seq_lens + page_size - 1) // page_size
+    num_pages_before = (pre_lens + page_size - 1) // page_size
+    num_new_pages = num_pages_after - num_pages_before
+
+    num_page_start_loc_self = (seq_len + page_size - 1) // page_size - (
+        pre_len + page_size - 1
+    ) // page_size
+    sum_num_new_pages = tl.sum(num_new_pages)
+    new_page_start_loc = sum_num_new_pages - num_page_start_loc_self
+
+    # Part 1: fill the old partial page
+    last_loc = tl.load(last_loc_ptr + pid)
+    num_part1 = (
+        min(seq_len, (pre_len + page_size - 1) // page_size * page_size) - pre_len
+    )
+    offset_one_page = tl.arange(0, page_size)
+    tl.store(
+        out_indices + output_start_loc + offset_one_page,
+        last_loc + 1 + offset_one_page,
+        mask=offset_one_page < num_part1,
+    )
+    if pre_len + num_part1 == seq_len:
+        return
+
+    # Part 2: fill the new full pages
+    num_part2 = (
+        seq_len // page_size * page_size
+        - (pre_len + page_size - 1) // page_size * page_size
+    )
+
+    offset_many_page = tl.arange(0, max_num_extend_tokens)
+    page_start = tl.load(
+        free_page_ptr + new_page_start_loc + offset_many_page // page_size,
+        mask=offset_many_page < num_part2,
+    )
+    tl.store(
+        out_indices + output_start_loc + num_part1 + offset_many_page,
+        page_start * page_size + offset_many_page % page_size,
+        mask=offset_many_page < num_part2,
+    )
+    if pre_len + num_part1 + num_part2 == seq_len:
+        return
+
+    # Part 3: fill the new partial page
+    num_part3 = seq_len - seq_len // page_size * page_size
+    start_loc = tl.load(
+        free_page_ptr + new_page_start_loc + num_page_start_loc_self - 1
+    )
+    tl.store(
+        out_indices + output_start_loc + num_part1 + num_part2 + offset_one_page,
+        start_loc * page_size + offset_one_page,
+        mask=offset_one_page < num_part3,
+    )
 
 
 def setup_alloc_test(bs, seq_len, prefix_len=0, device="cuda"):
@@ -53,7 +135,7 @@ class TestAllocExtend(unittest.TestCase):
         compile_times = []
         for _ in range(3):
             start = time.perf_counter()
-            alloc_extend_kernel[(bs,)](
+            alloc_extend_kernel_old[(bs,)](
                 prefix_lens,
                 seq_lens,
                 last_loc,
@@ -87,7 +169,7 @@ class TestAllocExtend(unittest.TestCase):
         compile_times = []
         for _ in range(3):
             start = time.perf_counter()
-            alloc_extend_kernel_v2[(bs,)](
+            alloc_extend_kernel[(bs,)](
                 prefix_lens,
                 seq_lens,
                 last_loc,
@@ -126,7 +208,7 @@ class TestAllocExtend(unittest.TestCase):
             max_extend_tokens_pow2 = next_power_of_2(extend_num_tokens)
             bs_pow2 = next_power_of_2(bs)
 
-            alloc_extend_kernel[(bs,)](
+            alloc_extend_kernel_old[(bs,)](
                 prefix_lens,
                 seq_lens,
                 last_loc,
@@ -157,7 +239,7 @@ class TestAllocExtend(unittest.TestCase):
             max_extend_tokens_pow2 = next_power_of_2(extend_num_tokens)
             bs_pow2 = next_power_of_2(bs)
 
-            alloc_extend_kernel[(bs,)](
+            alloc_extend_kernel_old[(bs,)](
                 prefix_lens,
                 seq_lens,
                 last_loc,
@@ -193,7 +275,7 @@ class TestAllocExtend(unittest.TestCase):
                 extend_num_tokens,
             ) = setup_alloc_test(bs, seq_len, prefix_len, "cuda")
 
-            alloc_extend_kernel_v2[(bs,)](
+            alloc_extend_kernel[(bs,)](
                 prefix_lens,
                 seq_lens,
                 last_loc,
@@ -222,7 +304,7 @@ class TestAllocExtend(unittest.TestCase):
                 extend_num_tokens,
             ) = setup_alloc_test(bs, seq_len, prefix_len, "cuda")
 
-            alloc_extend_kernel_v2[(bs,)](
+            alloc_extend_kernel[(bs,)](
                 prefix_lens,
                 seq_lens,
                 last_loc,
@@ -261,7 +343,7 @@ class TestAllocExtend(unittest.TestCase):
         max_extend_tokens_pow2 = next_power_of_2(extend_num_tokens)
         bs_pow2 = next_power_of_2(bs)
 
-        alloc_extend_kernel[(bs,)](
+        alloc_extend_kernel_old[(bs,)](
             prefix_lens,
             seq_lens,
             last_loc,
@@ -274,7 +356,7 @@ class TestAllocExtend(unittest.TestCase):
         torch.cuda.synchronize()
 
         # Run v2
-        alloc_extend_kernel_v2[(bs,)](
+        alloc_extend_kernel[(bs,)](
             prefix_lens,
             seq_lens,
             last_loc,
