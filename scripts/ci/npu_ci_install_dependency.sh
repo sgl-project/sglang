b38600c6c276056f7cbc8093d434e9ff78953d34
wangqi <2801406898@qq.com>
2026-01-23 15:14:56 +0800

    sglang dependcy chang to dev_npu mode

--- scripts/ci/npu_ci_install_dependency.sh
+++ scripts/ci/npu_ci_install_dependency.sh
@@ -53,4 +53,4 @@ mkdir sgl-kernel-npu
 
 ### Install SGLang
 rm -rf python/pyproject.toml && mv python/pyproject_other.toml python/pyproject.toml
-${PIP_INSTALL} -v -e "python[srt_npu]"
+${PIP_INSTALL} -v -e "python[dev_npu]"
