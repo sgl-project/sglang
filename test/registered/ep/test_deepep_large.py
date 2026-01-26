d2e5d32f1227c394e4e3ac765b658d531339c233
alisonshao <a.shao@wustl.edu>
2026-01-26 14:12:30 -0800

    Fix isort import order in test_deepep_large.py

--- test/registered/ep/test_deepep_large.py
+++ test/registered/ep/test_deepep_large.py
@@ -4,9 +4,9 @@ from types import SimpleNamespace
 import requests
 
 from sglang.srt.utils import kill_process_tree
+from sglang.test.ci.ci_register import register_cuda_ci
 from sglang.test.few_shot_gsm8k import run_eval as run_eval_few_shot_gsm8k
 from sglang.test.send_one import BenchArgs, send_one_prompt
-from sglang.test.ci.ci_register import register_cuda_ci
 from sglang.test.test_utils import (
     DEFAULT_DEEPEP_MODEL_NAME_FOR_TEST,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
