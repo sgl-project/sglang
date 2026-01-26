11fe40ba9108bc6cbf1f54fc841ff543bc711cab
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-01-26 12:01:46 +0300

    Update unquant.py

--- python/sglang/srt/layers/quantization/unquant.py
+++ python/sglang/srt/layers/quantization/unquant.py
@@ -466,8 +466,8 @@ class UnquantizedFusedMoEMethod(FusedMoEMethodBase, MultiPlatformOp):
         quant_info = TorchNpuKernelsQuantInfo(
                 w13_weight=layer.w13_weight,
                 w2_weight=layer.w2_weight,
-                w13_scale=layer.w13_weight_bias,
-                w2_scale=layer.w2_weight_bias,
+                w13_scale=layer.w13_weight_bias if layer.w13_weight_bias else None,
+                w2_scale=layer.w2_weight_bias if layer.w2_weight_bias else None,
             )
         return self.runner.run(dispatch_output, quant_info)
 
