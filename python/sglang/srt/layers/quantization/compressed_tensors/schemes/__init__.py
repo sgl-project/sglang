00dcfc174280214076a7838ffd88eab1fdac1bf9
Tamir Baydasov <41994229+TamirBaydasov@users.noreply.github.com>
2026-01-27 13:31:27 +0300

    Update __init__.py

--- python/sglang/srt/layers/quantization/compressed_tensors/schemes/__init__.py
+++ python/sglang/srt/layers/quantization/compressed_tensors/schemes/__init__.py
@@ -2,18 +2,21 @@
 
 from .compressed_tensors_scheme import CompressedTensorsScheme
 from .compressed_tensors_w4a4_nvfp4 import CompressedTensorsW4A4Fp4
+from .compressed_tensors_w4a4_nvfp4_moe import CompressedTensorsW4A4Nvfp4MoE
+from .compressed_tensors_w4a8_int8_moe import NPUCompressedTensorsW4A8Int8DynamicMoE
 from .compressed_tensors_w8a8_fp8 import CompressedTensorsW8A8Fp8
+from .compressed_tensors_w8a8_fp8_moe import CompressedTensorsW8A8Fp8MoE
 from .compressed_tensors_w8a8_int8 import (
     CompressedTensorsW8A8Int8,
     NPUCompressedTensorsW8A8Int8,
 )
+from .compressed_tensors_w8a8_int8_moe import NPUCompressedTensorsW8A8Int8DynamicMoE
 from .compressed_tensors_w8a16_fp8 import CompressedTensorsW8A16Fp8
 from .compressed_tensors_wNa16 import WNA16_SUPPORTED_BITS, CompressedTensorsWNA16
-from .compressed_tensors_w4a4_nvfp4_moe import CompressedTensorsW4A4Nvfp4MoE
-from .compressed_tensors_w4a8_int8_moe import NPUCompressedTensorsW4A8Int8DynamicMoE
-from .compressed_tensors_w8a8_fp8_moe import CompressedTensorsW8A8Fp8MoE
-from .compressed_tensors_w8a8_int8_moe import NPUCompressedTensorsW8A8Int8DynamicMoE
-from .compressed_tensors_wNa16_moe import CompressedTensorsWNA16MoE, NPUCompressedTensorsW4A16Int4DynamicMoE
+from .compressed_tensors_wNa16_moe import (
+    CompressedTensorsWNA16MoE,
+    NPUCompressedTensorsW4A16Int4DynamicMoE,
+)
 
 __all__ = [
     "CompressedTensorsScheme",
