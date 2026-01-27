d9b7829d5bc34318f34db15903c157d28671f542
Tamir Baydasov <41994229+TamirBaydasov@users.noreply.github.com>
2026-01-27 17:03:31 +0300

    Update __init__.py

--- python/sglang/srt/layers/quantization/compressed_tensors/schemes/__init__.py
+++ python/sglang/srt/layers/quantization/compressed_tensors/schemes/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from .compressed_tensors_scheme import CompressedTensorsScheme
+from .compressed_tensors_w4a4_mxint4_moe import CompressedTensorsMxInt4MoE
 from .compressed_tensors_w4a4_nvfp4 import CompressedTensorsW4A4Fp4
 from .compressed_tensors_w4a4_nvfp4_moe import CompressedTensorsW4A4Nvfp4MoE
 from .compressed_tensors_w4a8_int8_moe import NPUCompressedTensorsW4A8Int8DynamicMoE
@@ -33,4 +34,5 @@ __all__ = [
     "CompressedTensorsW4A4Fp4",
     "CompressedTensorsW4A4Nvfp4MoE",
     "NPUCompressedTensorsW4A8Int8DynamicMoE",
+    "CompressedTensorsMxInt4MoE",
 ]
