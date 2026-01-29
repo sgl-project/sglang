0ee27fb3d0c8a3842bfd64896e4b7283e2e4af82
Ljubomir Josifovski <ljubomir@gigul2>
2026-01-29 10:48:12 +0000

    save

--- sgl-kernel/include/ATen/hip/impl/HIPGuardImplMasqueradingAsCUDA.h
+++ sgl-kernel/include/ATen/hip/impl/HIPGuardImplMasqueradingAsCUDA.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <c10/hip/HIPGuard.h>
+#include <c10/hip/HIPStream.h>
 
 namespace c10::hip {
 using OptionalHIPGuardMasqueradingAsCUDA = c10::cuda::OptionalCUDAGuard;
@@ -8,4 +9,7 @@ using OptionalHIPGuardMasqueradingAsCUDA = c10::cuda::OptionalCUDAGuard;
 
 namespace at::hip {
 using OptionalHIPGuardMasqueradingAsCUDA = c10::cuda::OptionalCUDAGuard;
+inline hipStream_t getCurrentHIPStream() {
+  return c10::cuda::getCurrentCUDAStream().stream();
+}
 }  // namespace at::hip
