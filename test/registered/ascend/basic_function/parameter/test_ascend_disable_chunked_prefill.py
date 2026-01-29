1f76fc1e6f47b1e242d466a348c951d28c8e55b9
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-01-29 16:52:48 +0800

    Create test_ascend_disable_chunked_prefill.py

---
+++ test/registered/ascend/basic_function/parameter/test_ascend_disable_chunked_prefill.py
@@ -0,0 +1,62 @@
+import unittest
+from types import SimpleNamespace
+
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.srt.utils import kill_process_tree
+from sglang.test.run_eval import run_eval
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+from sglang.test.ascend.test_ascend_utils import Llama_3_1_8B_Instruct_WEIGHTS_PATH
+
+register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
+
+
+class TestNoChunkedPrefill(CustomTestCase):
+    """Testcase: Verify Llama-3.1-8B-Instruct accuracy ≥ 0.65 with chunked prefill disabled.
+
+    [Test Category] Parameter
+    [Test Target] --chunked-prefill-size
+    """
+    
+    @classmethod
+    def setUpClass(cls):
+        # disable chunked prefill (set to -1)
+        cls.model = Llama_3_1_8B_Instruct_WEIGHTS_PATH
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=[
+                "--disable-radix-cache",
+                "--chunked-prefill-size",
+                "-1",
+                "--attention-backend",
+                "ascend",
+                "--disable-cuda-graph",
+            ],
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_mmlu(self):
+        args = SimpleNamespace(
+            base_url=self.base_url,
+            model=self.model,
+            eval_name="mmlu",
+            num_examples=64,
+            num_threads=32,
+        )
+        metrics = run_eval(args)
+        self.assertGreaterEqual(metrics["score"], 0.65)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
