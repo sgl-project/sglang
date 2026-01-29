64999bebe671666505fc6f2a790ffd54773686b5
Felix Marty <Felix.Marty@amd.com>
2026-01-29 09:17:42 -0600

    add gsm8k test

--- test/registered/quant/test_online_quantization.py
+++ test/registered/quant/test_online_quantization.py
@@ -1,6 +1,6 @@
 import io
 import re
-
+import os
 from sglang.test.ci.ci_register import register_amd_ci, register_cuda_ci
 
 register_cuda_ci(est_time=103, suite="stage-b-test-small-1-gpu")
@@ -9,17 +9,24 @@ from sglang.srt.utils import kill_process_tree
 from sglang.srt.utils.common import is_cuda_alike
 from sglang.test.test_utils import (
     DEFAULT_SMALL_MODEL_NAME_FOR_TEST_QWEN,
+    DEFAULT_SMALL_MOE_MODEL_NAME_FOR_TEST_BASE,
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
     popen_launch_server,
 )
+from types import SimpleNamespace
+from sglang.test.few_shot_gsm8k import run_eval
 
 
 class TestOnlineQuantizationMemoryLoad(CustomTestCase):
     @classmethod
     def setUpClass(cls):
-        cls.model = DEFAULT_SMALL_MODEL_NAME_FOR_TEST_QWEN
+        cls.SGLANG_USE_AITER = os.environ.get("SGLANG_USE_AITER", None)
+        
+        # DEFAULT_SMALL_MOE_MODEL_NAME_FOR_TEST_BASE has a shape not compatible with aiter.
+        os.environ["SGLANG_USE_AITER"] = "0"
+
         cls.base_url = DEFAULT_URL_FOR_TEST
         cls.stdout = io.StringIO()
         cls.stderr = io.StringIO()
@@ -63,8 +70,54 @@ class TestOnlineQuantizationMemoryLoad(CustomTestCase):
         cls.stdout.close()
         cls.stderr.close()
 
+        if cls.SGLANG_USE_AITER:
+            os.environ["SGLANG_USE_AITER"] = cls.SGLANG_USE_AITER
+
+
+class TestOnlineQuantizationMemoryLoadDense(TestOnlineQuantizationMemoryLoad):
+    model = DEFAULT_SMALL_MODEL_NAME_FOR_TEST_QWEN
+
+    def test_peak_memory(self):
+        if not is_cuda_alike():
+            self.skipTest("not is_cuda_alike")
+        
+        # Original BF16 model: 2.887 GiB
+        assert self.peak_memory < 2
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            num_shots=8,
+            data_path=None,
+            num_questions=500,
+            max_new_tokens=512,
+            parallel=128,
+            host="http://127.0.0.1",
+            port=int(self.base_url.split(":")[-1]),
+        )
+        metrics = run_eval(args)
+        print(f"{metrics=}")
+        self.assertGreater(metrics["accuracy"], 0.01)
+
+class TestOnlineQuantizationMemoryLoadMOE(TestOnlineQuantizationMemoryLoad):
+    model = DEFAULT_SMALL_MOE_MODEL_NAME_FOR_TEST_BASE
+
     def test_peak_memory(self):
         if not is_cuda_alike():
             self.skipTest("not is_cuda_alike")
 
-        assert self.peak_memory < 2
+        # Original BF16 model: 26.695 GiB
+        assert self.peak_memory < 15.5
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            num_shots=8,
+            data_path=None,
+            num_questions=500,
+            max_new_tokens=512,
+            parallel=128,
+            host="http://127.0.0.1",
+            port=int(self.base_url.split(":")[-1]),
+        )
+        metrics = run_eval(args)
+        print(f"{metrics=}")
+        self.assertGreater(metrics["accuracy"], 0.03)
