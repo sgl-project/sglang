7b1addf914a82e8933001ca1717bd3eb22f8c6ce
JiaruiChang5268 <71279366+JiaruiChang5268@users.noreply.github.com>
2026-01-31 08:49:38 +0800

    [NPU] support llama-3.2-11B-vision-instruct mode for NPU (#17492)
    Co-authored-by: McZyWu <zhuoyun.wu.23@ucl.ac.uk>
    Co-authored-by: chenyang08056032 <chenyang08056032@163.com>
    Co-authored-by: Hexq0210 <893781835@qq.com>

--- python/sglang/srt/layers/linear.py
+++ python/sglang/srt/layers/linear.py
@@ -354,7 +354,7 @@ class ColumnParallelLinear(LinearBase):
         )
         if bias:
             self.bias = Parameter(
-                torch.empty(self.output_size_per_partition, dtype=params_dtype)
+                torch.zeros(self.output_size_per_partition, dtype=params_dtype)
             )
             set_weight_attrs(
                 self.bias,
@@ -1302,7 +1302,7 @@ class RowParallelLinear(LinearBase):
         )
 
         if bias:
-            self.bias = Parameter(torch.empty(self.output_size, dtype=params_dtype))
+            self.bias = Parameter(torch.zeros(self.output_size, dtype=params_dtype))
             set_weight_attrs(
                 self.bias,
                 {
