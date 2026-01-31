10d244d96627a4852edb58ab76732dde0c732679
JiaruiChang5268 <71279366+JiaruiChang5268@users.noreply.github.com>
2026-01-31 08:49:38 +0800

    [NPU] support llama-3.2-11B-vision-instruct mode for NPU (#17492)
    Co-authored-by: McZyWu <zhuoyun.wu.23@ucl.ac.uk>
    Co-authored-by: chenyang08056032 <chenyang08056032@163.com>
    Co-authored-by: Hexq0210 <893781835@qq.com>

---
+++ test/registered/ascend/vlm_models/test_ascend_llama_3_2_11b_vision_instruct.py
@@ -0,0 +1,29 @@
+import unittest
+
+from sglang.test.ascend.vlm_utils import TestVLMModels
+from sglang.test.ci.ci_register import register_npu_ci
+
+register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
+
+
+class TestLlama3211BVisionInstruct(TestVLMModels):
+    model = (
+        "/root/.cache/modelscope/hub/models/LLM-Research/Llama-3.2-11B-Vision-Instruct"
+    )
+    mmmu_accuracy = 0.2
+    other_args = [
+        "--trust-remote-code",
+        "--mem-fraction-static",
+        "0.8",
+        "--attention-backend",
+        "ascend",
+        "--disable-cuda-graph",
+        "--disable-radix-cache",
+    ]
+
+    def test_vlm_mmmu_benchmark(self):
+        self._run_vlm_mmmu_test()
+
+
+if __name__ == "__main__":
+    unittest.main()
