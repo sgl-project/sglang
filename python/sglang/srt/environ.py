e44400256f3aca65082b6a13ea610fd4c8bad886
Mohammad Miadh Angkad <176301910+mmangkad@users.noreply.github.com>
2026-02-01 16:48:58 +0800

    Use DeepGEMM BF16 NT for unquantized linear

--- python/sglang/srt/environ.py
+++ python/sglang/srt/environ.py
@@ -339,6 +339,7 @@ class Envs:
     SGLANG_DG_CACHE_DIR = EnvStr(os.path.expanduser("~/.cache/deep_gemm"))
     SGLANG_DG_USE_NVRTC = EnvBool(False)
     SGLANG_USE_DEEPGEMM_BMM = EnvBool(False)
+    SGLANG_USE_DEEPGEMM_BF16_GEMM = EnvBool(False)
     SGLANG_CHUNKED_PREFIX_CACHE_THRESHOLD = EnvInt(8192)
 
     # DeepEP
