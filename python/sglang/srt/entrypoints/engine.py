093f2710fbd667ba153bdd8d89b01ba3abe77df1
Noa Neria <noa@run.ai>
2026-02-01 17:24:05 +0200

    restore log
    Signed-off-by: Noa Neria <noa@run.ai>

--- python/sglang/srt/entrypoints/engine.py
+++ python/sglang/srt/entrypoints/engine.py
@@ -153,6 +153,7 @@ class Engine(EngineBase):
                 kwargs["log_level"] = "error"
             server_args = self.server_args_class(**kwargs)
         self.server_args = server_args
+        logger.info(f"{server_args=}")
 
         # Shutdown the subprocesses automatically when the program exits
         atexit.register(self.shutdown)
