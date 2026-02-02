08140a4b7daa79b372dfc0aea0ba8306a07c3378
Baizhou Zhang <sobereddiezhang@gmail.com>
2026-02-02 18:03:15 +0800

    impl

--- python/sglang/srt/environ.py
+++ python/sglang/srt/environ.py
@@ -334,7 +334,7 @@ class Envs:
     # DeepGemm
     SGLANG_ENABLE_JIT_DEEPGEMM = EnvBool(True)
     SGLANG_JIT_DEEPGEMM_PRECOMPILE = EnvBool(True)
-    SGLANG_JIT_DEEPGEMM_PRECOMPILE_MAX_KERNEL_NUM = EnvInt(-1)
+    SGLANG_JIT_DEEPGEMM_FAST_WARMUP = EnvBool(False)
     SGLANG_JIT_DEEPGEMM_COMPILE_WORKERS = EnvInt(4)
     SGLANG_IN_DEEPGEMM_PRECOMPILE_STAGE = EnvBool(False)
     SGLANG_DG_CACHE_DIR = EnvStr(os.path.expanduser("~/.cache/deep_gemm"))
