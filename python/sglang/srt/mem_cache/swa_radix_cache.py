fd983b09b68c076d448a55266a29ffdfdff2d06e
EkiRui <374389385@qq.com>
2026-02-03 09:44:20 +0800

    [Performance] Optimize radix cache eviction performance (#14339)
    Signed-off-by: Xingrui Yi <yixingrui@linux.alibaba.com>
    Co-authored-by: Xuchun Shang <xuchun.shang@gmail.com>

--- python/sglang/srt/mem_cache/swa_radix_cache.py
+++ python/sglang/srt/mem_cache/swa_radix_cache.py
@@ -1104,19 +1104,6 @@ class SWARadixCache(BasePrefixCache):
 
         self.full_evictable_size_ -= len(node.key)
 
-    def _collect_leaves(self) -> List[TreeNode]:
-        ret_list = []
-        stack = [self.root_node]
-
-        while stack:
-            cur_node = stack.pop()
-            if len(cur_node.children) == 0:
-                ret_list.append(cur_node)
-            else:
-                stack.extend(cur_node.children.values())
-
-        return ret_list
-
     def _collect_nontombstone_nodes(self) -> List[TreeNode]:
         ret_list = []
         stack = [self.root_node]
