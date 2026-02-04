f3dc575558fb69cae25214afa162ba3174100c32
TamirBaydasov <mr.jeijy@gmail.com>
2026-02-04 17:06:34 +0300

    Quark schemes initial implementation

--- python/sglang/srt/layers/quantization/quark/schemes/__init__.py
+++ python/sglang/srt/layers/quantization/quark/schemes/__init__.py
@@ -2,6 +2,14 @@
 
 from .quark_scheme import QuarkScheme
 from .quark_w4a4_mxfp4 import QuarkW4A4MXFP4
+from .quark_w4a4_mxfp4_moe import QuarkW4A4MXFp4MoE
 from .quark_w8a8_fp8 import QuarkW8A8Fp8
+from .quark_w8a8_fp8_moe import QuarkW8A8FP8MoE
 
-__all__ = ["QuarkScheme", "QuarkW4A4MXFP4", "QuarkW8A8Fp8"]
+__all__ = [
+    "QuarkScheme",
+    "QuarkW4A4MXFP4",
+    "QuarkW8A8Fp8",
+    "QuarkW4A4MXFp4MoE",
+    "QuarkW8A8FP8MoE",
+]
