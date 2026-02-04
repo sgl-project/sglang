594682d30ec3c53e64acf3db257ff8f5de127239
cy <chenyang08056032@163.com>
2026-02-04 16:04:24 +0800

    modify lint

--- test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_ascend_deepep_auto_qwen3_235b_a22b_w8a8.py
+++ test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_ascend_deepep_auto_qwen3_235b_a22b_w8a8.py
@@ -4,9 +4,9 @@ from types import SimpleNamespace
 
 from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.test_ascend_utils import QWEN3_235B_A22B_W8A8_WEIGHTS_PATH
+from sglang.test.few_shot_gsm8k import run_eval as run_gsm8k
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.run_eval import run_eval
-from sglang.test.few_shot_gsm8k import run_eval as run_gsm8k
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
