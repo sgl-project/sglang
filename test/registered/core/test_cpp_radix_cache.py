aa04b57daac9c4a17ca97782d27fcfb18913856e
Wenhao Yang <jackyang2613@gmail.com>
2026-02-04 08:34:36 +0000

    [CI][AMD] add radix cache test to AMD PR CI

--- test/registered/core/test_cpp_radix_cache.py
+++ test/registered/core/test_cpp_radix_cache.py
@@ -3,7 +3,7 @@ from types import SimpleNamespace
 
 from sglang.srt.environ import envs
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ci.ci_register import register_cuda_ci
+from sglang.test.ci.ci_register import register_cuda_ci, register_amd_ci
 from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
     DEFAULT_MODEL_NAME_FOR_TEST,
@@ -15,7 +15,7 @@ from sglang.test.test_utils import (
 
 # Note: AMD registration removed - test_cpp_radix_cache fails on AMD due to C++ radix tree issues
 register_cuda_ci(est_time=60, suite="nightly-1-gpu", nightly=True)
-
+register_amd_ci(est_time=60, suite="stage-b-test-small-1-gpu-amd", nightly=True)
 
 class TestCppRadixCache(CustomTestCase):
     @classmethod
