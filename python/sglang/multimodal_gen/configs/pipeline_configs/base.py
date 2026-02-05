8129d411da282923daafde42c1b2d85dad0451ec
Mick <mickjagger19@icloud.com>
2026-02-05 18:48:56 +0800

    upd

--- python/sglang/multimodal_gen/configs/pipeline_configs/base.py
+++ python/sglang/multimodal_gen/configs/pipeline_configs/base.py
@@ -23,7 +23,9 @@ from sglang.multimodal_gen.configs.models.encoders import BaseEncoderOutput
 from sglang.multimodal_gen.configs.models.encoders.t5 import T5Config
 from sglang.multimodal_gen.configs.sample.sampling_params import DataType
 from sglang.multimodal_gen.configs.utils import update_config_from_args
-from sglang.multimodal_gen.runtime.distributed import sequence_model_parallel_all_gather
+from sglang.multimodal_gen.runtime.distributed.communication_op import (
+    sequence_model_parallel_all_gather,
+)
 from sglang.multimodal_gen.runtime.distributed.parallel_state import (
     get_sp_parallel_rank,
     get_sp_world_size,
