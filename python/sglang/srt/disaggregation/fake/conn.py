d84ebcd5c0e0ca71c68358fe285e0d03a7dc3b55
Estrella-xx <jixingxing5@h-partners.com>
2026-02-06 11:50:28 +0800

    refactor FAKE transfer backend and remove --disaggregation_decode_enable_fake_auto parameter

--- python/sglang/srt/disaggregation/fake/conn.py
+++ python/sglang/srt/disaggregation/fake/conn.py
@@ -8,13 +8,27 @@ from sglang.srt.disaggregation.base.conn import (
     BaseKVManager,
     BaseKVReceiver,
     BaseKVSender,
+    KVArgs,
     KVPoll,
 )
+from sglang.srt.disaggregation.utils import DisaggregationMode
+from sglang.srt.server_args import ServerArgs
 
 logger = logging.getLogger(__name__)
 
 
-# For warmup reqs, we don't kv transfer, we use the fake sender and receiver
+# For warmup reqs, we don't kv transfer, we use the fake manager, sender and receiver
+class FakeKVManager(BaseKVManager):
+    def __init__(
+        self,
+        args: KVArgs,
+        disaggregation_mode: DisaggregationMode,
+        server_args: ServerArgs,
+        is_mla_backend: Optional[bool] = False,
+    ):
+        super().__init__(args, disaggregation_mode, server_args, is_mla_backend)
+
+
 class FakeKVSender(BaseKVSender):
     def __init__(
         self,
