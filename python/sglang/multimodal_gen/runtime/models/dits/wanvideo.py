00248d85c798c6c4cb84aa5a7ac7685178867219
Makcum888e <79456407+Makcum888e@users.noreply.github.com>
2026-02-08 05:45:30 +0300

    [diffusion] platform: support WAN/FLUX/Qwen-Image/Qwen-Image-edit on Ascend (#13662)
    Co-authored-by: dhx98 <haox.dai@gmail.com>
    Co-authored-by: DHX98 <haoxiand@andrew.cmu.edu>
    Co-authored-by: ronnie_zheng <zl19940307@163.com>
    Co-authored-by: DHX98 <DHX98@noreply.gitcode.com>
    Co-authored-by: Yuhao Yang <47235274+yhyang201@users.noreply.github.com>

--- python/sglang/multimodal_gen/runtime/models/dits/wanvideo.py
+++ python/sglang/multimodal_gen/runtime/models/dits/wanvideo.py
@@ -854,7 +854,7 @@ class WanTransformer3DModel(CachableDiT, OffloadableDiTMixin):
 
         encoder_hidden_states = (
             encoder_hidden_states.to(orig_dtype)
-            if current_platform.is_mps()
+            if not current_platform.is_amp_supported()
             else encoder_hidden_states
         )  # cast to orig_dtype for MPS
 
