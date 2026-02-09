bc1e7c494fcf2fd26fc194bc2bfc65c685b2cdea
Liangsheng Yin <lsyincs@gmail.com>
2026-02-08 21:43:31 -0800

    tiny rename

--- python/sglang/srt/speculative/multi_layer_eagle_worker_v2.py
+++ python/sglang/srt/speculative/multi_layer_eagle_worker_v2.py
@@ -352,9 +352,9 @@ class MultiLayerEagleDraftWorker(BaseDraftWorker):
             hidden_states=target_hidden_states,
             verified_id=next_token_ids,
             new_seq_lens=batch.seq_lens,
-            # draft mode is same with decode mode, only 1 num token per batch
-            num_tokens_per_batch=1,
-            num_tokens_for_logprob_per_batch=1,
+            # draft mode is same with decode mode, only 1 token per req
+            num_tokens_per_req=1,
+            num_tokens_for_logprob_per_req=1,
         )
 
         batch.spec_info = next_draft_input
@@ -411,8 +411,8 @@ class MultiLayerEagleDraftWorker(BaseDraftWorker):
         # Batch 2: Draft extend
         draft_input = EagleDraftInput(
             hidden_states=batch_result.logits_output.hidden_states,
-            num_tokens_per_batch=self.speculative_num_steps + 1,
-            num_tokens_for_logprob_per_batch=1,
+            num_tokens_per_req=self.speculative_num_steps + 1,
+            num_tokens_for_logprob_per_req=1,
         )
 
         # Prepare for draft extend in a separate stream
