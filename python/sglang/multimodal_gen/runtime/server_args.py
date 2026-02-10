84a4d31c117d1abf0e9a2f4a4bef02c0cd797618
Mick <mickjagger19@icloud.com>
2026-02-10 09:51:17 +0800

    upd

--- python/sglang/multimodal_gen/runtime/server_args.py
+++ python/sglang/multimodal_gen/runtime/server_args.py
@@ -365,7 +365,7 @@ class ServerArgs:
 
     def adjust_pipeline_config(self):
         # enable parallel folding when SP is enabled
-        if (self.tp_size not in (-1, 1)) or self.sp_degree <= 1:
+        if self.tp_size != 1 or self.sp_degree <= 1:
             return
 
         enabled = False
@@ -449,8 +449,9 @@ class ServerArgs:
         # configure logger before use
         configure_logger(server_args=self)
 
+        self.check_server_args()
+
         self.adjust_offload()
-        self.adjust_pipeline_config()
 
         if self.attention_backend in ["fa3", "fa4"]:
             self.attention_backend = "fa"
@@ -480,8 +481,6 @@ class ServerArgs:
         # TODO: remove hard code
         initial_master_port = (self.master_port or 30005) + random.randint(0, 100)
         self.master_port = self.settle_port(initial_master_port, 37)
-
-        self.check_server_args()
 
         # log clean server_args
         try:
@@ -998,7 +997,7 @@ class ServerArgs:
             raise ValueError("DP is not yet supported")
 
     def check_server_args(self) -> None:
-        """Validate inference arguments for consistency"""
+        """Check, Validate and Normalize inference arguments for consistency"""
         # layerwise offload
         if current_platform.is_mps():
             self.use_fsdp_inference = False
@@ -1093,6 +1092,7 @@ class ServerArgs:
         # allocate all remaining gpus for sp-size
         self.check_server_sp_args()
 
+        # after this point, all parallel settings are normalized
         if self.enable_cfg_parallel:
             if self.num_gpus == 1:
                 raise ValueError(
@@ -1107,6 +1107,8 @@ class ServerArgs:
                     "cache-dit is enabled with hybrid parallelism (SP + TP). "
                     "Proceeding anyway (SGLang integration may support this mode)."
                 )
+
+        self.adjust_pipeline_config()
 
     def _set_default_attention_backend(self) -> None:
         """Configure ROCm defaults when users do not specify an attention backend."""
