31d2c74b5646e53ddc78ec9899f790c9f52a3b77
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-02-11 18:41:20 +0800

    Update test_ascend_deepep_auto_deepseek_R1_0528_w4a8_per_channel.py

--- test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_ascend_deepep_auto_deepseek_R1_0528_w4a8_per_channel.py
+++ test/registered/ascend/basic_function/parallel_strategy/expert_parallelism/test_ascend_deepep_auto_deepseek_R1_0528_w4a8_per_channel.py
@@ -3,10 +3,12 @@ import unittest
 from types import SimpleNamespace
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.run_eval import run_eval
-from sglang.test.few_shot_gsm8k import run_eval as run_eval_gsm8k
-from sglang.test.ascend.test_ascend_utils import DEEPSEEK_R1_0528_W4A8_PER_CHANNEL_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import (
+    DEEPSEEK_R1_0528_W4A8_PER_CHANNEL_WEIGHTS_PATH,
+)
 from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.few_shot_gsm8k import run_eval as run_eval_gsm8k
+from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
@@ -24,7 +26,9 @@ class TestDeepEpAutoDeepseekR1(CustomTestCase):
     [Test Category] Parameter
     [Test Target] --moe-a2a-backend; --deepep-mode
     """
+
     accuracy = 0.96
+
     @classmethod
     def setUpClass(cls):
         cls.model = MODEL_PATH
@@ -135,6 +139,7 @@ class TestDeepEpAutoDeepseekR1(CustomTestCase):
             self.accuracy,
             f'Accuracy of {self.model} is {str(metrics["accuracy"])}, is lower than {self.accuracy}',
         )
+
 
 if __name__ == "__main__":
     unittest.main()
