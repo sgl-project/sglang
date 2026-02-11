cd68407b9e0e5272efa5b61a167ec36945dbb610
yhyang201 <yhyang201@gmail.com>
2026-02-11 15:29:11 +0000

    upd

--- python/sglang/srt/layers/attention/linear/kda_backend.py
+++ python/sglang/srt/layers/attention/linear/kda_backend.py
@@ -14,27 +14,17 @@ from sglang.srt.layers.attention.mamba.causal_conv1d_triton import (
     causal_conv1d_update,
 )
 from sglang.srt.layers.radix_linear_attention import RadixLinearAttention
-from sglang.srt.utils import is_cpu, is_cuda, is_npu
+from sglang.srt.utils import is_cpu, is_npu
 
-if is_cuda():
-    from sglang.srt.layers.attention.mamba.causal_conv1d import (
-        causal_conv1d_fn as causal_conv1d_fn_cuda,
-    )
+if is_npu():
+    from sgl_kernel_npu.mamba.causal_conv1d import causal_conv1d_update_npu
 
-    causal_conv1d_fn = causal_conv1d_fn_cuda
-elif is_npu():
-    from sgl_kernel_npu.mamba.causal_conv1d import (
-        causal_conv1d_fn_npu,
-        causal_conv1d_update_npu,
-    )
-
-    causal_conv1d_fn = causal_conv1d_fn_npu
     causal_conv1d_update = causal_conv1d_update_npu
 elif is_cpu():
-    from sgl_kernel.mamba import causal_conv1d_fn_cpu, causal_conv1d_update_cpu
+    from sgl_kernel.mamba import causal_conv1d_update_cpu
 
-    causal_conv1d_fn = causal_conv1d_fn_cpu
     causal_conv1d_update = causal_conv1d_update_cpu
+
 from sglang.srt.model_executor.forward_batch_info import ForwardBatch
 from sglang.srt.model_executor.model_runner import ModelRunner
 
