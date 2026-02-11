58c112b74d5c5ac173734f70f4b9a0a30ac5f573
Rain Jiang <rain-jiang@outlook.com>
2026-02-10 21:13:01 -0800

    lint fix

--- python/sglang/srt/layers/attention/nsa_backend.py
+++ python/sglang/srt/layers/attention/nsa_backend.py
@@ -2000,14 +2000,18 @@ class NativeSparseAttnBackend(
             device_sm = get_device_sm()
 
             # when nsa prefill impl is trtllm, use its max chunk capacity as mha max kv len
-            mha_max_kv_len = forward_batch.get_max_chunk_capacity() if self.nsa_prefill_impl == 'trtllm' else self.nsa_index_topk
+            mha_max_kv_len = (
+                forward_batch.get_max_chunk_capacity()
+                if self.nsa_prefill_impl == "trtllm"
+                else self.nsa_index_topk
+            )
 
             # Requirements: H200/B200, short sequences, supported dtype, fits in chunk
             self.use_mha = (
                 (
                     device_sm == 90 or (device_sm >= 100 and device_sm < 110)
                 )  # SM90/SM100 only
-                and max_kv_len <= mha_max_kv_len # Short enough for MHA
+                and max_kv_len <= mha_max_kv_len  # Short enough for MHA
                 and forward_batch.token_to_kv_pool.dtype
                 in [torch.bfloat16, torch.float8_e4m3fn]
                 and sum_seq_lens
