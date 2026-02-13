0c561c4ea7d2ece5d757dd3e8a88aa107bd3dee2
OrangeRedeng <sae.20@bk.ru>
2026-02-13 12:40:45 +0300

    Fix Lint

--- python/sglang/multimodal_gen/runtime/loader/component_loaders/transformer_loader.py
+++ python/sglang/multimodal_gen/runtime/loader/component_loaders/transformer_loader.py
@@ -17,12 +17,9 @@ from sglang.multimodal_gen.runtime.server_args import ServerArgs
 from sglang.multimodal_gen.runtime.utils.hf_diffusers_utils import (
     get_diffusers_component_config,
 )
-from sglang.multimodal_gen.runtime.utils.quantization_utils import (
-    get_quant_config,
-)
 from sglang.multimodal_gen.runtime.utils.logging_utils import init_logger
+from sglang.multimodal_gen.runtime.utils.quantization_utils import get_quant_config
 from sglang.multimodal_gen.utils import PRECISION_TO_TYPE
-
 from sglang.srt.utils import is_npu
 
 _is_npu = is_npu()
