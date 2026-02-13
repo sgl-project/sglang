b48fe1d95eceedc680ca179f8ee87fc4f52dc594
Hao Jin <hjjn.amzn@gmail.com>
2026-02-12 16:19:22 -0800

    [Diffusion] [BUG] Fix missing initialization of GLM-Image text encoder config (#18704)
    Co-authored-by: Hao Jin <Hao Jin>

--- python/sglang/multimodal_gen/configs/pipeline_configs/glm_image.py
+++ python/sglang/multimodal_gen/configs/pipeline_configs/glm_image.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass, field
 import torch
 from diffusers.image_processor import VaeImageProcessor
 
+from python.sglang.multimodal_gen.configs.models.encoders.base import EncoderConfig
+from python.sglang.multimodal_gen.configs.models.encoders.t5 import T5Config
 from sglang.multimodal_gen.configs.models import DiTConfig, VAEConfig
 from sglang.multimodal_gen.configs.models.dits.glmimage import GlmImageDitConfig
 from sglang.multimodal_gen.configs.models.vaes.glmimage import GlmImageVAEConfig
@@ -24,6 +26,10 @@ class GlmImagePipelineConfig(ImagePipelineConfig):
     vae_tiling: bool = False
 
     vae_sp: bool = False
+
+    text_encoder_configs: tuple[EncoderConfig, ...] = field(
+        default_factory=lambda: (T5Config(),)
+    )
 
     dit_config: DiTConfig = field(default_factory=GlmImageDitConfig)
     # VAE
