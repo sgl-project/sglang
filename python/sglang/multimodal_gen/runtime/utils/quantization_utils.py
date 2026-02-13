654da96223b0af6565f7f5981090d6c99d261b21
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-02-13 12:23:20 +0300

    Create quantization_utils.py

---
+++ python/sglang/multimodal_gen/runtime/utils/quantization_utils.py
@@ -0,0 +1,34 @@
+from typing import Any, Dict, List, Optional, Union, cast
+
+def replace_prefix(key: str, prefix_mapping: dict[str, str]) -> str:
+    for prefix, new_prefix in prefix_mapping.items():
+        if key.startswith(prefix):
+            key = key.replace(prefix, new_prefix, 1)
+    return key
+
+
+def find_quant_modelslim_config(model_config):
+    quant_config_file = Path(model_config["model_path"], "quant_model_description.json")
+    quant_cfg = None
+    if quant_config_file.is_file():
+        with open(quant_config_file) as f:
+            quant_cfg = json.load(f)
+        # This field is required for flagless model loading but is not present in
+        # modelslim model description, so we're adding it here manually.
+        quant_cfg["quant_method"] = "modelslim"
+
+    return quant_cfg
+
+
+def get_quant_config(
+    model_config,
+    packed_modules_mapping: Dict[str, List[str]] = {},
+    remap_prefix: Dict[str, str] | None = None,
+) -> QuantizationConfig:
+    quant_cfg = find_quant_modelslim_config(model_config)
+
+    if quant_cfg is not None:
+        quant_cls = get_quantization_config(quant_cfg["quant_method"])
+        return quant_cls.from_config(quant_cfg)
+    else:
+        return None
