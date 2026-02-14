49be27209d02dbe2b3b06c314cb34408c57b7ae8
McZyWu <zhuoyun.wu.23@ucl.ac.uk>
2026-02-14 19:19:45 +0800

    revise user case

--- test/registered/ascend/llm_models/test_ascend_kimilinear_48b.py
+++ test/registered/ascend/llm_models/test_ascend_kimilinear_48b.py
@@ -1,14 +1,27 @@
 import unittest
 
 from sglang.test.ascend.gsm8k_ascend_mixin import GSM8KAscendMixin
+from sglang.test.ascend.test_ascend_utils import KIMI_LINEAR_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
 
-register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
+register_npu_ci(
+    est_time=400,
+    suite="nightly-1-npu-a3",
+    nightly=True,
+    disabled="run failed",
+)
+
 
 
 class TestKimiLinear(GSM8KAscendMixin, CustomTestCase):
-    model = "/root/.cache/modelscope/hub/models/moonshotai/Kimi-Linear-48B-A3B-Instruct"
+    """Testcase: Verify that the inference accuracy of the moonshotai/Kimi-Linear-48B-A3B-Instruct model on the GSM8K dataset is no less than 0.88.
+
+    [Test Category] Model
+    [Test Target] moonshotai/Kimi-Linear-48B-A3B-Instruct
+    """
+
+    model = KIMI_LINEAR_WEIGHTS_PATH
     accuracy = 0.88
     other_args = [
         "--trust-remote-code",
