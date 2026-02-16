d0e6d0fee21d052dddd7a3564f837be283a64758
Mick <mickjagger19@icloud.com>
2026-02-16 15:24:08 +0800

    upd

--- python/sglang/multimodal_gen/runtime/pipelines_core/schedule_batch.py
+++ python/sglang/multimodal_gen/runtime/pipelines_core/schedule_batch.py
@@ -26,7 +26,7 @@ from sglang.multimodal_gen.configs.sample.teacache import (
 )
 from sglang.multimodal_gen.runtime.server_args import ServerArgs
 from sglang.multimodal_gen.runtime.utils.logging_utils import init_logger
-from sglang.multimodal_gen.runtime.utils.perf_logger import RequestTimings
+from sglang.multimodal_gen.runtime.utils.perf_logger import RequestMetrics
 from sglang.multimodal_gen.utils import align_to
 
 logger = init_logger(__name__)
@@ -152,7 +152,7 @@ class Req:
     VSA_sparsity: float = 0.0
 
     # stage logging
-    timings: Optional["RequestTimings"] = None
+    timings: Optional["RequestMetrics"] = None
 
     # results
     output: torch.Tensor | None = None
@@ -267,7 +267,7 @@ class Req:
         if self.guidance_scale_2 is None:
             self.guidance_scale_2 = self.guidance_scale
 
-        self.timings = RequestTimings(request_id=self.request_id)
+        self.timings = RequestMetrics(request_id=self.request_id)
 
         if self.is_warmup:
             self.set_as_warmup()
@@ -329,8 +329,8 @@ class OutputBatch:
     error: str | None = None
     output_file_paths: list[str] | None = None
 
-    # logged timings info, directly from Req.timings
-    timings: Optional["RequestTimings"] = None
+    # logged metrics info, directly from Req.timings
+    timings: Optional["RequestMetrics"] = None
 
     # For ComfyUI integration: noise prediction from denoising stage
     noise_pred: torch.Tensor | None = None
