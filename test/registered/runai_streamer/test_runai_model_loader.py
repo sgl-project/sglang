b985e6ed118251c26fb0a3510c01003f4e20589d
Noa Neria <noa@run.ai>
2026-02-18 15:12:34 +0200

    estimated test time 120 seconds
    Signed-off-by: Noa Neria <noa@run.ai>

--- test/registered/runai_streamer/test_runai_model_loader.py
+++ test/registered/runai_streamer/test_runai_model_loader.py
@@ -4,7 +4,7 @@ import sglang as sgl
 from sglang.srt.environ import temp_set_env
 from sglang.test.ci.ci_register import register_cuda_ci
 
-register_cuda_ci(est_time=600, suite="stage-b-test-small-1-gpu")
+register_cuda_ci(est_time=120, suite="stage-b-test-small-1-gpu")
 
 runai_load_format = "runai_streamer"
 test_gcs_model = "gs://vertex-model-garden-public-us/codegemma/codegemma-2b/"
