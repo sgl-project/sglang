88e69e5abc8fd646df2fef75d3609d7767376416
Johnsonms <lizhaofu@gmail.com>
2026-02-18 01:24:50 +0000

    Fix lint error

--- sgl-kernel/python/sgl_kernel/sampling.py
+++ sgl-kernel/python/sgl_kernel/sampling.py
@@ -3,8 +3,6 @@ from typing import Optional, Union
 import torch
 from sgl_kernel.utils import _to_tensor_scalar_tuple
 
-import flashinfer.sampling
-
 
 def _top_k_renorm_probs_internal(
     probs: torch.Tensor,
@@ -79,6 +77,8 @@ def top_k_renorm_probs(
     if probs.device.type == "musa":
         return _top_k_renorm_probs_internal(probs, *_to_tensor_scalar_tuple(top_k))
     else:
+        import flashinfer.sampling
+
         return flashinfer.sampling.top_k_renorm_probs(probs, top_k)
 
 
@@ -118,6 +118,8 @@ def top_p_renorm_probs(
     if probs.device.type == "musa":
         return _top_p_renorm_probs_internal(probs, *_to_tensor_scalar_tuple(top_p))
     else:
+        import flashinfer.sampling
+
         return flashinfer.sampling.top_p_renorm_probs(probs, top_p)
 
 
@@ -463,6 +465,8 @@ def top_k_mask_logits(
     if logits.device.type == "musa":
         return _top_k_mask_logits_internal(logits, *_to_tensor_scalar_tuple(top_k))
     else:
+        import flashinfer.sampling
+
         return flashinfer.sampling.top_k_mask_logits(logits, top_k)
 
 
