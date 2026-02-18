5892be78295c88fe0a33788c00c5e5f34d59fa7b
SYChen123 <csy512356@ant-intl.com>
2026-02-19 00:08:13 +0800

    fix illegal mem access

--- python/sglang/srt/speculative/ngram_worker_v2.py
+++ python/sglang/srt/speculative/ngram_worker_v2.py
@@ -234,9 +234,6 @@ class NGRAMWorkerV2(NGRAMWorker):
                 accept_index,
             ) = verify_input.sample(model_worker_batch, logits_output, vocab_mask)
             new_seq_lens = model_worker_batch.seq_lens + accept_length
-            verify_done = torch.get_device_module(self.device).Event()
-            verify_done.record()
-
             verified_tokens = predict[accept_index].flatten()
 
             # copy kvcache will not use the new_seq_lens
@@ -247,6 +244,9 @@ class NGRAMWorkerV2(NGRAMWorker):
                 self.token_to_kv_pool_allocator,
                 self.draft_token_num,
             )
+            verify_done = torch.get_device_module(self.device).Event()
+            verify_done.record()
+
             self._update_ngram_cache(model_worker_batch)
             model_worker_batch.forward_mode = ForwardMode.DECODE
 
@@ -259,7 +259,7 @@ class NGRAMWorkerV2(NGRAMWorker):
                 batch_result.next_token_ids,
                 batch_result.can_run_cuda_graph,
             )
-            new_seq_lens = model_worker_batch.seq_lens
+            new_seq_lens = model_worker_batch.seq_lens.clone()
 
             verified_tokens = torch.zeros(
                 bs, self.draft_token_num, dtype=torch.int32
