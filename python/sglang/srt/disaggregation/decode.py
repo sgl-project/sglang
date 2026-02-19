19a07b58527e44950797f854bb1d1b638cf6e150
Yangmin Li <yangmin@nvidia.com>
2026-02-18 23:08:34 -0800

    format

--- python/sglang/srt/disaggregation/decode.py
+++ python/sglang/srt/disaggregation/decode.py
@@ -173,7 +173,9 @@ class HybridMambaDecodeReqToTokenPool(HybridReqToTokenPool):
         )
         self.enable_mamba_extra_buffer = enable_mamba_extra_buffer
         self.enable_memory_saver = enable_memory_saver
-        effective_mamba_size = (mamba_size if mamba_size is not None else size) + pre_alloc_size
+        effective_mamba_size = (
+            mamba_size if mamba_size is not None else size
+        ) + pre_alloc_size
         self._init_mamba_pool(
             size=effective_mamba_size,
             mamba_spec_state_size=size + pre_alloc_size,
