85f63f40bb48dc83cf068cd89e5f282893c6cb05
Mick <mickjagger19@icloud.com>
2026-02-19 20:24:10 +0800

    upd

--- python/sglang/multimodal_gen/test/server/testcase_configs.py
+++ python/sglang/multimodal_gen/test/server/testcase_configs.py
@@ -397,7 +397,16 @@ ONE_GPU_CASES_A: list[DiffusionTestCase] = [
     ),
     DiffusionTestCase(
         "zimage_image_t2i",
-        DiffusionServerArgs(model_path="Tongyi-MAI/Z-Image-Turbo", modality="image"),
+        DiffusionServerArgs(
+            model_path="Tongyi-MAI/Z-Image-Turbo", modality="image", warmup=True
+        ),
+        T2I_sampling_params,
+    ),
+    DiffusionTestCase(
+        "zimage_image_t2i_warmup",
+        DiffusionServerArgs(
+            model_path="Tongyi-MAI/Z-Image-Turbo", modality="image", warmup=True
+        ),
         T2I_sampling_params,
     ),
     # Multi-LoRA test case for Z-Image-Turbo
