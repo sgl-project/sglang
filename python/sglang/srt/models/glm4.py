25c06b95359d64834ac106a4f7bd969beaf641db
JD-ETH <jaedon.guo@gmail.com>
2026-02-22 02:54:35 +0000

    bugfix

--- python/sglang/srt/models/glm4.py
+++ python/sglang/srt/models/glm4.py
@@ -464,11 +464,11 @@ class Glm4ForCausalLM(nn.Module):
         # Stacked params mapping for unified weight loading API
         self.stacked_params_mapping = [
             # (param_name, shard_name, shard_id)
-            ("qkv_proj", "q_proj", "q"),
-            ("qkv_proj", "k_proj", "k"),
-            ("qkv_proj", "v_proj", "v"),
-            ("gate_up_proj", "gate_proj", 0),
-            ("gate_up_proj", "up_proj", 1),
+            (".qkv_proj", ".q_proj", "q"),
+            (".qkv_proj", ".k_proj", "k"),
+            (".qkv_proj", ".v_proj", "v"),
+            (".gate_up_proj", ".gate_proj", 0),
+            (".gate_up_proj", ".up_proj", 1),
         ]
 
         # For EAGLE3 support
