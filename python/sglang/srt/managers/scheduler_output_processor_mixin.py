0e0536a05ea57a0eeecb3484f877c5d062c4cfbf
siyu <liusy58@linux.alibaba.com>
2026-02-25 10:13:03 +0800

    Delete mm.feature after decode phase (#17324)

--- python/sglang/srt/managers/scheduler_output_processor_mixin.py
+++ python/sglang/srt/managers/scheduler_output_processor_mixin.py
@@ -453,6 +453,13 @@ class SchedulerOutputProcessorMixin:
             req.check_finished(new_accepted_len)
 
             if req.finished():
+                # delete feature to save memory
+                if req.multimodal_inputs is not None:
+                    for mm_item in req.multimodal_inputs.mm_items:
+                        pixel_values = mm_item.feature
+                        if isinstance(pixel_values, torch.Tensor):
+                            mm_item.feature = None
+                            del pixel_values
                 self.maybe_collect_routed_experts(req)
 
                 if self.server_args.disaggregation_decode_enable_offload_kvcache:
