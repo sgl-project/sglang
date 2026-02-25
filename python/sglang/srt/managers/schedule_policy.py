5185b516e2ed465a1c538238715fb996bfb81363
Michelle Wu <michellewu351@gmail.com>
2026-02-25 21:08:56 +0800

    Update schedule_policy.py

--- python/sglang/srt/managers/schedule_policy.py
+++ python/sglang/srt/managers/schedule_policy.py
@@ -719,6 +719,12 @@ class PrefillAdder:
     def add_one_req(
         self, req: Req, has_chunked_req: bool, truncation_align_size: Optional[int]
     ):
+        if (self.prefill_delayer_single_pass is not None) and (
+            not self.prefill_delayer_single_pass.negotiate_should_allow_prefill(
+                local_prefillable=True
+            )
+        ):
+            return AddReqResult.OTHER
         # TODO support cp with multiple requests
         # Enabling context parallelism currently presents precision issues;
         # therefore, the prefill-batch setting is temporarily set to 1.
@@ -764,13 +770,6 @@ class PrefillAdder:
             input_tokens = self.ceil_paged_tokens(req.extend_input_len)
 
             if input_tokens >= self.rem_input_tokens and len(self.can_run_list) != 0:
-                return AddReqResult.OTHER
-
-            if (self.prefill_delayer_single_pass is not None) and (
-                not self.prefill_delayer_single_pass.negotiate_should_allow_prefill(
-                    local_prefillable=True
-                )
-            ):
                 return AddReqResult.OTHER
 
             if self.dllm_config is not None:
