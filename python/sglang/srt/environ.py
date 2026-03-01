04179b4416c6653ae350c352d2bc128410ea9247
hnyls2002 <lsyincs@gmail.com>
2026-03-01 12:52:12 -0800

    remove duplicate

--- python/sglang/srt/environ.py
+++ python/sglang/srt/environ.py
@@ -246,7 +246,6 @@ class Envs:
     # Scheduler: others:
     SGLANG_EMPTY_CACHE_INTERVAL = EnvFloat(-1)  # in seconds. Set if you observe high memory accumulation over a long serving period.
     SGLANG_DISABLE_CONSECUTIVE_PREFILL_OVERLAP = EnvBool(False)
-    SGLANG_ENABLE_OPT_PREBUILT_BATCH = EnvBool(False)
     SGLANG_SCHEDULER_MAX_RECV_PER_POLL = EnvInt(-1)
     SGLANG_EXPERIMENTAL_CPP_RADIX_TREE = EnvBool(False)
     SGLANG_DYNAMIC_CHUNKING_SMOOTH_FACTOR = EnvFloat(0.75)
