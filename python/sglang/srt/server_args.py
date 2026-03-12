e647e004f438710d0ce6c001c4d61e6ec2e7ca48
chenxu214 <justin_cc2025@163.com>
2026-03-12 20:39:53 +0800

    clean

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -2573,9 +2573,12 @@ class ServerArgs:
             fuse_mode = os.environ.get("SGLANG_NPU_FUSED_MOE_MODE", None)
             if fuse_mode not in ["1", "2"]:
                 raise ValueError(
-                    f"Wrong value of {fuse_mode=}, the NPU only support 1 or 2.")
+                    f"Wrong value of {fuse_mode=}, the NPU only support 1 or 2."
+                )
             elif fuse_mode == "2":
-                assert self.quantization == "modelslim", "When fuse_mode is set to 2, the NPU supports only ModelSlim quantization."
+                assert (
+                    self.quantization == "modelslim"
+                ), "When fuse_mode is set to 2, the NPU supports only ModelSlim quantization."
         if self.moe_a2a_backend == "flashinfer":
             self.ep_size = self.tp_size
             logger.warning(
