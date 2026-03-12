8909c578ea3177f15fac22af20e34e61b79bdc9c
Roi Koren <roik@nvidia.com>
2026-03-12 12:10:51 +0200

    Relax KL threshold

--- test/manual/models/test_nvidia_nemotron_nano_v2.py
+++ test/manual/models/test_nvidia_nemotron_nano_v2.py
@@ -28,7 +28,7 @@ class TestNvidiaNemotronNanoV2BF16ExtraBuffer(
     cache_chunk_size = 256
     gsm8k_accuracy_thres = 0.87
     kl_div_thres = 0.002
-    kl_div_thres_prefill = 0.008
+    kl_div_thres_prefill = 0.01
     other_args = [
         "--max-mamba-cache-size",
         "256",
