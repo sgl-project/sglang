6b2e79ea140559d9e339a6acf89c2da94d4f3b0c
ZLkanyo009 <407120045@qq.com>
2026-03-17 07:56:58 +0000

    [Bugfix] fix qwen3vl hang when --mm-enable-dp-encoder is enable

--- python/sglang/srt/models/qwen3_vl.py
+++ python/sglang/srt/models/qwen3_vl.py
@@ -329,6 +329,7 @@ class Qwen3VLMoeVisionModel(nn.Module, RotaryPosMixin):
                 self.num_position_embeddings,
                 self.hidden_size,
                 quant_config=quant_config,
+                enable_tp=not use_data_parallel,
                 use_attn_tp_group=is_dp_attention_enabled(),
                 enable_tp=False if use_data_parallel else True,
                 prefix=add_prefix("pos_embed", prefix),
