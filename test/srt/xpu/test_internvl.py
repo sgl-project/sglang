4f50912aa0df4dedaf09965e14cbfd94607bb45d
CaoE <e.cao@intel.com>
2026-03-19 14:15:37 +0000

    reslove conflicts

--- test/srt/xpu/test_internvl.py
+++ test/srt/xpu/test_internvl.py
@@ -143,8 +143,5 @@ class TestInternVL35_2BTritonServer(ImageOpenAITestMixin, InternVLXPUServerBase)
         pass
 
 
-# Delete the mixin classes so that they are not collected by pytest
-del ImageOpenAITestMixin
-
 if __name__ == "__main__":
     unittest.main()
