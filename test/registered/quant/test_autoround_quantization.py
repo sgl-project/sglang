5531a071ea0be54718904ca1592887561b0cb7cf
Wang, Mengni <mengni.wang@intel.com>
2026-03-31 14:31:19 +0800

    Update test_autoround_quantization.py

--- test/registered/quant/test_autoround_quantization.py
+++ test/registered/quant/test_autoround_quantization.py
@@ -24,7 +24,7 @@ from sglang.test.test_utils import (
     popen_launch_server,
 )
 
-register_cuda_ci(est_time=120, suite="stage-b-test-large-1-gpu")
+register_cuda_ci(est_time=120, suite="stage-b-test-1-gpu-large")
 
 
 class TestAutoRoundQuantization(CustomTestCase):
