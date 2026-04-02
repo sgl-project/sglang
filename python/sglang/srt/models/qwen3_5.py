fbb68ad0691df75a2d7e109f1acc645a6d908f4d
Chen, Todd <zhenchen@amd.com>
2026-04-02 00:51:20 -0500

    revert changes in Qwen3_5MoeForCausalLM

--- python/sglang/srt/models/qwen3_5.py
+++ python/sglang/srt/models/qwen3_5.py
@@ -1110,14 +1110,6 @@ class Qwen3_5MoeForCausalLM(Qwen3_5ForCausalLM):
     ) -> None:
         super().__init__(config=config, quant_config=quant_config, prefix=prefix)
 
-    def _get_num_fused_shared_experts(self):
-        if not (
-            hasattr(self.model, "layers")
-            and len(self.model.layers) > 0
-            and hasattr(self.model.layers[0].mlp, "num_fused_shared_experts")
-        ):
-            return 0
-        return self.model.layers[0].mlp.num_fused_shared_experts
 
     def load_weights(self, weights: Iterable[Tuple[str, torch.Tensor]]):
         stacked_params_mapping = [
@@ -1134,9 +1126,6 @@ class Qwen3_5MoeForCausalLM(Qwen3_5ForCausalLM):
             ("in_proj_ba.", "in_proj_a.", 1),
         ]
 
-        num_experts_base = self.config.num_experts
-        num_fused_shared_experts = self._get_num_fused_shared_experts()
-        num_experts = num_experts_base + num_fused_shared_experts
 
         # Params for weights, fp8 weight scales, fp8 activation scales
         # (param_name, weight_name, expert_id, shard_id)
@@ -1144,7 +1133,7 @@ class Qwen3_5MoeForCausalLM(Qwen3_5ForCausalLM):
             ckpt_gate_proj_name="gate_proj",
             ckpt_down_proj_name="down_proj",
             ckpt_up_proj_name="up_proj",
-            num_experts=num_experts,
+            num_experts=self.config.num_experts,
         )
 
         # Skip loading extra parameters for GPTQ/modelopt models.
