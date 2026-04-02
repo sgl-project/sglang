3485ea2129ebf23fa64fd648b98792fc05e0a2c6
konka <1094918@gmail.com>
2026-04-02 21:05:14 +0800

    update npu_gguf test file

--- test/registered/ascend/basic_function/quant/test_npu_gguf.py
+++ test/registered/ascend/basic_function/quant/test_npu_gguf.py
@@ -14,7 +14,6 @@ from sglang.test.test_utils import (
     popen_launch_server,
 )
 
-register_npu_ci(est_time=400, suite="stage-b-test-1-npu-a2", nightly=False)
 register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 logger = logging.getLogger(__name__)
