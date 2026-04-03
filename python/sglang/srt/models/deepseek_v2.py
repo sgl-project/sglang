680810740e5e3148a0e24c6bad91d1ee6c231008
wuyuefeng <565948592@qq.com>
2026-04-03 11:05:19 +0800

    [NPU] enable index Cache for npu

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -1422,7 +1422,6 @@ class DeepseekV2AttentionMLA(
                 forward_batch,
                 zero_allocator,
                 layer_scatter_modes,
-                prev_topk_indices,
             )
         else:
             raise NotImplementedError
