5a783aaab4b0d5e27950b6a81fb6e1b24c6be45c
DarkSharpness <2040703891@qq.com>
2026-04-06 12:20:03 +0800

    minor: reduce ci time

--- python/sglang/jit_kernel/tests/test_softmax.py
+++ python/sglang/jit_kernel/tests/test_softmax.py
@@ -9,8 +9,8 @@ from sglang.jit_kernel.softmax import softmax_sampling
 from sglang.jit_kernel.utils import get_ci_test_range
 from sglang.test.ci.ci_register import register_cuda_ci
 
-register_cuda_ci(est_time=60, suite="stage-b-kernel-unit-1-gpu-large")
-register_cuda_ci(est_time=300, suite="nightly-kernel-1-gpu", nightly=True)
+register_cuda_ci(est_time=40, suite="stage-b-kernel-unit-1-gpu-large")
+register_cuda_ci(est_time=60, suite="nightly-kernel-1-gpu", nightly=True)
 
 
 DEVICE = "cuda"
