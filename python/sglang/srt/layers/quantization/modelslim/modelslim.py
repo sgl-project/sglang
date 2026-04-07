df09417935999620e4ff709806055b7102771274
iridiumine <42236072+iridiumine@users.noreply.github.com>
2026-04-07 20:41:22 +0800

    revert modelslim modify

--- python/sglang/srt/layers/quantization/modelslim/modelslim.py
+++ python/sglang/srt/layers/quantization/modelslim/modelslim.py
@@ -153,16 +153,9 @@ class ModelSlimConfig(QuantizationConfig):
                 key = "vision_model"
             elif "visual" in prefix:
                 key = "visual"
-            if (
-                "vision_tower" in prefix
-                or "mm_projector" in prefix
-                or "in_proj_qkvz" in prefix
-                or "in_proj_ba" in prefix
-            ):
+            if "vision_tower" in prefix or "mm_projector" in prefix:
                 prefix = prefix.replace(r"attn.qkv_proj", r"wqkv")
                 prefix = prefix.replace(r"attn.proj", r"wo")
-                prefix = prefix.replace(r"in_proj_qkvz", r"in_proj_qkv")
-                prefix = prefix.replace(r"in_proj_ba", r"in_proj_b")
             packed_modules_mapping_subset = self.packed_modules_mapping.get(key, {})
             prefix_in_quant_config = prefix
             proj_name = prefix.split(".")[-1]
