b3c7ce45cea1cff1fc50e7a979116c76999ccfb6
iridiumine <iridiumine@users.noreply.github.com>
2026-04-08 03:07:52 +0000

    move fused_gdn_gating_kernel_without_sigmoid to sgl-kernel-npu

--- python/sglang/srt/hardware_backend/npu/attention/ascend_gdn_backend.py
+++ python/sglang/srt/hardware_backend/npu/attention/ascend_gdn_backend.py
@@ -1,15 +1,15 @@
 from typing import Optional, Tuple, Union
 
 import torch
-from sgl_kernel_npu.fla.fused_gdn_gating import fused_gdn_gating_npu
+from sgl_kernel_npu.fla.fused_gdn_gating import (
+    fused_gdn_gating_npu,
+    fused_gdn_gating_kernel_without_sigmoid,
+)
 from sgl_kernel_npu.mamba.causal_conv1d import (
     causal_conv1d_fn_npu,
     causal_conv1d_update_npu,
 )
 
-from sglang.srt.layers.attention.fla.fused_gdn_gating import (
-    fused_gdn_gating_kernel_without_sigmoid,
-)
 from sglang.srt.layers.attention.linear.gdn_backend import (
     GDNAttnBackend,
     GDNKernelDispatcher,
