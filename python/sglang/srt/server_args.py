00a63400fe643ee8de65bc6b6eddc5401ec106a3
Baizhou Zhang <sobereddiezhang@gmail.com>
2026-04-08 22:51:05 -0700

    fix default setting

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -1490,8 +1490,10 @@ class ServerArgs:
             self.nsa_decode_backend = "tilelang"
         elif kv_cache_dtype == "fp8_e4m3":
             if major >= 10:
-                self.nsa_prefill_backend = "trtllm"
-                self.nsa_decode_backend = "trtllm"
+                if not user_set_prefill:
+                    self.nsa_prefill_backend = "trtllm"
+                if not user_set_decode:
+                    self.nsa_decode_backend = "trtllm"
             else:
                 # flashmla_auto dispatches to flashmla_sparse/flashmla_kv based on hardware and heuristics
                 if not user_set_prefill:
