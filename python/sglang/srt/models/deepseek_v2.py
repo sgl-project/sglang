47988825bb49424b656b914eb52aac3e50667255
Colin Zeng <Colin.Zeng@amd.com>
2026-04-10 20:28:28 +0000

    GLM-5(.1) MXFP4 inference fix

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -2146,7 +2146,9 @@ class DeepseekV2Model(nn.Module):
 
 class DeepseekV2ForCausalLM(nn.Module, DeepseekV2WeightLoaderMixin):
     # for quark model load
-    packed_modules_mapping = {}
+    packed_modules_mapping = {
+        "gate_up_proj": ["gate_proj", "up_proj"],
+    }
 
     def __init__(
         self,
