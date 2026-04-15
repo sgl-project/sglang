0a755561250dcf0a7ca351565978af53b689f5af
Emelin Maksim <makcum888e@mail.ru>
2026-04-15 17:43:38 +0800

    [diffusion][npu]fix path

--- python/sglang/multimodal_gen/test/server/ascend/testcase_configs_npu.py
+++ python/sglang/multimodal_gen/test/server/ascend/testcase_configs_npu.py
@@ -5,7 +5,7 @@ ONE_NPU_CASES: list[DiffusionTestCase] = [
     DiffusionTestCase(
         "flux_image_t2i_npu",
         DiffusionServerArgs(
-            model_path="/home/weights/black-forest-labs/FLUX.1-dev",
+            model_path="/root/.cache/modelscope/hub/models/black-forest-labs/FLUX.1-dev",
         ),
         T2I_sampling_params,
         run_consistency_check=False,
@@ -14,7 +14,7 @@ ONE_NPU_CASES: list[DiffusionTestCase] = [
     DiffusionTestCase(
         "wan2_1_t2v_1.3b_1_npu",
         DiffusionServerArgs(
-            model_path="/home/weights/Wan-AI/Wan2.1-T2V-1.3B-Diffusers",
+            model_path="/root/.cache/modelscope/hub/models/Wan-AI/Wan2.1-T2V-1.3B-Diffusers",
         ),
         DiffusionSamplingParams(
             prompt=T2V_PROMPT,
@@ -28,7 +28,7 @@ TWO_NPU_CASES: list[DiffusionTestCase] = [
     DiffusionTestCase(
         "flux_2_image_t2i_2npu",
         DiffusionServerArgs(
-            model_path="/home/weights/black-forest-labs/FLUX.2-dev",
+            model_path="/root/.cache/modelscope/hub/models/black-forest-labs/FLUX.2-dev",
             num_gpus=2,
             tp_size=2,
         ),
@@ -38,7 +38,7 @@ TWO_NPU_CASES: list[DiffusionTestCase] = [
     DiffusionTestCase(
         "qwen_image_t2i_2npu",
         DiffusionServerArgs(
-            model_path="/home/weights/Qwen/Qwen-Image",
+            model_path="/root/.cache/modelscope/hub/models/Qwen/Qwen-Image",
             num_gpus=2,
             # test ring attn
             ulysses_degree=1,
@@ -54,7 +54,7 @@ EIGHT_NPU_CASES: list[DiffusionTestCase] = [
     DiffusionTestCase(
         "wan2_2_t2v_14b_w8a8_8npu",
         DiffusionServerArgs(
-            model_path="/home/weights/Eco-Tech/Wan2.2-T2V-A14B-Diffusers-w8a8",
+            model_path="/root/.cache/modelscope/hub/models/Eco-Tech/Wan2.2-T2V-A14B-Diffusers-w8a8",
             num_gpus=8,
             tp_size=4,
         ),
