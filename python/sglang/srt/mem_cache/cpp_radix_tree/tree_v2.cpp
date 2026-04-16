88983fef827942658bff8c30d38850a630dd2ae4
sigama-w <1443307488@qq.com>
2026-04-16 20:30:02 +0800

    fix cpp

--- python/sglang/srt/mem_cache/cpp_radix_tree/tree_v2.cpp
+++ python/sglang/srt/mem_cache/cpp_radix_tree/tree_v2.cpp
@@ -19,23 +19,26 @@
 
 namespace radix_tree_v2 {
 
-static NodeHandle node2id(TreeNode* node) {
-  return node->node_id;
-}
+static NodeHandle node2id(TreeNode *node) { return node->node_id; }
 
 // compare function for the TreeNode pointers based on their time
 // we use LRU, so we want to evict the least recently used nodes
 // since std::priority_queue is a max-heap, we need to reverse the comparison
-static constexpr auto cmp = [](TreeNode* lhs, TreeNode* rhs) { return lhs->time() > rhs->time(); };
+static constexpr auto cmp = [](TreeNode *lhs, TreeNode *rhs) {
+  return lhs->time() > rhs->time();
+};
 
-RadixTree::RadixTree(bool disabled, std::optional<std::size_t> host_size, std::size_t page_size, std::size_t threshold)
-    : m_impl(std::make_unique<Impl>(disabled, host_size.has_value(), page_size, host_size.value_or(0), threshold)) {}
+RadixTree::RadixTree(bool disabled, std::optional<std::size_t> host_size,
+                     std::size_t page_size, std::size_t threshold)
+    : m_impl(std::make_unique<Impl>(disabled, host_size.has_value(), page_size,
+                                    host_size.value_or(0), threshold)) {}
 
 RadixTree::~RadixTree() = default;
 
 std::tuple<std::vector<at::Tensor>, std::size_t, NodeHandle, NodeHandle>
-RadixTree::match_prefix(const token_vec_t& _key) {
-  if (m_impl->disabled) return {};
+RadixTree::match_prefix(const token_vec_t &_key) {
+  if (m_impl->disabled)
+    return {};
 
   const auto key = token_slice{_key.data(), m_impl->align(_key.size())};
   const auto [host_node, _] = m_impl->tree_walk(key);
@@ -46,14 +49,17 @@ RadixTree::match_prefix(const token_vec_t& _key) {
 
   // collect all the device indices
   std::vector<at::Tensor> indices{};
-  walk_to_root(device_node, [&](TreeNode* n) { indices.push_back(n->device_indices()); });
+  walk_to_root(device_node,
+               [&](TreeNode *n) { indices.push_back(n->device_indices()); });
   std::reverse(indices.begin(), indices.end());
 
-  return {std::move(indices), host_hit_length, node2id(device_node), node2id(host_node)};
+  return {std::move(indices), host_hit_length, node2id(device_node),
+          node2id(host_node)};
 }
 
 std::vector<at::Tensor> RadixTree::evict(std::size_t num_tokens) {
-  if (m_impl->disabled || num_tokens == 0) return {};
+  if (m_impl->disabled || num_tokens == 0)
+    return {};
   auto heap = std::priority_queue{cmp, m_impl->collect_leaves_device()};
   std::vector<at::Tensor> evicted_values;
   // evict nodes until we reach the desired number of tokens
@@ -63,22 +69,26 @@ std::vector<at::Tensor> RadixTree::evict(std::size_t num_tokens) {
     heap.pop();
     // when ref_count == 0, can't be writing through
     _assert(node->on_gpu() && node->ref_count == 0);
-    if (!node->is_io_free()) continue;  // skip nodes that are undergoing IO (i.e. indices protected)
+    if (!node->is_io_free())
+      continue; // skip nodes that are undergoing IO (i.e. indices protected)
     evicted_values.push_back(node->device_indices());
     num_evict += node->length();
     const auto parent = node->parent();
     m_impl->remove_device_node(node);
     if (parent->is_leaf_device() && parent->ref_count == 0)
-      heap.push(parent);  // push parent to the heap if it is now a free leaf
+      heap.push(parent); // push parent to the heap if it is now a free leaf
   }
 
   return evicted_values;
 }
 
-std::tuple<std::vector<std::tuple<IOTicket, at::Tensor, at::Tensor>>, std::size_t>
-RadixTree::writing_through(const token_vec_t& _key, at::Tensor value) {
-  if (m_impl->disabled) return {};
-  _assert(_key.size() == std::size_t(value.size(0)), "Key and value must have the same size");
+std::tuple<std::vector<std::tuple<IOTicket, at::Tensor, at::Tensor>>,
+           std::size_t>
+RadixTree::writing_through(const token_vec_t &_key, at::Tensor value) {
+  if (m_impl->disabled)
+    return {};
+  _assert(_key.size() == std::size_t(value.size(0)),
+          "Key and value must have the same size");
 
   // just align the key to the page size, clip the unaligned tail
   const auto key = token_slice{_key.data(), m_impl->align(_key.size())};
@@ -89,42 +99,45 @@ RadixTree::writing_through(const token_vec_t& _key, at::Tensor value) {
   // insert and create a new node if the remaining part of the key is not empty
   if (host_prefix_length != key.size()) {
     m_impl->create_device_node(
-        host_node,
-        {key.begin() + host_prefix_length, key.end()},
+        host_node, {key.begin() + host_prefix_length, key.end()},
         value.slice(/*dim=*/0, host_prefix_length, key.size()));
   }
 
   // add the hit count for the device node
-  walk_to_root(host_node, [&](TreeNode* n) { n->hit_count++; });
+  walk_to_root(host_node, [&](TreeNode *n) { n->hit_count++; });
 
   std::vector<std::tuple<IOTicket, at::Tensor, at::Tensor>> result;
 
   // don't write through if hicache is disabled (no host memory), fast path
-  if (!m_impl->use_hicache) return {std::move(result), host_prefix_length};
+  if (!m_impl->use_hicache)
+    return {std::move(result), host_prefix_length};
   throw std::runtime_error("Not implemented yet");
 }
 
-std::tuple<IOTicket, std::vector<at::Tensor>> RadixTree::loading_onboard(NodeHandle, at::Tensor) {
-  if (m_impl->disabled) return {};
+std::tuple<IOTicket, std::vector<at::Tensor>>
+RadixTree::loading_onboard(NodeHandle, at::Tensor) {
+  if (m_impl->disabled)
+    return {};
   throw std::runtime_error("Not implemented yet");
 }
 
 void RadixTree::commit_writing_through(IOTicket, bool) {
-  if (m_impl->disabled) return;
+  if (m_impl->disabled)
+    return;
   throw std::runtime_error("Not implemented yet");
 }
 
 void RadixTree::commit_loading_onboard(IOTicket, bool) {
-  if (m_impl->disabled) return;
+  if (m_impl->disabled)
+    return;
   throw std::runtime_error("Not implemented yet");
 }
 
-void RadixTree::reset() {
-  m_impl->reset();
-}
+void RadixTree::reset() { m_impl->reset(); }
 
 void RadixTree::lock_ref(NodeHandle node_id, bool increment) {
-  if (m_impl->disabled) return;
+  if (m_impl->disabled)
+    return;
   m_impl->lock_ref(node_id, increment);
 }
 
@@ -136,8 +149,6 @@ std::size_t RadixTree::protected_size() const {
   return m_impl->protected_size();
 }
 
-std::size_t RadixTree::total_size() const {
-  return m_impl->total_size();
-}
+std::size_t RadixTree::total_size() const { return m_impl->total_size(); }
 
-}  // namespace radix_tree_v2
+} // namespace radix_tree_v2
--- python/sglang/srt/mem_cache/cpp_radix_tree/tree_v2_node.h
+++ python/sglang/srt/mem_cache/cpp_radix_tree/tree_v2_node.h
@@ -16,10 +16,11 @@
 namespace radix_tree_v2 {
 
 struct std_vector_hash {
-  // see https://stackoverflow.com/questions/20511347/a-good-hash-function-for-a-vector
-  std::size_t operator()(const token_vec_t& vec) const {
+  // see
+  // https://stackoverflow.com/questions/20511347/a-good-hash-function-for-a-vector
+  std::size_t operator()(const token_vec_t &vec) const {
     std::size_t hash = 0;
-    for (const auto& token : vec) {
+    for (const auto &token : vec) {
       hash ^= token + 0x9e3779b9 + (hash << 6) + (hash >> 2);
     }
     return hash;
@@ -27,23 +28,18 @@ struct std_vector_hash {
 };
 
 struct TreeNode {
- public:
-  using childern_map_t = std::unordered_map<token_vec_t, std::unique_ptr<TreeNode>, std_vector_hash>;
+public:
+  using childern_map_t =
+      std::unordered_map<token_vec_t, std::unique_ptr<TreeNode>,
+                         std_vector_hash>;
   using iterator_t = typename childern_map_t::iterator;
   using const_iterator_t = typename childern_map_t::const_iterator;
   using timestamp_t = std::chrono::steady_clock::time_point;
 
   TreeNode(std::size_t node_id_)
-      : ref_count(0),
-        hit_count(0),
-        m_io_locked(std::nullopt),
-        m_io_status(IOStatus::None),
-        m_io_ticket(),
-        m_tokens(),
-        m_device_indices(),
-        m_host_indices(),
-        m_parent(),
-        m_children(),
+      : ref_count(0), hit_count(0), m_io_locked(std::nullopt),
+        m_io_status(IOStatus::None), m_io_ticket(), m_tokens(),
+        m_device_indices(), m_host_indices(), m_parent(), m_children(),
         m_last_access_time(std::chrono::steady_clock::now()),
         node_id(node_id_) {}
 
@@ -51,93 +47,67 @@ struct TreeNode {
     m_last_access_time = time;
   }
 
-  bool is_root() const {
-    return m_parent == nullptr;
-  }
+  bool is_root() const { return m_parent == nullptr; }
 
-  timestamp_t time() const {
-    return m_last_access_time;
-  }
+  timestamp_t time() const { return m_last_access_time; }
 
-  bool on_gpu() const {
-    return m_device_indices.defined();
-  }
+  bool on_gpu() const { return m_device_indices.defined(); }
 
-  bool on_cpu() const {
-    return m_host_indices.defined();
-  }
+  bool on_cpu() const { return m_host_indices.defined(); }
 
-  bool on_gpu_only() const {
-    return on_gpu() && !on_cpu();
-  }
+  bool on_gpu_only() const { return on_gpu() && !on_cpu(); }
 
-  bool on_cpu_only() const {
-    return !on_gpu() && on_cpu();
-  }
+  bool on_cpu_only() const { return !on_gpu() && on_cpu(); }
 
-  bool on_both() const {
-    return on_gpu() && on_cpu();
-  }
+  bool on_both() const { return on_gpu() && on_cpu(); }
 
-  std::size_t length() const {
-    return m_tokens.size();
-  }
+  std::size_t length() const { return m_tokens.size(); }
 
-  bool is_leaf() const {
-    return m_children.empty();
-  }
+  bool is_leaf() const { return m_children.empty(); }
 
   bool is_leaf_device() const {
-    for (const auto& [_, child] : m_children)
-      if (child->on_gpu()) return false;  // at least one child is on the device
+    for (const auto &[_, child] : m_children)
+      if (child->on_gpu())
+        return false; // at least one child is on the device
     return true;
   }
 
-  void add_child(const token_vec_t& v, std::unique_ptr<TreeNode>&& child) {
+  void add_child(const token_vec_t &v, std::unique_ptr<TreeNode> &&child) {
     child->m_parent = this;
     m_children[v] = std::move(child);
   }
 
-  void add_child(iterator_t it, std::unique_ptr<TreeNode>&& child) {
+  void add_child(iterator_t it, std::unique_ptr<TreeNode> &&child) {
     child->m_parent = this;
     it->second = std::move(child);
   }
 
-  void erase_child(const token_vec_t& v) {
+  void erase_child(const token_vec_t &v) {
     _assert(m_children.erase(v) > 0, "Child node not found");
   }
 
-  iterator_t find_child(const token_vec_t& v) {
-    return m_children.find(v);
-  }
+  iterator_t find_child(const token_vec_t &v) { return m_children.find(v); }
 
-  iterator_t begin() {
-    return m_children.begin();
-  }
+  iterator_t begin() { return m_children.begin(); }
 
-  iterator_t end() {
-    return m_children.end();
-  }
+  iterator_t end() { return m_children.end(); }
 
-  const_iterator_t begin() const {
-    return m_children.begin();
-  }
+  const_iterator_t begin() const { return m_children.begin(); }
 
-  const_iterator_t end() const {
-    return m_children.end();
-  }
+  const_iterator_t end() const { return m_children.end(); }
 
-  TreeNode* parent() {
-    return m_parent;
-  }
+  TreeNode *parent() { return m_parent; }
 
   // set up all data structures except for parent-child relationship
-  friend void split_prefix(TreeNode* new_node, TreeNode* old_node, std::size_t prefix_length) {
+  friend void split_prefix(TreeNode *new_node, TreeNode *old_node,
+                           std::size_t prefix_length) {
     auto tokens = std::move(old_node->m_tokens);
-    _assert(0 < prefix_length && prefix_length < tokens.size(), "Invalid prefix size for split");
+    _assert(0 < prefix_length && prefix_length < tokens.size(),
+            "Invalid prefix size for split");
 
     // set up tokens
-    old_node->m_tokens = token_vec_t(tokens.begin() + prefix_length, tokens.end());
+    old_node->m_tokens =
+        token_vec_t(tokens.begin() + prefix_length, tokens.end());
     new_node->m_tokens = std::move(tokens);
     new_node->m_tokens.resize(prefix_length);
 
@@ -145,12 +115,14 @@ struct TreeNode {
     const int64_t new_size = new_node->length();
     const int64_t old_size = old_node->length();
     if (old_node->m_device_indices.defined()) {
-      auto new_indices = old_node->m_device_indices.split_with_sizes({new_size, old_size});
+      auto new_indices =
+          old_node->m_device_indices.split_with_sizes({new_size, old_size});
       new_node->m_device_indices = std::move(new_indices[0]);
       old_node->m_device_indices = std::move(new_indices[1]);
     }
     if (old_node->m_host_indices.defined()) {
-      auto new_indices = old_node->m_host_indices.split_with_sizes({new_size, old_size});
+      auto new_indices =
+          old_node->m_host_indices.split_with_sizes({new_size, old_size});
       new_node->m_host_indices = std::move(new_indices[0]);
       old_node->m_host_indices = std::move(new_indices[1]);
     }
@@ -159,8 +131,9 @@ struct TreeNode {
     new_node->ref_count = old_node->ref_count;
     new_node->hit_count = old_node->hit_count;
 
-    // If the old node (child) was locked for IO, the new node (parent) does not need
-    // to be locked, since it is naturally protected by the child node's lock.
+    // If the old node (child) was locked for IO, the new node (parent) does not
+    // need to be locked, since it is naturally protected by the child node's
+    // lock.
     if (old_node->m_io_locked.has_value()) {
       new_node->m_io_locked = false;
       new_node->m_io_status = old_node->m_io_status;
@@ -173,30 +146,18 @@ struct TreeNode {
     const auto a = token_slice{key}.subspan(offset);
     const auto b = token_slice{m_tokens}.subspan(offset);
     const auto [it_a, it_b] = std::ranges::mismatch(a, b);
-    return it_a - a.begin();  // return the index of the first differing token
+    return it_a - a.begin(); // return the index of the first differing token
   }
 
-  at::Tensor device_indices() const {
-    return m_device_indices;
-  }
-  at::Tensor host_indices() const {
-    return m_host_indices;
-  }
+  at::Tensor device_indices() const { return m_device_indices; }
+  at::Tensor host_indices() const { return m_host_indices; }
 
   // visiting tokens are always unsafe (use `diff_key` instead)
-  token_vec_t& _unsafe_tokens() {
-    return m_tokens;
-  }
-  at::Tensor& _unsafe_device_indices() {
-    return m_device_indices;
-  }
-  at::Tensor& _unsafe_host_indices() {
-    return m_host_indices;
-  }
+  token_vec_t &_unsafe_tokens() { return m_tokens; }
+  at::Tensor &_unsafe_device_indices() { return m_device_indices; }
+  at::Tensor &_unsafe_host_indices() { return m_host_indices; }
 
-  bool is_io_free() const {
-    return m_io_status == IOStatus::None;
-  }
+  bool is_io_free() const { return m_io_status == IOStatus::None; }
 
   bool is_io_device_to_host() const {
     return m_io_status == IOStatus::DeviceToHost;
@@ -208,50 +169,49 @@ struct TreeNode {
 
   void root_reset() {
     _assert(is_root(), "Only root node can call root_reset");
-    _assert(
-        m_io_status == IOStatus::None && m_io_locked == std::nullopt,
-        "IO operation in progress, cannot reset root node");
-    _assert(this->m_tokens.empty(), "Root node tokens should be empty on reset");
-    _assert(
-        !this->m_device_indices.defined() && !this->m_host_indices.defined(),
-        "Root node indices should be always be empty and never assigned");
+    _assert(m_io_status == IOStatus::None && m_io_locked == std::nullopt,
+            "IO operation in progress, cannot reset root node");
+    _assert(this->m_tokens.empty(),
+            "Root node tokens should be empty on reset");
+    _assert(!this->m_device_indices.defined() &&
+                !this->m_host_indices.defined(),
+            "Root node indices should be always be empty and never assigned");
     m_children.clear();
     this->access();
   }
 
- public:
+public:
   std::size_t ref_count;
   std::size_t hit_count;
 
- private:
+private:
   enum class IOStatus : std::uint8_t {
     None,
     HostToDevice,
     DeviceToHost,
   };
 
-  std::optional<bool> m_io_locked;  // whether the node is locked in IO operation
+  std::optional<bool> m_io_locked; // whether the node is locked in IO operation
   IOStatus m_io_status;
   IOTicket m_io_ticket;
 
   token_vec_t m_tokens;
-  at::Tensor m_device_indices;  // indices of device value
-  at::Tensor m_host_indices;    // indices of host value
-  TreeNode* m_parent;
+  at::Tensor m_device_indices; // indices of device value
+  at::Tensor m_host_indices;   // indices of host value
+  TreeNode *m_parent;
   childern_map_t m_children;
   timestamp_t m_last_access_time;
 
- public:
-  const std::size_t node_id;  // unique ID for the node
+public:
+  const std::size_t node_id; // unique ID for the node
 };
 
-template <typename F>
-inline TreeNode* walk_to_root(TreeNode* t, const F& f) {
+template <typename F> inline TreeNode *walk_to_root(TreeNode *t, const F &f) {
   while (!t->is_root()) {
     f(t);
     t = t->parent();
   }
-  return t;  // return the root node
+  return t; // return the root node
 }
 
-}  // namespace radix_tree_v2
+} // namespace radix_tree_v2
