7cf98cb1ef1dc80959d7a58e7840923226a88d50
Mick <mickjagger19@icloud.com>
2026-04-18 09:36:50 +0800

    upd

--- python/sglang/multimodal_gen/test/server/gpu_cases.py
+++ python/sglang/multimodal_gen/test/server/gpu_cases.py
@@ -517,7 +517,9 @@ TWO_GPU_CASES_A = [
         "ltx_2_3_two_stage_ti2v_2gpus",
         DiffusionServerArgs(
             model_path="Lightricks/LTX-2.3",
-            extras=["--pipeline-class-name LTX2TwoStagePipeline --ltx2-two-stage-device-mode original"],
+            extras=[
+                "--pipeline-class-name LTX2TwoStagePipeline --ltx2-two-stage-device-mode original"
+            ],
         ),
         TI2V_sampling_params,
     ),
