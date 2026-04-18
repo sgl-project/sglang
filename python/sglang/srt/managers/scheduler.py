e78b2902f2c23fc5c4cb9ea798611311c6538cb3
Zaire404 <3147879462@qq.com>
2026-04-18 07:22:41 +0000

    update
    Signed-off-by: Zaire404 <3147879462@qq.com>

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1465,7 +1465,7 @@ class Scheduler(
                     pop_and_process()
             elif current_batch is None:
                 # When the server is idle, do self-check and re-init some states
-                self.self_check_during_idle()
+                self.on_idle()
 
             # Run sample of the current batch
             # It depends on the result of the last batch (e.g., grammar), so we run it after the last batch is processed.
