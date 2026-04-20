9bb7075c554bdb338eb8eeddf3c06dd06adfecc6
qinsir5522 <15769255522@163.com>
2026-04-20 09:09:01 +0800

    Clean code.

--- python/sglang/srt/layers/moe/topk.py
+++ python/sglang/srt/layers/moe/topk.py
@@ -430,9 +430,7 @@ def fused_topk_torch_native(
         scores = scoring_func_impl(gating_output)
         if correction_bias.dim() != scores.dim():
             correction_bias = correction_bias.unsqueeze(0)
-        scores_for_choice = scores.view(
-            -1, n_routed_experts
-        ) + correction_bias
+        scores_for_choice = scores.view(-1, n_routed_experts) + correction_bias
         topk_ids = torch.topk(scores_for_choice, k=topk, dim=-1, sorted=False)[1]
         topk_weights = scores.gather(1, topk_ids)
     else:
