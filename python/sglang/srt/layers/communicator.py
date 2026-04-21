165209249f4c18433e1c86651a875936db8381f8
Jinyang Yuan <154768711+jinyangyuan-nvidia@users.noreply.github.com>
2026-04-20 20:46:19 -0700

    Integrate MixedComm from FlashInfer to fuse communication kernels when attention TP and DP are both used
    Signed-off-by: Jinyang Yuan <154768711+jinyangyuan-nvidia@users.noreply.github.com>

--- python/sglang/srt/layers/communicator.py
+++ python/sglang/srt/layers/communicator.py
@@ -1212,14 +1212,14 @@ class CommunicateSummableTensorPairFn:
             get_local_dp_buffer(),
             hidden_states,
         )
-        if should_use_dp_reduce_scatterv():
+        if allow_reduce_scatter and forward_batch.dp_padding_mode.is_max_len():
+            dp_reduce_scatter_tensor(hidden_states, global_hidden_states)
+        elif should_use_dp_reduce_scatterv():
             get_tp_group().reduce_scatterv(
                 global_hidden_states,
                 output=hidden_states,
                 sizes=get_dp_global_num_tokens(),
             )
-        elif allow_reduce_scatter and forward_batch.dp_padding_mode.is_max_len():
-            dp_reduce_scatter_tensor(hidden_states, global_hidden_states)
         else:
             dp_scatter(hidden_states, global_hidden_states, forward_batch)
         return hidden_states, residual
