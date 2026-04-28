7b7843a9a96ee5935764b350c763cdf3da8e0f08
hnyls2002 <lsyincs@gmail.com>
2026-04-28 15:47:45 -0700

    restore TODO about cpu vs gpu max for max_extend_len

--- python/sglang/srt/layers/attention/wave_backend.py
+++ python/sglang/srt/layers/attention/wave_backend.py
@@ -292,6 +292,9 @@ class WaveAttnBackend(AttentionBackend):
                 )
             )
             mask_indptr = None
+            # TODO(FIXME): This will trigger an invalid Eagle tree when using
+            # `max(spec_info.num_accepted_tokens_cpu)`.
+            # It might have been forgotten to update somewhere.
             max_extend_len = torch.max(spec_info.num_accepted_tokens).item()
             num_kv_splits = None
             attn_logits = None
