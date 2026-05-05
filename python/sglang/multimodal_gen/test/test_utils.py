3e88de073de148877b350934b438373e36ccaef7
Mick <mickjagger19@icloud.com>
2026-05-05 09:40:51 +0800

    Update diffusion ci-data pin

--- python/sglang/multimodal_gen/test/test_utils.py
+++ python/sglang/multimodal_gen/test/test_utils.py
@@ -33,7 +33,7 @@ if TYPE_CHECKING:
 
 logger = init_logger(__name__)
 
-SGL_TEST_FILES_CI_DATA_REVISION = "35e022eee6c7e082724494ebb5771d605c76749d"
+SGL_TEST_FILES_CI_DATA_REVISION = "437539b330592b1421d239b22d7d76b4a6d08fda"
 SGL_TEST_FILES_CONSISTENCY_GT_ROOT = (
     "https://raw.githubusercontent.com/"
     f"sgl-project/ci-data/{SGL_TEST_FILES_CI_DATA_REVISION}/"
