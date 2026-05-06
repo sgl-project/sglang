fbd9fb73c8e7c7d51218bac851590e96ed2dc132
Ho-Ren (Jack) Chuang <horenchuang@bytedance.com>
2026-05-06 05:31:43 +0000

    Fold Q8KV8 kerutils stub into config

--- python/sglang/jit_kernel/csrc/flashmla_q8kv8_sparse_prefill/config.h
+++ python/sglang/jit_kernel/csrc/flashmla_q8kv8_sparse_prefill/config.h
@@ -6,10 +6,39 @@
 #include <cutlass/cluster_launch.hpp>
 
 #include "defines.h"
-#include "kerutils_stub.h"
 #include "params.h"
 #include <cooperative_groups.h>
+#include <cstdio>
+#include <cstdlib>
 #include <math_constants.h>
+
+#define KU_ASSERT(cond)                                                             \
+  do {                                                                              \
+    if (!(cond)) {                                                                  \
+      fprintf(stderr, "KU_ASSERT failed (%s:%d): %s\n", __FILE__, __LINE__, #cond); \
+      exit(1);                                                                      \
+    }                                                                               \
+  } while (0)
+
+#define KU_CUDA_CHECK(call)                                                                     \
+  do {                                                                                          \
+    cudaError_t err = (call);                                                                   \
+    if (err != cudaSuccess) {                                                                   \
+      fprintf(stderr, "CUDA error (%s:%d): %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
+      exit(1);                                                                                  \
+    }                                                                                           \
+  } while (0)
+
+#define KU_CHECK_KERNEL_LAUNCH() KU_CUDA_CHECK(cudaGetLastError())
+
+namespace ku {
+
+template <typename T>
+__host__ __device__ __forceinline__ T ceil_div(T a, T b) {
+  return (a + b - 1) / b;
+}
+
+}  // namespace ku
 
 namespace sm90::fwd {
 
