c0b4f305605e51a2835806f47a7de956b14697e9
kousaka <wanghanpei@bytedance.com>
2026-05-07 06:40:51 +0000

    add vit-pack support

--- python/sglang/srt/environ.py
+++ python/sglang/srt/environ.py
@@ -530,6 +530,8 @@ class Envs:
     SGLANG_MM_BUFFER_SIZE_MB = EnvInt(0)
     SGLANG_MM_PRECOMPUTE_HASH = EnvBool(False)
     SGLANG_VIT_ENABLE_CUDA_GRAPH = EnvBool(False)
+    SGLANG_VIT_PACK = EnvBool(False)
+    SGLANG_VIT_INFER_TIMES = EnvInt(1)
     SGLANG_MM_SKIP_COMPUTE_HASH = EnvBool(False)
 
 
