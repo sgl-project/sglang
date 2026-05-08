3f20976594168341cf2f30eb9e0a4cd5be2cedc4
thechaos16 <thechaos16@gmail.com>
2026-05-09 00:38:06 +0900

    feat: add eagle3 for gemma3_causallm

--- python/sglang/srt/models/gemma3_causal.py
+++ python/sglang/srt/models/gemma3_causal.py
@@ -12,7 +12,7 @@
 # limitations under the License.
 # ==============================================================================
 import copy
-from typing import Iterable, Optional, Set, Tuple
+from typing import Iterable, List, Optional, Set, Tuple
 
 import einops
 import torch
@@ -599,6 +599,7 @@ class Gemma3TextModel(PreTrainedModel):
             prefix=add_prefix("layers", prefix),
         )
         self.norm = Gemma3RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.layers_to_capture = []
         self.post_init()
 
     def forward(
@@ -614,8 +615,11 @@ class Gemma3TextModel(PreTrainedModel):
         else:
             hidden_states = input_embeds
 
+        aux_hidden_states = []
         if _is_cpu and _is_cpu_amx_available:
-            for layer in self.layers:
+            for i, layer in enumerate(self.layers):
+                if i in self.layers_to_capture:
+                    aux_hidden_states.append(hidden_states.clone())
                 layer_outputs = layer(
                     positions=positions,
                     position_embeddings_global=None,
@@ -631,7 +635,9 @@ class Gemma3TextModel(PreTrainedModel):
 
             position_embeddings_global = self.rotary_emb(hidden_states, positions)
             position_embeddings_local = self.rotary_emb_local(hidden_states, positions)
-            for layer in self.layers:
+            for i, layer in enumerate(self.layers):
+                if i in self.layers_to_capture:
+                    aux_hidden_states.append(hidden_states.clone())
                 layer_outputs = layer(
                     positions=positions,
                     position_embeddings_global=position_embeddings_global,
@@ -643,6 +649,8 @@ class Gemma3TextModel(PreTrainedModel):
                 hidden_states = layer_outputs[0]
 
         hidden_states = self.norm(hidden_states)
+        if len(aux_hidden_states) > 0:
+            return hidden_states, aux_hidden_states
 
         return hidden_states
 
@@ -722,6 +730,7 @@ class Gemma3ForCausalLM(PreTrainedModel):
                 quant_config=quant_config,
                 prefix=add_prefix("lm_head", prefix),
             )
+        self.capture_aux_hidden_states = False
         self.post_init()
 
     def get_input_embeddings(self) -> nn.Embedding:
@@ -745,9 +754,16 @@ class Gemma3ForCausalLM(PreTrainedModel):
         hidden_states = self.model(
             input_ids, positions, forward_batch, input_embeds, **kwargs
         )
+        aux_hidden_states = None
+        if self.capture_aux_hidden_states:
+            hidden_states, aux_hidden_states = hidden_states
 
         return self.logits_processor(
-            input_ids, hidden_states, self.model.embed_tokens, forward_batch
+            input_ids,
+            hidden_states,
+            self.model.embed_tokens,
+            forward_batch,
+            aux_hidden_states,
         )
 
     @torch.no_grad()
@@ -861,6 +877,31 @@ class Gemma3ForCausalLM(PreTrainedModel):
         #         "Some weights are not initialized from checkpoints: %s", unloaded_params
         #     )
         return loaded_params
+
+    def get_embed_and_head(self):
+        return self.model.embed_tokens.weight, self.lm_head.weight
+
+    def set_embed_and_head(self, embed, head):
+        del self.model.embed_tokens.weight
+        del self.lm_head.weight
+        self.model.embed_tokens.weight = embed
+        self.lm_head.weight = head
+        torch.cuda.empty_cache()
+        torch.cuda.synchronize()
+
+    def set_eagle3_layers_to_capture(self, layer_ids: Optional[List[int]] = None):
+        if not self.pp_group.is_last_rank:
+            return
+
+        if layer_ids is None:
+            self.capture_aux_hidden_states = True
+            num_layers = self.config.num_hidden_layers
+            self.model.layers_to_capture = [2, num_layers // 2, num_layers - 3]
+        else:
+            self.capture_aux_hidden_states = True
+            # we plus 1 here because in sglang, for the ith layer, it takes the output
+            # of the (i-1)th layer as aux hidden state
+            self.model.layers_to_capture = [val + 1 for val in layer_ids]
 
 
 EntryClass = Gemma3ForCausalLM
