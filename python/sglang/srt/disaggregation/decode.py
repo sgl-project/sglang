04e86c5181050d325fd89625c8505c36d08e3c58
hsiehchiachi <hsiehchiachi@163.com>
2026-05-09 13:38:35 +0800

    [hisparse] support input length exceeding max_total_num_tokens in decode side

--- python/sglang/srt/disaggregation/decode.py
+++ python/sglang/srt/disaggregation/decode.py
@@ -478,6 +478,8 @@ class DecodePreallocQueue:
 
     def _check_if_req_exceed_kv_capacity(self, req: Req) -> bool:
         if len(req.origin_input_ids) > self.max_total_num_tokens:
+            if self.scheduler.enable_hisparse and len(req.origin_input_ids) <= self.scheduler.hisparse_max_req_len:
+                return False
             message = f"Request {req.rid} exceeds the maximum number of tokens: {len(req.origin_input_ids)} > {self.max_total_num_tokens}"
             logger.error(message)
             prepare_abort(req, message, status_code=HTTPStatus.BAD_REQUEST)
