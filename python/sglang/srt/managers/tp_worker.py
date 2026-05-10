30c55c7228d2d5377ea05cee767e057943af8d4d
fzyzcjy <ch271828n@outlook.com>
2026-05-10 22:26:34 +0800

    we-move-save-get: Move weight save and get_weights_by_name methods onto WeightExporter
    Refactor chain ID: we-move-save-get

--- python/sglang/srt/managers/tp_worker.py
+++ python/sglang/srt/managers/tp_worker.py
@@ -180,7 +180,7 @@ class BaseTpWorker(ABC):
         return success, message
 
     def get_weights_by_name(self, recv_req: GetWeightsByNameReqInput):
-        parameter = self.model_runner.get_weights_by_name(
+        parameter = self.model_runner.weight_exporter.get_weights_by_name(
             recv_req.name, recv_req.truncate_size
         )
         return parameter
