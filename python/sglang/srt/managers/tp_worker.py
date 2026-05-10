a9502aa3a83c99ed766e748553d42663854bd34c
fzyzcjy <ch271828n@outlook.com>
2026-05-10 17:44:11 +0800

    Introduce WeightExporter and move weights send group methods

--- python/sglang/srt/managers/tp_worker.py
+++ python/sglang/srt/managers/tp_worker.py
@@ -124,7 +124,7 @@ class BaseTpWorker(ABC):
         self, recv_req: InitWeightsSendGroupForRemoteInstanceReqInput
     ):
         success, message = (
-            self.model_runner.init_weights_send_group_for_remote_instance(
+            self.model_runner.weight_exporter.init_weights_send_group_for_remote_instance(
                 recv_req.master_address,
                 recv_req.ports,
                 recv_req.group_rank,
@@ -138,10 +138,12 @@ class BaseTpWorker(ABC):
     def send_weights_to_remote_instance(
         self, recv_req: SendWeightsToRemoteInstanceReqInput
     ):
-        success, message = self.model_runner.send_weights_to_remote_instance(
-            recv_req.master_address,
-            recv_req.ports,
-            recv_req.group_name,
+        success, message = (
+            self.model_runner.weight_exporter.send_weights_to_remote_instance(
+                recv_req.master_address,
+                recv_req.ports,
+                recv_req.group_name,
+            )
         )
         return success, message
 
