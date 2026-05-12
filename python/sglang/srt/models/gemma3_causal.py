0e7b0b64bbd11aa5baf9ff493914267ea7cb797f
McZyWu <zhuoyun.wu.23@ucl.ac.uk>
2026-05-12 14:41:48 +0800

    lint checks

--- python/sglang/srt/models/gemma3_causal.py
+++ python/sglang/srt/models/gemma3_causal.py
@@ -575,7 +575,7 @@ class Gemma3TextModel(PreTrainedModel):
         global_config = copy.deepcopy(config)
         global_config.rope_parameters = {
             "rope_theta": global_theta,
-            "factor": config.rope_parameters['full_attention']['factor'],
+            "factor": config.rope_parameters["full_attention"]["factor"],
             "rope_type": "linear",
         }
         self.rotary_emb = Gemma3RotaryEmbedding(config=global_config)
