82cb8da1306f79ef91ff97bcd8ffdb7bd6721550
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-05-13 22:26:37 +0300

    Fix lint

--- python/sglang/srt/layers/moe/utils.py
+++ python/sglang/srt/layers/moe/utils.py
@@ -211,7 +211,7 @@ def get_deepep_output_dtype(self) -> DeepEPOutputDtype:
     # 4. flashinfer_cutedsl and is_cutlass expects BF16 dispatch
     if (
         get_moe_runner_backend().is_flashinfer_cutedsl()
-        or get_moe_runner_backend().is_cutlass():
+        or get_moe_runner_backend().is_cutlass()
     ):
         return DeepEPOutputDtype.BF16
 
