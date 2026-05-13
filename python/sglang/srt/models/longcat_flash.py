17c334aab42b1fe9439c79e65e91280664b6d985
LinyuanLi0046 <liliny163@163.com>
2026-05-13 13:01:42 +0800

    bug fix

--- python/sglang/srt/models/longcat_flash.py
+++ python/sglang/srt/models/longcat_flash.py
@@ -301,10 +301,13 @@ class LongcatFlashMoE(nn.Module):
         final_hidden_states = self.experts(hidden_states, topk_output)
         final_hidden_states *= self.routed_scaling_factor
 
+        if self.tp_size > 1 and get_moe_a2a_backend().is_deepep():
+            zero_expert_result *= self.tp_size
+
         if self.zero_expert_type is not None and hidden_states.shape[0] > 0:
             final_hidden_states += zero_expert_result.to(final_hidden_states.device)
 
-        if self.tp_size > 1:
+        if self.tp_size > 1 and not get_moe_a2a_backend().is_deepep():
             final_hidden_states = tensor_model_parallel_all_reduce(final_hidden_states)
 
         return final_hidden_states.view(num_tokens, hidden_dim)
