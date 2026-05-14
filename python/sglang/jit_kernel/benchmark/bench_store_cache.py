d89021fc3103741bb82872cd45596c91ec79ff64
DarkSharpness <2040703891@qq.com>
2026-05-14 17:59:53 +0800

    feat: refactor JIT kernel benchmark

--- python/sglang/jit_kernel/benchmark/bench_store_cache.py
+++ python/sglang/jit_kernel/benchmark/bench_store_cache.py
@@ -1,30 +1,16 @@
-import itertools
-from typing import Tuple
-
 import torch
-import triton
-import triton.testing
 
+from sglang.jit_kernel.benchmark import marker
 from sglang.jit_kernel.benchmark.utils import (
     DEFAULT_DEVICE,
-    DEFAULT_DTYPE,
-    DEFAULT_QUANTILES,
+    create_empty,
+    create_random,
     get_benchmark_range,
 )
 from sglang.jit_kernel.kvcache import store_cache
 from sglang.test.ci.ci_register import register_cuda_ci
 
 register_cuda_ci(est_time=9, suite="base-b-kernel-benchmark-1-gpu-large")
-
-
-def sglang_jit_store_cache(
-    k: torch.Tensor,
-    v: torch.Tensor,
-    k_cache: torch.Tensor,
-    v_cache: torch.Tensor,
-    indices: torch.Tensor,
-) -> None:
-    store_cache(k, v, k_cache, v_cache, indices)
 
 
 @torch.compile()
@@ -57,9 +43,7 @@ def torch_streams_store_cache(
     current_stream.wait_stream(alt_stream)
 
 
-NUM_LAYERS = 8
-CACHE_SIZE = 2 * 1024 * 1024 // NUM_LAYERS
-
+CACHE_SIZE = 1 * 1024 * 1024
 BS_RANGE = get_benchmark_range(
     full_range=[2**n for n in range(0, 15)],
     ci_range=[16],
@@ -68,66 +52,30 @@ ITEM_SIZE = get_benchmark_range(
     full_range=[64, 128, 256, 512, 1024],
     ci_range=[1024],
 )
+FN_MAP = {
+    "jit": store_cache,
+    "torch_compile": torch_compile_store_cache,
+    "torch_streams": torch_streams_store_cache,
+}
 
-LINE_VALS = ["jit", "torch_compile", "torch_streams"]
-LINE_NAMES = ["SGL JIT Kernel", "PyTorch Compile", "PyTorch 2 Stream"]
-STYLES = [("blue", "--"), ("red", ":"), ("green", "-.")]
-X_NAMES = ["item_size", "batch_size"]
-CONFIGS = list(itertools.product(ITEM_SIZE, BS_RANGE))
 
-
-@triton.testing.perf_report(
-    triton.testing.Benchmark(
-        x_names=X_NAMES,
-        x_vals=CONFIGS,
-        line_arg="provider",
-        line_vals=LINE_VALS,
-        line_names=LINE_NAMES,
-        styles=STYLES,
-        ylabel="us",
-        plot_name="store-kvcache-performance",
-        args={},
-    )
-)
-def benchmark(
-    batch_size: int, item_size: int, provider: str
-) -> Tuple[float, float, float]:
-    k = torch.randn(
-        (NUM_LAYERS, batch_size, item_size), dtype=DEFAULT_DTYPE, device=DEFAULT_DEVICE
-    )
-    v = torch.randn(
-        (NUM_LAYERS, batch_size, item_size), dtype=DEFAULT_DTYPE, device=DEFAULT_DEVICE
-    )
-    k_cache = torch.randn(
-        (NUM_LAYERS, CACHE_SIZE, item_size), dtype=DEFAULT_DTYPE, device=DEFAULT_DEVICE
-    )
-    v_cache = torch.randn(
-        (NUM_LAYERS, CACHE_SIZE, item_size), dtype=DEFAULT_DTYPE, device=DEFAULT_DEVICE
-    )
-    indices = torch.randperm(CACHE_SIZE, device=DEFAULT_DEVICE)[:batch_size]
-    torch.cuda.synchronize()
-
-    FN_MAP = {
-        "jit": sglang_jit_store_cache,
-        "torch_compile": torch_compile_store_cache,
-        "torch_streams": torch_streams_store_cache,
-    }
-
-    def fn():
-        impl = FN_MAP[provider]
-        for i in range(NUM_LAYERS):
-            impl(k[i], v[i], k_cache[i], v_cache[i], indices)
-
-    # Custom time calculation: divide by NUM_LAYERS
-    ms, min_ms, max_ms = triton.testing.do_bench_cudagraph(
-        fn, quantiles=DEFAULT_QUANTILES
-    )
-    return (
-        1000 * ms / NUM_LAYERS,
-        1000 * max_ms / NUM_LAYERS,
-        1000 * min_ms / NUM_LAYERS,
+@marker.mark_args("item_size", ITEM_SIZE)
+@marker.mark_args("batch_size", BS_RANGE)
+@marker.mark_benchmark("impl", ["jit", "torch_compile", "torch_streams"])
+def benchmark(batch_size: int, item_size: int, impl: str):
+    torch.manual_seed(42)
+    k = create_random(batch_size, item_size)
+    k_cache = create_empty(CACHE_SIZE, item_size)
+    v = create_random(batch_size, item_size)
+    v_cache = create_empty(CACHE_SIZE, item_size)
+    indices = torch.randint(0, CACHE_SIZE, (batch_size,), device=DEFAULT_DEVICE)
+    return marker.bench_one_function(
+        FN_MAP[impl],
+        input_args=(k, v, k_cache, v_cache, indices),
+        graph_clone_args=(0, 1, 4),
+        memory_args=(k, v, indices),
     )
 
 
 if __name__ == "__main__":
-    benchmark.run(print_data=True)
+    benchmark.run()
---
+++ python/sglang/jit_kernel/benchmark/marker.py
@@ -0,0 +1,291 @@
+import itertools
+import os
+from collections import OrderedDict
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    Iterable,
+    List,
+    Literal,
+    NamedTuple,
+    Optional,
+    Tuple,
+    TypeAlias,
+    TypeVar,
+)
+
+import torch
+
+from sglang.jit_kernel.utils import cache_once
+
+F = TypeVar("F", bound=Callable[..., "BenchResult"])
+Metric: TypeAlias = 'float | Literal["avg"]'
+
+TYPE_LIST = (bool, int, float, str, torch.dtype, torch.device, None.__class__)
+DISABLE_LOG_BANDWIDTH = os.environ.get("SGLANG_KERNEL_DISABLE_LOG_BANDWIDTH") == "1"
+
+
+@cache_once
+def _get_stream(device_id: int) -> torch.cuda.Stream:
+    return torch.cuda.Stream(device=device_id)
+
+
+def _clone(in_: Any) -> Any:
+    if isinstance(in_, torch.Tensor):
+        return in_.clone()
+    elif isinstance(in_, (list, tuple)):
+        return type(in_)(_clone(x) for x in in_)
+    elif isinstance(in_, dict):
+        return {k: _clone(v) for k, v in in_.items()}
+    elif isinstance(in_, TYPE_LIST):
+        return in_
+    # NOTE: avoid silent error
+    raise ValueError(f"unsupported type: {type(in_)}")
+
+
+def _get_nbytes(in_: Any) -> int:
+    if isinstance(in_, torch.Tensor):
+        return in_.nbytes
+    elif isinstance(in_, (list, tuple)):
+        return sum(_get_nbytes(x) for x in in_)
+    elif isinstance(in_, dict):
+        return sum(_get_nbytes(v) for v in in_.values())
+    elif isinstance(in_, TYPE_LIST):
+        return 0
+    # NOTE: avoid silent error
+    raise ValueError(f"unsupported type: {type(in_)}")
+
+
+def _process_metrics(times: list[float], metrics: tuple[Metric, ...]) -> list[float]:
+    results: list[float] = []
+    times = sorted(x / 1000 for x in times)  # convert to seconds and sort
+    for metric in metrics:
+        if metric == "avg":
+            results.append(sum(times) / len(times))
+        else:
+            assert 0 <= metric <= 1, f"invalid metric: {metric}"
+            which = min(int(len(times) * metric), len(times) - 1)
+            results.append(times[which])
+    return results
+
+
+class BenchResult(NamedTuple):
+    metrics: Tuple[Metric, ...]
+    times: List[float]  # in seconds
+    memory_footprint: Optional[int]
+
+
+def bench_one_function(
+    fn: Callable,
+    *,
+    input_args: Tuple[Any, ...] = (),
+    input_kwargs: Dict[str, Any] = {},
+    use_cuda_graph: bool = True,
+    warmup_iters: int = 50,
+    replay_iters: int = 1000,
+    metrics: Tuple[Metric, ...] = (0.5, "avg"),
+    stream: torch.cuda.Stream | None = None,
+    # NOTE: should only clone the read args to avoid L2 cache effect in cuda graph
+    graph_clone_args: Iterable[int] | Literal["all"] | None = "all",
+    graph_clone_kwargs: Iterable[str] | Literal["all"] | None = "all",
+    # NOTE: for memory-bandwidth profiling
+    memory_args: Iterable[torch.Tensor] | Literal["all"] | None = None,
+    extra_memory_footprint: int | None = None,
+) -> BenchResult:
+    """
+    Benchmark a function using CUDA graph or naive loop.
+
+    :param fn: Function to benchmark
+    :param input_args: Positional arguments to pass to the function
+    :param input_kwargs: Keyword arguments to pass to the function
+    :param use_cuda_graph: Whether to use CUDA graph for benchmarking
+    :param warmup_iters: Number of warm-up iterations to run before benchmarking
+    :param replay_iters: Number of iterations to run for benchmarking
+    :param metrics: Metrics to compute from the timing results (quantiles in [0, 1] or "avg")
+    :param stream: CUDA stream to use for benchmarking (if None, a new stream will be created)
+    :param graph_clone_args: Indices of input_args to clone for each iteration.
+                             Only the read args need to be cloned to avoid L2 cache effect.
+    :param graph_clone_kwargs: Keys of input_kwargs to clone for each iteration.
+                               Only the read args need to be cloned to avoid L2 cache effect.
+    :param memory_args: Optional sequence of tensors to calculate total memory footprint.
+                        Used for memory bandwidth estimation in the profile report.
+    :param extra_memory_footprint: Additional memory footprint to consider.
+                                   Usually used for kernels with dynamic input shape.
+    """
+    # first warmup the function
+    device_id = torch.cuda.current_device()
+    if stream is None:
+        stream = _get_stream(device_id)
+    old_current_stream = torch.cuda.current_stream(device_id)
+    result: List[float] = []
+    with torch.cuda.device(device_id), torch.cuda.stream(stream):
+        stream.wait_stream(old_current_stream)
+        for _ in range(warmup_iters):
+            fn(*input_args, **input_kwargs)
+        if use_cuda_graph:
+            # NOTE: by default, reduce all the CPU-side overhead
+            rep_count = 4
+            loop_iters = 100
+            graph = torch.cuda.CUDAGraph()
+            input_args_list = [input_args] * rep_count
+            input_kwargs_list = [input_kwargs] * rep_count
+            if graph_clone_args == "all":
+                graph_clone_args = range(len(input_args))
+            elif graph_clone_args is None:
+                graph_clone_args = []
+            if graph_clone_kwargs == "all":
+                graph_clone_kwargs = input_kwargs.keys()
+            elif graph_clone_kwargs is None:
+                graph_clone_kwargs = []
+            graph_clone_args = set(graph_clone_args)
+            graph_clone_kwargs = set(graph_clone_kwargs)
+            # NOTE: we rotate the buffer here to avoid L2 cache effect
+            for i in range(1, rep_count):
+                input_args_list[i] = tuple(
+                    _clone(input_args[j]) if j in graph_clone_args else input_args[j]
+                    for j in range(len(input_args))
+                )
+                input_kwargs_list[i] = dict(
+                    (k, (_clone(v) if k in graph_clone_kwargs else v))
+                    for k, v in input_kwargs.items()
+                )
+            with torch.cuda.graph(graph, stream=stream):
+                for _ in range(loop_iters // rep_count):
+                    for args, kwargs in zip(input_args_list, input_kwargs_list):
+                        fn(*args, **kwargs)
+            # warm up the graph
+            graph.replay()
+            # then replay the graph and measure the time
+            tic = torch.cuda.Event(enable_timing=True)
+            toc = torch.cuda.Event(enable_timing=True)
+            for _ in range(max(replay_iters // loop_iters, 10)):
+                tic.record(stream)
+                graph.replay()
+                toc.record(stream)
+                stream.synchronize()
+                result.append(tic.elapsed_time(toc) / loop_iters)
+        else:
+            # NOTE: no cuda graph, naive loop
+            empty_tensor = torch.empty(64 * 1024 * 1024, device=f"cuda:{device_id}")
+            tic = torch.cuda.Event(enable_timing=True)
+            toc = torch.cuda.Event(enable_timing=True)
+            for _ in range(max(replay_iters, 10)):
+                empty_tensor.zero_()  # cold the L2 cache
+                tic.record(stream)
+                fn(*input_args, **input_kwargs)
+                toc.record(stream)
+                stream.synchronize()
+                result.append(tic.elapsed_time(toc))
+
+    stream.synchronize()
+    result = _process_metrics(result, metrics)
+    memory_footprint = None
+    if memory_args is not None:
+        if memory_args == "all":
+            memory_footprint = _get_nbytes(input_args) + _get_nbytes(input_kwargs)
+        else:
+            memory_footprint = sum(arg.nbytes for arg in memory_args)
+    if extra_memory_footprint is not None:
+        memory_footprint = (memory_footprint or 0) + extra_memory_footprint
+    return BenchResult(metrics, result, memory_footprint)
+
+
+class Benchmark(Generic[F]):
+    def __init__(self, fn: F, line_arg: str, line_vals: List[str], *, unit: str):
+        self.benchmark_fn = fn
+        self.benchmark_configs: dict[str, List[Any]] = OrderedDict()
+        self.line_arg = line_arg
+        self.line_vals = line_vals
+        assert unit in ("us", "ms", "s")
+        UNIT_SCALE = {"us": 1e-6, "ms": 1e-3, "s": 1.0}
+        self.unit = unit
+        self.unit_scale = UNIT_SCALE[unit]
+
+    def run(self) -> None:
+        def get_width(s: Any, min_width: int = 10) -> int:
+            return max(len(str(s)) + 2, min_width)
+
+        def format_latency(r: float) -> str:
+            length = len(str(int(r)))
+            if length < 5:
+                return f"{r:.4f}"
+            # decrease number of the digits
+            digits = max(0, 4 - (length - 5))
+            return f"{r:.{digits}f}"
+
+        def format_bandwidth(b: float) -> str:
+            return f"{b:.2f}"
+
+        results: List[List[float]] = []
+        bandwidth_results: List[List[float]] = []
+        benchmark_configs = OrderedDict(reversed(self.benchmark_configs.items()))
+        should_log_bandwidth = False
+        for system in self.line_vals:
+            system_list = []
+            bandwidth_list = []
+            for config in itertools.product(*benchmark_configs.values()):
+                config_dict = dict(zip(benchmark_configs.keys(), config))
+                config_dict.update({self.line_arg: system})
+                result = self.benchmark_fn(**config_dict)
+                system_list.append(result.times[0] / self.unit_scale)
+                if not DISABLE_LOG_BANDWIDTH and result.memory_footprint is not None:
+                    should_log_bandwidth = True
+                    memory_giga_bytes = result.memory_footprint / (1024**3)
+                    bandwidth = memory_giga_bytes / result.times[0]
+                    bandwidth_list.append(bandwidth)
+            results.append(system_list)
+            bandwidth_results.append(bandwidth_list)
+
+        num_ids = len(results[0])
+        id_width = len(str(num_ids - 1))
+        args_widths = [
+            max(get_width(v) for v in [key] + vals)
+            for key, vals in benchmark_configs.items()
+        ]
+        system_widths = [
+            get_width(f"{system}({self.unit})", 15) for system in self.line_vals
+        ]
+        bandwidth_widths: List[int] = []
+        if should_log_bandwidth:
+            bandwidth_widths = [
+                get_width(f"{system}(GB/s)", 15) for system in self.line_vals
+            ]
+        # id, args... , system0, system1, ...
+        print(" " * id_width, end="")
+        for key, width in zip(benchmark_configs.keys(), args_widths):
+            print(f"{key:>{width}}", end="")
+        for system, width in zip(self.line_vals, system_widths):
+            system_name = f"{system}({self.unit})"
+            print(f"{system_name:>{width}}", end="")
+        if should_log_bandwidth:
+            for system, width in zip(self.line_vals, bandwidth_widths):
+                system_name = f"{system}(GB/s)"
+                print(f"{system_name:>{width}}", end="")
+        print()
+        for id, config in enumerate(itertools.product(*benchmark_configs.values())):
+            print(f"{id:<{id_width}}", end="")
+            for arg, width in zip(config, args_widths):
+                print(f"{str(arg):>{width}}", end="")
+            for system_result, width in zip(results, system_widths):
+                print(f"{format_latency(system_result[id]):>{width}}", end="")
+            if should_log_bandwidth:
+                for bandwidth_result, width in zip(bandwidth_results, bandwidth_widths):
+                    print(f"{format_bandwidth(bandwidth_result[id]):>{width}}", end="")
+            print()
+
+
+def mark_benchmark(line_arg: str, line_vals: List[str], *, unit: str = "us"):
+    def decorator(fn: F) -> Benchmark[F]:
+        return Benchmark(fn, line_arg, line_vals, unit=unit)
+
+    return decorator
+
+
+def mark_args(name: str, vals: List[Any]):
+    def decorator(bench: Benchmark[F]) -> Benchmark[F]:
+        bench.benchmark_configs[name] = vals
+        return bench
+
+    return decorator
