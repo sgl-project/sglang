c26a24fdc90642f7976fb7311ec20364f1a6cac8
hnyls2002 <lsyincs@gmail.com>
2026-05-15 16:30:11 -0700

    [Spec] drop dead check_forward_draft_extend_after_decode; document is_draft_input/is_verify_input as cross-algo phase guards; explain pp_rank=0

--- python/sglang/srt/speculative/multi_layer_eagle_worker_v2.py
+++ python/sglang/srt/speculative/multi_layer_eagle_worker_v2.py
@@ -131,7 +131,7 @@ class MultiLayerEagleDraftWorker(BaseDraftWorker):
                 server_args=server_args,
                 gpu_id=gpu_id,
                 tp_rank=tp_rank,
-                pp_rank=0,  # FIXME
+                pp_rank=0,  # spec workers don't support pipeline parallelism
                 dp_rank=dp_rank,
                 moe_ep_rank=moe_ep_rank,
                 attn_cp_rank=attn_cp_rank,
