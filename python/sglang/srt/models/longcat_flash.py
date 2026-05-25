57d26f13b921d0ff26b2c8321ca899e4a10e8277
LinyuanLi0046 <liliny163@163.com>
2026-05-25 15:14:45 +0800

    Revert "bugfix"
    This reverts commit 49c34aa8b160ba557bdcea950a80550f0bd65ca4.

--- python/sglang/srt/models/longcat_flash.py
+++ python/sglang/srt/models/longcat_flash.py
@@ -478,9 +478,7 @@ class LongcatFlashDecoderLayer(nn.Module):
     ) -> torch.Tensor:
         # first_attn
         if get_moe_a2a_backend().is_deepep() and not self.is_first_layer:
-            residual = residual.tensor_split(self.attn_tp_size)[
-                self.attn_tp_rank
-            ].contiguous()
+            residual = residual.tensor_split(self.attn_tp_size)[self.attn_tp_rank]
         hidden_states, residual = self.moe_layer_communicator.prepare_attn(
             hidden_states, residual, forward_batch
         )
@@ -522,7 +520,7 @@ class LongcatFlashDecoderLayer(nn.Module):
             if not self.is_last_layer and get_moe_a2a_backend().is_deepep():
                 mlp_hidden_states = mlp_hidden_states.tensor_split(self.attn_tp_size)[
                     self.attn_tp_rank
-                ].contiguous()
+                ]
 
             with self.device_module.stream(self.double_stream_state.moe_alt_stream):
                 self.double_stream_state.moe_alt_stream.wait_event(self.double_stream_state.first_attn_finished)
@@ -553,7 +551,7 @@ class LongcatFlashDecoderLayer(nn.Module):
             if not self.is_last_layer and get_moe_a2a_backend().is_deepep():
                 hidden_states = hidden_states.tensor_split(self.attn_tp_size)[
                     self.attn_tp_rank
-                ].contiguous()
+                ]
 
             hidden_states = moe_hidden_states + hidden_states
         return hidden_states, residual
