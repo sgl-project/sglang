df39faf5161511cc661162707916a0abadb781a1
fzyzcjy <ch271828n@outlook.com>
2026-05-26 08:23:26 +0800

    style(kv_canary): apply pre-commit isort + black auto-fixes

--- test/registered/kv_canary/test_self_e2e_perturb_real_kv_unused_cache.py
+++ test/registered/kv_canary/test_self_e2e_perturb_real_kv_unused_cache.py
@@ -6,8 +6,8 @@ from typing import ClassVar
 from sglang.srt.kv_canary.config import CanaryMode
 from sglang.srt.kv_canary.perturb.config import TargetGroupKind
 from sglang.test.ci.ci_register import register_cuda_ci
-from sglang.test.kv_canary.e2e_base import CanaryE2EBase
 from sglang.test.kv_canary.consts import SWA_POOL_SERVER_ARGS
+from sglang.test.kv_canary.e2e_base import CanaryE2EBase
 
 register_cuda_ci(est_time=60, stage="extra-a", runner_config="1-gpu-small")
 
