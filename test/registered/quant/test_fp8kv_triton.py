95ebb3631eb774ef7b2b169847114cc059934122
YC Yen-Ching Tseng <yctseng@amd.com>
2026-05-28 23:30:50 +0800

    change register: stage-b-test-1-gpu-small-amd-mi35x

--- test/registered/quant/test_fp8kv_triton.py
+++ test/registered/quant/test_fp8kv_triton.py
@@ -13,7 +13,7 @@ from sglang.test.test_utils import (
 )
 
 register_cuda_ci(est_time=73, stage="extra-a", runner_config="1-gpu-large")
-register_amd_ci(est_time=85, suite="stage-b-test-1-gpu-large-amd")
+register_amd_ci(est_time=85, suite="stage-b-test-1-gpu-small-amd-mi35x")
 
 
 class TestFP8KVCacheTritonBackend(CustomTestCase):
