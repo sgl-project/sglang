9d1e2174d68da7c81478278519539fc3bb0bab5d
晟海 <huangtingwei.htw@antgroup.com>
2026-05-28 18:39:22 +0800

    mooncake store double-tag bug fix

--- python/sglang/srt/mem_cache/storage/mooncake_store/mooncake_store.py
+++ python/sglang/srt/mem_cache/storage/mooncake_store/mooncake_store.py
@@ -664,8 +664,7 @@ class MooncakeStore(HiCacheStorage, MooncakeBaseStore):
         pool_transfers: Optional[List[PoolTransfer]] = None,
         extra_info: Optional[HiCacheStorageExtraInfo] = None,
     ) -> PoolTransferResult:
-        qkeys = self._tag_keys(keys)
-        kv_pages = self.batch_exists(qkeys, extra_info)
+        kv_pages = self.batch_exists(keys, extra_info)
 
         hit_count: dict = {PoolName.KV: kv_pages} if kv_pages else {}
         final_pages = kv_pages
@@ -674,8 +673,9 @@ class MooncakeStore(HiCacheStorage, MooncakeBaseStore):
             if final_pages == 0:
                 break
             component_keys, key_multiplier = self._get_hybrid_page_component_keys(
-                qkeys, transfer
+                keys, transfer
             )
+            component_keys = self._tag_keys(component_keys)
             ex = self._batch_exist(component_keys)
             if key_multiplier > 0:
                 page_exists = [
