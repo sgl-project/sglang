21d61e153210f4128be39ceaf10b05b8718b7e31
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-05-29 21:39:14 +0300

    Update linear_method_npu.py

--- python/sglang/srt/hardware_backend/npu/quantization/linear_method_npu.py
+++ python/sglang/srt/hardware_backend/npu/quantization/linear_method_npu.py
@@ -117,11 +117,59 @@ class NPU_W4A4DynamicLinearMethod(_NPULinearMethodBase):
         layer.weight.data = layer.weight.data.transpose(0, 1).contiguous()
         layer.weight_scale.data = layer.weight_scale.data.flatten()
         layer.weight_offset.data = layer.weight_offset.data.flatten()
-        layer.weight.data = npu_format_cast(layer.weight.data)
-        layer.weight.data = torch.ops.npu.npu_convert_weight_to_int4pack(
-            layer.weight.data.to(torch.int32)
+        weight.data = self._w4a4_pack_int4(weight.data)
+        weight.data = weight.data.transpose(-2, -1).contiguous()        
+        weight.data = npu_format_cast(weight.data)                                          
+        weight.data = self._pack_to_int32(weight.data)
         )
 
+    def _pack_int4(self, weight) -> torch.Tensor:
+        """
+        Pack int4 weight to int8 weight
+        @param weight: torch.Tensor, int4 weight
+        @return: torch.Tensor, int8 weight
+        """
+        weight = weight.to(torch.int8)
+        e = 0  # number of experts
+        if len(weight.shape) == 2:
+            k, n = weight.shape
+        elif len(weight.shape) == 3:
+            e, k, n = weight.shape
+        n_new = n // 2 + n % 2
+      
+        if n_new != n // 2:
+            raise AssertionError("n dimension should be even")
+      
+        weight = weight.reshape(-1, 2)
+        weight0 = weight[:, :1]
+        weight1 = weight[:, 1:]
+      
+        weight1_4 = torch.bitwise_left_shift(weight1, 4)
+        weight2_4 = weight0 & 0b00001111
+      
+        weight_add = torch.bitwise_or(weight1_4, weight2_4)
+        if e == 0:
+            weight_res = weight_add.reshape(k, n_new)
+        else:
+            weight_res = weight_add.reshape(e, k, n_new)
+        return weight_res
+  
+  
+    def _w4a4_pack_int4(self, save_quant_weight):
+        """
+        Pack int4 weight to int8 weight
+        @param save_quant_weight: torch.Tensor, int4 weight
+        @return: torch.Tensor, int8 weight
+        """
+        weight = save_quant_weight.transpose(-1, -2).contiguous()
+        packed_weight_tensor = self._pack_int4(weight)
+        packed_weight_tensor = packed_weight_tensor.transpose(-1, -2).contiguous()
+        return packed_weight_tensor
+
+    def _pack_to_int32(self, weight: torch.Tensor):
+        # pack 4 int8(int4*2) to int32, because in pytorch, we need to use int32 to represent int4
+        return weight.view(torch.int32).contiguous()
+    
     def apply(
         self,
         layer: torch.nn.Module,
