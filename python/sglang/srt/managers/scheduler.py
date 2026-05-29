3ab3fd60eb68e32c6d61b794697618f636b69985
fzyzcjy <ch271828n@outlook.com>
2026-05-29 09:50:58 +0800

    Apply black reformat

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -2031,9 +2031,7 @@ class Scheduler(
 
     def _prefetch_kvcache(self, req: Req):
         if self.enable_hicache_storage:
-            full_token_ids = req.init_next_round_input(
-                self.tree_cache, cow_mamba=False
-            )
+            full_token_ids = req.init_next_round_input(self.tree_cache, cow_mamba=False)
             last_host_node = req.last_host_node
             if last_host_node.backuped or last_host_node is self.tree_cache.root_node:
                 last_hash = last_host_node.get_last_hash_value()
