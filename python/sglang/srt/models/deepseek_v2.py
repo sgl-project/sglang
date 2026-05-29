d572b1edf15f385708f0cb3d542f6efb9e5a1cf8
Qiaolin-Yu <liin1211@outlook.com>
2026-05-29 10:24:01 +0000

    disable when enabling pcg

--- python/sglang/srt/models/deepseek_v2.py
+++ python/sglang/srt/models/deepseek_v2.py
@@ -723,6 +723,7 @@ class DeepseekV2MoE(nn.Module):
                     ModelOptFp4LinearMethod,
                 )
                 and fc1_n % 128 == 0
+                and get_global_server_args().disable_piecewise_cuda_graph
             ):
                 self.shared_experts.gate_up_proj._interleave_for_swiglu_fusion = True
                 self.shared_experts._enable_nvfp4_gemm_swiglu_fusion = True
