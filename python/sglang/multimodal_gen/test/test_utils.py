e06656f07c582ae3e72c13e124c80f4369f60e2e
Mick <mickjagger19@icloud.com>
2026-06-02 07:19:16 +0800

    [diffusion] Pin refreshed Cosmos3 Nano T2V GT

--- python/sglang/multimodal_gen/test/test_utils.py
+++ python/sglang/multimodal_gen/test/test_utils.py
@@ -33,7 +33,7 @@ if TYPE_CHECKING:
 
 logger = init_logger(__name__)
 
-SGL_TEST_FILES_CI_DATA_REVISION = "697af1290be5105363073cfe25aa3099fc2b42bc"
+SGL_TEST_FILES_CI_DATA_REVISION = "e3d61ba322f5b249c8f78fff2408dbfb70e0d197"
 SGL_TEST_FILES_CONSISTENCY_GT_ROOT = (
     "https://raw.githubusercontent.com/"
     f"sgl-project/ci-data/{SGL_TEST_FILES_CI_DATA_REVISION}/"
