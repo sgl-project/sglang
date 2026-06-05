2b6ef678831735ecf65fac9677df36d005ccc02c
Mick <mickjagger19@icloud.com>
2026-06-05 21:49:51 +0800

    Use shared Ideogram4 CI sampling params

--- python/sglang/multimodal_gen/test/test_utils.py
+++ python/sglang/multimodal_gen/test/test_utils.py
@@ -33,7 +33,7 @@ if TYPE_CHECKING:
 
 logger = init_logger(__name__)
 
-SGL_TEST_FILES_CI_DATA_REVISION = "56c7b2339e94a2263d6609354f5fc82471ae392f"
+SGL_TEST_FILES_CI_DATA_REVISION = "6551865afad2e250f5f4b63469b4b914d2d16860"
 SGL_TEST_FILES_CONSISTENCY_GT_ROOT = (
     "https://raw.githubusercontent.com/"
     f"sgl-project/ci-data/{SGL_TEST_FILES_CI_DATA_REVISION}/"
