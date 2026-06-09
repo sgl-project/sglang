0dbcce2451ab808c5bf37c287a4c976fda87fd58
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-06-09 18:23:18 +0300

    Update torch_npu.py

--- python/sglang/srt/layers/moe/token_dispatcher/torch_npu.py
+++ python/sglang/srt/layers/moe/token_dispatcher/torch_npu.py
@@ -91,7 +91,7 @@ class TorchNpuDispatcher(BaseDispatcher):
             # Prefill
             self.init_routing_prefill = NPUMoEInitRouting_Quant()
             self.finalize_routing_prefill = NPUMoETokenUnpermute()
-            self.group_list_type_decode = 0
+            self.group_list_type_decode = 1
             # Decode
             self.init_routing_decode = NPUMoEInitRouting_v2()
             self.finalize_routing_decode = NPUFinalizeRouting(drop_pad_mode=2)
