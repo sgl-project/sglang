4177cb8325b5696c2a32b853c45aa59f40c11b07
15050188022 <wanghao129@baidu.com>
2026-06-09 20:09:40 +0800

    Fix OOT SWA allocator factory selection

--- python/sglang/srt/mem_cache/allocator/swa.py
+++ python/sglang/srt/mem_cache/allocator/swa.py
@@ -4,6 +4,7 @@ from sglang.srt.mem_cache.allocator.base import BaseTokenToKVPoolAllocator
 from sglang.srt.mem_cache.allocator.paged import PagedTokenToKVPoolAllocator
 from sglang.srt.mem_cache.allocator.token import TokenToKVPoolAllocator
 from sglang.srt.mem_cache.base_swa_memory_pool import BaseSWAKVPool
+from sglang.srt.platforms import current_platform
 from sglang.srt.utils import is_npu
 from sglang.srt.utils.common import get_num_new_pages
 
@@ -56,6 +57,10 @@ class SWATokenToKVPoolAllocator(BaseTokenToKVPoolAllocator):
         else:
             if _is_npu:
                 PagedTokenToKVPoolAllocatorClass = NPUPagedTokenToKVPoolAllocator
+            elif current_platform.is_out_of_tree():
+                PagedTokenToKVPoolAllocatorClass = (
+                    current_platform.get_paged_allocator_cls()
+                )
             else:
                 PagedTokenToKVPoolAllocatorClass = PagedTokenToKVPoolAllocator
             self.full_attn_allocator = PagedTokenToKVPoolAllocatorClass(
