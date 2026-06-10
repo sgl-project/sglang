7004dbf782f465cb7856869b35cf483189836562
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-06-10 15:07:50 +0300

    Update torch_npu.py

--- python/sglang/srt/layers/moe/token_dispatcher/torch_npu.py
+++ python/sglang/srt/layers/moe/token_dispatcher/torch_npu.py
@@ -84,18 +84,18 @@ class TorchNpuDispatcher(BaseDispatcher):
         if self.ascend_dispatcher_output_dtype == DispatcherOutputDtype.BF16:
             # Prefill
             self.init_routing_prefill = NPUMoEInitRouting_v2(quant_mode=-1)
-            self.finalize_routing_prefill = NPUFinalizeRouting(drop_pad_mode=0)
+            self.finalize_routing_prefill = NPUFinalizeRouting(drop_pad_mode=2)
             self.group_list_type_prefill = 1
             # Decode
             self.init_routing_decode = NPUMoEInitRouting_v2(quant_mode=-1)
-            self.finalize_routing_decode = NPUFinalizeRouting(drop_pad_mode=0)
+            self.finalize_routing_decode = NPUFinalizeRouting(drop_pad_mode=2)
             self.group_list_type_decode = 1
 
         elif self.ascend_dispatcher_output_dtype == DispatcherOutputDtype.INT8:
             # Prefill
-            self.init_routing_prefill = NPUMoEInitRouting_v2(quant_mode=1)
-            self.finalize_routing_prefill = NPUMoETokenUnpermute()
-            self.group_list_type_prefill = 1
+            self.init_routing_prefill = NPUMoEInitRouting_v1()
+            self.finalize_routing_prefill = NPUFinalizeRouting(drop_pad_mode=2)
+            self.group_list_type_prefill = 0
             # Decode
             self.init_routing_decode = NPUMoEInitRouting_v2(quant_mode=1)
             self.finalize_routing_decode = NPUMoETokenUnpermute()
