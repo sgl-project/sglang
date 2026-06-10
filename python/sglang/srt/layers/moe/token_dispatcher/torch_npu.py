4e24947a213664050a823005656c5e17ff394bd2
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-06-10 13:04:24 +0300

    Update torch_npu.py

--- python/sglang/srt/layers/moe/token_dispatcher/torch_npu.py
+++ python/sglang/srt/layers/moe/token_dispatcher/torch_npu.py
@@ -94,8 +94,8 @@ class TorchNpuDispatcher(BaseDispatcher):
             self.group_list_type_prefill = 1
             # Decode
             self.init_routing_decode = NPUMoEInitRouting_Quant() #NPUMoEInitRouting_v2(quant_mode=1)
-            self.finalize_routing_decode = NPUFinalizeRouting(drop_pad_mode=0)
-            self.group_list_type_decode = 1
+            self.finalize_routing_decode = NPUFinalizeRouting(drop_pad_mode=2)
+            self.group_list_type_decode = 0
 
         else:
             raise ValueError(
