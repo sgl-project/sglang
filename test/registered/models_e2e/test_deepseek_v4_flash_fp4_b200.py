d6e680521bff23abad2b2f07cd2c5da8e49124f3
Baizhou Zhang <sobereddiezhang@gmail.com>
2026-06-12 17:47:26 -0700

    Update test/registered/models_e2e/test_deepseek_v4_flash_fp4_b200.py

--- test/registered/models_e2e/test_deepseek_v4_flash_fp4_b200.py
+++ test/registered/models_e2e/test_deepseek_v4_flash_fp4_b200.py
@@ -87,7 +87,7 @@ class TestDSV4FlashFP4B200Balanced(
     """Balanced recipe: TP=4, DP=4, DeepEP, EAGLE (1-step spec)."""
 
     gsm8k_accuracy_thres = 0.93
-    accept_length_thres = 1.6
+    accept_length_thres = 1.8
     bs_1_speed_thres = 100
 
     @classmethod
