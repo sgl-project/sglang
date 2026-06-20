2392b74db7f08cca00fe6b82c284bb5dbd928729
BBuf <1182563586@qq.com>
2026-06-20 11:47:04 +0800

    [diffusion] Remove config backend unit tests

--- python/sglang/multimodal_gen/test/unit/test_sana_pipeline_config.py
+++
@@ -1,5 +0,0 @@
-from sglang.multimodal_gen.configs.pipeline_configs.sana import SanaPipelineConfig
-
-
-def test_sana_vae_precision_defaults_to_fp32():
-    assert SanaPipelineConfig().vae_precision == "fp32"
