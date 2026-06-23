f0ee89b3a6c3f41886d4ea2ff9888a1e418302d9
fzyzcjy <ch271828n@outlook.com>
2026-06-23 14:23:10 +0800

    wu-move-from-distributed-move: Move update_weights_from_distributed onto WeightUpdater (cut+paste)
    Refactor chain ID: wu-move-from-distributed-move

--- python/sglang/srt/managers/tp_worker.py
+++ python/sglang/srt/managers/tp_worker.py
@@ -159,15 +159,14 @@ class BaseTpWorker(ABC):
     def update_weights_from_distributed(
         self, recv_req: UpdateWeightsFromDistributedReqInput
     ):
-        from sglang.srt.model_executor.model_runner import ModelRunner
-
-        success, message = ModelRunner.update_weights_from_distributed(
-            self.model_runner.weight_updater,
-            recv_req.names,
-            recv_req.dtypes,
-            recv_req.shapes,
-            recv_req.group_name,
-            recv_req.load_format,
+        success, message = (
+            self.model_runner.weight_updater.update_weights_from_distributed(
+                recv_req.names,
+                recv_req.dtypes,
+                recv_req.shapes,
+                recv_req.group_name,
+                recv_req.load_format,
+            )
         )
         return success, message
 
