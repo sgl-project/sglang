6f2ba36e4b581dbdbf85e63d81ccc4f8151fbee7
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-06-29 14:56:25 +0300

    Update and rename test_npu_qwen3.5_35b_fuseep.py to test_npu_qwen3_30b_fuseep.py

--- test/registered/ascend/llm_models/test_npu_qwen3_30b_fuseep.py
+++ test/registered/ascend/llm_models/test_npu_qwen3_30b_fuseep.py
@@ -1,21 +1,21 @@
 import unittest
 
 from sglang.test.ascend.gsm8k_ascend_mixin import GSM8KAscendMixin
-from sglang.test.ascend.test_ascend_utils import QWEN3_5_35B_W8A8_MTP_MODELSLIM_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import QWEN3_30B_A3B_W8A8_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import CustomTestCase
 
 register_npu_ci(est_time=400, suite="per-commit-4-npu-a3")
 
 
-class TestQwen3535Bw8a8FuseEP(GSM8KAscendMixin, CustomTestCase):
-    """Testcase: Verify that the inference accuracy of the Eco-Tech/Qwen3.5-35B-A3B-w8a8-mtp model on the GSM8K dataset is no less than 0.85.
+class TestQwen330Bw8a8FuseEP(GSM8KAscendMixin, CustomTestCase):
+    """Testcase: Verify that the inference accuracy of the Qwen/Qwen3-30B-A3B-w8a8 model on the GSM8K dataset is no less than 0.85.
 
     [Test Category] Model
-    [Test Target] Eco-Tech/Qwen3.5-35B-A3B-w8a8-mtp
+    [Test Target] Qwen/Qwen3-30B-A3B-w8a8
     """
 
-    model = QWEN3_5_35B_W8A8_MTP_MODELSLIM_WEIGHTS_PATH
+    model = QWEN3_30B_A3B_W8A8_WEIGHTS_PATH
     accuracy = 0.85
     other_args = [
         "--trust-remote-code",
