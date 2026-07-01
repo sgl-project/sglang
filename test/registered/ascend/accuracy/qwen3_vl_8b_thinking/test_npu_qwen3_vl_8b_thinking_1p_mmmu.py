a9996e7d5ef46c310d9d7e6b1db1f0016ba07bbd
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-07-01 15:55:31 +0800

    Update test_npu_qwen3_vl_8b_thinking_1p_mmmu.py

--- test/registered/ascend/accuracy/qwen3_vl_8b_thinking/test_npu_qwen3_vl_8b_thinking_1p_mmmu.py
+++ test/registered/ascend/accuracy/qwen3_vl_8b_thinking/test_npu_qwen3_vl_8b_thinking_1p_mmmu.py
@@ -1,7 +1,7 @@
 import unittest
 
 from sglang.test.ascend.e2e.test_npu_accuracy_utils import (
-    TestNpuAccuracyTestCaseBase,
+    TestAscendAccuracyTestCaseBase,
 )
 from sglang.test.ascend.e2e.test_npu_performance_utils import (
     QWEN3_VL_8B_THINKING_MODEL_PATH,
@@ -21,22 +21,17 @@ ENVS = {
     "HCCL_SOCKET_IFNAME": "lo",
     "GLOO_SOCKET_IFNAME": "lo",
     "HCCL_OP_EXPANSION_MODE": "AIV",
-    "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
-    "SGLANG_ENABLE_SPEC_V2": "1",
+    "HCCL_BUFFSIZE": "2000",
 }
 
 OTHER_ARGS = [
     "--trust-remote-code",
-    "--nnodes",
-    "1",
-    "--node-rank",
-    "0",
     "--attention-backend",
     "ascend",
     "--device",
     "npu",
     "--max-running-requests",
-    16,
+    64,
     "--max-prefill-tokens",
     16384,
     "--disable-radix-cache",
@@ -45,42 +40,30 @@ OTHER_ARGS = [
     "--tp-size",
     2,
     "--mem-fraction-static",
-    0.894,
-    "--cuda-graph-bs",
-    1,
-    5,
-    15,
-    16,
+    0.8,
     "--dtype",
     "bfloat16",
-    # "--speculative-draft-model-quantization",
-    # "unquant",
-    # "--speculative-algorithm",
-    # "EAGLE3",
-    # "--speculative-draft-model-path",
-    # QWEN3_8B_EAGLE_MODEL_PATH,
-    # "--speculative-num-steps",
-    # 4,
-    # "--speculative-eagle-topk",
-    # 1,
-    # "--speculative-num-draft-tokens",
-    # 5,
     "--reasoning-parser",
     "qwen3-thinking",
     "--tool-call-parser",
     "qwen",
+    "--enable-multimodal",
+    "--mm-attention-backend",
+    "ascend_attn",
+    "--base-gpu-id",
+    4,
 ]
 
 
-class TestQwen3(TestNpuAccuracyTestCaseBase):
-    model = QWEN3_VL_8B_THINKING_MODEL_PATH
+class TestQwen3(TestAscendAccuracyTestCaseBase):
+    model = "/home/weights/Qwen3-VL-8B-Thinking"
     envs = ENVS
     other_args = OTHER_ARGS
     accuracy = 0.741
     datasets = ["mmmu"]
     few_shot_num = 0
-    generation_config = {"max_tokens": 65536, "temperature": 1.0}
-    eval_batch_size = 16
+    generation_config = {"max_tokens": 65536}
+    eval_batch_size = 64
 
     def test_mmmu(self):
         self.run_accuracy()
