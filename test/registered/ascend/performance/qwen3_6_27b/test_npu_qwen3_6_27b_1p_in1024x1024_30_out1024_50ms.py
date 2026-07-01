597ffc83d0c0b3b17659eef1547ba64b60f65689
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-07-01 16:07:55 +0800

    Update test_npu_qwen3_6_27b_1p_in1024x1024_30_out1024_50ms.py

--- test/registered/ascend/performance/qwen3_6_27b/test_npu_qwen3_6_27b_1p_in1024x1024_30_out1024_50ms.py
+++ test/registered/ascend/performance/qwen3_6_27b/test_npu_qwen3_6_27b_1p_in1024x1024_30_out1024_50ms.py
@@ -95,7 +95,8 @@ class TestNPUQwen3_6_27B_1P_In1024x1024_30_Out1024_50ms(TestNpuPerformanceTestCa
     model = QWEN3_6_27B_MODEL_PATH
     other_args = QWEN3_6_27B_1024_OTHER_ARGS
     envs = QWEN3_6_27B_1024_ENVS
-    dataset_name = "random"
+    backend = "sglang-oai-chat"
+    dataset_name = "image"
     max_concurrency = 48
     num_prompts = 48
     input_len = 30
