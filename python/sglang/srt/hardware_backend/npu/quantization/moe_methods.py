6deec083673de5919f88d492c6f14f21d3b29381
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-07-02 15:48:25 +0300

    Update moe_methods.py

--- python/sglang/srt/hardware_backend/npu/quantization/moe_methods.py
+++ python/sglang/srt/hardware_backend/npu/quantization/moe_methods.py
@@ -1071,8 +1071,6 @@ class NPUUnquantMoEMethod(_NPUMoEMethodBase):
         if self.hidden_states_quantizer is not None and pertoken_scale is None:
             hidden_states, pertoken_scale = self.hidden_states_quantizer(hidden_states)
 
-        print(pertoken_scale.dtype)
-
         scale_args = {
             "scale": [weight_scale],
             "per_token_scale": [pertoken_scale],
