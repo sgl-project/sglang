04ffc94bb53c55ec7cf280b617ee9a0269e8a767
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-07-04 16:21:01 +0800

    Update test_npu_qwen3_6_35b_a3b_1p_in64k_out1k_50ms.py

--- test/registered/ascend/performance/qwen3_6_35b_a3b/test_npu_qwen3_6_35b_a3b_1p_in64k_out1k_50ms.py
+++ test/registered/ascend/performance/qwen3_6_35b_a3b/test_npu_qwen3_6_35b_a3b_1p_in64k_out1k_50ms.py
@@ -40,7 +40,7 @@ QWEN3_6_35B_A3B_64K_1K_OTHER_ARGS = [
     "--chunked-prefill-size",
     -1,
     "--max-total-tokens",
-    470784,
+    600000,
     "--max-prefill-tokens",
     65536,
     "--disable-radix-cache",
@@ -85,7 +85,7 @@ class TestNPUQwen3_6_35BA3B_1P_In64k_Out1k_50ms(TestNpuPerformanceTestCaseBase):
     """Test NPU performance for Qwen3.6-35B-A3B 1p in64k out1k 50ms"""
 
     benchmark_tool = BENCHMARK_TOOL_DEFAULT
-    dataset_type = AISBENCHMARK_DATASET_DEFAULT
+    aisbench_dataset_type = AISBENCHMARK_DATASET_DEFAULT
     model = QWEN3_6_35B_A3B_MODEL_PATH
     other_args = QWEN3_6_35B_A3B_64K_1K_OTHER_ARGS
     envs = QWEN3_6_35B_A3B_64K_1K_ENVS
