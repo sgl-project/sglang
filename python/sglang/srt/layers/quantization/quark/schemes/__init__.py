2039fc0eba04480b988f0679ea0b9c706e0ea614
Colin Zeng <Colin.Zeng@amd.com>
2026-07-10 09:49:27 +0000

    restructure

--- python/sglang/srt/layers/quantization/quark/schemes/__init__.py
+++ python/sglang/srt/layers/quantization/quark/schemes/__init__.py
@@ -4,8 +4,7 @@ from .quark_scheme import QuarkLinearScheme, QuarkMoEScheme
 from .quark_w4a4_mxfp4 import QuarkW4A4MXFP4
 from .quark_w4a4_mxfp4_moe import QuarkW4A4MXFp4MoE
 from .quark_w4a8_mxfp4_moe import QuarkW4A8MXFp4MoE
-from .quark_w8a8_fp8 import QuarkW8A8Fp8
-from .quark_w8a8_fp8_block import QuarkW8A8Fp8Block
+from .quark_w8a8_fp8 import QuarkW8A8Fp8, QuarkW8A8Fp8Block
 from .quark_w8a8_fp8_moe import QuarkW8A8FP8MoE
 
 __all__ = [
