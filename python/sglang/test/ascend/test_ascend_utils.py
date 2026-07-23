9b6e50b6eb60d6e070166ec363d4ee3dd3f201d1
l30056750 <1964551841@qq.com>
2026-07-23 19:42:45 +0800

    GPU case transplantation

--- python/sglang/test/ascend/test_ascend_utils.py
+++ python/sglang/test/ascend/test_ascend_utils.py
@@ -108,6 +108,12 @@ KIMI_K2_5_EAGLE3_MODEL_PATH = os.path.join(
 )
 LING_LITE_WEIGHTS_PATH = os.path.join(MODEL_WEIGHTS_DIR, "inclusionAI/Ling-lite")
 LLAMA_2_7B_WEIGHTS_PATH = os.path.join(MODEL_WEIGHTS_DIR, "LLM-Research/Llama-2-7B")
+LLAMA_2_7B_HF_WEIGHTS_PATH = os.path.join(
+    MODEL_WEIGHTS_DIR, "LLM-Research/Llama-2-7b-hf"
+)
+LLAMA_2_7B_LORA_TRIVIA_QA_WEIGHTS_PATH = os.path.join(
+    MODEL_WEIGHTS_DIR, "LLM-Research/Llama_2_7b_lora-trivia_qa"
+)
 LLAMA_3_1_8B_INSTRUCT_WEIGHTS_PATH = os.path.join(
     MODEL_WEIGHTS_DIR, "AI-ModelScope/Llama-3.1-8B-Instruct"
 )
--- test/registered/ascend/basic_function/mambacache/test_npu_qwen3_next_models_mtp.py
+++ test/registered/ascend/basic_function/mambacache/test_npu_qwen3_next_models_mtp.py
@@ -5,7 +5,6 @@ from sglang.test.ascend.test_ascend_utils import (
 )
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.kits.eval_accuracy_kit import GSM8KMixin
-from sglang.test.kits.kl_divergence_kit import KLDivergenceMixin
 from sglang.test.kits.prefix_cache_branching_kit import PrefixCacheBranchingMixin
 from sglang.test.server_fixtures.default_fixture import (
     DefaultServerBase,
@@ -18,9 +17,7 @@ register_npu_ci(est_time=600, suite="full-8-npu-a3", nightly=True)
 QWEN3_NEXT_MODEL = QWEN3_NEXT_80B_A3B_INSTRUCT_WEIGHTS_FOR_TEST.model_path
 
 
-class TestQwen3NextMTPTopk(
-    GSM8KMixin, KLDivergenceMixin, PrefixCacheBranchingMixin, DefaultServerBase
-):
+class TestQwen3NextMTPTopk(GSM8KMixin, PrefixCacheBranchingMixin, DefaultServerBase):
     model = QWEN3_NEXT_MODEL
     cache_chunk_size = 64
     gsm8k_accuracy_thres = 0.93
@@ -53,7 +50,7 @@ class TestQwen3NextMTPTopk(
     ]
 
 
-class TestQwen3NextMTPV2(GSM8KMixin, KLDivergenceMixin, DefaultServerBase):
+class TestQwen3NextMTPV2(GSM8KMixin, DefaultServerBase):
     model = QWEN3_NEXT_MODEL
     gsm8k_accuracy_thres = 0.93
     kl_div_thres = 0.0035
