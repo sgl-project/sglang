133f0ef4fb57c8f57f934f444e6ffdd69b2e4531
Rain Jiang <rain-jiang@outlook.com>
2026-07-24 15:23:50 -0700

    remove the duplicate mods

--- rust/sglang-server/src/lib.rs
+++ rust/sglang-server/src/lib.rs
@@ -24,16 +24,6 @@ mod tokenizer_manager;
 
 use std::net::SocketAddr;
 
-mod environ;
-mod error;
-mod fsm;
-mod ids;
-mod message;
-mod ring;
-mod runtime;
-mod tokenizer_manager;
-mod utils;
-
 use pyo3::prelude::*;
 use pyo3::pybacked::PyBackedBytes;
 use pyo3::types::PyBytes;
