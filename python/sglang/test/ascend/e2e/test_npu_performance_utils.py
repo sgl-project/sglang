a3382d54eb19ad0bd09007edc325b4b931662ca6
Sugar920 <121632458+Sugar920@users.noreply.github.com>
2026-07-28 20:25:42 +0800

    Update GLM5 model path in test_npu_performance_utils.py

--- python/sglang/test/ascend/e2e/test_npu_performance_utils.py
+++ python/sglang/test/ascend/e2e/test_npu_performance_utils.py
@@ -155,7 +155,7 @@ MOONLIGHT_16B_A3B_MODEL_PATH = (
     "/root/.cache/modelscope/hub/models/moonshotai/Moonlight-16B-A3B-Instruct"
 )
 GLM5_TOP64_PRUNED_GSM8K_MODEL_PATH = (
-    "/root/.cache/modelscope/hub/models/yzgjhdxf_bak/GLM-5-top64-pruned-gsm8k"
+    "/root/.cache/modelscope/hub/models/yzgjhdxf/GLM-5-top64-pruned-gsm8k"
 )
 GLM_5_1_W4A8_MODEL_PATH = "/root/.cache/modelscope/hub/models/Eco-Tech/GLM-5.1-w4a8"
 MINIMAX_M2_5_W8A8_MODEL_PATH = (
