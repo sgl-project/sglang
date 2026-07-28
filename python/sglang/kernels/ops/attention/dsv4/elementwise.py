94e1ebb74e76bb7d859f67c7ce294994d12c70d2
MeowMiaoJ <miaomiao.jiang@intel.com>
2026-07-28 09:25:53 +0000

    [UPDATE] keep the origin name of two kernels and import from beginning

--- python/sglang/kernels/ops/attention/dsv4/elementwise.py
+++ python/sglang/kernels/ops/attention/dsv4/elementwise.py
@@ -150,9 +150,7 @@ def fused_q_norm_rope(
 ) -> None:
     freqs_real = torch.view_as_real(freqs_cis).flatten(-2)
     if _is_xpu:
-        from sgl_kernel import fused_q_norm_rope as xpu_fused_q_norm_rope
-
-        xpu_fused_q_norm_rope(q_input, q_output, freqs_real, positions, eps)
+        sgl_kernel.fused_q_norm_rope(q_input, q_output, freqs_real, positions, eps)
         return
     head_dim = q_input.shape[-1]
     rope_dim = freqs_real.shape[-1]
@@ -281,11 +279,7 @@ def fused_k_norm_rope_flashmla(
 ) -> None:
     freqs_real = torch.view_as_real(freqs_cis).flatten(-2)
     if _is_xpu:
-        from sgl_kernel import (
-            fused_k_norm_rope_flashmla as xpu_fused_k_norm_rope_flashmla,
-        )
-
-        xpu_fused_k_norm_rope_flashmla(
+        sgl_kernel.fused_k_norm_rope_flashmla(
             kv, kv_weight, freqs_real, positions, out_loc, kvcache, eps, page_size
         )
         return
