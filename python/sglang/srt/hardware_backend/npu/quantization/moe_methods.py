336fe32402f043b1a3a4ffbf0ab8408e259d6dac
LinyuanLi0046 <liliny163@163.com>
2026-07-29 20:06:46 +0800

    clean

--- python/sglang/srt/hardware_backend/npu/quantization/moe_methods.py
+++ python/sglang/srt/hardware_backend/npu/quantization/moe_methods.py
@@ -215,17 +215,12 @@ class NPUW4A8MXFP4MoEMethod(_NPUMoEMethodBase):
         ).transpose(-1, -2)
 
         weight_scale = getattr(layer, f"{weight_prefix}_weight_scale")
-        scale = weight_scale.data.transpose(1, 2)
-        scale = (
-            scale.transpose(-1, -2)
-            .reshape(
-                scale.shape[0],
-                scale.shape[2],
-                scale.shape[1] // 2,
-                2,
-            )
-            .transpose(1, 2)
-        )
+        scale = weight_scale.data.reshape(
+            weight_scale.shape[0],
+            weight_scale.shape[1],
+            weight_scale.shape[2] // 2,
+            2,
+        ).transpose(1, 2)
         weight_scale.data = scale
 
         # The refactored Ascend dispatchers currently support BF16 and INT8.
