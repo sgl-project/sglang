514965f73ed9d7976e9ae248a83f9f60a40b689f
hzh0425 <hzh0425@apache.org>
2026-08-04 19:23:29 +0800

    Fix

--- python/sglang/srt/models/deepseek_v4_nextn.py
+++ python/sglang/srt/models/deepseek_v4_nextn.py
@@ -245,7 +245,7 @@ class DeepseekV4ForCausalLMNextN(DeepseekV4ForCausalLM):
             config.hidden_size,
             quant_config=quant_config,
             prefix=add_prefix("model.shared_head.head", prefix),
-            use_attn_tp_group=get_server_args().enable_dp_lm_head,
+            use_attn_tp_group=get_parallel().enable_dp_lm_head,
         )
         self.logits_processor = LogitsProcessor(config)
 
