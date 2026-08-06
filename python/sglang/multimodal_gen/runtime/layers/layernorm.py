db62121589f1abed4188cff54bede324cdcb257b
Seraphim Volochaev <116020688+Svoloch2940194@users.noreply.github.com>
2026-08-06 04:28:40 -0700

    fix logs

--- python/sglang/multimodal_gen/runtime/layers/layernorm.py
+++ python/sglang/multimodal_gen/runtime/layers/layernorm.py
@@ -451,14 +451,15 @@ class FP32LayerNorm(CustomOp, nn.LayerNorm):
         try:
             import attentions  # noqa: F401
         except ImportError:
-            from sglang.multimodal_gen.runtime.utils.logging_utils import init_logger
-
-            logger = init_logger(__name__)  # pylint: disable=invalid-name
-            logger.warning(
-                "The 'attentions' library is not installed. Falling back to native layernorm. "
-                "Installing this library may improve performance on NPU."
-                "See: sgl-project/sgl-kernel-npu"
-            )
+            if current_patform.is_npu:
+                from sglang.multimodal_gen.runtime.utils.logging_utils import init_logger
+    
+                logger = init_logger(__name__)  # pylint: disable=invalid-name
+                logger.warning(
+                    "The 'attentions' library is not installed. Falling back to native layernorm. "
+                    "Installing this library may improve performance on NPU."
+                    "See: sgl-project/sgl-kernel-npu"
+                )
             self._forward_method = self.forward_native
 
     def _cached_fp32_param(
