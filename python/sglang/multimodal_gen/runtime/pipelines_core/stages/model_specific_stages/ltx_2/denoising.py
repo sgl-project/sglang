94c0c7d3859b83ed3c8725c204a261c8343c4861
Elizaveta Martirosian <elizaveta.martirosian@gmail.com>
2026-08-06 22:14:58 +0300

    replace float64 on float32

--- python/sglang/multimodal_gen/runtime/pipelines_core/stages/model_specific_stages/ltx_2/denoising.py
+++ python/sglang/multimodal_gen/runtime/pipelines_core/stages/model_specific_stages/ltx_2/denoising.py
@@ -24,6 +24,7 @@ from sglang.multimodal_gen.runtime.distributed.parallel_state import (
     get_classifier_free_guidance_world_size,
 )
 from sglang.multimodal_gen.runtime.managers.forward_context import set_forward_context
+
 from sglang.multimodal_gen.runtime.pipelines_core.diffusion_scheduler_utils import (
     clone_scheduler_runtime,
 )
@@ -39,10 +40,15 @@ from sglang.multimodal_gen.runtime.pipelines_core.stages.denoising import (
 from sglang.multimodal_gen.runtime.pipelines_core.stages.validators import (
     StageValidators as V,
 )
+from sglang.multimodal_gen.runtime.platforms import (
+    current_platform,
+)
 from sglang.multimodal_gen.runtime.server_args import (
     ServerArgs,
     is_ltx2_two_stage_pipeline_name,
 )
+
+_is_npu = current_platform.is_npu()
 
 LTX23_RES2S_STEP_NOISE_SEED = -1
 LTX23_RES2S_SUBSTEP_NOISE_SEED = 9999
@@ -521,7 +527,7 @@ class LTX2DenoisingStage(DenoisingStage):
         noise = torch.randn(
             reference_tensor.shape,
             generator=generator,
-            dtype=torch.float64,
+            dtype= torch.float32 if _is_npu else torch.float64,
             device=reference_tensor.device,
         )
         noise = (noise - noise.mean()) / noise.std()
