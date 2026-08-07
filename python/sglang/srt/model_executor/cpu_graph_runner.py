7e68a051c28258dd95d176e7696c7f8c17cf7a5b
xinguozhu-2026 <xinguo.zhu@intel.com>
2026-08-06 23:04:42 -0700

    kernel(front): add front code for fp8_per_tensor_gemm

--- python/sglang/srt/model_executor/cpu_graph_runner.py
+++ python/sglang/srt/model_executor/cpu_graph_runner.py
@@ -448,6 +448,19 @@ def register_fake_ops(tp_size: int):
         N = mat2.shape[0]
         return mat1.new_empty(M, N, dtype=out_dtype)
 
+    @register_cpu_compile_fake("fp8_per_tensor_scaled_mm_cpu")
+    def _(
+        mat1,
+        mat2,
+        scales2,
+        bias,
+        out_dtype,
+        is_vnni,
+    ):
+        M = mat1.shape[0]
+        N = mat2.shape[0]
+        return mat1.new_empty(M, N, dtype=out_dtype)
+
     @register_cpu_compile_fake("mxfp4_scaled_mm_cpu")
     def _(mat1, mat2, scales2, bias, is_vnni):
         sizes = list(mat1.shape)
