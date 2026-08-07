c463d6c42089f9e60f4556001894a57f15662bc7
hnyls2002 <lsyincs@gmail.com>
2026-08-06 21:21:26 -0700

    add beam_group to req fakes in unit tests

--- test/registered/unit/managers/test_batch_result_processor_hidden_states.py
+++ test/registered/unit/managers/test_batch_result_processor_hidden_states.py
@@ -41,6 +41,7 @@ def _make_processor(server_mode: str = "full") -> SchedulerBatchResultProcessor:
         model_worker=Mock(),
         logprob_result_processor=None,
         output_streamer=Mock(),
+        beam_coordinator=Mock(),
         abort_request=lambda *args, **kwargs: None,
     )
 
@@ -59,6 +60,7 @@ class _PrefillReq:
         self.grammar = None
         self.require_reasoning = False
         self.customized_info = None
+        self.beam_group = None
 
     def finished(self):
         return False
@@ -77,6 +79,7 @@ class _DecodeReq:
         self.return_logprob = False
         self.return_sampling_mask = False
         self.grammar = None
+        self.beam_group = None
         self.time_stats = Mock()
 
     def finished(self):
