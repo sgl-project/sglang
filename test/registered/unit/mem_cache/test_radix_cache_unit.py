12e354ffc5f42a1e3e1cfc3328251357b68ff1f1
jthomson04 <jwillthomson19@gmail.com>
2026-08-07 15:20:03 -0700

    test: set cache salt on radix request fixture
    Signed-off-by: jthomson04 <jwillthomson19@gmail.com>

--- test/registered/unit/mem_cache/test_radix_cache_unit.py
+++ test/registered/unit/mem_cache/test_radix_cache_unit.py
@@ -434,6 +434,7 @@ class TestRadixCache(unittest.TestCase):
             req_pool_idx=0,
             cache_protected_len=0,
             extra_key=None,
+            cache_salt=None,
             priority=0,
             last_node=cache.root_node,
         )
