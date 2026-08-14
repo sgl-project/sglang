18553c0bc9c04e79a452c0a98453698e70303775
yuchengliu1 <yucheng.liu@intel.com>
2026-08-14 19:42:18 +0800

    Revert "disable deepseek_ocr UT"

--- test/registered/xpu/test_deepseek_ocr.py
+++ test/registered/xpu/test_deepseek_ocr.py
@@ -23,11 +23,7 @@ from sglang.test.test_utils import (
     popen_launch_server,
 )
 
-register_xpu_ci(
-    est_time=360,
-    suite="stage-b-test-1-gpu-xpu",
-    disabled="Temporarily disabled. The failure found in PR31751 and may introduced by other PR.",
-)
+register_xpu_ci(est_time=360, suite="stage-b-test-1-gpu-xpu")
 
 
 class TestDeepSeekOCR(CustomTestCase):
