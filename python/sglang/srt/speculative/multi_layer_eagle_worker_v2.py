5e73c89b34d8e4ae7422b2a3cce2a85508cf1101
Liangsheng Yin <hnyls2002@gmail.com>
2026-08-16 16:01:26 -0700

    [Spec] Simplify compute_spec_v2_logprobs signature and skip identity gathers (#35058)

--- python/sglang/srt/speculative/multi_layer_eagle_worker_v2.py
+++ python/sglang/srt/speculative/multi_layer_eagle_worker_v2.py
@@ -1038,7 +1038,6 @@ class MultiLayerEagleWorkerV2(BaseSpecWorker):
             plan_stream=self.plan_stream,
             plan_stream_ctx=self.plan_stream_ctx,
             topk=self.topk,
-            num_steps=self.speculative_num_steps,
             num_draft_tokens=self.speculative_num_draft_tokens,
             device=self.device,
             metadata_ready_pre_pad=False,
