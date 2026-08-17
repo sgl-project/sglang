c365cf2ef7a8c87114e3c34fbe24125b530b695c
gjsheu <gjsheu@163.com>
2026-08-17 13:28:36 +0000

    feat(npu): add Ascend MemCache HiCache backend

--- python/sglang/srt/hardware_backend/npu/utils.py
+++ python/sglang/srt/hardware_backend/npu/utils.py
@@ -77,9 +77,16 @@ def set_default_server_args(args: "ServerArgs"):
     # NPU does not support CustomAllReduce
     args.disable_custom_all_reduce = True
 
-    # handles hierarchical cache configs
-    if args.enable_hierarchical_cache:
-        args.hicache_io_backend = "kernel_ascend"
+    # # handles hierarchical cache configs
+
+    if (
+        args.enable_hierarchical_cache
+        or args.disaggregation_decode_enable_offload_kvcache
+    ):
+        # Respect user override; only patch default values
+        if args.hicache_io_backend == "kernel":
+            args.hicache_io_backend = "kernel_ascend"
+
         if args.use_mla_backend():
             args.hicache_mem_layout = "page_first_kv_split"
         else:
