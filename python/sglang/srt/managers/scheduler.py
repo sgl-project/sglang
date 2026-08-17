1e533212dd93201b0015df4c9771c74dc507e607
hanwlax <hanbo39@huawei.com>
2026-08-17 09:58:45 +0800

    test script

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -1786,6 +1786,43 @@ class Scheduler(
             Tuple[ScheduleBatch, Union[GenerationBatchResult, EmbeddingBatchResult]]
         ] = deque()
 
+        enable_profiling = os.getenv("SGLANG_NPU_PROFILING",
+                                     "0") == "1" and torch.distributed.get_rank() == 0  # envs.SGLANG_NPU_PROFILING.get()
+        prof_bs = int(os.getenv("SGLANG_NPU_PROFILING_BS", 1))  # envs.SGLANG_NPU_PROFILING_BS.get()
+        prof_step = int(os.getenv("SGLANG_NPU_PROFILING_STEP", 10))  # envs.SGLANG_NPU_PROFILING_STEP.get()
+        profiling_stage: str = os.getenv("SGLANG_NPU_PROFILING_STAGE",
+                                         "decode")  # envs.SGLANG_NPU_PROFILING_STAGE.get()
+        profiling_path: str = os.getenv("SGLANG_NPU_PROFILING_PATH", "profiling")  # envs.SGLANG_NPU_PROFILING_PATH.get()
+        
+        if enable_profiling:
+            prof_cnt = 0
+            import torch_npu
+
+            experimental_config = torch_npu.profiler._ExperimentalConfig(
+                aic_metrics=torch_npu.profiler.AiCMetrics.PipeUtilization,
+                profiler_level=torch_npu.profiler.ProfilerLevel.Level2,
+                l2_cache=False,
+                data_simplification=False,
+            )
+            prof = torch_npu.profiler.profile(
+                activities=[
+                    torch_npu.profiler.ProfilerActivity.CPU,
+                    torch_npu.profiler.ProfilerActivity.NPU,
+                ],
+                on_trace_ready=torch_npu.profiler.tensorboard_trace_handler(
+                    profiling_path
+                ),
+                schedule=torch_npu.profiler.schedule(
+                    wait=1, warmup=1, active=10, repeat=1, skip_first=1
+                ),
+                record_shapes=True,
+                profile_memory=True,
+                with_stack=False,
+                with_flops=False,
+                with_modules=False,
+                experimental_config=experimental_config,
+            )
+
         def pop_and_process():
             # Process the results of the last batch
             tmp_batch, tmp_result = self.result_queue.popleft()
@@ -1827,10 +1864,34 @@ class Scheduler(
 
             # Launch the current batch
             if batch:
+                if enable_profiling:
+                    is_prof_stage = False
+                    if (
+                            profiling_stage == "decode" and batch.forward_mode.is_decode()
+                    ) or (
+                            profiling_stage == "prefill" and batch.forward_mode.is_extend()
+                    ):
+                        is_prof_stage = True
+
+                    if len(batch.reqs) >= prof_bs and prof_cnt == 0 and is_prof_stage:
+                        prof.start()
+                        prof_cnt += 1
+                    if prof_cnt > 0 and is_prof_stage:
+                        prof_cnt += 1
+                    if prof_cnt == prof_step and is_prof_stage:
+                        torch.npu.synchronize()
+                        prof.stop()
                 batch_result = self.run_batch(batch)
                 # Fence result processing behind this forward's shared reads.
                 self._apply_war_barrier()
                 self.result_queue.append((batch.copy(), batch_result))
+                if (
+                        enable_profiling
+                        and prof_cnt > 0
+                        and prof_cnt < prof_step
+                        and is_prof_stage
+                ):
+                    prof.step()
             else:
                 batch_result = None
                 self._sched_idled = True
---
+++ run_32p_mix_dspark.sh
@@ -0,0 +1,339 @@
+#!/bin/bash
+
+echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+sysctl -w vm.swappiness=10
+sysctl -w kernel.numa_balancing=0
+sysctl -w kernel.sched_migration_cost_ns=50000
+
+MODEL_PATH=/home/weights/Kimi-K3-w4a8-int-moe
+DRAFT_MODEL_PATH=/home/weights/Kimi-K3-DSpark
+
+unset https_proxy
+unset http_proxy
+unset HTTPS_PROXY
+unset HTTP_PROXY
+
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+source /usr/local/Ascend/nnal/atb/set_env.sh
+
+export SGLANG_NPU_PROFILING_BS=4
+# export SGLANG_NPU_PROFILING=1
+export SGLANG_NPU_PROFILING_PATH="/home/hanwlax/workspace/progress/kimi_k3/profiling"
+SGLANG_REPO_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
+export PYTHONPATH="${SGLANG_REPO_DIR}/python:/home/hanwlax/test-codes/sgl-kernel-npu/python/sgl_kernel_npu:${PYTHONPATH:-}"
+# export PYTHONPATH="${SGLANG_REPO_DIR}/python:${PYTHONPATH:-}"
+
+D_IP=('192.168.25.209' '192.168.25.212' '192.168.25.216' '192.168.25.217')
+if [[ "${ENABLE_NPU_GRAPH:-1}" == "1" ]]; then
+    GRAPH_ARGS=(--cuda-graph-bs 2 4 8 16)
+else
+    GRAPH_ARGS=(--disable-cuda-graph)
+fi
+LOCAL_HOST1=`hostname -I|awk -F " " '{print$1}'`
+LOCAL_HOST2=`hostname -I|awk -F " " '{print$2}'`
+echo "${LOCAL_HOST1}"
+echo "${LOCAL_HOST2}"
+
+for i in "${!D_IP[@]}";
+do
+    if [[ "$LOCAL_HOST1" == "${D_IP[$i]}" || "$LOCAL_HOST2" == "${D_IP[$i]}" ]];
+    then
+        echo "Mixed -> ${D_IP[$i]}"
+
+        export SGLANG_SET_CPU_AFFINITY=1
+        export SGLANG_ONE_VISIBLE_DEVICE_PER_PROCESS=1
+        export SGLANG_NPU_USE_TRITON_PREFIX_KV_CACHE_STORE=1
+        export SGLANG_K3_SHARED_EXPERTS_ATTN_TP=1
+        export SGLANG_K3_DENSE_MLP_ATTN_TP=1
+        export SGLANG_ENABLE_OVERLAP_PLAN_STREAM=1
+        export SGLANG_ENABLE_SPEC_V2=1
+        export SGLANG_RAGGED_VERIFY_MODE=static
+        export SGLANG_DSPARK_FOLDED_PROPOSAL=0
+        export SGLANG_DSPARK_FOLDED_SAMPLING=0
+        export SGLANG_DSPARK_STACKED_CTX_KV=0
+        export SGLANG_DSPARK_EMBED_IN_GRAPH=0
+        export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
+        export HCCL_SOCKET_IFNAME=enp196s0f0
+        export GLOO_SOCKET_IFNAME=enp196s0f0
+        export STREAMS_PER_DEVICE=32
+        export DEEP_NORMAL_MODE_USE_INT8_QUANT=1
+        export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=128
+        export HCCL_BUFFSIZE=2000
+        export DEEPEP_NORMAL_LONG_SEQ_ROUND=64
+        export DEEPEP_NORMAL_LONG_SEQ_PER_ROUND_TOKENS=512
+        export HCCL_OP_EXPANSION_MODE=AIV
+        export HCCL_BUFFSIZE=200
+        export DEEPEP_HCCL_BUFFSIZE=1800
+
+        unset ASCEND_CUSTOM_OPP_PATH
+        unset SGLANG_NPU_FUSED_MOE_MODE
+        unset ENABLE_PROFILING
+        unset SGLANG_K3_TRACE_STATE_FILE
+        unset SGLANG_K3_TRACE_HIDDEN_FILE
+        unset ASCEND_RT_VISIBLE_DEVICES
+        unset ASCEND_LAUNCH_BLOCKING
+
+        sglang serve \
+            --model-loader-extra-config '{"enable_multithread_load": true}' \
+            --dist-init-addr 192.168.25.209:5000 --nnodes 4 --node-rank $i \
+            --model-path $MODEL_PATH \
+            --tokenizer-path $MODEL_PATH \
+            --trust-remote-code \
+            --attention-backend ascend \
+            --device npu \
+            --quantization modelslim \
+            --dtype bfloat16 \
+            --tp-size 64 \
+	          --enable-dp-attention --dp-size 4 --enable-dp-lm-head \
+            --mem-fraction-static 0.75 \
+            --max-mamba-cache-size 180 \
+            --chunked-prefill-size 16384 \
+            "${GRAPH_ARGS[@]}" \
+            --reasoning-parser kimi_k3 \
+            --max-running-requests 64 \
+            --host 0.0.0.0 \
+            --port 30000 \
+	          --moe-a2a-backend deepep \
+            --deepep-mode auto \
+            --speculative-algorithm DSPARK \
+            --speculative-draft-model-path "$DRAFT_MODEL_PATH" \
+            --speculative-dspark-block-size 7 \
+            --speculative-draft-attention-backend ascend \
+            --speculative-eagle-topk 1 \
+            --speculative-draft-model-quantization unquant \
+            --watchdog-timeout 9000  2>&1 | tee "logs/run_32p_mix_$(date +%Y-%m-%d_%H-%M-%S).log"
+        exit 1
+    fi
+done
+
+exit 1
+
+# spec options
+            --speculative-algorithm DSPARK \
+            --speculative-draft-model-path "$DRAFT_MODEL_PATH" \
+            --speculative-dspark-block-size 7 \
+            --speculative-draft-attention-backend ascend \
+            --speculative-eagle-topk 1 \
+            --speculative-draft-model-quantization unquant \
+
+python3 -m sglang.test.few_shot_gsm8k --num-questions 50 --num-shots 5 --host 0.0.0.0 --port 30000 --data-path /home/zkk/gsm8k.jsonl
+
+curl --location 'http://0.0.0.0:30000/flush_cache' --header 'Content-Type: application/json'
+python -m sglang.bench_serving \
+  --dataset-path /home/zkk/datasets/ShareGPT_V3_unfiltered_cleaned_split.json \
+  --dataset-name random \
+  --backend sglang \
+  --host 0.0.0.0 \
+  --port 30000 \
+  --max-concurrency 16 \
+  --random-input-len 8000 \
+  --random-output-len 1000 \
+  --num-prompts 16 \
+  --disable-ignore-eos \
+  --random-range-ratio 1 \
+  --warmup-request 0
+
+
+curl -s http://127.0.0.1:30000/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "/home/weights/Kimi-K3-w4a8-int-moe",
+    "messages": [{"role": "user", "content": "The capital of France is"}],
+    "max_tokens": 20,
+    "temperature": 0
+  }'
+
+# 8k_1k_bs1
+curl --location 'http://0.0.0.0:30000/flush_cache' --header 'Content-Type: application/json'
+python -m sglang.bench_serving \
+  --dataset-path /home/zkk/datasets/ShareGPT_V3_unfiltered_cleaned_split.json \
+  --dataset-name random \
+  --backend sglang \
+  --host 0.0.0.0 \
+  --port 30000 \
+  --max-concurrency 1 \
+  --random-input-len 8000 \
+  --random-output-len 1000 \
+  --num-prompts 1 \
+  --disable-ignore-eos \
+  --random-range-ratio 1 \
+  --flush-cache \
+  --extra-request-body '{"routed_dp_rank": 0}' \
+  --warmup-request 0 2>&1 | tee "logs/8k_1k_bs1_$(date +%Y-%m-%d_%H-%M-%S).log"
+
+# 8k_1k_bs32
+curl --location 'http://0.0.0.0:30000/flush_cache' --header 'Content-Type: application/json'
+python -m sglang.bench_serving \
+  --dataset-path /home/zkk/datasets/ShareGPT_V3_unfiltered_cleaned_split.json \
+  --dataset-name random \
+  --backend sglang \
+  --host 0.0.0.0 \
+  --port 30000 \
+  --max-concurrency 32 \
+  --random-input-len 8000 \
+  --random-output-len 1000 \
+  --num-prompts 32 \
+  --disable-ignore-eos \
+  --random-range-ratio 1 \
+  --warmup-request 0 \
+  --output-details \
+  --output-file /home/hanwlax/workspace/progress/kimi_k3/bench_serving_logs/itl_chunk4096_bs32.jsonl \
+  --flush-cache 2>&1 | tee "8k_1k_bs32_$(date +%Y-%m-%d_%H-%M-%S).log"
+
+  LOG_DIR=/home/hanwlax/workspace/progress/kimi_k3/bench_serving_logs
+
+# 128k_1k_bs1
+curl --location 'http://0.0.0.0:30000/flush_cache' --header 'Content-Type: application/json'
+python -m sglang.bench_serving \
+  --dataset-path /home/zkk/datasets/ShareGPT_V3_unfiltered_cleaned_split.json \
+  --dataset-name random \
+  --backend sglang \
+  --host 0.0.0.0 \
+  --port 30000 \
+  --max-concurrency 1 \
+  --random-input-len 128000 \
+  --random-output-len 1000 \
+  --num-prompts 1 \
+  --seed 42 \
+  --disable-ignore-eos \
+  --random-range-ratio 1 \
+  --warmup-request 0 \
+  --flush-cache \
+  --extra-request-body '{"routed_dp_rank": 0}' 2>&1 | tee "128k_1k_bs1_$(date +%Y-%m-%d_%H-%M-%S).log"
+
+# 128k_1k_99cache_bs1
+curl --location 'http://0.0.0.0:30000/flush_cache' --header 'Content-Type: application/json'
+python3 -m sglang.bench_serving \
+    --dataset-name generated-shared-prefix \
+    --backend sglang --host 192.168.25.209 \
+    --port 30000 \
+    --max-concurrency 1 \
+    --gsp-num-groups 1 \
+    --gsp-prompts-per-group 1 \
+    --gsp-system-prompt-len 127620 \
+    --gsp-question-len 0 \
+    --gsp-output-len 1 \
+    --warmup-requests 0 \
+    --seed 1 \
+    --extra-request-body '{"routed_dp_rank": 0}'
+
+python3 -m sglang.bench_serving \
+    --dataset-name generated-shared-prefix \
+    --backend sglang --host 192.168.25.209 \
+    --port 30000 \
+    --max-concurrency 1 \
+    --gsp-num-groups 1 \
+    --gsp-prompts-per-group 1 \
+    --gsp-system-prompt-len 127620 \
+    --gsp-question-len 1280 \
+    --gsp-output-len 1000 \
+    --warmup-requests 0 \
+    --seed 1 \
+    --extra-request-body '{"routed_dp_rank": 0}'
+
+# 128k_1k_99cache_bs4
+curl --location 'http://0.0.0.0:30000/flush_cache' --header 'Content-Type: application/json'
+python3 -m sglang.bench_serving \
+    --dataset-name generated-shared-prefix \
+    --backend sglang --host 192.168.25.209 \
+    --port 30000 \
+    --max-concurrency 1 \
+    --gsp-num-groups 1 \
+    --gsp-prompts-per-group 4 \
+    --gsp-system-prompt-len 127620 \
+    --gsp-question-len 0 \
+    --gsp-output-len 1 \
+    --warmup-requests 0 \
+    --seed 1 \
+    --extra-request-body '{"routed_dp_rank": 0}'
+
+python3 -m sglang.bench_serving \
+    --dataset-name generated-shared-prefix \
+    --backend sglang --host 192.168.25.209 \
+    --port 30000 \
+    --max-concurrency 1 \
+    --gsp-num-groups 1 \
+    --gsp-prompts-per-group 4 \
+    --gsp-system-prompt-len 127620 \
+    --gsp-question-len 1280 \
+    --gsp-output-len 1000 \
+    --warmup-requests 0 \
+    --seed 1 \
+    --extra-request-body '{"routed_dp_rank": 0}'
+
+evalscope eval \
+    --model /home/weights/Kimi-K3-w4a8-int-moe \
+    --api-url http://127.0.0.1:30000/v1 \
+    --api-key EMPTY \
+    --work-dir "/home/hanwlax/workspace/progress/kimi_k3/gpqa/result_$(date +%Y-%m-%d_%H-%M-%S)" \
+    --no-timestamp \
+    --eval-type openai_api \
+    --datasets gpqa_diamond \
+    --dataset-args '{
+      "gpqa_diamond": {
+        "local_path": "/home/hanwlax/datasets/gpqa",
+        "subset_list": ["gpqa_diamond"],
+        "default_subset": "gpqa_diamond"
+      }
+    }' \
+    --generation-config '{
+      "max_tokens": 131072,
+      "timeout": 10000,
+      "temperature": 1.0,
+      "top_p": 0.95,
+      "extra_body": {
+        "reasoning_effort": "max"
+      }
+    }' \
+    --eval-batch-size 32 \
+    --seed 42 2>&1 | tee "gpqa_$(date +%Y-%m-%d_%H-%M-%S).log"
+
+#hot 2
+curl --location 'http://127.0.0.1:30000/flush_cache' --header 'Content-Type: application/json'
+python -m sglang.bench_serving \
+  --dataset-path /home/hanwlax/datasets/shareGPT/sharegpt_natural_shared_128k_32.json \
+  --dataset-name random \
+  --backend sglang \
+  --host 127.0.0.1 \
+  --port 30000 \
+  --max-concurrency 4 \
+  --random-input-len 126720 \
+  --random-output-len 1 \
+  --num-prompts 4 \
+  --random-range-ratio 1 \
+  --seed 42 \
+  --warmup-requests 0
+
+ts=$(date +%Y-%m-%d_%H-%M-%S)
+
+python -m sglang.bench_serving \
+  --dataset-path /home/hanwlax/datasets/shareGPT/sharegpt_natural_shared_128k_32.json \
+  --dataset-name random \
+  --backend sglang \
+  --host 127.0.0.1 \
+  --port 30000 \
+  --max-concurrency 32 \
+  --random-input-len 128000 \
+  --random-output-len 1000 \
+  --num-prompts 32 \
+  --random-range-ratio 1 \
+  --seed 42 \
+  --warmup-requests 0 \
+  --cache-report \
+  --output-details \
+  --output-file "hit_cache_${ts}.jsonl" \
+  2>&1 | tee "hit_cache_${ts}.log"
+
+# profiling
+curl --location 'http://127.0.0.1:30000/start_profile' \
+  --header 'Content-Type: application/json' \
+  --data '{
+    "num_steps": 10,
+    "profile_by_stage": true,
+    "profile_stages": ["decode"],
+    "with_stack": false
+  }'
+
+curl --location 'http://127.0.0.1:30000/stop_profile'
+
+python3 -m sglang.test.few_shot_gsm8k --num-questions 50 --num-shots 5 --host 0.0.0.0 --port 30000 --data-path /home/zkk/gsm8k.jsonl
