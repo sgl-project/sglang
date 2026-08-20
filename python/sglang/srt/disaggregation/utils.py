daa3124c8c9c66b9a3cfd5c6ecf17961df66a1c6
chenxu214 <justin_cc2025@163.com>
2026-08-20 23:13:09 +0800

    Revert "bug fix pp + pd (#35589)"
    This reverts commit c1a740b645eaed60dc4543a17e423fd1a7210d64.

--- python/sglang/srt/disaggregation/utils.py
+++ python/sglang/srt/disaggregation/utils.py
@@ -1167,15 +1167,7 @@ def setup_state_kv_args(
                 item_lens = item_lens + draft_item_lens
             if isinstance(token_to_kv_pool, NPUMLATokenToKVPool):
                 kv_args.kv_buf_groups = (
-                    len(kv_args.kv_data_ptrs)
-                    // (
-                        token_to_kv_pool.layer_num
-                        + (
-                            draft_token_to_kv_pool.layer_num
-                            if draft_token_to_kv_pool
-                            else 0
-                        )
-                    )
+                    len(kv_args.kv_data_ptrs) // token_to_kv_pool.layer_num
                 )
                 kv_args.hidden_kv_layers = total_kv_layers
                 kv_args.draft_kv_layers = (
