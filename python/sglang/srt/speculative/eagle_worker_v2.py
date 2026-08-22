3bce3a0caa5eb85c6a92f43d5517d32d0159990b
Jiajun Li <guapisolo@gmail.com>
2026-08-22 12:45:22 -0700

    [27/35] [sglang-miles] Fix iter_runners reaching the draft runner on the wrong object
    (cherry picked from commit 1b2df159e182d88ea2a499e003b943aed9299b38)

--- python/sglang/srt/speculative/eagle_worker_v2.py
+++ python/sglang/srt/speculative/eagle_worker_v2.py
@@ -1062,7 +1062,7 @@ class EagleDraftWorker(EagleDraftWorkerBase):
 
 class EAGLEWorkerV2(BaseSpecWorker):
     def iter_runners(self) -> List[Tuple[str, "ModelRunner"]]:
-        return [("draft", self.draft_runner)]
+        return [("draft", self.draft_worker.draft_runner)]
 
     def __init__(
         self,
