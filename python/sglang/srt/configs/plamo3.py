a01d33ace2ff581418d5ee3155260017767e8695
Shinichi Hemmi <shemmi@preferred.jp>
2026-08-24 09:49:39 +0900

    [Model] Clean up PLaMo3 file headers

--- python/sglang/srt/configs/plamo3.py
+++ python/sglang/srt/configs/plamo3.py
@@ -1,24 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
-# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-# Copyright 2025 SGLang Team
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """PLaMo3 model configuration."""
 
+import logging
 from typing import Any, Optional
 
 from transformers.configuration_utils import PretrainedConfig
-from transformers.utils import logging
 
-logger = logging.get_logger(__name__)
+logger = logging.getLogger(__name__)
 
 
 def is_full_attn(sliding_window_pattern: int, layer_idx: int) -> bool:
