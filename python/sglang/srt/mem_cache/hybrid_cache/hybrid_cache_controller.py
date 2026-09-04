bc7b256043b226a920ace83650ee7f0b6b7109a4
augusto.yjh <augusto.yjh@antgroup.com>
2026-09-04 16:35:41 +0800

    fix: code format style

--- python/sglang/srt/mem_cache/hybrid_cache/hybrid_cache_controller.py
+++ python/sglang/srt/mem_cache/hybrid_cache/hybrid_cache_controller.py
@@ -606,9 +606,7 @@ class HybridCacheController(BaseHiCacheController):
             return hash_value, kv_hit_pages * self.page_size
 
         extra_info = HiCacheStorageExtraInfo(
-            prefix_keys=operation.prefix_keys.copy()
-            if operation.prefix_keys
-            else None,
+            prefix_keys=operation.prefix_keys.copy() if operation.prefix_keys else None,
             extra_info={"request_id": operation.request_id},
         )
         if operation.pool_transfers:
