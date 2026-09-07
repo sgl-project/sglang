e1d3ff2a3086f110380c344ac8b6d1e601ed8472
ZailiWang <zaili.wang@intel.com>
2026-09-07 11:53:40 +0800

    lint

--- python/sglang/srt/model_executor/cpu_graph_runner.py
+++ python/sglang/srt/model_executor/cpu_graph_runner.py
@@ -729,7 +729,9 @@ class CPUGraphRunner:
         )
         for bs in capture_range:
             if get_parallel().tp_rank == 0:
-                avail_mem = (get_available_cpu_memory() - psutil.virtual_memory().used) / (1 << 30)
+                avail_mem = (
+                    get_available_cpu_memory() - psutil.virtual_memory().used
+                ) / (1 << 30)
                 capture_range.set_description(
                     f"Capturing batches ({bs=} {avail_mem=:.2f} GB)"
                 )
