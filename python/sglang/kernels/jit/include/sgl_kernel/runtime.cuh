3978fa40e8b3e8366cfcf01cf6456023b63c5e45
AMD-yanfeiwang <yanfei.wang@amd.com>
2026-09-11 10:16:01 +0000

    [HiCache] Simplify JIT host pointer resolution

--- python/sglang/kernels/jit/include/sgl_kernel/runtime.cuh
+++ python/sglang/kernels/jit/include/sgl_kernel/runtime.cuh
@@ -50,43 +50,7 @@ namespace sglang {
 
 namespace host::runtime {
 
-class DeviceGuard {
- public:
-  explicit DeviceGuard(int target_device) {
-#ifdef USE_ROCM
-    RuntimeDeviceCheck(::hipGetDevice(&original_device_));
-    if (original_device_ != target_device) {
-      RuntimeDeviceCheck(::hipSetDevice(target_device));
-      changed_ = true;
-    }
-#else
-    RuntimeDeviceCheck(::cudaGetDevice(&original_device_));
-    if (original_device_ != target_device) {
-      RuntimeDeviceCheck(::cudaSetDevice(target_device));
-      changed_ = true;
-    }
-#endif
-  }
-
-  ~DeviceGuard() {
-    if (changed_) {
-#ifdef USE_ROCM
-      (void)::hipSetDevice(original_device_);
-#else
-      (void)::cudaSetDevice(original_device_);
-#endif
-    }
-  }
-
-  DeviceGuard(const DeviceGuard&) = delete;
-  DeviceGuard& operator=(const DeviceGuard&) = delete;
-
- private:
-  int original_device_ = 0;
-  bool changed_ = false;
-};
-
-inline void* device_accessible_ptr(const tvm::ffi::TensorView& tensor) {
+inline void* get_device_accessible_ptr(const tvm::ffi::TensorView& tensor) {
   void* ptr = tensor.data_ptr();
   const auto tensor_device_type = tensor.device().device_type;
   if (tensor_device_type != kDLCPU && tensor_device_type != kDLGPUHost) {
