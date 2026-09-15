b1121d85d40adae06e3d5b221a2ca39429800d88
chenxu214 <justin_cc2025@163.com>
2026-09-15 21:24:49 +0800

    zero bubble

--- python/sglang/srt/hardware_backend/npu/attention/ascend_hybrid_linear_attn_backend.py
+++ python/sglang/srt/hardware_backend/npu/attention/ascend_hybrid_linear_attn_backend.py
@@ -27,6 +27,7 @@ class AscendMambaAttnBackendBase(MambaAttnBackendBase):
     def __init__(self, model_runner: ModelRunner):
         super().__init__(model_runner)
         self.state_indices_list_gdn = []
+        self.needs_cpu_seq_lens = False
 
     def init_cuda_graph_state(self, max_bs: int, max_num_tokens: int):
         assert (
