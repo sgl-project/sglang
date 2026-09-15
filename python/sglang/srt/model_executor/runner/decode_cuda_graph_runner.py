93efde6e35f4f85db1ae4465cc8c640bfc0f2f82
hnyls2002 <lsyincs@gmail.com>
2026-09-15 01:20:51 -0700

    drop dead ngram attrs

--- python/sglang/srt/model_executor/runner/decode_cuda_graph_runner.py
+++ python/sglang/srt/model_executor/runner/decode_cuda_graph_runner.py
@@ -252,10 +252,6 @@ class DecodeCudaGraphRunner(BaseCudaGraphRunner):
         )
         self.enable_two_batch_overlap = get_exec().overlap.enable_two_batch_overlap
         self.use_ngram_embedding = model_runner.ngram_embedding_manager.enabled
-        if self.use_ngram_embedding:
-            hf_config = model_runner.model_config.hf_config
-            self.ngram_embedding_n = hf_config.ngram_embedding_n
-            self.ngram_embedding_k = hf_config.ngram_embedding_k
         self.speculative_algorithm = get_spec().speculative_algorithm
         self.enable_profile_cuda_graph = get_exec().graph.enable_profile_cuda_graph
 
