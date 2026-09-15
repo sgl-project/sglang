740e21ba75399b6f38e412fb4ce27ade3801a770
YZY00Raiser <2452658175@qq.com>
2026-09-15 19:22:34 +0800

    update config

--- test/registered/npu/accuracy/deepseek_v4_flash/test_npu_deepseek_v4_flash_w8a8_8p_gpqa.py
+++ test/registered/npu/accuracy/deepseek_v4_flash/test_npu_deepseek_v4_flash_w8a8_8p_gpqa.py
@@ -18,11 +18,11 @@ register_npu_ci(
     nightly=True,
 )
 
-register_npu_ci(
-    est_time=7200,
-    suite="nightly-acc-16-npu-a3-cann910",
-    nightly=True,
-)
+# register_npu_ci(
+#     est_time=7200,
+#     suite="nightly-acc-16-npu-a3-cann910",
+#     nightly=True,
+# )
 
 DEEPSEEK_V4_FLASH_W8A8_DSPARK_8P_ENVS = {
     "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
