929a91b65a69ff9c8b197f293c6d28ad9e6d9895
zhaozx-cn <zhaozx2116@163.com>
2026-09-16 06:05:51 +0000

    remove unused code
    Signed-off-by: zhaozx-cn <zhaozx2116@163.com>

--- python/sglang/srt/layers/moe/token_dispatcher/deepep.py
+++ python/sglang/srt/layers/moe/token_dispatcher/deepep.py
@@ -454,10 +454,6 @@ class _DeepEPDispatcherImplBase:
                 "use_fp8": True,
                 "use_nvfp4": False,
             },
-            DispatcherOutputDtype.MXFP8: {
-                "use_fp8": False,
-                "use_nvfp4": False,
-            },
             DispatcherOutputDtype.NVFP4: {
                 "use_fp8": False,
                 "use_nvfp4": True,
