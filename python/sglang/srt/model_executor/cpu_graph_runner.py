e28abf6f10cb8998573aa7020fe9766006c25836
ZailiWang <zaili.wang@intel.com>
2026-09-18 11:47:03 +0800

    funcimport

--- python/sglang/srt/model_executor/cpu_graph_runner.py
+++ python/sglang/srt/model_executor/cpu_graph_runner.py
@@ -48,6 +48,7 @@ from sglang.srt.runtime_context import (
 )
 from sglang.srt.utils import (
     empty_context,
+    get_availabl_cpu_memory,
     log_info_on_rank0,
     require_attn_tp_gather,
     require_gathered_buffer,
