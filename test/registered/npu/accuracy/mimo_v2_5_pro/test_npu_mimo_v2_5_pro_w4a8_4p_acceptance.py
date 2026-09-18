da3e43b8c839cf54d07a44609ccff80143810cda
HeYao <heyao@example.com>
2026-09-18 16:49:44 +0800

    Add test_npu_mimo_v2_5_pro_w4a8_8p_hle.py

--- test/registered/npu/accuracy/mimo_v2_5_pro/test_npu_mimo_v2_5_pro_w4a8_4p_acceptance.py
+++ test/registered/npu/accuracy/mimo_v2_5_pro/test_npu_mimo_v2_5_pro_w4a8_4p_acceptance.py
@@ -28,6 +28,8 @@ MIMO_V2_5_PRO_FP4_MODEL_PATH = (
 MIMO_V2_5_PRO_DFLASH_MODEL_PATH = (
     "/root/.cache/modelscope/hub/models/XiaomiMiMo/MiMo-V2.5-Pro-FP4-DFlash/dflash"
 )
+SHAREGPT_DATASET_PATH = "/tmp/ShareGPT_V3_unfiltered_cleaned_split.json"
+
 MIMO_V2_5_PRO_FP4_4P_ENVS = {
     "SGLANG_SET_CPU_AFFINITY": "1",
     "HCCL_BUFFSIZE": "300",
@@ -135,8 +137,10 @@ class TestNPUMiMoV2_5_Pro_W4A8_4P_Acceptance(CustomTestCase):
             str(parsed.port),
             "--model",
             self.model_path,
+            "--dataset-path",
+            SHAREGPT_DATASET_PATH,
             "--dataset-name",
-            "random-ids",
+            "random",
             "--tokenize-prompt",
             "--random-input-len",
             str(RANDOM_INPUT_LEN),
