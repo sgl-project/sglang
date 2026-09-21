8825b64ab97d0e9dd25b009cca110c091d2dcc4e
gengjinsong <gjsheu@163.com>
2026-09-21 15:10:04 +0800

    cleancode

--- python/sglang/srt/layers/rotary_embedding/mrope.py
+++ python/sglang/srt/layers/rotary_embedding/mrope.py
@@ -289,13 +289,14 @@ class MRotaryEmbedding(RotaryEmbedding):
             return self.forward_native(positions, query, key, fused_set_kv_buffer_arg)
 
         rotary_mode = "half" if self.is_neox_style else "interleave"
+        mrope_section = [0, 0, 0]
         query_out, key_out = torch_npu.npu_mrope(
             positions,
             query,
             key,
             self.cos_sin_cache,
             self.head_size,
-            mrope_section=[0, 0, 0],
+            mrope_section=mrope_section,
             rotary_mode=rotary_mode,
         )
         return query_out, key_out
